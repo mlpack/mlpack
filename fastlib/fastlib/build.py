1ed67604b94045d5c80ed66df9362893c834d92d
Garry Boyer <opensource@garryb.com>
2007-03-14 17:42:50 +0000

    hi

--- fastlib/fastlib/build.py
+++ fastlib/fastlib/build.py
@@ -12,7 +12,7 @@ librule(
     name = "fastlib_int",
     headers = ["fastlib.h"],
     deplibs = ["la:la", "base:base",
-             "fx:fx", "file:file", "col:col",
+             "fx:fx", "file:file_int", "col:col",
              "data:data", "math:math"
               "tree:tree", "par:par"
              ]
