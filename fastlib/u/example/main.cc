5d903d953f26970c58ee7db416b47c422a0a878c
Garry Boyer <opensource@garryb.com>
2007-03-28 19:15:41 +0000

    hi

--- fastlib/u/example/main.cc
+++ fastlib/u/example/main.cc
@@ -15,14 +15,14 @@ int main(int argc, char *argv[]) {
   int n_labels = fx_param_int(NULL, "n_labels", 0);
 
   Dataset dataset;
-  
+
   if (!PASSED(dataset.InitFromFile(data))) {
     fprintf(stderr, "main: Couldn't open file '%s'.\n", data);
     return 1;
   }
-  
+
   SimpleCrossValidator<KnnClassifier> cross_validator;
-  
+
   cross_validator.Init(&dataset, n_labels, 10, fx_root, "knn");
   
   cross_validator.Run();
