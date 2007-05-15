d2e21b6b27d4731a1440d7a4745ee2029afc8f1d
vasiloglou <nvasil@ismion.com>
2007-05-15 19:29:59 +0000

    Fixed build file

--- fastlib/u/nvasil/mmanager_with_tpie/build.py
+++ fastlib/u/nvasil/mmanager_with_tpie/build.py
@@ -1,14 +1,10 @@
 librule(name="tpiemm",
-		    headers=["memory_manager.h"],
-				deplibs=["u/nvasil/tpie:tpie", "libsigsegv.a"]
+		    headers=lglob("*.h"),
+				deplibs=["fastlib: fastlib u/nvasil/tpie:tpie", "libsigsegv.a"]
 		);
 binrule(
 		name="test",
 		sources=["memory_manager_unit.cc"],
 		headers=lglob("*.h"),
-		#cflags="-I../tpie -lsigsegv",
-		#linkables=["fastlib:fastlib",
-		#					 "u/nvasil/tpie:tpie",
-		#					 "libsigsegv.a"]
-		linkables=["fastlib:fastlib", ":tpiemm"]
+		linkables=[":tpiemm"]
 		);
