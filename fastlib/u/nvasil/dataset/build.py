69a8060f2f24cbdac8bece1dbede7632bd209612
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2007-05-15 19:17:59 +0000

    Improved build file

--- fastlib/u/nvasil/dataset/build.py
+++ fastlib/u/nvasil/dataset/build.py
@@ -1,4 +1,8 @@
 librule(name="bindataset",
 		    headers=lglob("*.h"),
-				deplibs=["faastlib:fastlibi","u/nvasil/loki:loki" ]
+				deplibs=["faastlib:fastlib","u/nvasil/loki:loki" ]
 		)
+binrule(name="test",
+		    sources=["binary_dataset_unit.cc"],
+				linkables=[":bindataset"]
+				)
