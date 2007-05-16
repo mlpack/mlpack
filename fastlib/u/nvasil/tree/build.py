91c5e47e17021b75326c622fd53387a907ea565f
vasiloglou <nvasil@ismion.com>
2007-05-16 00:05:31 +0000

    Making it compatible with both memory managers

--- fastlib/u/nvasil/tree/build.py
+++ fastlib/u/nvasil/tree/build.py
@@ -1,33 +1,29 @@
 librule(
-  	name="common",
-		headers=["../mmanager/memory_manager.h"]+
-		        lglob("../loki/*.h")+
-						["point.h", "computations_counter.h",
-						 "euclidean_metric.h", "null_statistics.h",
-						 "point_identity_discriminator.h"],
-						 deplibs=["u/nvasil/mmanager_with_tpie:tpiemm"]
+  	name="binarytree",
+		headers=lglob("*.h"),
+		deplibs=["fastlib:fastlib",
+		         "u/nvasil/loki:loki"
+						 ]
 		);
+
+librule(name="fortests",
+		deplibs=["u/nvasil/dataset:bindataset",
+		         "u/nvasil/mmanager:mmapmm",
+						 "u/nvasil/mmanager_with_tpie:tpiemm"])
 binrule(
 		name="hrect_test",
 		sources=["hyper_rectangle_unit.cc"],
-		headers=["hyper_rectangle.h", "hyper_rectangle_impl.h"],
-		linkables=["fastlib:fastlib", ":common"],	
+		linkables=[":binarytree", ":fortests"],	
 		);
 binrule(
 		name="node_test",
 		sources=["node_unit.cc"],
-		headers=["./node_impl.h", "./node.h", "../dataset/*.h",
-		         "hyper_rectangle.h", "hyper_rectangle_impl.h"],
-						 linkables=["fastlib:fastlib", ":common"]
+		linkables=[":binarytree", ":fortests"]
 		);
 binrule(
 		name="tree_test",
 		sources=["binary_tree_unit.cc"],
-		headers=["./node_impl.h", "./node.h", "../dataset/*.h",
-		         "hyper_rectangle.h", "hyper_rectangle_impl.h",
-						 "binary_tree.h", "binary_tree_impl.h", 
-						 "kd_pivoter1.h"],
-						 linkables=["fastlib:fastlib", ":common"]
+		linkables=[":binarytree", ":fortests"]
 		);
 
 
