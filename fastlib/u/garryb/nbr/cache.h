9f923ceaa1679a5c71463cd0266134588cdd60ec
Garry Boyer <opensource@garryb.com>
2007-05-19 23:19:07 +0000

    hi

--- fastlib/u/garryb/nbr/cache.h
+++ fastlib/u/garryb/nbr/cache.h
@@ -13,39 +13,38 @@ class SmallCache : public BlockDeviceWrapper {
 
  private:
   struct Metadata {
-    Metadata() : data(NULL), is_dirty(0) {}
+    Metadata() : data(NULL), lock_count(0) {}
     /** current version of the data, can be NULL */
     char *data;
-    char lock_count;
     char is_dirty;
+    char lock_count;
   };
 
  private:
   Mutex mutex_;
   ArrayList<Metadata> metadata_;
   BlockActionHandler *handler_;
+  mode_t mode_;
 
  public:
   virtual ~SmallCache();
+  
+  mode_t mode() const {
+    return mode_;
+  }
 
+  void Init(BlockDevice *inner_in, BlockActionHandler *handler_in);
+  
   char *StartRead(blockid_t blockid);
   char *StartWrite(blockid_t blockid);
   void StopRead(blockid_t blockid);
   void StopWrite(blockid_t blockid);
 
-  void Init(BlockDevice *inner_in, BlockActionHandler *handler_in) {
-    BlockDeviceWrapper::Init(inner_in);
-    handler_ = handler_in;
-  }
-
-  virtual void Read(blockid_t blockid, char *data) {
-    abort();
-  }
-
-  virtual void Write(blockid_t blockid, const char *data) {
-    abort();
-  }
-
+  virtual void Read(blockid_t blockid,
+      offset_t begin, offset_t end, char *data);
+  virtual void Write(blockid_t blockid,
+      offset_t begin, offset_t end, const char *data);
+  
   virtual blockid_t AllocBlock() {
     blockid_t blockid = BlockDeviceWrapper::AllocBlock();
     metadata_.Resize(n_blocks());
@@ -54,6 +53,7 @@ class SmallCache : public BlockDeviceWrapper {
 
  private:
   void PerformCacheMiss_(blockid_t blockid);
+  void Writeback_(blockid_block, offset_t begin, offset_t end);
 
   Metadata *GetBlock_(blockid_t blockid) {
     Metadata *metadata = &metadata_[blockid];
@@ -67,66 +67,12 @@ class SmallCache : public BlockDeviceWrapper {
   }
 };
 
-SmallCache::~SmallCache() {
-  for (index_t i = 0; i < metadata_.size(); i++) {
-    Metadata *metadata = &metadata_[i];
-    mem::Free(metadata->data);
-    DEBUG_ASSERT(metadata_[i].lock_count == 0);
-    DEBUG_POISON_PTR(metadata->data);
-  }
-}
-
-char *SmallCache::StartRead(blockid_t blockid) {
-  Lock();
-
-  Metadata *metadata = GetBlock_(blockid);
-  metadata->lock_count++;
-
-  Unlock();
-
-  return metadata->data;
-}
-
-char *SmallCache::StartWrite(blockid_t blockid) {
-  Lock();
-
-  Metadata *metadata = GetBlock_(blockid);
-  metadata->lock_count++;
-  metadata->is_dirty = 1;
-
-  Unlock();
-
-  return metadata->data;
-}
-
-void SmallCache::StopRead(blockid_t blockid) {
-  Lock();
-  Metadata *metadata = GetBlock_(blockid);
-  --metadata->lock_count;
-  Unlock();
-}
-
-void SmallCache::StopWrite(blockid_t blockid) {
-  StopRead(blockid);
-}
-
-void SmallCache::PerformCacheMiss_(blockid_t blockid) {
-  char *data;
-  Metadata *metadata = &metadata_[blockid];
-
-  data = mem::Alloc<char>(n_block_bytes());
-  inner_->Read(blockid, data);
-  handler_->BlockThaw(data);
-  metadata->data = data;
-}
-
 template<typename T>
 class CacheArrayBlockActionHandler : public BlockActionHandler {
   FORBID_COPY(CacheArrayBlockActionHandler);
 
  private:
   char *default_elem_;
-  index_t n_block_elems_;
   size_t n_elem_bytes_;
 
  public:
@@ -141,29 +87,31 @@ class CacheArrayBlockActionHandler : public BlockActionHandler {
     DEBUG_POISON_PTR(default_elem_);
   }
 
-  void Init(index_t n_block_elems_in, const T& default_obj) {
-    n_block_elems_ = n_block_elems_in;
+  void Init(const T& default_obj) {
     n_elem_bytes_ = ot::PointerFrozenSize(default_obj);
     default_elem_ = mem::Alloc<char>(n_elem_bytes_);
     ot::PointerFreeze(default_obj, default_elem_);
   }
 
-  void BlockInit(char *block) {
-    for (index_t i = 0; i < n_block_elems_; i++) {
+  void BlockInit(size_t bytes, char *block) {
+    index_t elems = bytes / n_elem_bytes;
+    for (index_t i = 0; i < elems; i++) {
       mem::CopyBytes(block, default_elem_, n_elem_bytes_);
       block += n_elem_bytes_;
     }
   }
 
-  void BlockRefreeze(char *block) {
-    for (index_t i = 0; i < n_block_elems_; i++) {
-      ot::PointerRefreeze(reinterpret_cast<T*>(block));
+  void BlockRefreeze(size_t bytes, const char *old_location, char *block) {
+    index_t elems = bytes / n_elem_bytes;
+    for (index_t i = 0; i < elems; i++) {
+      ot::PointerRefreeze(reinterpret_cast<const T*>(old_location), dest);
       block += n_elem_bytes_;
     }
   }
 
-  void BlockThaw(char *block) {
-    for (index_t i = 0; i < n_block_elems_; i++) {
+  void BlockThaw(size_t bytes, char *block) {
+    index_t elems = bytes / n_elem_bytes;
+    for (index_t i = 0; i < elems; i++) {
       ot::PointerThaw<T>(block);
       block += n_elem_bytes_;
     }
@@ -176,22 +124,7 @@ class CacheArray {
 
  public:
   typedef T Element;
-
-  enum Mode {
-    /**
-     * Only reading can be performed.
-     */
-    MODE_READ,
-    /**
-     * Writing can be performed, but it is not important if the data is
-     * written back.
-     */
-    MODE_TEMP,
-    /**
-     * Writing can be performed, and the data should be written back.
-     */
-    MODE_WRITE
-  };
+  typedef PointerType<T, AllowWrite>::Type;
 
  private:
   struct Metadata {
@@ -205,11 +138,11 @@ class CacheArray {
 
  private:
   SmallCache *cache_;
-  Mode mode_;
   index_t begin_;
   index_t end_;
   index_t begin_block_;
   index_t end_block_;
+  mode_t mode_;
   unsigned int n_block_elems_;
   unsigned int n_elem_bytes_;
   ArrayList<Metadata> metadata_;
@@ -229,12 +162,11 @@ class CacheArray {
     n_elem_bytes_ = n_elem_bytes_in;
     
     begin_block_ = begin_ / n_block_elems_;
-    end_block_ = ((end_ + n_block_elems_ - 1) / n_block_elems_) + 1;
+    last_block_ = (end_ + n_block_elems_ - 1) / n_block_elems_;
     
     metadata_.Init(end_block_ - begin_block_);
-
-    DEBUG_ASSERT(mode_ == MODE_READ
-        || mode_ == MODE_TEMP || mode_ == MODE_WRITE);
+    
+    mode_ = cache_in->mode();
   }
 
   index_t begin_index() const {
@@ -245,16 +177,15 @@ class CacheArray {
   }
 
   const Element *StartRead(index_t element_id) {
-    DEBUG_ONLY(BoundsCheck_(element_id));
-    return CheckoutElement_(element_id);
-  }
-
-  void StopRead(const Element *ptr, index_t element_id) {
-    DEBUG_ONLY(BoundsCheck_(element_id));
     return CheckoutElement_(element_id);
   }
 
   Element *StartWrite(index_t element_id) {
+    DBUG_ASSERT(mode_ != BlockDevice::READ);
+    return CheckoutElement_(element_id);
+  }
+
+  void StopRead(const Element *ptr, index_t element_id) {
     DEBUG_ONLY(BoundsCheck_(element_id));
     DEBUG_ASSERT(mode_ >= MODE_TEMP);
     ReleaseElement_(element_id);
@@ -286,7 +217,7 @@ class CacheArray {
     BlockDevice::blockid_t blockid = element_id / n_block_elems_;
     BlockDevice::offset_t offset =
         uint(element_id % n_block_elems_) * n_elem_bytes_;
-  
+
     Metadata *metadata = &metadata_[blockid - begin_block_];
     DEBUG_ONLY(metadata_->lock_count++);
     Element *ptr = reinterpret_cast<Element*>(metadata_->data + offset);
@@ -297,7 +228,7 @@ class CacheArray {
       return ptr;
     }
   }
-
+  
   void ReleaseElement_(index_t element_id) {
     DEBUG_ONLY(BoundsCheck_(element_id));
     DEBUG_ONLY(--metadata_[element_id / n_block_elems_ - begin_block_].lock_count);
@@ -306,24 +237,9 @@ class CacheArray {
 
 template<typename T>
 void CacheArray<T>::Flush() {
-  BlockDev::blockid_t block_count = end_block_ - begin_block_;
-  
-  for (BlockDev::blockid_t i = 0; i < block_count; i++) {
-    Metadata *metadata = &metadata_[i];
-    
-    DEBUG_ASSERT(metadata->lock_count == 0);
-    
-    if (unlikely(metadata->data != NULL)) {
-      index_t blockid = i + begin_block_;
-      
-      if (mode_ == MODE_WRITE) {
-        cache_->StopWrite(blockid);
-      } else {
-        cache_->StopRead(blockid);
-      }
-      metadata->data = NULL;
-    }
-  }
+  cache_->Flush(
+      begin_ / n_block_elems_, (begin_ % n_block_elems_) * n_elem_bytes_,
+      end_ / n_block_elems_, (end_ % n_block_elems_) * n_elem_bytes_);
 }
 
 template<typename T>
@@ -332,7 +248,7 @@ typename CacheArray<T>::Element* CacheArray<T>::HandleCacheMiss_(
   BlockDevice::blockid_t blockid = element_id / n_block_elems_;
   Metadata *metadata = &metadata_[blockid];
   
-  if (mode_ == MODE_WRITE) {
+  if (mode_ != BlockDevice::READ) {
     metadata->data = cache_->StartWrite(blockid);
   } else {
     metadata->data = cache_->StartRead(blockid);
