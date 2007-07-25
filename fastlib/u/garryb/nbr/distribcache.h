1a26248f7700d4c5a84a0646be5d0df259d79bb7
Garry Boyer <opensource@garryb.com>
2007-07-25 05:38:03 +0000

    hi

--- fastlib/u/garryb/nbr/distribcache.h
+++ fastlib/u/garryb/nbr/distribcache.h
@@ -54,9 +54,28 @@ class DistributedCache : public BlockDevice {
   };
 
  public:
-  enum { NOT_DIRTY = -1, FULLY_DIRTY = -2 };
+  enum {
+    /** The entire block is dirty. */
+    FULLY_DIRTY = -1,
+    /** Block is not dirty, or end of a dirty-list. */
+    NOT_DIRTY_OLD = -2,
+    /** Block is new. */
+    NOT_DIRTY_NEW = -3
+  };
 
-  /** Information we keep about the block. */
+  /**
+   * Information we keep about the block.
+   *
+   * I had to jump through lots of hoops to make this 16 bytes on
+   * 64-bit systems.  Don't make me sad by adding anything new to this!
+   *
+   * See the getters (i.e. is_owner etc) to figure out the actual parameters
+   * behave with respect to each other.  Some have much different semantics
+   * depending on whether the block is in.
+   *
+   * The locks variable could still be used for something when the block is
+   * out of core.
+   */
   struct BlockMetadata {
     BlockMetadata() {}
     ~BlockMetadata() {
@@ -65,14 +84,49 @@ class DistributedCache : public BlockDevice {
       }
     }
 
+    /** Pointer to data, or NULL if not in RAM. */
     char *data;
-    short dirty_ranges;
-    uint8 is_owner;
-    uint8 is_new;
-    int32 value; // either block's owner or local mapping
+    /**
+     * If negative, the binary complement of who owns it; otherwise,
+     * the block ID number.  Note we're using binary complement since
+     * unlike negation, ~0 is not 0.
+     */
+    int32 value;
+    /** Linked list of ranges that we've written. */
+    int16 dirty_ranges;
+    /** Number of locks, or negative if block is new. */
+    int16 locks;
 
-    int owner(struct DistributedCache *cache) const {
-      return unlikely(is_owner) ? cache->my_rank_ : value;
+    /** Determines the block's owner. */
+    int owner(const struct DistributedCache *cache) const {
+      return unlikely(value >= 0) ? cache->my_rank_ : (~value);
+    }
+    /** Determines whether I am the owner. */
+    bool is_owner() const {
+      return value >= 0;
+    }
+    /** Determines if block is in RAM. */
+    bool is_in_core() const {
+      return data != NULL;
+    }
+    /** Dirtiness check. */
+    bool is_dirty() const {
+      return dirty_ranges >= FULLY_DIRTY;
+    }
+    /** Determines whether the block should be treated as blank. */
+    bool is_new() const {
+      return dirty_ranges == NOT_DIRTY_NEW;
+    }
+    /** Determines if the block is busy. */
+    bool is_busy() const {
+      return locks != 0;
+    }
+    BlockDevice::blockid_t local_blockid() const {
+      // A block's ID is invalid if...
+      DEBUG_ASSERT_MSG(is_owner(), "no local blockid: it's not mine");
+      DEBUG_ASSERT_MSG(!is_in_core(), "no local blockid: it's in RAM!");
+      DEBUG_ASSERT_MSG(!is_new(), "no local blockid: it's newly allocated");
+      return value;
     }
   };
 
@@ -122,13 +176,16 @@ class DistributedCache : public BlockDevice {
  public:
   void Init(int channel_num);
 
-  void AcquireWrite(BlockDevice::blockid_t begin_block,
-      BlockDevice::offset_t begin_offset,
-      BlockDevice::blockid_t last_block,
-      BlockDevice::offset_t end_offset);
   void Commit();
   void Invalidate();
-  
+
+  char *StartWrite(BlockDevice::blockid_t blockid);
+  char *StartWrite(BlockDevice::blockid_t blockid,
+      BlockDevice::offset_t begin, BlockDevice::offset_t end);
+  char *StartRead(BlockDevice::blockid_t blockid);
+  void StopRead(BlockDevice::blockid_t blockid);
+  void StopWrite(BlockDevice::blockid_t blockid);
+
  private:
   void MarkDirty_(BlockMetadata *block,
       BlockDevice::blockid_t begin, BlockDevice::blockid_t end);
