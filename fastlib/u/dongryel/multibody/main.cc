1c843f20a4d2fa5011b7a52fdb0f5df076b40cf3
Dongryeol Lee <drselee@gmail.com>
2007-09-21 23:41:15 +0000

    Added the basic finite difference scheme, now debugging and unit testing remains

--- fastlib/u/dongryel/multibody/main.cc
+++ fastlib/u/dongryel/multibody/main.cc
@@ -2,34 +2,20 @@
 
 int main(int argc, char *argv[])
 {
-  const char *datafile_name;
   int leaflen;
-  Dataset dataset;
-  Matrix data;
   bool do_naive;
 
   fx_init(argc, argv);
 
   // PARSE INPUTS
-  datafile_name = fx_param_str(NULL, "data", "small.arff");
   leaflen = fx_param_int(NULL, "leaflen", 20);
   do_naive = fx_param_exists(NULL, "do_naive");
-
-  // READING DATA
-  fx_timer_start(NULL, "read_d");
-  // read the dataset and get the matrix
-  if (!PASSED(dataset.InitFromFile(datafile_name))) {
-    fprintf(stderr, "main: Couldn't open file '%s'.\n", datafile_name);
-    return 1;
-  }
-  data.Alias(dataset.matrix());
-  fx_timer_stop(NULL, "read_d");
 
   // Multibody computation
   fx_timer_start(NULL,"multibody");
   MultitreeMultibody<GaussianKernel, GaussianKernelDerivative> mtmb;
-  mtmb.Init(data);
-  mtmb.Compute();
+  mtmb.Init(0.1);
+  mtmb.Compute(0.1);
   fx_timer_stop(NULL, "multibody");
 
   // NAIVE
