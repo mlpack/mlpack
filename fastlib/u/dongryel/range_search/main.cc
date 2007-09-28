c03888366d21f39e6eb0d332de5dc8c7e0e1d34e
Dongryeol Lee <drselee@gmail.com>
2007-09-28 14:51:56 +0000

    All bugs fixed for single-tree orthogonal range queries

--- fastlib/u/dongryel/range_search/main.cc
+++ fastlib/u/dongryel/range_search/main.cc
@@ -6,14 +6,23 @@ int main(int argc, char *argv[]) {
   bool do_naive = fx_param_exists(NULL, "do_naive");
 
   OrthoRangeSearch fast_search;
+  ArrayList<int> fast_search_results;
+  fast_search_results.Init(0);
   fast_search.Init();
   fast_search.Compute();
+  fast_search.get_results(fast_search_results);
 
   // if naive option is specified, do naive algorithm
   if(do_naive) {
-    NaiveOrthoRangeSearch search; 
+    NaiveOrthoRangeSearch search;
+    ArrayList<int> naive_search_results;
+    naive_search_results.Init(0);
     search.Init();    
     search.Compute();
+    search.get_results(naive_search_results);
+
+    printf("Fast method got %d candidates, slow method got %d candidates\n",
+	   fast_search_results.size(), naive_search_results.size());
   }
   fx_done();
   return 0;
