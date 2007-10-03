2c67386c28c0337305a89d59f6a33222e8ce3d69
Dongryeol Lee <drselee@gmail.com>
2007-10-03 20:57:50 +0000

    Checkin before the porting

---
+++ fastlib/u/dongryel/kde/build.py
@@ -0,0 +1,28 @@
+
+librule(
+    name = "kde",                            # this line can be safely omitted
+    sources = [],                            # files that must be compiled
+    headers = ["kde.h"],                     # include files part of the 'lib'
+    deplibs = ["u/dongryel/series_expansion:series_expansion",
+               "fastlib:fastlib_int"]        # dependency
+    )
+
+binrule(
+    name = "kde_bin",                        # the executable name
+    sources = ["main.cc"],                   # compile multibody.cc
+    headers = [],                            # no extra headers
+    deplibs = [":multibody",
+               "u/dongryel/series_expansion:series_expansion",
+               "fastlib:fastlib_int"]
+    )
+
+# to build:
+# 1. make sure have environment variables set up:
+#    $ source /full/path/to/fastlib/script/fl-env /full/path/to/fastlib
+#    (you might want to put this in bashrc)
+# 2. fl-build main
+#    - this automatically will assume --mode=check, the default
+#    - type fl-build --help for help
+# 3. ./main
+#    - to build same target again, type: make
+#    - to force recompilation, type: make clean
