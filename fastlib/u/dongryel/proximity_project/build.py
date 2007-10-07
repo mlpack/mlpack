4494dd56a0c20464430b2f2b05fd92484db16ae0
Dongryeol Lee <drselee@gmail.com>
2007-10-07 18:59:37 +0000

    Spill kd-tree implemented

---
+++ fastlib/u/dongryel/proximity_project/build.py
@@ -0,0 +1,28 @@
+
+librule(
+    name = "proximity_project",              # this line can be safely omitted
+    sources = [],                            # files that must be compiled
+    headers = ["spill_kdtree.h",
+               "spill_kdtree_impl.h",
+               "general_spacetree.h"],       # include files part of the 'lib'
+    deplibs = ["fastlib:fastlib_int"]        # dependency
+    )
+
+binrule(
+    name = "proximity_project_bin",          # the executable name
+    sources = ["main.cc"],                   # compile multibody.cc
+    headers = [],                            # no extra headers
+    deplibs = [":proximity_project",
+               "fastlib:fastlib_int"]
+    )
+
+# to build:
+# 1. make sure have environment variables set up:
+#    $ source /full/path/to/fastlib/script/fl-env /full/path/to/fastlib
+#    (you might want to put this in bashrc)
+# 2. fl-build main
+#    - this automatically will assume --mode=check, the default
+#    - type fl-build --help for help
+# 3. ./main
+#    - to build same target again, type: make
+#    - to force recompilation, type: make clean
