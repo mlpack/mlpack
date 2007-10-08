ee2241811c593fb78226bb9560a829af4f204a61
Dongryeol Lee <drselee@gmail.com>
2007-10-08 16:26:31 +0000

    mean shift directory added, enhanced pruning rule for kde in effect

--- fastlib/u/dongryel/kde/main.cc
+++ fastlib/u/dongryel/kde/main.cc
@@ -10,12 +10,17 @@ int main(int argc, char *argv[]) {
   FastKde<GaussianKernel, GaussianKernelDerivative> fast_kde;
   fast_kde.Init();
   fast_kde.Compute(fx_param_double(NULL, "tau", 0.1));
-
+  //fast_kde.PrintDebug();
+  Vector fast_kde_results;
+  fast_kde_results.Alias(fast_kde.get_density_estimates());
+  
   if(do_naive) {
     NaiveKde<GaussianKernel> naive_kde;
     naive_kde.Init(fast_kde.get_query_dataset(), 
 		   fast_kde.get_reference_dataset());
     naive_kde.Compute();
+    //naive_kde.PrintDebug();
+    naive_kde.ComputeMaximumRelativeError(fast_kde_results);
   }
 
   fx_done();
---
+++ fastlib/u/dongryel/mean_shift/main.cc
@@ -0,0 +1,11 @@
+#include "fastlib/fastlib_int.h"
+#include "mean_shift.h"
+
+int main(int argc, char *argv[]) {
+
+  fx_init(argc, argv);
+
+
+  fx_done();
+  return 0;
+}
