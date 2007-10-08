5191c902fc892db53c58c8f4dcb8c3cf866e368f
Dongryeol Lee <drselee@gmail.com>
2007-10-08 04:02:56 +0000

    Finite difference debugged

--- fastlib/u/dongryel/kde/main.cc
+++ fastlib/u/dongryel/kde/main.cc
@@ -2,6 +2,22 @@
 #include "kde.h"
 
 int main(int argc, char *argv[]) {
-  
+
+  fx_init(argc, argv);
+
+  bool do_naive = fx_param_exists(NULL, "do_naive");
+
+  FastKde<GaussianKernel, GaussianKernelDerivative> fast_kde;
+  fast_kde.Init();
+  fast_kde.Compute(fx_param_double(NULL, "tau", 0.1));
+
+  if(do_naive) {
+    NaiveKde<GaussianKernel> naive_kde;
+    naive_kde.Init(fast_kde.get_query_dataset(), 
+		   fast_kde.get_reference_dataset());
+    naive_kde.Compute();
+  }
+
+  fx_done();
   return 0;
 }
