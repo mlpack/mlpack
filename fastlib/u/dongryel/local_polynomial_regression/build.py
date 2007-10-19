126fcf870cf3859dfdbfe95b2fdcdf625d31127b
Dongryeol Lee <drselee@gmail.com>
2007-10-19 05:04:50 +0000

    Started LPR

---
+++ fastlib/u/dongryel/local_polynomial_regression/build.py
@@ -0,0 +1,28 @@
+
+librule(
+    name = "lpr",                            # this line can be safely omitted
+    sources = [],                            # files that must be compiled
+    headers = ["lpr.h"],                     # include files part of the 'lib'
+    deplibs = ["u/dongryel/series_expansion:series_expansion",
+               "fastlib:fastlib_int"]        # dependency
+    )
+
+binrule(
+    name = "lpr_bin",                        # the executable name
+    sources = ["main.cc"],
+    headers = [],                            # no extra headers
+    deplibs = [":lpr",
+               "u/dongryel/series_expansion:series_expansion",
+               "fastlib:fastlib_int"]
+    )
+
+# to build:
+# 1. make sure have environment variables set up:
+#    $ source /full/path/to/fastlib/script/fl-env /full/path/to/fastlib
+#    (you might want to put this in bashrc)
+# 2. fl-build main
+#    - this automatically will assume --mode=check, the default
+#    - type fl-build --help for help
+# 3. ./main
+#    - to build same target again, type: make
+#    - to force recompilation, type: make clean
--- fastlib/u/dongryel/proximity_project/main.cc
+++ fastlib/u/dongryel/proximity_project/main.cc
@@ -4,6 +4,32 @@
 
 typedef GeneralBinarySpaceTree<DHrectBound<2>, Matrix, PCAStat> Tree;
 
+void PCA(const Matrix &data) {
+  
+  // compute PCA on the extracted submatrix
+  Matrix U, VT;
+  Vector s;
+  la::SVDInit(data, &s, &U, &VT);
+
+  // reduce the dimension in half
+  Matrix U_trunc;
+  int new_dimension = U.n_cols();
+  U_trunc.Init(new_dimension, U.n_rows());
+  for(index_t i = 0; i < new_dimension; i++) {
+    Vector s;
+    U.MakeColumnVector(i, &s);
+
+    for(index_t j = 0; j < U.n_rows(); j++) {
+      U_trunc.set(i, j, s[j]);
+    }
+  }
+
+  Matrix pca_transformed;
+  la::MulInit(U_trunc, data, &pca_transformed);
+
+  pca_transformed.PrintDebug();
+}
+
 int main(int argc, char *argv[]) {
   fx_init(argc, argv);
   const char *fname = fx_param_str(NULL, "data", NULL);
@@ -14,7 +40,15 @@ int main(int argc, char *argv[]) {
   int leaflen = fx_param_int(NULL, "leaflen", 20);
   Tree *root_ = 
     proximity::MakeSpillKdTreeMidpoint<Tree>(data_, leaflen, NULL);
-  
+
+  // recursively computed PCA
+  printf("Recursive PCA\n");
+  (root_->stat().pca_transformed_).PrintDebug();
+
+  // exhaustively compute PCA
+  printf("Exhaustive PCA\n");
+  PCA(data_);
+
   fx_done();
   return 0;
 }
