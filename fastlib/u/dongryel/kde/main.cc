3913fb8e5d1b1772942f24675d0b192c097164dd
Dongryeol Lee <drselee@gmail.com>
2007-10-25 18:47:35 +0000

    some minor changes in timing for FGT KDE

--- fastlib/u/dongryel/kde/main.cc
+++ fastlib/u/dongryel/kde/main.cc
@@ -38,8 +38,28 @@ int main(int argc, char *argv[]) {
   }
   else if(!strcmp(algorithm, "fgt")) {
     FGTKde fgt_kde;
+    Vector fgt_kde_results;
+    Matrix query_dataset;
+    Matrix reference_dataset;
     fgt_kde.Init();    
     fgt_kde.Compute();
+    fgt_kde_results.Copy(fgt_kde.get_density_estimates());
+    query_dataset.Copy(fgt_kde.get_query_dataset());
+    reference_dataset.Copy(fgt_kde.get_reference_dataset());
+    if(fx_param_exists(NULL, "fgt_kde_output")) {
+      fgt_kde.PrintDebug();
+    }
+
+    if(do_naive) {
+      NaiveKde<GaussianKernel> naive_kde;
+      naive_kde.Init(query_dataset, reference_dataset);
+      naive_kde.Compute();
+      
+      if(fx_param_exists(NULL, "naive_kde_output")) {
+	naive_kde.PrintDebug();
+      }
+      naive_kde.ComputeMaximumRelativeError(fgt_kde_results);
+    }
   }
   else if(!strcmp(algorithm, "fast")) {
     if(!strcmp(fx_param_str(NULL, "kernel", "gaussian"), "gaussian")) {
