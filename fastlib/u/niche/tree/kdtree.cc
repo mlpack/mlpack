4b96a249d79937be6cc24ffeb963129f420df7de
tekhnofiend <nishantmehta.x@gmail.com>
2007-11-01 02:42:28 +0000

    moved some stuff, nishant

--- fastlib/u/niche/tree/kdtree.cc
+++
@@ -1,6 +0,0 @@
-#include "fastlib/fastlib.h"
-
-int main(int argc, char *argv[]) {
-
-
-}
---
+++ fastlib/u/niche/tree/my_kde.cc
@@ -0,0 +1,17 @@
+/**
+ * @file my_kde.cc
+ *
+ * A multithreaded and cluster-parallel kernel density estimator
+ *
+ * TODO: Currently doesn't output anything.
+ */
+
+#include "thor/thor.h"
+#include "fastlib/fastlib.h"
+
+/**
+ * An N-Body-Reduce problem.
+ */
+class Kde {
+  //... definition for thresholded kernel density estimation follows ...
+};
