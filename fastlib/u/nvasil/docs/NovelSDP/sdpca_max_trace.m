030f32e8ed96b3ac5467aad98fafb0601e87f396
vasiloglou <nvasil@ismion.com>
2007-11-04 23:35:25 +0000

    this version of max trace is running. We actually needed to scale the objective function with something small (a*Trace(X)) a~10^-4 so that the dual becomes feasible.

--- fastlib/u/nvasil/docs/NovelSDP/sdpca_max_trace.m
+++ fastlib/u/nvasil/docs/NovelSDP/sdpca_max_trace.m
@@ -5,22 +5,34 @@ function [newx]=sdpca_max_trace(x, knn)
 %  \Sum\Sum X=0
 %  X_ii + X_jj -X_ij -X_ji =d_ij
 %  X \geq 0
+
 [n, m]=size(x);
 dist=zeros(n,1);
-At=sparse(knn*n+1,n^2);
-b=zeros(knn*n+1,1);
+A=zeros(n,n);
 for i=1:n
    for j=1:n
-      dist(j)=sum((x(i,:)-x(j,:)).^2);
+      dist(j)=x(i,:)*x(i,:)'+ x(j,:)*x(j,:)' -2*x(j,:)*x(i,:)' ;
    end
    [dist I]=sort(dist);
-   for j=1:knn
-     At((i-1)*knn+j, sub2ind([n n], i,i)) = 1;
-     At((i-1)*knn+j, sub2ind([n n], I(j+1),I(j+1))) = 1;
-     At((i-1)*knn+j, sub2ind([n n], I(j+1),i)) = -1;
-     At((i-1)*knn+j, sub2ind([n n], i, I(j+1))) = -1;
+   A(i,I(1:knn+1))=dist(1:knn+1);
+end
+A=max(A,A');
+num_of_neighbors=length(find(A(:)~=0));
+
+At=sparse(num_of_neighbors+1,n^2);
+b=zeros(num_of_neighbors+1,1);
+count=0;
+for i=1:n
+   [I]=find(A(i,:)~=0);
+   dist=A(i,I);
+   for j=1:length(I)
+     count=count+1;
+     At(count, sub2ind([n n], i,i)) = 1;
+     At(count, sub2ind([n n], I(j),I(j))) = 1;
+     At(count, sub2ind([n n], I(j),i)) = -1;
+     At(count, sub2ind([n n], i, I(j))) = -1;
+     b(count)=dist(j);
    end
-   b((i-1)*knn+1:i*knn)=dist(2:knn+1);
 end
 At(end,:)=1;
 b(end)=0;
@@ -28,7 +40,7 @@ c=-eye(n);
 c=c(:);
 K.s=n;
 pars.maxiter=90;
-pars.eps=10^-2;
+pars.eps=10^-3;
 pars.sdp=1;
-[s,y,info]=sedumi(At,b,c,K,pars);
+[s,y,info]=sedumi(At,b,10^-5*c,K,pars);
 newx=mat(s,n);
