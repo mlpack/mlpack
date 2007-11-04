a0beb62698ea4351f1656a2e60f21f6c1e5817cf
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2007-11-04 23:35:25 +0000

    this version of max trace is running. We actually needed to scale the objective function with something small (a*Trace(X)) a~10^-4 so that the dual becomes feasible.

--- fastlib/u/nvasil/docs/NovelSDP/test.m
+++ fastlib/u/nvasil/docs/NovelSDP/test.m
@@ -1,10 +1,10 @@
-N=800;
+N=500;
 t=rand(N,1)*3*pi+3*pi/2;
 h=rand(N,1)*3*pi+3*pi/2;
 x=[t.*cos(t) h, t.*sin(t)];
 %x=rand(N,5);
 x=x-repmat(mean(x),N,1);
-[newx]=sdpca_max_trace(x, 4);
+[newx]=sdpca_max_trace(x, 6);
 [newx]=sdpca_max_trace_sparse(x,7);
 [newx]=sdpca_max_trace_dense(x,5);
 [newx]=sdpca_min_max_eig_trad(x, 5);
@@ -20,5 +20,4 @@ d=d/max(d);
 plot((d(1:6)),'k')
 plot(v(:,1),v(:,2),'.')
 plot3(v(:,1),v(:,2),v(:,3),'.')
-
 plot3(x(:,1), x(:,2), x(:,3),'.');
