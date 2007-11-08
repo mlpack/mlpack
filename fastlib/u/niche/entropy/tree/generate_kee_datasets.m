d1edb58a70ffa0739f0d19b6e4a8e02f09e53900
tekhnofiend <nishantmehta.x@gmail.com>
2007-11-08 22:18:09 +0000

    added tree -niche

---
+++ fastlib/u/niche/entropy/tree/generate_kee_datasets.m
@@ -0,0 +1,29 @@
+function [] = generate_kee_datasets(N, mu, sigma, ...
+				    min_max_vals, resolution, ...
+				    data_filename, ...
+				    data_linspace_filename);
+%  generate_kee_datasets() - generate N Gaussian data points and save to file
+%  Usage:
+%  generate_kee_datasets(N, mu, sigma, [min_val max_val], resolution, data_filename, data_linspace_filename);
+
+
+min_val = min_max_vals(1);
+max_val = min_max_vals(2);
+
+% generate Gaussian data
+randn('state', sum(100*clock));
+data = normrnd(mu * ones(1,N),sigma);
+
+
+% linear spacing
+
+
+linear_spacing = ...
+    linspace(min_val, max_val, ...
+	     round((max_val - min_val) / resolution));
+
+
+csvwrite(data_filename, data');
+
+csvwrite(data_linspace_filename, [data linear_spacing]');
+
---
+++ fastlib/u/niche/entropy/tree/run_kde.sh
@@ -0,0 +1,1 @@
+./bi_fdkde --kde/h=.1 --kde/rel_error=0 --kde/gauss=kernel --data=data.csv --data_linspace=data_linspace.csv
