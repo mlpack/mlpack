585ee03b1352d72f2241c0502121e62716784212
Dongryeol Lee <drselee@gmail.com>
2007-11-28 02:38:18 +0000

    hierarhicical pca almost completed

--- fastlib/u/dongryel/proximity_project/pca_tree.h
+++ fastlib/u/dongryel/proximity_project/pca_tree.h
@@ -150,6 +150,10 @@ class PCAStat {
   
   Vector means_;
 
+  Matrix eigenvectors_;
+
+  Matrix eigenvalues_;
+
   /** Initialize the statistics */
   void Init() {
   }
@@ -171,65 +175,252 @@ class PCAStat {
     SubtractVectorFromMatrix(mean_centered_, means_, mean_centered_);
 
     // compute PCA on the extracted submatrix
-    Matrix U, VT;
-    Vector s_values;
-    la::SVDInit(mean_centered_, &s_values, &U, &VT);
-
-    // reduce the dimension in half
-    Matrix U_trunc;
-    la::TransposeInit(U, &U_trunc);
+    Matrix VT;
+    Vector evalues;
+    la::SVDInit(mean_centered_, &evalues, &eigenvectors_, &VT);
+    eigenvalues_.Init(evalues.length(), evalues.length());
+    eigenvalues_.SetZero();
+    for(index_t i = 0; i < evalues.length(); i++) {
+      eigenvalues_.set(i, i, evalues[i]);
+    }
 
     // transform coordinates
-    la::MulInit(U_trunc, mean_centered_, &pca_transformed_);
+    la::MulTransAInit(eigenvectors_, mean_centered_, &pca_transformed_);
+  }
 
-    printf("Leaf exhaustive:\n");
-    pca_transformed_.PrintDebug();
+  /**
+   * Takes the two eigenbases and 
+   */
+  void ComputeLeftsideNullSpaceBasis(const PCAStat& left_stat,
+				     const PCAStat& right_stat, 
+				     Matrix *leftside_nullspace_basis,
+				     Matrix *projection_of_right_eigenbasis,
+				     Vector *mean_diff,
+				     Vector *projection_of_mean_diff) {
+
+    Matrix left_eigenbasis, right_eigenbasis;
+    Vector left_means, right_means;
+    Matrix projection_residue_of_right_eigenbasis;
+    Vector projection_residue_of_mean_diff;
+
+    // left and right's eigenbasis and the mean vectors
+    left_eigenbasis.Alias(left_stat.eigenvectors_);
+    right_eigenbasis.Alias(right_stat.eigenvectors_);
+    left_means.Alias(left_stat.means_);
+    right_means.Alias(right_stat.means_);
+    la::SubInit(right_means, left_means, mean_diff);
+
+    // compute the projection of the right eigenbasis onto the left
+    // eigenbasis
+    la::MulTransAInit(left_eigenbasis, right_eigenbasis, 
+		      projection_of_right_eigenbasis);
+
+    // compute the residue of projection
+    projection_residue_of_right_eigenbasis.Copy(right_eigenbasis);
+    la::MulExpert(-1, left_eigenbasis, *projection_of_right_eigenbasis, 1,
+		  &projection_residue_of_right_eigenbasis);
+    
+    // project the difference in the two means onto the eigenbasis
+    // belonging to the first subspace
+    la::MulInit(*mean_diff, left_eigenbasis, projection_of_mean_diff);
+    
+    // compute the projection residue of the mean difference
+    projection_residue_of_mean_diff.Copy(*mean_diff);
+    la::MulExpert(-1, left_eigenbasis, *projection_of_mean_diff, 1,
+		  &projection_residue_of_mean_diff);
+    
+    // copy onto a temporary matrix for QR decomposition by filtering
+    // very small columns
+    Matrix span_set;
+    int dim = left_eigenbasis.n_rows();
+    span_set.Init(dim, 0);
+    
+    // loop over each column residue vectors
+    for(index_t i = 0; i < projection_residue_of_right_eigenbasis.n_cols(); 
+	i++) {
+      Vector residue_vector;
+      double euclidean_norm;
+      projection_residue_of_right_eigenbasis.MakeColumnVector
+	(i, &residue_vector);
+      euclidean_norm = la::LengthEuclidean(residue_vector);
+
+      if(euclidean_norm > 0.001) {
+	Vector dest;
+	span_set.ResizeNoalias(span_set.n_cols() + 1);
+	span_set.MakeColumnVector(span_set.n_cols() - 1, &dest);
+	dest.CopyValues(residue_vector);
+      }
+    }
+    double euclidean_norm_of_projection_residue_of_mean_diff =
+      la::LengthEuclidean(projection_residue_of_mean_diff);
+    if(euclidean_norm_of_projection_residue_of_mean_diff > 0.001) {
+      Vector dest;
+      span_set.ResizeNoalias(span_set.n_cols() + 1);
+      span_set.MakeColumnVector(span_set.n_cols() - 1, &dest);
+      dest.CopyValues(projection_residue_of_mean_diff);
+    }
+
+    if(span_set.n_cols() > 0) {
+      Matrix dummy;
+      la::QRInit(span_set, leftside_nullspace_basis, &dummy);
+      span_set.PrintDebug();
+    }
+    else {
+      leftside_nullspace_basis->Init(dim, 0);
+    }
+  }
+
+  void SetupEigensystem(const PCAStat& left_stat, const PCAStat &right_stat, 
+			const Matrix &leftside_nullspace_basis,
+			const Matrix &projection_of_right_eigenbasis,
+			const Vector &mean_diff, 
+			const Vector &projection_of_mean_diff, 
+			Matrix *eigensystem) {
+
+    Matrix left_eigenbasis, right_eigenbasis, 
+      left_eigenvalues, right_eigenvalues;
+
+    // left and right's eigenbasis and the mean vectors
+    left_eigenbasis.Alias(left_stat.eigenvectors_);
+    right_eigenbasis.Alias(right_stat.eigenvectors_);
+    left_eigenvalues.Alias(left_stat.eigenvalues_);
+    right_eigenvalues.Alias(right_stat.eigenvalues_);
+
+    // precomputed factors
+    double factor1 = ((double) left_stat.count_) / ((double) count_);
+    double factor2 = ((double) right_stat.count_) / ((double) count_);
+    double factor3 = ((double) left_stat.count_ * right_stat.count_) /
+      ((double) count_ * count_);
+
+    if(leftside_nullspace_basis.n_cols() > 0) {
+      eigensystem->Init(left_eigenvalues.n_rows() + 
+			leftside_nullspace_basis.n_cols(),
+			left_eigenvalues.n_rows() +
+			leftside_nullspace_basis.n_cols());
+    }
+    else {
+      eigensystem->Init(left_eigenvalues.n_rows(),
+			left_eigenvalues.n_rows());
+    }
+    eigensystem->SetZero();
+    
+    // compute the top left part of the eigensystem
+    Matrix top_left, top_tmp;
+    la::MulInit(projection_of_right_eigenbasis, right_eigenvalues, &top_tmp);
+    la::MulTransBInit(top_tmp, projection_of_right_eigenbasis, &top_left);
+    for(index_t i = 0; i < left_eigenvalues.n_rows(); i++) {
+      for(index_t j = 0; j < left_eigenvalues.n_cols(); j++) {
+	eigensystem->set(i, j, factor1 * left_eigenvalues.get(i, j) +
+			 factor2 * top_left.get(i, j) +
+			 factor3 * projection_of_mean_diff[i] *
+			 projection_of_mean_diff[j]);
+      }
+    }
+    
+    if(leftside_nullspace_basis.n_cols() > 0) {
+      Matrix proj_rightside_eigenbasis_on_leftside_nullspace;
+      Vector proj_mean_diff_on_leftside_nullspace;
+      la::MulTransAInit(leftside_nullspace_basis, right_eigenbasis,
+			&proj_rightside_eigenbasis_on_leftside_nullspace);
+      la::MulInit(mean_diff, leftside_nullspace_basis,
+		  &proj_mean_diff_on_leftside_nullspace);
+      
+      // set up the eigensystem
+      Matrix top_right, bottom_left, bottom_right;
+      Matrix bottom_tmp;
+     
+      la::MulTransBInit(top_tmp, 
+			proj_rightside_eigenbasis_on_leftside_nullspace,
+			&top_right);
+      for(index_t i = 0; i < top_right.n_rows(); i++) {
+	for(index_t j = 0; j < top_right.n_cols(); j++) {
+	  eigensystem->set(i, j + top_left.n_cols(),
+			   factor2 * top_right.get(i, j) +
+			   factor3 * projection_of_mean_diff[i] *
+			   proj_mean_diff_on_leftside_nullspace[j]);
+	}
+      }
+
+      la::MulInit(proj_rightside_eigenbasis_on_leftside_nullspace,
+		  right_eigenvalues, &bottom_tmp);
+      la::MulTransBInit(bottom_tmp, projection_of_right_eigenbasis, 
+			&bottom_left);
+      for(index_t i = 0; i < bottom_left.n_rows(); i++) {
+	for(index_t j = 0; j < bottom_left.n_cols(); j++) {
+	  eigensystem->set(i + top_left.n_rows(), j,
+			   factor2 * bottom_left.get(i, j) +
+			   factor3 * proj_mean_diff_on_leftside_nullspace[i] *
+			   projection_of_mean_diff[j]);
+	}
+      }
+      la::MulTransBInit(bottom_tmp, 
+			proj_rightside_eigenbasis_on_leftside_nullspace,
+			&bottom_right);
+      for(index_t i = 0; i < bottom_right.n_rows(); i++) {
+	for(index_t j = 0; j < bottom_right.n_cols(); j++) {
+	  eigensystem->set(i + top_left.n_rows(), j + top_left.n_cols(),
+			   factor2 * bottom_right.get(i, j) +
+			   factor3 * proj_mean_diff_on_leftside_nullspace[i] *
+			   proj_mean_diff_on_leftside_nullspace[j]);
+	}
+      }
+    } // end of case for nonempty leftside nullspace
   }
 
   /** 
-   * domain-decomposition based PCA merging using two-sided affine
-   * transformation
+   * Merge two eigenspaces into one
    */
   void Init(const Matrix& dataset, index_t &start, index_t &count,
 	    const PCAStat& left_stat, const PCAStat& right_stat) {
 
-    int dim = dataset.n_rows();
-
-    double factor1 = ((double) left_stat.count_ + right_stat.count_) /
-      ((double) count);
-    double factor2 = 1.0 - factor1;
-    double factor3 = sqrt(((double) left_stat.count_ * right_stat.count_) /
-			  ((double) left_stat.count_ + right_stat.count_));
-
-    // set the starting index and the number of points in this node
+    // set up starting index and the count
     start_ = start;
     count_ = count;
 
-    // compute the combined mean using the left and the right means    
-    means_.Copy(left_stat.means_);
-    la::Scale(factor1, &means_);
-    la::AddExpert(factor2, right_stat.means_, &means_);
-    
-    // span that includes the difference in the two mean vectors and
-    // the subspace spanned by the right node
-    Matrix perturbation;
-    perturbation.Init(dim, right_stat.count_ + 1);
-    for(int i = 0; i < dim; i++) {
+    Matrix leftside_nullspace_basis, projection_of_right_eigenbasis;
+    Vector mean_diff, projection_of_mean_diff;
+    Matrix eigensystem;
 
-      for(int j = 0; j < right_stat.count_; j++) {
-	perturbation.set(i, j, right_stat.mean_centered_.get(i, j) +
-			 right_stat.means_[i] - means_[i]);
-      }
-      perturbation.set(right_stat.count_, i, factor3 * 
-		       (left_stat.means_[i] - right_stat.means_[i]));
+    ComputeLeftsideNullSpaceBasis(left_stat, right_stat, 
+				  &leftside_nullspace_basis,
+				  &projection_of_right_eigenbasis,
+				  &mean_diff, &projection_of_mean_diff);
+    SetupEigensystem(left_stat, right_stat,
+		     leftside_nullspace_basis,
+		     projection_of_right_eigenbasis,
+		     mean_diff, projection_of_mean_diff, &eigensystem);
+
+    // compute eigenvalues and eigenvectors of the system
+    Matrix rotation, combined_subspace;
+    Vector evalues;
+    la::EigenvectorsInit(eigensystem, &evalues, &rotation);
+    eigenvalues_.Init(evalues.length(), evalues.length());
+    eigenvalues_.SetZero();
+    for(index_t i = 0; i < evalues.length(); i++) {
+      eigenvalues_.set(i, i, evalues[i]);
     }
-    
-    // apply QR decomposition to get orthonormal basis of the span above
-    Matrix q, r;
-    la::QRInit(perturbation, &q, &r);
+    combined_subspace.Copy(left_stat.eigenvectors_);
+    combined_subspace.ResizeNoalias(combined_subspace.n_cols() + 
+				    leftside_nullspace_basis.n_cols());
+    for(index_t i = 0; i < leftside_nullspace_basis.n_cols(); i++) {
+      Vector source, dest;
+      leftside_nullspace_basis.MakeColumnVector(i, &source);
+      combined_subspace.MakeColumnVector(i + left_stat.eigenvectors_.n_cols(), 
+					 &dest);
+      dest.CopyValues(source);
+    }
+    la::MulInit(combined_subspace, rotation, &eigenvectors_);
+    eigenvectors_.PrintDebug();
+    eigenvalues_.PrintDebug();
 
-    // combined system
-    
+    // debugging
+    eigenvectors_.Destruct();
+    eigenvalues_.Destruct();
+    printf("Exhaustively checking...\n");
+    Init(dataset, start, count);
+    eigenvectors_.PrintDebug();
+    eigenvalues_.PrintDebug();
+    exit(0);
   }
 
   PCAStat() { }
