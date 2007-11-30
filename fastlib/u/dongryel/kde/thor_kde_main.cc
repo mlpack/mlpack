369915d72fff999023113507a6650fbf516308a2
Dongryeol Lee <drselee@gmail.com>
2007-11-30 19:00:24 +0000

    marched through the initialization and tree construction phase of thor

--- fastlib/u/dongryel/kde/thor_kde_main.cc
+++ fastlib/u/dongryel/kde/thor_kde_main.cc
@@ -3,17 +3,14 @@
 #include "thor_kde.h"
 #include "u/dongryel/series_expansion/kernel_aux.h"
 
-typedef ThorKde<GaussianKernel, GaussianKernelAux> GThorKde;
-
 int main(int argc, char *argv[]) {
   fx_init(argc, argv);
-  GThorKde thor_kde;
-
-  thor::RpcDualTree<GThorKde, DualTreeRecursiveBreadth<GThorKde> >
-    (fx_submodule(fx_root, "gnp", "gnp"), thor_kde.GNP_CHANNEL,
-     thor_kde.parameters_, thor_kde.q_tree_, thor_kde.r_tree_, 
-     &thor_kde.q_results_, &thor_kde.global_result_);
-
+  ThorKde<GaussianKernel, GaussianKernelAux> thor_kde;
+ 
+  // initialize and compute
+  thor_kde.Init(fx_root);
+  thor_kde.Compute();
+ 
   fx_done();
   return 0;
 }
