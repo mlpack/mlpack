4c7fc71133be246cef95a21984125a3aba646106
Dongryeol Lee <drselee@gmail.com>
2007-11-30 00:47:49 +0000

    skeleton thor KDE compiles, but needs to fill in function

--- fastlib/u/dongryel/kde/thor_kde_main.cc
+++ fastlib/u/dongryel/kde/thor_kde_main.cc
@@ -1,10 +1,19 @@
 #include "fastlib/fastlib_int.h"
-#include "u/dongryel/series_expansion/kernel_aux.h"
+#include "thor/thor.h"
 #include "thor_kde.h"
+#include "u/dongryel/series_expansion/kernel_aux.h"
+
+typedef ThorKde<GaussianKernel, GaussianKernelAux> GThorKde;
 
 int main(int argc, char *argv[]) {
   fx_init(argc, argv);
-  ThorKde<GaussianKernel, GaussianKernelAux> thor_kde_;
+  GThorKde thor_kde;
+
+  thor::RpcDualTree<GThorKde, DualTreeRecursiveBreadth<GThorKde> >
+    (fx_submodule(fx_root, "gnp", "gnp"), thor_kde.GNP_CHANNEL,
+     thor_kde.parameters_, thor_kde.q_tree_, thor_kde.r_tree_, 
+     &thor_kde.q_results_, &thor_kde.global_result_);
+
   fx_done();
   return 0;
 }
