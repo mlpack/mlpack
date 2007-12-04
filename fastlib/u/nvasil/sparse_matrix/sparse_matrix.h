68a060fd73cdeab7621fd1e383ed61b41d6daf05
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2007-12-04 03:06:08 +0000

    decided to add sparse vector

--- fastlib/u/nvasil/sparse_matrix/sparse_matrix.h
+++ fastlib/u/nvasil/sparse_matrix/sparse_matrix.h
@@ -34,19 +34,37 @@
 class SparseMatrix {
  public:
   SparseMatrix();
-	SparseMatrix(index_t dimension);
+	// Constructor, note num_of_rows is always greater or 
+	// equal to num_of_columns
+	SparseMatrix(index_t num_of_rows, index_t num_of_columns);
+	// This constructor creates square matrices
+	SparseMatrix(index_t num_of_rows);
+	// Copy constructor
   SparseMatrix(const SparseMatrix);
 	~SparseMatrix();
 	void Destruct();
-  void Init(index_t dimension, index_t nnz_per_row);
-  void Init(index_t dimension, index_t *nnz_per_row);
-	void Init(const std::vector<index_t> &indices, const Vector values, 
+	// Initializer: nnz_per_row is the  estimated non zero elements per row
+  void Init(index_t num_of_rows, index_t num_of_columns, 
+			      index_t nnz_per_row);
+	// Initializer nnz_per_row has the estimated non zero elements per row
+	// notice that it is not the same for every row;
+  void Init(index_t num_of_rows, index_t num_of_columns, index_t *nnz_per_row);
+	// This initializer takes as an input a vector of the row indices along
+	// with the column indices and the corresponding values and creates a sparse
+	// matrix. If the dimension (number of rows)and the expected (nnz elements per row)
+	// are set to a negative value, the function will automatically detect it 
+	void Init(const std::vector<index_t> &row_indices,
+		       	const std::vector<index_t> &col_indices,
+			      const Vector values, 
 			      index_t nnz_per_row, 
 			      index_t dimension);
-  template<typename T>
-	void Init(const index_t *indices, const T *values, index_t num_of_elemets);
+	// Initialize from a text file in the following format
+	// row column value \n
 	void Init(std::string textfile);
+	// Initialize the diagonal 
 	void InitDiagonal(const Vector &vec); 
+	// Initialize the diagonal with a constant
+	void InitDiagonal(const double value);
 	void StartLoading();
   void LoadRow();
   void EndLoading();
@@ -68,13 +86,13 @@ class SparseMatrix {
 	index_t get_nnz();
  
  private:
-	index_t dimension_;
+	index_t num_of_rows_;
+	index_t num_of_columns_;
 	Epetra_SerialComm comm_;
 	Epetra_Map *map_;
   Epetra_CrsMatrix *matrix_;
   index_t *MyglobalElements_;
   void AllRowsLoad(Vector &rows, Vector &columns);
-			
 
 };
 
