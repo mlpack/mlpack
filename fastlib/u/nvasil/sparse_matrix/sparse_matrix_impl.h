4571161a1868c2b385e002a0ab0cfa8b7930895c
vasiloglou <nvasil@ismion.com>
2007-12-09 20:15:00 +0000

    :

--- fastlib/u/nvasil/sparse_matrix/sparse_matrix_impl.h
+++ fastlib/u/nvasil/sparse_matrix/sparse_matrix_impl.h
@@ -31,7 +31,7 @@ void SparseMatrix::Init(index_t num_of_rows,
 				   num_of_rows_, num_of_columns_);
 	}
 	map_ = new Epetra_Map(num_of_rows_, 0, comm_);
-	matrix_ = new Epetra_CrsMatrix(Copy , map_, nnz_per_row);
+	matrix_ = new Epetra_CrsMatrix((Epetra_DataAccess)0, *map_, nnz_per_row);
 }
 
 void SparseMatrix::Init(index_t num_of_rows,
@@ -44,7 +44,7 @@ void SparseMatrix::Init(index_t num_of_rows,
 				   num_of_rows_, num_of_columns_);
 	}
 	map_ = new Epetra_Map(num_of_rows_, 0, comm_);
-	matrix_ = new Epetra_CrsMatrix(Copy , map_, nnz_per_row);
+	matrix_ = new Epetra_CrsMatrix((Epetra_DataAccess)0 , *map_, nnz_per_row);
 }
 
 
@@ -58,8 +58,8 @@ void SparseMatrix::Init(const std::vector<index_t> &rows,
   } else {
 		num_of_rows_ = 0;
 		num_of_columns_=0;
-		map<index_t, index_t> frequencies;
-		for(index_t i=0; i<rows.length(); i++) {
+		std::map<index_t, index_t> frequencies;
+		for(index_t i=0; i<(index_t)rows.size(); i++) {
 			frequencies[rows[i]]++;
 			frequencies[columns[i]]++;
 		  if (rows[i]>num_of_rows_) {
@@ -69,12 +69,12 @@ void SparseMatrix::Init(const std::vector<index_t> &rows,
 			  num_of_columns_ = columns[i];
 			}
 		}
-    if (frequency.size()-1!=num_of_rows_) {
+    if ((index_t)frequencies.size()-1!=num_of_rows_) {
 		  NONFATAL("Some of the rows are zeros only!");
 		}
 		index_t *nnz= new index_t[dimension_];
-		for(index_t i=0; i<num_of_rows__; i++) {
-		  nnz[i]=frequency[i];
+		for(index_t i=0; i<num_of_rows_; i++) {
+		  nnz[i]=frequencies[i];
 		}
 	  Init(num_of_rows_, num_of_columns_, nnz);	
 		delete []nnz;
@@ -82,62 +82,103 @@ void SparseMatrix::Init(const std::vector<index_t> &rows,
  	}
 }
 
+void SparseMatrix::Init(const std::vector<index_t> &rows,
+                        const std::vector<index_t> &columns,	
+                        const std::vector<double>  &values, 
+                        index_t nnz_per_row, 
+                        index_t dimension) {
+	Vector temp;
+	temp.Alias((double *)&values[0], values.size());
+	Init(rows, columns, temp, nnz_per_row, dimension);
+
+}
 void SparseMatrix::StartLoadingRows() {
-   MyGlobalElements_ = matrix_->MyGlobalElements();
+   my_global_elements_ = map_->MyGlobalElements();
 }
 
 void SparseMatrix::LoadRow(index_t row, 
 		                       std::vector<index_t> &columns, 
 		                       Vector &values) {
-  matrix_->InsertGlobalValues(MyGlobalElements_[row], 
+	DEBUG_ASSERT(values.length() == (index_t)columns.size());
+  matrix_->InsertGlobalValues(my_global_elements_[row], 
+		                          values.length(), 
+															values.ptr(), 
+															&columns[0]);
+
+}
+
+void SparseMatrix::LoadRow(index_t row, 
+		                       index_t *columns,
+		                       Vector &values) {
+  matrix_->InsertGlobalValues(my_global_elements_[row], 
+		                          values.length(), 
+															values.ptr(), 
+															&columns[0]);
+
+}
+
+void SparseMatrix::LoadRow(index_t row, 
+		                       std::vector<index_t> &columns,
+		                       std::vector<double>  &values) {
+  matrix_->InsertGlobalValues(my_global_elements_[row], 
 		                          values.size(), 
-															&row_values, 
-															&columns);
+															&values[0], 
+															&columns[0]);
+
+}
+
+void SparseMatrix::LoadRow(index_t row,
+	                         index_t num,	
+		                       index_t *columns,
+		                       double  *values) {
+	matrix_->InsertGlobalValues(my_global_elements_[row], 
+		                          num, 
+															values, 
+															columns);
 
 }
 
 void SparseMatrix::EndLoading() {
   matrix_->FillComplete();
-	matrix_->MakeDataContiguous();
 }
 
-void SparseMatrix::Load(Vector &rows, 
-		                    std::vector<index_t> &columns, 
-												Vector &values) {
-  MyGlobalElements_ = matrix_->MyGlobalElements();
+void SparseMatrix::Load(const std::vector<index_t> &rows, 
+		                    const std::vector<index_t> &columns, 
+												const Vector &values) {
+  my_global_elements_ = map_->MyGlobalElements();
 	index_t i=0;
 	index_t cur_row = rows[i];
   index_t prev_row= rows[i];
-	vector<index_t> indices;
-  vector<index_t> row_values;	
+	std::vector<index_t> indices;
+	std::vector<double> row_values;	
 	while (true) {
-	  while (rows[curr_row]==rows[prev_row]) {
-		  indices.push_back(colums[i]);
+	  while (rows[cur_row]==rows[prev_row]) {
+		  indices.push_back(columns[i]);
 			row_values.push_back(values[i]);
 			i++;
 		  prev_row=i-1;
-		  curr_row=i;	
+		  cur_row=i;	
     }
-    matrix_->InsertGlobalValues(MyGlobalElements_[curr_row], 
-				                        values.size(), 
-																&row_values, 
-																&indices);
+    matrix_->InsertGlobalValues(my_global_elements_[cur_row], 
+				                        values.length(), 
+																&row_values[0], 
+																&indices[0]);
 		if (i==dimension_) {
 		  break;
 		}
 	}
 }
 
-double SparseMatrix::get(ndex_t r, index_t c) {
+double SparseMatrix::get(index_t r, index_t c) {
   DEBUG_BOUNDS(r, num_of_rows_);
 	DEBUG_BOUNDS(c, num_of_columns_);
-	index_t global_row = MyGlobalElements_[r];
+	index_t global_row = my_global_elements_[r];
 	index_t num_of_entries;
 	double *values;
 	index_t *indices;
-  matrix_->ExtractctGlobalRowView(global_row, num_of_entires, values, indices);
+  matrix_->ExtractGlobalRowView(global_row, num_of_entries, values, indices);
 	index_t *pos = std::find(indices, indices+num_of_entries, c);
-	if (*pos==indices+num_of_entries) {
+	if (pos==indices+num_of_entries) {
 	  return 0;
 	}
   return values[*pos];
@@ -147,9 +188,9 @@ void SparseMatrix::set(index_t r, index_t c, double v) {
   DEBUG_BOUNDS(r, num_of_rows_);
 	DEBUG_BOUNDS(c, num_of_columns_);
 	if (get(r,c)!=0) {
-	  matrix_->InsertGlobalValues(MyGlobalElements_[r], 1, &v, &c);
+	  matrix_->InsertGlobalValues(my_global_elements_[r], 1, &v, &c);
 	} else {
-    matrix_->ReplaceGlobalValues(MyGlobalElements_[r], 1, &v, &c); 
+    matrix_->ReplaceGlobalValues(my_global_elements_[r], 1, &v, &c); 
 	}
 }
 void SparseMatrix::MakeSymmetric() {
@@ -157,8 +198,8 @@ void SparseMatrix::MakeSymmetric() {
 	double  *values;
 	index_t *indices;
   for(index_t i=0; i<dimension_; i++) {
-    index_t global_row = MyGlobalElements_[i];
-    matrix_->ExtractctGlobalRowView(global_row, num_of_entries, values, indices);
+    index_t global_row = my_global_elements_[i];
+    matrix_->ExtractGlobalRowView(global_row, num_of_entries, values, indices);
     for(index_t j=0; j<num_of_entries; j++) {
 		  if (unlikely(get(i, indices[j])!=values[j])) {
 			  set(i, indices[j], values[j]);
