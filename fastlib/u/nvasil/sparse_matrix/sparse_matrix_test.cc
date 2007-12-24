0bb26f0db599afbe1aa444a232404225e5809952
vasiloglou <nvasil@ismion.com>
2007-12-24 21:37:29 +0000

    allmost running, most of the test except for basic operations are running

--- fastlib/u/nvasil/sparse_matrix/sparse_matrix_test.cc
+++ fastlib/u/nvasil/sparse_matrix/sparse_matrix_test.cc
@@ -159,8 +159,80 @@ class SparseMatrixTest {
     eigvectors.PrintDebug();
 		NONFATAL("Test Eigenvector success!!\n");
 	}
-	
-  void TestAll(){
+	void TestLinSolve() {
+		TestInit1();
+		Vector b,x;
+		b.Init(num_of_cols_);
+		b.SetZero();
+		x.Init(num_of_cols_);
+		x.SetAll(1);
+		smat_->MakeSymmetric();
+		smat_->EndLoading();
+	  smat_->LinSolve(b, &x);
+		x.PrintDebug();
+    NONFATAL("Test Linear Solve success!!\n");
+	}
+	void TestBasicOperations() {
+		SparseMatrix a("A.txt");
+		SparseMatrix b("B.txt");
+		SparseMatrix a_plus_b("AplusB.txt");
+		SparseMatrix a_minus_b("AminusB.txt");
+		SparseMatrix a_times_b("AtimesB.txt");
+		SparseMatrix a_dot_times_b("AdottimesB.txt");
+		SparseMatrix temp;
+		temp.Init(20,20, 3);
+		Sparsem::Add(a, b, &temp);
+		for(index_t i=0; i<20; i++) {
+		  for(index_t j=0; j<20; j++) {
+			  TEST_DOUBLE_APPROX(a_plus_b.get(i,j), temp.get(i,j), 
+						               std::numeric_limits<double>::epsilon());
+			}
+		}
+		temp.Destruct();
+		NONFATAL("Matrix addition sucess!!\n");
+		temp.Init(20,20, 3);
+		Sparsem::Subtract(a, b, &temp);
+		for(index_t i=0; i<20; i++) {
+		  for(index_t j=0; j<20; j++) {
+			  TEST_DOUBLE_APPROX(a_minus_b.get(i,j), temp.get(i,j), 
+						               std::numeric_limits<double>::epsilon());
+			}
+		}
+		temp.Destruct();
+		NONFATAL("Matrix subtraction success!!\n");
+    temp.Init(20,20, 3);
+		Sparsem::Multiply(a, b, &temp);
+		for(index_t i=0; i<20; i++) {
+		  for(index_t j=0; j<20; j++) {
+			  TEST_DOUBLE_APPROX(a_times_b.get(i,j), temp.get(i,j), 
+						               std::numeric_limits<double>::epsilon());
+			}
+		}
+		temp.Destruct();
+		NONFATAL("Matrix multiplicationn success!!\n");
+    temp.Init(20,20, 3);
+		Sparsem::DotMultiply(a, b, &temp);
+		for(index_t i=0; i<20; i++) {
+		  for(index_t j=0; j<20; j++) {
+			  TEST_DOUBLE_APPROX(a_dot_times_b.get(i,j), temp.get(i,j), 
+						               std::numeric_limits<double>::epsilon());
+			}
+		}
+		temp.Destruct();
+		NONFATAL("Matrix dot multiplicationn success!!\n");
+    temp.Init(20,20, 3);
+		Sparsem::Multiply(a, 3.45, &temp);
+		for(index_t i=0; i<20; i++) {
+		  for(index_t j=0; j<20; j++) {
+			  TEST_DOUBLE_APPROX(a.get(i,j), 3.45*temp.get(i,j), 
+						               std::numeric_limits<double>::epsilon());
+			}
+		}
+		temp.Destruct();
+		NONFATAL("Matrix scalar multiplicationn success!!\n");
+		
+	}
+  void TestAll() {
 		Init();
     TestInit1();
 		Destruct();
@@ -179,12 +251,18 @@ class SparseMatrixTest {
 		Init();
 		TestEig();
 		Destruct();
+		Init();
+		TestLinSolve();
+		Destruct();
+		Init();
+		TestBasicOperations();
+		Destruct();
 	}
 		
  private:
 	SparseMatrix *smat_;
-  static const index_t num_of_cols_  = 120;
-	static const index_t num_of_rows_  = 120;
+  static const index_t num_of_cols_  = 80;
+	static const index_t num_of_rows_  = 80;
 	static const index_t num_of_nnz_   = 4;
   double               mat_[num_of_rows_][num_of_cols_];
 	std::vector<index_t>  indices_;
