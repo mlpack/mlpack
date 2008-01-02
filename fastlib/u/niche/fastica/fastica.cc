516f374ed00d08496156d0f739027d1e52798b42
tekhnofiend <nishantmehta.x@gmail.com>
2008-01-02 19:19:00 +0000

    updated niche/fastica

--- fastlib/u/niche/fastica/fastica.cc
+++ fastlib/u/niche/fastica/fastica.cc
@@ -1,15 +1,39 @@
 #include "fastlib/fastlib.h"
 
+void whiten(Matrix X, Matrix &X_whitened) {
+  Matrix X_transpose, X_squared, D, E, E_transpose, X_temp, X_temp2;
+  Vector D_vector;
+
+  la::TransposeInit(X, &X_transpose);
+  MulInit(X, X_transpose, &X_squared);
+
+  la::EigenvectorsInit(X_squared, &D_vector, &E);
+  la::TransposeInit(E, &E_transpose);
+  D.InitDiagonal(D_vector);
+  int n_dims = D.n_rows();
+  for(int i = 0; i < n_dims; i++) {
+    D.set(i, i, pow(D.get(i, i), -.5));
+  }
+
+  MulInit(E, D, &X_temp);
+  MulInit(X_temp, E_transpose, &X_temp2);
+  MulInit(X_temp2, X, &X_whitened);
+}
+
+
 int main(int argc, char *argv[]) {
   fx_init(argc, argv);
 
   const char *data = fx_param_str(NULL, "data", NULL);
 
-  Matrix X;
+  Matrix X, whitenedX;
   data::Load(data, &X);
 
   index_t n_dims = X.n_rows();
   index_t n_points = X.n_cols();
+
+  whiten(X, &X_whitened);
+
 
   fx_done();
 
