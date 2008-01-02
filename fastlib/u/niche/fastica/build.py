0d33de9d71b23cf48763862278eb58c49c5fc3c4
tekhnofiend <nishantmehta.x@gmail.com>
2008-01-02 18:42:41 +0000

    added stuff to niche/fastica

---
+++ fastlib/u/niche/fastica/build.py
@@ -0,0 +1,1 @@
+binrule(name = "fastica", sources=["fastica.cc"], linkables=["fastlib:fastlib"])
---
+++ fastlib/u/niche/fastica/fastica.cc
@@ -0,0 +1,17 @@
+#include "fastlib/fastlib.h"
+
+int main(int argc, char *argv[]) {
+  fx_init(argc, arv);
+
+  const char *data = fx_param_str(NULL, "data", NULL);
+
+  Matrix X;
+  data::Load(data, &X);
+
+  index_t n_dims = X.n_rows();
+  index_t n_points = X.n_cols();
+
+  fx_done();
+
+  return 0;
+}
