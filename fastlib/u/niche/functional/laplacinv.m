e53f9a31f681d6bdeba3d6f264154d6a12334434
niche <niche@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-01-05 19:50:28 +0000

    moved stuff from u/niche/functional to my local matlab toolbox -nishant

--- fastlib/u/niche/functional/laplacinv.m
+++
@@ -1,5 +0,0 @@
-% laplacinv() - sample from a laplacian distribution
-
-function x = laplacinv(p, mu, b);
-
-x = mu - b * sign(p - 0.5) .* log(1 - 2 * abs(p - 0.5));
--- fastlib/u/niche/functional/laplacpdf.m
+++
@@ -1,5 +0,0 @@
-% laplacpdf() - laplacian probability density function
-
-function p = laplacpdf(x, mu, b);
-
-p = (1 / (2 * b)) * exp(-1 * abs(x - mu) / b);
