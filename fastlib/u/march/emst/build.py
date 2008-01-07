6b4a355de98c7aa217f812d005e08a0576bd00c8
Bill March <march@ices.utexas.edu>
2008-01-07 20:31:31 +0000

    added Union-Find data structure

--- fastlib/u/march/emst/build.py
+++ fastlib/u/march/emst/build.py
@@ -1,6 +1,19 @@
 binrule(
 	name = "emst",
 	sources = ["emst.cc"],
-	headers = ["emst.h", "emsttree.h"],
+	headers = ["emst.h"],
+	linkables = ["fastlib:fastlib"]
+)
+
+binrule(
+	name = "union_find_test",
+	sources = ["union_find_test.cc"],
+	headers = ["union_find.h"],
+	linkables = ["fastlib:fastlib"]
+)
+
+binrule(
+	name = "testing",
+	sources = ["testing.cc"],
 	linkables = ["fastlib:fastlib"]
 )
