5adbbb64ba09d3cb61e4a7cd9048c88f26385ee1
Bill March <march@ices.utexas.edu>
2008-01-10 22:58:55 +0000

    updated emst

--- fastlib/u/march/emst/build.py
+++ fastlib/u/march/emst/build.py
@@ -1,19 +1,19 @@
 binrule(
 	name = "emst",
 	sources = ["emst.cc"],
-	headers = ["emst.h"],
-	linkables = ["fastlib:fastlib"]
+	headers = ["emst.h", "union_find.h"],
+	deplibs = ["fastlib:fastlib"]
 )
 
 binrule(
 	name = "union_find_test",
 	sources = ["union_find_test.cc"],
 	headers = ["union_find.h"],
-	linkables = ["fastlib:fastlib"]
+	deplibs = ["fastlib:fastlib"]
 )
 
 binrule(
 	name = "testing",
 	sources = ["testing.cc"],
-	linkables = ["fastlib:fastlib"]
+	deplibs = ["fastlib:fastlib"]
 )
