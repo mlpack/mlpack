bbf89585029b90650ef9be921adf1cd65565809a
rriegel <rriegel@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-01-15 23:15:07 +0000

    allnn_main.cc documented

--- fastlib/u/march/platonic_allnn/allnn_main.cc
+++ fastlib/u/march/platonic_allnn/allnn_main.cc
@@ -1,110 +1,135 @@
-// The file should include it's header
+/**
+ * @file allnn_main.h
+ *
+ * This file contains a "platonic" example of FASTlib code.  It is a
+ * rudimentary dual-tree algorithm for all-nearest-neighbors, but more
+ * importantly, it demonstrates several useful functions for common
+ * tasks as well as proper coding style.
+ *
+ * Note however that the degree of documentation in this file well
+ * exceeds expectations.  You should always provide Doxygen-formatted
+ * comments (those starting with slash-star-star) for classes, their
+ * members, and functions, but snippets of code only deserve
+ * documentation if it is not immediately clear what they do.  (Use
+ * slash-slash or slash-star for line-by-line comments that Doxygen
+ * should ignore.)  Here, we assume you are a total beginner with
+ * FASTlib and a novice with C++, so many additional explanations have
+ * been provided.
+ */
+
+// To begin, note the "@file" at the top of the previous comment
+// block.  This tells Doxygen to parse the file; accordingly, a
+// similar line should appear at the top of all of your files.
+
+// Reusable code should be linked through a librule's header file.
 #include "allnn.h"
 
-
 int main(int argc, char* argv[]) {
- 
-  // This line starts the FASTexec system for reading in parameters.
-  // Always call it at the beginning of main to use FASTexec.
-  // It's arguments are the same as main's.
+
+  // Always initialize FASTexec with main's inputs at the beggining of
+  // your program.  This reads the command line, among other things.
   fx_init(argc, argv);
-  
-  /////////// READING PARAMETERS AND LOADING DATA ////////////////////////
-  // The filename where the queries are stored
-  const char* queries_file_name = fx_param_str_req(NULL, "q");
-  
-  // Read in the data from the command line
-  Matrix queries;
-  data::Load(queries_file_name, &queries);
-  
-  // The same as above
+
+  ////////// READING PARAMETERS AND LOADING DATA /////////////////////
+
+  // The reference data file is a required parameter.
   const char* references_file_name = fx_param_str_req(NULL, "r");
+
+  // The query data file defaults to the references.
+  const char* queries_file_name =
+      fx_param_str(NULL, "q", references_file_name);
+
+  // FASTlib classes only poison data in their default constructors;
+  // declarations must be followed by Init or an equivalent function.
   Matrix references;
+  Matrix queries;
+
+  // data::Load inits a matrix with the contents of a .csv or .arff.
   data::Load(references_file_name, &references);
-  
-  //////////// DUAL-TREE ALLNN //////////////////////////////////////////////
-  
-  // Make an AllNN object
+  data::Load(queries_file_name, &queries);
+
+  ////////// DUAL-TREE ALLNN /////////////////////////////////////////
+
   AllNN allnn;
   
-  // We need a submodule to hold our allnn computation.
-  struct datanode* allnn_module = fx_submodule(NULL, "allnn", "allnn_module");
-  
-  // We'll time the tree-building, which will be stored in the allnn module
-  fx_timer_start(allnn_module, "tree_building");
-  
-  // Initializing the AllNN object with our parameters.
-  allnn.Init(queries, references, allnn_module);
-  
-  // Stop the timer we started above
-  fx_timer_stop(allnn_module, "tree_building");
-  
-  // An array for our output
-  ArrayList<index_t> results;
-  
-  // Starting another timer
-  fx_timer_start(allnn_module, "dual_tree_computation");
-   
-  // This function will do the work of actually computing the neighbors
-  // The list of nearest neighbors will be in *results
-  allnn.ComputeNeighbors(&results);
-  
-  // Stopping the timer above
-  fx_timer_stop(allnn_module, "dual_tree_computation");
-  
-  /////////////// NAIVE ALLNN ////////////////////////////
-  
-  // Specify a command line parameter to check the computation against naive
-  // By default, we'll skip the naive computation.  Use --do_naive=1 on the command line
-  // to perform it
-  int do_naive = fx_param_bool(NULL, "do_naive", 0);
-    
-  // if we want to do the naive check
-  if (do_naive) {
-    
-    // create another module
-    struct datanode* naive_module = fx_submodule(NULL, "naive", "naive_module");
-    // create another object for it
-    AllNN naive_allnn;
-    // initialize the object, this time without building the trees
-    naive_allnn.InitNaive(queries, references, naive_module);
-  
-    // results for the naive computation
-    ArrayList<index_t> naive_results;
+  // FASTexec organizes parameters and results into submodules.  Think
+  // of this as creating a new folder named "allnn_module" under the
+  // rood directory (NULL) for the AllNN object to work inside.  Here,
+  // we initialize it with all parameters defined "--allnn/...=...".
+  struct datanode* allnn_module =
+      fx_submodule(NULL, "allnn", "allnn_module");
 
-    // timing for the naive computation
-    fx_timer_start(naive_module, "naive_time");
-    
-    // We'll just use the naive computation for this
+  // The Init function readies our object for action.  In this
+  // example, we built the AllNN class to expect all of its inputs at
+  // the time of Init; other designs might do things differently.
+  allnn.Init(queries, references, allnn_module);
+
+  // ArrayLists are prefered over standard C/C++ arrays.  These grow
+  // dynamically and check bounds when compiled with "--mode=debug".
+  // An index_t is an integer with compile-time specified size.
+  ArrayList<index_t> results;
+
+  // Tell the AllNN object to perform its computation.  We pass it a
+  // fresh ArrayList by pointer (hence the &) as a mental note that
+  // this input will be modified, i.e. filled with results.
+  allnn.ComputeNeighbors(&results);
+
+  ////////// NAIVE ALLNN /////////////////////////////////////////////
+
+  // Compare results with naive if run with "--do_naive=true".
+  if (fx_param_bool(NULL, "do_naive", 0)) {
+
+    // Our design of the AllNN class renders it usable only once;
+    // different code could clean out intermediate results, but for
+    // this example, we'll just create another instance.
+    AllNN naive_allnn;
+
+    // This time we'll use a different Init function so we can avoid
+    // building the tree.  Also, note that submodules and parameters
+    // may be created/accessed in-line.  (More on this shortly.)
+    naive_allnn.InitNaive(queries, references,
+        fx_submodule(NULL, "naive", "naive_module"));
+
+    ArrayList<index_t> naive_results;
     naive_allnn.ComputeNaive(&naive_results);
-    
-    // stopping the timer
-    fx_timer_stop(naive_module, "naive_time");
-    
-    // Element by element, we'll compare the results of the dual-tree and naive computations, issuing a warning if they disagree
-    // NOTE: this warning only appears when compiled in DEBUG mode
-    for (index_t i = 0; i < results.size(); i++) {
-      DEBUG_WARN_MSG_IF(results[i] != naive_results[i], "i = %d, results[i] = %d, naive_results[i] = %d", i, results[i], naive_results[i]);;
+
+    // A quick sanity check, now that we have naive results.  We don't
+    // want to run the for-loop unless debugging, hence the #ifdef.
+    // Most debug-only commands can instead by handled by DEBUG_ONLY
+    // or other debugging macros.  See base/debug.h for more details.
+#ifdef DEBUG
+    for (index_t i = 0; i < results.size(); ++i) {
+      // Prints a message if results are different.  Note the peculiar
+      // syntax "%"LI"d" used to properly format index_t values; this
+      // alerts printf that index_t is long if compiled as such.
+      DEBUG_WARN_MSG_IF(results[i] != naive_results[i],
+          "i = %"LI"d, results[i] = %"LI"d, naive_results[i] = %"LI"d",
+          i, results[i], naive_results[i]);
     }
-    
+#endif // DEBUG
+
   }
-  
-  //////////// OUTPUT RESULTS ///////////////////////////////////////
-  
-  // The name of the file we will write results to
-  // Defaults to "output.csv"
-  const char* output_filename = fx_param_str(NULL, "output_filename", "output.csv");
-  
-  // Need the file pointer for print
+
+  ////////// OUTPUT RESULTS //////////////////////////////////////////
+
+  const char* output_filename =
+      fx_param_str(NULL, "output_filename", "output.csv");
+
+  // We encourage you to use C-style file streams and print buffers
+  // rather than C++'s complicated equivalents.
   FILE* output_file = fopen(output_filename, "w");
-  
-  // Will print results to the output file
+
+  // The ot namespace stands for object traversal and provides many
+  // features.  Here, we pretty-print an ArrayList to file, though we
+  // could alternately have serialized it for later loading and reuse.
   ot::Print(results, output_file);
-  
-  // This is terminates FASTexec.  Always put it at the end of main when using FASTexec.
+
+  // We must tell FASTexec to wrap up when our code is done.  This
+  // emits its complete data structure--parameter settings, timers,
+  // and stored results--to stdout unless you call fx_silence first.
   fx_done();
-  
-  // We're at the end of main
+
+  // main should return 0 if the program terminates normally.
   return 0;
-  
+
 }
