938b0e620744b991bf83cc16bd52576d9bd04890
niche <niche@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-01-15 00:04:12 +0000

    updated niche/functional

--- fastlib/u/niche/functional/run_test.m
+++ fastlib/u/niche/functional/run_test.m
@@ -1,4 +1,4 @@
 prelim_funcica;
 
 [h_Y1_train_set, h_Y2_train_set, h_Y_train_set, h_Y1_test_set, ...
- h_Y2_test_set, h_Y_test_set] = call_funcica(data, t, s);
+ h_Y2_test_set, h_Y_test_set] = test_smooth_funcica(data, t, s);
