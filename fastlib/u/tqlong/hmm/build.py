19cf0abcbb6069d842b327877e36fe849a481292
tqlong <tqlong@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-01-17 22:36:58 +0000

    HMM codes

--- fastlib/u/tqlong/hmm/build.py
+++ fastlib/u/tqlong/hmm/build.py
@@ -1,32 +1,4 @@
 binrule(
-    name = "main",                 # the executable name
-    sources = ["main.cc","discreteHMM.cc","support.cc","discreteDST.cc"],         
-    headers = ["discreteHMM.h", "support.h","discreteDST.h"],              
-    deplibs = ["fastlib:fastlib_int"]       # depends on example in this folder
-    )
-
-binrule(
-    name = "gen",                 # the executable name
-    sources = ["main.cc","discreteDST.cc","support.cc"],         
-    headers = ["discreteHMM.h", "support.h", "gaussianHMM.h"],              
-    deplibs = ["fastlib:fastlib_int"]       # depends on example in this folder
-    )
-
-binrule(
-    name = "mix",                 # the executable name
-    sources = ["main.cc","support.cc","mixgaussHMM.cc","mixtureDST.cc", "gaussianHMM.cc"],         
-    headers = ["support.h", "mixgaussHMM.h","mixtureDST.h", "gaussianHMM.h"],              
-    deplibs = ["fastlib:fastlib_int"]       # depends on example in this folder
-    )
-
-binrule(
-    name = "mixgen",                 # the executable name
-    sources = ["main.cc","support.cc","mixgaussHMM.cc","mixtureDST.cc",  "gaussianHMM.cc"],         
-    headers = ["support.h", "mixgaussHMM.h","mixtureDST.h", "gaussianHMM.h"],              
-    deplibs = ["fastlib:fastlib_int"]       # depends on example in this folder
-    )
-
-binrule(
     name = "hmm",                 # the executable name
     sources = ["hmm.cc","support.cc","discreteHMM.cc","gaussianHMM.cc","mixgaussHMM.cc","mixtureDST.cc"],
     headers = ["support.h", "discreteHMM.h","gaussianHMM.h","mixgaussHMM.h","mixtureDST.h"],
