c71f9e0e19595b1d81040ec72100e8aa18af8753
jwaters6 <jwaters6@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-01-18 22:21:24 +0000

    First upload of molecular dynamics code

---
+++ fastlib/u/jim/md/LennardJones_main.cc
@@ -0,0 +1,67 @@
+/**
+ * @file LennardJones_main.cc
+ */
+
+
+#include "LennardJones.h"
+
+int main(int argc, char *argv[])
+{
+  // Parse Inputs
+  fx_init(argc, argv);
+  const char* fp;
+  fp = fx_param_str(NULL, "data", "default.txt");
+  
+  Matrix atom_mat;
+  FILE *tree, *naive;
+  double time = 0;
+  double eps, sig, mass, cutoff, time_step, stop_time;
+  LennardJones simulation;
+  LennardJones naive_test;  
+  
+  // Initialize with default values:
+  // Values are in eV, Angstroms, and amu.
+  // this places time (roughly) in units of nanoseconds
+  // Default values correspond to Argon, taken from Ashcroft & Mermin
+  eps = fx_param_double(0, "eps", 0.0104);
+  sig = fx_param_double(0, "sig", 2.74);
+  mass = fx_param_double(0, "m", 40);
+  time_step = fx_param_double(0, "dt", 1.0e-3);
+  stop_time = fx_param_double(0, "tf", 1.0e0);
+  cutoff = fx_param_double(0, "r_max", 6*sig);
+  cutoff = cutoff*cutoff;
+
+  naive = fopen("out_naive.dat", "w+");
+  tree = fopen("out_tree.dat", "w+");
+
+  // Read Atom Matrix
+  data::Load(fp, &atom_mat);
+
+  simulation.Init(atom_mat, eps, sig, mass, cutoff);
+  simulation.UpdateVelocities(time_step/2);
+  
+  naive_test.InitNaive(atom_mat, eps, sig, mass);
+  naive_test.UpdateVelocitiesNaive(time_step/2);
+  
+  
+  
+
+  while (time < stop_time){
+    simulation.UpdateVelocities(time_step);
+    simulation.UpdatePositions(time_step);
+    
+    naive_test.UpdatePositionsNaive(time_step);
+    naive_test.UpdateVelocitiesNaive(time_step);
+    
+    time = time + time_step;
+  
+  }
+
+simulation.WritePositions(tree);
+  naive_test.WritePositions(naive);
+
+  fx_done();
+  fclose(naive);
+  fclose(tree);
+
+}
