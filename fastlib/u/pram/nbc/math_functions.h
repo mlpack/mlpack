8f10dbf7f9c72fb4f8c23a774cbe738c5e9764d9
Parikshit Ram <p.ram@gatech.edu>
2008-01-18 02:25:29 +0000

    first upload of nbc

---
+++ fastlib/u/pram/nbc/math_functions.h
@@ -0,0 +1,79 @@
+#include "fastlib/fastlib.h"
+#include "fastlib/fastlib_int.h"
+
+
+/* Finds the index of the minimum element in each row of a matrix */
+
+void min_element( Matrix& element, index_t *indices ){
+	
+	index_t last = element.n_cols() - 1;
+	index_t first, lowest;
+	index_t i;
+	
+	for( i = 0; i < element.n_rows(); i++ ){
+		
+		first = lowest = 0;
+		if(first == last){
+			indices[ i ] = last;
+		}
+		while(++first <= last){
+			if( element.get( i , first ) < element.get( i , lowest ) ){
+				lowest = first;
+			}
+		}
+		indices[ i ] = lowest;
+	}
+	return;
+}
+
+int max_element_index( float *array, int length ){
+  
+	int last = length - 1;
+	int first = 0;
+       	int highest = 0;
+	
+	if( first == last ){
+		return last;
+	}
+	while( ++first <= last ){
+		if( array[ first ] > array[ highest ] ) {
+			highest = first;
+		}
+	}
+	return highest;
+}
+
+int max_element_index( ArrayList<float>& array ){
+
+        int last = array.size() - 1;
+        int first = 0;
+       	int highest = 0;
+
+	if( first == last ){
+                return last;
+        }
+        while( ++first <= last ){
+                if( array[ first ] > array[ highest ] ) {
+                        highest = first;
+                }
+	}
+	return highest;
+}
+
+int max_element_index( ArrayList<double>& array ){
+
+        int last = array.size() - 1;
+        int first = 0;
+       	int highest = 0;
+
+	if( first == last ){
+                return last;
+        }
+        while( ++first <= last ){
+                if( array[ first ] > array[ highest ] ) {
+                        highest = first;
+                }
+	}
+	return highest;
+}
+
---
+++ fastlib/u/pram/nbc/nbc_main.cc
@@ -0,0 +1,60 @@
+/**
+ * @file nbc_main.cc
+ * 
+ * This program test drives the Simple Naive Bayes Classifier
+ * 
+ */
+#include "simple_nbc.h"
+
+
+int main(int argc, char* argv[]) {
+
+  fx_init(argc, argv);
+
+  ////// READING PARAMETERS AND LOADING DATA //////
+
+  const char *training_data_filename = fx_param_str_req(NULL, "training_data");
+  Matrix training_data;
+  data::Load(training_data_filename, &training_data);
+
+  const char *testing_data_filename = fx_param_str_req(NULL, "testing_data");
+  Matrix testing_data;
+  data::Load(testing_data_filename, &testing_data);
+
+  ////// SIMPLE NAIVE BAYES CLASSIFICATION ASSUMING THE DATA TO BE UNIFORMLY DISTRIBUTED //////
+
+  SimpleNaiveBayesClassifier nbc;
+
+  struct datanode* nbc_module = fx_submodule(NULL, "simple_nbc", "simple_nbc_module");
+  const int number_of_classes = fx_param_int_req(NULL, "number_of_classes");
+
+  ////// Timing the training of the Naive Bayes Classifier //////
+  fx_timer_start(nbc_module, "training_classifier");
+
+  nbc.InitTrain(training_data, number_of_classes);
+
+  fx_timer_stop(nbc_module, "training_classifier");
+
+  ////// Timing the testing of the Naive Bayes Classifier //////
+  ArrayList<Vector> results;
+
+  fx_timer_start(nbc_module, "testing_classifier");
+
+  nbc.Classify(testing_data, &results);
+
+  fx_timer_stop(nbc_module, "testing_classifier");
+
+  ////// OUTPUT RESULTS //////
+
+  const char *output_filename = fx_param_str(NULL, "output_filename", "output.csv");
+
+  FILE *output_file = fopen(output_filename, "w");
+
+  ot::Print(results, output_file);
+
+  fclose(results);
+
+  fx_done();
+
+  return 0;
+}
