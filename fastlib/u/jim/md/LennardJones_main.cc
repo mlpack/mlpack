bbb4c47a478071823f1b21acd08dc54b1ae9cf2a
jwaters6 <jwaters6@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-01-23 22:29:41 +0000

    M    md/build.py M    md/AtomTree.h M    md/LennardJones.h M    md/LennardJones_main.cc

--- fastlib/u/jim/md/LennardJones_main.cc
+++ fastlib/u/jim/md/LennardJones_main.cc
@@ -17,60 +17,60 @@ int main(int argc, char *argv[])
   const char* fp;
   fp = fx_param_str(NULL, "data", "default.txt");
   
-  Matrix atom_mat;
-  FILE *tree, *naive;
-  double time = 0;
-  double eps, sig, mass, cutoff, time_step, stop_time;
+  Matrix atom_matrix;
+  FILE *tree_file;
+  double time = 0, time_step, stop_time;
+  bool do_naive;
   LennardJones simulation;
-  LennardJones naive_test;  
-  
-  // Initialize with default values:
-  // Values are in eV, Angstroms, and amu.
-  // this places time (roughly) in units of nanoseconds
-  // Default values correspond to Argon, taken from Ashcroft & Mermin
-  eps = fx_param_double(0, "eps", 0.0104);
-  sig = fx_param_double(0, "sig", 2.74);
-  mass = fx_param_double(0, "m", 40);
-  time_step = fx_param_double(0, "dt", 1.0e-3);
-  stop_time = fx_param_double(0, "tf", 1.0e0);
-  cutoff = fx_param_double(0, "r_max", 6*sig);
-  cutoff = cutoff*cutoff;
-
-  naive = fopen("out_naive.dat", "w+");
-  tree = fopen("out_tree.dat", "w+");
+  struct datanode* parameters = fx_submodule(NULL, "param", "parameters");
 
   // Read Atom Matrix
-  data::Load(fp, &atom_mat);
+  data::Load(fp, &atom_matrix);
+  
+  time_step = fx_param_double(0, "dt", 1.0e-3);
+  stop_time = fx_param_double(0, "tf", 1.0e0);
+  do_naive = fx_param_bool(0, "check", 0);
+ 
+  tree_file = fopen("out_tree.dat", "w+");
 
-
-  /**
-   * Begin simulation, and run to end time.
-   */
-  simulation.Init(atom_mat, eps, sig, mass, cutoff);
+  // Begin simulation, and run to end time.   
+  simulation.Init(atom_matrix, parameters);
   simulation.UpdateVelocities(time_step/2);
-  
-  naive_test.InitNaive(atom_mat, eps, sig, mass);
-  naive_test.UpdateVelocitiesNaive(time_step/2);
-  
   while (time < stop_time){
     simulation.UpdateVelocities(time_step);
-    simulation.UpdatePositions(time_step);
-    
-    naive_test.UpdatePositionsNaive(time_step);
-    naive_test.UpdateVelocitiesNaive(time_step);
-    
+    simulation.UpdatePositions(time_step);  
     time = time + time_step;
-  
   }
 
-  /**
-   * Record final positions according to both methods.
-   */
-  simulation.WritePositions(tree);
-  naive_test.WritePositions(naive);
+  // Record final positions according to both methods.   
+  simulation.WritePositions(tree_file);
+  
+  // Record Naive data, compare to tree-base
+  if (do_naive){
+    LennardJones naive_test;
+    FILE *naive_file;
+    time = 0;
+    naive_file = fopen("out_naive.dat", "w+");
+
+    // Initialize and advance naive simulation
+    Matrix naive_atom_matrix; 
+    naive_atom_matrix.Copy(atom_matrix);
+    naive_test.InitNaive(naive_atom_matrix, parameters);
+    naive_test.UpdateVelocitiesNaive(time_step/2);
+    while(time < stop_time){ 
+      naive_test.UpdatePositionsNaive(time_step);
+      naive_test.UpdateVelocitiesNaive(time_step);
+      time = time + time_step;
+    }
+
+    // Record results and compare to tree simulation
+    naive_test.WritePositions(naive_file);
+    fclose(naive_file);
+    simulation.CompareToNaive(naive_atom_matrix);
+  }
 
   fx_done();
-  fclose(naive);
-  fclose(tree);
+
+  fclose(tree_file);
 
 }
--- fastlib/u/jim/md/build.py
+++ fastlib/u/jim/md/build.py
@@ -1,23 +1,13 @@
 
 librule(
-    name = "example",              # this line can be safely omitted
-    sources = ["LennardJones_main.cc"],       # files that must be compiled
-    headers = ["AtomTree.h", "LennardJones.h"],      # include files part of the 'lib'
-    deplibs = ["fastlib:fastlib", "fastlib:fastlib_int"]  # depends on fastlib core
+    name = "md",                  
+    headers = ["AtomTree.h", "LennardJones.h"],     
+    deplibs = ["fastlib:fastlib", "fastlib:fastlib_int"]
     )
 
 binrule(
-    name = "main",                 # the executable name
-    linkables = [":example"]
+    name = "main",
+    sources = ["LennardJones_main.cc"],
+    linkables = [":md"]
     )
 
-# to build:
-# 1. make sure have environment variables set up:
-#    $ source /full/path/to/fastlib/script/fl-env /full/path/to/fastlib
-#    (you might want to put this in bashrc)
-# 2. fl-build main
-#    - this automatically will assume --mode=check, the default
-#    - type fl-build --help for help
-# 3. ./main
-#    - to build same target again, type: make
-#    - to force recompilation, type: make clean
