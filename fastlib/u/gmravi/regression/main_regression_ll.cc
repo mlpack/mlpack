bec878aeee4e18d434f5b1155961409a0014a5c6
gmravi2003 <gmravi2003@gatech.edu>
2008-01-28 21:27:50 +0000

    Partially completed diles

---
+++ fastlib/u/gmravi/regression/main_regression_ll.cc
@@ -0,0 +1,95 @@
+
+#include "fastlib/fastlib_int.h"
+#include "regression_ll.h"
+#include "dataset_scaler.h"
+
+
+int main(int argc, char *argv[]){
+
+
+  fx_init(argc,argv);
+  Matrix q_matrix;
+  Matrix r_matrix;
+  Vector rset_weights;
+
+  //Reading parameters and loading data
+
+  struct datanode* regression_module =
+    fx_submodule(NULL, "regression", "regression_module");
+  
+  // The reference data file is a required parameter.
+  const char* reference_file_name = fx_param_str_req(regression_module, "data");
+  
+  // The query data file defaults to the references.
+  const char* query_file_name =
+    fx_param_str(regression_module, "query", reference_file_name);
+
+  // flag for telling whether references are equal to queries
+  bool queries_equal_references = 
+    !strcmp(query_file_name, reference_file_name);
+  
+  // data::Load inits a matrix with the contents of a .csv or .arff.
+  data::Load(reference_file_name, &r_matrix);
+  
+  if(queries_equal_references) {
+    q_matrix.Alias(r_matrix);
+  }
+
+  else {
+    data::Load(query_file_name, &q_matrix);
+  }
+
+  
+  // confirm whether the user asked for scaling of the dataset
+  if(!strcmp(fx_param_str(regression_module, "scaling", "none"), "range")) {
+    DatasetScaler::ScaleDataByMinMax(q_matrix, r_matrix,
+                                     queries_equal_references);
+  }
+ 
+  //Get the bandwidth for kernel calculations and the tolerance limit. 
+  //Both default to 0.2
+
+  double bandwidth=fx_param_double(regression_module,"bandwidth",0.2);
+  double tau=fx_param_double(regression_module,"tau",0.2);
+
+  //Get the weights for the reference set
+  const char *rwfname=NULL;
+  if (fx_param_exists (NULL, "dwgts")){
+    
+    //rwfname is the filename having the refernece weights
+    rwfname =  fx_param_str (NULL, "dwgts", NULL);
+  }
+  
+  if (rwfname != NULL){
+    
+    Dataset ref_weights;
+    ref_weights.InitFromFile (rwfname);
+    rset_weights.Copy (ref_weights.matrix ().GetColumnPtr (0), ref_weights.matrix ().n_rows ());	//Note rset_weights_ is a vector of weights
+    
+  }
+  
+  else{
+    
+    rset_weights.Init (r_matrix.n_cols ());
+    rset_weights.SetAll (1);
+    
+  }
+
+  //Get the length of the leaf. Defaulted to 2
+
+  index_t leaf_length=fx_param_int(regression_module,"leaf_length",2);
+
+  if(!strcmp(fx_param_str(regression_module,"kernel","gaussian"),"gaussian")){
+
+    FastRegression<GaussianKernel> fast_regression;
+    fast_regression.Init(q_matrix,r_matrix,bandwidth,tau,leaf_length,
+			 rset_weights);
+
+    fast_regression.Compute();
+  }
+
+}
+
+
+
+
