ab2f10c5dd3107a6b997615efc5fa985a9e47ec8
Dongryeol Lee <drselee@gmail.com>
2008-02-03 19:41:01 +0000

    Base case of the phase 1 algorithm flashed out

--- fastlib2/contrib/dongryel/regression/local_linear_krylov_main.cc
+++ fastlib2/contrib/dongryel/regression/local_linear_krylov_main.cc
@@ -6,7 +6,9 @@ int main(int argc, char *argv[]) {
   // Initialize FastExec...
   fx_init(argc, argv);
 
-
+  // Declare local linear krylov object.
+  LocalLinearKrylov<GaussianKernel> local_linear;
+  
   // Finalize FastExec and print output results.
   fx_done();
   return 0;
