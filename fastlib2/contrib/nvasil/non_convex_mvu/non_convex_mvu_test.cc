3f1cc8cff12d30547f9c785ea451c42f28de564d
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-02-17 23:33:10 +0000

    It works suspiciously I am not sure it is right yet

--- fastlib2/contrib/nvasil/non_convex_mvu/non_convex_mvu_test.cc
+++ fastlib2/contrib/nvasil/non_convex_mvu/non_convex_mvu_test.cc
@@ -17,6 +17,7 @@
  */
 
 #include "non_convex_mvu.h"
+#include "fastlib/base/test.h"
 
 class NonConvexMVUTest {
  public:
@@ -44,32 +45,111 @@ class NonConvexMVUTest {
     NOTIFY("TestInit passed!!\n");
   }
   
-  void TestComputeLocalOptimum() {
-    NOTIFY("Testing ComputeLocalOptimum() ...\n");
-    engine_->Init("test_data_3_1000.csv", 5);
-    engine_->set_new_dimension(3);
-    engine_->ComputeLocalOptimum();
-    NOTIFY("TestComputeLocalOptimum() passed!!\n");
-  }
-  void TestComputeLocalOptimumBFGS() {
-    NOTIFY("Testing ComputeLocalOptimumBFGS() ...\n");
+ void TestFeasibilityGradient(){
+    NOTIFY("Testing  Feasibility and Gradient...\n");
     engine_->Init("swiss_roll.csv", 5);
     engine_->set_new_dimension(3);
-    engine_->set_mem_bfgs(150);
-    engine_->ComputeLocalOptimumBFGS();
-    data::Save("results.csv", engine_->coordinates());
-    NOTIFY("TestComputeLocalOptimum() passed!!\n");
+    engine_->coordinates_.Copy(engine_->data_);
+    double distance_constraint, center_constraint;
+    // Test to see if it really evaluates the 
+    // constraints correctly
+    engine_->ComputeRelativeFeasibilityError_<1>(
+        &distance_constraint, &center_constraint);
+    TEST_DOUBLE_APPROX(distance_constraint, 0.0, 1e-5);
+    engine_->ComputeFeasibilityError_<1>(
+        &distance_constraint, &center_constraint);
+    TEST_DOUBLE_APPROX(distance_constraint, 0.0, 1e-5);
+    // test to see if the lagrangian evaluates to 0
+    engine_->centering_lagrange_mult_.Init(3);
+    engine_->centering_lagrange_mult_.SetAll(0.0);
+    TEST_DOUBLE_APPROX(engine_->ComputeLagrangian_<0>(engine_->coordinates_), 
+        0, 1e-5);
+    
+    engine_->gradient_.Copy(engine_->data_);
+    engine_->lagrange_mult_.Init(5 *engine_->num_of_points_);
+    engine_->lagrange_mult_.SetAll(0.0);
+    engine_->centering_lagrange_mult_.Init(3);
+    engine_->centering_lagrange_mult_.SetAll(0.0);
+    engine_->sigma_=1;
+    engine_->ComputeGradient_<0>();
+    la::SubFrom(engine_->gradient_, &engine_->coordinates_);
+    engine_->ComputeFeasibilityError_<0>(
+        &distance_constraint, &center_constraint);
+    TEST_DOUBLE_APPROX(distance_constraint, 0.0, 1e-5);
+
+    // These are dummy initializations because of fastlib
+    engine_->previous_gradient_.Init(1, 1);
+    engine_->previous_coordinates_.Init(1, 1);
+    engine_->ro_bfgs_.Init(1);
+    engine_->s_bfgs_.Init();
+    engine_->y_bfgs_.Init();
+    NOTIFY("TestFeasibilityGradient() passed!!\n"); 
   }
- 
+  void TestComputeLocalOptimumBFGS1() {
+    NOTIFY("Testing ComputeLocalOptimumBFGS() ...\n");
+    engine_->Init("test_data_3_100.csv", 5);
+    engine_->set_new_dimension(3);
+    engine_->set_sigma(100);
+    engine_->set_gamma(3);
+    engine_->set_eta(0.25);
+    engine_->set_distance_tolerance(1e-5);
+    engine_->set_gradient_tolerance(1e-5);
+    engine_->set_mem_bfgs(150);
+    engine_->ComputeLocalOptimumBFGS<2>();
+    data::Save("results.csv", engine_->coordinates());
+    NOTIFY("TestComputeLocalOptimumBFGS1() passed!!\n");
+  }
+  void TestComputeLocalOptimumBFGS2() {
+    NOTIFY("Testing ComputeLocalOptimumBFGS() ...\n");
+    engine_->Init("test_data_3_100.csv", 5);
+    engine_->set_new_dimension(2);
+    engine_->set_sigma(100);
+    engine_->set_gamma(3);
+    engine_->set_eta(0.25);
+    engine_->set_distance_tolerance(1e-5);
+    engine_->set_gradient_tolerance(1e-5);
+    engine_->set_mem_bfgs(150);
+    engine_->ComputeLocalOptimumBFGS<2>();
+    data::Save("results.csv", engine_->coordinates());
+    NOTIFY("TestComputeLocalOptimumBFGS2() passed!!\n");
+  }
+  void TestComputeLocalOptimumBFGS3() {
+    NOTIFY("Testing ComputeLocalOptimumBFGS() ...\n");
+    engine_->Init("test_data_3_1000.csv", 5);
+    engine_->set_new_dimension(2);
+    engine_->set_sigma(1e6);
+    engine_->set_gamma(4);
+    engine_->set_eta(0.9);
+    engine_->set_distance_tolerance(1e-5);
+    engine_->set_gradient_tolerance(1e-5);
+    engine_->set_mem_bfgs(150);
+    engine_->ComputeLocalOptimumBFGS<2>();
+    data::Save("results.csv", engine_->coordinates());
+    NOTIFY("TestComputeLocalOptimumBFGS3() passed!!\n");
+  }
+  void TestComputeLocalOptimumBFGS4() {
+    NOTIFY("Testing ComputeLocalOptimumBFGS() ...\n");
+    engine_->Init("swiss_roll.csv", 5);
+    engine_->set_new_dimension(2);
+    engine_->set_sigma(1e3);
+    engine_->set_gamma(8);
+    engine_->set_eta(0.6);
+    engine_->set_distance_tolerance(1e-3);
+    engine_->set_gradient_tolerance(1e-5);
+    engine_->set_mem_bfgs(150);
+    engine_->ComputeLocalOptimumBFGS<2>();
+    data::Save("results.csv", engine_->coordinates());
+    NOTIFY("TestComputeLocalOptimumBFGS4() passed!!\n");
+  }
   void TestAll() {
     Init();
     TestInit();
     Destruct();
-   // Init();
-   // TestComputeLocalOptimum();
-   //  Destruct();
     Init();
-    TestComputeLocalOptimumBFGS();
+    TestFeasibilityGradient();
+    Destruct();
+    Init();
+    TestComputeLocalOptimumBFGS4();
     Destruct();
   }
   
