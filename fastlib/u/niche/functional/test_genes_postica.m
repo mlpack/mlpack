fb6175cdbac43dc8200a732096c5be5b632d9166
tekhnofiend <nishantmehta.x@gmail.com>
2008-02-17 22:25:19 +0000

    updated niche/functional

--- fastlib/u/niche/functional/test_genes_postica.m
+++ fastlib/u/niche/functional/test_genes_postica.m
@@ -9,33 +9,88 @@ nong1_indices = find(phases ~=g1_phase & phases ~= unknown_phase);
 
 used_scores = pc_scores;
 
-used_scores = used_scores(1:5,:);
+used_scores = used_scores([1],:);
 
 svm_data = [used_scores(:,g1_indices) used_scores(:,nong1_indices)]';
 svm_labels = [1 * ones(length(g1_indices),1);
 	      -1 * ones(length(nong1_indices),1)];
+%{
+correct = 0;
+same_p = 0;
+
+for i = 1:size(svm_data, 1)
+  train_data = svm_data([1:(i-1) (i+1):end]);
+  train_labels = svm_labels([1:(i-1) (i+1):end]);
+  
+  g1_mu = mean(train_data(find(train_labels == 1)));
+  g1_sigma = std(train_data(find(train_labels == 1)));
+  nong1_mu = mean(train_data(find(train_labels == -1)));
+  nong1_sigma = std(train_data(find(train_labels == -1)));
+  
+  p_g1 = normpdf(svm_data(i), g1_mu, g1_sigma);
+  p_nong1 = normpdf(svm_data(i), nong1_mu, nong1_sigma);
+  
+  if svm_labels(i) == 1
+    if p_g1 > p_nong1
+      correct = correct + 1;
+    end
+  else
+    if p_nong1 > p_g1
+      correct = correct + 1;
+    end
+  end
+  
+  if p_g1 == p_nong1
+    same_p = same_p + 1;
+  end
+  
+end
+
+correct / size(svm_data,1)
+%}
+
+
+
 
 % write data so that we can efficiently call svmltrain many times
 svmlwrite('funknet', svm_data, svm_labels);
 
 % set initial svm options %
 svm_options = ...
-    svmlopt('Kernel', 2, 'KernelParam', .01, 'C', .2, 'ComputeLOO', 1, ...
+    svmlopt('Kernel', 2, 'KernelParam', 3, 'C', .2, 'ComputeLOO', 1, ...
 	    'ExecPath','/home/niche/matlab/toolboxes/svml');
 
 
 % let's do a retarded random sampling of sigma and C on a grid!
 
 
-num_sigma_epochs = 100;
-num_C_epochs = 100;
+num_sigma_epochs = 75;
+%100;
+sigma_init = 1e-3;
+%1e-4;
+sigma_grow = 1.1;
+%num_order_epochs = 4;
+%order_init = 1;
+%order_inc = 1;
+
+num_C_epochs = 3;
+%81;
+C_init = 30;
+%1e-2;
+C_grow = 1 + 1/3;
+%1.1;
+
+
 
 loocv_errors = zeros(num_sigma_epochs, num_C_epochs);
+%loocv_errors = zeros(num_order_epochs, num_C_epochs);
 
 
-sigma = 1e-4;
+sigma = sigma_init;
+%order = order_init;
 for sigma_epoch = 1:num_sigma_epochs
-  C = 1e-3;
+  %for order_epoch = 1:num_order_epochs
+  C = C_init;
   for C_epoch = 1:num_C_epochs
     
     %  C = 10 * exp(-rand * 10);
@@ -44,6 +99,9 @@ for sigma_epoch = 1:num_sigma_epochs
     % set option for C (the regularization parameter)
     svm_options = ...
 	svmlopt(svm_options, 'KernelParam', sigma, 'C', C);
+    %    svm_options = ...
+    %	svmlopt(svm_options, 'KernelParam', order, 'C', C);
+    
     
     latestSVM = svml('latestSVM', svm_options);
     
@@ -53,18 +111,20 @@ for sigma_epoch = 1:num_sigma_epochs
     load loocv_error.txt
 
     C_array(C_epoch) = C;
-    C = C * 1.1;
+    C = C * C_grow;
     
 
 
     loocv_errors(sigma_epoch, C_epoch) = loocv_error;
+    %loocv_errors(order_epoch, C_epoch) = loocv_error;
     
-    %  C = C * 1.01; % geometrically increase C
-    %  C_epoch = C_epoch + 1;
   end
   
   sigma_array(sigma_epoch) = sigma;  
-  sigma = sigma * 1.1;
+  sigma = sigma * sigma_grow;
+  %order_array(order_epoch) = order;  
+  %order = order + order_inc;
+
 end
 
 % for i=1:size(svm_data, 1)
@@ -81,3 +141,4 @@ end
 % cluster genes using ICs?
 
 % find a way to do fPCA/fICA using multiple sets of curves
+
