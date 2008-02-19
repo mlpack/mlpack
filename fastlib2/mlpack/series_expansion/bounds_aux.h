a80c88ce9a7467f67993ce2220aeefd200867e39
Dongryeol Lee <drselee@gmail.com>
2008-02-19 02:53:02 +0000

    Change in bound computing function

--- fastlib2/mlpack/series_expansion/bounds_aux.h
+++ fastlib2/mlpack/series_expansion/bounds_aux.h
@@ -2,7 +2,6 @@
 #define BOUNDS_AUX_H
 
 #include "fastlib/fastlib_int.h"
-#include "tree/bounds.h"
 
 namespace bounds_aux {
   
@@ -10,10 +9,11 @@ namespace bounds_aux {
   void MaxDistanceSq(const DHrectBound<t_pow> &bound1, 
 		     const DHrectBound<t_pow> &bound2,
 		     Vector &furthest_point_in_bound1,
-		     double *furthest_dsqd) {
+		     double &furthest_dsqd) {
 		       
     int dim = furthest_point_in_bound1.length();
-    *furthest_dsqd = 0;
+    furthest_dsqd = 0;
+
     for (index_t d = 0; d < dim; d++) {
       
       const DRange &bound1_range = bound1.get(d);
@@ -31,9 +31,39 @@ namespace bounds_aux {
 	furthest_point_in_bound1[d] = bound1_range.hi;
 	v = v2;
       }
-      (*furthest_dsqd) += math::PowAbs<t_pow, 1>(v); // v is non-negative
+      furthest_dsqd += math::PowAbs<t_pow, 1>(v); // v is non-negative
     }
-    *furthest_dsqd = math::Pow<2, t_pow>(*furthest_dsqd);
+    furthest_dsqd = math::Pow<2, t_pow>(furthest_dsqd);
+  }
+
+  template<int t_pow>
+  void MaxDistanceSq(const DHrectBound<t_pow> &bound1, 
+		     Vector &bound2_centroid,
+		     Vector &furthest_point_in_bound1,
+		     double &furthest_dsqd) {
+		       
+    int dim = furthest_point_in_bound1.length();
+    furthest_dsqd = 0;
+
+    for (index_t d = 0; d < dim; d++) {
+      
+      const DRange &bound1_range = bound1.get(d);
+      
+      double v1 = bound2_centroid[d] - bound1_range.lo;
+      double v2 = bound1_range.hi - bound2_centroid[d];
+      double v;
+      
+      if(v1 > v2) {
+	furthest_point_in_bound1[d] = bound1_range.lo;
+	v = v1;
+      }
+      else {
+	furthest_point_in_bound1[d] = bound1_range.hi;
+	v = v2;
+      }
+      furthest_dsqd += math::PowAbs<t_pow, 1>(v); // v is non-negative
+    }
+    furthest_dsqd = math::Pow<2, t_pow>(furthest_dsqd);
   }
 };
 
