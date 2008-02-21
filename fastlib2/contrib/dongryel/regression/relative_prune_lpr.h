a3568c4026f17dff8e1c11e410f1832af3b7ceb4
Dongryeol Lee <drselee@gmail.com>
2008-02-21 19:46:51 +0000

    Fix in the bug in pruning

--- fastlib2/contrib/dongryel/regression/relative_prune_lpr.h
+++ fastlib2/contrib/dongryel/regression/relative_prune_lpr.h
@@ -73,10 +73,14 @@ class RelativePruneLpr {
 		       &tmp_numerator_dl);
       double new_numerator_norm_l = qnode->stat().numerator_norm_l_ + 
 	MatrixUtil::EntrywiseLpNorm(tmp_numerator_dl, 1);
+      double new_numerator_used_error = qnode->stat().numerator_used_error_ +
+	qnode->stat().postponed_numerator_used_error_;
+      double new_numerator_n_pruned = qnode->stat().numerator_n_pruned_ +
+	qnode->stat().postponed_numerator_n_pruned_;
+
       double numerator_allowed_err = 
-	(relative_error * new_numerator_norm_l - 
-	 qnode->stat().numerator_used_error_) /
-	(numerator_total_alloc_error - qnode->stat().numerator_n_pruned_);
+	(relative_error * new_numerator_norm_l - new_numerator_used_error) /
+	(numerator_total_alloc_error - new_numerator_n_pruned);
 
       // Refine the lower bound norm using the new lower bound info
       // for the denominator B^T W(q) B.
@@ -84,12 +88,17 @@ class RelativePruneLpr {
 		       &tmp_denominator_dl);
       double new_denominator_norm_l = qnode->stat().denominator_norm_l_ + 
 	MatrixUtil::EntrywiseLpNorm(tmp_denominator_dl, 1);
+      double new_denominator_used_error = 
+	qnode->stat().denominator_used_error_ +
+	qnode->stat().postponed_denominator_used_error_;
+      double new_denominator_n_pruned =	qnode->stat().denominator_n_pruned_ +
+	qnode->stat().postponed_denominator_n_pruned_;
+
       double denominator_allowed_err = 
 	(relative_error * new_denominator_norm_l - 
-	 qnode->stat().denominator_used_error_) /
-	(denominator_total_alloc_error - qnode->stat().denominator_n_pruned_);
+	 new_denominator_used_error) /
+	(denominator_total_alloc_error - new_denominator_n_pruned);
         
-      /*
       // Refine the bound using the new info for the weight diagram
       // numerator matrix.
       la::AddOverwrite(qnode->stat().postponed_weight_diagram_numerator_l_, 
@@ -98,17 +107,21 @@ class RelativePruneLpr {
       double new_weight_diagram_numerator_norm_l = 
 	qnode->stat().weight_diagram_numerator_norm_l_ + 
 	MatrixUtil::EntrywiseLpNorm(tmp_weight_diagram_numerator_dl, 1);
+      double new_weight_diagram_numerator_used_error =
+	qnode->stat().weight_diagram_numerator_used_error_ +
+	qnode->stat().postponed_weight_diagram_numerator_used_error_;
+
       double weight_diagram_numerator_allowed_err = 
 	(relative_error * new_weight_diagram_numerator_norm_l - 
-	 qnode->stat().weight_diagram_numerator_used_error_) /
-	(denominator_total_alloc_error - qnode->stat().denominator_n_pruned_);
-      */
+	 new_weight_diagram_numerator_used_error) /
+	(denominator_total_alloc_error - new_denominator_n_pruned);
 
       // this is error per each query/reference pair for a fixed query
       // for the numerator and the denominator used for computing the
       // regression estimates.
       double kernel_error = 0.5 * kernel_value_range.width();
-      
+      double squared_kernel_error = 0.5 * squared_kernel_value_range.width();
+
       // This is the error per each query/reference pair for a fixed
       // query for the weight diagram numerator matrix approximation.
       //double squared_kernel_error = 0.5 * squared_kernel_value_range.width();
@@ -125,19 +138,62 @@ class RelativePruneLpr {
 	(rnode->stat().sum_data_outer_products_error_norm_);
       denominator_n_pruned = rnode->stat().sum_data_outer_products_alloc_norm_;
 
-      /*
       // The total norm error for each query point for approximating
       // the B^T W(q)^2 B matrix.
       weight_diagram_numerator_used_error = squared_kernel_error *
 	(rnode->stat().sum_data_outer_products_error_norm_);
-      */
 
       // Check pruning condition. Note that this pruning criterion
       // does not enforce error directly on the weight diagram
       // computation.
       return (numerator_used_error <= numerator_allowed_err &&
-	      denominator_used_error <= denominator_allowed_err);
+	      denominator_used_error <= denominator_allowed_err &&
+	      weight_diagram_numerator_used_error <= 
+	      weight_diagram_numerator_allowed_err);
     }
+
+    template<typename QueryTree, typename ReferenceTree>
+    bool Prunable(double relative_error, double total_alloc_error,
+		  QueryTree *qnode, ReferenceTree *rnode, 
+		  const DRange &dsqd_range, const DRange &kernel_value_range, 
+		  Vector &delta_l, Vector &delta_e, double &delta_used_error,
+		  double &delta_n_pruned) {
+      
+      // Compute the vector component lower and upper bound changes. This
+      // assumes that the maximum kernel value is 1.
+      la::ScaleOverwrite(kernel_value_range.lo,
+			 rnode->stat().sum_targets_weighted_by_data_,
+			 &delta_l);
+      la::ScaleOverwrite(kernel_value_range.mid(),
+			 rnode->stat().sum_targets_weighted_by_data_,
+			 &delta_e);
+      
+      // Compute the L1 norm of the most refined lower bound.
+      double new_ll_vector_norm_l = 
+	qnode->stat().ll_vector_norm_l_ +
+	MatrixUtil::EntrywiseLpNorm(qnode->stat().postponed_ll_vector_l_, 1) +
+	MatrixUtil::EntrywiseLpNorm(delta_l, 1);
+      double new_ll_vector_used_error = qnode->stat().ll_vector_used_error_ +
+	qnode->stat().postponed_ll_vector_used_error_;
+      double new_ll_vector_n_pruned = qnode->stat().ll_vector_n_pruned_ +
+	qnode->stat().postponed_ll_vector_n_pruned_;
+
+      // Compute the allowed amount of error for pruning the given query
+      // and reference pair.
+      double allowed_err = 
+	(relative_error * new_ll_vector_norm_l - new_ll_vector_used_error) /
+	(total_alloc_error - new_ll_vector_n_pruned);
+
+      // Record how much error and pruned portion will be if pruning
+      // were to succeed.
+      delta_used_error = 0.5 * kernel_value_range.width() * 
+	(rnode->stat().sum_target_weighted_data_error_norm_);
+      delta_n_pruned = rnode->stat().sum_target_weighted_data_error_alloc_;
+      
+      // check pruning condition  
+      return (delta_used_error <= allowed_err);
+    }
+  
 };
 
 #endif
