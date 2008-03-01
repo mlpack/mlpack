08e82abbcec95681a03edfd63b0ae3e4b50ba7e0
Dongryeol Lee <drselee@gmail.com>
2008-03-01 00:56:11 +0000

    Added a utility for computing the coordinate of the point that is farthest away from a given point inside a given bounding ball

--- fastlib2/mlpack/series_expansion/bounds_aux.h
+++ fastlib2/mlpack/series_expansion/bounds_aux.h
@@ -65,6 +65,35 @@ namespace bounds_aux {
     }
     furthest_dsqd = math::Pow<2, t_pow>(furthest_dsqd);
   }
+
+  template<int t_pow, typename TVector>
+  void MaxDistanceSq(const DBallBound < LMetric<t_pow>, TVector > &bound1, 
+		     Vector &bound2_centroid,
+		     Vector &furthest_point_in_bound1,
+		     double &furthest_dsqd) {
+		       
+    furthest_dsqd = 0;
+    
+    // First compute the distance between the centroid of the bounding
+    // ball and the given point.
+    double distance = 
+      LMetric<t_pow>::Distance(bound1.center(), bound2_centroid);
+    
+    // Compute the unit vector that has the same direction as the
+    // vector pointing from the given point to the bounding ball
+    // center.
+    Vector unit_vector;
+    la::SubInit(bound2_centroid, bound1.center(), &unit_vector);
+    la::Scale(1.0 / distance, &unit_vector);
+
+    furthest_point_in_bound1.CopyValues(bound1.center());
+    la::AddExpert(bound1.radius(), unit_vector, &furthest_point_in_bound1);
+    
+    furthest_dsqd = math::Pow<2, t_pow>
+      (la::RawLMetric<t_pow>(bound2_centroid.length(), 
+			     furthest_point_in_bound1.ptr(), 
+			     bound2_centroid.ptr()));
+  }
 };
 
 #endif
