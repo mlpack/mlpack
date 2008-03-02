c69e48ef2b50c2c1e3e3231906034c27a86a9998
Dongryeol Lee <drselee@gmail.com>
2008-03-02 22:13:27 +0000

    Some work for setting up the experiment for the dense version of the algorithm has been completed. I now need to look at the krylov subspace version for correctness since I do some fancy stuff for the normalization constants

--- fastlib2/contrib/dongryel/regression/dense_lpr_main.cc
+++ fastlib2/contrib/dongryel/regression/dense_lpr_main.cc
@@ -26,12 +26,15 @@ int main(int argc, char *argv[]) {
 
   // The reference data file is a required parameter.
   const char* references_file_name = fx_param_str
-    (NULL, "data", "alldata_deltacolors_stdized");
+    (NULL, "data", 
+     "/net/hc293/dongryel/Research/RegressionDatasets/alldata_deltacolors_stdized");
   
   // The file containing the reference target values is a required
   // parameter.
   const char* reference_targets_file_name = 
-    fx_param_str(NULL, "dtarget", "alldata_zs");
+    fx_param_str
+    (NULL, "dtarget", 
+     "/net/hc293/dongryel/Research/RegressionDatasets/alldata_zs");
 
   // The query data file defaults to the references.
   const char* queries_file_name =
@@ -56,6 +59,8 @@ int main(int argc, char *argv[]) {
 
   // Store the results computed by the tree-based results.
   Vector fast_lpr_results;
+  ArrayList<DRange> fast_lpr_confidence_bands;
+  double fast_lpr_root_mean_square_deviation = 0;
   bool fast_lpr_has_run = false;
   if(!strcmp(fx_param_str_req(lpr_module, "method"), "dt-dense-quick")) {
     printf("Running the DT-DENSE-LPR with Deng and Moore's prune rule.\n");
@@ -65,8 +70,10 @@ int main(int argc, char *argv[]) {
     fast_lpr.get_regression_estimates(&fast_lpr_results);
     printf("Finished the DT-DENSE-LPR with Deng and Moore's prune rule.\n");
     printf("Root mean square deviation of %g...\n",
-	   fast_lpr.root_mean_square_deviation());
+	   (fast_lpr_root_mean_square_deviation = 
+	    fast_lpr.root_mean_square_deviation()));
     fast_lpr_has_run = true;
+    fast_lpr.get_confidence_bands(&fast_lpr_confidence_bands);
   }
   else if(!strcmp(fx_param_str_req(lpr_module, "method"), "st-dense-quick")) {
     printf("Running the ST-DENSE-LPR with Deng and Moore's prune rule.\n");
@@ -76,8 +83,10 @@ int main(int argc, char *argv[]) {
     fast_lpr.get_regression_estimates(&fast_lpr_results);
     printf("Finished the ST-DENSE-LPR with Deng and Moore's prune rule.\n");
     printf("Root mean square deviation of %g...\n",
-	   fast_lpr.root_mean_square_deviation());
+	   (fast_lpr_root_mean_square_deviation = 
+	    fast_lpr.root_mean_square_deviation()));
     fast_lpr_has_run = true;
+    fast_lpr.get_confidence_bands(&fast_lpr_confidence_bands);
   }
   else if(!strcmp(fx_param_str_req(lpr_module, "method"), 
 		  "dt-dense-relative")) {
@@ -89,8 +98,10 @@ int main(int argc, char *argv[]) {
     fast_lpr.get_regression_estimates(&fast_lpr_results);
     printf("Finished the DT-DENSE-LPR algorithm with relative prune rule.\n");
     printf("Root mean square deviation of %g...\n",
-	   fast_lpr.root_mean_square_deviation());
+	   (fast_lpr_root_mean_square_deviation = 
+	    fast_lpr.root_mean_square_deviation()));
     fast_lpr_has_run = true;
+    fast_lpr.get_confidence_bands(&fast_lpr_confidence_bands);
   }
   else if(!strcmp(fx_param_str_req(lpr_module, "method"), 
 		  "st-dense-relative")) {
@@ -102,31 +113,65 @@ int main(int argc, char *argv[]) {
     fast_lpr.get_regression_estimates(&fast_lpr_results);
     printf("Finished the ST-DENSE-LPR algorithm with relative prune rule.\n");
     printf("Root mean square deviation of %g...\n",
-	   fast_lpr.root_mean_square_deviation());
+	   (fast_lpr_root_mean_square_deviation = 
+	    fast_lpr.root_mean_square_deviation()));
     fast_lpr_has_run = true;
+    fast_lpr.get_confidence_bands(&fast_lpr_confidence_bands);
   }
 
   // Do naive algorithm.
   printf("Running the naive algorithm...\n");
   Vector naive_lpr_results;
   NaiveLpr<EpanKernel> naive_lpr;
+  ArrayList<DRange> naive_lpr_confidence_bands;
   naive_lpr.Init(references, reference_targets, lpr_module);
   naive_lpr.PrintDebug();
   naive_lpr.get_regression_estimates(&naive_lpr_results);
+  naive_lpr.get_confidence_bands(&naive_lpr_confidence_bands);
   printf("Finished running the naive algorithm...\n");
   printf("Got root mean square deviation of %g...\n",
 	 naive_lpr.root_mean_square_deviation());
 
   if(fast_lpr_has_run) {
-    printf("Maximum relative difference in regression estimate: %g\n",
-	   MatrixUtil::MaxRelativeDifference(naive_lpr_results,
-					     fast_lpr_results));
+    fx_format_result(lpr_module, "max_relative_difference_regression_estimate",
+		     "%g", MatrixUtil::MaxRelativeDifference
+		     (naive_lpr_results, fast_lpr_results));
+    fx_format_result(lpr_module, "avg_relative_difference_regression_estimate",
+		     "%g", MatrixUtil::AvgRelativeDifference
+		     (naive_lpr_results, fast_lpr_results));
+
+    fx_format_result
+      (lpr_module, "relative_error_root_mean_square_deviation", "%g",
+       fabs(fast_lpr_root_mean_square_deviation -
+	    naive_lpr.root_mean_square_deviation()) /
+       fabs(naive_lpr.root_mean_square_deviation()));
+
+    DRange confidence_band_max_relative_difference =
+      MatrixUtil::MaxRelativeDifference
+      (naive_lpr_confidence_bands, fast_lpr_confidence_bands);
+    DRange confidence_band_avg_relative_difference =
+      MatrixUtil::AvgRelativeDifference
+      (naive_lpr_confidence_bands, fast_lpr_confidence_bands);
+
+    fx_format_result
+      (lpr_module, "lower_quantile_avg_relative_error", "%g",
+       confidence_band_avg_relative_difference.lo);
+    fx_format_result
+      (lpr_module, "lower_quantile_max_relative_error", "%g",
+       confidence_band_max_relative_difference.lo);
+    fx_format_result
+      (lpr_module, "upper_quantile_avg_relative_error", "%g",
+       confidence_band_avg_relative_difference.hi);
+    fx_format_result
+      (lpr_module, "upper_quantile_max_relative_error", "%g",
+       confidence_band_max_relative_difference.hi);
   }
   else {
 
     // This is to prevent the code from complaining from uninitialized
     // object stuff.
     fast_lpr_results.Init(0);
+    fast_lpr_confidence_bands.Init();
   }
 
   // Finalize FastExec and print output results.
