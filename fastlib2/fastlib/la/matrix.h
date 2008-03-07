d62cc74f2c2d464f7ffbff9fd7631c40cf96d9b9
rriegel <rriegel@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-03-07 03:59:59 +0000

    templatized matrices

--- fastlib2/fastlib/la/matrix.h
+++ fastlib2/fastlib/la/matrix.h
@@ -38,21 +38,22 @@
  * assert(orig[4] == 9);
  * @endcode
  */
-class Vector {
+template<typename T>
+class GenVector {
  private:
   /** The pointer to the array of doubles. */
-  double *ptr_;
+  T *ptr_;
   /** The length of the vector. */
   index_t length_;
   /** Whether this should be freed, i.e. it is not an alias. */
   bool should_free_;
   
-  OT_DEF_ONLY(Vector) {
+  OT_DEF_ONLY(GenVector) {
     OT_MY_OBJECT(length_);
     OT_MALLOC_ARRAY(ptr_, length_);
   }
   
-  OT_FIX(Vector) {
+  OT_FIX(GenVector) {
     should_free_ = true;
   }
   
@@ -60,23 +61,23 @@ class Vector {
   /**
    * Creates a completely uninitialized Vector which must be initialized.
    */
-  Vector() {
+  GenVector() {
     DEBUG_ONLY(Uninitialize_());
   }
   
   /**
    * Copy constructor -- for use in collections.
    */
-  Vector(const Vector& other) {
+  GenVector(const GenVector& other) {
     DEBUG_ONLY(Uninitialize_());
     Copy(other);
   }
-  ASSIGN_VIA_COPY_CONSTRUCTION(Vector);
+  ASSIGN_VIA_COPY_CONSTRUCTION(GenVector);
   
   /**
    * Destroys the Vector, freeing the memory if this copy is not an alias.
    */
-  ~Vector() {
+  ~GenVector() {
     Destruct();
   }
   
@@ -84,7 +85,7 @@ class Vector {
    * Uninitializes so that you can call another initializer.
    */
   void Destruct() {
-    DEBUG_ASSERT_MSG(ptr_ != BIG_BAD_POINTER(double),
+    DEBUG_ASSERT_MSG(ptr_ != BIG_BAD_POINTER(T),
        "You forgot to initialize a Vector before it got automatically freed.");
     
     /* mark slow case as "unlikely" even if it might be the likely case */
@@ -101,7 +102,7 @@ class Vector {
    * values in it.
    */
   void Init(index_t in_length) {
-    ptr_ = mem::Alloc<double>(in_length);
+    ptr_ = mem::Alloc<T>(in_length);
     length_ = in_length;
     should_free_ = true;
   }
@@ -109,7 +110,7 @@ class Vector {
   /**
    * Sets all elements to the same value.
    */
-  void SetAll(double d) {
+  void SetAll(T d) {
     mem::RepeatConstruct(ptr_, d, length_);
   }
   
@@ -126,7 +127,7 @@ class Vector {
    *
    * @param other the vector to explicitly copy
    */
-  void Copy(const Vector& other) {
+  void Copy(const GenVector& other) {
     Copy(other.ptr(), other.length());
   }
 
@@ -136,7 +137,7 @@ class Vector {
    * @param doubles the array of doubles to copy
    * @param in_length the number of doubles in the array
    */
-  void Copy(const double *doubles, index_t in_length) {
+  void Copy(const T *doubles, index_t in_length) {
     DEBUG_ONLY(AssertUninitialized_());
     
     ptr_ = mem::AllocBitCopy(doubles, in_length);
@@ -147,7 +148,7 @@ class Vector {
   /**
    * Alias a particular memory region of doubles.
    */
-  void Alias(double *in_ptr, index_t in_length) {
+  void Alias(T *in_ptr, index_t in_length) {
     DEBUG_ONLY(AssertUninitialized_());
     
     ptr_ = in_ptr;
@@ -158,7 +159,7 @@ class Vector {
   /**
    * Implements the "Copiable" interface using .
    */
-  void WeakCopy(const Vector& other) {
+  void WeakCopy(const GenVector& other) {
     Alias(other);
   }
   
@@ -167,7 +168,7 @@ class Vector {
    *
    * @param other the other vector
    */
-  void Alias(const Vector& other) {
+  void Alias(const GenVector& other) {
     // we trust in good faith that a const vector won't be abused
     Alias(other.ptr_, other.length());
   }
@@ -178,7 +179,7 @@ class Vector {
    *
    * @param other a pointer to the vector whose contents will be owned
    */
-  void Own(Vector* other) {
+  void Own(GenVector* other) {
     Own(other->ptr_, other->length());
     
     DEBUG_ASSERT(other->should_free_);
@@ -189,7 +190,7 @@ class Vector {
    * Become owner of a particular pointer in memory that was allocated
    * with mem::Alloc<double>.
    */
-  void Own(double *in_ptr, index_t in_length) {
+  void Own(T *in_ptr, index_t in_length) {
     DEBUG_ONLY(AssertUninitialized_());
     
     ptr_ = in_ptr;
@@ -205,7 +206,7 @@ class Vector {
    * @param len the length
    * @param dest an UNINITIALIZED vector to use
    */
-  void MakeSubvector(index_t start_index, index_t len, Vector* dest) {
+  void MakeSubvector(index_t start_index, index_t len, GenVector* dest) {
     DEBUG_BOUNDS(start_index, length_);
     DEBUG_BOUNDS(start_index + len, length_ + 1);
     
@@ -220,7 +221,7 @@ class Vector {
    *
    * @param other an identically sized vector to swap values with
    */
-  void SwapValues(Vector* other) {
+  void SwapValues(GenVector* other) {
     DEBUG_ASSERT(length() == other->length());
     mem::BitSwap(ptr_, other->ptr_, length_);
   }
@@ -230,7 +231,7 @@ class Vector {
    *
    * @param other the vector to copy from
    */
-  void CopyValues(const Vector& other) {
+  void CopyValues(const GenVector& other) {
     DEBUG_ASSERT(length() == other.length());
     mem::BitCopy(ptr_, other.ptr_, length_);
   }
@@ -241,7 +242,7 @@ class Vector {
    * @param src_ptr the vector to copy from, must have at least
    *        length() elements
    */
-  void CopyValues(const double *src_ptr) {
+  void CopyValues(const T *src_ptr) {
     mem::BitCopy(ptr_, src_ptr, length_);
   }
   
@@ -268,21 +269,21 @@ class Vector {
   /**
    * A pointer to the C-style array containing the elements of this vector.
    */
-  double *ptr() {
+  T *ptr() {
     return ptr_;
   }
   
   /**
    * A pointer to the C-style array containing the elements of this vector.
    */
-  const double *ptr() const {
+  const T *ptr() const {
     return ptr_;
   }
   
   /**
    * Gets the i'th element of this vector.
    */
-  double operator [] (index_t i) const {
+  T operator [] (index_t i) const {
     DEBUG_BOUNDS(i, length_);
     return ptr_[i];
   }
@@ -290,7 +291,7 @@ class Vector {
   /**
    * Gets a mutable reference to the i'th element of this vector.
    */
-  double &operator [] (index_t i) {
+  T &operator [] (index_t i) {
     DEBUG_BOUNDS(i, length_);
     return ptr_[i];
   }
@@ -308,7 +309,7 @@ class Vector {
    * }
    * @endcode
    */
-  double get(index_t i) const {
+  T get(index_t i) const {
     DEBUG_BOUNDS(i, length_);
     return ptr_[i];
   }
@@ -319,15 +320,17 @@ class Vector {
   }
   
   void Uninitialize_() {
-    DEBUG_ONLY(ptr_ = BIG_BAD_POINTER(double));
+    DEBUG_ONLY(ptr_ = BIG_BAD_POINTER(T));
     DEBUG_ONLY(length_ = BIG_BAD_NUMBER);
   }
   
   void AssertInitialized_() {
-    DEBUG_ASSERT_MSG(ptr_ != BIG_BAD_POINTER(double),
+    DEBUG_ASSERT_MSG(ptr_ != BIG_BAD_POINTER(T),
         "Vector was not initialized.");
   }
 };
+
+typedef GenVector<double> Vector;
 
 /**
  * Double-precision column-major matrix for use with LAPACK.
@@ -339,10 +342,11 @@ class Vector {
  * TODO: If it's not entirely obvious or well documented how to use this
  * class please let the FASTlib people know.
  */
-class Matrix {
+template<typename T>
+class GenMatrix {
  private:
   /** Linearized matrix (column-major). */
-  double *ptr_;
+  T *ptr_;
   /** Number of rows. */
   index_t n_rows_;
   /** Number of columns. */
@@ -350,13 +354,13 @@ class Matrix {
   /** Whether I am a strong copy (not an alias). */
   bool should_free_;
 
-  OT_DEF_ONLY(Matrix) {
+  OT_DEF_ONLY(GenMatrix) {
     OT_MY_OBJECT(n_rows_);
     OT_MY_OBJECT(n_cols_);
     OT_MALLOC_ARRAY(ptr_, n_elements());
   }
 
-  OT_FIX(Matrix) {
+  OT_FIX(GenMatrix) {
     should_free_ = true;
   }
 
@@ -364,7 +368,7 @@ class Matrix {
   /**
    * Creates a Matrix with uninitialized elements of the specified size.
    */
-  Matrix(index_t in_rows, index_t in_cols) {
+  GenMatrix(index_t in_rows, index_t in_cols) {
     DEBUG_ONLY(Uninitialize_());
     Init(in_rows, in_cols);
   }
@@ -372,23 +376,23 @@ class Matrix {
   /**
    * Copy constructor -- for use in collections.
    */
-  Matrix(const Matrix& other) {
+  GenMatrix(const GenMatrix<T>& other) {
     DEBUG_ONLY(Uninitialize_());
     Copy(other);
   }
-  ASSIGN_VIA_COPY_CONSTRUCTION(Matrix);
+  ASSIGN_VIA_COPY_CONSTRUCTION(GenMatrix);
 
   /**
    * Creates a matrix that can be initialized.
    */
-  Matrix() {
+  GenMatrix() {
     DEBUG_ONLY(Uninitialize_());
   }
 
   /**
    * Empty destructor.
    */
-  ~Matrix() {
+  ~GenMatrix() {
     Destruct();
   }
   
@@ -397,7 +401,7 @@ class Matrix {
    * on this again.
    */
   void Destruct() {
-    DEBUG_ASSERT_MSG(ptr_ != BIG_BAD_POINTER(double),
+    DEBUG_ASSERT_MSG(ptr_ != BIG_BAD_POINTER(T),
        "You forgot to initialize a Matrix before it got automatically freed.");
     if (unlikely(should_free_)) {
       mem::DebugPoison(ptr_, n_rows_ * n_cols_);
@@ -414,7 +418,7 @@ class Matrix {
    */
   void Init(index_t in_rows, index_t in_cols) {
     DEBUG_ONLY(AssertUninitialized_());
-    ptr_ = mem::Alloc<double>(in_rows * in_cols);
+    ptr_ = mem::Alloc<T>(in_rows * in_cols);
     n_rows_ = in_rows;
     n_cols_ = in_cols;
     should_free_ = true;
@@ -423,7 +427,7 @@ class Matrix {
   /**
    * Creates a diagonal matrix.
    */
-  void InitDiagonal(const Vector& v) {
+  void InitDiagonal(const GenVector<T>& v) {
     Init(v.length(), v.length());
     SetDiagonal(v);
   }
@@ -431,7 +435,7 @@ class Matrix {
   /**
    * Sets the entire matrix to zero.
    */
-  void SetAll(double d) {
+  void SetAll(T d) {
     mem::RepeatConstruct(ptr_, d, n_elements());
   }
 
@@ -447,7 +451,7 @@ class Matrix {
   /**
    * Makes this a diagonal matrix whose diagonals are the values in v.
    */
-  void SetDiagonal(const Vector& v) {
+  void SetDiagonal(const GenVector<T>& v) {
     DEBUG_ASSERT(n_rows() == v.length());
     DEBUG_ASSERT(n_cols() == v.length());
     SetZero();
@@ -462,7 +466,7 @@ class Matrix {
    *
    * @param other the vector to explicitly copy
    */
-  void Copy(const Matrix& other) {
+  void Copy(const GenMatrix& other) {
     Copy(other.ptr(), other.n_rows(), other.n_cols());    
   }
 
@@ -473,7 +477,7 @@ class Matrix {
    * @param n_rows_in the number of rows
    * @param n_cols_in the number of columns
    */
-  void Copy(const double *ptr_in, index_t n_rows_in, index_t n_cols_in) {
+  void Copy(const T *ptr_in, index_t n_rows_in, index_t n_cols_in) {
     DEBUG_ONLY(AssertUninitialized_());
     
     ptr_ = mem::AllocBitCopy(ptr_in, n_rows_in * n_cols_in);
@@ -489,7 +493,7 @@ class Matrix {
    *
    * @param other the other vector
    */
-  void Alias(const Matrix& other) {
+  void Alias(const GenMatrix& other) {
     // we trust in good faith that const-ness won't be abused
     Alias(other.ptr_, other.n_rows(), other.n_cols());
   }
@@ -501,7 +505,7 @@ class Matrix {
    * @param n_rows_in the number of rows
    * @param n_cols_in the number of columns
    */
-  void Alias(double *ptr_in, index_t n_rows_in, index_t n_cols_in) {
+  void Alias(T *ptr_in, index_t n_rows_in, index_t n_cols_in) {
     DEBUG_ONLY(AssertUninitialized_());
     
     ptr_ = ptr_in;
@@ -515,8 +519,8 @@ class Matrix {
    *
    * @param row_vector the vector to alias
    */
-  void AliasRowVector(const Vector& row_vector) {
-    Alias(const_cast<double*>(row_vector.ptr()), 1, row_vector.length());
+  void AliasRowVector(const GenVector<T>& row_vector) {
+    Alias(const_cast<T*>(row_vector.ptr()), 1, row_vector.length());
   }
   
   /**
@@ -524,8 +528,8 @@ class Matrix {
    *
    * @param col_vector the vector to alias
    */
-  void AliasColVector(const Vector& col_vector) {
-    Alias(const_cast<double*>(col_vector.ptr()), col_vector.length(), 1);
+  void AliasColVector(const GenVector<T>& col_vector) {
+    Alias(const_cast<T*>(col_vector.ptr()), col_vector.length(), 1);
   }
   
   /**
@@ -533,7 +537,7 @@ class Matrix {
    *
    * This is identical to Alias.
    */
-  void WeakCopy(const Matrix& other) {
+  void WeakCopy(const GenMatrix& other) {
     Alias(other);
   }
   
@@ -545,7 +549,7 @@ class Matrix {
    *
    * @param other a pointer to the other matrix
    */
-  void Own(Matrix* other) {
+  void Own(GenMatrix* other) {
     Own(other->ptr(), other->n_rows(), other->n_cols());
     
     DEBUG_ASSERT(other->should_free_);
@@ -561,7 +565,7 @@ class Matrix {
    * @param n_rows_in the number of rows
    * @param n_cols_in the number of columns
    */
-  void Own(double *ptr_in, index_t n_rows_in, index_t n_cols_in) {
+  void Own(T *ptr_in, index_t n_rows_in, index_t n_cols_in) {
     DEBUG_ONLY(AssertUninitialized_());
     
     ptr_ = ptr_in;
@@ -578,7 +582,7 @@ class Matrix {
    * @param dest an UNINITIALIZED matrix
    */
   void MakeColumnSlice(index_t start_col, index_t n_cols_new,
-      Matrix *dest) const {
+      GenMatrix *dest) const {
     DEBUG_BOUNDS(start_col, n_cols_);
     DEBUG_BOUNDS(start_col + n_cols_new, n_cols_ + 1);
     dest->Alias(ptr_ + start_col * n_rows_,
@@ -606,7 +610,7 @@ class Matrix {
    * @return a reshaped matrix backed by the original
    */
   void MakeReshaped(index_t n_rows_in, index_t n_cols_in,
-      Matrix *dest) const {
+      GenMatrix *dest) const {
     DEBUG_ASSERT(n_rows_in * n_cols_in == n_rows() * n_cols());
     dest->Alias(ptr_, n_rows_in, n_cols_in);
   }
@@ -618,7 +622,7 @@ class Matrix {
    * @param dest a pointer to an uninitialized vector, which will be
    *        initialized as an alias to the particular column
    */
-  void MakeColumnVector(index_t col, Vector *dest) const {
+  void MakeColumnVector(index_t col, GenVector<T> *dest) const {
     DEBUG_BOUNDS(col, n_cols_);
     dest->Alias(n_rows_ * col + ptr_, n_rows_);
   }
@@ -633,7 +637,7 @@ class Matrix {
    *        initialized as an alias to the particular column's subvector
    */
   void MakeColumnSubvector(index_t col, index_t start_row, index_t n_rows_new,
-      Vector *dest) const {
+      GenVector<T> *dest) const {
     DEBUG_BOUNDS(col, n_cols_);
     DEBUG_BOUNDS(start_row, n_rows_);
     DEBUG_BOUNDS(start_row + n_rows_new, n_rows_ + 1);
@@ -648,7 +652,7 @@ class Matrix {
    * @return an array where the i'th element is the i'th row of that
    *         particular column
    */
-  double *GetColumnPtr(index_t col) {
+  T *GetColumnPtr(index_t col) {
     DEBUG_BOUNDS(col, n_cols_);
     return n_rows_ * col + ptr_;
   }
@@ -661,7 +665,7 @@ class Matrix {
    * @return an array where the i'th element is the i'th row of that
    *         particular column
    */
-  const double *GetColumnPtr(index_t col) const {
+  const T *GetColumnPtr(index_t col) const {
     DEBUG_BOUNDS(col, n_cols_);
     return n_rows_ * col + ptr_;
   }
@@ -688,7 +692,7 @@ class Matrix {
    *
    * @param other an identically sized vector to swap values with
    */
-  void SwapValues(Matrix* other) {
+  void SwapValues(GenMatrix* other) {
     DEBUG_ASSERT(n_cols() == other->n_cols());
     DEBUG_ASSERT(n_rows() == other->n_rows());
     mem::BitSwap(ptr_, other->ptr_, n_elements());
@@ -699,7 +703,7 @@ class Matrix {
    *
    * @param other the vector to copy from
    */
-  void CopyValues(const Matrix& other) {
+  void CopyValues(const GenMatrix& other) {
     DEBUG_ASSERT(n_rows() == other.n_rows());
     DEBUG_ASSERT(n_cols() == other.n_cols());
     mem::BitCopy(ptr_, other.ptr_, n_elements());
@@ -728,7 +732,7 @@ class Matrix {
    *
    * This is suitable for BLAS and LAPACK calls.
    */
-  const double *ptr() const {
+  const T *ptr() const {
     return ptr_;
   }
   
@@ -738,7 +742,7 @@ class Matrix {
    *
    * This is suitable for BLAS and LAPACK calls.
    */
-  double *ptr() {
+  T *ptr() {
     return ptr_;
   }
   
@@ -748,7 +752,7 @@ class Matrix {
    * @param r the row number
    * @param c the column number
    */
-  double get(index_t r, index_t c) const {
+  T get(index_t r, index_t c) const {
     DEBUG_BOUNDS(r, n_rows_);
     DEBUG_BOUNDS(c, n_cols_);
     return ptr_[c * n_rows_ + r];
@@ -761,7 +765,7 @@ class Matrix {
    * @param c the column number
    * @param v the value to set
    */ 
-  void set(index_t r, index_t c, double v) {
+  void set(index_t r, index_t c, T v) {
     DEBUG_BOUNDS(r, n_rows_);
     DEBUG_BOUNDS(c, n_cols_);
     ptr_[c * n_rows_ + r] = v;
@@ -774,7 +778,7 @@ class Matrix {
    * part of an array; use ColumnSlice or Reshaped instead to make
    * subsections.
    */
-  double &ref(index_t r, index_t c) {
+  T &ref(index_t r, index_t c) {
     DEBUG_BOUNDS(r, n_rows_);
     DEBUG_BOUNDS(c, n_cols_);
     return ptr_[c * n_rows_ + r];
@@ -858,6 +862,8 @@ class SmallVector : public Vector {
     return array_[i];
   }
 };
+
+typedef GenMatrix<double> Matrix;
 
 /**
  * Low-overhead matrix if size is known at compile time.
