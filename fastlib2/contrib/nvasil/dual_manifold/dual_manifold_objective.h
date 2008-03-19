b2513a25a2e21dc49ec1a1f627efe7e8234b26c1
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-03-19 00:15:23 +0000

    fisrt check in

---
+++ fastlib2/contrib/nvasil/dual_manifold/dual_manifold_objective.h
@@ -0,0 +1,48 @@
+/*
+ * =====================================================================================
+ * 
+ *       Filename:  dual_manifold_objective.h
+ * 
+ *    Description:  
+ * 
+ *        Version:  1.0
+ *        Created:  03/18/2008 07:45:50 PM EDT
+ *       Revision:  none
+ *       Compiler:  gcc
+ * 
+ *         Author:  Nikolaos Vasiloglou (NV), nvasil@ieee.org
+ *        Company:  Georgia Tech Fastlab-ESP Lab
+ * 
+ * =====================================================================================
+ */
+
+#ifndef DUAL_MANIFOLD_OBJECTIVES_H_
+#define DUAL_MANIFOLD_OBJECTIVES_H_
+
+#include "fastlib/fastlib.h"
+#include "../l_bfgs/optimization_utils.h"
+
+class DualMaxVariance {
+ public:
+  void Init(datanode *module, Matrix *other_part, 
+      ArrayList<std::pair<index_t, index_t> > *pairs_to_consider,
+      ArrayList<double> *dot_prod_values);
+  void ComputeGradient(Matrix &coordinates, Matrix *gradient);
+  void ComputeObjective(Matrix &coordinates, double *objective);
+  void ComputeFeasibilityError(Matrix &coordinates, double *error);
+  double ComputeLagrangian(Matrix &coordinates);
+  void UpdateLagrangeMult(Matrix &coordinates);
+  void Project(Matrix *coordinates);
+  void set_sigma(double sigma); 
+  
+ private:
+  datanode *module_;
+  ArrayList<std::pair<index_t, index_t> > *pairs_to_consider_;
+  ArrayList<double> *dot_prod_values_;
+  Matrix *other_part_;
+  double sigma_;
+};
+
+#include "dual_manifold_objective_impl.h"
+#endif // DUAL_MANIFOLD_OBJECTIVES_H_
+
