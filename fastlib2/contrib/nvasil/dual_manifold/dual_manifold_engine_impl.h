77ee59a4390b99d0f2bcaf51c2f1d324a27e02cb
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-03-19 03:48:39 +0000

    Added some extra files

---
+++ fastlib2/contrib/nvasil/dual_manifold/dual_manifold_engine_impl.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ * 
+ *       Filename:  dual_manifold_engine_impl.h
+ * 
+ *    Description:  
+ * 
+ *        Version:  1.0
+ *        Created:  03/18/2008 11:34:02 PM EDT
+ *       Revision:  none
+ *       Compiler:  gcc
+ * 
+ *         Author:  Nikolaos Vasiloglou (NV), nvasil@ieee.org
+ *        Company:  Georgia Tech Fastlab-ESP Lab
+ * 
+ * =====================================================================================
+ */
+
+template<typename OptimizedFunction>
+DualManifoldEngine<OptimizedFunction>::Init(datanode *module,
+    Matrix &matrix1, Matrix &matrix2, 
+    ArrayList<std::pair<index_t, index_t> > &pairs_to_consider,
+    ArrayList<double> &dot_prod_values) {
+
+}
+
