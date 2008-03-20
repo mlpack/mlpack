24d13ef945665d27d0f98e470226ef73db5f19df
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-03-20 15:56:45 +0000

    compiles

--- fastlib2/contrib/nvasil/mvu/build.py
+++ fastlib2/contrib/nvasil/mvu/build.py
@@ -1,3 +1,6 @@
 librule(name="mvu",
 		headers=["mvu_objectives.h", "mvu_objectives_impl.h"],
 		deplibs=["fastlib:fastlib"] )
+binrule(name="ncmvu",
+		sources=["main.cc"],
+		deplibs=["fastlib:fastlib", ":mvu", "contrib/nvasil/l_bfgs:l_bfgs"])
--- fastlib2/contrib/nvasil/mvu/main.cc
+++ fastlib2/contrib/nvasil/mvu/main.cc
@@ -24,22 +24,56 @@
 int main(int argc, char *argv[]){
   fx_init(argc, argv);
   std::string optimized_function=fx_param_str(NULL, "optfun", "mvu");
-  std::string data_file=fx_param_req(NULL, "data_file");
+  std::string data_file=fx_param_str_req(NULL, "data_file");
   Matrix data_mat;
-  data::Load(data_file.c_str(), &data_mat);
+  if (data::Load(data_file.c_str(), &data_mat)==SUCCESS_FAIL) {
+    FATAL("Didn't manage to load %s", data_file.c_str());
+  }
   datanode *optfun_node=fx_param_node(NULL, "optfun");
   datanode *l_bfgs_node=fx_param_node(NULL, "l_bfgs");
-  
-  switch optimized_function {
-    case "mvu":
-      MaxVariance opt_function;
-      opt_function.Init(optfun_node, data_mat);
-      l_bfgs<MaxVariance> engine;
-      engine.Init(&opt_function, l_bfgs_node);
-      engine.ComputeLocalOptimum();
-    case "mfu":
-    case "mvuineq":
-    default:
+  std::string result_file=fx_param_str(NULL, "result_file", "result.csv");
+  bool done=false;
+    
+  if (optimized_function == "mvu") {
+    MaxVariance opt_function;
+    opt_function.Init(optfun_node, data_mat);
+    LBfgs<MaxVariance> engine;
+    engine.Init(&opt_function, l_bfgs_node);
+    engine.ComputeLocalOptimumBFGS();
+    Matrix result;
+    engine.GetResults(&result);
+    if (data::Save(result_file.c_str(), result)==SUCCESS_FAIL) {
+      FATAL("Didn't manage to save %s", result_file.c_str());
+    }
+    done=true;
+  }
+  if (optimized_function=="mfu") {
+    MaxVarianceInequalityOnFurthest opt_function;
+    opt_function.Init(optfun_node, data_mat);
+    LBfgs<MaxVarianceInequalityOnFurthest> engine;
+    engine.Init(&opt_function, l_bfgs_node);
+    engine.ComputeLocalOptimumBFGS();
+    Matrix result;
+    engine.GetResults(&result);
+    if (data::Save(result_file.c_str(), result)==SUCCESS_FAIL) {
+      FATAL("Didn't manage to save %s", result_file.c_str());
+    }
+    done=true;
+  }
+  if (optimized_function == "mvuineq"){
+    MaxFurthestNeighbors opt_function;
+    opt_function.Init(optfun_node, data_mat);
+    LBfgs<MaxFurthestNeighbors> engine;
+    engine.Init(&opt_function, l_bfgs_node);
+    engine.ComputeLocalOptimumBFGS();
+    Matrix result;
+    engine.GetResults(&result);
+    if (data::Save(result_file.c_str(), result)==SUCCESS_FAIL) {
+      FATAL("Didn't manage to save %s", result_file.c_str());
+    }
+    done=true;
+  }
+  if (done==false) {
     FATAL("The method you provided %s is not supported", 
         optimized_function.c_str());
   }
