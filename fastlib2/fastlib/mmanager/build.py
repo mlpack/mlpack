5679851a0944b75f10f3c6331ecc89264e3a810f
Dongryeol Lee <drselee@gmail.com>
2008-04-11 23:59:18 +0000

    Minor editing of the build file

--- fastlib2/fastlib/mmanager/build.py
+++ fastlib2/fastlib/mmanager/build.py
@@ -2,4 +2,5 @@ librule(name="mmapmm",
         tests=["memory_manager_test.cc"],
         headers=["memory_manager.h","../base/common.h"],
         sources=["memory_manager.cc"],
+        deplibs=["fastlib/base:base"]
         )
