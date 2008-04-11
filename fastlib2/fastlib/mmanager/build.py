08f35fb6789737e9bf6fc8ed96c6d16e2b613583
Dongryeol Lee <drselee@gmail.com>
2008-04-11 17:03:30 +0000

    Separated the global variable declarations into a separate cc file

--- fastlib2/fastlib/mmanager/build.py
+++ fastlib2/fastlib/mmanager/build.py
@@ -1,5 +1,5 @@
 librule(name="mmapmm",
         tests=["memory_manager_test.cc"],
-		    headers=["memory_manager.h"],
-				deplibs=["fastlib:fastlib"]
-				)
+        headers=["memory_manager.h","../base/common.h"],
+        sources=["memory_manager.cc"],
+        )
