68f512058d8ebc2d09d0eaed57c2b57be5f4c76c
Dongryeol Lee <drselee@gmail.com>
2008-04-15 18:28:28 +0000

    More compilation fix. Now starting the fix-point algorithm.

--- fastlib2/contrib/dongryel/pca/main.cc
+++ fastlib2/contrib/dongryel/pca/main.cc
@@ -1,17 +1,25 @@
 #include "fastlib/fastlib.h"
 #include "pca.h"
 
+void TestEigenDecomposeCovariance(const Matrix &dataset) {
+  Matrix principal_components;
+  Vector eigen_values;
+  Pca::EigenDecomposeCovariance(dataset, &eigen_values, &principal_components);
+
+  principal_components.PrintDebug();
+
+  eigen_values.PrintDebug();
+}
+
 int main(int argc, char *argv[]) {
  
   fx_init(argc, argv);
   const char *file_name = fx_param_str(NULL, "data", NULL);
   Matrix dataset;
   data::Load(file_name, &dataset);
-
-  Matrix principal_components;
-  Vector eigen_values;
-  Pca::EigenDecomposeCovariance(dataset, &eigen_values, &principal_components);
   
+  TestEigenDecomposeCovariance(dataset);
+
   fx_done();
   return 0;
 }
