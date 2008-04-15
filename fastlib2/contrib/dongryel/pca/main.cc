a67d885678e25cdbdf046cbd95263a7f79324f95
Dongryeol Lee <drselee@gmail.com>
2008-04-15 22:01:41 +0000

    Completed the fixed-point algorithm for PCA

--- fastlib2/contrib/dongryel/pca/main.cc
+++ fastlib2/contrib/dongryel/pca/main.cc
@@ -1,10 +1,27 @@
 #include "fastlib/fastlib.h"
 #include "pca.h"
 
+void TestFixedPointAlgorithm(const Matrix &dataset) {
+  Matrix principal_components;
+  Vector eigen_values;
+  
+  fx_timer_start(fx_root, "fixed_point_algorithm");
+  Pca::FixedPointAlgorithm(dataset, &eigen_values, &principal_components,
+			   dataset.n_rows() / 2, 0.01);
+  fx_timer_stop(fx_root, "fixed_point_algorithm");
+
+  principal_components.PrintDebug();
+
+  eigen_values.PrintDebug();
+}
+
 void TestEigenDecomposeCovariance(const Matrix &dataset) {
   Matrix principal_components;
   Vector eigen_values;
+
+  fx_timer_start(fx_root, "eigen_decompose_covariance");
   Pca::EigenDecomposeCovariance(dataset, &eigen_values, &principal_components);
+  fx_timer_stop(fx_root, "eigen_decompose_covariance");
 
   principal_components.PrintDebug();
 
@@ -19,6 +36,8 @@ int main(int argc, char *argv[]) {
   data::Load(file_name, &dataset);
   
   TestEigenDecomposeCovariance(dataset);
+
+  TestFixedPointAlgorithm(dataset);
 
   fx_done();
   return 0;
