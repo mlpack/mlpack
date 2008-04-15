9b7fa53615158d2aa283abac8ed1555a0232d56e
Dongryeol Lee <drselee@gmail.com>
2008-04-15 18:28:28 +0000

    More compilation fix. Now starting the fix-point algorithm.

--- fastlib2/contrib/dongryel/pca/pca.h
+++ fastlib2/contrib/dongryel/pca/pca.h
@@ -7,8 +7,11 @@ class Pca {
 
  private:
 
-  static void ComputeCovariance(const Matrix &data, const Vector &mean, 
-				Matrix *covariance) {
+  static void ComputeCovariance_(const Matrix &data, Matrix *covariance) {
+
+    // Compute the mean vector.
+    Vector mean;
+    ComputeMean_(data, &mean);
 
     // Allocate D by D covariance matrix and initialize it to zero.
     covariance->Init(data.n_rows(), data.n_rows());
@@ -26,7 +29,7 @@ class Pca {
     la::Scale(1.0 / ((double) data.n_cols()), covariance);
   }
 
-  static void ComputeMean(const Matrix &data, Vector *mean) {
+  static void ComputeMean_(const Matrix &data, Vector *mean) {
     mean->Init(data.n_rows());
     mean->SetZero();
 
@@ -38,24 +41,66 @@ class Pca {
     la::Scale(1.0 / ((double) data.n_cols()), mean);    
   }
 
+  static void RandomUnitVector_(Vector &random_unit_vector) {
+    
+    bool done = false;
+    double length;
+
+    while(!done) {
+
+      for(index_t i = 0; i < random_unit_vector.length(); i++) {
+	random_unit_vector[i] = -1 + 2 * math::Random();
+      }
+      if((length = la::LengthEuclidean(random_unit_vector)) < 1 &&
+	 length > 0) {
+	done = true;
+      }
+    }
+    
+    la::Scale(1.0 / length, &random_unit_vector);
+  }
+
  public:
 
-  /** @brief Computes the principal components of the given dataset.
+  /** @brief Computes the principal components of the given dataset
+   *         using the usual eigendecomposition of the covariance
+   *         matrix.
    */
   static void EigenDecomposeCovariance(const Matrix &data,
 				       Vector *eigen_values,
 				       Matrix *principal_components) {
 
-    // Compute the mean of the column vectors.
-    Vector mean_vector;
-    ComputeMean(data, &mean_vector);
-
     // First, compute the covariance matrix of the dataset.
     Matrix covariance;
-    ComputeCovariance(data, mean_vector, &covariance);
+    ComputeCovariance_(data, &covariance);
     
     // Compute the eigenvalues/eigenvectors of the covariance matrix.
     la::EigenvectorsInit(covariance, eigen_values, principal_components);
+  }
+
+  /** @brief Computes the principal components of the given dataset
+   *         using the fixed-point algorithm.
+   *
+   *  Alok Sharma and Kuldip K. Paliwal. Fast Principal Component
+   *  Analysis Using Fixed-point Algorithm. Pattern Recognition
+   *  Letters 28 (2007) 1151--1155.
+   */
+  static void FixedPointAlgorithm(const Matrix &data,
+				  Vector *eigen_values,
+				  Matrix *principal_components,
+				  double epsilon) {
+    
+    // First, compute the covariance matrix of the dataset.
+    Matrix covariance;
+    ComputeCovariance_(data, &covariance);
+
+    // The current number of principal components to be searched.
+    int current_num_components = 1;
+
+    do {
+      
+
+    } while(tolerance > epsilon);
   }
 
 };
