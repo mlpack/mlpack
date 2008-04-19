42b1f704e1405a662b99707e55fa59a70e95fdbd
Dongryeol Lee <drselee@gmail.com>
2008-04-19 22:15:51 +0000

    Fixed the compilation errors. Plus added the module to compute principal angles between two subspaces.

--- fastlib2/contrib/dongryel/pca/main.cc
+++ fastlib2/contrib/dongryel/pca/main.cc
@@ -1,5 +1,22 @@
 #include "fastlib/fastlib.h"
 #include "pca.h"
+#include "contrib/dongryel/proximity_project/subspace_stat.h"
+#include "contrib/dongryel/proximity_project/gen_metric_tree.h"
+
+typedef GeneralBinarySpaceTree<DBallBound < LMetric<2>, Vector>, Matrix, SubspaceStat > GTree;
+
+void TestSvdLeftSingularVector(const Matrix &dataset) {
+
+  Matrix principal_components;
+  Vector eigen_values;
+  
+  fx_timer_start(fx_root, "svd");
+  Pca::SvdLeftSingularVector(dataset, &eigen_values, &principal_components);
+  fx_timer_stop(fx_root, "svd");
+  
+  principal_components.PrintDebug();
+  eigen_values.PrintDebug();
+}
 
 void TestFixedPointAlgorithm(const Matrix &dataset) {
   Matrix principal_components;
@@ -34,10 +51,20 @@ int main(int argc, char *argv[]) {
   const char *file_name = fx_param_str(NULL, "data", NULL);
   Matrix dataset;
   data::Load(file_name, &dataset);
-  
-  TestEigenDecomposeCovariance(dataset);
 
-  TestFixedPointAlgorithm(dataset);
+  int leaflen = fx_param_int(NULL, "leaflen", 30);
+
+  printf("Constructing the tree...\n");
+  fx_timer_start(NULL, "svd_tree");
+
+  ArrayList<int> old_from_new;
+  GTree *root = proximity::MakeGenMetricTree<GTree>
+    (dataset, leaflen, &old_from_new);
+
+  fx_timer_stop(NULL, "svd_tree");
+  printf("Finished constructing the tree...\n");
+
+  delete root;
 
   fx_done();
   return 0;
