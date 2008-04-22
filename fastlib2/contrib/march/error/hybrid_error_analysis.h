3e007d32fa32c20a72ea38187450e02fd6d319ce
Bill March <march@ices.utexas.edu>
2008-04-22 20:51:44 +0000

    changed stuff

--- fastlib2/contrib/march/error/hybrid_error_analysis.h
+++ fastlib2/contrib/march/error/hybrid_error_analysis.h
@@ -37,6 +37,19 @@ private:
     double gauss_rel = 0.0;
     double hybrid_rel = 0.0;
     
+    double abs_error_max = 0.0;
+    double rel_error_max = 0.0;
+    double exp_error_max = 0.0;
+    double gauss_error_max = 0.0;
+    double hybrid_error_max = 0.0;
+
+    double abs_error_max_rel = 0.0;
+    double rel_error_max_rel = 0.0;
+    double exp_error_max_rel = 0.0;
+    double gauss_error_max_rel = 0.0;
+    double hybrid_error_max_rel = 0.0;
+    
+    
     for (index_t i = 0; i < num_points_; i++) { 
     
       double naive_val = naive_vec[i];
@@ -46,6 +59,45 @@ private:
       double this_exp = fabs(naive_val - exp_vec[i]);
       double this_gauss = fabs(naive_val - gauss_vec[i]);
       double this_hybrid = fabs(naive_val - hybrid_vec[i]);
+      
+      if (this_abs > abs_error_max) {
+        abs_error_max = this_abs;
+      }
+      if (this_rel > rel_error_max) {
+        rel_error_max = this_rel;
+      }
+      if (this_exp > exp_error_max) {
+        exp_error_max = this_exp;
+      }
+      if (this_gauss > gauss_error_max) {
+        gauss_error_max = this_gauss;
+      }
+      if (this_hybrid > hybrid_error_max) {
+        hybrid_error_max = this_hybrid;
+      }
+      
+      
+      this_abs = this_abs/naive_val;
+      this_rel = this_rel/naive_val;
+      this_exp = this_exp/naive_val;
+      this_gauss = this_gauss/naive_val;
+      this_hybrid = this_hybrid/naive_val;
+      
+      if (this_abs > abs_error_max_rel) {
+        abs_error_max_rel = this_abs;
+      }
+      if (this_rel > rel_error_max_rel) {
+        rel_error_max_rel = this_rel;
+      }
+      if (this_exp > exp_error_max_rel) {
+        exp_error_max_rel = this_exp;
+      }
+      if (this_gauss > gauss_error_max_rel) {
+        gauss_error_max_rel = this_gauss;
+      }
+      if (this_hybrid > hybrid_error_max_rel) {
+        hybrid_error_max_rel = this_hybrid;
+      }
       
       abs_error = abs_error + this_abs;
       rel_error = rel_error + this_rel;
@@ -72,7 +124,20 @@ private:
     fx_format_result(exp_mod_, "total_relative_error", "%g", exp_rel);
     fx_format_result(gauss_mod_, "total_relative_error", "%g", gauss_rel);
     fx_format_result(hybrid_mod_, "total_relative_error", "%g", hybrid_rel);
-      
+    
+    fx_format_result(abs_mod_, "max_absolute_error", "%g", abs_error_max);
+    fx_format_result(rel_mod_, "max_absolute_error", "%g", rel_error_max);
+    fx_format_result(exp_mod_, "max_absolute_error", "%g", exp_error_max);
+    fx_format_result(gauss_mod_, "max_absolute_error", "%g", gauss_error_max);
+    fx_format_result(hybrid_mod_, "max_absolute_error", "%g", hybrid_error_max);  
+  
+    fx_format_result(abs_mod_, "max_relative_error", "%g", abs_error_max_rel);
+    fx_format_result(rel_mod_, "max_relative_error", "%g", rel_error_max_rel);
+    fx_format_result(exp_mod_, "max_relative_error", "%g", exp_error_max_rel);
+    fx_format_result(gauss_mod_, "max_relative_error", "%g", 
+                     gauss_error_max_rel);
+    fx_format_result(hybrid_mod_, "max_relative_error", "%g", 
+                     hybrid_error_max_rel);
   
   } // TotalAbsoluteError_()
   
