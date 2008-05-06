9abfd50df596f585ac83f35a0406a5c5ed032822
vasiloglou <nvasil@ismion.com>
2008-05-06 07:23:17 +0000

    it is running

--- fastlib2/contrib/nvasil/all_centroid_knn/all_centroid_knn_test.cc
+++ fastlib2/contrib/nvasil/all_centroid_knn/all_centroid_knn_test.cc
@@ -16,4 +16,52 @@
  * =====================================================================================
  */
 
+#include "all_centroid_knn.h"
+
+class TestAllCentroidkNN {
+ public:
+  void Init() {
+    engine_ = new AllCentroidkNN();
+  }
+  void Destruct() {
+    delete engine_;
+  }
+  void Test1() {
+    Init();
+    Matrix points;
+    data::Load("/net/hg200/nvasil/dataset/swiss_roll/swiss_roll_10000.csv" , &points);
+    datanode *module=fx_submodule(NULL, "/", "temp");
+    engine_->Init(points, NULL);
+    NOTIFY("Number of centroids %i", engine_->centroid_counter_);
+    Matrix centroids;
+    NOTIFY("Computing centroids...\n");
+    engine_->ComputeCentroids(&centroids);
+    NOTIFY("Centroids computed...\n");
+    ArrayList<index_t> centroid_ids;
+    Matrix features;
+    features.Copy(points);
+    NOTIFY("Retrieving centroids ...");
+    engine_->RetrieveCentroids(8, &centroid_ids, &features);
+    NOTIFY("Centroids retrieved...");
+    ArrayList<index_t> resulting_neighbors;
+    NOTIFY("Computing all k centroid distances");
+    ArrayList<double> distances;
+    engine_->AllkCentroids(centroids, centroid_ids, &resulting_neighbors, 
+      &distances); 
+    NOTIFY("k-centroid distances computed");
+    Destruct();
+  }
+  void TestAll() {
+    Test1();
+  }
+ private:
+  AllCentroidkNN *engine_;  
+};
+
+int main(int argc, char *argv[]) {
+  fx_init(argc, argv);
+  TestAllCentroidkNN test;
+  test.TestAll();
+  fx_done();
+}
 
