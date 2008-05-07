938696ab03edc6ef6cf8770ce539d14d4795ef95
Parikshit Ram <p.ram@gatech.edu>
2008-05-07 18:53:20 +0000

    trying a new thing so saving stuff in the svn

--- fastlib2/contrib/pram/covertrees/distances.h
+++ fastlib2/contrib/pram/covertrees/distances.h
@@ -6,22 +6,24 @@
 
 namespace pdc {
 
-  double DistanceEuclidean(Vector& x, Vector& y, double upper_bound) {
+  template<typename T>
+  T DistanceEuclidean(GenVector<T>& x, GenVector<T>& y, T upper_bound) {
 
     DEBUG_SAME_SIZE(x.length(), y.length());
-    double *vx = x.ptr();
-    double *vy = y.ptr();
+    T *vx = x.ptr();
+    T *vy = y.ptr();
     index_t length = x.length();
-    double s = 0;
+    T s = 0;
     index_t batch = 120;
-    index_t t =(index_t) ((double) length / (double) batch);
+    index_t t =(index_t) ((T) length / (T) batch);
     index_t t1 = t;
 
     upper_bound *= upper_bound;
 
+    
     do {
       do {
-	double d = *vx++ - *vy++;
+	T d = *vx++ - *vy++;
 	s += d * d;
 	d = *vx++ - *vy++;
 	s += d * d;
@@ -40,7 +42,7 @@ namespace pdc {
     length = length - t1 * 120;
 
     while (length) {
-      double d = *vx++ - *vy++;
+      T d = *vx++ - *vy++;
       s += d * d;
       d = *vx++ - *vy++;
       s += d * d;
@@ -53,7 +55,15 @@ namespace pdc {
     else {
       return sqrt(s);
     }
+    
+    /* 
+    do {
+      T d = *vx++ - *vy++;
+      s += d * d;
+    } while(--length);
 
+    return sqrt(s);
+    */
   }
 
 };
