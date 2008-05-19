bf53e2bf7d0ccc2719aa9e5a55a1d97cbc9958c7
Dongryeol Lee <drselee@gmail.com>
2008-05-19 18:45:17 +0000

    Initial checkin of the matrix-factorized FMM

---
+++ fastlib2/contrib/dongryel/matrix_factorized_fmm/build.py
@@ -0,0 +1,42 @@
+# Library build rule for the series expansion implementation
+librule(
+    name = "series_expansion",
+    sources = ["series_expansion_aux.cc"],
+    headers = ["cur_decomposition.h",
+               "farfield_expansion.h",
+               "farfield_expansion_impl.h",
+               "matrix_factorized_farfield_expansion.h",
+               "matrix_factorized_farfield_expansion_impl.h",
+               "mult_farfield_expansion.h",
+               "mult_farfield_expansion_impl.h",
+               "kernel_aux.h",
+               "local_expansion.h",
+               "local_expansion_impl.h",
+               "matrix_factorized_local_expansion.h",
+               "matrix_factorized_local_expansion_impl.h",
+               "mult_local_expansion.h",
+               "mult_local_expansion_impl.h",
+               "mult_series_expansion_aux.h",
+               "series_expansion_aux.h",
+               "mult_series_expansion_aux.h"],
+    deplibs = ["fastlib:fastlib_int"]
+    )
+
+# Test driver for series expansion library
+binrule(
+    name = "main",
+    sources = ["main.cc"],
+    headers = [],
+    deplibs = [":series_expansion"]
+    )
+
+# to build:
+# 1. make sure have environment variables set up:
+#    $ source /full/path/to/fastlib/script/fl-env /full/path/to/fastlib
+#    (you might want to put this in bashrc)
+# 2. fl-build main
+#    - this automatically will assume --mode=check, the default
+#    - type fl-build --help for help
+# 3. ./main
+#    - to build same target again, type: make
+#    - to force recompilation, type: make clean
