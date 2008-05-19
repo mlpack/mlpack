3791faeec82bf9dfcd927e5e31efb1d3159ebcd8
Dongryeol Lee <drselee@gmail.com>
2008-05-19 17:01:40 +0000

    Added the local-to-local translation operator.

--- fastlib2/mlpack/series_expansion/matrix_factorized_local_expansion.h
+++ fastlib2/mlpack/series_expansion/matrix_factorized_local_expansion.h
@@ -22,15 +22,33 @@ class MatrixFactorizedLocalExpansion {
    */
   Vector center_;
   
-  /** @brief The incoming representation: the pseudo-distribution.
+  /** @brief The coefficients translated from the outgoing
+   *         representation.
    */
-  Vector incoming_representation_;
+  Vector coeffs_;
+
+  /** @brief The incoming representation: the pseudo-distribution
+   *         which is defined only for leaf nodes.
+   */
+  Vector *incoming_representation_;
   
   /** @brief The query point indices that form the incoming skeleton,
    *         the pseudo-points that represent the query point
    *         distribution.
    */
   ArrayList<index_t> incoming_skeleton_;
+
+  /** @brief The first half of the local-to-local translation
+   *         operator, which basically states the beginning index of
+   *         the parent coefficients to take.
+   */
+  index_t local_to_local_translation_begin_;
+  
+  /** @brief Another half of the local-to-local translation operator,
+   *         which basically states the number of coefficients to take
+   *         starting from the index begin_.
+   */
+  index_t local_to_local_translation_count_;
 
   /** @brief The auxiliary methods for the kernel (derivative,
    *         truncation error bound).
@@ -42,17 +60,28 @@ class MatrixFactorizedLocalExpansion {
    */
   const typename TKernelAux::TKernel *kernel_;
   
-  /** pointer to the precomputed constants inside kernel auxiliary object */
+  /** @brief The pointer to the precomputed constants inside kernel
+   *         auxiliary object.
+   */
   const typename TKernelAux::TSeriesExpansionAux *sea_;
 
   OT_DEF(MatrixFactorizedLocalExpansion) {
-    OT_MY_OBJECT(incoming_representation_);
+    OT_MY_OBJECT(coeffs_);
+    OT_PTR_NULLABLE(incoming_representation_);
     OT_MY_OBJECT(incoming_skeleton_);
+    OT_MY_OBJECT(local_to_local_translation_begin_);
+    OT_MY_OBJECT(local_to_local_translation_count_);
   }
 
  public:
   
-  // getters and setters
+  // Getters and setters
+  
+  /** @brief Gets the local moments.
+   */
+  Vector &coeffs() {
+    return coeffs_;
+  }
   
   /** @brief Gets the squared bandwidth value.
    */
@@ -84,37 +113,37 @@ class MatrixFactorizedLocalExpansion {
     return incoming_skeleton_;
   }
 
+  /** @brief Gets the beginning index of the local-to-local
+   *         translation operator.
+   */
+  index_t local_to_local_translation_begin() const {
+    return local_to_local_translation_begin_;
+  }
+
+  /** @brief Gets the count of the local-to-local translation
+   *         operator.
+   */
+  index_t local_to_local_translation_count() const {
+    return local_to_local_translation_count_;
+  }
+  
   // interesting functions...
   
-  /**
-   * Accumulates the local moment represented by the given reference
-   * data into the coefficients
-   */
-  void AccumulateCoeffs(const Matrix& data, const Vector& weights,
-			int begin, int end, int order);
-
-  /**
-   * This does not apply for local coefficients.
-   */
-  void RefineCoeffs(const Matrix& data, const Vector& weights,
-		    int begin, int end, int order) { }
-  
-  /**
-   * Evaluates the local coefficients at the given point
+  /** @brief Evaluates the local coefficients at the given point
    */
   double EvaluateField(const Matrix& data, int row_num) const;
   double EvaluateField(const Vector& x_q) const;
   
-  /**
-   * Initializes the current local expansion object with the given
-   * center.
+  /** @brief Initializes the current local expansion object with the
+   *         given center.
    */
-  void Init(const Vector& center, const TKernelAux &ka);
-  void Init(const TKernelAux &ka);
+  void Init(const Vector& center, const TKernelAux &ka, index_t begin,
+	    index_t count);
+  void Init(const TKernelAux &ka, index_t begin, index_t count);
 
-  /**
-   * Computes the required order for evaluating the local expansion
-   * for any query point within the specified region for a given bound.
+  /** @brief Computes the required order for evaluating the local
+   *         expansion for any query point within the specified region
+   *         for a given bound.
    */
   template<typename TBound>
   int OrderForEvaluating(const TBound &far_field_region,
@@ -123,21 +152,23 @@ class MatrixFactorizedLocalExpansion {
 			 double max_dist_sqd_regions,
                          double max_error, double *actual_error) const;
 
-  /**
-   * Prints out the series expansion represented by this object.
+  /** @brief Prints out the series expansion represented by this
+   *         object.
    */
   void PrintDebug(const char *name="", FILE *stream=stderr) const;
 
-  /**
-   * Translate from a far field expansion to the expansion here.
-   * The translated coefficients are added up to the ones here.
+  /** @brief Trains the incoming skeleton sampling from the set of
+   *         reference leaf nodes.      
    */
-  void TranslateFromFarField
-  (const MatrixFactorizedFarFieldExpansion<TKernelAux> &se);
-  
-  /**
-   * Translate to the given local expansion. The translated coefficients
-   * are added up to the passed-in local expansion coefficients.
+  template<typename Tree>
+  void TrainBasisFunctions
+  (const Matrix& query_set, int begin, int end,
+   const Matrix *reference_set = NULL,
+   const ArrayList<Tree *> *reference_leaf_nodes = NULL);
+
+  /** @brief Translate to the given local expansion. The translated
+   *         coefficients are added up to the passed-in local
+   *         expansion coefficients.
    */
   void TranslateToLocal(MatrixFactorizedLocalExpansion &se);
 
--- fastlib2/mlpack/series_expansion/matrix_factorized_local_expansion_impl.h
+++ fastlib2/mlpack/series_expansion/matrix_factorized_local_expansion_impl.h
@@ -31,19 +31,23 @@ double MatrixFactorizedLocalExpansion<TKernelAux>::EvaluateField
 }
 
 template<typename TKernelAux>
-void LocalExpansion<TKernelAux>::Init(const Vector& center,
-				      const TKernelAux &ka) {
+void MatrixFactorizedLocalExpansion<TKernelAux>::Init
+(const Vector& center, const TKernelAux &ka) {
   
-  // copy kernel type, center, and bandwidth squared
+  // Copy kernel type, center, and bandwidth squared
   kernel_ = &(ka.kernel_);
   center_.Copy(center);
   order_ = -1;
   sea_ = &(ka.sea_);
   ka_ = &ka;
+
+  // Set the incoming representation to be null. This is only valid
+  // for a leaf node.
+  incoming_representation_ = NULL;
 }
 
 template<typename TKernelAux>
-void LocalExpansion<TKernelAux>::Init(const TKernelAux &ka) {
+void MatrixFactorizedLocalExpansion<TKernelAux>::Init(const TKernelAux &ka) {
   
   // copy kernel type, center, and bandwidth squared
   kernel_ = &(ka.kernel_);
@@ -51,11 +55,15 @@ void LocalExpansion<TKernelAux>::Init(const TKernelAux &ka) {
   sea_ = &(ka.sea_);
   center_.Init(sea_->get_dimension());
   ka_ = &ka;
+
+  // Set the incoming representation to be null. This is only valid
+  // for a leaf node.
+  incoming_representation_ = NULL;
 }
 
 template<typename TKernelAux>
 template<typename TBound>
-int LocalExpansion<TKernelAux>::OrderForEvaluating
+int MatrixFactorizedLocalExpansion<TKernelAux>::OrderForEvaluating
 (const TBound &far_field_region, 
  const TBound &local_field_region, double min_dist_sqd_regions,
  double max_dist_sqd_regions, double max_error, double *actual_error) const {
@@ -67,14 +75,30 @@ int LocalExpansion<TKernelAux>::OrderForEvaluating
 }
 
 template<typename TKernelAux>
-void LocalExpansion<TKernelAux>::TranslateFromFarField
-(const FarFieldExpansion<TKernelAux> &se) {
-
-}
+template<typename Tree>
+void MatrixFactorizedLocalExpansion<TKernelAux>::TrainBasisFunctions
+(const Matrix &query_set, int begin, int end, const Matrix *reference_set,
+ const ArrayList<Tree *> *reference_leaf_nodes) {
   
-template<typename TKernelAux>
-void LocalExpansion<TKernelAux>::TranslateToLocal(LocalExpansion &se) {
+  
+}
 
+template<typename TKernelAux>
+void MatrixFactorizedLocalExpansion<TKernelAux>::TranslateToLocal
+(MatrixFactorizedLocalExpansion &se) {
+  
+  // Local-to-local translation involves determining the indices of
+  // the query points that belong to the local moment to be
+  // translated.
+  index_t beginning_index = se.local_to_local_translation_begin();
+  index_t count = se.local_to_local_translation_count();
+  
+  // Reference to the destination coefficients.
+  Vector &destination_coeffs = se.coeffs();
+
+  for(index_t i = 0; i < count; i++) {
+    destination_coeffs[i] += coeffs_[i + beginning_index];
+  }
 }
 
 #endif
