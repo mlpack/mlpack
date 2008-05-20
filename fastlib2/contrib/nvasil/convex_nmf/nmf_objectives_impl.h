388453d9438fa40d4fe77728aea43a43e3034694
vasiloglou <nvasil@ismion.com>
2008-05-20 19:14:52 +0000

    close to compiling

--- fastlib2/contrib/nvasil/convex_nmf/nmf_objectives_impl.h
+++ fastlib2/contrib/nvasil/convex_nmf/nmf_objectives_impl.h
@@ -4,14 +4,15 @@ void BigSdpNmfObjective::Init(fx_module *module,
 			ArrayList<index_t> &columns,
       ArrayList<double>  &values) {
   module_=module;
-	rows_.Copy(rows);
-	columns_.Copy(columns);
-	values_.Copy(values);
+	rows_.InitCopy(rows);
+	columns_.InitCopy(columns);
+	values_.InitCopy(values);
 	num_of_rows_=std::max_element(rows_.begin(), rows_.end())+1;
 	num_of_columns_=std::max_element(columns_.begin(), columns_.end())+1;
 	eq_lagrange_mult_.Init(values_.size());
   rank_=fx_param_int(module_, "rank", 3);
-	new_dim=fx_param_int(module_, "new_dim", 5);
+	new_dim_=fx_param_int(module_, "new_dim", 5);
+	offset_h_ = num_of_rows_*new_dim_;
 }
 
 void BigSdpNmfObjective::Destruct() {
@@ -25,11 +26,11 @@ void BigSdpNmfObjective::ComputeGradient(Matrix &coordinates, Matrix *gradient) 
   gradient->CopyValues(coordinates);
 	la::Scale(2, gradient);
 	for(index_t i=0; i<values_.size(); i++) {
-    index_t w =rows_(i)*new_dim_;
-    index_t h =offset_h_+cols_(i)*new_dim_;
+    index_t w =rows_[i]*new_dim_;
+    index_t h =offset_h_+columns_[i]*new_dim_;
     double diff=la::Dot(rank_*new_dim_,
 				                coordinates.GetColumnPtr(w),
-												coordinates.GetColumnPtr(h))-values_(i);
+												coordinates.GetColumnPtr(h))-values_[i];
 	  la::AddExpert(rank_*new_dim_, -eq_lagrange_mult_[i]+2*sigma_*diff,
 				 gradient->GetColumnPtr(w), gradient->GetColumnPtr(h));	
 		la::AddExpert(rank_*new_dim_, -eq_lagrange_mult_[i]+2*sigma_*diff, 
@@ -42,18 +43,18 @@ void BigSdpNmfObjective::ComputeObjective(Matrix &coordinates, double *objective
   *objective=0;	
 	for(index_t i=0; i<coordinates.n_cols(); i++) {
 	   *objective+=la::Dot(coordinates.n_rows(), 
-				 ccoordinates.GetColumnPtr(i), coordinates.GetColumnPtr(i));
+				 coordinates.GetColumnPtr(i), coordinates.GetColumnPtr(i));
 	}
 }
 
-void BigSdpNmObjectivef::ComputeFeasibilityError(Matrix &coordinates, double *error) {
+void BigSdpNmfObjective::ComputeFeasibilityError(Matrix &coordinates, double *error) {
   *error=0;
 	for(index_t i=0; i<values_.size(); i++) {
-    index_t w =rows_(i)*new_dim_;
-    index_t h =offset_h_+cols_(i)*new_dim_;
+    index_t w =rows_[i]*new_dim_;
+    index_t h =offset_h_+columns_[i]*new_dim_;
     double diff=la::Dot(rank_*new_dim_,
 				                coordinates.GetColumnPtr(w),
-												coordinates.GetColumnPtr(h))-values_(i);
+												coordinates.GetColumnPtr(h))-values_[i];
 		*error+=diff*diff;
    		
 	}
@@ -63,11 +64,11 @@ double BigSdpNmfObjective::ComputeLagrangian(Matrix &coordinates) {
   double lagrangian=0;
   ComputeObjective(coordinates, &lagrangian);
   for(index_t i=0; i<values_.size(); i++) {
-    index_t w =rows_(i)*new_dim_;
-    index_t h =offset_h_+cols_(i)*new_dim_;
+    index_t w =rows_[i]*new_dim_;
+    index_t h =offset_h_+columns_[i]*new_dim_;
     double diff=la::Dot(rank_*new_dim_,
 				                coordinates.GetColumnPtr(w),
-												coordinates.GetColumnPtr(h))-values_(i);
+												coordinates.GetColumnPtr(h))-values_[i];
 		lagrangian+=(sigma_*diff-eq_lagrange_mult_[i])*diff;
 	}
 	return lagrangian;
@@ -75,11 +76,11 @@ double BigSdpNmfObjective::ComputeLagrangian(Matrix &coordinates) {
 
 void BigSdpNmfObjective::UpdateLagrangeMult(Matrix &coordinates) {
   for(index_t i=0; i<values_.size(); i++) {
-    index_t w =rows_(i)*new_dim_;
-    index_t h =offset_h_+cols_(i)*new_dim_;
+    index_t w =rows_[i]*new_dim_;
+    index_t h =offset_h_+columns_[i]*new_dim_;
     double diff=la::Dot(rank_*new_dim_,
 				                coordinates.GetColumnPtr(w),
-												coordinates.GetColumnPtr(h))-values_(i);
+												coordinates.GetColumnPtr(h))-values_[i];
 		eq_lagrange_mult_[i]-=sigma_*diff;
 	}
 }
@@ -87,7 +88,7 @@ void BigSdpNmfObjective::UpdateLagrangeMult(Matrix &coordinates) {
 void BigSdpNmfObjective::Project(Matrix *coordinates) {
   for(index_t i=0; i<coordinates->n_cols(); i++) {
 	  if (coordinates->get(0, i)<0) {
-		  coordinates->set(0, i)=0;
+		  coordinates->set(0, i, 0.0);
 		}
 	}
 }
@@ -97,10 +98,10 @@ void BigSdpNmfObjective::set_sigma(double sigma) {
 }
 
 void BigSdpNmfObjective::GiveInitMatrix(Matrix *init_data) {
-  init_data->Init(rank_, (num_of_rows_+num_of_columns_)*new_dim);
-	for(index_t i=0; i<init_data.n_rows(); i++) {
-	  for(index_t j=0; j<init_data.n_cols(); j++) {
-		  init_data.set(i, j, math::Random());
+  init_data->Init(rank_, (num_of_rows_+num_of_columns_)*new_dim_);
+	for(index_t i=0; i<init_data->n_rows(); i++) {
+	  for(index_t j=0; j<init_data->n_cols(); j++) {
+		  init_data->set(i, j, math::Random());
 		}
 	}
 }
