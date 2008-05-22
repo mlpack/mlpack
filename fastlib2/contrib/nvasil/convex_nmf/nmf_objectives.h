4976c5e8487eb85fceec3a3184d7bc558b26965e
vasiloglou <nvasil@ismion.com>
2008-05-22 23:05:00 +0000

    Put the code in order, and ready to play with the original formulation

--- fastlib2/contrib/nvasil/convex_nmf/nmf_objectives.h
+++ fastlib2/contrib/nvasil/convex_nmf/nmf_objectives.h
@@ -3,7 +3,7 @@
 #include "fastlib/fastlib.h"
 #include "../l_bfgs/optimization_utils.h"
 
-class BigSdpNmfObjective {
+class BigSdpNmfObjectiveMaxVar {
  public:
 	void Init(fx_module *module, 
 			ArrayList<index_t> &rows,
@@ -33,7 +33,71 @@ class BigSdpNmfObjective {
 	ArrayList<index_t> columns_;
   ArrayList<double>  values_;
   Vector eq_lagrange_mult_; 
+};
 
+class BigSdpNmfObjectiveMinVar {
+ public:
+	void Init(fx_module *module, 
+			ArrayList<index_t> &rows,
+			ArrayList<index_t> &columns,
+      ArrayList<double>  &values);
+  void Destruct();
+  void ComputeGradient(Matrix &coordinates, Matrix *gradient);
+  void ComputeObjective(Matrix &coordinates, double *objective);
+  void ComputeFeasibilityError(Matrix &coordinates, double *error);
+  double ComputeLagrangian(Matrix &coordinates);
+  void UpdateLagrangeMult(Matrix &coordinates);
+  void Project(Matrix *coordinates);
+  void set_sigma(double sigma); 
+  void GiveInitMatrix(Matrix *init_data);
+	bool IsDiverging(double objective); 
+ 
+ private:
+  fx_module *module_;
+  double sigma_;
+	index_t num_of_columns_;
+	index_t num_of_rows_;
+	index_t new_dim_;
+	index_t rank_;
+	index_t offset_h_;
+	index_t offset_h_mat_; // we need this to know wher h matrix starts 
+	ArrayList<index_t> rows_;
+	ArrayList<index_t> columns_;
+  ArrayList<double>  values_;
+  Vector eq_lagrange_mult_; 
+};
+
+
+class BigSdpNmfObjectiveMinVarIneq {
+ public:
+	void Init(fx_module *module, 
+			ArrayList<index_t> &rows,
+			ArrayList<index_t> &columns,
+      ArrayList<double>  &values);
+  void Destruct();
+  void ComputeGradient(Matrix &coordinates, Matrix *gradient);
+  void ComputeObjective(Matrix &coordinates, double *objective);
+  void ComputeFeasibilityError(Matrix &coordinates, double *error);
+  double ComputeLagrangian(Matrix &coordinates);
+  void UpdateLagrangeMult(Matrix &coordinates);
+  void Project(Matrix *coordinates);
+  void set_sigma(double sigma); 
+  void GiveInitMatrix(Matrix *init_data);
+	bool IsDiverging(double objective); 
+ 
+ private:
+  fx_module *module_;
+  double sigma_;
+	index_t num_of_columns_;
+	index_t num_of_rows_;
+	index_t new_dim_;
+	index_t rank_;
+	index_t offset_h_;
+	index_t offset_h_mat_; // we need this to know wher h matrix starts 
+	ArrayList<index_t> rows_;
+	ArrayList<index_t> columns_;
+  ArrayList<double>  values_;
+  Vector ineqeq_lagrange_mult_; 
 };
 
 #include "nmf_objectives_impl.h"
