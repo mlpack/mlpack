d51c5063c185d4b01efd5deb3a430460158c06c8
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-05-23 04:42:04 +0000

    It turns out SDP with rank one gives the same error with just simple NMF

--- fastlib2/contrib/nvasil/convex_nmf/main.cc
+++ fastlib2/contrib/nvasil/convex_nmf/main.cc
@@ -5,15 +5,16 @@ int main(int argc, char *argv[]) {
   fx_module *fx_root;
 	fx_root=fx_init(argc, argv, NULL);
 	fx_module *nmf_module=fx_submodule(fx_root, "/engine");
-	NmfEngine<BigSdpNmfObjectiveMinVar> engine;
-	fx_set_param_str(nmf_module, "data_file", 
+//	NmfEngine<ClassicNmfObjective> engine;
+  NmfEngine<BigSdpNmfObjectiveMinVar> engine;
+  fx_set_param_str(nmf_module, "data_file", 
       "/net/hg200/nvasil/dataset/orl_faces/orl_test_faces_474.csv");
-	fx_set_param_int(nmf_module, "sdp_rank", 4);
+	fx_set_param_int(nmf_module, "sdp_rank", 3);
   fx_set_param_int(nmf_module, "new_dim", 20);
 	fx_set_param_double(nmf_module, "l_bfgs/sigma", 0.1);
 	fx_set_param_double(nmf_module, "l_bfgs/norm_grad_tolerance", 10);
-	fx_set_param_double(nmf_module, "l_bfgs/desired_feasibility", 5);
-  fx_set_param_double(nmf_module, "l_bfgs/feasibility_tolerance", 0.01);
+	fx_set_param_double(nmf_module, "l_bfgs/desired_feasibility", 0);
+  fx_set_param_double(nmf_module, "l_bfgs/feasibility_tolerance", 1e-4);
   fx_set_param_int(nmf_module, "l_bfgs/mem_bfgs", 5);
   engine.Init(nmf_module);
   engine.ComputeNmf();
--- fastlib2/contrib/nvasil/convex_nmf/nmf_objectives_impl.h
+++ fastlib2/contrib/nvasil/convex_nmf/nmf_objectives_impl.h
@@ -239,16 +239,16 @@ void BigSdpNmfObjectiveMinVarIneq::Init(fx_module *module,
 	values_.InitCopy(values);
 	num_of_rows_=*std::max_element(rows_.begin(), rows_.end())+1;
 	num_of_columns_=*std::max_element(columns_.begin(), columns_.end())+1;
-	eq_lagrange_mult_.Init(values_.size());
-  eq_lagrange_mult_.SetAll(0);
+	ineq_lagrange_mult_.Init(values_.size());
+  ineq_lagrange_mult_.SetAll(1.0);
 	offset_h_ = num_of_rows_*new_dim_;
 }
 
-void BigSdpNmfObjectiveMinVar::Destruct() {
+void BigSdpNmfObjectiveMinVarIneq::Destruct() {
   rows_.Renew();
 	columns_.Renew();
   values_.Renew();
-	eq_lagrange_mult_.Destruct();
+	ineq_lagrange_mult_.Destruct();
 }
 
 void BigSdpNmfObjectiveMinVarIneq::ComputeGradient(Matrix &coordinates, 
@@ -263,10 +263,12 @@ void BigSdpNmfObjectiveMinVarIneq::ComputeGradient(Matrix &coordinates,
     double diff=la::Dot(rank_*new_dim_,
 				                coordinates.GetColumnPtr(w),
 												coordinates.GetColumnPtr(h))-values_[i];
-	  la::AddExpert(rank_*new_dim_, -eq_lagrange_mult_[i]+2*sigma_*diff,
-				 coordinates.GetColumnPtr(w), gradient->GetColumnPtr(h));	
-		la::AddExpert(rank_*new_dim_, -eq_lagrange_mult_[i]+2*sigma_*diff, 
-				coordinates.GetColumnPtr(h), gradient->GetColumnPtr(w));
+    if (sigma_*diff <= ineq_lagrange_mult_[i]) {
+	    la::AddExpert(rank_*new_dim_, -ineq_lagrange_mult_[i]+2*sigma_*diff,
+			    coordinates.GetColumnPtr(w), gradient->GetColumnPtr(h));	
+		  la::AddExpert(rank_*new_dim_, -ineq_lagrange_mult_[i]+2*sigma_*diff, 
+				  coordinates.GetColumnPtr(h), gradient->GetColumnPtr(w));
+    }
   }	
 }
 
@@ -288,8 +290,10 @@ void BigSdpNmfObjectiveMinVarIneq::ComputeFeasibilityError(Matrix &coordinates,
     double diff=la::Dot(rank_*new_dim_,
 				                coordinates.GetColumnPtr(w),
 												coordinates.GetColumnPtr(h))-values_[i];
-		*error+=diff*diff;
- 	}
+    if (diff < 0) {
+		  *error=std::max(*error, fabs(diff));
+ 	  }
+  }
 }
 
 
@@ -297,13 +301,17 @@ double BigSdpNmfObjectiveMinVarIneq::ComputeLagrangian(Matrix &coordinates) {
   double lagrangian=0;
   ComputeObjective(coordinates, &lagrangian);
   for(index_t i=0; i<values_.size(); i++) {
-    // equalities
+    // inequalities
     index_t w =rows_[i]*new_dim_;
     index_t h =offset_h_+columns_[i]*new_dim_;
     double diff=la::Dot(rank_*new_dim_,
 				                coordinates.GetColumnPtr(w),
 												coordinates.GetColumnPtr(h))-values_[i];
-		lagrangian+=(sigma_*diff-eq_lagrange_mult_[i])*diff;
+		if (sigma_*diff <= ineq_lagrange_mult_[i]) {
+      lagrangian+=(sigma_*diff-ineq_lagrange_mult_[i])*diff;
+    } else {
+      lagrangian+=-math::Pow<2,1>(ineq_lagrange_mult_[i])/sigma_;
+    }
 	}
 	return lagrangian;
 }
@@ -312,11 +320,11 @@ void BigSdpNmfObjectiveMinVarIneq::UpdateLagrangeMult(Matrix &coordinates) {
   for(index_t i=0; i<values_.size(); i++) {
     index_t w =rows_[i]*new_dim_;
     index_t h =offset_h_+columns_[i]*new_dim_;
-    // equalities
+    // inequalities
     double diff=la::Dot(rank_*new_dim_,
 				                coordinates.GetColumnPtr(w),
 												coordinates.GetColumnPtr(h))-values_[i];
-		eq_lagrange_mult_[i]-=sigma_*diff;
+		ineq_lagrange_mult_[i]=std::max(ineq_lagrange_mult_[i]-sigma_*diff, 0.0);
  	}
 }
 
@@ -330,15 +338,122 @@ void BigSdpNmfObjectiveMinVarIneq::set_sigma(double sigma) {
 
 void BigSdpNmfObjectiveMinVarIneq::GiveInitMatrix(Matrix *init_data) {
   init_data->Init(rank_, (num_of_rows_+num_of_columns_)*new_dim_);
-	for(index_t i=0; i<init_data->n_rows(); i++) {
+  double	mean_value=0.0;
+  for(index_t i=0; i<values_.size(); i++) {
+    mean_value+=values_[i];
+  }
+  mean_value/=values_.size();
+  for(index_t i=0; i<init_data->n_rows(); i++) {
 	  for(index_t j=0; j<init_data->n_cols(); j++) {
-		  init_data->set(i, j, math::Random());
+		  init_data->set(i, j, math::Random(0, 1e-5));
 		}
 	}
 }
 
 
 bool BigSdpNmfObjectiveMinVarIneq::IsDiverging(double objective) {
+  return false;
+} 
+
+////////////////////////////////////////////////////////////////////////
+void ClassicNmfObjective::Init(fx_module *module, 
+			ArrayList<index_t> &rows,
+			ArrayList<index_t> &columns,
+      ArrayList<double>  &values) {
+  module_=module;
+	new_dim_=fx_param_int(module_, "new_dim", 5); 
+  rank_=fx_param_int(module_, "rank", 3);
+  rows_.InitCopy(rows);
+	columns_.InitCopy(columns);
+	values_.InitCopy(values);
+	num_of_rows_=*std::max_element(rows_.begin(), rows_.end())+1;
+	num_of_columns_=*std::max_element(columns_.begin(), columns_.end())+1;
+	offset_h_ = num_of_rows_*new_dim_;
+}
+
+void ClassicNmfObjective::Destruct() {
+  rows_.Renew();
+	columns_.Renew();
+  values_.Renew();
+}
+
+void ClassicNmfObjective::ComputeGradient(Matrix &coordinates, 
+    Matrix *gradient) {
+  // Gradient on the objective  
+	gradient->SetAll(0.0);
+  for(index_t i=0; i<values_.size(); i++) {
+    index_t w =rows_[i]*new_dim_;
+    index_t h =offset_h_+columns_[i]*new_dim_;
+    double diff=la::Dot(rank_*new_dim_,
+				                coordinates.GetColumnPtr(w),
+												coordinates.GetColumnPtr(h))-values_[i];
+	    la::AddExpert(rank_*new_dim_, diff,
+			    coordinates.GetColumnPtr(w), gradient->GetColumnPtr(h));	
+		  la::AddExpert(rank_*new_dim_, diff, 
+				  coordinates.GetColumnPtr(h), gradient->GetColumnPtr(w)); 
+  }
+  //NOTIFY(" gradient_norm %lg", la::Dot(gradient->n_elements(), 
+  //      gradient->ptr(), gradient->ptr())); 
+  //NOTIFY("coordinate_norm%lg", la::Dot(coordinates.n_elements(), 
+  //      coordinates.ptr(), coordinates.ptr()));  
+
+}
+
+void ClassicNmfObjective::ComputeObjective(Matrix &coordinates, 
+    double *objective) {
+  *objective=0;	
+	for(index_t i=0; i<values_.size(); i++) {
+    index_t w =rows_[i]*new_dim_;
+    index_t h =offset_h_+columns_[i]*new_dim_;
+    double diff=la::Dot(rank_*new_dim_,
+				                coordinates.GetColumnPtr(w),
+												coordinates.GetColumnPtr(h))-values_[i];
+		  *objective+=diff*diff;
+  }
+}
+
+void ClassicNmfObjective::ComputeFeasibilityError(Matrix &coordinates, 
+    double *error) {
+  *error=0;
+	for(index_t i=0; i<values_.size(); i++) {
+    index_t w =rows_[i]*new_dim_;
+    index_t h =offset_h_+columns_[i]*new_dim_;
+    double diff=la::Dot(rank_*new_dim_,
+				                coordinates.GetColumnPtr(w),
+												coordinates.GetColumnPtr(h))-values_[i];
+		  *error+=diff*diff;
+  }
+}
+
+
+double ClassicNmfObjective::ComputeLagrangian(Matrix &coordinates) {
+  double lagrangian=0;
+  ComputeObjective(coordinates, &lagrangian);
+	return lagrangian;
+}
+
+void ClassicNmfObjective::UpdateLagrangeMult(Matrix &coordinates) {
+}
+
+void ClassicNmfObjective::Project(Matrix *coordinates) {
+  OptUtils::NonNegativeProjection(coordinates);
+}
+
+void ClassicNmfObjective::set_sigma(double sigma) {
+  sigma_=sigma;
+}
+
+void ClassicNmfObjective::GiveInitMatrix(Matrix *init_data) {
+  init_data->Init(rank_, (num_of_rows_+num_of_columns_)*new_dim_);
+  for(index_t i=0; i<init_data->n_rows(); i++) {
+	  for(index_t j=0; j<init_data->n_cols(); j++) {
+		  init_data->set(i, j, math::Random(0, 0.1));
+		}
+	}
+}
+
+
+bool ClassicNmfObjective::IsDiverging(double objective) {
   return false;
 } 
 
