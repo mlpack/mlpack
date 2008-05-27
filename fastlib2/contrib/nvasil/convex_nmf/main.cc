a7cbfeb0ef3598a90497924516fe5c5e1729b9e4
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-05-27 06:14:27 +0000

    the bug in the gradient was found now it is working but it seems to me that the minimum has a lot of points

--- fastlib2/contrib/nvasil/convex_nmf/main.cc
+++ fastlib2/contrib/nvasil/convex_nmf/main.cc
@@ -24,15 +24,16 @@ int main(int argc, char *argv[]) {
 	fx_root=fx_init(argc, argv, NULL);
 	fx_module *nmf_module=fx_submodule(fx_root, "/engine");
   SdpNmfEngine<SmallSdpNmf> engine;
-  fx_set_param_str(nmf_module, "data_file", "1.csv");
+  fx_set_param_str(nmf_module, "data_file", "5.csv");
   //    "/net/hg200/nvasil/dataset/orl_faces/orl_test_faces_100.csv");
-  fx_set_param_int(nmf_module, "new_dim", 1);
+  fx_set_param_int(nmf_module, "new_dim", 2);
 	fx_set_param_double(nmf_module, "l_bfgs/sigma", 1);
-  fx_set_param_double(nmf_module, "l_bfgs/gamma", 1.7);
-  fx_set_param_double(nmf_module, "l_bfgs/wolfe_sigma2", 0.9999);
+  fx_set_param_double(nmf_module, "l_bfgs/gamma", 2);
+  fx_set_param_double(nmf_module, "l_bfgs/wolfe_sigma2", 0.99);
+  fx_set_param_double(nmf_module, "l_bfgs/wolfe_sigma1", 1e-4);
   fx_set_param_double(nmf_module, "l_bfgs/use_default_termination", false);
-  fx_set_param_int(nmf_module, "l_bfgs/mem_bfgs", 10); 
-  fx_set_param_double(nmf_module, "optfun/desired_duality_gap", 1e-3); 
+  fx_set_param_int(nmf_module, "l_bfgs/mem_bfgs", 4); 
+  fx_set_param_double(nmf_module, "optfun/desired_duality_gap", 1e-4); 
 	fx_set_param_double(nmf_module, "optfun/gradient_tolerance", 1e-2);
   engine.Init(nmf_module);
   engine.ComputeNmf();
@@ -40,6 +41,6 @@ int main(int argc, char *argv[]) {
 	Matrix h_mat;
   engine.GetW(&w_mat);
 	engine.GetH(&h_mat);
-  data::Save("W.csv", w_mat);
-	data::Save("H.csv", h_mat);
+  data::Save("w.csv", w_mat);
+	data::Save("h.csv", h_mat);
 }
