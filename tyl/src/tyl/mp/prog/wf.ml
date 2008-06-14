9a588f21982a0e77b868fd8a0f6d5388eab11185
sooraj <sooraj@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-06-14 03:45:29 +0000

    simplified everything to more closely match the thesis

--- tyl/src/tyl/mp/prog/wf.ml
+++ tyl/src/tyl/mp/prog/wf.ml
@@ -1,34 +1,28 @@
-open Ast
+open Ast 
+open List
 
-let all = List.for_all
-let fromList xx : context = ""
+let wfType t = true
 
-let isType t = true
-
-let (%) a b = (a,b)
-
-let rec (|-) ctxt = function
-  | EVar x                  , t'    -> failWith "FIXME"
+let rec isOfType ctxt e t = match e,t with 
+  | EVar x                  , t'    -> failwith "FIXME"
   | EConst (Bool _)         , TBool -> true
   | EConst (Rational _)     , TReal -> true
-  | EConst (Int _)          , TReal -> true
-  | EUnaryOp (Neg,e)        , TReal -> (ctxt |- e  % TReal)
-  | EUnaryOp (Not,e)        , TBool -> (ctxt |- e  % TBool)
-  | EBinaryOp (Plus,e1,e2)  , TReal -> (ctxt |- e1 % TReal) && (ctxt |- e2 % TReal)
-  | EBinaryOp (Minus,e1,e2) , TReal -> (ctxt |- e1 % TReal) && (ctxt |- e2 % TReal)
-  | EBinaryOp (Mult,e1,e2)  , TReal -> (ctxt |- e1 % TReal) && (ctxt |- e2 % TReal)
-  | EBinaryOp (Or,e1,e2)    , TBool -> (ctxt |- e1 % TBool) && (ctxt |- e2 % TBool)
-  | EBinaryOp (And,e1,e2)   , TBool -> (ctxt |- e1 % TBool) && (ctxt |- e2 % TBool)
+  | EUnaryOp (Neg,e')       , TReal -> isOfType ctxt e' TReal
+  | EUnaryOp (Not,e')       , TBool -> isOfType ctxt e' TBool
+  | EBinaryOp (Plus,e1,e2)  , TReal -> isOfType ctxt e1 TReal && isOfType ctxt e2 TReal
+  | EBinaryOp (Minus,e1,e2) , TReal -> isOfType ctxt e1 TReal && isOfType ctxt e2 TReal
+  | EBinaryOp (Mult,e1,e2)  , TReal -> isOfType ctxt e1 TReal && isOfType ctxt e2 TReal
+  | EBinaryOp (Or,e1,e2)    , TBool -> isOfType ctxt e1 TBool && isOfType ctxt e2 TBool
+  | EBinaryOp (And,e1,e2)   , TBool -> isOfType ctxt e1 TBool && isOfType ctxt e2 TBool
   | _ -> false
 
-let rec (|=) ctxt = function
+let rec wfProp ctxt c = match c with 
   | CBoolVal _ -> true
-  | 
+  | _ -> failwith "FIXME"
 
-let rec isProg = function 
-  | PLet _ -> failWith "FIXME"
-  | PMarked (_,p) -> isProg p
+let wfProg p = match p with 
   | PMain (_,xx,e,c) -> 
-      let ctxt = fromList xx in 
+      let ctxt = "" in 
       let (_,tt) = split xx in 
-        all isType tt && (ctxt |- e % TReal) && (ctxt |= c)
+        for_all wfType tt && isOfType ctxt e TReal && wfProp ctxt c 
+
--- tyl/src/tyl/mp/util/id.mli
+++ tyl/src/tyl/mp/util/id.mli
@@ -1,20 +1,6 @@
 type t
 
 val equal : t -> t -> bool
+module Set : Set.S with type elt = t  
 val fresh : Set.t -> t -> t (* return an id not in set, using input id as root of returned it *)
 
-module Set : Set.S with type elt = t  
-
-(*
-val compare : t -> t -> int
-val toString : t -> string
-val shorter : t -> t -> t
-val shortest : t list -> t option
-val getMark : t -> Pos.range option
-val concat : t list -> t
-  
-module Map : Map.S with type key = t
-
-val freshl : Set.t -> t -> int -> t list (* return k id's not in given set *)
-val freshll : Set.t -> t list -> int -> t list list (* return k id's for each given id *)
-*)
