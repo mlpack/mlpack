470fd776eebd26fe6a39dcc3b781e80f710adcad
sooraj <sooraj@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-06-30 04:29:12 +0000

    fixed _tags to be less promiscuous

--- tyl/src/examples.ml
+++ tyl/src/examples.ml
@@ -1,4 +1,6 @@
-open Edsl
+open Tyl.Mp.Edsl
+open Tyl.Mp.Show
+open Printf
 
 let x = name "x"
 let w = name "w"
@@ -13,3 +15,7 @@ let example1 =
     where [(x,real);(w,real)]
     subject_to ((x <= w) |/ (x >= w + litR 4.0))
 
+;;
+
+printf "%s\n\n" (showp example0) ;;
+printf "%s\n\n" (showp example1) ;;
