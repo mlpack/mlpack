2375168d014807f30a311835a338da1c2313b79d
sooraj <sooraj@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-06-30 04:20:02 +0000

    moved files to 'mp'

---
+++ tyl/src/tyl/mp/ast.ml
@@ -0,0 +1,67 @@
+(* Naming conventions:
+   i - index
+   e - expr
+   t - type
+   p - prog
+   c - prop
+   z - prop type
+   x - variable/id
+   cs,xts - plural (sets/lists of things)
+*)
+
+open Util 
+open List
+
+type nullOp = Bool of bool | Int of int | Real of float
+type unaryOp = Neg | Not 
+type binaryOp = Plus | Minus | Mult | Or | And
+type numRel = Equal | Lte | Gte
+type propOp = Disj | Conj
+type quant = Exists
+type direction = Min | Max
+
+
+type 'a interval = 'a option * 'a option
+type refinedReal = Discrete of int interval | Continuous of float interval 
+type refinedBool = bool option
+
+(* this representation for 'typ' was chosen so that the data
+   constructors are also the coarse types of the object language *)
+type typ = 
+  | TReal of refinedReal
+  | TBool of refinedBool
+      
+type prop_typ = 
+  | ZProp
+
+type expr = 
+  | EVar of Id.t
+  | EConst of nullOp
+  | EUnaryOp of unaryOp * expr
+  | EBinaryOp of binaryOp * expr * expr
+
+type prop = 
+  | CBoolVal of bool
+  | CIsTrue of expr
+  | CNumRel of numRel * expr * expr
+  | CPropOp of propOp * prop list
+  | CQuant of quant * Id.t * typ * prop
+      
+type prog = 
+  | PMain of direction * ((Id.t * typ) list) * expr * prop
+
+module Context = struct
+  type t = (Id.t * typ) list
+  let empty = []
+  let add ctxt x t = (x,t)::ctxt
+  let fromList xts = xts
+  let coarseContains ctxt x t = 
+    let coarseEqual (x',t') = Id.equal x x' &&
+      match t,t' with
+        | TReal _ , TReal _ -> true
+        | TBool _ , TBool _ -> true
+        | _ -> false
+    in 
+      exists coarseEqual ctxt
+  let lookup ctxt x = snd (find (fun (x',_) -> Id.equal x x') ctxt)
+end
---
+++ tyl/src/tyl/mp/compiler.ml
@@ -0,0 +1,114 @@
+open Ast
+open List
+open Vars
+open Wf
+open Cnf
+
+module Id = Util.Id
+module C = Context
+module S = Util.Id.Set
+module E = Edsl
+
+let compileType t = match t with 
+  | TBool None -> E.discrete 0 1
+  | TBool (Some a) -> let b = if a then 1 else 0 in E.discrete b b 
+  | _ -> t (* numeric types are left unchanged *)
+
+let rec compileDLF e = 
+  assert (isDLF e) ;
+  match e with 
+    | EVar _ -> e 
+    | EConst (Bool true) -> E.one
+    | EConst (Bool false) -> E.zero
+    | EUnaryOp (Not,e') -> E.(-) E.one (compileDLF e')
+    | EBinaryOp (Or,e1,e2) -> E.(+) (compileDLF e1) (compileDLF e2)
+    | _ -> assert false
+
+let rec compileProp ctxt c = 
+  assert (disjVarsBounded ctxt c) ;
+  match c with 
+    | CBoolVal _ -> c
+    | CIsTrue e -> let e' = toCNF e in 
+        if isDLF e' then E.(>=) (compileDLF e') E.one else compileConj e'
+    | CNumRel _ -> c
+    | CPropOp (Disj,_) -> compileDisj ctxt c
+    | CPropOp (Conj,cs) -> CPropOp (Conj, map (compileProp ctxt) cs)
+    | CQuant (Exists,x,t,c) -> 
+        let t' = compileType t in
+        let c' = compileProp (C.add ctxt x t') c in
+          CQuant (Exists,x,t',c')
+
+and compileConj e = 
+  assert (isConj e) ; 
+  match e with 
+    | EBinaryOp (And,e1,e2) -> 
+        let c1 = compileProp C.empty (E.isTrue e1) in
+        let c2 = compileProp C.empty (E.isTrue e2) in 
+          E.(/|) c1 c2
+    | _ -> assert false
+
+and typeAsProp (x',t) = let x = EVar x' in
+  match t with
+    | TReal (Continuous (Some lo, Some hi)) -> E.(/|) (E.(<=) (E.litR lo) x) (E.(<=) x (E.litR hi))
+    | TReal (Continuous (Some lo, None   )) ->        (E.(<=) (E.litR lo) x)              
+    | TReal (Continuous (None   , Some hi)) ->                               (E.(<=) x (E.litR hi))
+    | TReal (Continuous (None   , None   )) -> E.propT
+    | TReal (Discrete   (Some lo, Some hi)) -> E.(/|) (E.(<=) (E.litI lo) x) (E.(<=) x (E.litI hi))
+    | TReal (Discrete   (Some lo, None   )) ->        (E.(<=) (E.litI lo) x)              
+    | TReal (Discrete   (None   , Some hi)) ->                               (E.(<=) x (E.litI hi))
+    | TReal (Discrete   (None   , None   )) -> E.propT
+    | TBool (Some true)  -> E.isTrue x
+    | TBool (Some false) -> E.isTrue (E.not x)
+    | TBool None         -> E.propT
+
+and typesAsProp ctxt c = 
+  let xs = S.elements (freeVarsc c) in 
+  let ts = map (C.lookup ctxt) xs in
+  let cs = map typeAsProp (combine xs ts) in
+    CPropOp (Conj,c::cs)
+
+(* pre: e and e' are numeric expressions *) 
+and scalee e e' = match e' with 
+  | EVar _ -> e'
+  | EConst (Int _) 
+  | EConst (Real _) -> E.( * ) e e'
+  | EUnaryOp (Neg,e'') -> E.neg (scalee e e'')
+  | EBinaryOp (Plus,e1,e2) -> E.(+) (scalee e e1) (scalee e e2)
+  | EBinaryOp (Minus,e1,e2) -> E.(-) (scalee e e1) (scalee e e2)
+  | EBinaryOp (Mult, EVar _, _) -> e'
+  | EBinaryOp (Mult, _, EVar _) -> e'
+  | EBinaryOp (Mult, e1, e2) -> 
+      let (x1s,x2s) = (freeVarse e1, freeVarse e2) in
+        if S.is_empty x1s && S.is_empty x2s
+        then e' 
+        else 
+          if not (S.is_empty x1s)
+          then E.( * ) e1 (scalee e e2)
+          else E.( * ) (scalee e e1) e2
+  | _ -> failwith "scalee: expression contains non-numeric syntax"
+
+and scalec e c = match c with 
+  | CBoolVal _ -> c
+  | CIsTrue e' -> E.isTrue (scalee e e') 
+  | CNumRel (op,e1,e2) -> CNumRel(op, scalee e e1, scalee e e2)
+  | CPropOp (op,cs) -> CPropOp(op, map (scalec e) cs)
+  | CQuant (Exists,x,t,c') -> 
+      if not (S.mem x (freeVarse e)) 
+      then CQuant (Exists,x,t,scalec e c')
+      else (* must alpha-convert *)
+        let x' = Id.fresh (freeVarse e) x in 
+        let c'' = subec (EVar x') x c' in 
+          CQuant (Exists,x',t,scalec e c'')
+
+and compileDisj ctxt c = 
+  assert (disjVarsBounded ctxt c) ;
+  match c with 
+    | CPropOp (Disj,cs) -> assert false        
+    | _ -> failwith "compileDisj: proposition is not a disjunction"
+        
+let compile (PMain (d,xts,e,c) as p) = 
+  let ctxt = C.fromList xts in
+    assert (isMP p && disjVarsBounded ctxt c) ;
+    let xts' = map (fun (x,t) -> (x,compileType t)) xts in 
+    let c' = compileProp ctxt c in
+      PMain (d,xts',e,c')
