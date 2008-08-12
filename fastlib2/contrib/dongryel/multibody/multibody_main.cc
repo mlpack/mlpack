6c89182aa2c7ab10540e8f868bfe32185384ec7f
Dongryeol Lee <drselee@gmail.com>
2008-08-12 18:54:46 +0000

    revert back to using the central-limit theorem-based Monte Carlo approximation. Putting confidence band on extreme order statistics is not a practical idea.

--- fastlib2/contrib/dongryel/multibody/multibody_main.cc
+++ fastlib2/contrib/dongryel/multibody/multibody_main.cc
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
   do_naive = fx_param_exists(fx_root, "do_naive");
   bandwidth = fx_param_double(fx_root, "bandwidth", 0.1);
   relative_error = fx_param_double(fx_root, "relative_error", 0.1);
-  kernel = fx_param_str(NULL, "kernel", "axilrodteller");
+  kernel = fx_param_str(fx_root, "kernel", "axilrodteller");
   
   // Multibody computation
   printf("Starting multitree multibody...\n");
@@ -34,15 +34,30 @@ int main(int argc, char *argv[])
     fx_timer_stop(fx_root, "multitree multibody");
     printf("Multitree multibody completed...\n");
     mtmb.PrintDebug(false);
-
+    
     // Do naive algorithm if requested.
     if (do_naive) {
+
+      // Get the approximated vectors from the multitree algorithm.
+      Matrix approximated, exact;
+      mtmb.get_force_vectors(&approximated);
+
       printf("Starting naive multibody...\n");
       fx_timer_start(fx_root, "naive_multibody");
       mtmb.NaiveCompute();
       fx_timer_stop(fx_root, "naive_multibody");
       printf("Finished naive multibody...\n");
       mtmb.PrintDebug(true);
+
+      // Get the exact vectors from the naive algorithm.
+      mtmb.get_force_vectors(&exact);
+            
+      double max_relative_l1_norm_error =
+	MultitreeMultibody<AxilrodTellerForceKernel<Tree, DHrectBound<2> >, Tree >::MaxRelativeL1NormError(approximated, exact);
+
+      // Compute the maximum L1 norm error and output the result.
+      fx_format_result(fx_root, "maximum L1 norm error", "%g", 
+		       max_relative_l1_norm_error);
     }
   }
 
