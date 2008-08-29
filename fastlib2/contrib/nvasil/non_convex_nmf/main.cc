2bae8c4deb7c4657a0c4b84e1da78b3c0d0cee2a
vasiloglou <nvasil@ismion.com>
2008-08-29 20:48:29 +0000

    Improved a lot

--- fastlib2/contrib/nvasil/non_convex_nmf/main.cc
+++ fastlib2/contrib/nvasil/non_convex_nmf/main.cc
@@ -1,35 +1,44 @@
 #include "fastlib/fastlib.h"
 #include "nmf_engine.h"
 
+ //typedef NmfEngine<ClassicNmfObjective> Engine;
+ //typedef NmfEngine<BigSdpNmfObjectiveMaxVarIsometric> Engine;
+ typedef NmfEngine<BigSdpNmfObjectiveMaxFurthestIsometric> Engine;
+ //typedef NmfEngine<NmfObjectiveIsometric> Engine;
+
+
 int main(int argc, char *argv[]) {
   fx_module *fx_root;
 	fx_root=fx_init(argc, argv, NULL);
 	fx_module *nmf_module=fx_submodule(fx_root, "/engine");
-  NmfEngine<ClassicNmfObjective> engine;
-  //NmfEngine<BigSdpNmfObjectiveMinVarDiagonalDominance> engine;
-  // NmfEngine<BigSdpNmfObjectiveMaxVarIsometric> engine;
+  Engine *engine;
   fx_set_param_str(nmf_module, "data_file", 
-     "/net/hg200/nvasil/dataset/orl_faces/orl_test_faces_100.csv");
-  //"/net/hg200/nvasil/dataset/amlall/amlall.csv");
+   // "/net/hg200/nvasil/dataset/corel/ColorHistogram_1000_30.csv");
+   // "/net/hg200/nvasil/dataset/amlall/amlall.csv");
+   // "/net/hg200/nvasil/dataset/orl_faces/orl_test_faces_472.csv");
+    "/net/hg200/nvasil/dataset/teapots/teapots_small.csv");
 	fx_set_param_int(nmf_module, "sdp_rank", 3);
-  fx_set_param_int(nmf_module, "new_dim", 30);
-  fx_set_param_int(nmf_module, "knns", 3);
+  fx_set_param_int(nmf_module, "new_dimension", 20);
+  fx_set_param_int(nmf_module, "optfun/knns", 7);
+  fx_set_param_double(nmf_module, "optfun/grad_tolerance", 1e-6);
+  fx_set_param_double(nmf_module, "optfun/feasibility_error",40);
 	fx_set_param_double(nmf_module, "l_bfgs/sigma", 10);
-	fx_set_param_double(nmf_module, "l_bfgs/gamma", 3);
-	fx_set_param_double(nmf_module, "l_bfgs/norm_grad_tolerance", 1e-7);
-	fx_set_param_double(nmf_module, "l_bfgs/desired_feasibility", 0.001);
-  fx_set_param_double(nmf_module, "l_bfgs/feasibility_tolerance", 1e-7);
-  fx_set_param_int(nmf_module, "l_bfgs/mem_bfgs", 8);
+	fx_set_param_double(nmf_module, "l_bfgs/gamma", 5);
+ 	fx_set_param_double(nmf_module, "l_bfgs/step_size", 3);
+  fx_set_param_int(nmf_module, "l_bfgs/mem_bfgs", 5);
 	fx_set_param_bool(nmf_module, "l_bfgs/silent", false);
-  engine.Init(nmf_module);
+  fx_set_param_bool(nmf_module, "l_bfgs/use_default_termination", false);
   for(index_t i=0; i<1; i++) {
-    engine.ComputeNmf();
+    engine = new Engine();
+    engine->Init(nmf_module);
+    engine->ComputeNmf();
 	  Matrix w_mat;
 	  Matrix h_mat;
-    engine.GetW(&w_mat);
-	  engine.GetH(&h_mat);
+    engine->GetW(&w_mat);
+	  engine->GetH(&h_mat);
     data::Save("W.csv", w_mat);
 	  data::Save("H.csv", h_mat);
+    delete engine;
   }
   fx_done(fx_root);
 }
--- fastlib2/contrib/nvasil/non_convex_nmf/nmf_objectives.h
+++ fastlib2/contrib/nvasil/non_convex_nmf/nmf_objectives.h
@@ -172,9 +172,63 @@ class BigSdpNmfObjectiveMaxVarIsometric {
   double sigma2_; // for inequalities
 	index_t num_of_columns_;
 	index_t num_of_rows_;
-	index_t new_dim_;
-	index_t offset_h_;
-	index_t offset_h_mat_; // we need this to know wher h matrix starts 
+	index_t new_dimension_;
+  double grad_tolerance_;
+	double desired_feasibility_error_;
+  index_t h_offset_; // we need this to know wher h matrix starts 
+  index_t w_offset_;
+	ArrayList<index_t> rows_;
+	ArrayList<index_t> columns_;
+  ArrayList<double>  values_;
+  AllkNN allknn_;
+  index_t knns_;
+  index_t leaf_size_;
+  ArrayList<std::pair<index_t, index_t> > nearest_neighbor_pairs_;
+  ArrayList<double> nearest_distances_;
+  index_t num_of_nearest_pairs_;
+  double sum_of_furthest_distances_;
+  double v_norm_;
+  double sum_all_distances_;
+  double sigma_ratio_;
+  double infeasibility1_;
+  double previous_infeasibility1_;
+  double infeasibility2_;
+  // this is for the equality for the values of the V
+  Vector eq_lagrange_mult1_;
+  Vector eq_lagrange_mult2_;
+};
+
+class BigSdpNmfObjectiveMaxVarIsometricRemoveMean {
+ public:
+  static const index_t MAX_KNNS=30;
+	void Init(fx_module *module, 
+			ArrayList<index_t> &rows,
+			ArrayList<index_t> &columns,
+      ArrayList<double>  &values);
+  void Destruct();
+  void ComputeGradient(Matrix &coordinates, Matrix *gradient);
+  void ComputeObjective(Matrix &coordinates, double *objective);
+  void ComputeFeasibilityError(Matrix &coordinates, double *error);
+  double ComputeLagrangian(Matrix &coordinates);
+  void UpdateLagrangeMult(Matrix &coordinates);
+  void Project(Matrix *coordinates);
+  void set_sigma(double sigma); 
+  void GiveInitMatrix(Matrix *init_data);
+	bool IsDiverging(double objective); 
+  bool IsOptimizationOver(Matrix &coordinates, Matrix &coordinates, double step);
+  bool IsIntermediateStepOver(Matrix &coordinates, Matrix &gradient, double step);
+
+ private:
+  fx_module *module_;
+  double sigma_;
+  double sigma2_; // for inequalities
+	index_t num_of_columns_;
+	index_t num_of_rows_;
+	index_t new_dimension_;
+  double grad_tolerance_;
+	double desired_feasibility_error_;
+  index_t h_offset_; // we need this to know wher h matrix starts 
+  index_t w_offset_;
 	ArrayList<index_t> rows_;
 	ArrayList<index_t> columns_;
   ArrayList<double>  values_;
@@ -194,6 +248,112 @@ class BigSdpNmfObjectiveMaxVarIsometric {
   Vector eq_lagrange_mult1_;
   Vector eq_lagrange_mult2_;
 };
+
+class BigSdpNmfObjectiveMaxFurthestIsometric {
+ public:
+  static const index_t MAX_KNNS=30;
+	void Init(fx_module *module, 
+			ArrayList<index_t> &rows,
+			ArrayList<index_t> &columns,
+      ArrayList<double>  &values);
+  void Destruct();
+  void ComputeGradient(Matrix &coordinates, Matrix *gradient);
+  void ComputeObjective(Matrix &coordinates, double *objective);
+  void ComputeFeasibilityError(Matrix &coordinates, double *error);
+  double ComputeLagrangian(Matrix &coordinates);
+  void UpdateLagrangeMult(Matrix &coordinates);
+  void Project(Matrix *coordinates);
+  void set_sigma(double sigma); 
+  void GiveInitMatrix(Matrix *init_data);
+	bool IsDiverging(double objective); 
+  bool IsOptimizationOver(Matrix &coordinates, Matrix &coordinates, double step);
+  bool IsIntermediateStepOver(Matrix &coordinates, Matrix &gradient, double step);
+
+ private:
+  fx_module *module_;
+  double sigma_;
+  double sigma2_; // for inequalities
+	index_t num_of_columns_;
+	index_t num_of_rows_;
+	index_t new_dimension_;
+  double grad_tolerance_;
+	double desired_feasibility_error_;
+  index_t h_offset_; // we need this to know wher h matrix starts 
+  index_t w_offset_;
+	ArrayList<index_t> rows_;
+	ArrayList<index_t> columns_;
+  ArrayList<double>  values_;
+  AllkNN allknn_;
+  index_t knns_;
+  AllkFN allkfn_;
+  index_t leaf_size_;
+  ArrayList<std::pair<index_t, index_t> > nearest_neighbor_pairs_;
+  ArrayList<double> nearest_distances_;
+  index_t num_of_nearest_pairs_;
+  index_t num_of_furthest_pairs_;
+  ArrayList<std::pair<index_t, index_t> > furthest_neighbor_pairs_;
+  ArrayList<double> furthest_distances_;
+  double sum_of_furthest_distances_;
+  double v_norm_;
+  double sum_all_distances_;
+  double sigma_ratio_;
+  double infeasibility1_;
+  double previous_infeasibility1_;
+  double infeasibility2_;
+  // this is for the equality for the values of the V
+  Vector eq_lagrange_mult1_;
+  Vector eq_lagrange_mult2_;
+};
+
+class NmfObjectiveIsometric {
+ public:
+  static const index_t MAX_KNNS=30;
+	void Init(fx_module *module, 
+			ArrayList<index_t> &rows,
+			ArrayList<index_t> &columns,
+      ArrayList<double>  &values);
+  void Destruct();
+  void ComputeGradient(Matrix &coordinates, Matrix *gradient);
+  void ComputeObjective(Matrix &coordinates, double *objective);
+  void ComputeFeasibilityError(Matrix &coordinates, double *error);
+  double ComputeLagrangian(Matrix &coordinates);
+  void UpdateLagrangeMult(Matrix &coordinates);
+  void Project(Matrix *coordinates);
+  void set_sigma(double sigma); 
+  void GiveInitMatrix(Matrix *init_data);
+	bool IsDiverging(double objective); 
+  bool IsOptimizationOver(Matrix &coordinates, Matrix &coordinates, double step);
+  bool IsIntermediateStepOver(Matrix &coordinates, Matrix &gradient, double step);
+
+ private:
+  fx_module *module_;
+  double sigma_;
+	index_t num_of_columns_;
+	index_t num_of_rows_;
+	index_t new_dimension_;
+  double grad_tolerance_;
+	double desired_feasibility_error_;
+  index_t h_offset_; // we need this to know wher h matrix starts 
+  index_t w_offset_;
+	ArrayList<index_t> rows_;
+	ArrayList<index_t> columns_;
+  ArrayList<double>  values_;
+  AllkNN allknn_;
+  index_t knns_;
+  index_t leaf_size_;
+  ArrayList<std::pair<index_t, index_t> > nearest_neighbor_pairs_;
+  ArrayList<double> nearest_distances_;
+  index_t num_of_nearest_pairs_;
+  double sum_of_furthest_distances_;
+  double v_norm_;
+  double sum_all_distances_;
+  double sigma_ratio_;
+  double infeasibility2_;
+  double previous_infeasibility2_;
+  // this is for the equality for the values of the V
+  Vector eq_lagrange_mult2_;
+};
+
 
 class BigSdpNmfObjectiveMinVarLocal {
  public:
@@ -257,9 +417,10 @@ class  ClassicNmfObjective {
   double sigma_;
 	index_t num_of_columns_;
 	index_t num_of_rows_;
-	index_t new_dim_;
+	index_t new_dimension_;
   double grad_tolerance_;
-	index_t offset_h_;
+	index_t h_offset_;
+  index_t w_offset_;
 	index_t offset_h_mat_; // we need this to know wher h matrix starts 
 	ArrayList<index_t> rows_;
 	ArrayList<index_t> columns_;
