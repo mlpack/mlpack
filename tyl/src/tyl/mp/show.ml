266e380bb5542472865c9efc176c156cef7a09b2
sooraj <sooraj@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-09-07 17:56:42 +0000

    moved Show stuff to one place

--- tyl/src/tyl/mp/show.ml
+++ tyl/src/tyl/mp/show.ml
@@ -2,6 +2,15 @@ open Ast
 open Format
 open List
 
+(* printing common datatypes *)
+let show_unit x = "()"
+let show_bool x = if x then "true" else "false"
+let show_pair a b (x,y) = "(" ^ a x ^ "," ^ b y ^ ")"
+let show_option a x = match x with Some y -> "(Some " ^ a y ^")" | None -> "None" 
+let show_int x = string_of_int x
+let show_float x = string_of_float x
+let show_list a xs = "[" ^ String.concat "," (List.map a xs) ^ "]" 
+
 let showt t = match t with
   | TBool None -> "bool"
   | TBool (Some b) -> sprintf "{%b}" b
