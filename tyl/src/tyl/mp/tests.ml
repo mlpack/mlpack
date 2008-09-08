8b0b8d3d41506c8081e40bcde5f5844862fe27df
sooraj <sooraj@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-09-08 20:31:19 +0000

    stable build with a more sensible dir layout

--- tyl/src/tyl/mp/tests.ml
+++ tyl/src/tyl/mp/tests.ml
@@ -1,35 +1,9 @@
 open Smallcheck
 open Ast
-open Wf
-open Dlist
-open Util
-open Pprint
-open Show
-
-(* constructors as functions *)
-
-let bool x = Bool x
-let int x = Int x
-let real x = Real x
-let discrete x = Discrete x
-let continuous x = Continuous x
-let treal x = TReal x
-let tbool x = TBool x
-let evar x = EVar x
-let econst x = EConst x
-let eunaryop x y = EUnaryOp (x,y)
-let ebinaryop x y z = EBinaryOp (x,y,z)
-let cboolval x = CBoolVal x
-let cistrue x = CIsTrue x
-let cnumrel x y z = CNumRel (x,y,z)
-let cpropop x y = CPropOp (x,y)
-let cquant w x y z = CQuant (w,x,y,z)
-let pmain w x y z = PMain (w,x,y,z)
-
 
 (* series generators *)
 
-let nullOps = pure bool %% bools ++ pure int %% ints ++ pure real %% floats
+let nullOps = pure _Bool %% bools ++ pure _Int %% ints ++ pure _Real %% floats
 let unaryOps = pure Neg ++ pure Not
 let binaryOps = pure Plus ++ pure Minus ++ pure Mult ++ pure Or ++ pure And
 let numRels = pure Equal ++ pure Lte ++ pure Gte
@@ -37,29 +11,27 @@ let propOps = pure Disj ++ pure Conj
 let quants = pure Exists
 let directions = pure Min
 let intervals a = pairs (options a) (options a)
-let refinedReals = pure discrete %% intervals ints ++ pure continuous %% intervals floats
+let refinedReals = pure _Discrete %% intervals ints ++ pure _Continuous %% intervals floats
 let refinedBools = options bools
 let ids = pure (Id.make "a") ++ pure (Id.make "b") ++ pure (Id.make "c") 
 
-let typs = pure treal %% refinedReals ++ pure tbool %% refinedBools
+let typs = pure _TReal %% refinedReals ++ pure _TBool %% refinedBools
 
 let (>-) x f = f x
 
 let rec exprs = fun n -> n >-
-     pure evar %% ids 
-  ++ pure econst %% nullOps 
-  ++ pure eunaryop %% unaryOps %% exprs
-  ++ pure ebinaryop %% binaryOps %% exprs %% exprs
+     pure _EVar %% ids 
+  ++ pure _EConst %% nullOps 
+  ++ pure _EUnaryOp %% unaryOps %% exprs
+  ++ pure _EBinaryOp %% binaryOps %% exprs %% exprs
 
 let rec props = fun n -> n >-
-     pure cboolval %% bools 
-  ++ pure cistrue %% exprs 
-  ++ pure cnumrel %% numRels %% exprs %% exprs
-  ++ pure cpropop %% propOps %% lists props 
-  ++ pure cquant %% quants %% ids %% typs %% props
+     pure _CBoolVal %% bools 
+  ++ pure _CIsTrue %% exprs 
+  ++ pure _CNumRel %% numRels %% exprs %% exprs
+  ++ pure _CPropOp %% propOps %% lists props 
+  ++ pure _CQuant %% quants %% ids %% typs %% props
 
-let progs = pure pmain %% directions %% lists (pairs ids typs) %% exprs %% props
+let progs = pure _PMain %% directions %% lists (pairs ids typs) %% exprs %% props
 
 let contexts = lists (pairs ids typs)
-
-;; Printf.printf "%s" % show_option pptyp $ evaluate 5 (forAll typs isType)
