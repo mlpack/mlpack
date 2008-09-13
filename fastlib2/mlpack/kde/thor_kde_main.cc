1b9751760ff3df1e44f0ec0eadb2ec138c310bd5
Dongryeol Lee <drselee@gmail.com>
2008-09-13 21:37:08 +0000

    Reduced 140 lines of code by refactoring.

--- fastlib2/mlpack/kde/thor_kde_main.cc
+++ fastlib2/mlpack/kde/thor_kde_main.cc
@@ -4,13 +4,13 @@
 #include "../series_expansion/kernel_aux.h"
 
 int main(int argc, char *argv[]) {
-  fx_init(argc, argv);
+  fx_init(argc, argv, NULL);
   ThorKde<GaussianKernelAux> thor_kde;
  
   // initialize and compute
   thor_kde.Init(fx_root);
   thor_kde.Compute(fx_root);
 
-  fx_done();
+  fx_done(fx_root);
   return 0;
 }
