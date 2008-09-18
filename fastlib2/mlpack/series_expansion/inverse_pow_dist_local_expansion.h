65a41a2f9b802f950e1a3135944ca65ef5bf9f25
Dongryeol Lee <drselee@gmail.com>
2008-09-18 19:31:20 +0000

    Added the local expansion for inverse power distance kernels.

---
+++ fastlib2/mlpack/series_expansion/inverse_pow_dist_local_expansion.h
@@ -0,0 +1,114 @@
+/**
+ * @file inverse_pow_dist_local_expansion.h
+ *
+ * This file contains prototype methods for creating a local expansion
+ * for an inverse power of distance functions.
+ *
+ * @author Dongryeol Lee (dongryel@cc.gatech.edu)
+ * @bug No known bugs.
+ */
+
+#ifndef INVERSE_POW_DIST_LOCAL_EXPANSION
+#define INVERSE_POW_DIST_LOCAL_EXPANSION
+
+#include <values.h>
+
+#include "fastlib/fastlib.h"
+#include "kernel_aux.h"
+#include "inverse_pow_dist_series_expansion_aux.h"
+
+class InversePowDistFarFieldExpansion;
+
+/** @brief The class defining a local expansion for inverse distance
+ *         kernel functions.
+ */
+class InversePowDistLocalExpansion {
+
+ private:
+
+  /** @brief The inverse power factor.
+   */
+  double inverse_power_;
+
+  /** @brief The center of the expansion. */
+  Vector center_;
+  
+  /** @brief The coefficients. */
+  ArrayList<GenMatrix<std::complex<double> > > coeffs_;
+  
+  /** @brief The order of approximation. */
+  int order_;
+
+  /** @brief The pointer to the precomputed constants inside kernel
+   *         auxiliary object.
+   */
+  const InversePowDistSeriesExpansionAux *sea_;
+
+  OT_DEF(InversePowDistLocalExpansion) {
+    OT_MY_OBJECT(inverse_power_);
+    OT_MY_OBJECT(center_);
+    OT_MY_OBJECT(coeffs_);
+    OT_MY_OBJECT(order_);
+  }
+
+ public:
+  
+  ////////// Getters/Setters //////////
+  
+  /** @brief Get the center of expansion.
+   */
+  Vector* get_center() {
+    return &center_;
+  }
+
+  /** @brief Retrieves the center of expansion.
+   */
+  const Vector* get_center() const {
+    return &center_;
+  }
+
+  /** @brief Retrieves the coefficients.
+   */
+  const Vector& get_coeffs() const {
+    return coeffs_;
+  }
+  
+  /** @brief Retrieves the approximation order.
+   */
+  int get_order() const { 
+    return order_;
+  }
+
+  /** @brief Get the maximum possible approximation order.
+   */
+  int get_max_order() const {
+    return sea_->get_max_order();
+  }
+
+  /** @brief Sets the approximation order.
+   */
+  void set_order(int new_order) {
+    order_ = new_order;
+  }
+ 
+  void Accumulate(const Vector &v, double weight, int order);
+
+  /**
+   * Accumulates the local moment represented by the given reference
+   * data into the coefficients
+   */
+  void AccumulateCoeffs(const Matrix& data, const Vector& weights,
+			int begin, int end, int order);
+
+  /**
+   * Prints out the series expansion represented by this object.
+   */
+  void PrintDebug(const char *name="", FILE *stream=stderr) const;
+
+};
+
+#define INSIDE_INVERSE_POW_DIST_LOCAL_EXPANSION_H
+#include "inverse_pow_dist_local_expansion_impl.h"
+#undef INSIDE_INVERSE_POW_DIST_LOCAL_EXPANSION_H
+
+#endif
--- fastlib2/mlpack/series_expansion/inverse_pow_dist_series_expansion_aux.h
+++ fastlib2/mlpack/series_expansion/inverse_pow_dist_series_expansion_aux.h
@@ -30,12 +30,124 @@ class InversePowDistSeriesExpansionAux {
    *         constants. $n$ index the most outer ArrayList, and (a, b)
    *         are the indices on each matrix.
    */
-  ArrayList<Matrix> precomputed_constants_;
+  ArrayList<Matrix> multiplicative_constants_;
+
+  /** @brief These are the $A(n, m, \lambda) = (-1)^n (n - m)! 2^m
+   *         \left( \frac{\lambda}{2} \right)_m$.
+   */
+  ArrayList<Matrix> a_constants_;
   
+  /** @brief These are the $T(m, k, \lambda) = (-1)^{m + k} (\lambda -
+   *         1)_{2k} m choose k$.
+   */
+  Matrix t_constants_;
+
+  /** @brief Precomputed factorial values.
+   */
+  Vector factorials_;
+
   OT_DEF_BASIC(InversePowDistSeriesExpansionAux) {
     OT_MY_OBJECT(dim_);
     OT_MY_OBJECT(max_order_);
-    OT_MY_OBJECT(precomputed_constants_);
+    OT_MY_OBJECT(multiplicative_constants_);
+    OT_MY_OBJECT(factorials_);
+  }
+
+  double Factorial_(int index) {
+    
+    if(index < 0) {
+      return 0;
+    }
+    else {
+      return factorials_[index];
+    }
+  }
+
+  void ComputeAConstants_() {
+
+    a_constants_.Init(max_order_);
+
+    for(index_t n = 0; n < a_constants_.size(); n++) {
+      
+      // Allocate $(n + 1)$ by $(n + 1)$ matrix.
+      a_constants_[n].Init(n + 1, n + 1);
+      a_constants_[n].SetZero();
+
+      // The reference to the matrix.
+      Matrix &n_th_order_matrix = a_constants_[n];
+
+      double two_raised_to_m = 1.0;
+      for(index_t m = 0; m <= n; m++) {
+
+	for(index_t lambda_index = 0; lambda_index <= m; lambda_index++) {
+	  n_th_order_matrix.set
+	    (m, lambda_index, Factorial_(n - m) * two_raised_to_m * 
+	     PochammerValue(lambda_ / 2.0 + n, m));
+	  if(n % 2 == 1) {
+	    n_th_order_matrix.set(m, lambda_index,
+				  -n_th_order_matrix.get(m, lambda_index));
+	  }
+	}
+
+	two_raised_to_m *= 2.0;
+      }
+    }    
+  }
+
+  void ComputeTConstants_() {
+
+    t_constants_.Init(max_order_ + 1, max_order_ + 1);
+    t_constants_.SetZero();
+
+    for(index_t m = 0; m < t_constants_.n_rows(); m++) {
+      for(index_t k = 0; k <= m; k++) {
+
+	t_constants_.set(m, k, PochammerValue(lambda_ - 1, 2 * k) *
+			 math::BinomialCoefficient(m, k));
+	if((m + k) % 2 == 1) {
+	  t_constants_.set(m, k, t_constants_.get(m, k));
+	}
+      }
+    }
+  }
+
+  void ComputeFactorials_() {
+
+    factorials_.Init(max_order_ + 1);
+    factorials_[0] = 1;
+    for(index_t i = 1; i < factorials_.length(); i++) {
+      factorials_[i] *= factorials_[i - 1] * i;      
+    }
+  }
+
+  void ComputeMultiplicativeConstants_() {
+    
+    multiplicative_constants_.Init(max_order_);
+
+    for(index_t n = 0; n < multiplicative_constants_.size(); n++) {
+      
+      // Allocate $(n + 1)$ by $(n + 1)$ matrix.
+      multiplicative_constants_[n].Init(n + 1, n + 1);
+      multiplicative_constants_[n].SetZero();
+
+      // The reference to the matrix.
+      Matrix &n_th_order_matrix = multiplicative_constants_[n];
+
+      double two_raised_to_a = 1.0;
+      for(index_t a = 0; a <= n; a++) {
+
+	for(index_t b = 0; b <= a; b++) {
+	  n_th_order_matrix.set(a, b, math::BinomialCoefficient(n, a) * 
+				math::BinomialCoefficient(a, b) / 
+				(two_raised_to_a * factorials_[n]));
+	  if(n % 2 == 1) {
+	    n_th_order_matrix.set(a, b, -n_th_order_matrix.get(a, b));
+	  }
+	}
+
+	two_raised_to_a *= 2.0;
+      }
+    }
   }
 
  public:
@@ -52,38 +164,6 @@ class InversePowDistSeriesExpansionAux {
     max_order_ = new_order;
   }
 
-  void ComputeConstants() {
-    
-    double n_factorial = 1.0;
-    precomputed_constants_.Init(max_order_);
-
-    for(index_t n = 0; n < precomputed_constants_.size(); n++) {
-      
-      // Allocate $(n + 1)$ by $(n + 1)$ matrix.
-      precomputed_constants_[n].Init(n + 1, n + 1);
-
-      // The reference to the matrix.
-      Matrix &n_th_order_matrix = precomputed_constants_[n];
-
-      double two_raised_to_a = 1.0;
-      for(index_t a = 0; a <= n; a++) {
-
-	for(index_t b = 0; b <= a; b++) {
-	  n_th_order_matrix.set(a, b, math::BinomialCoefficient(n, a) * 
-				math::BinomialCoefficient(a, b) / 
-				(two_raised_to_a * n_factorial));
-	  if(n % 2 == 1) {
-	    n_th_order_matrix.set(a, b, -n_th_order_matrix.get(a, b));
-	  }
-	}
-
-	two_raised_to_a *= 2.0;
-      }
-
-      n_factorial *= (n + 1);
-    }
-  }
-
   /** @brief Initialize the auxiliary object with precomputed
    *         quantities for order up to max_order for the given
    *         dimensionality.
@@ -91,6 +171,79 @@ class InversePowDistSeriesExpansionAux {
   void Init(int max_order, int dim) {
     max_order_ = max_order;
     dim_ = dim;
+
+    // Compute factorial values.
+    ComputeFactorials_();
+
+    // Compute multiplicative constants that must be multiplied by
+    // evaluating a far field/local expansion.
+    ComputeMultiplicativeConstants_();
+
+    ComputeAConstants_();
+
+    ComputeTConstants_();
+  }
+
+  double PochammerValue(double base, int index) const {
+    
+    double result = 1.0;
+
+    for(index_t i = 0; i < index; i++) {
+      result *= (base + i);
+    }
+    return result;
+  }
+
+  /** @brief Evaluates the Geganbauer polynomials with the given
+   *         argument at several values efficiently using a recurrence
+   *         relationship.
+   *
+   *  @param argument              The argument to the polynomials.
+   *  @param evaluated_polynomials The table of evaluated polynomial values.
+   */
+  void GegenbauerPolynomials(double argument, 
+			     Matrix &evaluated_polynomials) {
+    
+    // lambda_index = lambda / 2 + 1, ...
+    for(index_t lambda_index = 0; lambda_index < 
+	  evaluated_polynomials.n_cols(); lambda_index++) {
+      
+      double effective_lambda = lambda_ / 2.0 + lambda_index;
+      evaluated_polynomials.set(0, lambda_index, 1.0);
+      evaluated_polynomials.set(1, lambda_index, 2 * effective_lambda *
+				argument);
+
+      // Apply the recurrence.
+      for(index_t n = 2; n < evaluated_polynomials.n_rows(); n++) {
+	evaluated_polynomials.set
+	  (n, lambda_index, 2 * (n + effective_lambda - 1) * argument *
+	   evaluated_polynomials.get(n - 1, lambda_index) -
+	   (n + 2 * effective_lambda - 2) *
+	   evaluated_polynomials.get(n - 2, lambda_index));
+      }
+    }
+  }
+
+  /** @brief Computes the required P factor.
+   *
+   *  @param evaluated_polynomial The table of evaluated Gegenbauer
+   *         polynomials using $cos(theta)$.
+   */
+  void ComputePFactor(double radius, double theta, double phi,
+		      int n, int m, double lambda,
+		      const Matrix &evaluated_polynomials,
+		      std::complex<double> &result) {
+    
+    // Common factor.
+    double common_factor = pow(sin(theta), m) / pow(radius, n + lambda);
+
+    // Compute the real part.
+    result.real() = common_factor * cos(m * phi) * 
+      evaluated_polynomials.get(n - m, m);
+
+    // Then compute the imaginary part.
+    result.imag() = common_factor * sin(m * phi) *
+      evaluated_polynomials.get(n - m, m);
   }
 
   /** @brief This function assumes that the base is a root of unity,
