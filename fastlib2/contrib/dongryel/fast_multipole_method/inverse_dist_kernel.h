0101659db11a288066a3f8efdf28b048387fa7bb
Dongryeol Lee <drselee@gmail.com>
2008-09-29 21:01:19 +0000

    Added the inverse distance power kernel.

---
+++ fastlib2/contrib/dongryel/fast_multipole_method/inverse_dist_kernel.h
@@ -0,0 +1,31 @@
+#ifndef INVERSE_DIST_KERNEL_H
+#define INVERSE_DIST_KERNEL_H
+
+#include "fastlib/fastlib.h"
+
+class InverseDistKernel {
+  
+ private:
+  double lambda_;
+  
+ public:
+
+  void Init(double lambda_in) {
+    lambda_ = lambda_in;
+  }
+
+  double EvalUnnorm(double dist) const {
+    return EvalUnnormOnSq(dist * dist);
+  }
+
+  double EvalUnnormOnSq(double sqdist) const {
+    return 1.0 / pow(sqdist, lambda_ / 2.0);
+  }
+
+  DRange RangeUnnormOnSq(const DRange &range) const {
+    return DRange(EvalUnnormOnSq(range.hi), EvalUnnormOnSq(range.lo));
+  }
+
+};
+
+#endif
