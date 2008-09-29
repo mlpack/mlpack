db13bc4bf8586635389b1157791971884f69630f
ashish <ashish@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-09-29 19:08:42 +0000

    Added About module.

---
+++ tylesBase/src/about.mli
@@ -0,0 +1,9 @@
+(** General information about TylesBase. *)
+
+val major_version : int
+val minor_version : int
+
+val release_date : int * int * int
+  (** [(year, month, day)] of the current release. *)
+
+val version : string
