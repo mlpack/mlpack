689fe5a95e43aa82c68c963ac8f2a3805dd33bc8
Dongryeol Lee <drselee@gmail.com>
2008-10-09 17:56:49 +0000

    More files added.

--- fastlib2/contrib/dongryel/lprcde/nwrcde_delta.h
+++ fastlib2/contrib/dongryel/lprcde/nwrcde_delta.h
@@ -7,22 +7,28 @@ class NWRCdeDelta {
   
  public:
   
+  DRange dsqd_range;
+  DRange kernel_value_range;
   double nwr_numerator_sum_l;
   double denominator_sum_l;
-  double n_pruned_l;
+  double n_pruned;
+  double used_error;
   
  public:
   
-  void Init(int num_queries) {
-    
-    // Reset the delta quantities to zero.
-    SetZero();
+  template<typename Tree>
+  void ComputeDelta(Tree *qnode, Tree *rnode) {
+    dsqd_range.lo = qnode->bound().MinDistanceSq(rnode->bound());
+    dsqd_range.hi = qnode->bound().MaxDistanceSq(rnode->bound());
+    kernel_value_range = ka_.kernel_.RangeUnnormOnSq(dsqd_range);
+    n_pruned += rnode->sum_of_target_values_;
   }
-  
+
   void SetZero() {
+    dsqd_range.Reset();
     nwr_numerator_sum_l = 0;
     nwr_denominator_sum_l = 0;
-    n_pruned_l = 0;
+    n_pruned = 0;
   }
   
 };
--- fastlib2/contrib/dongryel/lprcde/nwrcde_query_summary.h
+++ fastlib2/contrib/dongryel/lprcde/nwrcde_query_summary.h
@@ -8,8 +8,9 @@ class NWRCdeQuerySummary {
  public:
   
   double nwr_numerator_sum_l;
-  double denominator_sum_l;
+  double nwr_denominator_sum_l;
   double n_pruned_l;
+  double used_error_u;
   
  public:
   
@@ -23,12 +24,14 @@ class NWRCdeQuerySummary {
     nwr_numerator_sum_l = DBL_MAX;
     nwr_denominator_sum_l = DBL_MAX;
     n_pruned_l = DBL_MAX;
+    used_error_u = 0;
   }
   
   void SetZero() {
     nwr_numerator_sum_l = 0;
     nwr_denominator_sum_l = 0;
     n_pruned_l = 0;
+    used_error_u = 0;
   }
   
 };
