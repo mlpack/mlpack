0de0795c7b90986f0e349d1b113ca9da7c5a57e2
vasiloglou <nvasil@ismion.com>
2008-10-14 20:08:55 +0000

    Compiled the svm now I have to test it

--- fastlib2/contrib/nvasil/mvu/mvu_classification.h
+++ fastlib2/contrib/nvasil/mvu/mvu_classification.h
@@ -84,7 +84,68 @@ private:
   double sum_of_nearest_distances_;
 };
 
-class MaxFurthestNeighborsSvmSemiSupervised;
+class MaxFurthestNeighborsSvmSemiSupervised {
+public:
+  static const index_t MAX_KNNS=30;
+  /**
+   * Initialize with this one if you want to load points from matrices
+   * and then compute neighbors
+   */
+  void Init(fx_module *module, Matrix &labeled_data, 
+      Matrix &unlabeled_data, Matrix &labels);
+  /**
+   * Use this one if you want to load the nearest neighbors and 
+   * the furthest neighbors from a file
+   */
+  void Init(fx_module *module);
+  void Destruct();
+  void ComputeGradient(Matrix &coordinates, Matrix *gradient);
+  void ComputeObjective(Matrix &coordinates, double *objective);
+  void ComputeFeasibilityError(Matrix &coordinates, double *error);
+  double ComputeLagrangian(Matrix &coordinates);
+  void UpdateLagrangeMult(Matrix &coordinates);
+  void Project(Matrix *coordinates);
+  void set_sigma(double sigma); 
+  void set_lagrange_mult(double val);
+  bool IsDiverging(double objective); 
+  bool IsOptimizationOver(Matrix &coordinates, 
+      Matrix &gradient, double step);
+  bool IsIntermediateStepOver(Matrix &coordinates, 
+      Matrix &gradient, double step);
+  void GiveInitMatrix(Matrix *init_matrix);
+ index_t num_of_points();
+ 
+private:
+  datanode *module_;
+  AllkNN allknn_;
+  AllkFN allkfn_;
+  index_t knns_;
+  index_t leaf_size_;
+  index_t num_of_labeled_;
+  index_t num_of_unlabeled_;
+  index_t labeled_offset_;
+  index_t unlabeled_offset_;
+  Vector svm_signs_;
+  ArrayList<std::pair<index_t, index_t> > nearest_neighbor_pairs_;
+  ArrayList<double> nearest_distances_;
+  Vector eq_lagrange_mult_;
+  Vector ineq_lagrange_mult_;
+  index_t num_of_nearest_pairs_;
+  index_t num_of_furthest_pairs_;
+  ArrayList<std::pair<index_t, index_t> > furthest_neighbor_pairs_;
+  ArrayList<double> furthest_distances_;
+  double sum_of_furthest_distances_;
+  double sigma_;
+  index_t num_of_points_;
+  index_t new_dimension_;
+  double grad_tolerance_;
+  double desired_feasibility_error_;
+  double infeasibility1_;
+  double previous_infeasibility1_;
+  double sum_of_nearest_distances_;
+  index_t anchor_point_;
+};
+
 
 #include "mvu_classification_impl.h"
 #endif
