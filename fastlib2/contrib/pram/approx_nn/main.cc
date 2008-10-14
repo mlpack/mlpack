a22866dc0bdb24e399c1c7d5be001ada3d78f507
Parikshit Ram <p.ram@gatech.edu>
2008-10-14 21:04:04 +0000

    approx_nn.h completed, main.cc modified for testing

--- fastlib2/contrib/pram/approx_nn/main.cc
+++ fastlib2/contrib/pram/approx_nn/main.cc
@@ -1,35 +1,31 @@
-/*
- * =====================================================================================
- *
- *       Filename:  main.cc
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  07/14/2008 07:15:55 PM EDT
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Nikolaos Vasiloglou (NV), nvasil@ieee.org
- *        Company:  Georgia Tech Fastlab-ESP Lab
- *
- * =====================================================================================
- */
-
 #include <string>
-#include "allnn.h"
+#include "approx_nn.h"
 
 int main (int argc, char *argv[]) {
-  fx_module *fx_root=fx_init(argc, argv, NULL);
-  AllNN allnn;
-  Matrix data_for_tree;
-  std::string filename=fx_param_str_req(fx_root, "file");
-  NOTIFY("Loading file...");
-  data::Load(filename.c_str(), &data_for_tree);
+  fx_module *root = fx_init(argc, argv, NULL);
+
+  ApproxNN approx_nn, naive_nn, exact_nn;
+  Matrix qdata, rdata;
+  std::string qfile = fx_param_str_req(root, "q");
+  std::string rfile = fx_param_str_req(root, "r");
+  NOTIFY("Loading files...");
+  data::Load(qfile.c_str(), &qdata);
+  data::Load(rfile.c_str(), &rdata);
   NOTIFY("File loaded...");
-  allnn.Init(data_for_tree, fx_root);
-  //GenVector<index_t> resulting_neighbors_tree;
-  //GenVector<double> resulting_distances_tree;
+
+  struct datanode *ann_module
+    fx_submodule(root, "ann");
+
+  fx_param_int(ann_module, "epsilon", 5);
+  fx_param_double(ann_module, "alpha", 0.90);
+
+  fx_timer_start(ann_module, "naive_init");
+  naive_nn.InitNaive(qdata, rdata, 1);
+  fx_timer_stop(ann_module, "naive_init");
+
+  exact_nn.Init(qdata, rdata, ann_module);
+
+  approx_nn.Init(qdata, rdata, ann_module);
   NOTIFY("Computing Neighbors...");
   allnn.ComputeNeighbors(NULL, NULL);
   NOTIFY("Neighbors Computed...");
