3b000997b6a2eb881819af00d45ff96f41d9dd43
Dongryeol Lee <drselee@gmail.com>
2008-10-14 18:26:10 +0000

    Removed the upper bound maintainers.

--- fastlib2/contrib/dongryel/lprcde/nwrcde_query_result.h
+++ fastlib2/contrib/dongryel/lprcde/nwrcde_query_result.h
@@ -11,7 +11,6 @@ class NWRCdeQueryResult {
   Vector nwr_numerator_sum_e;
   Vector nwr_denominator_sum_l;
   Vector nwr_denominator_sum_e;
-  Vector nwr_denominator_sum_u;
   Vector nwr_numerator_n_pruned;
   Vector nwr_denominator_n_pruned;
   Vector nwr_numerator_used_error;
@@ -24,6 +23,17 @@ class NWRCdeQueryResult {
    */
   Vector final_nwr_estimates;
 
+  OT_DEF_BASIC(NWRCdeQueryResult) {
+    OT_MY_OBJECT(nwr_numerator_sum_l);
+    OT_MY_OBJECT(nwr_numerator_sum_e);
+    OT_MY_OBJECT(nwr_denominator_sum_l);
+    OT_MY_OBJECT(nwr_denominator_sum_e);
+    OT_MY_OBJECT(nwr_numerator_n_pruned);
+    OT_MY_OBJECT(nwr_denominator_n_pruned);
+    OT_MY_OBJECT(nwr_numerator_used_error);
+    OT_MY_OBJECT(nwr_denominator_used_error);
+  }
+
  public:
   
   void ApplyPostponed(const NWRCdeQueryPostponed &postponed_in, 
@@ -33,7 +43,6 @@ class NWRCdeQueryResult {
     nwr_numerator_sum_e[q_index] += postponed_in.nwr_numerator_sum_e;
     nwr_denominator_sum_l[q_index] += postponed_in.nwr_denominator_sum_l;
     nwr_denominator_sum_e[q_index] += postponed_in.nwr_denominator_sum_e;
-    nwr_denominator_sum_u[q_index] += postponed_in.nwr_denominator_sum_u;
     nwr_numerator_n_pruned[q_index] += postponed_in.nwr_numerator_n_pruned;
     nwr_denominator_n_pruned[q_index] += postponed_in.nwr_denominator_n_pruned;
     nwr_numerator_used_error[q_index] += postponed_in.nwr_numerator_used_error;
@@ -48,7 +57,6 @@ class NWRCdeQueryResult {
     nwr_numerator_sum_e.Init(num_queries);
     nwr_denominator_sum_l.Init(num_queries);
     nwr_denominator_sum_e.Init(num_queries);
-    nwr_denominator_sum_u.Init(num_queries);
     nwr_numerator_n_pruned.Init(num_queries);
     nwr_denominator_n_pruned.Init(num_queries);
     nwr_numerator_used_error.Init(num_queries);
@@ -81,7 +89,6 @@ class NWRCdeQueryResult {
     nwr_numerator_sum_e.SetZero();
     nwr_denominator_sum_l.SetZero();
     nwr_denominator_sum_e.SetZero();
-    nwr_denominator_sum_u.SetZero();
     nwr_numerator_n_pruned.SetZero();
     nwr_denominator_n_pruned.SetZero();
     nwr_numerator_used_error.SetZero();
