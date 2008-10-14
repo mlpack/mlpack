0093366e215c1acb380a7b7df4a7373086fc970d
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-10-14 20:08:55 +0000

    Compiled the svm now I have to test it

--- fastlib2/contrib/nvasil/mvu/main_classifier.cc
+++ fastlib2/contrib/nvasil/mvu/main_classifier.cc
@@ -31,8 +31,7 @@ int main(int argc, char *argv[]) {
   std::string labels_file = fx_param_str_req(fx_root, "labels_file");
   std::string unlabeled_points_file=fx_param_str_req(fx_root, "unlabeled_points_file");
   index_t num_of_classes = fx_param_int_req(fx_root, "num_of_classes");
-  MaxFurthestNeighborsSemiSupervised opt_fun;
-  LBfgs<MaxFurthestNeighborsSemiSupervised> engine;
+  std::string mode = fx_param_str(fx_root, "mode", "svm");
   Matrix labeled_data_points;
   data::Load(labeled_points_file.c_str(), &labeled_data_points);
   Matrix unlabeled_data_points;
@@ -63,28 +62,76 @@ int main(int argc, char *argv[]) {
   fx_module *opt_fun_module = fx_submodule(fx_root, "opt_fun");
   fx_module *l_bfgs_module = fx_submodule(fx_root, "l_bfgs");
   fx_set_param_double(l_bfgs_module, "use_default_termination", false);
-  opt_fun.Init(opt_fun_module, labeled_data_points, unlabeled_data_points);
-  engine.Init(&opt_fun, l_bfgs_module);
-  engine.ComputeLocalOptimumBFGS();
+  Matrix result;
+  if (mode == "mvu") {
+    MaxFurthestNeighborsSemiSupervised opt_fun;
+    LBfgs<MaxFurthestNeighborsSemiSupervised> engine;
+    opt_fun.Init(opt_fun_module, labeled_data_points, unlabeled_data_points);
+    engine.Init(&opt_fun, l_bfgs_module);
+    engine.ComputeLocalOptimumBFGS();
+    result.Copy(*engine.coordinates());
+  } else {
+    if (mode == "svm") {
+      MaxFurthestNeighborsSvmSemiSupervised opt_fun;
+      LBfgs<MaxFurthestNeighborsSvmSemiSupervised> engine;
+      opt_fun.Init(opt_fun_module, 
+                   labeled_data_points, 
+                   unlabeled_data_points, 
+                   labels);
+      engine.Init(&opt_fun, l_bfgs_module);
+      engine.ComputeLocalOptimumBFGS();
+      result.Copy(*engine.coordinates());
+    } else {
+      FATAL("This mode (%s) is not supported", mode.c_str());
+    }
+  }
  
   index_t new_dimension =fx_param_int_req(fx_root, "/opt_fun/new_dimension");
   index_t num1=labeled_data_points.n_cols();
   labeled_data_points.Destruct(); 
-  labeled_data_points.Copy(engine.coordinates()->GetColumnPtr(0), new_dimension, num1);  
+  labeled_data_points.Copy(result.GetColumnPtr(0), new_dimension, num1);  
   index_t num2=unlabeled_data_points.n_cols();
   unlabeled_data_points.Destruct();
-  unlabeled_data_points.Copy(engine.coordinates()->GetColumnPtr(num1), 
-      new_dimension, num2);  
+  unlabeled_data_points.Copy(result.GetColumnPtr(num1), 
+      new_dimension, num2);   
+  double total_score;
   if (fx_param_exists(fx_root, "validation_file")) {
     Matrix classification_results;
     NOTIFY("Computing the unfolded optimization score");
-    double total_score = ComputeClassificationScore(fx_root, 
-                                                    labeled_data_points,
-                                                    labels,
-                                                    unlabeled_data_points, 
-                                                    &classification_results);
+    if (mode=="mvu") {
+      total_score = ComputeClassificationScore(fx_root, 
+                                               labeled_data_points,
+                                               labels,
+                                               unlabeled_data_points, 
+                                               &classification_results);
+    } else {
+      if (mode=="svm") {
+        // find the classification score
+        std::string validation_file =fx_param_str_req(fx_root, "validation_file");
+        Matrix validation_labels;
+        data::Load(validation_file.c_str(), &validation_labels);
+        total_score=0.0;
+        index_t anchor_point=0;
+        double anchor_label=labels.get(0, 0);
+        double *p1=labeled_data_points.GetColumnPtr(anchor_point);
+        for(index_t i=0; i<validation_labels.n_cols(); i++)  {
+          double *p2=unlabeled_data_points.GetColumnPtr(i);
+          double dot_product=la::Dot(new_dimension, p1, p2);
+          if (dot_product >=0  and anchor_label==validation_labels.get(0, i)) {
+            total_score+=1;
+          } else {
+            if (dot_product <=0  and anchor_label!=validation_labels.get(0, i)) {
+              total_score+=1;
+            }
+          }
+        }
+        total_score = 100.0 * total_score/validation_labels.n_cols();
+      } else {
+        FATAL("This mode (%s) is not supported", mode.c_str());
+      }
+    }
     data::Save("classification_results", classification_results);
-    fx_result_double(fx_root, "classification_score", total_score);
+    fx_result_double(fx_root, "unfolded_classification_score", total_score);
     NOTIFY("Unfolded Classification Results %lg%%", total_score);
   }
   fx_done(fx_root);
--- fastlib2/contrib/nvasil/mvu/mvu_classification_impl.h
+++ fastlib2/contrib/nvasil/mvu/mvu_classification_impl.h
@@ -350,4 +350,339 @@ index_t  MaxFurthestNeighborsSemiSupervised::num_of_points() {
   return num_of_points_;
 }
 
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+void MaxFurthestNeighborsSvmSemiSupervised::Init(fx_module *module, 
+    Matrix &labeled_data, 
+    Matrix &unlabeled_data,
+    Matrix &labels) {
+
+  anchor_point_=0;
+  module_=module;
+  knns_ = fx_param_int(module_, "knns", 5);
+  leaf_size_ = fx_param_int(module_, "leaf_size", 20);
+  grad_tolerance_ = fx_param_double(module_, "grad_tolerance", 1e-3);
+  desired_feasibility_error_ = fx_param_double(module_, "desired_feasibility_error", 10);
+  new_dimension_=fx_param_int_req(module_, "new_dimension");
+  num_of_labeled_ = labeled_data.n_cols();
+  num_of_unlabeled_ = unlabeled_data.n_cols();
+  num_of_points_=num_of_labeled_+num_of_unlabeled_;
+  previous_infeasibility1_=DBL_MAX;
+  labeled_offset_=0;
+  unlabeled_offset_=num_of_labeled_;
+  svm_signs_.Init(labels.n_cols());
+  ineq_lagrange_mult_.Init(svm_signs_.length());
+  ineq_lagrange_mult_.SetAll(1.0);
+  for(index_t i=0; i<labels.n_cols(); i++) {
+    if (labels.get(0, i)==labels.get(0, anchor_point_)) {
+      svm_signs_[i]=1.0;
+    } else {
+      svm_signs_[i]=-1.0;
+    }
+  }
+  DEBUG_ASSERT_MSG(labeled_data.n_rows()==unlabeled_data.n_rows(), 
+      "Labeled data points don't have the same dimension with unlabeled");
+  Matrix  data_points;
+  data_points.Init(labeled_data.n_rows(), num_of_labeled_+num_of_unlabeled_);
+  data_points.CopyColumnFromMat(labeled_offset_, 0, labeled_data.n_cols(), labeled_data);
+  data_points.CopyColumnFromMat(unlabeled_offset_, 0, unlabeled_data.n_cols(), unlabeled_data);
+
+  NOTIFY("Nearest neighbor constraints ...\n");
+  NOTIFY("Building tree with data ...\n");
+  if (knns_==0) {
+     allknn_.Init(data_points, leaf_size_, MAX_KNNS); 
+  } else {
+    allknn_.Init(data_points, leaf_size_, knns_); 
+  }
+  NOTIFY("Tree built ...\n");
+  NOTIFY("Computing neighborhoods ...\n");
+  ArrayList<index_t> from_tree_neighbors;
+  ArrayList<double>  from_tree_distances;
+  allknn_.ComputeNeighbors(&from_tree_neighbors,
+                           &from_tree_distances);
+
+  NOTIFY("Neighborhoods computed...\n");
+  if (knns_==0) {
+    NOTIFY("Auto-tuning the knn...\n" );
+    MaxVarianceUtils::EstimateKnns(from_tree_neighbors,
+        from_tree_distances,
+        MAX_KNNS, 
+        data_points.n_cols(),
+        data_points.n_rows(),
+        &knns_); 
+    NOTIFY("Optimum knns is %i", knns_);
+    fx_format_result(module_, "optimum_knns", "%i",knns_);
+    MaxVarianceUtils::ConsolidateNeighbors(from_tree_neighbors,
+        from_tree_distances,
+        MAX_KNNS,
+        knns_,
+        &nearest_neighbor_pairs_,
+        &nearest_distances_,
+        &num_of_nearest_pairs_);
+  } else { 
+    NOTIFY("Consolidating neighbors...\n");
+    MaxVarianceUtils::ConsolidateNeighbors(from_tree_neighbors,
+        from_tree_distances,
+        knns_,
+        knns_,
+        &nearest_neighbor_pairs_,
+        &nearest_distances_,
+        &num_of_nearest_pairs_);
+  }
+  sum_of_nearest_distances_=0;
+  for(index_t i=0; i<nearest_distances_.size(); i++) {
+   sum_of_nearest_distances_+=math::Pow<1,2>(nearest_distances_[i]);
+  }
+  NOTIFY("Sum of all nearest distances:%lg", sum_of_nearest_distances_);
+  fx_result_double(module_, "sum_of_nearest_distances", sum_of_nearest_distances_);
+  fx_format_result(module_, "num_of_constraints", "%i", num_of_nearest_pairs_);
+  eq_lagrange_mult_.Init(num_of_nearest_pairs_);
+  eq_lagrange_mult_.SetAll(1.0);
+  NOTIFY("Furtherst neighbor constraints ...\n");
+  NOTIFY("Building tree with data ...\n");
+  allkfn_.Init(data_points, leaf_size_, 1); 
+  NOTIFY("Tree built ...\n");
+  NOTIFY("Computing furthest neighborhoods ...\n");
+  from_tree_neighbors.Renew();
+  from_tree_distances.Renew();
+  allkfn_.ComputeNeighbors(&from_tree_neighbors,
+                           &from_tree_distances);
+  NOTIFY("Furthest Neighbors computed...\n");
+  NOTIFY("Consolidating neighbors...\n");
+  MaxVarianceUtils::ConsolidateNeighbors(from_tree_neighbors,
+      from_tree_distances,
+      1,
+      1,
+      &furthest_neighbor_pairs_,
+      &furthest_distances_,
+      &num_of_furthest_pairs_);
+  double max_nearest_distance=0;
+  for(index_t i=0; i<num_of_nearest_pairs_; i++) {
+    max_nearest_distance=std::max(nearest_distances_[i], max_nearest_distance);
+  }
+  sum_of_furthest_distances_=-max_nearest_distance*
+      data_points.n_cols()*num_of_furthest_pairs_;
+ 
+  NOTIFY("Lower bound for optimization %lg", sum_of_furthest_distances_);
+  fx_format_result(module_, "lower_optimal_bound", "%lg", sum_of_furthest_distances_);
+}
+
+void MaxFurthestNeighborsSvmSemiSupervised::Destruct() {
+  allknn_.Destruct();
+  allkfn_.Destruct();
+  nearest_neighbor_pairs_.Renew();
+  nearest_distances_.Renew();
+  eq_lagrange_mult_.Destruct();
+  ineq_lagrange_mult_.Destruct();
+  furthest_neighbor_pairs_.Renew();
+  furthest_distances_.Renew();
+}
+
+void MaxFurthestNeighborsSvmSemiSupervised::ComputeGradient(Matrix &coordinates, Matrix *gradient) {
+  index_t dimension=coordinates.n_rows();
+  gradient->SetAll(0.0);
+  // objective
+  for(index_t i=0; i<num_of_furthest_pairs_; i++) {
+    double a_i_r[dimension];
+    index_t n1=furthest_neighbor_pairs_[i].first;
+    index_t n2=furthest_neighbor_pairs_[i].second;
+    double *point1 = coordinates.GetColumnPtr(n1);
+    double *point2 = coordinates.GetColumnPtr(n2);
+    la::SubOverwrite(dimension, point2, point1, a_i_r);
+
+    la::AddExpert(dimension, -1.0, a_i_r,
+        gradient->GetColumnPtr(n1));
+    la::AddTo(dimension, a_i_r,
+        gradient->GetColumnPtr(n2));
+  }
+  // equality constraints
+  for(index_t i=0; i<num_of_nearest_pairs_; i++) {
+    double a_i_r[dimension];
+    index_t n1=nearest_neighbor_pairs_[i].first;
+    index_t n2=nearest_neighbor_pairs_[i].second;
+    double *point1 = coordinates.GetColumnPtr(n1);
+    double *point2 = coordinates.GetColumnPtr(n2);
+    double dist_diff = la::DistanceSqEuclidean(dimension, point1, point2) 
+                           -nearest_distances_[i];
+    la::SubOverwrite(dimension, point2, point1, a_i_r);
+
+    la::AddExpert(dimension,
+        -eq_lagrange_mult_[i]+dist_diff*sigma_,
+        a_i_r, 
+        gradient->GetColumnPtr(n1));
+    la::AddExpert(dimension,
+        eq_lagrange_mult_[i]-dist_diff*sigma_,
+        a_i_r, 
+        gradient->GetColumnPtr(n2));
+  }
+  // inequality constraints
+  for(index_t i=0; i<ineq_lagrange_mult_.length(); i++) {
+    double dot_prod=la::Dot(coordinates.n_rows(),
+                            coordinates.GetColumnPtr(anchor_point_),
+                            coordinates.GetColumnPtr(i)) * svm_signs_[i];
+    if (sigma_*dot_prod*svm_signs_[i] <= ineq_lagrange_mult_[i] ) {
+      double *p1=coordinates.GetColumnPtr(anchor_point_);
+      double *p2=coordinates.GetColumnPtr(i);
+      la::Scale(coordinates.n_rows(), -2*ineq_lagrange_mult_[i]+ sigma_*dot_prod, p1);
+      la::Scale(coordinates.n_rows(), -2*ineq_lagrange_mult_[i]+ sigma_*dot_prod, p2);
+      la::SubFrom(coordinates.n_rows(), p1, gradient->GetColumnPtr(anchor_point_));
+      la::SubFrom(coordinates.n_rows(), p2, gradient->GetColumnPtr(i));
+    }
+  }
+}
+
+void MaxFurthestNeighborsSvmSemiSupervised::ComputeObjective(Matrix &coordinates, 
+    double *objective) {
+  *objective=0;
+  index_t dimension = coordinates.n_rows();
+  for(index_t i=0; i<num_of_furthest_pairs_; i++) {
+    index_t n1=furthest_neighbor_pairs_[i].first;
+    index_t n2=furthest_neighbor_pairs_[i].second;
+    double *point1 = coordinates.GetColumnPtr(n1);
+    double *point2 = coordinates.GetColumnPtr(n2);
+    double diff = la::DistanceSqEuclidean(dimension, point1, point2);
+    *objective-=diff;   
+  }
+}
+
+void MaxFurthestNeighborsSvmSemiSupervised::ComputeFeasibilityError(Matrix &coordinates, double *error) {
+  index_t dimension=coordinates.n_rows();
+  *error=0;
+  for(index_t i=0; i<num_of_nearest_pairs_; i++) {
+    index_t n1=nearest_neighbor_pairs_[i].first;
+    index_t n2=nearest_neighbor_pairs_[i].second;
+    double *point1 = coordinates.GetColumnPtr(n1);
+    double *point2 = coordinates.GetColumnPtr(n2);
+    double dist_diff = la::DistanceSqEuclidean(dimension, 
+                                               point1, point2) 
+                           -nearest_distances_[i];
+    *error+=fabs(dist_diff);
+  }
+  *error = *error *100.0/sum_of_nearest_distances_;
+  double error1=0;
+  for(index_t i=0; i<svm_signs_.length(); i++) {
+    double dot_prod=la::Dot(coordinates.n_rows(),
+                            coordinates.GetColumnPtr(anchor_point_),
+                            coordinates.GetColumnPtr(i)) * svm_signs_[i]; 
+    if (dot_prod!=svm_signs_[i]) {
+      error1+=1;
+    } 
+  }
+  NOTIFY("Classification Error:%lg",100.0 * error1/svm_signs_.length());
+}
+
+double MaxFurthestNeighborsSvmSemiSupervised::ComputeLagrangian(Matrix &coordinates) {
+  index_t dimension=coordinates.n_rows();
+  double lagrangian=0;
+  ComputeObjective(coordinates, &lagrangian);
+  for(index_t i=0; i<num_of_nearest_pairs_; i++) {
+    index_t n1=nearest_neighbor_pairs_[i].first;
+    index_t n2=nearest_neighbor_pairs_[i].second;
+    double *point1 = coordinates.GetColumnPtr(n1);
+    double *point2 = coordinates.GetColumnPtr(n2);
+    double dist_diff = la::DistanceSqEuclidean(dimension, point1, point2) 
+                           -nearest_distances_[i];
+    lagrangian+=dist_diff*dist_diff*sigma_/2
+        -eq_lagrange_mult_[i]*dist_diff;
+  }
+  for(index_t i=0; i<=ineq_lagrange_mult_.length(); i++) {
+    double dot_prod=la::Dot(coordinates.n_rows(),
+                            coordinates.GetColumnPtr(anchor_point_),
+                            coordinates.GetColumnPtr(i)) * svm_signs_[i]; 
+    if ( sigma_ * dot_prod  <= ineq_lagrange_mult_[i]) {
+      lagrangian+=(-ineq_lagrange_mult_[i] + sigma_*dot_prod)*dot_prod;
+    } else {
+      lagrangian+=-math::Pow<2,1>(ineq_lagrange_mult_[i])/(2*sigma_);
+    }   
+  }
+  
+  return lagrangian;
+}
+
+void MaxFurthestNeighborsSvmSemiSupervised::UpdateLagrangeMult(Matrix &coordinates) {
+  index_t dimension=coordinates.n_rows();
+  for(index_t i=0; i<num_of_nearest_pairs_; i++) {
+    index_t n1=nearest_neighbor_pairs_[i].first;
+    index_t n2=nearest_neighbor_pairs_[i].second;
+    double *point1 = coordinates.GetColumnPtr(n1);
+    double *point2 = coordinates.GetColumnPtr(n2);
+    double dist_diff =la::DistanceSqEuclidean(dimension, point1, point2) 
+                            -nearest_distances_[i];
+    eq_lagrange_mult_[i]-=sigma_*dist_diff;
+  }
+  for(index_t i=0; i<ineq_lagrange_mult_.length(); i++) {
+    double dot_prod=la::Dot(coordinates.n_rows(),
+                            coordinates.GetColumnPtr(anchor_point_),
+                            coordinates.GetColumnPtr(i)) * svm_signs_[i]; 
+    ineq_lagrange_mult_[i] = std::max(ineq_lagrange_mult_[i]
+                                      -sigma_*dot_prod, 0.0);
+  }
+}
+
+void MaxFurthestNeighborsSvmSemiSupervised::set_sigma(double sigma) {
+  sigma_=sigma;
+}
+
+void MaxFurthestNeighborsSvmSemiSupervised::set_lagrange_mult(double val) {
+  eq_lagrange_mult_.SetAll(val);
+  ineq_lagrange_mult_.SetAll(val);
+}
+bool MaxFurthestNeighborsSvmSemiSupervised::IsDiverging(double objective) {
+  if (objective < sum_of_furthest_distances_) {
+    NOTIFY("objective(%lg) < sum_of_furthest_distances (%lg)", objective,
+        sum_of_furthest_distances_);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void MaxFurthestNeighborsSvmSemiSupervised::Project(Matrix *coordinates) {
+  OptUtils::RemoveMean(coordinates);
+}
+
+bool MaxFurthestNeighborsSvmSemiSupervised::IsOptimizationOver(
+    Matrix &coordinates, Matrix &gradient, double step) {
+  
+  ComputeFeasibilityError(coordinates, &infeasibility1_);
+  if (infeasibility1_<desired_feasibility_error_ || 
+      fabs(infeasibility1_-previous_infeasibility1_)<0.1)  {
+    NOTIFY("Optimization is over");
+    return true;
+  } else {
+    previous_infeasibility1_=infeasibility1_;
+    return false; 
+  }
+}
+
+bool MaxFurthestNeighborsSvmSemiSupervised::IsIntermediateStepOver(
+    Matrix &coordinates, Matrix &gradient, double step) {
+  double norm_gradient=math::Pow<1,2>(la::Dot(gradient.n_elements(), 
+                               gradient.ptr(), 
+                               gradient.ptr()));
+  double feasibility_error;
+  ComputeFeasibilityError(coordinates, &feasibility_error);
+
+  if (norm_gradient*step < grad_tolerance_ 
+      ||  feasibility_error<desired_feasibility_error_) {
+    return true;
+  }
+  return false;
+
+}
+
+void MaxFurthestNeighborsSvmSemiSupervised::GiveInitMatrix(Matrix *init_matrix) {
+  init_matrix->Init(new_dimension_, num_of_points_);
+  for(index_t i=0; i<num_of_points_; i++) {
+    for(index_t j=0; j<new_dimension_; j++) {
+      init_matrix->set(j, i, math::Random(0.0, 1.0));
+    }
+  }
+}
+
+index_t  MaxFurthestNeighborsSvmSemiSupervised::num_of_points() {
+  return num_of_points_;
+}
 
