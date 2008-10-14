da47f7cd335d398b03a31d49ac477273e8bc8fc8
Dongryeol Lee <drselee@gmail.com>
2008-10-14 03:53:56 +0000

    Now supports Epanechnikov kernel, but I need to optimize the pruning rule more.

--- fastlib2/contrib/dongryel/lprcde/nwrcde_main.cc
+++ fastlib2/contrib/dongryel/lprcde/nwrcde_main.cc
@@ -7,6 +7,61 @@
 #include "mlpack/series_expansion/kernel_aux.h"
 #include "nwrcde.h"
 
+template<typename TKernel>
+void StartComputation(const Matrix &queries, const Matrix &references,
+		      const Matrix &reference_targets,
+		      struct datanode *nwrcde_module) {
+
+  // Output file name for the fast algorithm.
+  const char *output_file_name = fx_param_str(nwrcde_module, "output",
+                                              "nwrcde_results.txt");
+
+  // Declare the computation object.
+  NWRCde<TKernel> algorithm;
+  NWRCdeQueryResult query_results;
+  algorithm.Init(references, reference_targets, nwrcde_module);
+  algorithm.Compute(queries, &query_results);
+  query_results.PrintDebug(output_file_name);
+
+  // If the do_naive flag is specified, then run the naive algorithm
+  // as well.
+  if(fx_param_exists(nwrcde_module, "do_naive")) {
+    NWRCdeQueryResult naive_query_results;
+    const char *naive_output_file_name =
+      fx_param_str(nwrcde_module, "naive_output", "naive_nwrcde_results.txt");
+
+    algorithm.NaiveCompute(queries, &naive_query_results);
+    naive_query_results.PrintDebug(naive_output_file_name);
+
+    // Compute the difference between the naively computed estimates
+    // and the approximated estimates.
+    Vector difference;
+    la::SubInit(naive_query_results.final_nwr_estimates,
+                query_results.final_nwr_estimates, &difference);
+
+    // The maximum relative error.
+    double max_relative_error = 0;
+    int within_limit = 0;
+    for(index_t i = 0; i < queries.n_cols(); i++) {
+      double relative_error =
+        (naive_query_results.final_nwr_estimates[i] ==
+         query_results.final_nwr_estimates[i]) ?
+        0:(fabs(difference[i]) /
+           fabs(naive_query_results.final_nwr_estimates[i]));
+      max_relative_error = std::max(max_relative_error, relative_error);
+
+      if(relative_error < fx_param_double(nwrcde_module, "relative_error",
+                                          0.1)) {
+        within_limit++;
+      }
+    }
+    fx_format_result(nwrcde_module, "max_relative_error", "%g",
+                     max_relative_error);
+    fx_format_result(nwrcde_module, "under_relative_error_limit", "%d",
+                     within_limit);
+  }
+}
+
 int main(int argc, char *argv[]) {
   
   // Initialize FastExec...
@@ -33,10 +88,6 @@ int main(int argc, char *argv[]) {
   const char* queries_file_name = fx_param_str(fx_root, "query",
 					       references_file_name);
 
-  // Output file name for the fast algorithm.
-  const char *output_file_name = fx_param_str(nwrcde_module, "output",
-					      "nwrcde_results.txt");
-
   // query and reference datasets and target training values.
   Matrix references;
   Matrix reference_targets;
@@ -47,49 +98,14 @@ int main(int argc, char *argv[]) {
   data::Load(queries_file_name, &queries);
   data::Load(reference_targets_file_name, &reference_targets);
 
-  // Declare the computation object.
-  NWRCde<GaussianKernel> algorithm;
-  NWRCdeQueryResult query_results;
-  algorithm.Init(references, reference_targets, nwrcde_module);
-  algorithm.Compute(queries, &query_results);
-  query_results.PrintDebug(output_file_name);
-
-  // If the do_naive flag is specified, then run the naive algorithm
-  // as well.
-  if(fx_param_exists(nwrcde_module, "do_naive")) {
-    NWRCdeQueryResult naive_query_results;
-    const char *naive_output_file_name = 
-      fx_param_str(nwrcde_module, "naive_output", "naive_nwrcde_results.txt");
-
-    algorithm.NaiveCompute(queries, &naive_query_results);
-    naive_query_results.PrintDebug(naive_output_file_name);
-
-    // Compute the difference between the naively computed estimates
-    // and the approximated estimates.
-    Vector difference;
-    la::SubInit(naive_query_results.final_nwr_estimates, 
-		query_results.final_nwr_estimates, &difference);
-    
-    // The maximum relative error.
-    double max_relative_error = 0;
-    int within_limit = 0;
-    for(index_t i = 0; i < queries.n_cols(); i++) {
-      double relative_error = 
-	(naive_query_results.final_nwr_estimates[i] == 
-	 query_results.final_nwr_estimates[i]) ?
-	0:(fabs(difference[i]) / 
-	   fabs(naive_query_results.final_nwr_estimates[i]));
-      max_relative_error = std::max(max_relative_error, relative_error);
-
-      if(relative_error < fx_param_double(nwrcde_module, "relative_error",
-					  0.1)) {
-	within_limit++;
-      }
-    }
-    fx_format_result(nwrcde_module, "max_relative_error", "%g",
-		     max_relative_error);
-    fx_format_result(nwrcde_module, "under_relative_error_limit", "%d",
-		     within_limit);
+  // Run the appropriate algorithm based on the kernel type.
+  if(!strcmp(fx_param_str(nwrcde_module, "kernel", "gaussian"), "gaussian")) {
+    StartComputation<GaussianKernel>(queries, references, reference_targets,
+				     nwrcde_module);
+  }
+  else if(!strcmp(fx_param_str(nwrcde_module, "kernel", "epan"), "epan")) {
+    StartComputation<EpanKernel>(queries, references, reference_targets,
+				 nwrcde_module);
   }
 
   // Finalize FastExec and print output results.
