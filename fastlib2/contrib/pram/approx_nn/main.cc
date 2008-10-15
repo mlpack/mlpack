432b408893b619cc1950fc9bd49f36e43c1a04ef
Parikshit Ram <p.ram@gatech.edu>
2008-10-15 03:10:19 +0000

    all files completed - testing and debugging ensues

--- fastlib2/contrib/pram/approx_nn/main.cc
+++ fastlib2/contrib/pram/approx_nn/main.cc
@@ -14,20 +14,41 @@ int main (int argc, char *argv[]) {
   NOTIFY("File loaded...");
 
   struct datanode *ann_module
-    fx_submodule(root, "ann");
+    = fx_submodule(root, "ann");
 
   fx_param_int(ann_module, "epsilon", 5);
   fx_param_double(ann_module, "alpha", 0.90);
 
+  ArrayList<index_t> nac, exc, apc;
+  ArrayList<double> din, die, dia;
+
+  // Naive computation
   fx_timer_start(ann_module, "naive_init");
   naive_nn.InitNaive(qdata, rdata, 1);
   fx_timer_stop(ann_module, "naive_init");
 
-  exact_nn.Init(qdata, rdata, ann_module);
+  fx_timer_start(ann_module, "naive");
+  naive_nn.ComputeNaive(&nac, &din);
+  fx_timer_start(ann_module, "naive");
 
+  // Exact computation
+  fx_timer_start(ann_module, "exact_init");
+  exact_nn.Init(qdata, rdata, ann_module);
+  fx_timer_stop(ann_module, "exact_init");
+
+  fx_timer_start(ann_module, "exact");
+  exact_nn.ComputeNeighbors(&exc, &die);
+  fx_timer_stop(ann_module, "exact");
+
+  // Approximate computation
+  fx_timer_start(ann_module, "approx_init");
   approx_nn.Init(qdata, rdata, ann_module);
-  NOTIFY("Computing Neighbors...");
-  allnn.ComputeNeighbors(NULL, NULL);
-  NOTIFY("Neighbors Computed...");
+  fx_timer_stop(ann_module, "approx_init");
+
+  ArrayList<double> prob;
+  fx_timer_start(ann_module, "approx");
+  approx_nn.ComputeApprox(&apc, &dia, &prob);
+  fx_timer_stop(ann_module, "approx");
+
   fx_done(fx_root);
 }
