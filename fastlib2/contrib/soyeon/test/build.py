b872d986f1e1239e720c6c1b29199c0761471549
soyeon <soyeon@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-10-27 20:29:05 +0000

    Added a more advanced result

--- fastlib2/contrib/soyeon/test/build.py
+++ fastlib2/contrib/soyeon/test/build.py
@@ -1,3 +1,12 @@
 binrule(name="hello",
         sources=["hello_world.cc"],
         deplibs=["fastlib:fastlib"])
+
+librule(name="lin_test",
+        headers=["linear_algebra_class.h"],
+        sources=["linear_algebra_class.cc"],
+        deplibs=["fastlib:fastlib"])
+
+binrule(name="main", 
+        sources=["main.cc"], 
+        deplibs=[":lin_test"])
---
+++ fastlib2/contrib/soyeon/test/main.cc
@@ -0,0 +1,7 @@
+#include "fastlib/fastlib.h"
+#include "lin_algebra_class.h"
+
+int main(int argc, char *argv[]) {
+
+  QuadraticAlgebraClass my_objective;
+}
