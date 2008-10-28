11401969fce87a5807bcab0643c8d81211855c34
soyeon <soyeon@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-10-28 19:25:23 +0000

    It compiled

--- fastlib2/contrib/soyeon/test/lin_algebra_class.cc
+++ fastlib2/contrib/soyeon/test/lin_algebra_class.cc
@@ -1,13 +1,16 @@
 #include "lin_algebra_class.h"
 
-void QuadraticObjective::Init(Matrix &quadratiic_term, Vector &linear_term) {
+void QuadraticObjective::Init(Matrix &quadratic_term, Vector &linear_term) {
   DEBUG_SAME_SIZE(quadratic_term.n_rows(), quadratic_term.n_cols());
   DEBUG_SAME_SIZE(linear_term.length(), quadratic_term.n_rows());
   DEBUG_ASSERT_MSG(linear_term.length()==quadratic_term.n_rows(), 
       "Quadratic term and linear term don't have the same dimension %i != %i",
-      quadratic_term.n_rows(), linear_term.lenght());
+      quadratic_term.n_rows(), linear_term.length());
   quadratic_term_.Copy(quadratic_term);
   linear_term_.Copy(linear_term);
+  Matrix quadratic_trans;
+  la::TransposeInit(quadratic_term_, &quadratic_trans);
+  la::AddInit(quadratic_term_, quadratic_trans, &sum_quad_);
 }
 
 void QuadraticObjective::ComputeObjective(Vector &x, double *objective) {
@@ -18,17 +21,14 @@ void QuadraticObjective::ComputeObjective(Vector &x, double *objective) {
 }
 
 void QuadraticObjective::ComputeGradient(Vector &x, Vector *gradient){
-  Matrix quadratic_trans;
-  la::TransposeInit(&quadratic_term_, *quadratic_trans);
+  
   //Matrix sum_quad;
-  la::AddInit(&quadratic_term_, &quadratic_trans, *sum_quad);
   Vector temp2;
-  la::MulInit(&sum_quad, &x, *temp2);
-  la::AddInit(&linear_term_, &temp2, *gradient);
-
+  la::MulInit(sum_quad_, x, &temp2);
+  la::AddInit(linear_term_, temp2, gradient);
 }
 
 void QuadraticObjective::ComputeHessian(Vector &x, Matrix *hessian){
-  *hessian = &sum_quad;
+  hessian = &sum_quad_;
 }
 
