75be5cb9009d7072fd53926ef013a77365f8bb88
soyeon <soyeon@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-10-28 03:19:24 +0000

    update2

--- fastlib2/contrib/soyeon/test/lin_algebra_class.cc
+++ fastlib2/contrib/soyeon/test/lin_algebra_class.cc
@@ -1,6 +1,6 @@
 #include "lin_algebra_class.h"
 
-QuadraticObjective::Init(Matrix &quadratiic_term, Vector &linear_term) {
+void QuadraticObjective::Init(Matrix &quadratiic_term, Vector &linear_term) {
   DEBUG_SAME_SIZE(quadratic_term.n_rows(), quadratic_term.n_cols());
   DEBUG_SAME_SIZE(linear_term.length(), quadratic_term.n_rows());
   DEBUG_ASSERT_MSG(linear_term.length()==quadratic_term.n_rows(), 
@@ -14,21 +14,21 @@ void QuadraticObjective::ComputeObjective(Vector &x, double *objective) {
   DEBUG_SAME_SIZE(x.length(), quadratic_term_.n_rows()); 
   Vector temp1;
   la::MulInit(x, quadratic_term_, &temp1);
-  *objective = la::Dot(temp1, x) + la::Dot(linear_term, x);
+  *objective = la::Dot(temp1, x) + la::Dot(linear_term_, x);
 }
 
-void ComputeGradient(Vector &x, Vector *gradient){
+void QuadraticObjective::ComputeGradient(Vector &x, Vector *gradient){
   Matrix quadratic_trans;
-  la::TransposeInit(&quadratic_term, *quadratic_trans);
+  la::TransposeInit(&quadratic_term_, *quadratic_trans);
   //Matrix sum_quad;
-  la::AddInit(&quadratic_term, &quadratic_trans, *sum_quad);
+  la::AddInit(&quadratic_term_, &quadratic_trans, *sum_quad);
   Vector temp2;
   la::MulInit(&sum_quad, &x, *temp2);
-  la::AddInit(&linear_term, &temp2, *gradient);
+  la::AddInit(&linear_term_, &temp2, *gradient);
 
 }
 
-void ComputeHessian(Vector &x, Matrix *hessian){
+void QuadraticObjective::ComputeHessian(Vector &x, Matrix *hessian){
   *hessian = &sum_quad;
 }
 
--- fastlib2/contrib/soyeon/test/main.cc
+++ fastlib2/contrib/soyeon/test/main.cc
@@ -3,5 +3,24 @@
 
 int main(int argc, char *argv[]) {
 
+  fx_init(argc,argv);
+
   QuadraticAlgebraClass my_objective;
+
+  const char* current_x_file_name = fx_param_str_req(NULL, "xdata");
+  const char* quadratic_term_file_name = fx_param_str_req(NULL, "qdata");
+  const char* linear_term_file_name = fx_param_str_req(NULL, "ldata");
+
+  Vector current_x;
+  Matrix quad;
+  Vector lin;
+
+  data::Load(current_x_file_name, &current_x);  
+  data::Load(quadratic_term_file_name, &quad);
+  data::Load(linear_term_file_name, &lin);
+
+  my_objective.Init(quad, lin);
+  my_objective.ComputeObjective()
+
+
 }
