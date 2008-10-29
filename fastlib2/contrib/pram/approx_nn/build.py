001b7e06f52af5433f7666ab6e6908ebfdec1532
Parikshit Ram <p.ram@gatech.edu>
2008-10-29 07:23:24 +0000

    main file added to drive the dual tree version

--- fastlib2/contrib/pram/approx_nn/build.py
+++ fastlib2/contrib/pram/approx_nn/build.py
@@ -1,8 +1,14 @@
+
 librule(name="approxnn",
     headers=["approx_nn.h"],
-    #tests=["allnn_test.cc"],
-    deplibs=["fastlib:fastlib", "fastlib/tree:tree" ]
-    );
+    deplibs=["fastlib:fastlib", "fastlib/tree:tree" ]);
+librule(name="approxnn_dual",
+    headers=["approx_nn_dual.h"],
+    deplibs=["fastlib:fastlib", "fastlib/tree:tree" ]);
 binrule(name="main",
     sources=["main.cc"],
     deplibs=[":approxnn"]);
+binrule(name="main_dual",
+    sources=["main_dual.cc"],
+    deplibs=[":approxnn_dual"]);
+
