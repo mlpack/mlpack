1932d9e4e357357e28e85524c08e203afd0f694c
soyeon <soyeon@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-10-29 13:43:43 +0000

    review&add comments

--- fastlib2/contrib/soyeon/test/main.cc
+++ fastlib2/contrib/soyeon/test/main.cc
@@ -3,19 +3,35 @@
 
 int main(int argc, char *argv[]) {
 
+//<<<<<<< .mine
+  //fx_init(argc,argv);
+//=======
+  //parsing
   fx_module *fx_root = fx_init(argc, argv, NULL);
+//>>>>>>> .r2718
 
   QuadraticObjective my_objective;
 
+/*<<<<<<< .mine
+  const char* current_x_file_name = fx_param_str_req(NULL, "xdata");
+  const char* quadratic_term_file_name = fx_param_str_req(NULL, "qdata");
+  const char* linear_term_file_name = fx_param_str_req(NULL, "ldata");
+=======
+>>>>>>> .r2718*/
 
+  //optional input file, default file "xinit.csv"
   const char* initial_solution_file = fx_param_str(fx_root, "xinit_file", "xinit.csv");
+  
+  //Reguired input file
   const char* quadratic_term_file = fx_param_str_req(fx_root, "qdata");
   const char* linear_term_file = fx_param_str_req(fx_root, "ldata");
-  index_t problem_id = fx_param_int_req(fx_root, "problem_id");
+  //index_t problem_id = fx_param_int_req(fx_root, "problem_id");
 
   Vector initial_x;
   Matrix quadratic_term;
   Vector linear_term;
+  
+  //fastlib only load data as Matrix
   Matrix initial_x_mat;
   if (data::Load(initial_solution_file, &initial_x_mat)==SUCCESS_FAIL) {
     FATAL("File %s not found", initial_solution_file);
@@ -31,6 +47,12 @@ int main(int argc, char *argv[]) {
   my_objective.Init(quadratic_term, linear_term);
   double dummy_objective;
   my_objective.ComputeObjective(initial_x, &dummy_objective);
+  
+
+  fx_done(fx_root);
+
+
+
 
 
 }
