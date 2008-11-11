1b827daa9affe637fe1a07edb4c9728ef49f7b9d
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-11-11 21:50:55 +0000

    Finished first stage unconstrained optimization

--- fastlib2/mlpack/optimization/optimizer.h
+++ fastlib2/mlpack/optimization/optimizer.h
@@ -21,65 +21,256 @@
 #include "fastlib/fastlib.h"
 #include "NFL.h"
 #include "OptQNewton.h"
+#include "OptCG.h"
+
 namespace optim {
-template <typename METHOD, typename Objective, typename Constraints>
-class Optimizer {
+
+template<typename Method>
+class OptimizationTrait;
+
+template<>
+class OptimizationTrait<Method> {
  public:
-   void Init(Method *method, Objective *objective) {
-     method_    = method;
-     objective_ = objective;
-   }
-   void Optimize() {
+  typedef void NlpType;
+  static void InitializeMethod(fx_module *module, Method *method);
+
+};
+
+// Conjugate Gradient
+class OptimizationTrait<OPTPP::OptCG> {
+ public:
+  typedef OPTPP::NLF1 NlpType;
+  static void InitializeMethod(fx_module *module, Method *method) {
+    std::string search_strategy = fx_param_str(module, 
+                                      "search_strategy",
+                                      "line_search");
+    if (search_strategy=="line_search") {
+      method_->setSearchStrategy(OPTPP::LineSearch);
+    } else {
+      if (search_strategy=="trust_region") {
+        method_->setSearchStrategy(OPTPP::TrustRegion);
+      } else {
+        if (search_strategy=="trust_pds") {
+          method_->setSearchStrategy(OPTPP::TrustPDS);
+        } else {
+          FATAL("The specified search strategy %s is not supported", 
+              search_strategy.c_str());
+        }
+      }
+    } 
+    if (method_->checkDeriv()==false) {
+      NONFATAL("Warning finite difference derivative/hessian doesn't much 
+          analytic");
+    }    
+
+  }
+};
+
+// Quasi-Newton BFGS
+template<>
+class OptimizationTrait<OPTPP::OptQNewton> {
+ public:
+  typedef OPTPP::NLF1 NlpType;
+  static void InitializeMethod(fx_module *module, Method *method) {
+     std::string search_strategy = fx_param_str(module, 
+                                      "search_strategy",
+                                      "line_search");
+    if (search_strategy=="line_search") {
+      method_->setSearchStrategy(OPTPP::LineSearch);
+    } else {
+      if (search_strategy=="trust_region") {
+        method_->setSearchStrategy(OPTPP::TrustRegion);
+        method_->setTRSize(fx_param_double(module_, "trust_region_size", 
+              method_->getTRSize()));
+      } else {
+        if (search_strategy=="trust_pds") {
+          method_->setSearchStrategy(OPTPP::TrustPDS);
+        } else {
+          FATAL("The specified search strategy %s is not supported", 
+              search_strategy.c_str());
+        }
+      }
+    } 
+    if (method_->checkDeriv()==false) {
+      NONFATAL("Warning finite difference derivative/hessian doesn't much 
+          analytic");
+    }    
+  }
+};
+
+// Quasi-Newton with Finite Difference approximation on the Hessian
+template<>
+class OptimizationTrait<OPTPP::OptFDNewton> {
+ public:
+  typedef OPTPP::NLF1 NlpType;
+  static void InitializeMethod(fx_module *module, Method *method) {
+    OptimizationTrait<OPTPP::OptQNewton>::InitializeMethod(
+        module_, method);  
+  }
+};
+
+// Newton  with analytic expression for the Hessian
+template<>
+class OptimizationTrait<OPTPP::OptNewton> {
+ public:
+  typedef OPTPP::NLF2 NlpType;
+  static void InitializeMethod(fx_module *module, Method *method) {
+   OptimizationTrait<OPTPP::OptQNewton>::InitializeMethod(
+        module_, method);  
+ 
+  }
+};
+
+
+// L-BFGS 
+template<>
+class OptimizationTrait<OPTPP::OptLBFGS> {
+ public:
+  typedef OPTPP::NLF1 NlpType;
+  static void InitializeMethod(fx_module *module, Method *method) {
+    OptimizationTrait<OPTPP::OptCG>::InitializeMethod(
+        module_, method);  
+  
+  }
+};
+
+template <typename Method, typename Objective>
+class StaticUnconstrainedOptimizer {
+ public:
+  UnconstrainedOptimizer() {
+    objective_=NULL;
+    method_=NULL;
+  }
+  ~UnconstrainedOptimizer() {
+    if (method_==NULL) {
+      delete method_;
+    }
+  }
+  void Init(fx_module *module, Objective *objective) {
+    module_ = module;
+    objective_ = objective;
+    dimension_=objective_->dimension();
+    OptimizationTraits<Method>::NlpType nlp(dimension_.
+        ComputeObjective, Initialize, NULL);
+    // setting some generic options for the optimization method
+    const char *status_file=fx_param_str(module_, "status_file", "status.txt" );
+    if (method_->setOutputFile(status_file, 0)==false) {
+      FATAL("Failed to open the status file %s for writing", status_file);
+    }
+    // Setup the tolerances 
+    OPTPP::Tols tols;
+    tols.setDefaultTol();
+    if (fx_param_exists(module_, "function_tol")) {
+      tols.setFTol(fx_param_double(module_, "function_tol"))
+    } else {
+      if (fx_param_exists(module_, "step_tol")) {
+        tols.setStepTol(fx_param_double(module_, "step_tol"));
+      } else {
+        if (fx_param_exists(module_, "grad_tol")) {
+          tols.setGTol(fx_param_double(module_, "grad_tol"));
+        } 
+      }
+    }
+    if (fx_param_exists(module_, "max_step")) {
+      tols.setMaxStep(module_, fx_param_double(module_, "max_step"));
+    }
+    if (fx_param_exists(module_, "min_step")) {
+      tols.setMinStep(module_, fx_param_double(module_, "min_step"));
+    }
+    if (fx_param_exists(module_, "line_search_tol")) {
+      tols.setLSTol(module_, fx_param_double(module_, "min_step"));
+    }
+    if (fx_param_exists(module_, "trust_region_tol")) {
+      tols.setTRTol(module_, fx_param_double(module_, "trust_region_tol"));
+    }
+  
    
-   }
+    tols.setMaxIter(fx_param_int(module_, "max_iter", 1000));
+    method_ = new Method(nlp, tols);
+
+    OptimizationTraits<Method>::InitializeMethod(module_, method_);
+    
+  }
+  void Destruct() {
+    if (method_==NULL) {
+      delete method_;
+    }
+  }
+  void Optimize(Vector *result) {
+    method_->Optimize();
+    NEWMAT::ColumnVector x=method_->getXPrev();
+    result->Copy(x.data(), dimension_); 
+    fx_result_bool(module_, "converged", method_->checkConvg());
+    fx_result_int(module_, "iterations", method_->getIter());
+    method_->Cleanup();
+  }
  
  private:
-  Objective *objective_;
-  METHOD *method_; 
+  fx_module *module_;
+  static Objective *objective_;
+  Method *method_; 
   index_t dimension_;
   
-  void Initialize(int ndim, NEWMAT::ColumnVector &x) {
+  static void Initialize(int ndim, NEWMAT::ColumnVector &x) {
     DEBUG_ASSERT(ndim==dimension_);
     Vector vec;  
     vec->Alias(x.data(), dimension_);
     objective_->GiveInit(&vec);
     
   }
-  void ComputeObjective(int ndim, NEWMAT::ColumnVector &x, 
+  static void ComputeObjective(int ndim, NEWMAT::ColumnVector &x, 
       double &fx, int &result) {
     DEBUG_ASSERT(ndim==dimension_);
     Vector vec;  
     vec->Alias(x.data(), dimension_);
     objective_->ComputeObjective(vec, &fx);
+    result = NLPFunction;
   };
   
-  void ComputeObjective(int mode, int ndim, NEWMAT::ColumnVector &x, 
+  static void ComputeObjective(int mode, int ndim, NEWMAT::ColumnVector &x, 
       double &fx, NEWMAT::ColumnVector &gx, int &result) {
     DEBUG_ASSERT(ndim==dimension_);
     Vector vecx;  
     vecx->Alias(x.data(), dimension_);
-    objective_->ComputeObjective(vecx, &fx);
-    Vector vecg;
-    vecg->Alias(gx.data(), dimension_);
-    objective_->ComputeGradient(vecx, &vec)g;   
+    if (mode & NLPFunction) {
+      objective_->ComputeObjective(vecx, &fx);
+      result = NLPFunction;
+    } else {
+      if (mode & NLPGradient) {
+        Vector vecg;
+        vecg->Alias(gx.data(), dimension_);
+        objective_->ComputeGradient(vecx, &vec);
+        result = NLPGradient;
+      }
+    }   
   }
     
-  void ComputeObjective(int mode, int ndim, NEWMAT::ColumnVector &x, 
+  static void ComputeObjective(int mode, int ndim, NEWMAT::ColumnVector &x, 
       double &fx, NEWMAT::ColumnVector &gx, 
     NEWMAT::SymmetricMatrix &hx, int &result) {
     DEBUG_ASSERT(ndim==dimension_);
     Vector vecx;  
     vecx->Alias(x.data(), dimension_);
-    objective_->ComputeObjective(vec, &fx);
-    Vector vecg;
-    vecg->Alias(gx.data(), dimension_);
-    objective_->ComputeGradient(vecx, &vecg); 
-    Matrix hessian;
-    hessian.Alias(hx.data(), dimension_, dimension_);  
-    objective_->ComputeHessian(vecx, hessian);  
-  }
- 
+    if (mode & NLPFunction) {
+      objective_->ComputeObjective(vec, &fx);
+      result = NLPFunction;
+    } else {
+      if (mode & NLPGradient) {
+        Vector vecg;
+        vecg->Alias(gx.data(), dimension_);
+        objective_->ComputeGradient(vecx, &vecg); 
+        result = NLPGradient;
+      } else {
+        if (mode & NLPHessian) {
+          Matrix hessian;
+          hessian.Alias(hx.data(), dimension_, dimension_);  
+          objective_->ComputeHessian(vecx, hessian);  
+          result = NLPHessian;
+        }
+      }
+    }
+  } 
 };
   
-};
+}; // Namespace optim
 #endif
