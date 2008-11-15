c69623b2b2a1b6597d3726880be71f05941333bb
gtg739c <gtg739c@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-11-15 12:04:26 +0000

    Constrained optimization compiled, it needs to be further tested, also I have to examine whether Hessian comes initialized or not

--- fastlib2/mlpack/optimization/optimizer.h
+++ fastlib2/mlpack/optimization/optimizer.h
@@ -34,6 +34,12 @@
 #include "opt++/include/OptFDNewton.h"
 #include "opt++/include/OptNewton.h"
 #include "opt++/include/OptQNewton.h"
+#include "opt++/include/BoundConstraint.h"
+#include "opt++/include/CompoundConstraint.h"
+#include "opt++/include/LinearEquation.h"
+#include "opt++/include/LinearInequality.h"
+#include "opt++/include/NonLinearEquation.h"
+#include "opt++/include/NonLinearInequality.h"
 
 
 namespace optim {
@@ -43,8 +49,21 @@ typedef OPTPP::OptQNewton  QNewton;
 typedef OPTPP::OptQNewton  BFGS;
 typedef OPTPP::OptFDNewton FDNewton;
 typedef OPTPP::OptNewton   Newton;
-typedef OPTPP::OptLBFGS       LBFGS;
+typedef OPTPP::OptLBFGS    LBFGS;
 
+enum ConstraintType {
+  NoConstraint       = 0,
+  BoundConstraint    = 2,
+  LinearEquality     = 4,
+  LinearInequality   = 8,
+  NonLinearEquality  = 16,
+  NonLinearInequality= 32
+};
+
+template <typename Method, 
+         typename Objective, 
+         ConstraintType Constraint=NoConstraint>
+class StaticOptppOptimizer;
 
 // The default is good for CG and L-BFGS
 template<typename Method>
@@ -144,30 +163,223 @@ class OptimizationTrait<OPTPP::OptNewton> {
   }
 };
 
-/*
- *
- */ 
 
-template <typename Method, typename Objective>
-class StaticUnconstrainedOptimizer {
+
+template<typename Method, typename Objective, bool Applicable>
+class BoundConstraintTrait {
  public:
-  StaticUnconstrainedOptimizer() {
+  static void UpdateConstraints(Objective *object, 
+       OPTPP::OptppArray<OPTPP::Constraint> *constraint_array) {  
+  }
+};
+
+template<typename Method, typename Objective>
+class BoundConstraintTrait<Method, Objective, true> {
+ public:
+  static void UpdateConstraints(Objective *objective, 
+       OPTPP::OptppArray<OPTPP::Constraint> *constraint_array) {
+    index_t dimension = objective->dimension();
+    OPTPP::Constraint bc;
+    Vector *lower_bound;
+    Vector *upper_bound;
+    objective->GetBoundConstraint(lower_bound, upper_bound);
+    if (lower_bound!=NULL and upper_bound==NULL) {
+      NEWMAT::ColumnVector lower(lower_bound->ptr(), lower_bound->length());
+      bc = new OPTPP::BoundConstraint(dimension, lower); 
+    } else {
+      if (lower_bound==NULL and upper_bound!=NULL){
+        NEWMAT::ColumnVector upper(upper_bound->ptr(), upper_bound->length());
+        bc = new OPTPP::BoundConstraint(dimension, upper, false);
+      } else {
+        NEWMAT::ColumnVector lower(lower_bound->ptr(), lower_bound->length());
+        NEWMAT::ColumnVector upper(upper_bound->ptr(), upper_bound->length());
+        bc = new OPTPP::BoundConstraint(dimension, lower, upper);
+      }
+    }
+    constraint_array->append(bc);
+  }
+
+};
+
+template<typename Method, typename Objective, bool Applicable>
+class LinearEqualityTrait {
+ public:
+  static void UpdateConstraints(Objective *objective, 
+       OPTPP::OptppArray<OPTPP::Constraint> *constraint_array) {
+  
+  }
+
+};
+
+template<typename Method, typename Objective>
+class LinearEqualityTrait<Method, Objective, true> {
+ public:
+   static void UpdateConstraints(Objective *objective, 
+       OPTPP::OptppArray<OPTPP::Constraint> *constraint_array) {
+     index_t dimension = objective->dimension();
+     Matrix *a_mat;
+     Vector *b_vec;
+     objective->GetLinearEquality(a_mat, b_vec);
+     NEWMAT::Matrix alpha(a_mat->ptr(), a_mat->n_rows(), a_mat->n_cols());
+     NEWMAT::ColumnVector beta(b_vec->ptr(), b_vec->length());  
+     OPTPP::Constraint  leq = new OPTPP::LinearEquation(alpha, beta);
+     constraint_array->append(leq);
+  }
+
+
+};
+
+template<typename Method, typename Objective, bool Applicable>
+class LinearInequalityTrait {
+ public:
+  static void UpdateConstraints(Objective *object, 
+       OPTPP::OptppArray<OPTPP::Constraint> *constraint_array) {
+  }
+
+};
+
+template<typename Method, typename Objective>
+class LinearInequalityTrait<Method, Objective, true> {
+ public:
+  static void UpdateConstraints(Objective *objective, 
+       OPTPP::OptppArray<OPTPP::Constraint> *constraint_array) {  
+    index_t dimension = objective->dimension();
+    OPTPP::Constraint lineq;
+    Matrix *a_mat;
+    Vector *left_b;
+    Vector *right_b;
+    objective->GetLinearInequalityConstraint(a_mat, left_b, right_b);
+    NEWMAT::Matrix alpha(a_mat->ptr(), a_mat->n_rows(), a_mat->n_cols());
+    DEBUG_ASSERT(a_mat!=NULL);
+    DEBUG_ASSERT(left_b!=NULL or right_b!=NULL); 
+    if (left_b!=NULL and right_b==NULL) {
+      NEWMAT::ColumnVector left(left_b->ptr(), left_b->length());
+      lineq = new OPTPP::LinearInequality(alpha, left); 
+    } else {
+      if (left_b==NULL and right_b!=NULL){
+        NEWMAT::ColumnVector right(right_b->ptr(), right_b->length());
+        lineq = new OPTPP::LinearInequality(alpha, right_b, false);
+      } else {
+        NEWMAT::ColumnVector left(left_b->ptr(), left_b->length());
+        NEWMAT::ColumnVector right(right_b->ptr(), right_b->length());
+        lineq = new OPTPP::LinearInequality(alpha, left, right);
+      }
+    }
+    constraint_array->append(lineq);
+  }
+};
+
+template<typename Method, typename Objective, bool Applicable>
+class NonLinearEqualityTrait {
+ public:
+  static void UpdateConstraints(Objective *object, 
+       OPTPP::OptppArray<OPTPP::Constraint> *constraint_array) {
+  }
+};
+
+template<typename Method, typename Objective>
+class NonLinearEqualityTrait<Method, Objective, true> {
+ public:
+  static void UpdateConstraints(Objective *objective, 
+       OPTPP::OptppArray<OPTPP::Constraint> *constraint_array) {
+    index_t dimension = objective->dimension();
+    OPTPP::NLP *nlp = new OPTPP::NLP(
+        new typename OptimizationTrait<Method>::NlpType(
+          StaticOptppOptimizer<Method, Objective>::
+          ComputeNonLinearEqualityConstraints));
+    OPTPP::Constraint eq = new OPTPP::NonLinearEquation(nlp,
+        objective->num_of_non_linear_equations());
+    constraint_array->append(eq);
+  }
+};
+
+template<typename Method, typename Objective, bool Applicable>
+class NonLinearInequalityTrait {
+ public: 
+  static void UpdateConstraints(Objective *objective, 
+      OPTPP::OptppArray<OPTPP::Constraint> *constraint_array) {
+  }
+  
+};
+
+template<typename Method, typename Objective>
+class NonLinearInequalityTrait<Method, Objective, true> {
+ public: 
+    static void UpdateConstraints(Objective *objective, 
+       OPTPP::OptppArray<OPTPP::Constraint> *constraint_array) {
+    index_t dimension = objective->dimension();
+    OPTPP::NLP *nlp = new OPTPP::NLP(
+        new typename OptimizationTrait<Method>::NlpType(
+          StaticOptppOptimizer<Method, Objective>::
+          ComputeNonLinearInequalityConstraints));
+    Vector *left_b;
+    Vector *right_b;
+    objective->GetNonLinearInequalityConstraintBounds(left_b, right_b);
+    OPTPP::Constraint ineq; 
+    if (left_b==NULL and right_b==NULL) {
+      ineq = new OPTPP::NonLinearEquation(nlp, 
+          objective->num_of_non_linear_inequalities());
+    } else {
+      if (left_b!=NULL and right_b==NULL) {
+        NEWMAT::ColumnVector left(left_b->ptr(), left_b->length());    
+        ineq = new OPTPP::NonLinearInequality(nlp, left,
+            objective->num_of_non_linear_inequalities());  
+      } else {   
+        if (left_b==NULL and right_b!=NULL) {
+          NEWMAT::ColumnVector right(right_b->ptr(), right_b->length());    
+          ineq = new OPTPP::NonLinearInequality(nlp, right, false,
+              objective->num_of_non_linear_inequalities());  
+   
+        } else {
+          if (left_b!=NULL and right_b!=NULL) {
+            NEWMAT::ColumnVector right(right_b->ptr(), right_b->length());    
+            NEWMAT::ColumnVector left(left_b->ptr(), left_b->length());    
+            ineq = new OPTPP::NonLinearInequality(nlp, left, right,
+                objective->num_of_non_linear_inequalities());
+          
+          }
+        }
+      }
+    } 
+  } 
+};
+
+/*  Wrapper to Opt++ so that your life is easier 
+ *  when it comes to interfacing Fastlib
+ */ 
+template <typename Method, typename Objective, ConstraintType Constraint>
+class StaticOptppOptimizer {
+ public:
+  StaticOptppOptimizer() {
     objective_=NULL;
     method_=NULL;
     nlp_=NULL;
+    compound_constraint_=NULL;
   }
-  ~StaticUnconstrainedOptimizer() {
-    if (method_!=NULL) {
-      delete method_;
-    }
-    if (nlp_!=NULL) {
-      delete nlp_;
-    }
+  ~StaticOptppOptimizer() {
+    Destruct();
   }
   void Init(fx_module *module, Objective *objective) {
     module_ = module;
     objective_ = objective;
     dimension_=objective_->dimension();
+    OPTPP::OptppArray<OPTPP::Constraint> constraint_array;
+    BoundConstraintTrait<Method, Objective, 
+     (bool)(Constraint & BoundConstraint)>::UpdateConstraints(objective_, 
+          &constraint_array);
+    LinearEqualityTrait<Method, Objective, 
+     (bool)(Constraint & LinearEquality)>::UpdateConstraints(objective_, 
+          &constraint_array);
+    LinearInequalityTrait<Method, Objective, 
+     (bool)(Constraint & LinearInequality)>::UpdateConstraints(objective_, 
+          &constraint_array);
+    NonLinearEqualityTrait<Method, Objective, 
+     (bool)(Constraint & NonLinearEquality)>::UpdateConstraints(objective_, 
+          &constraint_array);
+    NonLinearInequalityTrait<Method, Objective, 
+     (bool)(Constraint & NonLinearInequality)>::UpdateConstraints(objective_, 
+          &constraint_array);
+ 
     nlp_ = new typename OptimizationTrait<Method>::NlpType(dimension_, ComputeObjective, 
         Initialize, (OPTPP::CompoundConstraint *)NULL);
     // Setup the tolerances 
@@ -214,6 +426,10 @@ class StaticUnconstrainedOptimizer {
       delete nlp_;
       nlp_=NULL;
     }
+    if (compound_constraint_!=NULL) {
+      delete compound_constraint_;
+      compound_constraint_=NULL;
+    }
   }
   void Optimize(Vector *result) {
     method_->optimize();
@@ -228,6 +444,8 @@ class StaticUnconstrainedOptimizer {
   fx_module *module_;
   static Objective *objective_;
   typename OptimizationTrait<Method>::NlpType *nlp_;
+  OPTPP::CompoundConstraint *compound_constraint_;
+
   Method *method_; 
   index_t dimension_;
   
@@ -251,13 +469,14 @@ class StaticUnconstrainedOptimizer {
     vecx.Alias((double *)x.data(), ndim);
     if (mode & OPTPP::NLPFunction) {
       objective_->ComputeObjective(vecx, &fx);
+      result = OPTPP::NLPFunction;   
     } 
     if (mode & OPTPP::NLPGradient) {
        Vector vecg;
        vecg.Alias(gx.data(), ndim);
        objective_->ComputeGradient(vecx, &vecg);
+       result = OPTPP::NLPGradient;   
     }
-    result = OPTPP::NLPFunction & OPTPP::NLPGradient;   
   }
     
   static void ComputeObjective(int mode, int ndim, const NEWMAT::ColumnVector &x, 
@@ -267,23 +486,107 @@ class StaticUnconstrainedOptimizer {
     vecx.Alias(x.data(), ndim);
     if (mode & OPTPP::NLPFunction) {
       objective_->ComputeObjective(vecx, &fx);
+      result = OPTPP::NLPFunction;
     }
     if (mode & OPTPP::NLPGradient) {
       Vector vecg;
       vecg.Alias(gx.data(), ndim);
       objective_->ComputeGradient(vecx, &vecg); 
+      result=OPTPP::NLPGradient;
     }
     if (mode & OPTPP::NLPHessian) {
       Matrix hessian;
       hessian.Alias(hx.data(), ndim, ndim);  
       objective_->ComputeHessian(vecx, hessian);  
+      result = OPTPP::NLPHessian;
     }
-    result = OPTPP::NLPFunction & OPTPP::NLPGradient & OPTPP::NLPHessian;   
   } 
+
+  static void ComputeNonLinearEqualityConstraints(int mode, 
+      const NEWMAT::ColumnVector &x, NEWMAT::ColumnVector &cvalue, 
+      int &result) {
+    Vector vecx;  
+    vecx.Alias(x.data(), x.Nrows());
+    Vector vecc;
+    vecc.Alias(cvalue.data(), cvalue.Nrows());
+    objective_->ComputeNonLinearEqualityConstraints(vecx, &vecc);
+    result = OPTPP::NLPConstraint;
+  
+  }
+  static void ComputeNonLinearEqualityConstraints(int mode, 
+      int ndim, const NEWMAT::ColumnVector &x,  
+      NEWMAT::ColumnVector &cvalue, NEWMAT::Matrix &cJacobian,
+      int &result) {
+    Vector vecx;  
+    vecx.Alias((double *)x.data(), ndim);
+    if (mode & OPTPP::NLPConstraint) {
+      Vector vecc;
+      vecc.Alias(cvalue.data(), ndim);
+      objective_->ComputeNonLinearEqualityConstraints(vecx, &vecc);
+      result = OPTPP::NLPConstraint;
+    } 
+    if (mode & OPTPP::NLPCJacobian) {
+       Matrix cjacob;
+       cjacob.Alias(cJacobian.data(), cJacobian.Nrows(), cJacobian.Ncols());
+       objective_->ComputeNonLinearEqualityConstraintsJacobian(vecx, &cjacob);
+       result = OPTPP::NLPCJacobian;   
+    } 
+  }
+  static void ComputeNonLinearEqualityConstraints(int mode, 
+      int ndim, const NEWMAT::ColumnVector &x,  
+      NEWMAT::ColumnVector &cvalue, NEWMAT::Matrix &cJacobian,
+      OPTPP::OptppArray<NEWMAT::SymmetricMatrix> &cHessian,
+      int &result) {
+    FATAL("Not supported yet");
+  }
+ 
+  static void ComputeNonLinearInequalityConstraints(int mode, 
+      const NEWMAT::ColumnVector &x, NEWMAT::ColumnVector &cvalue, 
+      int &result) {
+    Vector vecx;  
+    vecx.Alias(x.data(), x.Nrows());
+    Vector vecc;
+    vecc.Alias(cvalue.data(), x.Nrows());
+    objective_->ComputeNonLinearInequalityConstraints(vecx, &vecc);
+    result = OPTPP::NLPConstraint;  
+  }
+
+  // We still have to test wether cJacobian and Hessian etc get initialized
+  // properly
+  static void ComputeNonLinearInequalityConstraints(int mode, 
+      int ncon, const NEWMAT::ColumnVector &x,  
+      NEWMAT::ColumnVector &cvalue, NEWMAT::Matrix &cJacobian,
+      int &result) {
+    Vector vecx;  
+    vecx.Alias((double *)x.data(), x.Nrows());
+    if (mode & OPTPP::NLPConstraint) {
+      Vector vecc;
+      vecc.Alias(cvalue.data(), x.Nrows());
+      objective_->ComputeNonLinearInequalityConstraints(vecx, &vecc);
+      result = OPTPP::NLPConstraint;
+    } 
+    if (mode & OPTPP::NLPCJacobian) {
+       Matrix cjacob;
+       cjacob.Alias(cJacobian.data(), cJacobian.Nrows(), cJacobian.Ncols());
+       objective_->ComputeNonLinearInequalityConstraintsJacobian(vecx, 
+           &cjacob);
+       result = OPTPP::NLPCJacobian;   
+    }
+  
+  }
+  static void ComputeNonLinearInequalityConstraints(int mode, 
+      int ndim, const NEWMAT::ColumnVector &x,  
+      NEWMAT::ColumnVector &cvalue, NEWMAT::Matrix &cJacobian,
+      OPTPP::OptppArray<NEWMAT::SymmetricMatrix> &cHessian,
+      int &result) {
+    FATAL("Not supported yet");
+  }
+
 };
 
-template<typename Method, typename Objective>
-  Objective *StaticUnconstrainedOptimizer<Method, Objective>::objective_=NULL;
+template<typename Method, typename Objective, ConstraintType Constraint>
+  Objective *StaticOptppOptimizer<Method, Objective, 
+      Constraint>::objective_=NULL;
 
 }; // Namespace optim
 #endif
