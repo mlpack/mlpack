6e47b28dc0d9bedbc17d54657f8fd5e25e252272
tqlong <tqlong@9d5b8971-822b-0410-80eb-d18c1038ef23>
2008-11-21 04:37:34 +0000

    QUIC-SVD small changes

--- fastlib2/contrib/tqlong/quicsvd/quicsvd_main.cc
+++ fastlib2/contrib/tqlong/quicsvd/quicsvd_main.cc
@@ -53,10 +53,10 @@ int main(int argc, char* argv[]) {
   // parse target relative error, default = 0.1
   const double targetRelErr = fx_param_double(NULL, "relErr", 0.1);
 
-  fx_timer_start(NULL, "quicsvd");
+  fx_timer_start(NULL, "quicsvd_time");
   // call the QUIC-SVD method
   QuicSVD::SVDInit(A, targetRelErr, &s, &U, &VT);
-  fx_timer_stop(NULL, "quicsvd");
+  fx_timer_stop(NULL, "quicsvd_time");
   
   if (fx_param_exists(NULL, "U_out"))
     data::Save(fx_param_str(NULL, "U_out", NULL), U);
