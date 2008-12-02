83b9bd22a87a9b2e72ed8e9901dc98cc71f5353d
Dongryeol Lee <drselee@gmail.com>
2008-12-02 04:14:22 +0000

    Scatter plot for KDE added.

--- fastlib2/mlpack/mlpackdemo/ComputeAlgorithms.m
+++ fastlib2/mlpack/mlpackdemo/ComputeAlgorithms.m
@@ -1,4 +1,4 @@
-function ComputeAlgorithms(data_file, method, knn)
+function ComputeAlgorithms(data_file, method, handles)
 
   % Dimension-reduction algorithms.
   if strcmp(method, 'PCA')==1
@@ -10,17 +10,19 @@ function ComputeAlgorithms(data_file, method, knn)
   % Nearest neighbor algorithms.
   if strcmp(method, 'EXACT')==1
     data_file = ['/net/hg200/dongryel/MLPACK_test_datasets/' data_file '.csv'];
-    command = ['setenv LD_LIBRARY_PATH /usr/lib/gcc/x86_64-redhat-linux5E/4.1.2 && cd ../allknn && ./allknn_exe --reference_file=' data_file ' --knns=' int2str(knn)];
-    system(command);
+    data_file = '/net/hc293/dongryel/Research/fastlib2/mlpack/mlpackdemo/dummy.csv';
+    command = ['setenv LD_LIBRARY_PATH /usr/lib/gcc/x86_64-redhat-linux5E/4.1.2 && cd ../allknn && ./allknn_exe --reference_file=' data_file ' --knns=' int2str(handles.knn)];
+    [status, result] = system(command);
     % Convert the list to the adjacency matrix.
-    ConvertKnnResultToAdjacencyMatrix()
+    ConvertKnnResultToAdjacencyMatrix();
   end
   if strcmp(method, 'APPROX')==1
   end
   
   % More algorithms.
-  if strcmp(method, 'KDE')==1
+  if strcmp(method, 'KDE')==1    
     data_file = ['/net/hg200/dongryel/MLPACK_test_datasets/' data_file '.csv'];
+    data_file = '/net/hc293/dongryel/Research/fastlib2/mlpack/mlpackdemo/dummy.csv';
     command = ['setenv LD_LIBRARY_PATH /usr/lib/gcc/x86_64-redhat-linux5E/4.1.2 && cd ../kde/ && ./kde_cv_bin --data=' data_file ' --kde/kernel=gaussian --kde/probability=0.8 --kde/relative_error=0.1'];
     [status, result] = system(command);
     % Parse the optimal bandwidth...
@@ -31,6 +33,13 @@ function ComputeAlgorithms(data_file, method, knn)
     kde_command = ['setenv LD_LIBRARY_PATH /usr/lib/gcc/x86_64-redhat-linux5E/4.1.2 && cd ../kde/ && ./dualtree_kde_bin --data=' data_file ' --kde/kernel=gaussian --kde/probability=0.8 --kde/relative_error=0.1 --kde/bandwidth=' num2str(optimal_bandwidth) ' --kde/fast_kde_output=kde_output.txt --kde/loo'];
     % Run the KDE on the optimal bandwidth.
     [kde_status, kde_result] = system(kde_command)
+    % Get the handle to the GUI plot, and plot the density.
+    get(handles.axes1);
+    data_matrix = load('dummy.csv');
+    density_values = load('../kde/kde_output.txt');
+    size(data_matrix)
+    size(density_values)
+    scatter3(data_matrix(:, 1), data_matrix(:, 2), density_values);
   end
   if strcmp(method, 'Range search')==1
       
@@ -41,4 +50,3 @@ function ComputeAlgorithms(data_file, method, knn)
   if strcmp(method, 'Decision Trees')==1
       
   end
-
