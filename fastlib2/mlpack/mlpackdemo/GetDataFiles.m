e7518f01ba5c5c4fa246014112a2d93d5b63fdb7
Dongryeol Lee <drselee@gmail.com>
2008-12-03 18:31:27 +0000

    Orthogonal range search interfaced in, now need to do plotting...

--- fastlib2/mlpack/mlpackdemo/GetDataFiles.m
+++ fastlib2/mlpack/mlpackdemo/GetDataFiles.m
@@ -1,3 +1,5 @@
 function data_files=GetDataFiles()
 % this function retrurns the available data files
-  data_files = {'/net/hg200/dongryel/MLPACK_test_datasets/LayoutHistogram.csv', '/net/hg200/dongryel/MLPACK_test_datasets/LayoutHistogram-rnd.csv'};
+  data_files = {'/net/hg200/dongryel/MLPACK_test_datasets/LayoutHistogram.csv', '/net/hg200/dongryel/MLPACK_test_datasets/LayoutHistogram-rnd.csv',...
+      '/net/hg200/dongryel/MLPACK_test_datasets/mnist10k_test.csv', '/net/hg200/dongryel/MLPACK_test_datasets/mnist60k_train.csv',...
+      '/scratch/nips2008_datasets/sj2-50000-2.csv'};
