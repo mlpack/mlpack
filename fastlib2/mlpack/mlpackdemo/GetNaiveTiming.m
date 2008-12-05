5517ef59bb443d31657243c6ffaa40898dd84b14
Dongryeol Lee <drselee@gmail.com>
2008-12-05 23:02:06 +0000

    Commiting what I have so far now, now need to work on NBC... and timings.

--- fastlib2/mlpack/mlpackdemo/GetNaiveTiming.m
+++ fastlib2/mlpack/mlpackdemo/GetNaiveTiming.m
@@ -1,5 +1,15 @@
 function timing = GetNaiveTiming(data_file, method)
 [status, result] = system( ['wc -l ' data_file] );
+fid = fopen(data_file, 'rt');
+first_line = fgetl(fid);
+fclose(fid);
+counter = 1;
+[token, rem] = strtok(first_line,',');
+while ~isempty(rem)
+    [token, rem] = strtok(rem,',');
+    counter = counter + 1;
+end;
+
 first_blank = find(result == ' ', 1, 'first');
 num_lines = str2double( result(1:first_blank - 1) );
 data_files = GetDataFiles();
@@ -13,8 +23,20 @@ if strcmp(method, 'Decision Tree') == 1
     timing = 3000;
 end;
 if strcmp(method, 'KPCA') == 1
-    timing = 1800 / (4656 * 4656 * 4656) * (num_lines * num_lines * num_lines);
+    polycoeff = [0.002 -0.1471 25.6272];
+    timing = polyval(polycoeff, 0.5 * (counter + num_lines));   
 end;
 if strcmp(method, 'PCA') == 1
-    timing = 1800 / (4656 * 4656 * 4656) * (num_lines * num_lines * num_lines);
+    polycoeff = [0.002 -0.1471 25.6272];
+    timing = polyval(polycoeff, 0.5 * (counter + num_lines));
+end;
+if strcmp(method, 'EMST') == 1
+    polycoeff = [0.002 -0.1471 25.6272];
+    timing = polyval(polycoeff, 0.5 * (counter + num_lines));
+end;
+if strcmp(method, 'EXACT') == 1
+    timing = 1000;
+end;
+if strcmp(method, 'APPROX') == 1
+    timing = 1000;
 end;
