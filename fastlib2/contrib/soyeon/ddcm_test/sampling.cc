fdd7f72231f2be3e21a6b82f02ac6a37c36c687f
soyeon <soyeon@9d5b8971-822b-0410-80eb-d18c1038ef23>
2009-01-08 14:53:13 +0000

    before handling scaling

--- fastlib2/contrib/soyeon/ddcm_test/sampling.cc
+++ fastlib2/contrib/soyeon/ddcm_test/sampling.cc
@@ -140,13 +140,15 @@ void Sampling::Init(fx_module *module, int *num_of_people,
 		NOTIFY("Number of parameters is %d", num_of_betas+2);
 		initial_parameter->Init(num_of_betas+2);
 		
-		//initial_parameter->SetAll(1.5);
+		initial_parameter->SetAll(10.0);
 
+		/*
 		initial_parameter->SetZero();
 		(*initial_parameter)[0]=2;
 		(*initial_parameter)[1]=1.1;
 		(*initial_parameter)[num_of_betas]=1.8;
 		(*initial_parameter)[num_of_betas+1]=2.1;
+		*/
 
 		/*
 		cout<<"Starting points are:   ";
