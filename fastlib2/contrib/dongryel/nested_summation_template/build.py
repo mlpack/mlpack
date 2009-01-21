2e79577d24d4abea6af2da25a505c80dde961e60
Dongryeol Lee <drselee@gmail.com>
2009-01-21 02:13:26 +0000

    Compilation error fix and basic stratification using priority queue works now.

--- fastlib2/contrib/dongryel/nested_summation_template/build.py
+++ fastlib2/contrib/dongryel/nested_summation_template/build.py
@@ -8,7 +8,6 @@ librule(
                "strata.h",
                "sum.h"],    
     deplibs = ["fastlib:fastlib_int",
-               "contrib/dongryel/multitree_template:multitree_template",
                "contrib/dongryel/proximity_project:proximity_project",
                "mlpack/series_expansion:series_expansion"]
     )
