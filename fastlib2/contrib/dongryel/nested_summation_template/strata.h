260c5c778510802c6f347ad221ba1d754a1b13e3
Dongryeol Lee <drselee@gmail.com>
2009-01-21 02:13:26 +0000

    Compilation error fix and basic stratification using priority queue works now.

--- fastlib2/contrib/dongryel/nested_summation_template/strata.h
+++ fastlib2/contrib/dongryel/nested_summation_template/strata.h
@@ -84,16 +84,88 @@ class Strata {
     Reset();
   }
 
+  /** @brief Takes a root of the tree, and expands to such that the
+   *         number of tuples in the monochromatic sense in the strata
+   *         is at most fraction of the number of tuples formed among
+   *         the root node. The expansion is using the number of data
+   *         points times sum of per-dimension variance heuristic.
+   */
+  void Init(TreeType *root_in, index_t num_times_replicated,
+	    double fraction_desired) {
+
+    // Set the total number of terms.
+    node_list.Init();
+    total_num_terms = root_in->count();
+
+    // The priority queue used for the expansion.
+    std::priority_queue<TreeType *, std::vector<TreeType *>,
+      CompareTreeType> frontier;
+    frontier.push(root_in);
+    total_num_stratum = 0;
+
+    // Compute the n-tuple formed among the points at the root node.
+    double total_n_tuples_on_root = 
+      math::BinomialCoefficient(root_in->count() - 1, num_times_replicated);
+    double total_n_tuples_so_far = total_n_tuples_on_root;
+
+    // First expand the tree and form the frontier which will become
+    // the list of strata. This loop might have cancellation issues,
+    // since I subtract and add the corrections, but I do this since
+    // STL priority queue does not provide iterators...
+    do {
+      TreeType *popped_node = frontier.top();
+      frontier.pop();
+      if(popped_node->is_leaf()) {
+	popped_node->stat().in_strata = true;
+	node_list.PushBackRaw();
+	node_list[node_list.size() - 1] = popped_node;
+      }
+      else {
+	total_n_tuples_so_far -= 
+	  math::BinomialCoefficient(popped_node->count() - 1,
+				    num_times_replicated);
+	total_n_tuples_so_far +=
+	  math::BinomialCoefficient(popped_node->left()->count() - 1,
+				    num_times_replicated);
+	total_n_tuples_so_far +=
+	  math::BinomialCoefficient(popped_node->right()->count() - 1,
+				    num_times_replicated);
+	frontier.push(popped_node->left());
+	frontier.push(popped_node->right());
+      }
+
+      // Repeat until the priority queue is not empty and we have
+      // still more nodes to expand...
+    } while(frontier.size() > 0 && total_n_tuples_so_far >
+	    fraction_desired * total_n_tuples_on_root);
+
+    // Empty the priority queue.
+    while(!frontier.empty()) {
+      TreeType *popped_node = frontier.top();
+      popped_node->stat().in_strata = true;
+      frontier.pop();
+      node_list.PushBackRaw();
+      node_list[node_list.size() - 1] = popped_node;
+    }    
+    total_num_stratum = node_list.size();
+
+    // The 0th row stores the sum of the samples, the 1st row stores
+    // the squared sum for each stratum.
+    statistics_for_each_stratum.Init(2, total_num_stratum);
+    output_allocation_for_each_stratum.Init(total_num_stratum);
+    Reset();
+  }
+
   /** @brief Clear all the samples and start over.
    */
   void Reset() {
-    for(index_t i = 0; i < totla_num_stratum; i++) {
-      num_samples_for_each_stratum = 0;
-      output_allocation_for_each_stratum = 0;
+    for(index_t i = 0; i < total_num_stratum; i++) {
+      //num_samples_for_each_stratum = 0;
+      output_allocation_for_each_stratum[i] = 0;
     }
     total_num_samples_so_far = 0;
     statistics_for_each_stratum.SetZero();
-    total_samples_to_allocate = 0;    
+    //total_samples_to_allocate = 0;    
   }
 
 };
