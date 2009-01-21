8b5fe6cf6ebf8a006fac5b8a882a2044008ccd4b
Dongryeol Lee <drselee@gmail.com>
2009-01-21 03:31:28 +0000

    Monochromatic trick added.

--- fastlib2/contrib/dongryel/multitree_template/build.py
+++ fastlib2/contrib/dongryel/multitree_template/build.py
@@ -6,5 +6,6 @@ librule(
                "multitree_utility.h"],
     deplibs = ["fastlib:fastlib_int",
                "contrib/dongryel/proximity_project:proximity_project",
+               "contrib/dongryel/nested_summation_template:nested_summation_template",
                "mlpack/series_expansion:series_expansion"]
     )
