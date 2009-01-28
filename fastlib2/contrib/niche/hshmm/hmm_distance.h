9c6812f9105a44dc411b47e7f8df6e4c714e57b0
niche <niche@9d5b8971-822b-0410-80eb-d18c1038ef23>
2009-01-28 21:07:21 +0000

    added code for ObservableKernel

--- fastlib2/contrib/niche/hshmm/hmm_distance.h
+++ fastlib2/contrib/niche/hshmm/hmm_distance.h
@@ -8,7 +8,7 @@ class HMM_Distance {
  public:
 
   static double Compute(HMM hmm_a, HMM hmm_b, int num_times) {
-    double dist = 0;
+    //double dist = 0;
     
     int n1 = hmm_a.n_states();
     int n2 = hmm_b.n_states();
@@ -101,10 +101,150 @@ dist =
   }
 
   
-  static double ObservableKernel(Distribution distribution1,
-				 Distribution distribution2) {
+  // Note: This is the multivariate Gaussian case.
+  // TODO: mixture of Gaussian case (nearly trivial)
+  static double ObservableKernel(Distribution x,
+				 Distribution y) {
     double val = 1;
+
+    int n = x.n_dims();    
+
+    double lambda = 1; //NOTE: CHANGE THIS LATER
+
+    Matrix sigma_x, sigma_y;
+
+    sigma_x.Copy(x.sigma());
+    sigma_y.Copy(y.sigma());
+
+
+    // 1) compute norm constants by which result should eventually be divided
+    double x_norm_constant =
+      pow(2 * M_PI, ((double)n) / 2.)
+      * sqrt(fabs(la::Determinant(sigma_x)));
+    double y_norm_constant =
+      pow(2 * M_PI, ((double)n) / 2.)
+      * sqrt(fabs(la::Determinant(sigma_y)));
+
+
+    // 2) compute the first, non-exponentiated term
     
+    // take care of the 1/2 scalars in the exponent
+    la::Scale(2, &sigma_x);
+    la::Scale(2, &sigma_y);
+
+
+    Matrix sigma_x_inv;
+    la::InverseInit(sigma_x, &sigma_x_inv);
+
+    Matrix sigma_y_inv;
+    la::InverseInit(sigma_y, &sigma_y_inv);
+
+    Matrix product;
+    la::MulInit(sigma_x_inv, sigma_y_inv, &product);
+
+    Matrix sum;
+    la::AddInit(sigma_x_inv, sigma_y_inv, &sum);
+    
+    la::Scale(lambda, &sum);
+
+    la::AddTo(product, &sum);
+
+    double first_term = pow(2*M_PI, n) / sqrt(fabs(la::Determinant(sum)));
+
+
+    // 3) compute the second, exponentiated term
+    
+    Matrix eye;
+    eye.Init(n, n);
+    eye.SetZero();
+    for(int i = 0; i < n; i++) {
+      eye.set(i, i, 1);
+    }
+    
+    Matrix lambda_sigma_x;
+    la::ScaleInit(lambda, sigma_x, &lambda_sigma_x);
+
+    Matrix alpha;
+    la::AddInit(lambda_sigma_x, eye, &alpha);
+    la::Inverse(&alpha); // alpha = inv(lambda * sigma_x + I)
+    
+    Matrix A;
+    la::AddInit(sigma_x, sigma_y, &A);
+    la::Scale(lambda, &A);
+    la::AddTo(eye, &A);
+    la::Inverse(&A); // A = inv(lambda * (sigma_x + sigma_y) + I)
+
+
+    Matrix beta;
+    la::AddInit(sigma_x, sigma_y, &beta);
+    la::Inverse(&beta); // beta = inv(sigma_x + sigma_y)
+    
+    Vector temp_vec1, temp_vec2;
+
+    la::MulInit(x.mu(), alpha, &temp_vec1);
+    la::MulInit(temp_vec1, sigma_y, &temp_vec2);
+    la::MulOverwrite(temp_vec2, beta, &temp_vec1);
+    la::MulOverwrite(temp_vec1, sigma_x, &temp_vec2);
+    la::MulOverwrite(temp_vec2, alpha, &temp_vec1);
+    double result = lambda * lambda * la::Dot(temp_vec1, x.mu());
+
+    la::MulOverwrite(x.mu(), alpha, &temp_vec1);
+    la::MulOverwrite(temp_vec1, sigma_x, &temp_vec2);
+    la::MulOverwrite(temp_vec2, beta, &temp_vec1);
+    result += lambda * la::Dot(temp_vec1, y.mu());
+
+    la::MulOverwrite(x.mu(), alpha, &temp_vec1);
+    la::MulOverwrite(temp_vec1, sigma_y, &temp_vec2);
+    la::MulOverwrite(temp_vec2, A, &temp_vec1);
+    result -= lambda * lambda * la::Dot(temp_vec1, x.mu());
+
+    la::MulOverwrite(x.mu(), alpha, &temp_vec1);
+    la::MulOverwrite(temp_vec1, sigma_y, &temp_vec2);
+    la::MulOverwrite(temp_vec2, A, &temp_vec1);
+    la::MulOverwrite(temp_vec1, sigma_y_inv, &temp_vec2);
+    result -= lambda * la::Dot(temp_vec2, y.mu());
+
+    la::MulOverwrite(x.mu(), alpha, &temp_vec1);
+    la::MulOverwrite(temp_vec1, sigma_y, &temp_vec2);
+    la::MulOverwrite(temp_vec2, A, &temp_vec1);
+    la::MulOverwrite(temp_vec1, sigma_x, &temp_vec2);
+    la::MulOverwrite(temp_vec2, sigma_y_inv, &temp_vec1);
+    result -= lambda * lambda * la::Dot(temp_vec1, y.mu());
+
+     
+    la::MulOverwrite(y.mu(), beta, &temp_vec1);
+    la::MulOverwrite(temp_vec1, sigma_x, &temp_vec2);
+    la::MulOverwrite(temp_vec2, alpha, &temp_vec1);
+    result += lambda * la::Dot(temp_vec1, x.mu());
+    
+    la::MulOverwrite(y.mu(), beta, &temp_vec1);
+    la::MulOverwrite(temp_vec1, sigma_x, &temp_vec2);
+    la::MulOverwrite(temp_vec2, sigma_y_inv, &temp_vec1);
+    result += la::Dot(temp_vec1, y.mu());
+
+    la::MulOverwrite(y.mu(), A, &temp_vec1);
+    result -= lambda * la::Dot(temp_vec1, x.mu());
+
+    la::MulOverwrite(y.mu(), A, &temp_vec1);
+    la::MulOverwrite(temp_vec1, sigma_y_inv, &temp_vec2);
+    result -= la::Dot(temp_vec2, y.mu());
+
+    la::MulOverwrite(y.mu(), A, &temp_vec1);
+    la::MulOverwrite(temp_vec1, sigma_x, &temp_vec2);
+    la::MulOverwrite(temp_vec2, sigma_y_inv, &temp_vec1);
+    result -= lambda * la::Dot(temp_vec1, y.mu());
+    
+    la::MulOverwrite(x.mu(), alpha, &temp_vec1);
+    result += lambda * lambda * la::Dot(temp_vec1, x.mu());
+    la::MulOverwrite(y.mu(), sigma_y_inv, &temp_vec1);
+    result -= la::Dot(temp_vec1, y.mu());
+    result -= lambda * la::Dot(x.mu(), x.mu());
+
+    double second_term = exp(result);
+
+    val =
+      first_term * second_term
+      / (x_norm_constant * y_norm_constant);
     
     return val;
   }
