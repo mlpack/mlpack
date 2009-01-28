e49b69bbbcfb0f666a5c5056942bb4aeb0b55d33
Dongryeol Lee <drselee@gmail.com>
2009-01-28 16:56:01 +0000

    Templatized the computation part.

--- fastlib2/contrib/dongryel/fast_multipole_method/multibody_potential_main.cc
+++ fastlib2/contrib/dongryel/fast_multipole_method/multibody_potential_main.cc
@@ -3,6 +3,54 @@
 #include "../multitree_template/multitree_dfs.h"
 #include "../nested_summation_template/strata.h"
 
+class MultibodyPotentialComputer {
+
+ public:
+
+  template<typename TKernel>
+  static void Compute(Matrix &references, struct datanode *multibody_module) {
+
+    // Instantiate a multi-tree problem...
+    ArrayList<const Matrix *> sets;
+    sets.Init(MultibodyPotentialProblem<TKernel>::order);
+    for(index_t i = 0; 
+	i < MultibodyPotentialProblem<TKernel>::order; i++) {
+      sets[i] = &references;
+    }
+    
+    MultiTreeDepthFirst< MultibodyPotentialProblem<TKernel > > 
+      algorithm;
+    typename MultibodyPotentialProblem<TKernel>::MultiTreeQueryResult 
+      results;
+    typename MultibodyPotentialProblem<TKernel>::MultiTreeQueryResult 
+      naive_results;
+    algorithm.InitMonoChromatic(sets, (const ArrayList<const Matrix *> *) NULL,
+				multibody_module);
+    
+    fx_timer_start(fx_root, "multitree");
+    algorithm.Compute(NULL, &results);
+    fx_timer_stop(fx_root, "multitree");
+    
+    results.PrintDebug("potentials.txt");
+    printf("Got %d finite difference prunes...\n",
+	   results.num_finite_difference_prunes);
+    
+    fx_timer_start(fx_root, "naive_code");
+    algorithm.NaiveCompute((const ArrayList<const Matrix *> *) NULL,
+			   &naive_results);
+    fx_timer_stop(fx_root, "naive_code");
+    naive_results.PrintDebug("naive_potentials.txt");
+    double max_relative_error, positive_max_relative_error,
+      negative_max_relative_error;
+    naive_results.MaximumRelativeError(results, &max_relative_error,
+				       &negative_max_relative_error,
+				       &positive_max_relative_error);
+    printf("Maximum relative error: %g\n", max_relative_error);
+    printf("Positive max relative error: %g\n", positive_max_relative_error);
+    printf("Negative max relative error: %g\n", negative_max_relative_error);
+  }
+};
+
 int main(int argc, char *argv[]) {
 
   // Initialize FastExec (parameter handling stuff).
@@ -26,44 +74,19 @@ int main(int argc, char *argv[]) {
   // data::Load inits a matrix with the contents of a .csv or .arff.
   data::Load(references_file_name, &references);
 
-  // Instantiate a multi-tree problem...
-  ArrayList<const Matrix *> sets;
-  sets.Init(MultibodyPotentialProblem<ThreeBodyGaussianKernel>::order);
-  for(index_t i = 0; 
-      i < MultibodyPotentialProblem<ThreeBodyGaussianKernel>::order; i++) {
-    sets[i] = &references;
+  // Axilrod-Teller...
+  if(!strcmp(fx_param_str(multibody_module, "kernel", "none"), 
+	     "axilrodteller")) {
+    MultibodyPotentialComputer::Compute<ATPotentialKernel>(references,
+							   multibody_module);
   }
 
-  MultiTreeDepthFirst< MultibodyPotentialProblem<ThreeBodyGaussianKernel > > 
-    algorithm;
-  MultibodyPotentialProblem<ThreeBodyGaussianKernel>::MultiTreeQueryResult 
-    results;
-  MultibodyPotentialProblem<ThreeBodyGaussianKernel>::MultiTreeQueryResult 
-    naive_results;
-  algorithm.InitMonoChromatic(sets, (const ArrayList<const Matrix *> *) NULL,
-			      multibody_module);
-  
-  fx_timer_start(fx_root, "multitree");
-  algorithm.Compute(NULL, &results);
-  fx_timer_stop(fx_root, "multitree");
-  
-  results.PrintDebug("potentials.txt");
-  printf("Got %d finite difference prunes...\n",
-	 results.num_finite_difference_prunes);
-  
-  fx_timer_start(fx_root, "naive_code");
-  algorithm.NaiveCompute((const ArrayList<const Matrix *> *) NULL,
-			 &naive_results);
-  fx_timer_stop(fx_root, "naive_code");
-  naive_results.PrintDebug("naive_potentials.txt");
-  double max_relative_error, positive_max_relative_error,
-    negative_max_relative_error;
-  naive_results.MaximumRelativeError(results, &max_relative_error,
-				     &negative_max_relative_error,
-				     &positive_max_relative_error);
-  printf("Maximum relative error: %g\n", max_relative_error);
-  printf("Positive max relative error: %g\n", positive_max_relative_error);
-  printf("Negative max relative error: %g\n", negative_max_relative_error);
+  // Three-body Gaussian kernel...
+  else if(!strcmp(fx_param_str(multibody_module, "kernel", "none"),
+		  "threebodygaussian")) {
+    MultibodyPotentialComputer::Compute<ThreeBodyGaussianKernel>
+      (references, multibody_module);
+  }
 
   fx_done(fx_root);
   return 0;
