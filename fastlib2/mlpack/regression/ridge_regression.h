582ed64885e7512893adead43a9bbbb71c56afad
vasiloglou <nvasil@ismion.com>
2009-02-15 04:25:28 +0000

    added regression and ridge_regression but I haven't worked that much yet

---
+++ fastlib2/mlpack/regression/ridge_regression.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ * 
+ *       Filename:  ridge_regression.h
+ * 
+ *    Description:  
+ * 
+ *        Version:  1.0
+ *        Created:  02/14/2009 11:23:51 PM EST
+ *       Revision:  none
+ *       Compiler:  gcc
+ * 
+ *         Author:  Nikolaos Vasiloglou (NV), nvasil@ieee.org
+ *        Company:  Georgia Tech Fastlab-ESP Lab
+ * 
+ * =====================================================================================
+ */
+
+
