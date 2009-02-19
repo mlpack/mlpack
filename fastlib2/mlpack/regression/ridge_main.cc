b0fc3d0b60f131ffa6d6539879cbe2123a168680
Dongryeol Lee <drselee@gmail.com>
2009-02-19 19:27:50 +0000

    Some more bugs shaken out in ComputeSquareError function.

--- fastlib2/mlpack/regression/ridge_main.cc
+++ fastlib2/mlpack/regression/ridge_main.cc
@@ -20,23 +20,34 @@
 #include "ridge_regression.h"
 
 int main(int argc, char *argv[]) {
-  fx_module *module=fx_init(argc, argv, NULL);
-  std::string predictors_file = fx_param_str_req(module,"predictors"); 
+
+  fx_module *module = fx_init(argc, argv, NULL);
+  double lambda = fx_param_double(module, "lambda", 0.0);
+  std::string predictors_file = fx_param_str_req(module, "predictors"); 
   std::string predictions_file = fx_param_str_req(module, "predictions");
+
   Matrix predictors;
   if (data::Load(predictors_file.c_str(), &predictors)==SUCCESS_FAIL) {
     FATAL("Unable to open file %s", predictors_file.c_str());
   }
+
   Matrix predictions;
   if (data::Load(predictions_file.c_str(), &predictions)==SUCCESS_FAIL) {
     FATAL("Unable to open file %s", predictions_file.c_str());
   }
+
   RidgeRegression engine;
   engine.Init(module, predictors, predictions);
   NOTIFY("Computing Regression...");
-  engine.Regress(0);
+
+  if(fx_param_exists(module, "normal_equation_method")) {  
+    engine.Regress(lambda);
+  }
+  else {
+    engine.SVDRegress(lambda);
+  }
   NOTIFY("Regression Complete !");
-  double square_error =  engine.ComputeSquareError();
+  double square_error = engine.ComputeSquareError();
   NOTIFY("Squre Error:%g", square_error);
   fx_result_double(module, "square error", square_error);
   Matrix factors;
@@ -45,4 +56,5 @@ int main(int argc, char *argv[]) {
   NOTIFY("Saving factors...");
   data::Save(factors_file.c_str(), factors);  
   fx_done(module);
+  return 0;
 }
