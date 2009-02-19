32680a6bcd97c2f751de65986532c1e323f08cec
tqlong <tqlong@9d5b8971-822b-0410-80eb-d18c1038ef23>
2009-02-19 23:49:02 +0000

    graph

--- fastlib2/contrib/tqlong/graph/graph.h
+++ fastlib2/contrib/tqlong/graph/graph.h
@@ -1,4 +1,3 @@
-
 #ifndef __GRAPH_H
 #define __GRAPH_H
 #include "fastlib/fastlib.h"
@@ -11,6 +10,7 @@ class Graph {
  public:
   bool isEdge(index_t i, index_t j) const { return adjacent.get(i, j); }
   double getW(index_t i, index_t j) const { return weight.get(i, j); }
+  const Matrix& getW() const { return weight; }
 
   bool& refEdge(index_t i, index_t j) { return adjacent.ref(i, j); }
   double& refW(index_t i, index_t j) { return weight.ref(i, j); }
@@ -46,9 +46,9 @@ void BreadthFirstSearch(index_t s, index_t t, const Graph& g, Path* p) {
     int v = q.front(); q.pop();
     for (index_t u = 0; u < g.n_nodes(); u++)
       if (g.isEdge(u, v) && !visited[u]) {
-	q.push(u); 
-	previous[u] = v;
-	visited[u] = true;
+	      q.push(u); 
+	      previous[u] = v;
+	      visited[u] = true;
       }
   }
   p->Init();
@@ -61,17 +61,68 @@ void BreadthFirstSearch(index_t s, index_t t, const Graph& g, Path* p) {
 }
 
 template <class Graph>
-class MaxFlowAumentedGraph {
-  const Graph* g,
+class MaxFlowAugmentedGraph {
+  const Graph& g;
+  Matrix c;
+  Matrix f;
  public:
-  MaxFlowAumentedGraph()
+  MaxFlowAugmentedGraph(const Graph& g_, const Matrix& c_, Matrix& f_)
+    : g(g_) {
+    c.Alias(c_);
+    f.Alias(f_);
+  }
+  bool isEdge(index_t i, index_t j) const {
+    return (g.isEdge(i, j) && f.get(i, j) < c.get(i, j)) ||
+      (g.isEdge(j, i) && f.get(j, i) > 0);
+  }
+  
+  index_t n_nodes() const { return g.n_nodes(); }
+  
+  void ComputeMaxFlow(index_t s, index_t t) {
+    while (1) {
+      Path p;
+      BreadthFirstSearch(s, t, *this, &p);
+      //ot::Print(p);
+      if (p.size() == 0) break;
+      double val = CalAugmentValue(p);
+      AugmentPath(p, val);
+    }
+  }
+ private:
+  double CalAugmentValue(const Path& p) {
+    printf("Augment path\n");
+    double augmentValue = INFINITY;
+    for (index_t k = 0; k < p.size()-1; k++) {
+      index_t i = p[k], j = p[k+1];
+      double val;
+      if (g.isEdge(i, j) && f.get(i, j) < c.get(i, j)) // forward
+        val = c.get(i, j) - f.get(i, j);
+      else // backward
+        val = f.get(j, i);
+      printf("%d %d --> %f\n", i, j, val);
+      if (val < augmentValue) augmentValue = val;
+    }
+    return augmentValue;
+  }
+  
+  void AugmentPath(const Path& p, double val) {
+    for (index_t k = 0; k < p.size()-1; k++) {
+      index_t i = p[k], j = p[k+1];
+      if (g.isEdge(i, j) && f.get(i, j) < c.get(i, j)) // forward
+        f.ref(i, j) += val;
+      else // backward
+        f.ref(j, i) -= val;
+    }
+  }
 };
 
-// weight as capacity
+// Max flow from a correctly initialized flow (e.g. the zero flow)
+// Need isEdge() and n_nodes() function for class Graph
 template <class Graph>
-void MaxFlow(index_t s, index_t t, const Graph& g, Matrix* f) {
-  MaxFlowAugmentedGraph<Graph> ag(g, f);
-  
+void MaxFlow(index_t s, index_t t, const Graph& g, 
+  const Matrix& c, Matrix* f) {
+  MaxFlowAugmentedGraph<Graph> ag(g, c, *f);
+  ag.ComputeMaxFlow(s, t);
 }
 
 #endif
--- fastlib2/contrib/tqlong/graph/maxflow.cc
+++ fastlib2/contrib/tqlong/graph/maxflow.cc
@@ -1,8 +1,10 @@
 #include "graph.h"
 
 const fx_entry_doc maxflow_main_entries[] = {
-  {"file", FX_REQUIRED, FX_STR, NULL,
+  {"gcapa", FX_REQUIRED, FX_STR, NULL,
    "  File consists of the edges' capacity.\n"},
+  {"gflow", FX_REQUIRED, FX_STR, NULL,
+   "  Output file for the edges' flow.\n"},
   FX_ENTRY_DOC_DONE
 };
 
@@ -17,14 +19,24 @@ const fx_module_doc maxflow_main_doc = {
 
 int main(int argc, char* argv[]) {
   fx_init(argc, argv, &maxflow_main_doc );
-  const char *fn = fx_param_str_req(fx_root, "file");
-
+  const char *fn = fx_param_str_req(fx_root, "gcapa");
+  const char *fout = fx_param_str_req(fx_root, "gflow");
+  
   Graph g;
   g.InitFromFile(fn);
 
-  Path p;
-  BreadthFirstSearch(0, 3, g, &p);
-  ot::Print(p);
+  //Path p;
+  //BreadthFirstSearch(0, 3, g, &p);
+  //ot::Print(p);
+  
+  Matrix f;
+  f.Init(g.n_nodes(), g.n_nodes());
+  f.SetZero();
+  MaxFlow(0, 3, g, g.getW(), &f);
+  la::TransposeSquare(&f);
+  
+  data::Save(fout, f);
+  //ot::Print(f);
 
   fx_done(fx_root);
 }
