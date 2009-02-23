22c7be8ff268d7cc123d4806bd35e4959bf2137e
Dongryeol Lee <drselee@gmail.com>
2009-02-23 17:27:30 +0000

    The basic options added to the driver, but need to debug more.

--- fastlib2/mlpack/regression/ridge_main.cc
+++ fastlib2/mlpack/regression/ridge_main.cc
@@ -1,5 +1,5 @@
 /*
- * =====================================================================================
+ * ============================================================================
  *
  *       Filename:  ridge_main.cc
  *
@@ -13,21 +13,31 @@
  *         Author:  Nikolaos Vasiloglou (NV), nvasil@ieee.org
  *        Company:  
  *
- * =====================================================================================
+ * ============================================================================
  */
 #include <string>
 #include "fastlib/fastlib.h"
 #include "ridge_regression.h"
+#include "ridge_regression_util.h"
 
 int main(int argc, char *argv[]) {
 
   fx_module *module = fx_init(argc, argv, NULL);
-  double lambda = fx_param_double(module, "lambda", 0.0);
+  double lambda_min = fx_param_double(module, "lambda_min", 0.0);
+  double lambda_max = fx_param_double(module, "lambda_max", 0.0);
+  int num_lambdas_to_cv = fx_param_int(module, "num_lambdas", 1);
+  const char *mode = fx_param_str(module, "mode", "regress");  
+  if(lambda_min == lambda_max) {
+    num_lambdas_to_cv = 1;
+    if(!strcmp(mode, "crossvalidate")) {
+      fx_set_param_str(module, "mode", "regress");
+      mode = fx_param_str(module, "mode", "regress");
+    }
+  }
+
+  // Read the dataset and its labels.
   std::string predictors_file = fx_param_str_req(module, "predictors"); 
   std::string predictions_file = fx_param_str_req(module, "predictions");
-
-  // Initialize the random number seed.
-  srand(time(NULL));
 
   Matrix predictors;
   if (data::Load(predictors_file.c_str(), &predictors)==SUCCESS_FAIL) {
@@ -40,20 +50,82 @@ int main(int argc, char *argv[]) {
   }
 
   RidgeRegression engine;
-  engine.Init(module, predictors, predictions);
   NOTIFY("Computing Regression...");
 
   const char *method = fx_param_str(module, "method", "quicsvd");
-  if(!strcmp(method, "normal")) {  
-    engine.Regress(lambda);
+  
+  if(!strcmp(mode, "regress")) {
+
+    engine.Init(module, predictors, predictions);
+
+    if(!strcmp(method, "normal")) {  
+      engine.Regress(lambda_min);
+    }
+    else if(!strcmp(method, "quicsvd")) {
+      engine.QuicSVDRegress(lambda_min, 0.1);
+    }
+    else {
+      engine.SVDRegress(lambda_min);
+    }
   }
-  else if(!strcmp(method, "quicsvd")) {
-    engine.QuicSVDRegress(lambda, 0.1);
+  else if(!strcmp(mode, "crossvalidate")) {
+    NOTIFY("Crossvalidating for the optimal lambda in [ %g %g ] by trying %d values...",
+	   lambda_min, lambda_max, num_lambdas_to_cv);
+    engine.Init(module, predictors, predictions);
+    engine.CrossValidatedRegression(lambda_min, lambda_max, num_lambdas_to_cv);
   }
-  else {
-    engine.SVDRegress(lambda);
+  else if(!strcmp(mode, "featureselection")) {
+
+    Matrix predictor_indices_intermediate;
+    Matrix prune_predictor_indices_intermediate;
+    std::string predictor_indices_file = fx_param_str_req(module, 
+							  "predictor_indices");
+    std::string prune_predictor_indices_file = 
+      fx_param_str_req(module, "prune_predictor_indices");
+    if(data::Load(predictor_indices_file.c_str(),
+		  &predictor_indices_intermediate) == SUCCESS_FAIL) {
+      FATAL("Unable to open file %s", predictor_indices_file.c_str());
+    }
+    if(data::Load(prune_predictor_indices_file.c_str(),
+		  &prune_predictor_indices_intermediate) == SUCCESS_FAIL) {
+      FATAL("Unable to open file %s", prune_predictor_indices_file.c_str());
+    }
+
+    GenVector<index_t> predictor_indices;
+    GenVector<index_t> prune_predictor_indices;
+    predictor_indices.Init(predictor_indices_intermediate.n_cols());
+    prune_predictor_indices.Init
+      (prune_predictor_indices_intermediate.n_cols());
+    
+    // This is a pretty retarded way of copying from a double-matrix
+    // to an int vector. This can be simplified only if there were a
+    // way to read integer-based dataset directly without typecasting.
+    for(index_t i = 0; i < predictor_indices_intermediate.n_cols(); i++) {
+      predictor_indices[i] = 
+	(index_t) predictor_indices_intermediate.get(0, i);
+      prune_predictor_indices[i] = (index_t)
+	prune_predictor_indices_intermediate.get(0, i);
+    }
+    
+    // Run the feature selection.
+    GenVector<index_t> output_predictor_indices;
+    RidgeRegressionUtil::FeatureSelection(module, predictors, 
+					  predictor_indices,
+					  prune_predictor_indices,
+					  &output_predictor_indices);
+    engine.Init(module, predictors, output_predictor_indices, predictions);
+    if(!strcmp(method, "normal")) {  
+      engine.Regress(lambda_min);
+    }
+    else if(!strcmp(method, "quicsvd")) {
+      engine.QuicSVDRegress(lambda_min, 0.1);
+    }
+    else {
+      engine.SVDRegress(lambda_min);
+    }
   }
-  NOTIFY("Regression Complete !");
+
+  NOTIFY("Ridge Regression Model Training Complete!");
   double square_error = engine.ComputeSquareError();
   NOTIFY("Square Error:%g", square_error);
   fx_result_double(module, "square error", square_error);
@@ -61,7 +133,8 @@ int main(int argc, char *argv[]) {
   engine.factors(&factors);
   std::string factors_file = fx_param_str(module, "factors", "factors.csv");
   NOTIFY("Saving factors...");
-  data::Save(factors_file.c_str(), factors);  
+  data::Save(factors_file.c_str(), factors);
+
   fx_done(module);
   return 0;
 }
