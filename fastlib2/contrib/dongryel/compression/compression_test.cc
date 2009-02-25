dcca3d16321aeb62ac952a7635e323d22ec827e7
Dongryeol Lee <drselee@gmail.com>
2009-02-25 23:14:27 +0000

    Initial checkin of the compressed vector experiments.

---
+++ fastlib2/contrib/dongryel/compression/compression_test.cc
@@ -0,0 +1,17 @@
+#include "fastlib/fastlib.h"
+#include "compressed_matrix.h"
+
+int main(int argc, char *argv[]) {
+  fx_init(argc, argv, NULL);
+
+  CompressedVector<double> cv;
+  Vector v;
+  index_t length_of_vector = 3000;
+  v.Init(length_of_vector);
+  for(index_t i = 0; i < length_of_vector; i++) {
+    v[i] = i;
+  }
+  cv.Init(v);
+  fx_done(fx_root);
+  return 0;
+}
