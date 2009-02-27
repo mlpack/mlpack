222158c1be16e7fc828533f9fffce2fd48ccba7a
gmravi2003 <gmravi2003@gatech.edu>
2009-02-27 23:01:56 +0000

    fsdfsdf

--- fastlib2/contrib/gmravi/snare_spam/findknn.cc
+++
@@ -1,9 +0,0 @@
-#include "fastlib/fastlib.h"
-#include "mlpack/allknn/allknn.h"
-#include "findknn.h"
-int main(int argc, char *argv[]){
-  fx_init(argc,argv,NULL);
-  FindAllkNN find_all_knn;
-  find_all_knn.Init();
-  fx_done(NULL);
-}
