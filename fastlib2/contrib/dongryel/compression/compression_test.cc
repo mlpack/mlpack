22830dea6228b2bafcbd5aaa7ba99c348aeada4a
Dongryeol Lee <drselee@gmail.com>
2009-02-27 00:54:40 +0000

    Need to implement method for incrementing the vector.

--- fastlib2/contrib/dongryel/compression/compression_test.cc
+++ fastlib2/contrib/dongryel/compression/compression_test.cc
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[]) {
   fx_init(argc, argv, NULL);
 
-  CompressedVector<double, 10000> cv;
+  CompressedVector<double, 100000000> cv;
   Vector v;
-  index_t length_of_vector = 3000000;
+  index_t length_of_vector = 300000000;
   v.Init(length_of_vector);
   for(index_t i = 0; i < length_of_vector; i++) {
     v[i] = i;
@@ -15,6 +15,16 @@ int main(int argc, char *argv[]) {
   // Copy the vector to the compressed one.
   cv.Copy(v);
 
+  v.Destruct();
+  // Measure the time for the uncompressed vector.
+  //fx_timer_start(fx_root, "uncompressed_vector_dot_product");
+  //dot_product = 0;
+  //for(index_t i = 0; i < length_of_vector; i +=1000) {
+  //dot_product += math::Sqr(v[i]);
+  //}
+  //fx_timer_stop(fx_root, "uncompressed_vector_dot_product");
+  //printf("Dot product: %g\n", dot_product);
+
   // Measure the time for the compressed vector.
   fx_timer_start(fx_root, "compressed_vector_dot_product");
   double dot_product = 0;
@@ -22,15 +32,6 @@ int main(int argc, char *argv[]) {
     dot_product += math::Sqr(cv[i]);
   }
   fx_timer_stop(fx_root, "compressed_vector_dot_product");
-  printf("Dot product: %g\n", dot_product);
-
-  // Measure the time for the uncompressed vector.
-  fx_timer_start(fx_root, "uncompressed_vector_dot_product");
-  dot_product = 0;
-  for(index_t i = 0; i < length_of_vector; i +=1000) {
-    dot_product += math::Sqr(v[i]);
-  }
-  fx_timer_stop(fx_root, "uncompressed_vector_dot_product");
   printf("Dot product: %g\n", dot_product);
 
   fx_done(fx_root);
