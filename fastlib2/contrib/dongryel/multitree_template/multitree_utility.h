139a6e15d0695a215ff840322e304f70447be6d7
Dongryeol Lee <drselee@gmail.com>
2009-03-05 19:29:32 +0000

    Added the random combination code from Knuth's book.

--- fastlib2/contrib/dongryel/multitree_template/multitree_utility.h
+++ fastlib2/contrib/dongryel/multitree_template/multitree_utility.h
@@ -12,6 +12,50 @@ class MultiTreeUtility {
 
  public:
 
+  /** @brief Implements Knuth's random combination selector algorithm.
+   */
+  static void RandomCombination(index_t begin_inclusive, 
+				index_t end_exclusive, index_t num_times, 
+				index_t *output) {
+    
+    index_t t = 0, m = 0;
+    index_t range = end_exclusive - begin_inclusive;
+    
+    do {
+      double u = math::Random();
+      if((range - t) * u >= num_times - m) {
+	t++;
+	continue;
+      }
+
+      output[m] = begin_inclusive + t;
+      m++;
+      t++;
+      if(m >= num_times) {
+	break;
+      }
+    } while(1);
+  }
+
+  template<typename Tree>
+  static void RandomTuple(const ArrayList<Tree *> &nodes,
+			  ArrayList<index_t> &random_permutation) {
+
+    for(index_t block_pointer = 0; block_pointer < nodes.size(); ) {
+      
+      // Figure out how many blocks of nodes are equal.
+      index_t saved_block_pointer = block_pointer;
+      do {
+	block_pointer++;
+      } while(nodes[block_pointer] == nodes[saved_block_pointer]);
+      
+      RandomCombination(nodes[saved_block_pointer]->begin(),
+			nodes[saved_block_pointer]->end(),
+			saved_block_pointer - block_pointer,
+			random_permutation.begin() + saved_block_pointer);
+    }
+  }
+
   static void ShuffleAccordingToQueryPermutation
   (Matrix &v, const ArrayList<index_t> &permutation) {
     
