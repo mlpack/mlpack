e18b9d3d4a31eec437dcfcb3e517facb85a530b0
vasiloglou <nvasil@ismion.com>
2009-03-11 19:05:32 +0000

    I did something wrong

--- fastlib3/base/contrib/march/fock_matrix/fock_impl/build.py
+++
@@ -1,7 +0,0 @@
-librule(
-	name="eri",
-	headers=["eri.h", "shell_pair.h", "basis_shell.h", "basis_function.h"],
-	sources=["eri.cc", "shell_pair.cc"],
-	deplibs=["fastlib:fastlib"]
-)
-
--- fastlib3/base/contrib/march/fock_matrix/fock_impl/eri_test.cc
+++
@@ -1,7 +0,0 @@
-#include "eri.h"
-
-int main(int argc, char* argv[]) {
-
-
-
-} // main()
