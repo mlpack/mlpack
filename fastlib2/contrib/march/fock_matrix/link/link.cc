5af14883fbc5461a4ae50d5d11bb111db47e6204
Bill March <march@ices.utexas.edu>
2009-04-03 17:35:31 +0000

    fixed num pruned shell pairs

--- fastlib2/contrib/march/fock_matrix/link/link.cc
+++ fastlib2/contrib/march/fock_matrix/link/link.cc
@@ -28,7 +28,7 @@ void Link::PrescreeningLoop_() {
                                             
   fx_result_int(module_, "num_shell_pairs", num_shell_pairs_);
   fx_result_int(module_, "num_shell_pairs_screened", 
-                num_shells_ * (num_shells_ - 1) / 2);
+                num_shell_pairs_ - (num_shells_ * (num_shells_ - 1) / 2) - num_shells_);
   
   //// Find significant \nu for all \mu
   
