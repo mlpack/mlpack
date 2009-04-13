b34f59ba13a19ef03fb7a449752d0df0e742edfd
Bill March <march@ices.utexas.edu>
2009-04-13 19:50:57 +0000

    fixed weighted average of bounding boxes

--- fastlib2/fastlib/tree/bounds.h
+++ fastlib2/fastlib/tree/bounds.h
@@ -102,12 +102,52 @@ class DHrectBound {
       DRange range1 = box1.get(i);
       DRange range2 = box2.get(i);
       
-      double min_x = alpha1_min * range1.lo + alpha2_min * range2.lo;
-      min_x = min_x/(alpha1_max + alpha2_max);
+      // account for negative and positive ranges
+      double min_x = 0.0;
+      if (range1.lo >= 0.0) {
+        min_x += alpha1_min * range1.lo; 
+      }
+      else {
+        min_x += alpha1_max * range1.lo;
+      }
+      
+      if (range2.lo >= 0.0) {
+        min_x += alpha2_min * range2.lo; 
+      }
+      else {
+        min_x += alpha2_max * range2.lo;
+      }
 
-      double max_x = alpha1_max * range1.hi + alpha2_max * range2.hi;
-      max_x = max_x/(alpha1_min + alpha2_min);
-
+      if (min_x >= 0.0) {
+        min_x = min_x/(alpha1_max + alpha2_max);
+      }
+      else {
+        min_x = min_x/(alpha1_min + alpha2_min);
+      }
+      
+      
+      double max_x = 0.0;
+      if (range1.hi >= 0.0) {
+        max_x += alpha1_max * range1.hi;
+      }
+      else {
+        max_x += alpha1_min * range1.hi;
+      }
+      
+      if (range2.hi >= 0.0) {
+        max_x += alpha2_max * range2.hi;
+      }
+      else {
+        max_x += alpha2_min * range2.hi;
+      }
+      
+      if (max_x >= 0.0) {
+        max_x = max_x/(alpha1_min + alpha2_min);
+      }
+      else {
+        max_x = max_x/(alpha1_max + alpha2_max);
+      }
+      
       DRange out_range;
       out_range.Init(min_x, max_x);
       
