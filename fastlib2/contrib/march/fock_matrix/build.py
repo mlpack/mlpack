7d7e27433ce4f5188ebd0bdb10de75a9b6d81f2e
Bill March <march@ices.utexas.edu>
2009-06-17 15:32:29 +0000

    added qchem density reading

--- fastlib2/contrib/march/fock_matrix/build.py
+++ fastlib2/contrib/march/fock_matrix/build.py
@@ -16,5 +16,5 @@ binrule(
 	name="fock_matrix_main",
 	headers=["cfmm/cfmm_coulomb.h"],
 	sources=["fock_matrix_main.cc"],
-	deplibs=["fastlib:fastlib", "contrib/march/fock_matrix/prescreening:schwartz_prescreening", "contrib/march/fock_matrix/cfmm:cfmm_coulomb", "contrib/march/fock_matrix/naive:naive_fock_matrix", "contrib/march/fock_matrix/multi_tree:multi_tree_fock", "contrib/march/fock_matrix/link:link_exchange", ":fock_matrix_comparison"],
+	deplibs=["fastlib:fastlib", "contrib/march/fock_matrix/prescreening:schwartz_prescreening", "contrib/march/fock_matrix/cfmm:cfmm_coulomb", "contrib/march/fock_matrix/naive:naive_fock_matrix", "contrib/march/fock_matrix/multi_tree:multi_tree_fock", "contrib/march/fock_matrix/link:link_exchange", ":fock_matrix_comparison", "contrib/march/fock_matrix/chem_reader:chem_reader"],
 )
