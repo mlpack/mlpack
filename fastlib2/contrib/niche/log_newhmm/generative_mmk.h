f0f5b1ed80edf1f307fae99f3cb44e1709803607
niche <niche@9d5b8971-822b-0410-80eb-d18c1038ef23>
2009-11-06 02:38:05 +0000

    updates to ppk

--- fastlib2/contrib/niche/log_newhmm/generative_mmk.h
+++ fastlib2/contrib/niche/log_newhmm/generative_mmk.h
@@ -12,6 +12,10 @@
 
 #define INSIDE_GENERATIVE_MMK_IMPL_H
 
+double PPKLog(double rho,
+	      const Multinomial &x,
+	      const Multinomial &y);
+
 double GenerativeMMK(double lambda,
 		     const Multinomial &x,
 		     const Multinomial &y);
@@ -24,10 +28,28 @@ double GenerativeMMK(double lambda,
 		     const DiagGaussian &x,
 		     const DiagGaussian &y);
 
+double GenerativeMMKLog(double lambda,
+			const DiagGaussian &x,
+			const DiagGaussian &y);
+
+double PPKLog(double rho,
+	      const DiagGaussian &x,
+	      const DiagGaussian &y);
+
 template <typename TDistribution>
 double GenerativeMMK(double lambda, int n_T,
 		     const HMM<TDistribution> &hmm_a,
 		     const HMM<TDistribution> &hmm_b);
+
+template <typename TDistribution>
+double GenerativeMMKLog(double lambda, int n_T,
+		     const HMM<TDistribution> &hmm_a,
+			const HMM<TDistribution> &hmm_b);
+
+template <typename TDistribution>
+double PPKLog(double rho, int n_T,
+	      const HMM<TDistribution> &hmm_a,
+	      const HMM<TDistribution> &hmm_b);
 
 double GenerativeMMK(double lambda, double rho, int n_T,
 		     const HMM<Multinomial> &hmm_a,
@@ -38,21 +60,31 @@ void GenerativeMMKBatch(double lambda, int n_T,
 			const ArrayList<HMM<TDistribution> > &hmms,
 			Matrix *p_kernel_matrix);
 
+template <typename TDistribution>
+void PPKBatchLog(double rho, int n_T,
+		 const ArrayList<HMM<TDistribution> > &hmms,
+		 Matrix *p_kernel_matrix_log);
+
+template <typename TDistribution>
+void GenerativeMMKBatchLog(double lambda, int n_T,
+			   const ArrayList<HMM<TDistribution> > &hmms,
+			   Matrix *p_kernel_matrix_log);
+
 void GenerativeMMKBatch(double lambda, double rho, int n_T,
 			const ArrayList<HMM<Multinomial> > &hmms,
 			Matrix *p_kernel_matrix);
 
 double KDEGenerativeMMK(double lambda,
 			const Matrix &samples1,
-			const Matrix &samples2);
+			const Matrix &samples2,
+			double bandwidth1,
+			double bandwidth2);
 
 void KDEGenerativeMMKBatch(double lambda,
 			   const ArrayList<Matrix> &samplings,
 			   Matrix *p_kernel_matrix);
 
 void ScaleSamplingsToCube(ArrayList<Matrix> *p_samplings);
-
-
 
 #include "generative_mmk_impl.h"
 #undef INSIDE_GENERATIVE_MMK_IMPL_H
