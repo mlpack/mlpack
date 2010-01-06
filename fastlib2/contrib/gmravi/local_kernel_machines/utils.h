231d8384ec36c0d8d3448d17272b3889e0c826ab
gmravi2003 <gmravi2003@gatech.edu>
2010-01-06 02:34:44 +0000

    working version 3.0

---
+++ fastlib2/contrib/gmravi/local_kernel_machines/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void RemoveLastRowFromMatrixInit(Matrix &, Vector &);
+void AppendVectorToMatrixAsLastRow(Vector &, Matrix &, Matrix &);
+void ConvertOneColumnMatrixToVector(Matrix &, Vector &);
+
+#endif
