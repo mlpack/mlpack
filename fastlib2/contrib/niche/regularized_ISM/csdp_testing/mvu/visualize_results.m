2fde9ed1b8cd82a4d7df19856d845419df29a9cb
niche <niche@9d5b8971-822b-0410-80eb-d18c1038ef23>
2010-01-30 22:03:00 +0000

    added .../contrib/niche/regularizedISM with mvu example of csdp

---
+++ fastlib2/contrib/niche/regularized_ISM/csdp_testing/mvu/visualize_results.m
@@ -0,0 +1,9 @@
+X = load('swiss1500.sol_X');
+X = X(:,3:end);
+sparse_K = spconvert(X);
+K = full(sparse_K);
+K = K + K' - diag(diag(K));
+[V,D] = eig(K);
+plot(diag(D));
+scatter(V(:,end-1), V(:,end));
+%scatter3(V(:,end-2), V(:,end-1), V(:,end));
