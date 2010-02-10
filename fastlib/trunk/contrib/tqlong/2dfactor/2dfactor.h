77b3e2369a3f70371088d8de0cd4daa03911faa0
tqlong <tqlong@9d5b8971-822b-0410-80eb-d18c1038ef23>
2010-02-10 17:51:23 +0000

    2dPCA project on major basis

--- fastlib/trunk/contrib/tqlong/2dfactor/2dfactor.h
+++ fastlib/trunk/contrib/tqlong/2dfactor/2dfactor.h
@@ -5,12 +5,22 @@
 
 namespace la {
 
-void row2dPCA(ArrayList<Matrix>& imageList, 
-			  Vector& eigenValues, Matrix& rowBasis_out, Matrix& mean_out);
+  void row2dPCA(const ArrayList<Matrix>& imageList, 
+		Vector& eigenValues, Matrix& rowBasis_out, Matrix& mean_out);
 
-void col2dPCA(ArrayList<Matrix>& imageList, 
-			  Vector& eigenValues, Matrix& colBasis_out, Matrix& mean_out);
-			  
+  void col2dPCA(const ArrayList<Matrix>& imageList, 
+		Vector& eigenValues, Matrix& colBasis_out, Matrix& mean_out);
+
+  void RowCol2dPCA(const ArrayList<Matrix>& imageList, 
+		   Vector& rowEigenValues, Matrix& rowBasis, 
+		   Vector& colEigenValues, Matrix& colBasis, Matrix& mean);
+  void Get2dBasisMajor(double rowPart, const Vector& rowEigenValues, 
+		       const Matrix& rowBasis, double colPart, 
+		       const Vector& colEigenValues, const Matrix& colBasis,
+		       Matrix& rowBasisMajor_out, Matrix& colBasisMajor_out);
+  void Project2dBasis(const Matrix& A, const Matrix& rowBasis, 
+		      const Matrix& colBasis, Matrix& A_out);
+
 }; // namespace
 
 #endif
