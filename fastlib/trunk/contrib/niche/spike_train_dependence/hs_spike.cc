013c8f435150aa1a39b3b66d4600235c1b52c8d0
niche <niche@9d5b8971-822b-0410-80eb-d18c1038ef23>
2010-02-11 05:15:34 +0000

    updates to spike train dependence

--- fastlib/trunk/contrib/niche/spike_train_dependence/hs_spike.cc
+++ fastlib/trunk/contrib/niche/spike_train_dependence/hs_spike.cc
@@ -22,13 +22,19 @@ int main(int argc, char* argv[]) {
 
   Matrix x1, y1, x2, y2;
   spikes_pair.ConstructPoints(&x1, &y1, &x2, &y2);
-
+  
+  printf("x reference\n");
   double hsic_x1_y1 = HSIC(x1, y1);
-  printf("x reference hsic(x, y) = %f\n", hsic_x1_y1);
+  printf("hsic(x, y) = %f\n", hsic_x1_y1);
 
+  printf("y reference\n");
   double hsic_x2_y2 = HSIC(x2, y2);
   printf("y reference hsic(x, y) = %f\n", hsic_x2_y2);
 
+
+
+  SpikeSeqPair permed_spikes_pair;
+  spikes_pair.CreatePermutation(&permed_spikes_pair);
 
   fx_done(root);
 
