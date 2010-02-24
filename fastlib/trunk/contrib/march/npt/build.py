8a930c59bf255a91e57781a042e6add04f4fc2e1
Bill March <march@ices.utexas.edu>
2010-02-24 21:39:36 +0000

    wrote base case and naive code to use it, few more problems with passing partial tuples around, tests to come

--- fastlib/trunk/contrib/march/npt/build.py
+++ fastlib/trunk/contrib/march/npt/build.py
@@ -11,3 +11,16 @@ binrule(
     headers = [],
     deplibs = ["fastlib:fastlib", ":naive_two_point"]
 )
+
+librule(
+    name = "n_point_alg",
+    sources = ["matcher.cc", "n_point.cc"],
+    headers = ["matcher.h", "n_point.h"],
+    deplibs = ["fastlib:fastlib"]
+)
+
+binrule(
+    name = "n_point_main",
+    sources = ["n_point_main.cc"],
+    deplibs = ["fastlib:fastlib", ":n_point_alg"]
+)
