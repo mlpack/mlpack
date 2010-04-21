630d88a0d5adba6a07426154ab29932247a429d8
Bill March <march@ices.utexas.edu>
2010-04-21 19:20:03 +0000

    working on multi-bandwidth version

--- fastlib/trunk/contrib/march/npt/multi_matcher.h
+++ fastlib/trunk/contrib/march/npt/multi_matcher.h
@@ -7,11 +7,32 @@
  *
  */
 
+#ifndef MULTI_MATCHER_H
+#define MULTI_MATCHER_H
 
-
+#include "n_point_results.h"
+#include "fastlib/fastlib.h"
 
 class MultiMatcher {
+  
+private:
+  
+    
+  ////////// variables /////////////
+  
+  ArrayList<double> ranges_;
+  
+  Permutations perms_;
+  
+public:
 
-  int TestHrectPair();
+  
+  
+  void TestNodes_(const DHrectBound<2>& box1, const DHrectBound<2>& box2,
+                  ResultsTensor& results);
   
 }; // class MultiMatcher
+
+
+#endif 
+
