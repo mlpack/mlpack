6397828c2d90a4da193ce9bce11bb9a1fe21bc1c
Ryan Curtin <ryan@ratml.org>
2010-04-28 14:45:00 +0000

    Add compatibility layer to convert between Matrix and arma::mat (this is NOT fast!  but it's only here for compatibility)

---
+++ fastlib/branches/fastlib-armadillo/fastlib/base/arma_compat.cc
@@ -0,0 +1,42 @@
+/***
+ * @file arma_compat.cc
+ *
+ * Compatibility layer for GenMatrix and arma::mat classes.
+ *
+ */
+
+#include "arma_compat.h"
+
+/**
+ * Turn the supplied arma::mat into a FASTLIB Matrix.
+ *
+ * This wasn't written to be fast or efficient.
+ */
+void arma_compat::armaToMatrix(const arma::mat &mat, Matrix &mg) {
+  // initialize our matrix correctly
+  mg.Init(mat.n_rows, mat.n_cols);
+
+  // copy elementwise.  this is slow as hell
+  for(int r = 0; r < mat.n_rows; r++) {
+    for(int c = 0; c < mat.n_cols; c++) {
+      mg.set(r, c, mat(r, c));
+    }
+  }
+}
+
+/**
+ * Turn the supplied FASTLIB Matrix into an arma::mat object.
+ *
+ * This wasn't written to be fast or efficient.
+ */
+void arma_compat::matrixToArma(const Matrix &gm, arma::mat &mat) {
+  // initialize our matrix to the correct size
+  mat.set_size(gm.n_rows(), gm.n_cols());
+
+  // copy elementwise.  this is slow as hell
+  for(int r = 0; r < gm.n_rows(); r++) {
+    for(int c = 0; c < gm.n_cols(); c++) {
+      mat(r, c) = gm.get(r, c);
+    }
+  }
+}
