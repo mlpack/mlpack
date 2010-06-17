89cdf2be1de6ddeb91f58fc20d39229de6c34f60
Ryan Curtin <ryan@ratml.org>
2010-06-17 00:26:05 +0000

    Merge fastlib-armadillo/fastlib/base into fastlib-stl/fastlib/base

---
+++ fastlib/branches/fastlib-stl/fastlib/base/arma_compat.h
@@ -0,0 +1,28 @@
+/***
+ * @file arma_compat.h
+ *
+ * Compatibility layer for GenMatrix and arma::mat classes.
+ * This is not written to be fast but to provide an intermediate compatibility
+ * layer while converting from GenMatrix to arma::matrix internally.
+ *
+ */
+
+#ifndef ARMA_COMPAT_H
+#define ARMA_COMPAT_H
+
+#include <armadillo>
+#include "../la/matrix.h"
+
+namespace arma_compat {
+
+  void armaToMatrix(const arma::mat &mat, Matrix &mg);
+  void matrixToArma(const Matrix &gm, arma::mat &mat);
+
+  // write a column into an uninitialized vector
+  void armaColVector(const arma::mat& mat, int col, Vector& v);
+
+  void vectorToVec(const Vector& gv, arma::vec& vec);
+  void vecToVector(const arma::vec& vec, Vector& gv);
+}
+
+#endif /* ARMA_COMPAT_H */
