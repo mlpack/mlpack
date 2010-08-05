ee07d8fb6b0bb19e7a9e9f03934ef42a8c7c0d01
Dongryeol Lee <drselee@gmail.com>
2010-08-05 18:56:10 +0000

    More progress.

--- fastlib/trunk/contrib/dongryel/multigrid/multigrid_dev.h
+++ fastlib/trunk/contrib/dongryel/multigrid/multigrid_dev.h
@@ -18,14 +18,53 @@ void Multigrid<MatrixType, VectorType>::Coarsen_(
   const MultigridLevel &level_in,
   MultigridLevel *coarsened_level_out) {
 
+  const double threshold = 0.2;
+
   // Make a copy of the fine nodes from the points owned by the
   // previous level.
-  std::vector<int> fine_point_indices = level_in.point_indices();
-  std::random_shuffle(fine_point_indices.begin(), fine_point_indices.end());
+  const std::vector<int> &fine_point_indices = level_in.point_indices();
+  std::vector<int> shuffle_indices(fine_point_indices.size());
+  for (int i = 0; i < shuffle_indices.size(); i++) {
+    shuffle_indices[i] = i;
+  }
+  std::random_shuffle(shuffle_indices.begin(), shuffle_indices.end());
+
+  // The generated coarse points.
+  std::vector< std::pair<int, int> > coarse_point_indices;
 
   for (int i = 0; i < fine_point_indices.size(); i++) {
 
-  }
+    // The index of the fine node point.
+    int fine_point_index = shuffle_indices[i];
+
+    // The associated label of the fine node point.
+    int fine_point_label = fine_point_indices[ fine_point_index ];
+
+    // Compute the sum of the affinities between the current fine node
+    // point and the existing set of coarse points.
+    double sum_coarse_affinities = 0;
+    for (int j = 0; j < coarse_point_indices.size(); j++) {
+
+      // The index of the coarse node point.
+      int coarse_point_index = coarse_point_indices[j].first;
+      sum_coarse_affinities +=
+        level_in.get(fine_point_index, coarse_point_index);
+    }
+
+    // Compute the sum of the affinities between the current fine node
+    // and all of the points.
+    double sum_all_affinities = 0;
+    for (int j = 0; j < fine_point_indices.size(); j++) {
+      sum_all_affinities += level_in.get(fine_point_index, j);
+    }
+
+    // Add to the coarse set if the following condition is satisfied.
+    if (sum_affinities < threshold * sum_all_affinities) {
+      coarse_point_indices.push_back(
+        std::pair<int, int>(fine_point_index, fine_point_label));
+    }
+  } // end of looping over all fine nodes.
+
 }
 
 template<typename MatrixType, typename VectorType>
