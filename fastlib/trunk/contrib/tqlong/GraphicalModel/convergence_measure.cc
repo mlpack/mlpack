3960d2eb4adad745d88ab7725b832eb2acf70a0f
tqlong <tqlong@9d5b8971-822b-0410-80eb-d18c1038ef23>
2010-08-17 00:27:29 +0000

    Graphical Model: Message priority

---
+++ fastlib/trunk/contrib/tqlong/GraphicalModel/convergence_measure.cc
@@ -0,0 +1,9 @@
+#include "gm.h"
+
+BEGIN_GRAPHICAL_MODEL_NAMESPACE;
+
+const int ConvergenceMeasure::Iter     = 1 << 0;
+const int ConvergenceMeasure::Change   = 1 << 1;
+
+END_GRAPHICAL_MODEL_NAMESPACE;
+
