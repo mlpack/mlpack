64281eb1fc9fea3c980a63f095bac93c84e9f3ec
Dongryeol Lee <drselee@gmail.com>
2010-08-19 18:11:13 +0000

    Still debugging.

--- fastlib/trunk/contrib/dongryel/multigrid/multigrid_test.cc
+++ fastlib/trunk/contrib/dongryel/multigrid/multigrid_test.cc
@@ -16,23 +16,23 @@ class MultigridTest {
       Matrix *left_hand_side_out,
       Vector *right_hand_side_out) {
 
-      int num_dimensions = math::RandInt(10, 70);
+      int num_dimensions = math::RandInt(100, 200);
       left_hand_side_out->Init(num_dimensions, num_dimensions);
       right_hand_side_out->Init(num_dimensions);
-      
-      for(int j = 0; j < left_hand_side_out->n_cols(); j++) {
-	for(int i = 0; i < left_hand_side_out->n_rows(); i++) {
-	  if(i == j) {
-	    left_hand_side_out->set(i, j, 4.0);
-	  }
-	  else if(i == j + 1 || i + 1 == j) {
-	    left_hand_side_out->set(i, j, -1.0);
-	  }
-	  else {
-	    left_hand_side_out->set(i, j, 0.0);
-	  }
-	}
-	(*right_hand_side_out)[j] = math::Random(1.5, 4.0);
+
+      for (int j = 0; j < left_hand_side_out->n_cols(); j++) {
+        for (int i = 0; i < left_hand_side_out->n_rows(); i++) {
+          if (i == j) {
+            left_hand_side_out->set(i, j, 4.0);
+          }
+          else if (i == j + 1 || i + 1 == j) {
+            left_hand_side_out->set(i, j, -1.0);
+          }
+          else {
+            left_hand_side_out->set(i, j, 0.0);
+          }
+        }
+        (*right_hand_side_out)[j] = math::Random(1.5, 4.0);
       }
     }
 
@@ -45,13 +45,17 @@ class MultigridTest {
       Matrix left_hand_side;
       Vector right_hand_side;
       RandomSystem_(&left_hand_side, &right_hand_side);
-      
+
+      // Initialize the multigrid.
       multigrid.Init(left_hand_side, right_hand_side, 1000);
+
+      left_hand_side.PrintDebug();
+      right_hand_side.PrintDebug();
     }
 
     void Start() {
 
-      for (int i = 0; i < 40; i++) {
+      for (int i = 0; i < 1; i++) {
 
         // Do a trial.
         Trial();
