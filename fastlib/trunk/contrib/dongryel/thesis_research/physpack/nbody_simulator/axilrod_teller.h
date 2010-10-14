d884c9836fdd3c78feb27cac8c364e1c4df107bd
Dongryeol Lee <drselee@gmail.com>
2010-10-14 20:48:54 +0000

    More work on 3-body.

--- fastlib/trunk/contrib/dongryel/thesis_research/physpack/nbody_simulator/axilrod_teller.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/physpack/nbody_simulator/axilrod_teller.h
@@ -14,12 +14,84 @@ namespace nbody_simulator {
 class AxilrodTeller {
   public:
 
+    double minimum_negative_contribution(
+      const core::gnp::TripleRangeDistanceSq &range_in) const {
+
+      const arma::mat &min_distance_sq = range_in.min_distance_sq();
+      const arma::mat &max_distance_sq = range_in.max_distance_sq();
+      double numerator_first_part =
+        -3.0 * core::math::Pow<3, 1>(max_distance_sq.at(0, 1));
+      double max_diff = std::max(
+                          fabs(max_distance_sq.at(0, 2) - min_distance_sq.at(1, 2)),
+                          fabs(min_distance_sq.at(0, 2) - max_distance_sq.at(1, 2)));
+
+      double numerator_second_part =
+        -3.0 * core::math::Sqr(max_diff) *
+        (max_distance_sq.at(0, 2) + max_distance_sq.at(1, 2))
+
+        double denominator = 8.0 * pow(
+                               min_distance_sq.at(0, 1) *
+                               min_distance_sq.at(0, 2) *
+                               min_distance_sq.at(1, 2), 2.5);
+
+      return (numerator_first_part + numerator_second_part) / denominator;
+    }
+
+    double maximum_negative_contribution(
+      const core::gnp::TripleRangeDistanceSq &range_in) const {
+
+      const arma::mat &min_distance_sq = range_in.min_distance_sq();
+      const arma::mat &max_distance_sq = range_in.max_distance_sq();
+      double numerator_first_part =
+        -3.0 * core::math::Pow<3, 1>(min_distance_sq.at(0, 1));
+      double numerator_second_part = 0;
+      if(max_distance_sq.at(0, 2) <= min_distance_sq.at(1, 2) ||
+          max_distance_sq.at(1, 2) <= min_distance_sq.at(0, 2)) {
+        double difference =
+          std::min(
+            fabs(min_distance_sq.at(1, 2) - max_distance_sq.at(0, 2)),
+            fabs(min_distance_sq.at(0, 2) -  max_distance_sq.at(1, 2)));
+        numerator_second_part =
+          -3.0 * core::math::Sqr(diff) *
+          (min_distance_sq.at(0, 2) + min_distance_sq.at(1, 2));
+      }
+
+      double denominator = 8.0 * pow(
+                             max_distance_sq.at(0, 1) *
+                             max_distance_sq.at(0, 2) *
+                             max_distance_sq.at(1, 2), 2.5);
+
+      return (numerator_first_part + numerator_second_part) / denominator;
+    }
+
+    double minimum_positive_contribution(
+      const core::gnp::TripleRangeDistanceSq &range_in) const {
+
+    }
+
+    double maximum_positive_contribution(
+      const core::gnp::TripleRangeDistanceSq &range_in) const {
+
+    }
+
     core::math::Range RangeUnnormOnSq(
       const core::gnp::TripleRangeDistanceSq &range_in) const {
 
-      // Put the code for computing the minimum and the maximum
-      // using the global optimization based nonconvex optimization.
-      return core::math::Range(0.0, 0.0);
+      // Compute the negative contribution bound.
+      core::math::Range negative_range(
+        this->minimum_negative_contribution(range_in),
+        this->maximum_negative_contribution(range_in));
+
+      // Compute the positive contribution bound.
+      core::math::Range positive_range(
+        this->minimum_positive_contribution(range_in),
+        this->maximum_positive_contribution(range_in));
+
+      // Take the sum of two ranges.
+      core::math::Range overall_range = negative_range +
+                                        positive_range;
+
+      return overall_range;
     }
 
     double EvalUnnormOnSq(
