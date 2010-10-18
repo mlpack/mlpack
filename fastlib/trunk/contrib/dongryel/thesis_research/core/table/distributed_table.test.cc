fe7b8d19c693e1c215cac186dbb5a329faf417bb
Dongryeol Lee <drselee@gmail.com>
2010-10-18 03:30:16 +0000

    Now ready to plug back in the mailboxes.

--- fastlib/trunk/contrib/dongryel/thesis_research/core/table/distributed_table.test.cc
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/table/distributed_table.test.cc
@@ -181,6 +181,9 @@ int main(int argc, char *argv[]) {
 
   // Initialize the memory allocator.
   core::table::global_m_file_ = new core::table::MemoryMappedFile();
+  core::table::global_m_file_->Init(
+    std::string("tmp_file"), world.rank(),
+    world.rank() % (world.size() / 3), 5000000);
 
   // Seed the random number.
   srand(time(NULL) + world.rank());
