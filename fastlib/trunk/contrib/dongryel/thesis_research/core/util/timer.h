ee11f463e73bc07d03c78e0a8b816251509a6041
Dongryeol Lee <drselee@gmail.com>
2010-10-19 01:26:15 +0000

    Added a timer class.

---
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/util/timer.h
@@ -0,0 +1,69 @@
+/** @file timer.h
+ *
+ *  @author Dongryeol Lee (dongryel@cc.gatech.edu)
+ */
+
+#ifndef CORE_UTIL_TIMER_H
+#define CORE_UTIL_TIMER_H
+
+#include <sys/time.h>
+#include <string>
+#include <sstream>
+
+namespace core {
+namespace util {
+class Timer {
+
+  public:
+
+    void Start() {
+      gettimeofday(&start_, NULL);
+    }
+
+    void End() {
+      gettimeofday(&end_, NULL);
+    }
+
+    void Reset() {
+      checkpoints_.clear();
+    }
+
+    int CheckPoint() {
+      timeval curr_time;
+      gettimeofday(&curr_time, NULL);
+      checkpoints_.push_back(curr_time);
+      return (checkpoints_.size() - 1);
+    }
+
+    std::string GetTotalElapsedTimeString() {
+      timeval result;
+      timersub(&end_, &start_, &result);
+      std::stringstream str;
+      str << (result.tv_sec + result.tv_usec / 1000000.0);
+      return str.str();
+    }
+
+    double GetTotalElapsedTime() {
+      timeval result;
+      timersub(&end_, &start_, &result);
+      return (result.tv_sec + result.tv_usec / 1000000.0);
+    }
+
+    double GetElapsedTime(int checkpoint_id) {
+      timeval result;
+      timersub(&checkpoints_[checkpoint_id], &start_, &result);
+      return (result.tv_sec + result.tv_usec / 1000000.0);
+    }
+
+  private:
+
+    timeval start_;
+
+    timeval end_;
+
+    std::vector<timeval> checkpoints_;
+};
+};
+};
+
+#endif
--- fastlib/trunk/contrib/dongryel/thesis_research/physpack/nbody_simulator/nbody_simulator.cc
+++ fastlib/trunk/contrib/dongryel/thesis_research/physpack/nbody_simulator/nbody_simulator.cc
@@ -4,6 +4,7 @@
 #include "boost/mpi.hpp"
 #include "boost/serialization/string.hpp"
 #include "core/math/math_lib.h"
+#include "core/util/timer.h"
 #include "nbody_simulator_dev.h"
 #include <vector>
 #include <cstdlib>
@@ -16,13 +17,23 @@ int main(int argc, char *argv[]) {
     argc, argv, &nbody_simulator_arguments);
 
   // Instantiate a Nbody object.
+  core::util::Timer tree_build_timer;
+  tree_build_timer.Start();
   physpack::nbody_simulator::NbodySimulator nbody_simulator_instance;
   nbody_simulator_instance.Init(nbody_simulator_arguments);
+  tree_build_timer.End();
+  std::cout << tree_build_timer.GetTotalElapsedTime() << " seconds spent on "
+            "building the tree.\n";
 
   // Compute the result.
+  core::util::Timer compute_timer;
+  compute_timer.Start();
   physpack::nbody_simulator::NbodySimulatorResult nbody_simulator_result;
   nbody_simulator_instance.Compute(
     nbody_simulator_arguments, &nbody_simulator_result);
+  compute_timer.End();
+  std::cout << compute_timer.GetTotalElapsedTime() << " seconds spent on "
+            "the potential computation.\n";
 
   // Output the Nbody result to the file.
   std::cerr << "Writing the potentials to the file: " <<
