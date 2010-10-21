9387662a579040dd72d11e5624f4f9803983a3af
Dongryeol Lee <drselee@gmail.com>
2010-10-21 18:45:47 +0000

    Compiling.

--- fastlib/trunk/contrib/dongryel/thesis_research/physpack/nbody_simulator/nbody_simulator_tripletree.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/physpack/nbody_simulator/nbody_simulator_tripletree.h
@@ -151,50 +151,6 @@ class NbodySimulatorDelta {
 };
 
 class NbodySimulatorResult {
-  private:
-
-    template<typename TableType, typename TreeType>
-    void MonteCarloPostProcess_(TableType *table, TreeType *node) {
-      if(node->is_leaf()) {
-        typename TableType::TreeIterator node_iterator =
-          table->get_node_iterator(node);
-
-        // Reset the summary statistics.
-        node->stat().summary_.StartReaccumulate();
-
-        while(node_iterator.HasNext()) {
-          core::table::DenseConstPoint q_col;
-          int q_index;
-          node_iterator.Next(&q_col, &q_index);
-          this->ApplyPostponed(q_index, node->stat()->postponed_);
-
-          // Refine min and max summary statistics.
-          node->stat()->summary_.Accumulate(*this, q_index);
-        }
-        node->stat()->postponed_.SetZero();
-      }
-      else {
-
-        // Push postponeds to the children.
-        node->left()->stat()->postponed_.ApplyPostponed(
-          node->stat()->postponed_);
-        node->right()->stat()->postponed_.ApplyPostponed(
-          node->stat()->postponed_);
-        node->stat()->postponed_.SetZero();
-
-        // Recurse.
-        MonteCarloPostProcess_(table, node->left());
-        MonteCarloPostProcess_(table, node->right());
-
-        // Refine the summary statistics.
-        node->stat()->summary_.StartReaccumulate();
-        node->stat()->summary_.Accumulate(
-          node->left()->stat()->summary_, node->left()->stat()->postponed_);
-        node->stat()->summary_.Accumulate(
-          node->right()->stat()->summary_, node->right()->stat()->postponed_);
-      }
-    }
-
   public:
     std::vector< core::math::Range > negative_potential_;
     std::vector< core::math::Range > positive_potential_;
@@ -239,22 +195,22 @@ class NbodySimulatorResult {
       }
     }
 
-    template<typename GlobalType, typename TreeType, typename DeltaType>
+    template<typename GlobalType>
     void ApplyProbabilisticDelta(
-      const GlobalType &global,
-      core::gnp::TripleRangeDistanceSq &triple_range_distance_sq_in,
+      GlobalType &global,
+      const core::gnp::TripleRangeDistanceSq &triple_range_distance_sq_in,
       const std::vector<double> &failure_probabilities,
-      const DeltaType &delta_in) {
+      const NbodySimulatorDelta &delta_in) {
 
       for(int node_index = 0; node_index < 3; node_index++) {
 
-        typename GlobalType::TableType::TreeType *node =
+        core::table::Table::TreeType *node =
           triple_range_distance_sq_in.node(node_index);
         if(node_index == 0 || node !=
             triple_range_distance_sq_in.node(node_index - 1)) {
 
           // Get the iterator for the node.
-          typename GlobalType::TableType::TreeIterator node_it =
+          core::table::Table::TreeIterator node_it =
             global.query_table()->get_node_iterator(node);
           core::table::DenseConstPoint qpoint;
           int qpoint_index;
@@ -286,10 +242,6 @@ class NbodySimulatorResult {
             used_error_[qpoint_index] += delta_in.used_error_[node_index];
           }
           while(node_it.HasNext());
-
-          // Do a full refine by traversing the subtree rooted at the
-          // current node.
-          MonteCarloPostProcess_(global.table(), node);
         }
       } // end of looping over each node.
     }
