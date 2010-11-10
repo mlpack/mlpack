84534baaef1e76232493a95a898373c348ad56eb
Dongryeol Lee <drselee@gmail.com>
2010-11-10 04:35:54 +0000

    Boost Interprocess sharing works, but needs deep structuring to make sure that there are no virtual classes. The tree needs to have offset pointers.

--- fastlib/trunk/contrib/dongryel/thesis_research/core/table/dense_matrix.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/table/dense_matrix.h
@@ -8,13 +8,14 @@
 
 #include "dense_point.h"
 #include "memory_mapped_file.h"
+#include <boost/interprocess/offset_ptr.hpp>
 
 namespace core {
 namespace table {
 class DenseMatrix {
 
   private:
-    double *ptr_;
+    boost::interprocess::offset_ptr<double> ptr_;
 
     int n_rows_;
 
@@ -31,8 +32,8 @@ class DenseMatrix {
     }
 
     void swap_cols(int first_col, int second_col) {
-      double *first_ptr = ptr_ + first_col * n_rows_;
-      double *second_ptr = ptr_ + second_col * n_rows_;
+      double *first_ptr = ptr_.get() + first_col * n_rows_;
+      double *second_ptr = ptr_.get() + second_col * n_rows_;
       for(int i = 0; i < n_rows_; first_ptr++, second_ptr++, i++) {
         std::swap(*first_ptr, *second_ptr);
       }
@@ -57,10 +58,10 @@ class DenseMatrix {
 
     ~DenseMatrix() {
       if(core::table::global_m_file_) {
-        core::table::global_m_file_->Deallocate(ptr_);
+        core::table::global_m_file_->DestroyPtr(ptr_.get());
       }
       else {
-        delete[] ptr_;
+        delete[] ptr_.get();
       }
       Reset();
     }
@@ -69,19 +70,19 @@ class DenseMatrix {
       int n_rows_in, int n_cols_in) {
 
       ptr_ = (core::table::global_m_file_) ?
-             (double *)core::table::global_m_file_->Allocate(
-               n_rows_in * n_cols_in * sizeof(double)) :
+             core::table::global_m_file_->ConstructArray<double>(
+               n_rows_in * n_cols_in) :
              new double[n_rows_in * n_cols_in];
       n_rows_ = n_rows_in;
       n_cols_ = n_cols_in;
     }
 
     double *GetColumnPtr(int column_id) {
-      return ptr_ + column_id * n_rows_;
+      return ptr_.get() + column_id * n_rows_;
     }
 
     void CopyColumnVector(int column_id, double *point_out) const {
-      const double *ptr = ptr_ + column_id * n_rows_;
+      const double *ptr = ptr_.get() + column_id * n_rows_;
       for(int i = 0; i < n_rows_; ptr++, i++) {
         point_out[i] = (*ptr);
       }
@@ -89,7 +90,7 @@ class DenseMatrix {
 
     void MakeColumnVector(int column_id, std::vector<double> *point_out) const {
       point_out->resize(n_rows_);
-      const double *ptr = ptr_ + column_id * n_rows_;
+      const double *ptr = ptr_.get() + column_id * n_rows_;
       for(int i = 0; i < n_rows_; ptr++, i++) {
         (*point_out)[i] = (*ptr);
       }
@@ -98,12 +99,12 @@ class DenseMatrix {
     void MakeColumnVector(
       int i, core::table::DenseConstPoint *point_out) const {
       point_out->Alias(
-        ptr_ + i * n_rows_, n_rows_);
+        ptr_.get() + i * n_rows_, n_rows_);
     }
 
     void MakeColumnVector(int i, core::table::DensePoint *point_out) {
       point_out->Alias(
-        ptr_ + i * n_rows_, n_rows_);
+        ptr_.get() + i * n_rows_, n_rows_);
     }
 };
 };
