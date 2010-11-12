751ce0c800723b5f36316e6e1529904edf91b3a4
Dongryeol Lee <drselee@gmail.com>
2010-11-12 03:24:24 +0000

    Starting HPC homework 6.

---
+++ fastlib/trunk/contrib/dongryel/thesis_research/mlpack/parallel_kde/all_reduce.cc
@@ -0,0 +1,140 @@
+/** @file all_reduce.cc
+ *
+ *  @author Dongryeol Lee (gth840e@mail.gatech.edu)
+ *
+ *  An implementation of the all-reduce operation using MPI send and recv's.
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+#include <time.h>
+#include <values.h>
+#include <stdlib.h>
+
+double ApplyOp(MPI_Op op, double first_arg, double second_arg) {
+  double result = 0;
+  if(op == MPI_SUM) {
+    result = first_arg + second_arg;
+  }
+  else if(op == MPI_MAX) {
+    result = (first_arg > second_arg) ? first_arg : second_arg;
+  }
+  else if(op == MPI_MIN) {
+    result = (first_arg < second_arg) ? first_arg : second_arg;
+  }
+  return result;
+}
+
+int Allreduce(void *sbuf, void *rbuf, int count, MPI_Op op, MPI_Comm comm) {
+
+  // write your own code using MPI point to point ops to implement the
+  // CS6230_Allreduce here...
+  //
+  // the equivalent call using the MPI collective is
+  //
+  // MPI_Allreduce (sbuf, rbuf, count, MPI_DOUBLE, op, comm);
+
+  // The rank of the process.
+  int rank;
+  int num_processes;
+  MPI_Comm_size(comm, &num_processes);
+  MPI_Comm_rank(comm, &rank);
+
+  // Initialize the starting value for the sub-reduction result for
+  // the data owned by the current process.
+  double *sbuf_double = (double *) sbuf;
+  double sub_reduction;
+  if(op == MPI_SUM) {
+    sub_reduction = 0;
+  }
+  else if(op == MPI_MAX) {
+    sub_reduction = -DBL_MAX;
+  }
+  else if(op == MPI_MIN) {
+    sub_reduction = DBL_MAX;
+  }
+  else {
+    return MPI_ERR_OP;
+  }
+
+  // Reduce over the data held by the current process.
+  if(count > 0) {
+    sub_reduction = sbuf_double[0];
+    int i;
+
+    // Pair up for efficiency.
+    for(i = 1; i < count - 1; i += 2) {
+      double pair_reduction = ApplyOp(op, sbuf_double[i], sbuf_double[i + 1]);
+      sub_reduction = ApplyOp(op, sub_reduction, pair_reduction);
+    }
+    if(count % 2 == 0) {
+      sub_reduction = ApplyOp(op, sub_reduction, sbuf_double[count - 1]);
+    }
+  }
+
+  // Do the first half of the prefix scan operation so that the last
+  // process holds the result.
+  int i;
+  for(i = 1; i < num_processes; i *= 2) {
+
+    // If the current process ID's bit mask does not match i, then send.
+    if((rank & i) == 0) {
+      MPI_Send(&sub_reduction, 1, MPI_DOUBLE, rank + i, rank, comm);
+      break;
+    }
+
+    // If it does, then receive from its partner.
+    else {
+      double received;
+      MPI_Request receive_request;
+      MPI_Status status;
+      MPI_Irecv(
+        &received, 1, MPI_DOUBLE, rank - i, MPI_ANY_TAG,
+        comm, &receive_request);
+      MPI_Wait(&receive_request, &status);
+      sub_reduction = ApplyOp(op, sub_reduction, received);
+    }
+  }
+
+  // The last process broadcasts the result using a balanced binary
+  // tree rooted at it.
+
+  // Put a barrier so that all processes get here.
+  MPI_Barrier(comm);
+
+  return MPI_SUCCESS;
+}
+
+int main(int ac, char *av[]) {
+  int i;
+  int rank = -1;
+  int count = 100;
+  const int maxsize = 256 * 256;
+  const int vsize = 30;
+  double start, stop;
+  double sbuf[maxsize];
+  double rbuf[maxsize];
+
+  MPI_Init(&ac, &av);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  srand(time(NULL) + rank);
+  for(i = 0; i < maxsize; i++) {
+    sbuf[i] = rand() % 10;
+  }
+
+  start = MPI_Wtime();
+  Allreduce(sbuf, rbuf, vsize, MPI_SUM, MPI_COMM_WORLD);
+  stop = MPI_Wtime();
+
+  if(rank == 0) {
+    printf("Runtime = %g\n"
+           "Calls = %d\n"
+           "Time per call = %g us\n",
+           stop - start, count, 1e6 *(stop - start) / count);
+  }
+
+  MPI_Finalize();
+  return 0;
+}
