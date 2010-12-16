33b42d2dc11ae277b8c4f23fb7f7dd81b425fa4f
Dongryeol Lee <drselee@gmail.com>
2010-12-16 02:20:56 +0000

    Fixing.

--- fastlib/trunk/contrib/dongryel/thesis_research/mlpack/mixed_logit_dcm/mixed_logit_dcm_distribution.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/mlpack/mixed_logit_dcm/mixed_logit_dcm_distribution.h
@@ -28,39 +28,35 @@ class MixedLogitDCMDistribution {
      */
     void MixedLogitParameterGradientProducts(
       DCMTableType *dcm_table_in,
-      int person_index,
+      int person_index, int discrete_choice_index,
       const core::table::DensePoint &parameter_vector,
       const core::table::DensePoint &choice_probabilities,
-      core::table::DenseMatrix *product_out) {
+      core::table::DensePoint *product_out) {
 
-      // Get the number of discrete choices available for the given
-      // person.
-      int num_discrete_choices =
-        dcm_table_in->num_discrete_choices(person_index);
-      product_out->Init(this->num_parameters(), num_discrete_choices);
+      // Initialize the product.
+      product_out->Init(this->num_parameters());
 
-      // Compute the gradient matrix times vector for each discrete
-      // choice.
-      for(int i = 0; i < num_discrete_choices; i++) {
-        core::table::DensePoint attribute_vector;
-        dcm_table_in->get_attribute_vector(person_index, i, &attribute_vector);
+      // Compute the gradient matrix times vector for the person's
+      // discrete choice.
+      core::table::DensePoint attribute_vector;
+      dcm_table_in->get_attribute_vector(
+        person_index, discrete_choice_index, &attribute_vector);
 
-        core::table::DensePoint attribute_vector_sub_choice_probability_vector;
-        core::math::SubInit(
-          choice_probabilities, attribute_vector,
-          &attribute_vector_sub_choice_probability_vector);
+      core::table::DensePoint attribute_vector_sub_choice_probability_vector;
+      core::math::SubInit(
+        choice_probabilities, attribute_vector,
+        &attribute_vector_sub_choice_probability_vector);
 
-        // For each row index of the gradient,
-        for(int k = 0; k < this->num_parameters(); k++) {
+      // For each row index of the gradient,
+      for(int k = 0; k < this->num_parameters(); k++) {
 
-          // For each column index of the gradient,
-          double dot_product = 0;
-          for(int j = 0; j < attribute_vector.length(); j++) {
-            dot_product += attribute_vector_sub_choice_probability_vector[j] *
-                           this->MixedLogitParameterGradient(k, j);
-          }
-          product_out->set(k, i, dot_product);
+        // For each column index of the gradient,
+        double dot_product = 0;
+        for(int j = 0; j < attribute_vector.length(); j++) {
+          dot_product += attribute_vector_sub_choice_probability_vector[j] *
+                         this->MixedLogitParameterGradient(k, j);
         }
+        (*product_out)[k] = dot_product;
       }
     }
 };
