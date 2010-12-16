c97fb46a8b38943ca5ed84cca03bb6e6b6ff8ccc
Dongryeol Lee <drselee@gmail.com>
2010-12-16 02:20:56 +0000

    Fixing.

--- fastlib/trunk/contrib/dongryel/thesis_research/mlpack/mixed_logit_dcm/dcm_table.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/mlpack/mixed_logit_dcm/dcm_table.h
@@ -35,19 +35,16 @@ class DCMTable {
      */
     TableType *attribute_table_;
 
+    /** @brief The index of the discrete choice and the number of
+     *         discrete choices made by each person (in a
+     *         column-oriented matrix table form).
+     */
+    TableType *discrete_choice_set_info_;
+
     /** @brief The cumulative distribution on the number of discrete
      *         choices on the person scale.
      */
     std::vector<int> cumulative_num_discrete_choices_;
-
-    /** @brief The number of discrete choices available for each person.
-     */
-    std::vector<int> num_discrete_choices_per_person_;
-
-    /** @brief The number of parameters that give arise to the $\beta$
-     *         parameter.
-     */
-    int num_parameters_;
 
     /** @brief Used for sampling the outer-term of the simulated
      *         log-likelihood score.
@@ -65,49 +62,19 @@ class DCMTable {
     std::vector< core::monte_carlo::MeanVariancePair >
     simulated_choice_probabilities_;
 
-    /** @brief The gradient of the simulated log likelihood per person
-     *         per his/her discrete choice.
+    /** @brief The gradient of the simulated log likelihood per person.
      */
     std::vector <
     core::monte_carlo::MeanVariancePairVector >
     simulated_loglikelihood_gradients_;
 
     /** @brief The Hessian of the simulated log likelihood per person
-     *         per his/her discrete choice.
      */
     std::vector <
     core::monte_carlo::MeanVariancePairMatrix >
     simulated_loglikelihood_hessians_;
 
   private:
-
-    /** @brief Computes the current discrete choice with the highest
-     *         simulated probability for person_index-th person.
-     */
-    void DiscreteChoiceIndex_(
-      int person_index, int *discrete_choice_index_out,
-      double *highest_simulated_choice_probability_out) const {
-
-      // Examine the simulated choice probabilities for the given
-      // person, and select the discrete choice with the highest
-      // probability.
-      *discrete_choice_index_out = -1;
-      *highest_simulated_choice_probability_out = 0;
-      for(int j = 0;
-          j < num_discrete_choices_per_person_[person_index]; j++) {
-
-        // Get the simulated choice probability for the current
-        // discrete choice.
-        const core::monte_carlo::MeanVariancePair
-        &simulated_choice_probability = this->simulated_choice_probability(
-                                          person_index, j);
-        double probability = simulated_choice_probability.sample_mean();
-        if(probability > (*highest_simulated_choice_probability_out)) {
-          (*discrete_choice_index_out) = j;
-          (*highest_simulated_choice_probability_out) = probability;
-        }
-      }
-    }
 
     /** @brief Computes the choice probability vector for the person_index-th
      *         person for each of his/her potential choices given the
@@ -118,13 +85,13 @@ class DCMTable {
       int person_index, const core::table::DensePoint &parameter_vector,
       core::table::DensePoint *choice_probabilities) {
 
-      choice_probabilities->Init(
-        num_discrete_choices_per_person_[person_index]);
+      int num_discrete_choices = this->num_discrete_choices(person_index);
+      choice_probabilities->Init(num_discrete_choices);
 
       // First compute the normalizing sum.
       double normalizing_sum = 0.0;
-      for(int discrete_choice_index = 0; discrete_choice_index <
-          num_discrete_choices_per_person_[person_index];
+      for(int discrete_choice_index = 0;
+          discrete_choice_index < num_discrete_choices;
           discrete_choice_index++) {
 
         // Grab each attribute vector and take a dot product between
@@ -141,8 +108,8 @@ class DCMTable {
       }
 
       // Then, normalize.
-      for(int discrete_choice_index = 0; discrete_choice_index <
-          num_discrete_choices_per_person_[person_index];
+      for(int discrete_choice_index = 0;
+          discrete_choice_index < num_discrete_choices;
           discrete_choice_index++) {
         (*choice_probabilities)[discrete_choice_index] /= normalizing_sum;
       }
@@ -154,7 +121,13 @@ class DCMTable {
      *         the given person.
      */
     int num_discrete_choices(int person_index) const {
-      return num_discrete_choices_per_person_[person_index];
+      return static_cast<int>(
+               discrete_choice_set_info_->data().get(1, person_index));
+    }
+
+    int get_discrete_choice_index(int person_index) const {
+      return static_cast<int>(
+               discrete_choice_set_info_->data().get(0, person_index));
     }
 
     /** @brief Return the gradient of the current simulated log
@@ -164,7 +137,7 @@ class DCMTable {
     void SimulatedLoglikelihoodGradient(
       core::table::DensePoint *likelihood_gradient) const {
 
-      likelihood_gradient->Init(num_parameters_);
+      likelihood_gradient->Init(distribution_->num_parameters());
       likelihood_gradient->SetZero();
 
       // For each active person,
@@ -174,28 +147,24 @@ class DCMTable {
         // the person in the sample pool.
         int person_index = shuffled_indices_for_person_[i];
 
-        // Get the highest simulated choice probability for the
-        // given person.
-        int discrete_choice_index;
-        double highest_simulated_choice_probability;
-        DiscreteChoiceIndex_(
-          person_index, &discrete_choice_index,
-          &highest_simulated_choice_probability);
-        double inverse_highest_simulated_choice_probability =
-          1.0 / highest_simulated_choice_probability;
+        // Get the simulated choice probability for the given person.
+        int discrete_choice_index =
+          this->get_discrete_choice_index(person_index);
+        double simulated_choice_probability =
+          this->simulated_choice_probability(person_index);
+        double inverse_simulated_choice_probability =
+          1.0 / simulated_choice_probability;
 
-        // Get the gradient corresponding to the highest simulated
-        // choice probability.
+        // Get the gradient for the person.
         const core::monte_carlo::MeanVariancePairVector &gradient =
-          this->simulated_loglikelihood_gradient(
-            person_index, discrete_choice_index);
+          this->simulated_loglikelihood_gradient(person_index);
         core::table::DensePoint gradient_vector;
         gradient.sample_means(&gradient_vector);
 
         // Add the inverse probability weighted gradient vector for
         // the current person to the total tally.
         core::math::AddExpert(
-          inverse_highest_simulated_choice_probability,
+          inverse_simulated_choice_probability,
           gradient_vector, likelihood_gradient);
       }
 
@@ -216,14 +185,12 @@ class DCMTable {
 
         // Get the highest simulated choice probability for the
         // given person.
-        int discrete_choice_index;
-        double highest_simulated_choice_probability;
-        DiscreteChoiceIndex_(
-          person_index, &discrete_choice_index,
-          &highest_simulated_choice_probability);
+        int discrete_choice_index =
+          this->get_discrete_choice_index(person_index);
+        double simulated_choice_probability =
+          this->simulated_choice_probability(person_index);
         current_simulated_log_likelihood +=
-          log(
-            highest_simulated_choice_probability);
+          log(simulated_choice_probability);
       }
       current_simulated_log_likelihood /=
         static_cast<double>(num_active_people_);
@@ -237,31 +204,10 @@ class DCMTable {
     template<typename ArgumentType>
     void Init(ArgumentType &argument_in) {
 
-      // Set the number of parameters.
-      num_parameters_ = argument_in.distribution_->num_parameters();
-
       // Set the incoming attributes table and the number of choices
       // per person in the list.
       attribute_table_ = argument_in.attribute_table_;
-      num_discrete_choices_per_person_.resize(
-        argument_in.num_discrete_choices_per_person_->n_entries());
-
-      // This vector maintains the running simulated choice
-      // probabilities per person per discrete choice. It is indexed
-      // in the same way as the attribute table.
-      simulated_choice_probabilities_.resize(attribute_table_->n_entries());
-
-      // This vector maintains the gradients of the simulated
-      // loglikelihood per person per discrete choice.
-      simulated_loglikelihood_gradients_.resize(attribute_table_->n_entries());
-      for(unsigned int i = 0; i < simulated_loglikelihood_gradients_.size();
-          i++) {
-        simulated_loglikelihood_gradients_[i].Init(num_parameters_);
-      }
-
-      // This vector maintains the Hessians of the simulated
-      // loglikelihood per person per discrete choice.
-      simulated_loglikelihood_hessians_.resize(attribute_table_->n_entries());
+      discrete_choice_set_info_ = argument_in.num_discrete_choices_per_person_;
 
       // Initialize a randomly shuffled vector of indices for sampling
       // the outer term in the simulated log-likelihood.
@@ -286,12 +232,9 @@ class DCMTable {
           i++) {
         core::table::DensePoint point;
         argument_in.num_discrete_choices_per_person_->get(i - 1, &point);
-        int num_choices_for_current_person =
-          static_cast<int>(point[0]);
+        int num_choices_for_current_person = static_cast<int>(point[1]);
         cumulative_num_discrete_choices_[i] =
           cumulative_num_discrete_choices_[i - 1] +
-          num_choices_for_current_person;
-        num_discrete_choices_per_person_[i - 1] =
           num_choices_for_current_person;
       }
 
@@ -301,8 +244,6 @@ class DCMTable {
       core::table::DensePoint last_count_vector;
       argument_in.num_discrete_choices_per_person_->get(
         cumulative_num_discrete_choices_.size() - 1, &last_count_vector);
-      num_discrete_choices_per_person_[
-        cumulative_num_discrete_choices_.size() - 1 ] = last_count_vector[0];
       int last_count = static_cast<int>(last_count_vector[0]);
       if(cumulative_num_discrete_choices_[
             cumulative_num_discrete_choices_.size() - 1] +
@@ -311,6 +252,26 @@ class DCMTable {
                   "the number of total number of attribute vectors.\n";
         exit(0);
       }
+
+      // This vector maintains the running simulated choice
+      // probabilities per person.
+      simulated_choice_probabilities_.resize(
+        shuffled_indices_for_person_.size());
+
+      // This vector maintains the gradients of the simulated
+      // loglikelihood per person per discrete choice.
+      simulated_loglikelihood_gradients_.resize(
+        shuffled_indices_for_person_.size());
+      for(unsigned int i = 0; i < simulated_loglikelihood_gradients_.size();
+          i++) {
+        simulated_loglikelihood_gradients_[i].Init(
+          distribution_->num_parameters());
+      }
+
+      // This vector maintains the Hessians of the simulated
+      // loglikelihood per person per discrete choice.
+      simulated_loglikelihood_hessians_.resize(
+        shuffled_indices_for_person_.size());
     }
 
     /** @brief Adds the specified number of terms to the outer sum,
@@ -335,44 +296,34 @@ class DCMTable {
       // Given the parameter vector, compute the products between the
       // gradient of the $\beta$ with respect to $\theta$ and
       // $\bar{X}_i res_{i,j_i^*}(\beta^v(\theta))$.
-      core::table::DenseMatrix beta_gradient_products;
+      core::table::DensePoint beta_gradient_products;
+
+      // j_i^* index.
+      int discrete_choice_index = this->get_discrete_choice_index(person_index);
       distribution_->MixedLogitParameterGradientProducts(
-        attribute_table_, person_index, parameter_vector,
+        attribute_table_, person_index,
+        discrete_choice_index, parameter_vector,
         choice_probabilities, &beta_gradient_products);
 
-      // Loop through and update the simulated choice probabilities
+      // Update the simulated choice probabilities
       // and the simulated log-likelihood gradients.
-      int index = cumulative_num_discrete_choices_[person_index];
-      for(int num_discrete_choices = 0; num_discrete_choices <
-          num_discrete_choices_per_person_[person_index];
-          num_discrete_choices++, index++) {
+      simulated_choice_probabilities_[person_index].push_back(
+        choice_probabilities[discrete_choice_index]);
 
-        // Simulated choice probability update.
-        simulated_choice_probabilities_[index].push_back(
-          choice_probabilities[num_discrete_choices]);
-
-        // Simulated log-likelihood gradient update.
+      // Simulated log-likelihood gradient update.
 
 
-        // simulated_loglikelihood_gradients_[index].push_back();
-      }
+      // simulated_loglikelihood_gradients_[index].push_back();
+
     }
 
     const core::monte_carlo::MeanVariancePairVector &
-    simulated_loglikelihood_gradient(
-      int person_index, int discrete_choice_index) const {
-
-      int index = cumulative_num_discrete_choices_[person_index] +
-                  discrete_choice_index;
-      return simulated_loglikelihood_gradients_[index];
+    simulated_loglikelihood_gradient(int person_index) const {
+      return simulated_loglikelihood_gradients_[person_index];
     }
 
-    const core::monte_carlo::MeanVariancePair &simulated_choice_probability(
-      int person_index, int discrete_choice_index) const {
-
-      int index = cumulative_num_discrete_choices_[person_index] +
-                  discrete_choice_index;
-      return simulated_choice_probabilities_[index];
+    double simulated_choice_probability(int person_index) const {
+      return simulated_choice_probabilities_[person_index].sample_mean();
     }
 
     /** @brief Retrieve the discrete_choice_index-th attribute vector
