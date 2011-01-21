0a46962dcb3377ac4a7ddcebb2dc891b2e62e59c
Ryan Curtin <ryan@ratml.org>
2011-01-21 19:34:10 +0000

    Refactor AllNN into header and source file since it's not a templated class (that may change).  Copy the main executable over from trunk, where I modified it more heavily, then transition to armadillo components.

--- fastlib/branches/fastlib-stl/mlpack/allnn/main.cc
+++ fastlib/branches/fastlib-stl/mlpack/allnn/main.cc
@@ -19,23 +19,33 @@
 #include <string>
 #include "allnn.h"
 
-#include <armadillo>
-#include <fastlib/base/arma_compat.h>
+using namespace std;
+using namespace mlpack;
+using namespace allnn;
 
 int main (int argc, char *argv[]) {
-  fx_module *fx_root=fx_init(argc, argv, NULL);
+  fx_module *fx_root = fx_init(argc, argv, &allnn_doc);
   AllNN allnn;
+
   arma::mat data_for_tree;
-  
-  std::string filename=fx_param_str_req(fx_root, "file");
-  NOTIFY("Loading file...");
-  data::Load(filename.c_str(), data_for_tree);
-  NOTIFY("File loaded...");
+
+  string input_file = fx_param_str_req(fx_root, "input_file");
+  string output_file = fx_param_str(fx_root, "output_file", "output.csv");
+
+  NOTIFY("Loading file %s...", input_file.c_str());
+  data::Load(input_file.c_str(), data_for_tree);
   allnn.Init(&data_for_tree, fx_root);
- 
-  arma::vec output;
-  NOTIFY("Computing Neighbors...");
-  allnn.ComputeNeighbors(output);
-  NOTIFY("Neighbors Computed...");
+  
+  arma::Col<index_t> resulting_neighbors_tree;
+  arma::vec resulting_distances_tree;
+
+  NOTIFY("Computing neighbors...");
+
+  allnn.ComputeNeighbors(resulting_distances_tree, resulting_neighbors_tree);
+
+  NOTIFY("Saving results to %s...", output_file.c_str());
+
+  data::Save(output_file.c_str(), resulting_neighbors_tree, resulting_distances_tree); 
+
   fx_done(fx_root);
 }
