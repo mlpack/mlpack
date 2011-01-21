51fd553b27ca8ac5678f47678806a8cda6d9fef6
Dongryeol Lee <drselee@gmail.com>
2011-01-21 23:25:17 +0000

    I will be really back.

--- fastlib/trunk/contrib/dongryel/thesis_research/mlpack/distributed_kde/distributed_kde.test.cc
+++ fastlib/trunk/contrib/dongryel/thesis_research/mlpack/distributed_kde/distributed_kde.test.cc
@@ -217,7 +217,7 @@ class TestDistributed_Kde {
         num_dimensions = core::math::RandInt(3, 10);
       }
       boost::mpi::broadcast(world, num_dimensions, 0);
-      int num_points = core::math::RandInt(100, 301);
+      int num_points = core::math::RandInt(1000, 1501);
       std::vector< std::string > args;
 
       // Push in the random generate command.
