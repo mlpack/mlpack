a39d9ba49a2f9fa024a32d5445668b1217beb373
(no author) <(no author)@9d5b8971-822b-0410-80eb-d18c1038ef23>
2011-01-28 20:51:06 +0000

    More documentation added.

--- fastlib/trunk/contrib/dongryel/thesis_research/core/gnp/distributed_tripletree_dfs.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/gnp/distributed_tripletree_dfs.h
@@ -1,7 +1,7 @@
 /** @file distributed_tripletree_dfs.h
  *
- *  The prototype declaration for doing a generic distributed triple
- *  tree computation.
+ *  The prototype header for performing a distributed three-tuple
+ *  GNPs.
  *
  *  @author Dongryeol Lee (dongryel@cc.gatech.edu)
  */
@@ -10,38 +10,115 @@
 #define CORE_GNP_DISTRIBUTED_TRIPLETREE_DFS_H
 
 #include <boost/mpi/communicator.hpp>
+#include <boost/tuple/tuple.hpp>
+#include "core/gnp/tripletree_dfs.h"
 #include "core/math/range.h"
+#include "core/table/sub_table.h"
+#include "core/table/sub_table_list.h"
 
 namespace core {
 namespace gnp {
+
+/** @brief The triple tree distributed algorithm generator.
+ */
 template<typename DistributedProblemType>
 class DistributedTripletreeDfs {
 
   public:
 
+    /** @brief The table type.
+     */
     typedef typename DistributedProblemType::TableType TableType;
+
+    /** @brief The distributed computation problem.
+     */
     typedef typename DistributedProblemType::ProblemType ProblemType;
-    typedef typename DistributedProblemType::DistributedTableType DistributedTableType;
+
+    /** @brief The distributed table type.
+     */
+    typedef typename DistributedProblemType::DistributedTableType
+    DistributedTableType;
+
+    /** @brief The local tree type.
+     */
     typedef typename TableType::TreeType TreeType;
+
+    /** @brief The distributed tree type.
+     */
     typedef typename DistributedTableType::TreeType DistributedTreeType;
+
+    /** @brief The global constant type for the problem.
+     */
     typedef typename DistributedProblemType::GlobalType GlobalType;
+
+    /** @brief The type of the result being outputted.
+     */
     typedef typename DistributedProblemType::ResultType ResultType;
+
+    /** @brief The argument type of the computation.
+     */
     typedef typename DistributedProblemType::ArgumentType ArgumentType;
 
+    /** @brief The type of the object used for prioritizing the
+     *         computation.
+     */
+    typedef typename core::gnp::TripletreeDfs<ProblemType>::FrontierObjectType
+    FrontierObjectType;
+
+    /** @brief The type of the subtable in use.
+     */
+    typedef core::table::SubTable<TableType> SubTableType;
+
+    /** @brief The type of the list of subtables in use.
+     */
+    typedef core::table::SubTableList<SubTableType> SubTableListType;
+
   private:
 
+    /** @brief The pointer to the boost communicator.
+     */
     boost::mpi::communicator *world_;
 
+    /** @brief The problem definition for the distributed computation.
+     */
     DistributedProblemType *problem_;
 
+    /** @brief The distributed query table.
+     */
     DistributedTableType *query_table_;
 
+    /** @brief The distributed reference table.
+     */
     DistributedTableType *reference_table_;
+
+    /** @brief The maximum number of tree levels to serialize at a
+     *         time.
+     */
+    int max_num_levels_to_serialize_;
+
+    /** @brief The maximum number of work items to dequeue per
+     *         process.
+     */
+    int max_num_work_to_dequeue_per_stage_;
+
+    /** @brief The list of nodes (IDed by the begin/count) that have
+     *         points underneath (i.e. for which the base case
+     *         computation and the Monte Carlo computation are
+     *         possible).
+     */
+    std::map<int, int> points_serialized_underneath_;
+
+    /** @brief The list of unpruned triples of nodes.
+     */
+    std::vector < FrontierObjectType > unpruned_triple_nodes_;
 
   private:
 
+    /** @brief The collaborative way of exchanging items among all MPI
+     *         processes for a distributed computation.
+     */
     template<typename MetricType>
-    void ReduceScatter_(
+    void AllToAllReduce_(
       const MetricType &metric,
       typename DistributedProblemType::ResultType *query_results);
 
@@ -59,25 +136,62 @@ class DistributedTripletreeDfs {
       const MetricType &metric,
       TreeType *qnode, ResultType *query_results);
 
+    /** @brief The class used for prioritizing a computation object
+     *         (query, reference pair).
+     */
+    class PrioritizeTasks_:
+      public std::binary_function <
+        FrontierObjectType &, FrontierObjectType &, bool > {
+      public:
+        bool operator()(
+          const FrontierObjectType &a, const FrontierObjectType &b) const {
+          return a.get<2>() > b.get<2>();
+        }
+    };
+
   public:
 
+    /** @brief Sets the tweak parameters for the maximum number of
+     *         levels of trees to grab at a time and the maximum
+     *         number of work per stage to dequeue.
+     */
+    void set_work_params(
+      int max_num_levels_to_serialize_in,
+      int max_num_work_to_dequeue_per_stage_in);
+
+    /** @brief The default constructor.
+     */
+    DistributedTripletreeDfs();
+
+    /** @brief Returns the associated problem.
+     */
     DistributedProblemType *problem();
 
+    /** @brief Returns the distributed query table.
+     */
     DistributedTableType *query_table();
 
+    /** @brief Returns the distributed reference table.
+     */
     DistributedTableType *reference_table();
 
+    /** @brief Resets the statistics of the query tree.
+     */
     void ResetStatistic();
 
+    /** @brief Initializes the distributed dualtree engine.
+     */
     void Init(
       boost::mpi::communicator *world, DistributedProblemType &problem_in);
 
+    /** @brief Initiates the distributed computation.
+     */
     template<typename MetricType>
     void Compute(
       const MetricType &metric,
       typename DistributedProblemType::ResultType *query_results);
 };
-};
-};
+}
+}
 
 #endif
--- fastlib/trunk/contrib/dongryel/thesis_research/core/gnp/tripletree_dfs.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/gnp/tripletree_dfs.h
@@ -9,33 +9,65 @@
 #ifndef CORE_GNP_TRIPLETREE_DFS_H
 #define CORE_GNP_TRIPLETREE_DFS_H
 
+#include <boost/tuple/tuple.hpp>
 #include "core/math/range.h"
 #include "core/gnp/triple_range_distance_sq.h"
 
 namespace core {
 namespace gnp {
+
+/** @brief The tripletree algorithm template generator.
+ */
 template<typename ProblemType>
 class TripletreeDfs {
 
   public:
 
+    /** @brief The table type.
+     */
     typedef typename ProblemType::TableType TableType;
+
+    /** @brief The tree type.
+     */
     typedef typename TableType::TreeType TreeType;
+
+    /** @brief The global constant type for the problem.
+     */
     typedef typename ProblemType::GlobalType GlobalType;
+
+    /** @brief The result type for the problem.
+     */
     typedef typename ProblemType::ResultType ResultType;
+
+    /** @brief The type of the object used for prioritizing the
+     *         computation.
+     */
+    typedef boost::tuple <
+    TreeType *, boost::tuple<int, int, int>, double > FrontierObjectType;
 
   private:
 
+    /** @brief The pointer to the problem.
+     */
     ProblemType *problem_;
 
+    /** @brief The table object.
+     */
     TableType *table_;
 
+    /** @brief The number of deterministic prunes.
+     */
     int num_deterministic_prunes_;
 
+    /** @brief The number of Monte Carlo prunes.
+     */
     int num_monte_carlo_prunes_;
 
   private:
 
+    /** @brief Allocates the probability level when the recursion
+     *         splits a set of nodes.
+     */
     void AllocateProbabilities_(
       const std::vector<double> &failure_probabilites,
       const std::deque<bool> &node_is_split,
@@ -43,6 +75,10 @@ class TripletreeDfs {
       const std::vector<int> &deterministic_computation_count,
       std::vector<double> *new_failure_probabilities) const;
 
+    /** @brief Determines whether the second argument is equal to the
+     *         first argument in node ID index, or comes later in DFS
+     *         index.
+     */
     bool NodeIsAgreeable_(TreeType *node, TreeType *next_node) const;
 
     typename TableType::TreeIterator GetNextNodeIterator_(
@@ -52,6 +88,9 @@ class TripletreeDfs {
 
     void ResetStatisticRecursion_(TreeType *node);
 
+    /** @brief A helper function for generating all triple-tree
+     *         combination for a set of three nodes.
+     */
     template<typename MetricType>
     void RecursionHelper_(
       const MetricType &metric,
@@ -68,6 +107,8 @@ class TripletreeDfs {
 
     void PreProcess_(TreeType *node);
 
+    /** @brief Computes a triple of nodes naively.
+     */
     template<typename MetricType>
     void TripletreeBase_(
       const MetricType &metric,
@@ -105,6 +146,9 @@ class TripletreeDfs {
       const typename ProblemType::DeltaType &delta,
       typename ProblemType::ResultType *query_results);
 
+    /** @brief The canonical recursion function for tripletree
+     *         algorithms.
+     */
     template<typename MetricType>
     bool TripletreeCanonical_(
       const MetricType &metric,
@@ -121,23 +165,39 @@ class TripletreeDfs {
 
   public:
 
+    /** @brief Returns the number of deterministic prunes.
+     */
     int num_deterministic_prunes() const;
 
+    /** @brief Returns the number of Monte Carlo prunes.
+     */
     int num_monte_carlo_prunes() const;
 
+    /** @brief Returns the pointer to the problem.
+     */
     ProblemType *problem();
 
+    /** @brief Returns the pointer to the table.
+     */
     TableType *table();
 
     void ResetStatistic();
 
+    /** @brief Initializes the triple tree engine with a problem
+     *         specification.
+     */
     void Init(ProblemType &problem_in);
 
+    /** @brief Computes the triple tree problem naively.
+     */
     template<typename MetricType>
     void NaiveCompute(
       const MetricType &metric,
       typename ProblemType::ResultType *naive_query_results);
 
+    /** @brief Computes the triple tree problem using a tree-based
+     *         approach.
+     */
     template<typename MetricType>
     void Compute(
       const MetricType &metric,
