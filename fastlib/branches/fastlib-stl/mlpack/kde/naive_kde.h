260ae7830491d0becfad3e25e65a05d328f58342
Ryan Curtin <ryan@ratml.org>
2011-02-08 17:04:49 +0000

    Unbreak everything: update references to AllkNN: AllkNN -> mlpack::allknn::AllkNN

--- fastlib/branches/fastlib-stl/mlpack/kde/naive_kde.h
+++ fastlib/branches/fastlib-stl/mlpack/kde/naive_kde.h
@@ -179,7 +179,7 @@ class NaiveKde {
 
       // Initialize the kernels for each reference point.
       int knns = fx_param_int_req(module_, "knn");
-      AllkNN all_knn;
+      mlpack::allknn::AllkNN all_knn;
       all_knn.Init(&rset_, 20, knns);
       arma::Col<index_t> resulting_neighbors;
       arma::vec squared_distances;    
