e5dd24ce067958f66c2d412769e3a69b59177db8
tqlong <tqlong@9d5b8971-822b-0410-80eb-d18c1038ef23>
2011-02-14 05:09:30 +0000

    feature selection using submodular function inducing norm: started

---
+++ fastlib/trunk/contrib/tqlong/feature-selection/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 12, 2011
+ *      Author: tqlong
+ */
+
+#include <iostream>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+int main(int argc, char** argv)
+{
+  cout << "Feature selection using sub-modular function inducing norms...\n";
+  cout << "Solving optimization of the form: \\lambda \\Omega(w) + \\sum_i L(y_i, w'x_i)\n";
+  return 0;
+}
