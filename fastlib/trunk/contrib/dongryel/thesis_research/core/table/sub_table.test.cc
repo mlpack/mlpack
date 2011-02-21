80046a373b7760d8dd1a8c1eb275b9777e41842a
Dongryeol Lee <drselee@gmail.com>
2011-02-21 17:11:30 +0000

    So there is a problem with the node iterator in the subtable, so I'm fixing. After that everything should work.

--- fastlib/trunk/contrib/dongryel/thesis_research/core/table/sub_table.test.cc
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/table/sub_table.test.cc
@@ -44,6 +44,49 @@ class TestSubTable {
 
   private:
 
+    bool TestNodeIterator_(
+      TableType &original_table,
+      TreeType *node_from_original_table,
+      TableType &sub_table,
+      TreeType *node_from_sub_table) {
+
+      // Get the iterator for both nodes.
+      typename TableType::TreeIterator original_it =
+        original_table.get_node_iterator(node_from_original_table);
+      typename TableType::TreeIterator sub_it =
+        sub_table.get_node_iterator(node_from_sub_table);
+
+      // The result here.
+      bool result = true;
+
+      // Check whether the counts are the same.
+      result = (original_it.count() == sub_it.count());
+
+      // Loop through each iterator and compare.
+      while(result && sub_it.HasNext()) {
+        core::table::DensePoint original_point;
+        int original_point_id;
+        core::table::DensePoint copy_point;
+        int copy_point_id;
+        original_it.Next(&original_point, &original_point_id);
+        sub_it.Next(&copy_point, &copy_point_id);
+
+        // Check the IDs.
+        result = (original_point_id == copy_point_id);
+      }
+
+      if(result && (! node_from_sub_table->is_leaf())) {
+        bool left_result = TestNodeIterator_(
+                             original_table, node_from_original_table->left(),
+                             sub_table, node_from_sub_table->left());
+        bool right_result = TestNodeIterator_(
+                              original_table, node_from_original_table->right(),
+                              sub_table, node_from_sub_table->right());
+        result = left_result && right_result;
+      }
+      return result;
+    }
+
     void GenerateRandomDataset_(
       int num_dimensions,
       int num_points,
@@ -79,6 +122,7 @@ class TestSubTable {
   public:
 
     int StressTestMain(boost::mpi::communicator &world) {
+      /*
       for(int i = 0; i < 50; i++) {
         int num_dimensions;
         if(world.rank() == 0) {
@@ -91,24 +135,27 @@ class TestSubTable {
           exit(0);
         }
       }
+      */
 
       // Now each process extracts its own subtable and checks the
       // iterator against the iterator of the original table.
       for(int i = 0; i < 50; i++) {
-	int num_dimensions = core::math::RandInt(3, 20);
-	int num_points = core::math::RandInt(300, 501);
-	if(SelfStressTest(world, num_dimensions, num_points) == false) {
-	  printf("Process %d failed!\n", world.rank());
-	  break;
-	}
+        int num_dimensions = core::math::RandInt(3, 20);
+        int num_points = core::math::RandInt(300, 501);
+        if(SelfStressTest(world, num_dimensions, num_points) == false) {
+          printf("Process %d failed!\n", world.rank());
+          break;
+        }
+
+        printf("%d-th iteration done for Process %d\n\n", i, world.rank());
       }
 
       return 0;
     }
-  
+
     bool SelfStressTest(
       boost::mpi::communicator &world, int num_dimensions, int num_points) {
-      
+
       // Generate the random table.
       TableType random_table;
       GenerateRandomDataset_(
@@ -126,44 +173,51 @@ class TestSubTable {
       for(int k = 0; k < 10; k++) {
 
         // Think of how many levels of trees to serialize.
-        int max_num_levels_to_serialize = core::math::RandInt(2, 10);
-	printf("Serializing %d levels.\n", max_num_levels_to_serialize);
+        int max_num_levels_to_serialize = 3;
+        printf("Serializing %d levels.\n", max_num_levels_to_serialize);
 
         // The subtable to test.
         SubTableType subtable_to_save;
-	SubTableType subtable_to_load;
+        SubTableType subtable_to_load;
 
-	// Pick a random node to start and save it to a file, and
-	// reload it.
-	int random_node_id =
-	  core::math::RandInt(
+        // Pick a random node to start and save it to a file, and
+        // reload it.
+        int random_node_id =
+          core::math::RandInt(
             0, static_cast<int>(local_list_of_nodes.size()));
-	int begin = local_list_of_nodes[random_node_id]->begin();
-	int count = local_list_of_nodes[random_node_id]->count();
-	printf("Process %d chose %d %d to test\n", world.rank(), begin, count);
-	subtable_to_save.Init(
+        int begin = local_list_of_nodes[random_node_id]->begin();
+        int count = local_list_of_nodes[random_node_id]->count();
+        printf("Process %d chose %d %d to test\n", world.rank(), begin, count);
+        subtable_to_save.Init(
           &random_table,
-	  random_table.get_tree()->FindByBeginCount(begin, count),
-	  max_num_levels_to_serialize, false);
-	subtable_to_load.Init(0, max_num_levels_to_serialize, false);
+          random_table.get_tree()->FindByBeginCount(begin, count),
+          max_num_levels_to_serialize, false);
+        subtable_to_load.Init(0, max_num_levels_to_serialize, false);
 
-	{
-	  // Save.
-	  std::stringstream output_file_name_sstr;
-	  output_file_name_sstr << "output_file.csv" << world.rank();
-	  std::ofstream ofs(output_file_name_sstr.str().c_str());
-	  boost::archive::text_oarchive oa(ofs);
-	  oa << subtable_to_save;
-	}
-	{
-	  // Load.
-	  std::stringstream input_file_name_sstr;
-	  input_file_name_sstr << "input_file.csv" << world.rank();
-	  std::ifstream ifs(input_file_name_sstr.str().c_str());
-	  boost::archive::text_iarchive ia(ifs);
-	  ia >> subtable_to_load;
-	}
-	
+        // File name to test.
+        std::stringstream test_file_name_sstr;
+        test_file_name_sstr << "test_file.csv" << world.rank();
+        {
+          // Save.
+          std::ofstream ofs(test_file_name_sstr.str().c_str());
+          boost::archive::text_oarchive oa(ofs);
+          oa << subtable_to_save;
+        }
+        {
+          // Load.
+          std::ifstream ifs(test_file_name_sstr.str().c_str());
+          boost::archive::text_iarchive ia(ifs);
+          ia >> subtable_to_load;
+        }
+
+        // Test the iterator.
+        if(TestNodeIterator_(
+              random_table,
+              random_table.get_tree()->FindByBeginCount(begin, count),
+              *(subtable_to_load.table()),
+              subtable_to_load.table()->get_tree()) == false) {
+          return false;
+        }
 
       } // end of the trial loop.
 
