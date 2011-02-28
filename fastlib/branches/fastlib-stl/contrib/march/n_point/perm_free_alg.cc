86755a67cc194d086aeb523376ac715636103c42
Bill March <march@ices.utexas.edu>
2011-02-28 20:54:22 +0000

    writing body of matcher and node_tuple

--- fastlib/branches/fastlib-stl/contrib/march/n_point/perm_free_alg.cc
+++ fastlib/branches/fastlib-stl/contrib/march/n_point/perm_free_alg.cc
@@ -9,3 +9,28 @@
 
 #include "perm_free_alg.h"
 
+
+void npt::PermFreeAlg::BaseCaseHelper_();
+
+void npt::PermFreeAlg::BaseCase_(NodeTuple& nodes);
+
+void npt::PermFreeAlg::DepthFirstRecursion_(NodeTuple& nodes) {
+  
+  if (CanPrune_(nodes)) {
+    
+    num_prunes_++;
+    
+  }
+  else if (nodes.all_leaves()) {
+    
+    BaseCase_(nodes);
+    
+  } 
+  else {
+    
+    
+    
+  } 
+  
+  
+} // DepthFirstRecursion
--- fastlib/branches/fastlib-stl/contrib/march/n_point/perm_free_matcher.cc
+++ fastlib/branches/fastlib-stl/contrib/march/n_point/perm_free_matcher.cc
@@ -9,3 +9,60 @@
 
 #include "perm_free_matcher.h"
 
+
+
+bool npt::PermFreeMatcher::TestNodeTuple(NodeTuple& nodes) {
+  
+  for (index_t i = 0; i < upper_bounds_sq_.size(); i++) {
+    
+    if (nodes.lower_bound(i) > upper_bounds_sq_[i]) {
+      return false;
+    }
+
+    if (nodes.upper_bound(i) < lower_bounds_sq_[i]) {
+      return false;
+    }
+    
+  } // for i
+  
+  return true;
+  
+} // TestNodeTuple
+
+
+
+
+bool npt::PermFreeMatcher::TestPointPair(double dist_sq, index_t tuple_index_1, 
+                                         index_t tuple_index_2, 
+                                         ArrayList<bool>& permutation_ok) {
+  
+  
+  DEBUG_ASSERT(permutation_ok.size() == perms_.factorials(tuple_size_));
+  
+  bool any_matches = false;
+  
+  for (index_t i = 0; i < num_permutations_; i++) {
+    
+    if (!(permutation_ok[i])) {
+      continue;
+    } // does this permutation work?
+    
+    index_t template_index_1 = GetPermutationIndex_(i, tuple_index_1);
+    index_t template_index_2 = GetPermutationIndex_(i, tuple_index_2);
+    
+    
+    if (dist_sq <= upper_bounds_sq_mat_.ref(template_index_1, template_index_2)) {
+      any_matches = true;
+    } // this placement works
+    else {
+      permutation_ok[i] = false;
+    } // this placement doesn't work
+    
+  } // for i
+  
+  return any_matches;  
+  
+  
+} // TestPointPair
+
+
