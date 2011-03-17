d4a067ce20e9f73eb4c815564493adf86b69745e
pagarwal <pagarwal@9d5b8971-822b-0410-80eb-d18c1038ef23>
2011-03-17 15:27:30 +0000

    test if NR is working and start quasinewton optimizer

---
+++ fastlib/branches/fastlib-stl/contrib/pagarwal/optimizers/NR_test.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "../Numerical_recipes/nr3.h"
+#include "../Numerical_recipes/calendar.h"
+#include "../Numerical_recipes/moment.h"
+
+using namespace std;
+
+Int main(void) {
+        const Int NTOT = 20;
+        Int i, jd, nph = 2;
+        Doub frac, ave, vrnce;
+        VecDoub data(NTOT);
+        for (i = 0; i < NTOT; i++) {
+                flmoon(i, nph, jd, frac);
+                data[i] = jd;
+        }
+        avevar(data, ave, vrnce);
+        cout << "Average = " << setw(12) << ave << endl;
+        cout << "Variance = " << setw(13) << vrnce << endl;
+        return 0;
+}
