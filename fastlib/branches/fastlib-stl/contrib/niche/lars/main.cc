febd5bdddac2dff31fbb995675a614cb29629654
tekhnofiend <nishantmehta.x@gmail.com>
2011-04-03 03:52:06 +0000

    elastic net code seems to work!

--- fastlib/branches/fastlib-stl/contrib/niche/lars/main.cc
+++ fastlib/branches/fastlib-stl/contrib/niche/lars/main.cc
@@ -14,27 +14,46 @@ using namespace arma;
 using namespace std;
 
 int main(int argc, char* argv[]) {
+
+  bool use_cholesky = true;
+  double lambda_1 = 0.12;
+  double lambda_2 = 1.0;
   
-  int n = 100;
-  int p = 10;
+  //u32 n = 100;
+  //u32 p = 10;
 
-  std::srand(17);
-  mat X = randu<mat>(n,p);
+  //std::srand(17);
+  mat X;// = randu<mat>(n,p);
   X.load("X.dat", raw_ascii);
-
+  
+  u32 n = X.n_rows;
+  u32 p = X.n_cols;
+  
+  mat X_reg = zeros(n + p, p);
+  X_reg(span(0, n - 1), span::all) = X;
+  for(u32 i = 0; i < p; i++) {
+    X_reg(n + i, i) = sqrt(lambda_2);
+  }
+  X_reg.print("X_reg");
+  
+  /*
   mat beta = zeros(p,1);
   beta(0) = 1;
   beta(1) = -1;
   beta(9) = 1;
-
-  vec y = X * beta + 0.1 * randu<vec>(n);
+  */
+  
+  vec y;// = X * beta + 0.1 * randu<vec>(n);
   y.load("y.dat", raw_ascii);
-
-  bool use_cholesky = !true;
-  double lambda_1 = 0.12;//1.0;
+  
+  vec y_reg = zeros(n + p);
+  y_reg.subvec(0, n - 1) = y;
+  y_reg.print("y_reg");
+  
   
   Lars lars;
-  lars.Init(X, y, use_cholesky, lambda_1);
+  lars.Init(X, y, use_cholesky, lambda_1, lambda_2);
+  //lars.Init(X_reg, y_reg, use_cholesky, lambda_1);
   
   lars.DoLARS();
   
