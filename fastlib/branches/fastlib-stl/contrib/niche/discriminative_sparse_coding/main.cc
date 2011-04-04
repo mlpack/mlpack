7d50d80340824268cf2ba3d2f3fb2959f80ce997
niche <niche@9d5b8971-822b-0410-80eb-d18c1038ef23>
2011-04-04 16:40:57 +0000

    fixed DiscrSparseCoding compilation errors due to class renaming

--- fastlib/branches/fastlib-stl/contrib/niche/discriminative_sparse_coding/main.cc
+++ fastlib/branches/fastlib-stl/contrib/niche/discriminative_sparse_coding/main.cc
@@ -8,13 +8,13 @@
 #include <fastlib/fastlib.h>
 #include <armadillo>
 
-#include "discriminative_sparse_coding.h"
+#include "discr_sparse_coding.h"
 
 using namespace arma;
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  DiscriminativeSparseCoding dsc;
+  DiscrSparseCoding dsc;
   mat X = randu(10,100);
   vec y = randu(100);
   dsc.Init(X, y, 20, 1.0, 1.0);
