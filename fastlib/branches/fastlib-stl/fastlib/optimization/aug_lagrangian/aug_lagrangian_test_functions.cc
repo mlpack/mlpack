4b74a53ff628352872747262c9d16c58ca4d507b
Ryan Curtin <ryan@ratml.org>
2011-04-05 18:41:50 +0000

    Make AugLagrangianTestFunction work, and also create new test function (which works).

--- fastlib/branches/fastlib-stl/fastlib/optimization/aug_lagrangian/aug_lagrangian_test_functions.cc
+++ fastlib/branches/fastlib-stl/fastlib/optimization/aug_lagrangian/aug_lagrangian_test_functions.cc
@@ -10,6 +10,9 @@
 using namespace mlpack;
 using namespace mlpack::optimization;
 
+//
+// AugLagrangianTestFunction
+//
 AugLagrangianTestFunction::AugLagrangianTestFunction() {
   // Set the initial point to be (0, 0).
   initial_point_.zeros(2, 1);
@@ -52,5 +55,79 @@ void AugLagrangianTestFunction::GradientConstraint(int index,
     // c'_x1(x) = 1
     // c'_x2(x) = 1
     gradient.ones(2, 1); // Use a shortcut instead of assigning individually.
+  }
+}
+
+// 
+// GockenbachFunction
+//
+GockenbachFunction::GockenbachFunction() {
+  // Set the initial point to (0, 0, 1).
+  initial_point_.zeros(3, 1);
+  initial_point_[2] = 1;
+}
+
+double GockenbachFunction::Evaluate(const arma::mat& coordinates) {
+  // f(x) = (x_1 - 1)^2 + 2 (x_2 + 2)^2 + 3(x_3 + 3)^2
+  return ((std::pow(coordinates[0] - 1, 2)) +
+          (2 * std::pow(coordinates[1] + 2, 2)) +
+          (3 * std::pow(coordinates[2] + 3, 2)));
+}
+
+void GockenbachFunction::Gradient(const arma::mat& coordinates,
+                                  arma::mat& gradient) {
+  // f'_x1(x) = 2 (x_1 - 1)
+  // f'_x2(x) = 4 (x_2 + 2)
+  // f'_x3(x) = 6 (x_3 + 3)
+  gradient.set_size(3, 1);
+
+  gradient[0] = 2 * (coordinates[0] - 1);
+  gradient[1] = 4 * (coordinates[1] + 2);
+  gradient[2] = 6 * (coordinates[2] + 3);
+}
+
+double GockenbachFunction::EvaluateConstraint(int index,
+                                              const arma::mat& coordinates) {
+  double constraint = 0;
+  
+  switch(index) {
+    case 0: // g(x) = (x_3 - x_2 - x_1 - 1) = 0
+      constraint = (coordinates[2] - coordinates[1] - coordinates[0] - 1);
+      break;
+
+    case 1: // h(x) = (x_3 - x_1^2) >= 0
+      // To deal with the inequality, the constraint will simply evaluate to 0
+      // when h(x) >= 0.
+      constraint = std::min(0.0,
+          (coordinates[2] - std::pow(coordinates[0], 2)));
+      break;
+  }
+
+  // 0 will be returned for an invalid index (but this is okay).
+  return constraint;
+}
+
+void GockenbachFunction::GradientConstraint(int index,
+                                            const arma::mat& coordinates,
+                                            arma::mat& gradient) {
+  gradient.zeros(3, 1);
+
+  switch(index) {
+    case 0:
+      // g'_x1(x) = -1
+      // g'_x2(x) = -1
+      // g'_x3(x) = 1
+      gradient[0] = -1;
+      gradient[1] = -1;
+      gradient[2] = 1;
+      break;
+
+    case 1:
+      // h'_x1(x) = -2 x_1
+      // h'_x2(x) = 0
+      // h'_x3(x) = 1
+      gradient[0] = -2 * coordinates[0];
+      gradient[2] = 1;
+      break;
   }
 }
--- fastlib/branches/fastlib-stl/fastlib/optimization/aug_lagrangian/aug_lagrangian_test_functions.h
+++ fastlib/branches/fastlib-stl/fastlib/optimization/aug_lagrangian/aug_lagrangian_test_functions.h
@@ -13,6 +13,14 @@
 namespace mlpack {
 namespace optimization {
 
+/***
+ * This function is taken from "Practical Mathematical Optimization" (Snyman),
+ * section 5.3.8 ("Application of the Augmented Lagrangian Method").  It has
+ * only one constraint.
+ *
+ * The minimum that satisfies the constraint is x = [1, 4], with an objective
+ * value of 70.
+ */
 class AugLagrangianTestFunction {
  public:
   AugLagrangianTestFunction();
@@ -21,6 +29,37 @@ class AugLagrangianTestFunction {
   void Gradient(const arma::mat& coordinates, arma::mat& gradient);
 
   int NumConstraints() const { return 1; }
+
+  double EvaluateConstraint(int index, const arma::mat& coordinates);
+  void GradientConstraint(int index,
+                          const arma::mat& coordinates,
+                          arma::mat& gradient);
+
+  const arma::mat& GetInitialPoint() { return initial_point_; }
+
+ private:
+  arma::mat initial_point_;
+};
+
+/***
+ * This function is taken from M. Gockenbach's lectures on general nonlinear
+ * programs, found at:
+ * http://www.math.mtu.edu/~msgocken/ma5630spring2003/lectures/nlp/nlp.pdf
+ *
+ * The program we are using is example 2.5 from this document.
+ * I have arbitrarily decided that this will be called the Gockenbach function.
+ *
+ * The minimum that satisfies the two constraints is given as
+ *   x = [0.12288, -1.1078, 0.015100], with an objective value of about 29.634.
+ */
+class GockenbachFunction {
+ public:
+  GockenbachFunction();
+
+  double Evaluate(const arma::mat& coordinates);
+  void Gradient(const arma::mat& coordinates, arma::mat& gradient);
+
+  int NumConstraints() const { return 2; };
 
   double EvaluateConstraint(int index, const arma::mat& coordinates);
   void GradientConstraint(int index,
