2e400bffd21cc061d40f9c932e1ec0bd5f46367b
Dongryeol Lee <drselee@gmail.com>
2011-04-07 04:23:57 +0000

    Should be able to read one file across multiple MPI processes.

--- fastlib/trunk/contrib/dongryel/thesis_research/core/csv_parser/dataset_reader.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/csv_parser/dataset_reader.h
@@ -13,7 +13,117 @@
 
 namespace core {
 class DatasetReader {
+
+  private:
+
+    template<typename TableType>
+    static void Extract_(
+      const std::string &filename_in,
+      const std::string &filename_out,
+      int num_dimensions_in, int begin, int end) {
+
+      int count = end - begin;
+      TableType extracted_table;
+      extracted_table.Init(num_dimensions_in, count);
+
+      const char *filename = filename_in.c_str();
+      const char field_terminator = ',';
+      const char line_terminator  = '\n';
+      const char enclosure_char   = '"';
+
+      csv_parser file_parser;
+
+      // Specify the number of lines to skip.
+      file_parser.set_skip_lines(0);
+
+      // Specify the file to parse.
+      if(file_parser.init(filename) == false) {
+        return;
+      }
+
+      // Here we tell the parser how to parse the file.
+      file_parser.set_enclosed_char(enclosure_char, ENCLOSURE_OPTIONAL);
+      file_parser.set_field_term_char(field_terminator);
+      file_parser.set_line_term_char(line_terminator);
+
+      int row_index = 0;
+      int num_points = 0;
+      while(file_parser.has_more_rows()) {
+
+        // Get the record
+        csv_row row = file_parser.get_row();
+
+        if(row_index >= begin && row_index < end) {
+          for(unsigned i = 0; i < row.size(); i++) {
+            extracted_table.data().set(i, num_points, atof(row[i].c_str()));
+          }
+          num_points++;
+        }
+        row_index++;
+      }
+      extracted_table.Save(filename_out);
+    }
+
   public:
+
+    template<typename TableType>
+    static void SplitFile(const std::string &filename_in, int num_parts) {
+
+      // Count the number of points in total.
+      int num_dimensions = 0;
+      int total_num_points = CountNumPoints(filename_in, &num_dimensions);
+      int grain_size = total_num_points / num_parts;
+      for(int i = 0; i < num_parts; i++) {
+        int begin = i * grain_size;
+        int end = (i < num_parts - 1) ? (i + 1) * grain_size : total_num_points;
+        std::stringstream filename_out_sstr;
+        filename_out_sstr << filename_in << i;
+        std::string filename_out = filename_out_sstr.str();
+        Extract_<TableType>(
+          filename_in, filename_out, num_dimensions, begin, end);
+      }
+    }
+
+    /** @brief Counts the number of points in the dataset.
+     */
+    static int CountNumPoints(
+      const std::string &filename_in, int *num_dimensions) {
+
+      const char *filename = filename_in.c_str();
+      const char field_terminator = ',';
+      const char line_terminator  = '\n';
+      const char enclosure_char   = '"';
+
+      csv_parser file_parser;
+
+      // Specify the number of lines to skip.
+      file_parser.set_skip_lines(0);
+
+      // Specify the file to parse.
+      if(file_parser.init(filename) == false) {
+        return false;
+      }
+
+      // Here we tell the parser how to parse the file.
+      file_parser.set_enclosed_char(enclosure_char, ENCLOSURE_OPTIONAL);
+
+      file_parser.set_field_term_char(field_terminator);
+
+      file_parser.set_line_term_char(line_terminator);
+
+      int num_points = 0;
+
+      // Check to see if there are more records, then grab each row
+      // one at a time.
+      while(file_parser.has_more_rows()) {
+        // Get the record
+        csv_row row = file_parser.get_row();
+        *num_dimensions = row.size();
+        num_points++;
+      }
+      return num_points;
+    }
+
     static bool ParseDataset(
       const std::string &filename_in, core::table::DenseMatrix *dataset_out) {
 
