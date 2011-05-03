bebd8bc58134c7bdc964f76573b47661dfd0df63
niche <niche@9d5b8971-822b-0410-80eb-d18c1038ef23>
2011-05-03 01:02:15 +0000

    made lcc main.cc concordant with fx_init; we now accept parameters

--- fastlib/branches/fastlib-stl/contrib/niche/local_coordinate_coding/main.cc
+++ fastlib/branches/fastlib-stl/contrib/niche/local_coordinate_coding/main.cc
@@ -14,52 +14,52 @@ using namespace arma;
 using namespace std;
 
 int main(int argc, char* argv[]) {
-
-  double lambda = 0.05;
+  fx_module* root = fx_init(argc, argv, NULL);
   
+  double lambda = fx_param_double_req(NULL, "lambda"); // 0.05
+  // if using fx-run, one could just leave results_dir blank
+  const char* results_dir = fx_param_str(NULL, "results_dir", "");
+  
+  const char* data_fullpath = 
+    fx_param_str_req(NULL, "data_fullpath");
+  const char* initial_dictionary_fullpath = 
+    fx_param_str_req(NULL, "initial_dictionary");
+
+  u32 n_iterations = fx_param_int_req(NULL, "n_iterations");
+
   
   LocalCoordinateCoding lcc;
   
-  /*
-  u32 n_atoms = 100;
-
-  u32 n_dims = 10;
-  u32 n_points = 30;
-
-  mat X = randn(n_dims, n_points);
-  for(u32 i = 0; i < n_points; i++) {
-    X.col(i) /= norm(X.col(i), 2);
-  }
-  */
   mat X;
-  X.load("/home/niche/fastlib-stl/contrib/niche/local_coordinate_coding/X.dat");
+  //X.load("/home/niche/fastlib-stl/contrib/niche/local_coordinate_coding/X.dat");
+  X.load(data_fullpath);
 
-  mat D;
-  D.load("/home/niche/fastlib-stl/contrib/niche/local_coordinate_coding/D.dat");
-  
-  u32 n_atoms = D.n_cols;
+  mat initial_D;
+  //initial_D.load("/home/niche/fastlib-stl/contrib/niche/local_coordinate_coding/D.dat");
+  initial_D.load(initial_dictionary_fullpath);
+  u32 n_atoms = initial_D.n_cols;
   
   lcc.Init(X, n_atoms, lambda);
+  lcc.SetDictionary(initial_D);
   
-  lcc.SetDictionary(D);
-  
-  printf("n_atoms = %d\n", n_atoms);
-  
-  lcc.DoLCC(1);
-  //lcc.PrintDictionary();
-  mat Dic;
-  lcc.GetDictionary(Dic);
-  Dic.save("Dic.dat", raw_ascii);
-  
-  
-  /*
-  
-  u32 n_iterations = 10;
-
-  lcc.InitDictionary();
+  //printf("n_atoms = %d\n", n_atoms);
   
   lcc.DoLCC(n_iterations);
+
+  mat learned_D;
+  lcc.GetDictionary(learned_D);
+
+  if(strlen(results_dir) == 0) {
+    learned_D.save("D.dat", raw_ascii);
+  }
+  else {
+    char* learned_dictionary_fullpath = (char*) malloc(320 * sizeof(char));
+    sprintf(learned_dictionary_fullpath,
+	    "%s/D.dat",
+	    results_dir);
+    learned_D.save(learned_dictionary_fullpath, raw_ascii);
+    free(learned_dictionary_fullpath);
+  }
   
-  */
-  
+  fx_done(root);
 }
