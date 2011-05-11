73bf98d2eceac605e0c6a7068093d4e64dc21b6c
tekhnofiend <nishantmehta.x@gmail.com>
2011-05-11 18:19:55 +0000

    changed lcc implementation so that DoLCC does a final OptimizeCode step so that, at termination, we have a learned dictionary and the correct coding to that dictionary

--- fastlib/branches/fastlib-stl/contrib/niche/local_coordinate_coding/mnist_main.cc
+++ fastlib/branches/fastlib-stl/contrib/niche/local_coordinate_coding/mnist_main.cc
@@ -18,18 +18,20 @@ using namespace std;
 int main(int argc, char* argv[]) {
   fx_module* root = fx_init(argc, argv, NULL);
   
-  double lambda = fx_param_double_req(NULL, "lambda"); // 0.05
+  double lambda = fx_param_double_req(NULL, "lambda");
+  
   // if using fx-run, one could just leave results_dir blank
   const char* results_dir = fx_param_str(NULL, "results_dir", "");
   
   const char* data_dir = 
     fx_param_str_req(NULL, "data_dir");
+  
   //const char* initial_dictionary_fullpath = 
   //  fx_param_str_req(NULL, "initial_dictionary");
   
   u32 digit_1 = fx_param_int_req(NULL, "digit1");
   u32 digit_2 = fx_param_int_req(NULL, "digit2");
-
+  
   u32 n_iterations = fx_param_int_req(NULL, "n_iterations");
   
   u32 n_atoms = fx_param_int_req(NULL, "n_atoms");
@@ -61,9 +63,9 @@ int main(int argc, char* argv[]) {
   for(u32 i = 0; i < n_points; i++) {
     X.col(i) /= norm(X.col(i), 2);
   }
-
-
-
+  
+  
+  
   // run LCC
   
   LocalCoordinateCoding lcc;
@@ -79,7 +81,7 @@ int main(int argc, char* argv[]) {
   //lcc.SetDictionary(initial_D);
   
   //printf("n_atoms = %d\n", n_atoms);
-
+  
   wall_clock timer;
   timer.tic();
   lcc.DoLCC(n_iterations);
@@ -88,17 +90,24 @@ int main(int argc, char* argv[]) {
   
   mat learned_D;
   lcc.GetDictionary(learned_D);
-
+  
+  mat learned_V;
+  lcc.GetCoding(learned_V);
+  
   if(strlen(results_dir) == 0) {
     learned_D.save("D.dat", raw_ascii);
+    learned_V.save("V.dat", raw_ascii);
   }
   else {
-    char* learned_dictionary_fullpath = (char*) malloc(320 * sizeof(char));
-    sprintf(learned_dictionary_fullpath,
-	    "%s/D.dat",
-	    results_dir);
-    learned_D.save(learned_dictionary_fullpath, raw_ascii);
-    free(learned_dictionary_fullpath);
+    char* data_fullpath = (char*) malloc(320 * sizeof(char));
+
+    sprintf(data_fullpath, "%s/D.dat", results_dir);
+    learned_D.save(data_fullpath, raw_ascii);
+    
+    sprintf(data_fullpath, "%s/V.dat", results_dir);
+    learned_V.save(data_fullpath, raw_ascii);
+    
+    free(data_fullpath);
   }
   
   fx_done(root);
