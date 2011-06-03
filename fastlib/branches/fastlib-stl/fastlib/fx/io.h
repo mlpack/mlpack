5d795aa84006a3842cab829af234d335a2c49d3c
Ryan Curtin <ryan@ratml.org>
2011-06-03 22:57:48 +0000

    Fix odd formatting issues in io.h

--- fastlib/branches/fastlib-stl/fastlib/fx/io.h
+++ fastlib/branches/fastlib-stl/fastlib/fx/io.h
@@ -100,8 +100,8 @@ namespace po = boost::program_options;
 namespace mlpack {
 
 class IO {
-  public:
-   /*
+ public:
+  /*
    * Adds a parameter to the heirarchy. Use char* and not 
    * std::string since the vast majority of use cases will 
    * be literal strings.
@@ -112,12 +112,12 @@ class IO {
    * @param parent Full pathname of a parent module, default is root node.
    * @param required Indicates if parameter must be set on command line.
    */
-   static void Add(const char* identifier, 
-                   const char* description, 
-                   const char* parent=NULL, 
-                   bool required = false);
+  static void Add(const char* identifier, 
+                  const char* description, 
+                  const char* parent=NULL, 
+                  bool required = false);
     
-   /*
+  /*
    * Adds a parameter to the heirarchy. Use char* and not 
    * std::string since the vast majority of use cases will 
    * be literal strings.
@@ -128,18 +128,18 @@ class IO {
    * @param parent Full pathname of a parent module, default is root node.
    * @param required Indicates if parameter must be set on command line.
    */
-   template<class T>
-   static void Add(const char* identifier, 
-                   const char* description, 
-                   const char* parent, 
-                   bool required=false); 
+  template<class T>
+  static void Add(const char* identifier, 
+                  const char* description, 
+                  const char* parent, 
+                  bool required=false); 
                    
-   /* 
+  /* 
    * See if the specified flag was found while parsing. 
    * 
    * @param identifier The name of the parameter in question. 
    */
-   static bool CheckValue(const char* identifier);
+  static bool CheckValue(const char* identifier);
       
       
   /* 
@@ -147,57 +147,57 @@ class IO {
    * If found, will print out the appropriate information
    * and kill the program.
    */
-   static void DefaultMessages();
+  static void DefaultMessages();
    
-   /*
+  /*
    * Grab the value of type T found while parsing.  
    * Should use checkValue first.
    *
    * @param argc The number of arguments on the commandline.
    * @param argv The array of arguments as strings 
    */
-   template<typename T>
-   static T& GetValue(const char* identifier); 
+  template<typename T>
+  static T& GetValue(const char* identifier); 
    
-   /*
+  /*
    * Grab the description of the specified node.
    * 
    * @param identifier Name of the node in question.
    * @return Description of the node in question. 
    */
-   static std::string GetDescription(const char* identifier);
+  static std::string GetDescription(const char* identifier);
 
-   /*
+  /*
    * Parses the commandline for arguments.
    *
    * @param argc The number of arguments on the commandline.
    * @param argv The array of arguments as strings 
    */
-   static void ParseCommandLine(int argc, char** argv);
+  static void ParseCommandLine(int argc, char** argv);
     
-   /*
+  /*
    * Parses a stream for arguments
    *
    * @param stream The stream to be parsed.
    */
-   static void ParseStream(std::istream& stream);
+  static void ParseStream(std::istream& stream);
       
-   /* Prints out the current heirachy */
-   static void Print();
+  /* Prints out the current heirachy */
+  static void Print();
 
 // We only use PrefixedOutStream if the program is compiled with debug symbols.
 #ifdef DEBUG
-//Prints debug output with the appropriate tag.
-   static io::PrefixedOutStream Debug;
+  // Prints debug output with the appropriate tag.
+  static io::PrefixedOutStream Debug;
 #else
-//Dumps debug output into the bit nether regions.
-   static io::NullOutStream Debug;
+  // Dumps debug output into the bit nether regions.
+  static io::NullOutStream Debug;
 #endif
-//Prints output with their respective tags of [INFO], [WARN], and [FATAL]
-   static io::PrefixedOutStream Info;
-   static io::PrefixedOutStream Warn;
-   static io::PrefixedOutStream Fatal;
-   static std::ostream& cout; 
+  // Prints output with their respective tags of [INFO], [WARN], and [FATAL]
+  static io::PrefixedOutStream Info;
+  static io::PrefixedOutStream Warn;
+  static io::PrefixedOutStream Fatal;
+  static std::ostream& cout; 
    
   /*
    * Checks that all parameters specified as required 
@@ -205,57 +205,58 @@ class IO {
    * If they havent, prints an error message and kills the
    * program.
    */
-   static void RequiredOptions();
+  static void RequiredOptions();
 
-   /* Cleans up input pathnames, rendering strings such as /foo/bar
-      and foo/bar/ equivalent inputs */
-   static std::string SanitizeString(const char* str);
+  /* Cleans up input pathnames, rendering strings such as /foo/bar
+     and foo/bar/ equivalent inputs */
+  static std::string SanitizeString(const char* str);
  
-   /*
+  /*
    * Initializes a timer, available like a normal value specified on 
    * the command line.  Timers are of type timval
    *
    * @param timerName The name of the timer in question.
    */
-   static void StartTimer(const char* timerName);
+  static void StartTimer(const char* timerName);
      
-   /* 
+  /* 
    * Halts the timer, and replaces it's value with 
    * the delta time from it's start 
    *
    * @param timerName The name of the timer in question.
    */
-   static void StopTimer(const char* timerName);
+  static void StopTimer(const char* timerName);
    
   /*
    * Parses the values given on the command line,
    * overriding any default values.
    */
-   static void UpdateGmap();
+  static void UpdateGmap();
 
-   //Destructor
-   ~IO();
-  private:
-   //The documentation and names of options
-   po::options_description desc;
+  // Destructor
+  ~IO();
+  
+ private:
+  // The documentation and names of options
+  po::options_description desc;
     
-   //Values of the options given by user
-   po::variables_map vmap;
+  // Values of the options given by user
+  po::variables_map vmap;
+
+  // Pathnames of required options
+  std::list<std::string> requiredOptions;
     
-   //Pathnames of required options
-   std::list<std::string> requiredOptions;
-    
-   //Map of global values, stored here instead of in OptionsHierarchy
-   //For ease of implementation
-   std::map<std::string, boost::any> globalValues;
+  // Map of global values, stored here instead of in OptionsHierarchy
+  // For ease of implementation
+  std::map<std::string, boost::any> globalValues;
       
-   //Store a relative index of path names
-   io::OptionsHierarchy hierarchy;
+  // Store a relative index of path names
+  io::OptionsHierarchy hierarchy;
       
-   //The singleton, obviously
-   static IO* singleton;
+  // The singleton, obviously
+  static IO* singleton;
     
-   /* 
+  /* 
    * Not exposed to the outside, so as to spare users some ungainly
    * x.GetSingleton().foo() syntax.
    * In this case, the singleton is used to store data for the static methods, 
@@ -264,39 +265,39 @@ class IO {
    *
    * Returns the singleton instance for use in the static methods 
    */
-   static IO& GetSingleton();
+  static IO& GetSingleton();
    
-    /* 
-    * Properly formats strings such that there aren't too few or too many '/'s.
-    * 
-    * @param id The name of the parameter, eg bar in foo/bar.
-    * @param parent The full name of the parameter's parent, 
-    *   eg foo/bar in foo/bar/buzz.
-    * @param tname String identifier of the parameter's type.
-    * @param description String description of the parameter.
-    */
+  /* 
+   * Properly formats strings such that there aren't too few or too many '/'s.
+   * 
+   * @param id The name of the parameter, eg bar in foo/bar.
+   * @param parent The full name of the parameter's parent, 
+   *   eg foo/bar in foo/bar/buzz.
+   * @param tname String identifier of the parameter's type.
+   * @param description String description of the parameter.
+   */
   std::string ManageHierarchy(const char* id, 
-                               const char* parent, 
-                               std::string& tname, 
-                               const char* description = "");
+                              const char* parent, 
+                              std::string& tname, 
+                              const char* description = "");
     
-   /* Make the constructor private, to preclude unauthorized instances */
-   IO();
+  /* Make the constructor private, to preclude unauthorized instances */
+  IO();
    
-   /* 
+  /* 
    * Initialize desc with a particular name.
    * 
    * @param optionsName Name of the module, as far as boost is concerned.
    */ 
-   IO(std::string& optionsName);
+  IO(std::string& optionsName);
 
-   //Private copy constructor; don't want copies floating around.
-   IO(const IO& other);
+  // Private copy constructor; don't want copies floating around.
+  IO(const IO& other);
 };
 
-
-//Include the actual definitions of templated methods 
+// Include the actual definitions of templated methods 
 #include "io_impl.h"
-};
+
+}; // namespace mlpack
 
 #endif
