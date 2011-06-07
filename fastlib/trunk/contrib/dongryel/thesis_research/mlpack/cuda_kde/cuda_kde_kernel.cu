a9556926772dda3a2a5e12e89b6e962264643c67
Dongryeol Lee <drselee@gmail.com>
2011-06-07 01:52:58 +0000

    Finally linking CUDA, but still in progress for naive GPU KDE.

--- fastlib/trunk/contrib/dongryel/thesis_research/mlpack/cuda_kde/cuda_kde_kernel.cu
+++ fastlib/trunk/contrib/dongryel/thesis_research/mlpack/cuda_kde/cuda_kde_kernel.cu
@@ -1,4 +1,4 @@
-/** @file cuda_kde.cu
+/** @file cuda_kde_kernel.cu
  *
  *  CUDA implementation of KDE.
  *
@@ -7,6 +7,23 @@
 
 #include <cuda.h>
 
-__global__ void NbodyKernel() {
+__global__ void NbodyKernelOnDevice(
+  float *query, int num_query_points,
+  float *reference, int num_reference_points) {
 
+  // The shared memory that is used to load a list of reference
+  // points.
+  __shared__ float reference_point_shared_mem[1024];
+
+  int i = 3;
+}
+
+extern "C" {
+
+  void NbodyKernelOnHost(
+    float *query, int num_query_points,
+    float *reference, int num_reference_points) {
+    NbodyKernelOnDevice <<< 1, 1>>>(
+      query, num_query_points, reference, num_reference_points);
+  }
 }
