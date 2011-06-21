d292979c46d294b07c4bf1953b3fda96895a8bec
Dongryeol Lee <drselee@gmail.com>
2011-06-21 19:32:40 +0000

    Finished adaptive randomized range finder, now have to test it.

--- fastlib/trunk/contrib/dongryel/thesis_research/core/optimization/rand_range_finder.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/optimization/rand_range_finder.h
@@ -18,44 +18,44 @@ namespace optimization {
 class RandRangeFinder {
   private:
 
+    static void ComputeResidual_(
+      const std::vector< arma::vec *> &collected_basis_set,
+      arma::vec *residual_out) {
+
+      for(unsigned int i = 0; i < collected_basis_set.size(); i++) {
+
+        // Project onto the basis and subtract.
+        double dot_product =
+          arma::dot(*(collected_basis_set[i]), (*residual_out)) ;
+        (*residual_out) -= dot_product * (* (collected_basis_set[i]));
+      }
+    }
+
     /** @brief Projects the data matrix onto a random set of Gaussian
      *         noise vectors.
      */
+    template<typename MatrixType>
     static void ApplyGaussianNoise_(
-      const arma::mat &input, int rank,
-      core::table::CyclicArmaMat *output) {
+      const arma::mat &input, int rank, MatrixType *output) {
 
-      output->zeros(input.n_rows, rank);
       for(unsigned int j = 0; j < input.n_cols; j++) {
-        for(unsigned int i = 0; i < input.n_rows; i++) {
-          for(int k = 0; k < rank; k++) {
+        for(int k = 0; k < rank; k++) {
 
-            // Generate a standard Gaussian random number.
-            double rand_gaussian = core::math::RandGaussian(1.0);
+          // Generate a standard Gaussian random number.
+          double rand_gaussian = core::math::RandGaussian(1.0);
+          for(unsigned int i = 0; i < input.n_rows; i++) {
             output->at(i, k) += rand_gaussian * input.at(i, j);
           }
         }
       }
     }
 
-    static void ApplyGaussianNoise_(
-      const arma::mat &matrix, arma::vec *output) {
-
-      output->zeros(input.n_rows);
-      for(unsigned int j = 0; j < input.n_cols; j++) {
-
-        // Generate a standard Gaussian random number.
-        double rand_gaussian = core::math::RandGaussian(1.0);
-
-        // Add the data point weighted by the Gaussian noise.
-        (*output) += rand_gaussian * matrix.col(j);
-      }
-    }
-
-    static double MaxL2Norm_(const arma::mat &matrix) {
+    static double MaxL2Norm_(const core::table::CyclicArmaMat &matrix) {
       double max_l2_norm = 0.0;
       for(unsigned int i = 0; i < matrix.n_cols; i++) {
-        max_l2_norm = std::max(max_l2_norm, 2);
+        arma::vec matrix_column;
+        matrix.col(i, &matrix_column);
+        max_l2_norm = std::max(max_l2_norm, arma::norm(matrix_column, 2));
       }
       return max_l2_norm;
     }
@@ -81,12 +81,14 @@ class RandRangeFinder {
         rank = 1;
       }
       core::table::CyclicArmaMat candidate_basis_set;
+      candidate_basis_set.zeros(input.n_rows, rank);
       ApplyGaussianNoise_(input, rank, &candidate_basis_set);
-      double max_l2_norm = MaxL2Norm_(output);
+      double max_l2_norm = MaxL2Norm_(candidate_basis_set);
       double threshold = epsilon / (10.0 * sqrt(2.0 / arma::math::pi()));
 
       // Loop while the maximum norm is above the threshold.
       std::vector< arma::vec * > collected_basis_set;
+      arma::vec candidate_vector;
       while(max_l2_norm > threshold) {
 
         // Get the first vector in the list.
@@ -95,9 +97,35 @@ class RandRangeFinder {
         ComputeResidual_(collected_basis_set, &first_vector);
         arma::vec *normalized_residual = new arma::vec();
         *normalized_residual = first_vector / arma::norm(first_vector, 2);
+
+        // Grow the basis set.
         collected_basis_set->push_back(normalized_residual);
 
+        // Draw a new candidate vector.
+        candidate_basis_set.col(0, &candidate_vector);
+        ApplyGaussianNoise_(input, 1, &candidate_vector);
+        ComputeResidual_(collected_basis_set, &candidate_vector);
+        for(int i = 1; i < rank; i++) {
+          double dot_product =
+            arma::dot(
+              *normalized_residual, candidate_basis_set.col(i));
+          candidate_basis_set.col(i) =
+            candidate_basis_set.col(i) - dot_product * (*normalized_residual);
+        }
+
+        // Shift the starting index by one.
+        candidate_basis_set.ShiftStartingIndex();
+
+        // Update the max L2 norm.
+        max_l2_norm = MaxL2Norm_(candidate_basis_set);
+
       } // end of the while-loop.
+
+      // Copy out the basis set.
+      output->zeros(input.n_rows, collected_basis_set.size());
+      for(unsigned int i = 0; i < collected_basis_set.size(); i++) {
+        output->col(i) = * (collected_basis_set[i]) ;
+      }
     }
 };
 }
