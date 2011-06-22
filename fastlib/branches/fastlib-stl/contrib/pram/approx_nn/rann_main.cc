0404468f8d88e8a63b8d7a67796abe3bbed2d0d7
Parikshit Ram <p.ram@gatech.edu>
2011-06-22 21:28:38 +0000

    rank-approximate NN files added to new branch

--- fastlib/branches/fastlib-stl/contrib/pram/approx_nn/rann_main.cc
+++ fastlib/branches/fastlib-stl/contrib/pram/approx_nn/rann_main.cc
@@ -19,21 +19,23 @@ const fx_entry_doc approx_nn_main_dual_entries[] = {
   {"doexact", FX_PARAM, FX_BOOL, NULL,
    " A variable which decides whether we do"
    " the exact computation"
-   "(defaults to true).\n"},
+   "(defaults to false).\n"},
   {"doapprox", FX_PARAM, FX_BOOL, NULL,
    " A variable which decides whether we do"
    " the approximate computation(defaults to true).\n"},
   {"compute_error", FX_PARAM, FX_BOOL, NULL,
    " Whether to compute the rank and distance error"
    " for the approximate results.\n"},
+  {"rank_matrix", FX_PARAM, FX_STR, NULL,
+   " The file containing the rank of the reference points"
+   " with respect to the set of queries. Required only "
+   "for computing the true rank errors and failure "
+   "probabilities.\n"},
   {"result_file", FX_PARAM, FX_STR, NULL,
    " The file in which the nearest neighbor results are to be output"
    " (defaults to 'results.txt')\n"},
   {"print_results", FX_PARAM, FX_BOOL, NULL,
    " Whether to print out results or not\n"},
-  {"label_comp", FX_PARAM, FX_BOOL, NULL,
-   " Whether to compare the labels of"
-   " the nearest neighbor and the query point.\n"},
   FX_ENTRY_DOC_DONE
 };
 
@@ -51,22 +53,6 @@ const fx_module_doc approx_nn_main_dual_doc = {
   "It performs the exact, approximate"
   " and the naive computation.\n"
 };
-
-/**
- * This function checks if the neighbors computed 
- * by two different methods is the same.
- */
-void compare_neighbors(ArrayList<index_t>*, ArrayList<double>*, 
-                       ArrayList<index_t>*, ArrayList<double>*);
-
-void count_mismatched_neighbors(ArrayList<index_t>*,
-				ArrayList<double>*, 
-				ArrayList<index_t>*,
-				ArrayList<double>*);
-
-void find_rank_dist(Matrix&, Matrix&, ArrayList<index_t>*,
-		    ArrayList<double>*, ArrayList<index_t>*,
-		    ArrayList<double>*);
 
 void compute_classification_error(Matrix& lrdata, Matrix& lqdata,
 				  ArrayList<index_t>& neighbors,
@@ -148,11 +134,13 @@ int main (int argc, char *argv[]) {
 	FATAL("Error while opening %s...%s", result_file.c_str(),
 	      strerror(errno));
 
-      for(index_t i=0 ; i < nac.size()/knns ; i++)
+      for(index_t i=0 ; i < nac.size()/knns ; i++) {
+	fprintf(fp, "%"LI"d", i);
 	for(index_t j=0; j<knns; j++)
-	  fprintf(fp, "%"LI"d %"LI"d %lg\n", 
-		  i, nac[i*knns+j], din[i*knns+j]);
-
+	  fprintf(fp, ",%"LI"d,%lg", 
+		  nac[i*knns+j], din[i*knns+j]);
+	fprintf(fp, "\n");
+      }
       fclose(fp);
     }
   }
@@ -186,7 +174,8 @@ int main (int argc, char *argv[]) {
       for(index_t i=0 ; i < exc.size()/knns ; i++) {
 	fprintf(fp, "%"LI"d", i);
 	for(index_t j=0; j<knns; j++)
-	  fprintf(fp, ",%"LI"d", exc[i*knns+j]);
+	  fprintf(fp, ",%"LI"d,%lg",
+		  exc[i*knns+j], die[i*knns+j]);
 	fprintf(fp, "\n");
       }
       fclose(fp);
@@ -218,28 +207,32 @@ int main (int argc, char *argv[]) {
       fx_timer_stop(ann_module, "approx_init");
     }
 
+    // exit(0);
     NOTIFY("Compute");
     fx_timer_start(ann_module, "approx");
     approx_nn.ComputeApprox(&apc, &dia);
     fx_timer_stop(ann_module, "approx");
 
-    if (fx_param_bool(root, "print_results", false)) {
+    if (fx_param_bool(root, "print_results", true)) {
       FILE *fp=fopen(result_file.c_str(), "w");
       if (fp==NULL)
 	FATAL("Error while opening %s...%s", result_file.c_str(),
 	      strerror(errno));
       
-      for(index_t i=0 ; i < apc.size()/knns ; i++)
+      for(index_t i=0 ; i < apc.size()/knns ; i++) {
+	fprintf(fp, "%"LI"d", i);
 	for(index_t j=0; j<knns; j++)
-	  fprintf(fp, "%"LI"d %"LI"d %lg\n", 
-		  i, apc[i*knns+j], dia[i*knns+j]);
-      
+	  fprintf(fp, ",%"LI"d,%lg", 
+		  apc[i*knns+j], dia[i*knns+j]);
+	fprintf(fp, "\n");
+      }
       fclose(fp);
     }
 
     /**
      * computing the kNN classification error 
-     * if the labels are provided */ 
+     * if the labels are provided 
+     */ 
     if (fx_param_exists(root, "lr")) {
       FILE *fout = fopen(fx_param_str(root, "lab_res_file",
 				      "lab_res_file.txt"),"w");
@@ -247,6 +240,11 @@ int main (int argc, char *argv[]) {
       fclose(fout);
     }
 
+
+    /**
+     * Computing the error of the approximate neighbors
+     * Must provide the file containing the rank matrix 
+     */
     if (fx_param_bool(root, "compute_error", false)) {
       GenMatrix<index_t> rank_matrix;
       Matrix temp;
@@ -322,15 +320,15 @@ int main (int argc, char *argv[]) {
       index_t max_er = 0, min_er = rdata.n_cols();
       for (index_t i = 0; i < apc.size() / knns; i++) {
 	if (rank_matrix.get(apc[(i+1)*knns -1], i) > max_er)
-	  max_er = rank_matrix.get(apc[(i+1)*knns -1], i);
+	  max_er = rank_matrix.get(apc[(i+1)*knns -1], i) -1;
 	
 	if (rank_matrix.get(apc[(i+1)*knns -1], i) < min_er)
-	  min_er = rank_matrix.get(apc[(i+1)*knns -1], i);
+	  min_er = rank_matrix.get(apc[(i+1)*knns -1], i) -1;
 					
-	if (rank_matrix.get(apc[(i+1)*knns -1], i) > rank_error)
+	if (rank_matrix.get(apc[(i+1)*knns -1], i) -1 > rank_error)
 	  failed++;
 					
-	re += rank_matrix.get(apc[(i+1)*knns -1], i);
+	re += rank_matrix.get(apc[(i+1)*knns -1], i) -1;
       }
       double avg_rank = (double) re / (double) qdata.n_cols();
       double success_prob = (double) (qdata.n_cols() - failed)
@@ -347,193 +345,11 @@ int main (int argc, char *argv[]) {
       NOTIFY("Max error: %"LI"d,"
 	     " Min error: %"LI"d",
 	     max_er, min_er);
-    }
-  }
+    } // compute_error
+  } // doapprox
 
-  
-  // Approximate computation without a tree
-  if (fx_param_bool(root, "doapprox_nt", false)) {
-    ApproxNN approx_nt_nn;
-    NOTIFY("Approx No Tree");
-    NOTIFY("Init");
-    if (fx_param_exists(root, "q")) {
-      fx_timer_start(ann_module, "approx_nt_init");
-      approx_nt_nn.InitApprox(qdata, rdata, ann_module);
-      fx_timer_stop(ann_module, "approx_nt_init");
-    } else {
-      fx_timer_start(ann_module, "approx_nt_init");
-      approx_nt_nn.InitApprox(rdata, ann_module);
-      fx_timer_stop(ann_module, "approx_nt_init");
-    }
-    
-    NOTIFY("Compute");
-    fx_timer_start(ann_module, "approx_nt");
-    approx_nt_nn.ComputeApproxNoTree(&ap_nt_c, &di_nt_a);
-    fx_timer_stop(ann_module, "approx_nt");
-    
-    if (fx_param_bool(root, "print_results", false)) {
-      FILE *fp=fopen(result_file.c_str(), "w");
-      if (fp==NULL)
-	FATAL("Error while opening %s...%s", result_file.c_str(),
-	      strerror(errno));
-      
-      for(index_t i=0 ; i < ap_nt_c.size()/knns ; i++)
-	for(index_t j=0; j<knns; j++)
-	  fprintf(fp, "%"LI"d %"LI"d %lg\n", 
-		  i, ap_nt_c[i*knns+j], di_nt_a[i*knns+j]);
-      
-      fclose(fp);
-    }
-		
-    if (fx_param_bool(root, "compute_error", false)) {
-      double epsilon
-	= fx_param_double_req(ann_module, "epsilon");
-      index_t rank_error
-	= (index_t) (epsilon * (double) rdata.n_cols() / 100);
-      double alpha
-	= fx_param_double_req(ann_module, "alpha");
-      
-      GenMatrix<index_t> rank_matrix;
-      Matrix temp;
-      bool ranks_given = false;
-      if (fx_param_exists(root, "rank_matrix")) {
-	ranks_given = true;
-	std::string rank_file = fx_param_str_req(root, "rank_matrix");
-	NOTIFY("Loading rank file...huge file will take time");
-	data::Load(rank_file.c_str(), &temp);
-				
-	rank_matrix.Init(temp.n_rows(), temp.n_cols());
-	
-	for (index_t i = 0; i < temp.n_cols(); i++)
-	  for (index_t j = 0; j < temp.n_rows(); j++)
-	    rank_matrix.set(j, i, (index_t) temp.get(j, i));
-
-	NOTIFY("Done loading Rank file");
-      } else {
-	temp.Init(0,0);
-	rank_matrix.Init(0,0);
-      }
-
-      // computing average rank error
-      // and probability of failure
-      index_t re = 0, failed = 0;
-      
-      index_t max_er = 0, min_er = rdata.n_cols();
-      
-      for (index_t i = 0; i < ap_nt_c.size() / knns; i++) {
-	if (rank_matrix.get(ap_nt_c[(i+1)*knns -1], i) > max_er)
-	  max_er = rank_matrix.get(ap_nt_c[(i+1)*knns -1], i);
-					
-	if (rank_matrix.get(ap_nt_c[(i+1)*knns -1], i) < min_er)
-	  min_er = rank_matrix.get(ap_nt_c[(i+1)*knns -1], i);
-	
-	if (rank_matrix.get(ap_nt_c[(i+1)*knns -1], i) > rank_error)
-	  failed++;
-					
-	re += rank_matrix.get(ap_nt_c[(i+1)*knns -1], i);
-      }
-			
-      double avg_rank = (double) re / (double) qdata.n_cols();
-      double success_prob = (double) (qdata.n_cols() - failed)
-	/ (double) qdata.n_cols();
-
-      NOTIFY("Required rank error: %"LI"d,"
-	     " Required success Prob = %1.2lf",
-	     rank_error, alpha);
-      
-      NOTIFY("True Avg k Rank error: %6.2lf,"
-	     " True success prob = %1.2lf,",
-	     avg_rank, success_prob);
-
-      NOTIFY("Max error: %"LI"d,"
-	     " Min error: %"LI"d",
-	     max_er, min_er);
-    }
-  }
-
-  //  count_mismatched_neighbors(&exc, &die, &apc, &dia);
   fx_done(fx_root);
-}
-
-void compare_neighbors(ArrayList<index_t> *a, ArrayList<double> *da,
-		       ArrayList<index_t> *b, ArrayList<double> *db) {
-  NOTIFY("Comparing results for %"LI"d queries", a->size());
-  DEBUG_SAME_SIZE(a->size(), b->size());
-  index_t *x = a->begin();
-  index_t *y = a->end();
-  index_t *z = b->begin();
-  for(index_t i = 0; x != y; x++, z++, i++)
-    DEBUG_WARN_MSG_IF(*x != *z || (*da)[i] != (*db)[i], 
-                      "point %"LI"d brute: %"LI"d:%lf"
-		      " fast: %"LI"d:%lf",
-		      i, *z, (*db)[i], *x, (*da)[i]);
-}
-
-void count_mismatched_neighbors(ArrayList<index_t> *a, 
-				ArrayList<double> *da,
-				ArrayList<index_t> *b, 
-				ArrayList<double> *db) {
-  NOTIFY("Comparing results for %"LI"d queries", a->size());
-  DEBUG_SAME_SIZE(a->size(), b->size());
-  index_t *x = a->begin();
-  index_t *y = a->end();
-  index_t *z = b->begin();
-  index_t count_mismatched = 0;
-  for(index_t i = 0; x != y; x++, z++, i++)
-    if (*x != *z || (*da)[i] != (*db)[i])
-      ++count_mismatched;
-  NOTIFY("%"LI"d/%"LI"d errors", count_mismatched, a->size());
-}
-
-void find_rank_dist(Matrix &query, Matrix &reference,
-                    ArrayList<index_t> *in,
-		    ArrayList<double> *dist,
-		    ArrayList<index_t> *rank_error,
-		    ArrayList<double> *true_nn_dist) {
-  DEBUG_SAME_SIZE(in->size(), dist->size());
-  DEBUG_SAME_SIZE(in->size(), query.n_cols());
-  // initialize the rank and dist error vector
-  rank_error->Init(in->size());
-  true_nn_dist->Init(in->size());
-
-  // Looping over the queries
-  for (index_t i = 0; i < query.n_cols(); i++) {
-    Vector q, nn_r;
-    query.MakeColumnVector(i, &q);
-    reference.MakeColumnVector((*in)[i], &nn_r);
-
-    double test_dist
-      = la::DistanceSqEuclidean(q, nn_r);
-
-    double present_dist = (*dist)[i],
-      best_dist = test_dist;
-
-    DEBUG_ASSERT(test_dist == present_dist);
-
-    index_t rank = 0;
-
-    // Looping over the references
-    for (index_t j = 0; j < reference.n_cols(); j++) {
-
-      Vector r;
-      reference.MakeColumnVector(j, &r);
-
-      double this_dist = la::DistanceSqEuclidean(q, r);
-      if (this_dist < test_dist)
-	rank++;
-      
-      if (this_dist < best_dist)
-	best_dist = this_dist;
-      
-    }
-
-    // The actual rank error
-    (*rank_error)[i] = rank;
-
-    // The true nearest neighbor dist
-    (*true_nn_dist)[i] = best_dist;
-  }
-}
+} // end main
 
 
 /**
@@ -592,11 +408,3 @@ void compute_classification_error(Matrix& lrdata, Matrix& lqdata,
 	 error, lqdata.n_cols(), no_maj);
   fclose(fout);
 }
-
-
-/*
-  Things to try:
-  * try removing the error distribution strategy and check times
-  and average error and success probability
-
- */
