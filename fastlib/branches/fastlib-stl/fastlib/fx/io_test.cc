7e2f4d85eb040472a5e34ea47affd063ae1e97fa
nkauffman3 <nkauffman3@9d5b8971-822b-0410-80eb-d18c1038ef23>
2011-06-23 00:53:27 +0000

    --Conversion to Boost Unit Test Framework

--- fastlib/branches/fastlib-stl/fastlib/fx/io_test.cc
+++ fastlib/branches/fastlib-stl/fastlib/fx/io_test.cc
@@ -1,6 +1,5 @@
-#include "io.h"
 #include "optionshierarchy.h"
-
+#include "io.h"
 #include <string>
 #include <iostream>
 #include <sys/time.h>
@@ -8,11 +7,14 @@
 
 #define DEFAULT_INT 42
 
+#define BOOST_TEST_MODULE Something
+#include <boost/test/unit_test.hpp>
 
-//
-namespace mlpack {
-namespace io {
 
+using namespace mlpack;
+
+
+/*
 PROGRAM_INFO("MLPACK IO Test",
   "This is a simple test of the IO framework for input options and timers.  "
   "This particular text can be seen if you type --help.")
@@ -24,39 +26,51 @@ PARAM_INT("something_long_long_long", "A particularly long and needlessly "
   "but to test we need a "
   "really_really_really_long_long_long_long_long_long_long_long_word.", "",
   10);
-
-bool ASSERT(bool expression, const char* msg);
-void TestAll();
-bool TestHierarchy();
-bool TestIO();
-bool TestOption();
-
+*/
 
 /**
-* @brief Runs all the other tests, printing output as appropriate.
+* @brief Tests that inserting elements into an OptionsHierarchy
+*   properly updates the tree.
+*
+* @return True indicating all is well with OptionsHierarchy
 */
-void TestAll() {  
-  IO::StartTimer("TestTimer");
-  if (TestIO())
-    IO::Info << "Test IO Succeeded." << std::endl;
-  else
-    IO::Fatal << "Test IO Failed." << std::endl;
 
-  if (TestHierarchy())
-    IO::Info << "Test Hierarchy Passed." << std::endl;
-  else
-    IO::Fatal << "Test Hierarchy Failed." << std::endl;
+namespace mlpack {
+namespace io {
 
-  if (TestOption())
-    IO::Info << "Test Option Passed." << std::endl;
-  else
-    IO::Fatal << "Test Option Failed." << std::endl;
-  IO::StopTimer("TestTimer");
+BOOST_AUTO_TEST_CASE(TestHierarchy) {
+  OptionsHierarchy tmp = OptionsHierarchy("UTest");
 
-  IO::Info << "Elapsed uSecs: " << IO::GetParam<timeval>("TestTimer").tv_usec 
-            << std::endl;
+  std::string testName = std::string("UTest/test");
+  std::string testDesc = std::string("Test description.");
+  std::string testTID = TYPENAME(int);
+
+  //Check that the hierarchy is properly named.
+  std::string str = std::string("UTest");
+  OptionsData node = tmp.GetNodeData();
+  
+  BOOST_REQUIRE_EQUAL(str.compare(node.node), 0);
+
+  //Check that inserting a node actually inserts the node.
+  // Note, that since all versions of append simply call the most qualified
+  //    overload, we will only test that one. 
+  tmp.AppendNode(testName, testTID, testDesc);  
+  BOOST_REQUIRE(tmp.FindNode(testName) != NULL);
+
+  //Now check that the inserted node has the correct data.
+  OptionsHierarchy* testHierarchy = tmp.FindNode(testName);
+  OptionsData testData;
+  if (testHierarchy != NULL) {
+    node = testHierarchy->GetNodeData();
+  
+    BOOST_REQUIRE(testName.compare(node.node) == 0);
+    BOOST_REQUIRE(testDesc.compare(node.desc) == 0);
+    BOOST_REQUIRE(testTID.compare(node.tname) == 0);
+
+  } else {}
 }
-
+}
+}
 
 /**
 * @brief Tests that IO works as intended, namely that IO::Add
@@ -64,33 +78,28 @@ void TestAll() {
 *
 * @return True indicating all is well with IO, false otherwise.
 */
-bool TestIO() {
-  bool success = true;
-  success = success & ASSERT(IO::GetParam<int>("global/gint") == DEFAULT_INT,
-                    "IO::GetParam failed on gint");
-   
+BOOST_AUTO_TEST_CASE(TestIO) {
+  //  BOOST_REQUIRE_CLOSE(IO::GetParam<int>("global/gint") + 1e-6,
+  //   DEFAULT_INT + 1e-6, 1e-5); 
   
   //Check that the IO::HasParam returns false if no value has been specified
   //On the commandline or programmatically.
   IO::Add<bool>("bool", "True or False", "global");
-  success = success & ASSERT(IO::HasParam("global/bool") == false, 
-                    "IO::HasParam failed on global/bool");
+
+  BOOST_REQUIRE_EQUAL(IO::HasParam("global/bool"), false);
+
 
   IO::GetParam<bool>("global/bool") = true;
 
   //IO::HasParam should return true now.
-  success = success & ASSERT(IO::HasParam("global/bool") == true, 
-                              "IO::HasParam failed on global/bool #2");
-  success = success & ASSERT(IO::GetParam<bool>("global/bool") == true, 
-                    "IO::GetParam failed on global/bool");
-  success = success & ASSERT(IO::GetDescription("global/bool").compare(
-                              std::string("True or False")) == 0, 
-                            "IO::GetDescription failed on global/bool");
+  BOOST_REQUIRE_EQUAL(IO::HasParam("global/bool"), true);
+
+  BOOST_REQUIRE_EQUAL(IO::GetDescription("global/bool").compare(
+                              std::string("True or False")) , 0);
 
   //Check that SanitizeString is sanitary. 
   std::string tmp = IO::SanitizeString("/foo/bar/fizz");
-  success = success & ASSERT(tmp.compare(std::string("foo/bar/fizz/")) == 0, 
-                              "IO::SanitizeString failed on 'foo/bar/fizz'");
+  BOOST_REQUIRE_EQUAL(tmp.compare(std::string("foo/bar/fizz/")),0);
 
   //Now lets test the output functions.  Will have to eyeball it manually.
   IO::Debug << "Test the new lines...";
@@ -100,103 +109,23 @@ bool TestIO() {
   //Test IO::Debug 
   IO::Debug << "You shouldn't see this when DEBUG=OFF" << std::endl;
 
-
-  return success;
 }
 
-
 /**
-* @brief Tests that inserting elements into an OptionsHierarchy
-*   properly updates the tree.
-*
-* @return True indicating all is well with OptionsHierarchy
-*/
-bool TestHierarchy() {
-  bool success = true;
-  OptionsHierarchy tmp = OptionsHierarchy("UTest");
-  std::string testName = std::string("UTest/test");
-  std::string testDesc = std::string("Test description.");
-  std::string testTID = TYPENAME(int);
-
-  //Check that the hierarchy is properly named.
-  std::string str = std::string("UTest");
-  OptionsData node = tmp.GetNodeData();
-  success = success & ASSERT(str.compare(node.node) == 0,
-                              "OptionsHierarchy::GetNodeData failed on UTest");
-
-  //Check that inserting a node actually inserts the node.
-  /* Note, that since all versions of append simply call the most qualified
-      overload, we will only test that one. */
-  tmp.AppendNode(testName, testTID, testDesc);
-  success = success & ASSERT(tmp.FindNode(testName) != NULL, 
-                             "OptionsHierarchy::FindNode failed on UTest/test");
-
-  //Now check that the inserted node has the correct data.
-  OptionsHierarchy* testHierarchy = tmp.FindNode(testName);
-  OptionsData testData;
-  if (testHierarchy != NULL) {
-    node = testHierarchy->GetNodeData();
-    success = success & ASSERT(testName.compare(node.node) == 0 &&
-                                testDesc.compare(node.desc) == 0 &&
-                                testTID.compare(node.tname) == 0,
-                          "OptionsHierarchy::GetNodeData failed on UTest/test");
-  } else 
-    success = false;
-  return success;
-}
-
-
-/**
-* @brief Tests that the various PARAM_* macros work properly.
-*
+* @brief Tests that the various PARAM_* macros work properly
 * @return True indicating that all is well with IO & Options.
 */
-bool TestOption() {
-  
-  bool success = true;
+BOOST_AUTO_TEST_CASE(TestOption) {
   //This test will involve creating an option, and making sure IO reflects this.
   PARAM(int, "test", "test desc", "test_parent", DEFAULT_INT, false);
   
   //Does IO reflect this?
-  success = success & ASSERT(IO::HasParam("test_parent/test"),
-                              "IO::HasParam failed on parent/test");
+  BOOST_REQUIRE_EQUAL(IO::HasParam("test_parent/test"), true);
    
-
   std::string desc = std::string("test desc");
-  success = success & 
-    ASSERT(desc.compare(IO::GetDescription("test_parent/test")) == 0, 
-            "IO::GetDescription fails on test_parent/test");
-
-  success = success & 
-    ASSERT(IO::GetParam<int>("test_parent/test") == DEFAULT_INT, 
-            "IO::GetParam fails on test_parent/test");
-
-  return success;
+  
+  BOOST_REQUIRE(desc.compare(IO::GetDescription("test_parent/test"))==0);
+  BOOST_REQUIRE(IO::GetParam<int>("test_parent/test") == DEFAULT_INT);
 }
 
 
-/**
-* @brief If the expression is true, true is returned.  Otherwise
-*   an error message is printed and failure is returned.
-*
-* @param expression The expression to be evaluated.
-* @param msg The error message printed on failure.
-*
-* @return True indicating success and false indicates failure.
-*/
-bool ASSERT(bool expression, const char* msg) {
-  if (!expression) {
-    IO::Fatal << msg << std::endl; 
-    return false;
-  }
-  return true;
-}
-
-}; //namespace io
-}; //namespace mlpack
-
-int main(int argc, char** argv) {
-  mlpack::io::TestAll();
-  mlpack::IO::ParseCommandLine(argc, argv);
-  mlpack::IO::Warn << "Application did not terminate..." << std::endl;
-}
