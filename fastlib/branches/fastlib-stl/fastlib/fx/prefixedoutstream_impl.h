29ea965f2dfec0759d38746ba98db896e55d575a
Matthew Amidon <mamidon42@gmail.com>
2011-06-27 16:57:52 +0000

    Implemented default modules, hidden Info except on verbose.  Working on debuggable output streams.

--- fastlib/branches/fastlib-stl/fastlib/fx/prefixedoutstream_impl.h
+++ fastlib/branches/fastlib-stl/fastlib/fx/prefixedoutstream_impl.h
@@ -3,12 +3,15 @@
 
 template<typename T>
 void PrefixedOutStream::BaseLogic(T val) {
-  if (carriageReturned) {
+  if (carriageReturned && !isNullStream) {
     destination << prefix << val;
+    debugBuffer << prefix << val;
     carriageReturned = false;
   }
-  else
+  else if (!isNullStream) {
     destination << val;
+    debugBuffer << val;
+  }
 }
 
 #endif //MLPACK_IO_PREFIXED_OUT_STREAM_IMPL_H
