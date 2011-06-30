8915d8369bdd6da3d87076cb6340f5d29b0fad0c
Sterling Lewis Peet <sterling.peet@gatech.edu>
2011-06-30 19:46:23 +0000

    fix output file

--- fastlib/conf/jenkins-conf/runSloccount.sh
+++ fastlib/conf/jenkins-conf/runSloccount.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-if [ ! -d ".slocdata" ]
+DATA_FILE="sloccount.sc"
+DATA_DIR=".slocdata"
+
+if [ ! -d $DATA_DIR ]
 then
-  mkdir .slocdata
+  mkdir $DATA_DIR
 fi
-sloccount --wide --details --datadir .slocdata repo > .slocdata/sloccount.sc
+
+sloccount --wide --details --datadir .slocdata repo > $DATA_FILE
+
+mv $DATA_FILE $DATA_DIR
