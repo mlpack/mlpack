8a4bc5a588a06273c69be23a1f096ed87bce3002
nkauffman3 <nkauffman3@9d5b8971-822b-0410-80eb-d18c1038ef23>
2011-07-05 21:32:29 +0000

    --Convert to Boost Unit Test Framework -- Ticket 85

--- fastlib/branches/fastlib-stl/mlpack/emst/union_find_test.cc
+++ fastlib/branches/fastlib-stl/mlpack/emst/union_find_test.cc
@@ -10,90 +10,41 @@
 #include <fastlib/base/test.h>
 #include <fastlib/fastlib.h>
 
-class TestUnionFind {
-  
-private:
-  
-  UnionFind* test_union_find_;
-  static const index_t test_size_ = 10;
-  
-public: 
-  
-  void Init() {
-    
-    test_union_find_ = new UnionFind();
-    test_union_find_->Init(test_size_);
-  
-  }
-  
-  void Destruct() {
-   
-    delete test_union_find_;
-    
-  }
-  
-  void TestFind() {
-    
-    Init();
-    
-    for (index_t i = 0; i < test_size_; i++) {
-     
-      TEST_ASSERT(test_union_find_->Find(i) == i);
-      
-    }
-    test_union_find_->Union(0,1);
-    test_union_find_->Union(1, 2);
-    TEST_ASSERT(test_union_find_->Find(2) == test_union_find_->Find(0));
-    
-    Destruct();
-    
-    NONFATAL("TestFind passed.\n");
-    
-  }
-  
-  void TestUnion() {
-  
-    Init();
-    
-    test_union_find_->Union(0, 1);
-    
-    test_union_find_->Union(2, 3);
-    test_union_find_->Union(0, 2);
-    
-    test_union_find_->Union(5, 0);
-    test_union_find_->Union(0, 6);
-    
-    
-    TEST_ASSERT(test_union_find_->Find(0) == test_union_find_->Find(1));
-    TEST_ASSERT(test_union_find_->Find(2) == test_union_find_->Find(3));
-    TEST_ASSERT(test_union_find_->Find(1) == test_union_find_->Find(5));
-    TEST_ASSERT(test_union_find_->Find(6) == test_union_find_->Find(3));
-    
-    Destruct();
-    
-    NONFATAL("TestUnion passed.\n");
-    
-  }
-  
-  
-  void RunTests() {
-   
-    TestFind();
-    
-    TestUnion();
-    
-    NONFATAL("UnionFind unit tests passed.\n");
-    
-  }
-  
-  
-  
-  
-}; // class TestUnionFind
+#define BOOST_TEST_MODULE UnionFindTest
+#include <boost/test/unit_test.hpp>
 
-int main(int argc, char* argv[]) {
- 
-  TestUnionFind test;
-  test.RunTests();
+BOOST_AUTO_TEST_CASE(TestFind) { 
+    
+    static const index_t test_size_ = 10;  
+    UnionFind test_union_find_;
+    test_union_find_.Init(test_size_);
+
+    for (index_t i = 0; i < test_size_; i++) {
+      BOOST_REQUIRE(test_union_find_.Find(i) == i); 
+    }
+    test_union_find_.Union(0,1);
+    test_union_find_.Union(1, 2);
+    
+    BOOST_REQUIRE(test_union_find_.Find(2) == test_union_find_.Find(0));
+    
+  }
+
+BOOST_AUTO_TEST_CASE(TestUnion) { 
   
+    static const index_t test_size_ = 10;
+    UnionFind test_union_find_;
+    test_union_find_.Init(test_size_);
+
+    test_union_find_.Union(0, 1); 
+    test_union_find_.Union(2, 3);
+    test_union_find_.Union(0, 2);
+    test_union_find_.Union(5, 0);
+    test_union_find_.Union(0, 6);
+    
+    BOOST_REQUIRE(test_union_find_.Find(0) == test_union_find_.Find(1));
+    BOOST_REQUIRE(test_union_find_.Find(2) == test_union_find_.Find(3));
+    BOOST_REQUIRE(test_union_find_.Find(1) == test_union_find_.Find(5));
+    BOOST_REQUIRE(test_union_find_.Find(6) == test_union_find_.Find(3));
+    
 }
+
