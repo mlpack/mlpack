b470a514d56d503380e74de2519d21a50c38210d
Sterling Lewis Peet <sterling.peet@gatech.edu>
2011-07-06 19:13:02 +0000

    only run sloccount on the library, and not the contrib

--- fastlib/conf/jenkins-conf/runSloccount.sh
+++ fastlib/conf/jenkins-conf/runSloccount.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-DATA_FILE="sloccount.sc"
-DATA_DIR=".slocdata"
+DATA_FILE="reports/sloccount.sc"
+DATA_DIR="reports/slocdata"
+SRC_DIR="repo"
+SRC_DIR="fastlib-stl"
 
 if [ ! -d $DATA_DIR ]
 then
   mkdir $DATA_DIR
 fi
 
-sloccount --wide --details --datadir .slocdata repo > $DATA_FILE
+sloccount --wide --details --datadir .slocdata ${SRC_DIR}/CMake ${SRC_DIR}/fastlib ${SRC_DIR}/include ${SRC_DIR}/mlpack ${SRC_DIR}/physpack ${SRC_DIR}/script ${SRC_DIR}/util > $DATA_FILE
 
-mv $DATA_FILE $DATA_DIR
+#mv $DATA_FILE $DATA_DIR
