b445d1e84bba4824fc4c4ae3cfb0f97101479282
Sterling Lewis Peet <sterling.peet@gatech.edu>
2011-07-06 21:16:32 +0000

    auto-packager updated to use a secret file on the build server for subversion access

--- fastlib/conf/packages/jenkins/trunk/debian/auto-packager.sh
+++ fastlib/conf/packages/jenkins/trunk/debian/auto-packager.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 # Test the upstream jenkins mirror for war and plugin updates.
 #
+# The subversion password required for the script to commit its
+#  updates to the repository should be contained in a file called
+#  "secret.txt" found in the directory passed in as the first argument
+#
 # Copyright 2011  Sterling Peet <sterling.peet@gatech.edu>
 
 DEBEMAIL="sterling.peet@gatech.edu"
 DEBFULLNAME="Sterling Peet"
 export DEBEMAIL DEBFULLNAME
+
+SECRET_DIR=$1
+SECRET=$(cat ${SECRET_DIR}/secret.txt)
+
+SVN_MSG="[auto-packager] updating packaging to reflect new upstream release"
 
 svn-upgrade -u
 
@@ -22,7 +31,7 @@ then
   rm -rf $TDIR
   
   echo "[auto-packager] attempting to update the repository with changes"
-  svn ci -m "[auto-packager] updating packaging to reflect new upstream release"
+  echo svn ci -m $SVN_MSG --username speet3 --password $SECRET
 else
   echo "[auto-packager] svn-update reports local package is up-to-date"
 fi
