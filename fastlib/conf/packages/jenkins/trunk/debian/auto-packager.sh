d81a0b2fbe4c98e916f60b76be688eb9b3248e1e
Sterling Lewis Peet <sterling.peet@gatech.edu>
2011-07-08 14:32:05 +0000

    increased script robustness

--- fastlib/conf/packages/jenkins/trunk/debian/auto-packager.sh
+++ fastlib/conf/packages/jenkins/trunk/debian/auto-packager.sh
@@ -11,27 +11,44 @@ DEBEMAIL="sterling.peet@gatech.edu"
 DEBFULLNAME="Sterling Peet"
 export DEBEMAIL DEBFULLNAME
 
+PKG_NAME="jenkins"
+SVN_URL="http://svn.cc.gatech.edu/fastlab/fastlib/conf/packages/jenkins/trunk"
+SVN_USR="speet3"
+SVN_MSG="[auto-packager] updating packaging to reflect new upstream release"
 SECRET_DIR=$1
 SECRET=$(cat ${SECRET_DIR}/secret.txt)
+PWD=$(pwd)
 
-SVN_MSG="[auto-packager] updating packaging to reflect new upstream release"
 
+TDIR="$(mktemp -d)"
+trap '[ ! -d "$TDIR" ] || rm -r "$TDIR"' EXIT
+
+cd $TDIR
+svn co $SVN_URL $PKG_NAME
+cd $PKG_NAME
+svn cat -r 8500 debian/changelog > debian/changelog
 svn-upgrade -u
 
 if [ $? -eq 0 ]
 then
   echo "[auto-packager] uscan reports updates upstream, updating packaging info"
 
-  TDIR="$(mktemp -d)"
+  CTDIR="$(mktemp -d)"
   trap '[ ! -d "$TDIR" ] || rm -r "$TDIR"' EXIT
   
-  cp debian/changelog $TDIR/changelog
-  sed "s/UNRELEASED/$(lsb_release -cs)/" < $TDIR/changelog > debian/changelog
-   
-  rm -rf $TDIR
+  cp debian/changelog $CTDIR/changelog
+  sed "s/UNRELEASED/$(lsb_release -cs)/" < $CTDIR/changelog > debian/changelog
+  
+  echo "[auto-packager] New changelog contents:"
+  cat debian/changelog
+  rm -rf $CTDIR
   
   echo "[auto-packager] [auto-packager-warning] attempting to update the repository with changes"
-  svn ci -m $SVN_MSG --username speet3 --password $SECRET
+  echo svn ci -m "$SVN_MSG" --username $SVN_USR --password $SECRET
 else
   echo "[auto-packager] svn-update reports local package is up-to-date"
 fi
+
+cd $PWD
+rm -rf $TDIR
+
