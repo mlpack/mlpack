9ec2c0777453611adc9455bd6ff330a392e39ab4
Sterling Lewis Peet <sterling.peet@gatech.edu>
2011-07-15 18:04:40 +0000

    convert to uscan/sed/awk/dch instead of using svn-upgrade

--- fastlib/conf/packages/jenkins/trunk/debian/auto-packager.sh
+++ fastlib/conf/packages/jenkins/trunk/debian/auto-packager.sh
@@ -26,9 +26,8 @@ trap '[ ! -d "$TDIR" ] || rm -r "$TDIR"' EXIT
 cd $TDIR
 svn co $SVN_URL $PKG_NAME --username $SVN_USR --password $SECRET --non-interactive
 cd $PKG_NAME
-svn cat -r 8500 --username $SVN_USR --password $SECRET --non-interactive debian/changelog > debian/changelog
-svn-upgrade-fastlab -u --noninteractive # TODO: This doesn't work, svn up is called within, but need credentials
-
+# svn cat -r 8500 --username $SVN_USR --password $SECRET --non-interactive debian/changelog > debian/changelog
+uscan --destdir ../tarballs
 # TODO: instead of useing the convienence of svn-upgrade, use uscan, sed and dch to get the same effect.
 
 if [ $? -eq 0 ]
@@ -38,6 +37,10 @@ then
   CTDIR="$(mktemp -d)"
   trap '[ ! -d "$TDIR" ] || rm -r "$TDIR"' EXIT
   
+  NEW_VER=$(ls ../tarballs | sed -n "s/[^0-9]*\([0-9]\.[0-9]*\)[^0-9]*/\1-1/p")
+  NEW_VER=$(echo NEW_VER | awk '{print $1}')
+  dch -v $NEW_VER
+  
   cp debian/changelog $CTDIR/changelog
   sed "s/UNRELEASED/$(lsb_release -cs)/" < $CTDIR/changelog > debian/changelog
   
@@ -46,7 +49,7 @@ then
   rm -rf $CTDIR
   
   echo "[auto-packager] [auto-packager-warning] attempting to update the repository with changes"
-  echo svn ci -m "$SVN_MSG" --username $SVN_USR --password $SECRET # TODO: un-echo
+  echo svn ci -m "$SVN_MSG" --username $SVN_USR #--password $SECRET # TODO: un-echo
 else
   echo "[auto-packager] svn-update reports local package is up-to-date"
 fi
