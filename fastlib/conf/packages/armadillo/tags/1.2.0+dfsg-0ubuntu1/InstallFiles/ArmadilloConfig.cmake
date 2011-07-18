939ed0ca56b020596fc7464347dd215392e2b0f5
Sterling Lewis Peet <sterling.peet@gatech.edu>
2011-07-18 20:31:06 +0000

    Tagged from Package-Armadillo-stable #10

---
+++ fastlib/conf/packages/armadillo/tags/1.2.0+dfsg-0ubuntu1/InstallFiles/ArmadilloConfig.cmake
@@ -0,0 +1,16 @@
+# - Config file for the Armadillo package
+# It defines the following variables
+#  ARMADILLO_INCLUDE_DIRS - include directories for Armadillo
+#  ARMADILLO_LIBRARY_DIRS - library directories for Armadillo (normally not used!)
+#  ARMADILLO_LIBRARIES    - libraries to link against
+
+# Tell the user project where to find our headers and libraries
+set(ARMADILLO_INCLUDE_DIRS "/usr/include")
+set(ARMADILLO_LIBRARY_DIRS "/usr/lib")
+
+# Our library dependencies (contains definitions for IMPORTED targets)
+include("/usr/share/Armadillo/CMake/ArmadilloLibraryDepends.cmake")
+
+# These are IMPORTED targets created by ArmadilloLibraryDepends.cmake
+set(ARMADILLO_LIBRARIES armadillo)
+
