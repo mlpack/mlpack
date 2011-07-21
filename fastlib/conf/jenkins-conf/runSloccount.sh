f2466e80f5c5aa61ba9e67419fe8f2b09ac3e114
Sterling Lewis Peet <sterling.peet@gatech.edu>
2011-07-21 02:00:48 +0000

    Actually use the DATA_DIR after making it

--- fastlib/conf/jenkins-conf/runSloccount.sh
+++ fastlib/conf/jenkins-conf/runSloccount.sh
@@ -9,6 +9,6 @@ then
   mkdir -p $DATA_DIR
 fi
 
-sloccount --wide --details --datadir .slocdata ${SRC_DIR}/CMake ${SRC_DIR}/fastlib ${SRC_DIR}/include ${SRC_DIR}/mlpack ${SRC_DIR}/physpack ${SRC_DIR}/script ${SRC_DIR}/util > $DATA_FILE
+sloccount --wide --details --datadir $DATA_DIR ${SRC_DIR}/CMake ${SRC_DIR}/fastlib ${SRC_DIR}/include ${SRC_DIR}/mlpack ${SRC_DIR}/physpack ${SRC_DIR}/script ${SRC_DIR}/util > $DATA_FILE
 
 #mv $DATA_FILE $DATA_DIR
