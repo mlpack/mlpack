e3edf943e4570b90a493c58aa4550d4b82f1b4dd
Ryan Curtin <ryan@ratml.org>
2011-07-22 20:43:15 +0000

    Don't use external files for the test.  And clean up some odd formatting...

--- fastlib/branches/fastlib-stl/mlpack/quicsvd/quicsvd_test.cc
+++ fastlib/branches/fastlib-stl/mlpack/quicsvd/quicsvd_test.cc
@@ -6,51 +6,32 @@
 
 BOOST_AUTO_TEST_CASE(QuicSVDTest) { 
 
-    FILE * logfile;
-    logfile = fopen("LOG", "w");
+  Matrix A;
+  arma::mat tmpA = "0.0 1.0 2.0 3.0; 0.0 1.0 2.0 3.0";
+  arma_compat::armaToMatrix(tmpA, A);
+  QuicSVD svd(A, 0.1);
+  Vector s;
+  Matrix U, VT,S;
 
-    Matrix A;
-    arma::mat tmpA;
-    printf("Load data from input1.txt.\n");
-    data::Load("input2.txt", tmpA);
-    arma_compat::armaToMatrix(tmpA, A);
-    QuicSVD svd(A, 0.1);
-    Vector s;
-    Matrix U, VT,S;
+  svd.ComputeSVD(&s, &U, &VT);
+  arma::mat tmpU, tmpVT, tmpS;
+  arma_compat::matrixToArma(U, tmpU);
+  arma_compat::matrixToArma(VT, tmpVT);
+  S.InitDiagonal(s);
+  arma_compat::matrixToArma(S, tmpS);
 
-    svd.ComputeSVD(&s, &U, &VT);
-    arma::mat tmpU, tmpVT, tmpS;
-    arma_compat::matrixToArma(U, tmpU);
-    arma_compat::matrixToArma(VT, tmpVT);
-    S.InitDiagonal(s);
-    arma_compat::matrixToArma(S, tmpS);
-
-    data::Save("U.txt", tmpU);
-    data::Save("S.txt", tmpS);
-    data::Save("VT.txt", tmpVT);
- }
-
-
-BOOST_AUTO_TEST_CASE(CosineNodeTest) {
-    FILE * logfile;
-    logfile = fopen("LOG", "w");
-
-    Matrix A;
-    arma::mat tmpA;
-    data::Load("input.txt", tmpA);
-    arma_compat::armaToMatrix(tmpA, A);
-    CosineNode root(A);
-
-    //todo:  figure out what breaks Split()
-    //    root.Split();
+  // There is nobody to check this, so don't save it...
+//    data::Save("U.txt", tmpU);
+//    data::Save("S.txt", tmpS);
+//    data::Save("VT.txt", tmpVT);
 }
 
+BOOST_AUTO_TEST_CASE(CosineNodeTest) {
+  Matrix A;
+  arma::mat tmpA = "0.0 1.0 0.0 1.0; 0.0 0.0 1.0 1.0";
+  arma_compat::armaToMatrix(tmpA, A);
+  CosineNode root(A);
 
-
-
-
-
-
-
-
-
+  //todo:  figure out what breaks Split()
+  //    root.Split();
+}
