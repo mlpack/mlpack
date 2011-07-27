37fd218e07da765e89bc31d4a9329d057cac7e2c
Dongryeol Lee <drselee@gmail.com>
2011-07-27 14:23:45 +0000

    Now have to change the termination condition.

--- fastlib/trunk/contrib/dongryel/thesis_research/core/parallel/distributed_dualtree_subtable_routing.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/parallel/distributed_dualtree_subtable_routing.h
@@ -20,20 +20,33 @@ class DistributedDualtreeSubtableRouting {
 
     static int RouteTo(
       int source, const std::vector<int> &destinations,
-      int starting_bit_pos, int upper_limit_bit_pos) {
+      int starting_bit_pos, int upper_limit_bit_pos, int *chosen_bit_pos) {
 
-      // Compute the first differing least significant bit between the
-      // source and each destination.
-      int first_differing_least_sig_bit_pos = std::numeric_limits<int>::max();
-      for(unsigned int i = 0; i < destinations.size(); i++) {
-        first_differing_least_sig_bit_pos =
-          std::min(
-            first_differing_least_sig_bit_pos,
-            core::math::LeastSignificantDifferingBit<int>(
-              source, destinations[i], starting_bit_pos, upper_limit_bit_pos));
+      int route_destination = -1;
+
+      // If there is only one destination, then route directly.
+      if(destinations.size() == 1) {
+        *chosen_bit_pos = upper_limit_bit_pos;
+        route_destination = destinations[0];
+      }
+      else {
+
+        // Compute the first differing least significant bit between the
+        // source and each destination.
+        int first_differing_least_sig_bit_pos = std::numeric_limits<int>::max();
+        for(unsigned int i = 0; i < destinations.size(); i++) {
+          first_differing_least_sig_bit_pos =
+            std::min(
+              first_differing_least_sig_bit_pos,
+              core::math::LeastSignificantDifferingBit<int>(
+                source, destinations[i], starting_bit_pos,
+                upper_limit_bit_pos));
+        }
+        *chosen_bit_pos = first_differing_leaest_sig_bit_pos;
+        route_destination = source ^(1 << (* chosen_bit_pos));
       }
 
-      return first_differing_least_sig_bit_pos;
+      return route_destination;
     }
 };
 }
