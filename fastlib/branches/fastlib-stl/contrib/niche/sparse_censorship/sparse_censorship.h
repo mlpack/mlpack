ad6a4e50c0859362a62e5ad1451482b2a23a16d3
tekhnofiend <nishantmehta.x@gmail.com>
2011-08-07 19:12:45 +0000

    added some mostly empty files for sparse censorship

---
+++ fastlib/branches/fastlib-stl/contrib/niche/sparse_censorship/sparse_censorship.h
@@ -0,0 +1,28 @@
+/** @file sparse_censorship.h
+ *
+ *  This file implements a Sparse Coding model for documents, with censorship. It largely extends from SAGE (Sparse Additive Generative Models of Text), by Eisenstein et al.
+ *
+ *  @author Nishant Mehta (niche)
+ *  @bug No known bugs, but no known completed code
+ */
+
+#ifndef SPARSE_CENSORSHIP_H
+#define SPARSE_CENSORSHIP_H
+
+#define INSIDE_SPARSE_CENSORSHIP_H
+
+
+using namespace arma;
+using namespace std;
+
+
+class SparseCensorship {
+ private:
+  
+
+};
+
+#include "sparse_censorship_impl.h"
+#undef INSIDE_SPARSE_CENSORSHIP_H
+
+#endif
