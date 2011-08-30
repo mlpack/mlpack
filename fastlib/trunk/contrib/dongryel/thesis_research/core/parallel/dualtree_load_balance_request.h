7a0fd620679bb2b65c7be7755fc9841ba5b8da43
Dongryeol Lee <drselee@gmail.com>
2011-08-30 19:59:19 +0000

    Getting there with bug fixes.

--- fastlib/trunk/contrib/dongryel/thesis_research/core/parallel/dualtree_load_balance_request.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/parallel/dualtree_load_balance_request.h
@@ -18,9 +18,7 @@ class DualtreeLoadBalanceRequest {
   private:
     boost::scoped_array< boost::tuple<int, int, int> > query_subtable_ids_;
 
-    unsigned long int current_remaining_local_computation_;
-
-    bool needs_load_balancing_;
+    unsigned long int remaining_local_computation_;
 
     int num_existing_query_subtables_;
 
@@ -28,97 +26,77 @@ class DualtreeLoadBalanceRequest {
 
   public:
 
+    unsigned long int remaining_local_computation() const {
+      return remaining_local_computation_;
+    }
+
     unsigned long int remaining_extra_points_to_hold() const {
       return remaining_extra_points_to_hold_;
     }
 
-    bool needs_load_balancing() const {
-      return needs_load_balancing_;
-    }
-
     template<class Archive>
     void save(Archive &ar, const unsigned int version) const {
-
-      // Save the flag.
-      ar & needs_load_balancing_;
-      if(needs_load_balancing_) {
-        ar & num_existing_query_subtables_;
-        for(int i = 0; i < num_existing_query_subtables_; i++) {
-          ar & query_subtable_ids_[i].get<0>();
-          ar & query_subtable_ids_[i].get<1>();
-          ar & query_subtable_ids_[i].get<2>();
-        }
-        ar & current_remaining_local_computation_;
-        ar & remaining_extra_points_to_hold_;
+      ar & num_existing_query_subtables_;
+      for(int i = 0; i < num_existing_query_subtables_; i++) {
+        ar & query_subtable_ids_[i].get<0>();
+        ar & query_subtable_ids_[i].get<1>();
+        ar & query_subtable_ids_[i].get<2>();
       }
+      ar & remaining_local_computation_;
+      ar & remaining_extra_points_to_hold_;
     }
 
     /** @brief Serialize the object.
      */
     template<class Archive>
     void load(Archive &ar, const unsigned int version) {
-
-      // Load the flag.
-      ar & needs_load_balancing_;
-
-      if(needs_load_balancing_) {
-        ar & num_existing_query_subtables_;
-        if(num_existing_query_subtables_ > 0) {
-          boost::scoped_array <
-          boost::tuple<int, int, int> > tmp_array(
-            new boost::tuple<int, int, int>[
-              num_existing_query_subtables_]);
-          query_subtable_ids_.swap(tmp_array);
-          for(int i = 0; i < num_existing_query_subtables_; i++) {
-            ar & query_subtable_ids_[i].get<0>();
-            ar & query_subtable_ids_[i].get<1>();
-            ar & query_subtable_ids_[i].get<2>();
-          }
+      ar & num_existing_query_subtables_;
+      if(num_existing_query_subtables_ > 0) {
+        boost::scoped_array <
+        boost::tuple<int, int, int> > tmp_array(
+          new boost::tuple<int, int, int>[
+            num_existing_query_subtables_]);
+        query_subtable_ids_.swap(tmp_array);
+        for(int i = 0; i < num_existing_query_subtables_; i++) {
+          ar & query_subtable_ids_[i].get<0>();
+          ar & query_subtable_ids_[i].get<1>();
+          ar & query_subtable_ids_[i].get<2>();
         }
-        ar & current_remaining_local_computation_;
-        ar & remaining_extra_points_to_hold_;
       }
+      ar & remaining_local_computation_;
+      ar & remaining_extra_points_to_hold_;
     }
     BOOST_SERIALIZATION_SPLIT_MEMBER()
 
     DualtreeLoadBalanceRequest() {
-      current_remaining_local_computation_ = 0;
-      needs_load_balancing_ = false;
+      remaining_local_computation_ = 0;
       num_existing_query_subtables_ = 0;
       remaining_extra_points_to_hold_ = 0;
     }
 
     void Init(
-      bool needs_load_balancing_in,
       std::vector< boost::shared_ptr<SubTableType> > &query_subtables,
-      unsigned long int current_remaining_local_computation_in,
+      unsigned long int remaining_local_computation_in,
       unsigned long int remaining_extra_points_to_hold_in) {
 
-      // Set the flag.
-      needs_load_balancing_ = needs_load_balancing_in;
+      // Allocate the array.
+      num_existing_query_subtables_ = query_subtables.size();
+      if(num_existing_query_subtables_ > 0) {
+        boost::scoped_array <
+        boost::tuple<int, int, int> > tmp_array(
+          new boost::tuple<int, int, int>[ num_existing_query_subtables_]);
+        query_subtable_ids_.swap(tmp_array);
 
-      if(needs_load_balancing_) {
-
-        // Allocate the array.
-        num_existing_query_subtables_ = query_subtables.size();
-        if(num_existing_query_subtables_ > 0) {
-          boost::scoped_array <
-          boost::tuple<int, int, int> > tmp_array(
-            new boost::tuple<int, int, int>[ num_existing_query_subtables_]);
-          query_subtable_ids_.swap(tmp_array);
-
-          for(int i = 0; i < num_existing_query_subtables_ ; i++) {
-            int rank = query_subtables[i]->table()->rank();
-            int begin = query_subtables[i]->start_node()->begin();
-            int count = query_subtables[i]->start_node()->count();
-            query_subtable_ids_[i] =
-              boost::tuple<int, int, int>(rank, begin, count);
-          }
+        for(int i = 0; i < num_existing_query_subtables_ ; i++) {
+          int rank = query_subtables[i]->table()->rank();
+          int begin = query_subtables[i]->start_node()->begin();
+          int count = query_subtables[i]->start_node()->count();
+          query_subtable_ids_[i] =
+            boost::tuple<int, int, int>(rank, begin, count);
         }
 
         // Set the current estimated remaining local work.
-        current_remaining_local_computation_ =
-          current_remaining_local_computation_in;
+        remaining_local_computation_ = remaining_local_computation_in;
 
         // Set the maximum number of points to receive.
         remaining_extra_points_to_hold_ = remaining_extra_points_to_hold_in;
@@ -128,12 +106,12 @@ class DualtreeLoadBalanceRequest {
     DualtreeLoadBalanceRequest(
       bool needs_load_balancing_in,
       std::vector< boost::shared_ptr<SubTableType> > &query_subtables,
-      unsigned long int current_remaining_local_computation_in,
+      unsigned long int remaining_local_computation_in,
       unsigned long int remaining_extra_points_to_hold_in) {
 
       this->Init(
         needs_load_balancing_in, query_subtables,
-        current_remaining_local_computation_in,
+        remaining_local_computation_in,
         remaining_extra_points_to_hold_in);
     }
 };
