d49b6b0e2dab792630dff3f1a61c7983a184d996
niche <niche@9d5b8971-822b-0410-80eb-d18c1038ef23>
2011-09-07 21:58:41 +0000

    changed ThetaObjective() so that it takes into account theta_bar. Also, added ThetaBarObjective so we can update theta_bar

--- fastlib/branches/fastlib-stl/contrib/niche/non_fastlib/sparse_censorship/take_two_laplace_regularizer/ComputePhi.m
+++ fastlib/branches/fastlib-stl/contrib/niche/non_fastlib/sparse_censorship/take_two_laplace_regularizer/ComputePhi.m
@@ -1,5 +1,5 @@
-function phi = ComputePhi(theta, beta, eta, publishers, inds_by_doc)
-%function phi = ComputePhi(theta, beta, eta, publishers, inds_by_doc)
+function phi = ComputePhi(theta_bar, theta, beta, eta, publishers, inds_by_doc)
+%function phi = ComputePhi(theta_bar, theta, beta, eta, publishers, inds_by_doc)
 
 D = length(publishers);
 [V K] = size(beta);
@@ -14,13 +14,13 @@ end
 log_sum_exp_beta = logsumexp(beta);
 for d = 1:D
   p = publishers(d);
-  log_sum_exp_theta_d = logsumexp(theta(:,d));
+  log_sum_exp_theta_d = logsumexp(theta_bar + theta(:,d));
   for k = 1:K
     for v = inds_by_doc{d}
       % using logsumexp from Lightspeed, make sure it's in the path
       phi{d}(v,k) = ...
           beta(v,k) + eta(v,k,p) - log_sum_exp_beta(k) ...
-          + theta(k,d) - log_sum_exp_theta_d;
+          + theta_bar(k) + theta(k,d) - log_sum_exp_theta_d;
     end
   end
   phi{d}(inds_by_doc{d},:) = ...
