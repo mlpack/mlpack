9c8b856d2367531aa4d50f68832c8dfd75d94d0c
Ryan Curtin <ryan@ratml.org>
2011-09-07 06:18:18 +0000

    Convert from GenVector and GenMatrix to arma::vec and arma::mat, respectively. This is not tested.

--- fastlib/branches/fastlib-stl/mlpack/infomax_ica/infomax_ica.cc
+++ fastlib/branches/fastlib-stl/mlpack/infomax_ica/infomax_ica.cc
@@ -24,67 +24,65 @@ using namespace mlpack;
 /** 
  * Dummy constructor 
  */
-InfomaxICA::InfomaxICA(){
+InfomaxICA::InfomaxICA() { }
 
-}
-
-InfomaxICA::InfomaxICA(double lambda, index_t b, double epsilon):
-  lambda_(lambda),
-  b_(b),
-  epsilon_(epsilon){
-
-}
+InfomaxICA::InfomaxICA(double lambda, index_t b, double epsilon) :
+    lambda_(lambda),
+    b_(b),
+    epsilon_(epsilon) { }
 
 /**
  * Sphere the data, apply ica. This is the first function to call
  * after initializing the variables.
  */
-void InfomaxICA::applyICA(const Matrix &dataset){
-  double current_cos=DBL_MAX;
-  w_.Init(b_,b_);
-  data_.Init(dataset.n_rows(),dataset.n_cols());
-  la::ScaleOverwrite(1.0,dataset,&data_);
-  if (b_<data_.n_cols()){
+void InfomaxICA::applyICA(const arma::mat& dataset) {
+  double current_cos = DBL_MAX;
+  w_.set_size(b_, b_);
+  data_ = dataset; // Copy the matrix?  Stupid!
+  
+  if (b_ < data_.n_cols) {
     sphere(data_);
+
     // initial estimate for w is Id
-    Matrix i1 = eye(data_.n_rows(),double(1));
-    la::ScaleOverwrite(1.0,i1,&w_);    
-    while (epsilon_<=current_cos){
-      Matrix w_prev;
-      w_prev.Copy(w_);
+    arma::mat i1;
+    i1.eye(data_.n_rows, data_.n_rows);
+    w_ = i1; // Another copy?  Stupid!
+    while (epsilon_ <= current_cos){
+      arma::mat w_prev = w_;
       evaluateICA();
-      current_cos = w_delta(w_prev,w_);
+      current_cos = w_delta(w_prev, w_);
     }
-  }
-  else
-    IO::Warn << "Window size must be less than number of instances." << std::endl;
+  } else
+    IO::Fatal << "Window size must be less than number of instances."
+        << std::endl;
 }
 
 /**
  * Run infomax. Call this after initialization and applyICA.
  */
-void InfomaxICA::evaluateICA(){
-  Matrix BI = eye(w_.n_rows(),double(b_));
+void InfomaxICA::evaluateICA() {
+  arma::mat BI;
+  BI.eye(w_.n_rows, w_.n_rows);
+  BI *= b_;
+  
   // intermediate calculation variables
-  Matrix icv = Matrix(w_.n_rows(),b_);
-  Matrix icv2 = Matrix(w_.n_rows(),w_.n_cols());
-  Matrix icv4 = Matrix(w_.n_rows(),w_.n_cols());
-  for (index_t i=0;i<data_.n_cols();i+=b_){
-    if (i+b_<data_.n_cols()){
-      Matrix subm;
-      data_.MakeColumnSlice(i,b_,&subm);
-      la::MulExpert(1,false,w_,false,subm,0.0,&icv);
-      la::Scale(double(-1.0),&icv);
-      expM(icv);  
-      addOne(icv);
-      invertVals(icv);
-      la::Scale(-2.0,&icv);
-      addOne(icv);
-      la::MulExpert(1,false,icv,true,subm,0.0,&icv2);
-      la::AddOverwrite(icv2,BI,&icv4);
-      la::Scale(lambda_,&icv4);
-      la::MulExpert(1,false,icv4,false,w_,0.0,&icv2);
-      la::AddTo(w_.n_elements(),icv2.ptr(),w_.ptr());
+  arma::mat icv(w_.n_rows, b_);
+  arma::mat icv2(w_.n_rows, w_.n_cols);
+  arma::mat icv4(w_.n_rows, w_.n_cols);
+
+  for (index_t i = 0; i < data_.n_cols; i += b_) {
+    if ((i + b_) < data_.n_cols) {
+      // This is not tested.
+      icv = -2.0 * arma::pow(arma::exp(
+          -1 * (w_ * data_.cols(i, i + b_))) + 1, -1) + 1;
+
+      icv2 = icv * trans(data_.cols(i, i + b_));
+
+      icv4 = lambda_ * (icv2 + BI);
+
+      icv2 = icv4 * w_;
+
+      w_ += icv2;
     }
   }
 }
@@ -93,212 +91,126 @@ void InfomaxICA::evaluateICA(){
 /** 
  * Sphere the input data. 
  */
-void InfomaxICA::sphere(Matrix &data){
-  Matrix sample_covariance = sampleCovariance(data);
-  Matrix wz = sqrtm(sample_covariance);
-  Matrix wz_inverse = Matrix(wz.n_rows(),wz.n_cols());
-  Matrix data_sub_means = subMeans(data);
-  if (la::InverseOverwrite(wz,&wz_inverse)){
-    la::Scale(wz.n_cols()*wz.n_rows(),2.0,wz_inverse.ptr());
-    la::MulOverwrite(wz_inverse,data_sub_means,&data);
+void InfomaxICA::sphere(arma::mat& data) {
+  arma::mat sample_covariance = sampleCovariance(data);
+  arma::mat wz = sqrtm(sample_covariance);
+  arma::mat wz_inverse(wz.n_rows, wz.n_cols);
+  arma::mat data_sub_means = subMeans(data);
+
+  if (arma::inv(wz_inverse, wz)) {
+    // Not tested.
+    wz_inverse *= 2.0;
+    data = wz_inverse * data_sub_means;
   }
 }
 
 // Covariance matrix.
-Matrix InfomaxICA::sampleCovariance(const Matrix &m){
-  Matrix ttm = subMeans(m);
-  Matrix wm = Matrix(m.n_cols(),m.n_rows());  
-  la::TransposeOverwrite(ttm,&wm);
+arma::mat InfomaxICA::sampleCovariance(const arma::mat& m) {
+  // Not tested.
+  arma::mat ttm = subMeans(m);
+  arma::mat wm = trans(ttm);
 
-  Matrix twm = Matrix(ttm.n_rows(),ttm.n_rows());
-  Matrix output = Matrix(ttm.n_rows(),ttm.n_rows());
-  output.SetZero();
-  Matrix tttm = Matrix(wm);
+  arma::mat twm(ttm.n_rows, ttm.n_rows);
+  arma::mat output(ttm.n_rows, ttm.n_rows);
+  output.zeros();
 
-  la::Scale(tttm.n_rows()*tttm.n_cols(),1/(double(ttm.n_cols())-1),tttm.ptr());
-  la::MulTransAOverwrite(wm,tttm,&twm);
-  la::AddOverwrite(twm.n_rows()*twm.n_cols(),twm.ptr(),twm.ptr(),output.ptr());
-  la::Scale(output.n_rows()*output.n_cols(),double(0.5),output.ptr());
+  arma::mat tttm(wm);
+
+  tttm *= (1 / (double) (ttm.n_cols - 1));
+  twm = trans(wm) * tttm;
+  output = twm;
+
   return output;
 }
 
-Matrix InfomaxICA::subMeans(const Matrix &m){
-  Matrix output = Matrix(m);
-  Vector row_means = rowMean(output);
-  la::Scale(row_means.length(),-1.0,row_means.ptr());
-  for (index_t j=0;j<output.n_cols();j++){
-    la::AddTo(row_means.length(),row_means.ptr(),output.GetColumnPtr(j));
-  }  
+arma::mat InfomaxICA::subMeans(const arma::mat& m){
+  arma::mat output(m);
+  arma::vec row_means = rowMean(output);
+
+  for (index_t j = 0; j < output.n_cols; j++) {
+    output.col(j) -= row_means;
+  }
+
   return output;
 }
 
 /** 
  * Compute the sample mean of a column 
  */
-Vector InfomaxICA::rowMean(const Matrix &m){
-  Vector row_means;
-  row_means.Init(m.n_rows());
-  row_means.SetZero();
-  for (index_t j=0;j<m.n_cols();j++){
-    la::AddTo(row_means.length(),m.GetColumnPtr(j),row_means.ptr());
+arma::vec InfomaxICA::rowMean(const arma::mat& m){
+  arma::vec row_means(m.n_rows);
+  row_means.zeros();
+
+  for (index_t j = 0; j < m.n_cols; j++) {
+    row_means += m.col(j);
   }
-  la::Scale(row_means.length(),1/double(m.n_cols()),row_means.ptr());
+
+  row_means /= (double) m.n_cols;
+
   return row_means;
 }
 
 /** 
- *  Matrix square root using Cholesky decomposition method.  Assumes
- *  the input matrix is square.
+ * Matrix square root using Cholesky decomposition method.  Assumes the input
+ * matrix is square.
  */
-Matrix InfomaxICA::sqrtm(const Matrix &m){
-  Matrix output = Matrix(m.n_rows(), m.n_cols());
-  Matrix chol = Matrix(m);
-  if (la::Cholesky(&chol)){
-    Matrix u,vt;
-    Vector s;
-    la::TransposeSquare(&chol);
-    if (la::SVDInit(chol,&s,&u,&vt)){
-      Matrix S = Matrix(s.length(),s.length());
-      Matrix tm1 = Matrix(u.n_rows(),S.n_cols());
-      S.SetZero();
-      S.SetDiagonal(s);
-      la::MulExpert(1,false,u,false,S,0.0,&tm1);
-      la::MulExpert(1,false,tm1,true,u,0.0,&output);
-    }
-    else
-      IO::Warn << "infomaxICA sqrtm: SVD failed." << std::endl;      
-  }
-  else
-    IO::Warn << "infomaxICA sqrtm: Cholesky decomposition failed." << std::endl;
+arma::mat InfomaxICA::sqrtm(const arma::mat& m) {
+  arma::mat output(m.n_rows, m.n_cols);
+  arma::mat chol;
+
+  if (arma::chol(chol, m)) {
+    arma::mat u, v;
+    arma::vec s;
+
+    if (arma::svd(u, s, v, trans(chol))) {
+      arma::mat S(s.n_elem, s.n_elem);
+      S.zeros();
+      S.diag() = s;
+
+      arma::mat tm1 = u * S;
+      output = tm1 * trans(u);
+    } else
+      IO::Warn << "InfomaxICA sqrtm: SVD failed." << std::endl;      
+  } else
+    IO::Warn << "InfomaxICA sqrtm: Cholesky decomposition failed." << std::endl;
 
   return output;
 }
 
 // Compare w estimates for convergence
-double InfomaxICA::w_delta(const Matrix &w_prev, const Matrix &w_pres){
-  Matrix temp;
-  Vector delta_c;
-  Vector delta_r;
-  double delta_dot;
-  la::SubInit(w_pres,w_prev,&temp);
-  vectorize(temp,delta_r);
-  vectorize(temp,delta_c);
-  delta_dot = la::Dot(delta_r.length(),delta_r.ptr(),delta_c.ptr());
-  IO::Warn << "w change=" << delta_dot << std::endl;
+double InfomaxICA::w_delta(const arma::mat& w_prev, const arma::mat& w_pres) {
+  arma::mat temp = w_pres - w_prev;
+  arma::vec delta = reshape(temp, 1, temp.n_rows * temp.n_cols);
+  double delta_dot = arma::dot(delta, delta);
+
+  IO::Info << "w change: " << delta_dot << std::endl;
   return delta_dot;
-}
-
-// utility functions
-
-/** 
- * Inplace exp for each matrix element 
- */
-void InfomaxICA::expM(Matrix &m){
-  for (index_t i=0;i<m.n_rows();i++){
-    for (index_t j=0;j<m.n_cols();j++){
-      m.set(i,j,exp(m.get(i,j)));
-    }
-  }
-}
-
-/** 
- * Inplace M+1 
- */
-void InfomaxICA::addOne(Matrix &m){
-  for (index_t i=0;i<m.n_rows();i++){
-    for (index_t j=0;j<m.n_cols();j++){
-      m.set(i,j,m.get(i,j)+1);
-    }
-  }
-}
-
-/** 
- * Inplace x_{ij}^-1 
- */
-void InfomaxICA::invertVals(Matrix &m){
-  for (index_t i=0;i<m.n_rows();i++){
-    for (index_t j=0;j<m.n_cols();j++){
-      m.set(i,j,1/m.get(i,j));
-    }
-  }
-}
-
-/** 
- * Identity matrix function 
- */
-Matrix InfomaxICA::eye(index_t dim, double diagVal){
-  Matrix output;
-  Vector *diag = new Vector();
-  diag->Init(dim);
-  diag->SetAll(diagVal);
-  output.InitDiagonal(*diag);
-  return output;
-}
-
-/** 
- * Initialize the vector v to values of the matrix m. 
- */
-void InfomaxICA::vectorize(const Matrix &m, Vector &v){
-  index_t v_ind = 0;
-  v.Init(m.n_rows()*m.n_cols());
-  for (index_t i=0;i<m.n_rows();i++){
-    for (index_t j=0;j<m.n_cols();j++){
-      v[v_ind]=m.get(i,j);
-      v_ind++;
-    }
-  }
-  
-}
-
-/** 
- * Simple display matrix function 
- */
-void InfomaxICA::displayMatrix(const Matrix &m){
-  IO::Info << std::endl;
-  for (index_t i=0;i<m.n_rows();i++){
-    for (index_t j=0;j<m.n_cols();j++){
-      IO::Info << m.get(i,j);
-    }
-    IO::Info << std::endl;
-  }  
-}
-
-/** 
- * Simple display vector function 
- */
-void InfomaxICA::displayVector(const Vector &m){
-  IO::Info << std::endl;
-  for (index_t i=0;i<m.length();i++){
-    IO::Info << m[i];
-  }
-  IO::Info << std::endl;
 }
 
 /** 
  * Return the current unmixing matrix estimate. Requires a reference
  * to an uninitialized matrix.
  */
-void InfomaxICA::getUnmixing(Matrix &w){
-  w.Copy(w_);
+void InfomaxICA::getUnmixing(arma::mat& w) {
+  w = w_;
 }
 
 /** 
  * Return the source estimates, S. S is a reference to an
  * uninitialized matrix.
  */
-void InfomaxICA::getSources(const Matrix &dataset, Matrix &s){
-  s.Init(dataset.n_rows(),dataset.n_cols());
-  la::MulExpert(1.0,false,w_,false,dataset,0.0,&s);
+void InfomaxICA::getSources(const arma::mat& dataset, arma::mat& s) {
+  s = w_ * dataset;
 }
 
-void InfomaxICA::setLambda(const double lambda){
-  lambda_=lambda;
+void InfomaxICA::setLambda(const double lambda) {
+  lambda_ = lambda;
 }
 
-void InfomaxICA::setB(const index_t b){
-  b_=b;
+void InfomaxICA::setB(const index_t b) {
+  b_ = b;
 }
 
-void InfomaxICA::setEpsilon(const double epsilon){
-  epsilon_=epsilon;
+void InfomaxICA::setEpsilon(const double epsilon) {
+  epsilon_ = epsilon;
 }
--- fastlib/branches/fastlib-stl/mlpack/infomax_ica/main.cc
+++ fastlib/branches/fastlib-stl/mlpack/infomax_ica/main.cc
@@ -11,7 +11,7 @@
 #include <fastlib/data/dataset.h>
 
 #include <armadillo>
-#include <fastlib/base/arma_compat.h>
+
 
 
 
@@ -28,14 +28,13 @@ int main(int argc, char *argv[]) {
   int B = IO::GetParam<int>("info/B");
   double epsilon = IO::GetParam<double>("info/epsilon");
 
-  Matrix dataset;
-  arma::mat tmp_dataset;
-  data::Load(data_file_name, tmp_dataset);
-  arma_compat::armaToMatrix(tmp_dataset, dataset);
-  InfomaxICA *ica = new InfomaxICA(lambda, B, epsilon);
+  arma::mat dataset;
+  data::Load(data_file_name, dataset);
 
-  ica->applyICA(dataset);  
-  Matrix west;
-  ica->getUnmixing(west);
+  InfomaxICA ica(lambda, B, epsilon);
+  ica.applyICA(dataset);  
+  
+  arma::mat west;
+  ica.getUnmixing(west);
   //ica->displayMatrix(west);
 }
