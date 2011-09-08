da59986dbc13450fd32cc810358dd1405b255a48
Ryan Curtin <ryan@ratml.org>
2011-09-08 18:45:55 +0000

    I must be missing something, it seems like u64 is not actually defined on older Armadillo versions like I thought it was.

--- fastlib/branches/fastlib-stl/fastlib/base/arma_extend/typedef.hpp
+++ fastlib/branches/fastlib-stl/fastlib/base/arma_extend/typedef.hpp
@@ -1,6 +1,6 @@
 // Extensions to typedef u64 and s64 until that support is added into
 // Armadillo.  We only need to typedef s64 on Armadillo >= 1.2.0
-
+/*
 #if   (ARMA_VERSION_MAJOR >= 1) && \
       (ARMA_VERSION_MINOR >= 2) && \
       (ARMA_VERSION_PATCH >= 0)
@@ -13,6 +13,7 @@
     #error "don't know how to typedef 's64' on this system"
   #endif
 #else
+*/
   // We must typedef both u64 and s64.
   #if   ULONG_MAX >= 0xffffffffffffffff
     typedef unsigned long u64;
@@ -23,4 +24,4 @@
   #else
     #error "don't know how to typedef 'u64' on this system"
   #endif
-#endif
+//#endif
