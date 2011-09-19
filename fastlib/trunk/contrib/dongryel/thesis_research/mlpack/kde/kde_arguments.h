97cb2b7c120a8e43a07d18fdc389f3b572ce8c28
Bill March <march@ices.utexas.edu>
2011-09-19 21:05:26 +0000

    wrote simple, serial two_point_correlation code; currently only does DD and DR counts (and not at the same time)

--- fastlib/trunk/contrib/dongryel/thesis_research/mlpack/kde/kde_arguments.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/mlpack/kde/kde_arguments.h
@@ -10,137 +10,137 @@
 #include "core/metric_kernels/lmetric.h"
 
 namespace mlpack {
-namespace kde {
-
-/** @brief The class that manages the arguments necessary for KDE
- *         computation.
- */
-template<typename TableType>
-class KdeArguments {
-  public:
-
-    std::string densities_out_;
-
-    int leaf_size_;
-
-    TableType *reference_table_;
-
-    TableType *query_table_;
-
-    double effective_num_reference_points_;
-
-    double bandwidth_;
-
-    double absolute_error_;
-
-    double relative_error_;
-
-    double probability_;
-
-    std::string kernel_;
-
-    std::string series_expansion_type_;
-
-    core::metric_kernels::LMetric<2> *metric_;
-
-    bool tables_are_aliased_;
-
-    bool normalize_densities_;
-
-    int num_iterations_in_;
-
-  public:
-
-    template<typename GlobalType>
-    void Init(
-      TableType *reference_table_in, TableType *query_table_in,
-      GlobalType &global_in) {
-      reference_table_ = reference_table_in;
-      query_table_ = query_table_in;
-      effective_num_reference_points_ =
-        global_in.effective_num_reference_points();
-      bandwidth_ = global_in.bandwidth();
-      absolute_error_ = global_in.absolute_error();
-      relative_error_ = global_in.relative_error();
-      probability_ = global_in.probability();
-      kernel_ = global_in.kernel().name();
-      series_expansion_type_ = global_in.series_expansion_type();
-      tables_are_aliased_ = true;
-      normalize_densities_ = global_in.normalize_densities();
-    }
-
-    template<typename GlobalType>
-    void Init(GlobalType &global_in) {
-      reference_table_ = global_in.reference_table()->local_table();
-      if(reference_table_ != query_table_) {
-        query_table_ = global_in.query_table()->local_table();
-      }
-      effective_num_reference_points_ =
-        global_in.effective_num_reference_points();
-      bandwidth_ = global_in.bandwidth();
-      absolute_error_ = global_in.absolute_error();
-      relative_error_ = global_in.relative_error();
-      probability_ = global_in.probability();
-      kernel_ = global_in.kernel().name();
-      series_expansion_type_ = global_in.series_expansion_type();
-      tables_are_aliased_ = true;
-      normalize_densities_ = global_in.normalize_densities();
-    }
-
-    /** @brief The default constructor.
+  namespace kde {
+    
+    /** @brief The class that manages the arguments necessary for KDE
+     *         computation.
      */
-    KdeArguments() {
-      leaf_size_ = 0;
-      reference_table_ = NULL;
-      query_table_ = NULL;
-      effective_num_reference_points_ = 0.0;
-      bandwidth_ = 0.0;
-      absolute_error_ = 0.0;
-      relative_error_ = 0.0;
-      probability_ = 0.0;
-      kernel_ = "";
-      series_expansion_type_ = "";
-      metric_ = NULL;
-      tables_are_aliased_ = false;
-      normalize_densities_ = true;
-      num_iterations_in_ = 0;
-    }
-
-    /** @brief The destructor.
-     */
-    ~KdeArguments() {
-
-      // If the tables are not aliased,
-      if(tables_are_aliased_ == false) {
-
-        // If monchromatic, destroy only one of the tables.
-        if(reference_table_ == query_table_) {
-          if(reference_table_ != NULL) {
-            delete reference_table_;
-          }
-        }
-        else {
-
-          // Otherwise, we have to check and destroy both.
-          if(reference_table_ != NULL) {
-            delete reference_table_;
-          }
-          if(query_table_ != NULL) {
-            delete query_table_;
-          }
-        }
+    template<typename TableType>
+    class KdeArguments {
+    public:
+      
+      std::string densities_out_;
+      
+      int leaf_size_;
+      
+      TableType *reference_table_;
+      
+      TableType *query_table_;
+      
+      double effective_num_reference_points_;
+      
+      double bandwidth_;
+      
+      double absolute_error_;
+      
+      double relative_error_;
+      
+      double probability_;
+      
+      std::string kernel_;
+      
+      std::string series_expansion_type_;
+      
+      core::metric_kernels::LMetric<2> *metric_;
+      
+      bool tables_are_aliased_;
+      
+      bool normalize_densities_;
+      
+      int num_iterations_in_;
+      
+    public:
+      
+      template<typename GlobalType>
+      void Init(
+                TableType *reference_table_in, TableType *query_table_in,
+                GlobalType &global_in) {
+        reference_table_ = reference_table_in;
+        query_table_ = query_table_in;
+        effective_num_reference_points_ =
+        global_in.effective_num_reference_points();
+        bandwidth_ = global_in.bandwidth();
+        absolute_error_ = global_in.absolute_error();
+        relative_error_ = global_in.relative_error();
+        probability_ = global_in.probability();
+        kernel_ = global_in.kernel().name();
+        series_expansion_type_ = global_in.series_expansion_type();
+        tables_are_aliased_ = true;
+        normalize_densities_ = global_in.normalize_densities();
       }
-      reference_table_ = NULL;
-      query_table_ = NULL;
-
-      // Destroy the metric.
-      if(metric_ != NULL) {
-        delete metric_;
+      
+      template<typename GlobalType>
+      void Init(GlobalType &global_in) {
+        reference_table_ = global_in.reference_table()->local_table();
+        if(reference_table_ != query_table_) {
+          query_table_ = global_in.query_table()->local_table();
+        }
+        effective_num_reference_points_ =
+        global_in.effective_num_reference_points();
+        bandwidth_ = global_in.bandwidth();
+        absolute_error_ = global_in.absolute_error();
+        relative_error_ = global_in.relative_error();
+        probability_ = global_in.probability();
+        kernel_ = global_in.kernel().name();
+        series_expansion_type_ = global_in.series_expansion_type();
+        tables_are_aliased_ = true;
+        normalize_densities_ = global_in.normalize_densities();
+      }
+      
+      /** @brief The default constructor.
+       */
+      KdeArguments() {
+        leaf_size_ = 0;
+        reference_table_ = NULL;
+        query_table_ = NULL;
+        effective_num_reference_points_ = 0.0;
+        bandwidth_ = 0.0;
+        absolute_error_ = 0.0;
+        relative_error_ = 0.0;
+        probability_ = 0.0;
+        kernel_ = "";
+        series_expansion_type_ = "";
         metric_ = NULL;
+        tables_are_aliased_ = false;
+        normalize_densities_ = true;
+        num_iterations_in_ = 0;
       }
-    }
-};
-}
+      
+      /** @brief The destructor.
+       */
+      ~KdeArguments() {
+        
+        // If the tables are not aliased,
+        if(tables_are_aliased_ == false) {
+          
+          // If monchromatic, destroy only one of the tables.
+          if(reference_table_ == query_table_) {
+            if(reference_table_ != NULL) {
+              delete reference_table_;
+            }
+          }
+          else {
+            
+            // Otherwise, we have to check and destroy both.
+            if(reference_table_ != NULL) {
+              delete reference_table_;
+            }
+            if(query_table_ != NULL) {
+              delete query_table_;
+            }
+          }
+        }
+        reference_table_ = NULL;
+        query_table_ = NULL;
+        
+        // Destroy the metric.
+        if(metric_ != NULL) {
+          delete metric_;
+          metric_ = NULL;
+        }
+      }
+    };
+  }
 }
 
 #endif
