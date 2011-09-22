c5038115df78b3913f21fb263d98616cb271e55b
tekhnofiend <nishantmehta.x@gmail.com>
2011-09-22 03:25:21 +0000

    made PoissonSparseCoding call ComputePoissonFullObjective

--- fastlib/branches/fastlib-stl/contrib/niche/non_fastlib/exponential_family_sparse_coding/ComputePoissonFullObjective.m
+++ fastlib/branches/fastlib-stl/contrib/niche/non_fastlib/exponential_family_sparse_coding/ComputePoissonFullObjective.m
@@ -1,6 +1,4 @@
-function f = ComputeFullObjective(D, S, T, lambda)
-%function f = ComputeFullObjective(D, S, T, lambda)
-
-n = size(S, 2);
+function f = ComputePoissonFullObjective(D, S, T, lambda)
+%function f = ComputePoissonFullObjective(D, S, T, lambda)
 
 f = -trace(D' * T * S') + sum(sum(exp(D * S))) + lambda * sum(sum(abs(S)))
