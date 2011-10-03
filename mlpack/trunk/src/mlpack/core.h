eb0fea06615a5df243bc39de31b80af51efa55de
Ryan Curtin <ryan@ratml.org>
2011-10-03 01:45:16 +0000

    Add that core.h file which we were using in place of fastlib/fastlib.h.

---
+++ mlpack/trunk/src/mlpack/core.h
@@ -0,0 +1,31 @@
+/***
+ * @file mlpack_core.h
+ *
+ * Include all of the base components required to write MLPACK methods.
+ */
+#ifndef __MLPACK_CORE_H
+#define __MLPACK_CORE_H
+
+// First, standard includes.
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <cmath>
+#include <math.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <iostream>
+
+// And then the Armadillo library.
+#include <armadillo>
+
+// Now MLPACK-specific includes.
+#include <mlpack/core/data/dataset.h>
+#include <mlpack/core/math/math_lib.h>
+#include <mlpack/core/file/textfile.h>
+#include <mlpack/core/io/io.h>
+#include <mlpack/core/arma_extend/arma_extend.h>
+
+#endif
