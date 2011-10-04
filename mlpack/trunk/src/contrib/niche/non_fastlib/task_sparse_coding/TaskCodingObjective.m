4aebb6d9750e66eff5b3c75132e7b9064d4e5004
tekhnofiend <nishantmehta.x@gmail.com>
2011-10-04 20:36:29 +0000

    added error computation

--- mlpack/trunk/src/contrib/niche/non_fastlib/task_sparse_coding/TaskCodingObjective.m
+++ mlpack/trunk/src/contrib/niche/non_fastlib/task_sparse_coding/TaskCodingObjective.m
@@ -2,13 +2,17 @@ function f = TaskCodingObjective(X, Y, W, Z, lambda_w, lambda_z)
 %function f = TaskCodingObjective(X, Y, W, Z, lambda_w, lambda_z)
 %
 
+n_points = size(Y, 1);
 n_tasks = size(Z, 2);
 
 hinge_loss = 0;
+n_errors = 0;
 for t = 1:n_tasks
-  hinge_loss = hinge_loss ...
-      + sum(max(1 - Y(:,t) .* (X(:,:,t)' * (W * Z(:,t))), 0));
+  predictions = Y(:,t) .* (X(:,:,t)' * (W * Z(:,t)));
+  hinge_loss = hinge_loss + sum(max(1 - predictions, 0));
+  n_errors = n_errors + sum(predictions <= 0);
 end
 
+fprintf('Training Error: %f%%\n', 100 * n_errors / (n_tasks * n_points));
 f = hinge_loss + lambda_z * sum(sum(abs(Z))) + 0.5 * lambda_w * norm(W, 'fro')^2;
 fprintf('Objective Value: %f\n', f);
