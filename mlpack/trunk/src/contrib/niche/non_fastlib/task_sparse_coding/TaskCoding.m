845b150941dc6c2d3c248b93d2daa365542e8157
niche <niche@9d5b8971-822b-0410-80eb-d18c1038ef23>
2011-10-04 20:36:42 +0000

    various changes to TaskCoding.m

--- mlpack/trunk/src/contrib/niche/non_fastlib/task_sparse_coding/TaskCoding.m
+++ mlpack/trunk/src/contrib/niche/non_fastlib/task_sparse_coding/TaskCoding.m
@@ -1,6 +1,7 @@
-function [W, Z] = TaskCoding(X, Y, n_atoms, lambda_w, lambda_z, tol)
-%function [W, Z] = TaskCoding(X, Y, n_atoms, lambda_w, lambda_z, tol)
-%
+function [W, Z] = TaskCoding(X, Y, n_atoms, lambda_w, lambda_z, ...
+			     W_initial, Z_initial, tol)
+%function [W, Z] = TaskCoding(X, Y, n_atoms, lambda_w, lambda_z, ...
+%			     W_initial, Z_initial, tol)
 % X is tensor in R^{n_dims \times n_points \times n_tasks}
 % Y is a matrix in R^{n_points \times n_tasks}
 % n_atoms is the number of atoms in the dictionary of estimators
@@ -8,14 +9,27 @@ function [W, Z] = TaskCoding(X, Y, n_atoms, lambda_w, lambda_z, tol)
 % lambda_z is the regularization term for l1-norm penalties on each Z_t
 % tol is the tolerance within which the objective must have converged to stop the algorithm
 
+[n_dims n_points n_tasks] = size(X);
+
 if nargin < 6
+  % a crude initialization of W
+  W = normrnd(0, 1 , n_dims, n_atoms);
+else
+  fprintf('Using W_initial\n');
+  W = W_initial;
+end
+
+if nargin == 7
+  Z = Z_initial;
+  fprintf('Running Pegasos\n');
+  W = TaskCodingWStep(X, Y, Z, lambda_w);
+  obj = TaskCodingObjective(X, Y, W, Z, lambda_w, lambda_z);
+end
+
+if nargin < 8
   tol = 1e-6;
 end
 
-[n_dims n_points n_tasks] = size(X);
-
-% initialize W somehow
-W = normrnd(0,1,n_dims, n_atoms);
 
 last_obj = 1e99;
 converged = false;
@@ -23,8 +37,12 @@ n_max_iterations = 50;
 iteration_num = 0;
 while ~converged && iteration_num < n_max_iterations
   iteration_num = iteration_num + 1;
+  save LPSVM X Y W lambda_z;
+  fprintf('Running LPSVM\n');
   Z = TaskCodingZStep(X, Y, W, lambda_z);
   obj = TaskCodingObjective(X, Y, W, Z, lambda_w, lambda_z);
+  save Pegasos X Y Z lambda_w;
+  fprintf('Running Pegasos\n');
   W = TaskCodingWStep(X, Y, Z, lambda_w);
   obj = TaskCodingObjective(X, Y, W, Z, lambda_w, lambda_z);
   
