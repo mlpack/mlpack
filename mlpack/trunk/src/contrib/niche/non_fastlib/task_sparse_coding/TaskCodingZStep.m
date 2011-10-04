fa048ed2905c686c48fe3ac113ea5ef0a86a2e72
tekhnofiend <nishantmehta.x@gmail.com>
2011-10-04 20:34:47 +0000

    now passing delta parameter to lpsvm, as it fails miserably otherwise

--- mlpack/trunk/src/contrib/niche/non_fastlib/task_sparse_coding/TaskCodingZStep.m
+++ mlpack/trunk/src/contrib/niche/non_fastlib/task_sparse_coding/TaskCodingZStep.m
@@ -5,8 +5,11 @@ function Z = TaskCodingZStep(X, Y, W, lambda_z)
 n_tasks = size(Y, 2);
 n_atoms = size(W, 2);
 
+output = 0;
+nu = 1 / lambda_z;
 Z = zeros(n_atoms, n_tasks);
 for t = 1:n_tasks
   V = W' * X(:,:,t);
-  Z(:,t) = lpsvm(V', Y(:,t), 0, 1 / lambda_z, 0);
+  [Z(:,t), gamma] = lpsvm(V', Y(:,t), 1, nu, output, 10);
+  %fprintf('gamma = %f\n', gamma);
 end
