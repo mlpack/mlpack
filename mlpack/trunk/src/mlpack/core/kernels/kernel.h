ebd20ecdb8d0f8e20c311fc96975e0cc40d10cf0
James Cline <jamesclinedev@gmail.com>
2011-10-07 23:00:24 +0000

    helps to add the files

---
+++ mlpack/trunk/src/mlpack/core/kernels/kernel.h
@@ -0,0 +1,9 @@
+#ifndef __MLPACK_CORE_KERNELS_H
+#define __MLPACK_CORE_KERNELS_H
+
+// This file is just to make it easier to include all the kernels
+
+#include "linear_kernel.h"
+#include "gaussian_rbf_kernel.h"
+
+#endif
