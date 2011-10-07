3e3e3b100d5ca80e1833c88306c39b7959ab4b14
tekhnofiend <nishantmehta.x@gmail.com>
2011-10-07 16:40:29 +0000

    added pegasos minibatch without replacement (bouttou style)

--- mlpack/trunk/src/contrib/niche/non_fastlib/task_sparse_coding/pegasos.m
+++ mlpack/trunk/src/contrib/niche/non_fastlib/task_sparse_coding/pegasos.m
@@ -11,7 +11,7 @@ end
 if mini_batch_size == 1
   w = pegasos_trivial_minibatch(X, Y, lambda, n_iterations, w_initial);
 else
-  w = pegasos_minibatch(X, Y, lambda, mini_batch_size, n_iterations, w_initial);
+  w = pegasos_minibatch_bottou(X, Y, lambda, mini_batch_size, n_iterations, w_initial);
 end
 
 n_errors = sum(Y .* (X' * w) <= 0);
