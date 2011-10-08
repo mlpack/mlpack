3b132649f603f1e66d5828883456934b664b1a89
Dongryeol Lee <drselee@gmail.com>
2011-10-08 15:58:42 +0000

    Reference tree walker finished. This will save memory by conservatively expanding the reference frontier (which has direct impact on the hashed send list.

--- fastlib/trunk/contrib/dongryel/thesis_research/core/parallel/reference_tree_walker.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/parallel/reference_tree_walker.h
@@ -53,18 +53,21 @@ class ReferenceTreeWalker {
 
     TableType *local_reference_table_;
 
+    unsigned long int max_num_reference_points_to_pack_per_process_;
+
     int max_reference_subtree_size_;
 
     std::vector< unsigned long int >
     num_reference_points_assigned_per_process_;
 
-    core::gnp::DualtreeTace< std::pair<TreeType *, TreeType *> > trace_;
+    core::gnp::DualtreeTrace< std::pair<TreeType *, TreeType *> > trace_;
 
     bool weak_scaling_measuring_mode_;
 
   private:
 
     void HashSendList_(
+      boost::mpi::communicator &world,
       const std::pair<int, int> &local_rnode_id,
       int query_process_id,
       std::vector <
@@ -85,11 +88,11 @@ class ReferenceTreeWalker {
         hashed_essential_reference_subtrees->resize(
           hashed_essential_reference_subtrees->size() + 1);
         found_index = hashed_essential_reference_subtrees->size() - 1;
-        (*hashed_essential_reference_subtrees)[ found_index ].Init(*world_);
+        (*hashed_essential_reference_subtrees)[ found_index ].Init(world);
         (*hashed_essential_reference_subtrees)[
           found_index].object().Init(
-            reference_table_->local_table(),
-            reference_table_->local_table()->get_tree()->FindByBeginCount(
+            local_reference_table_,
+            local_reference_table_->get_tree()->FindByBeginCount(
               local_rnode_id.first, local_rnode_id.second), false);
         (*hashed_essential_reference_subtrees)[
           found_index].set_object_is_valid_flag(true);
@@ -105,6 +108,8 @@ class ReferenceTreeWalker {
     ReferenceTreeWalker() {
       query_table_ = NULL;
       local_reference_table_ = NULL;
+      max_num_reference_points_to_pack_per_process_ =
+        std::numeric_limits<unsigned long int>::max();
       max_reference_subtree_size_ = 0;
       weak_scaling_measuring_mode_ = false;
     }
@@ -124,18 +129,27 @@ class ReferenceTreeWalker {
       *hashed_essential_reference_subtrees_to_send,
       DistributedDualtreeTaskQueueType *distributed_tasks_in) {
 
-      // Push a blank argument to the trace for making the exit phase.
-      trace_.push_front(std::pair<TreeType *, TreeType *>(NULL, NULL));
-
-      // Pop the next item to visit in the list.
-      std::pair<TreeType *, TreeType *> args = trace_.back();
-      trace_.pop_back();
+      // Return if the walk is done.
+      if(trace_.empty()) {
+        return;
+      }
 
       do {
+
+        // Termination flag.
+        bool should_terminate = false;
+
+        // Push a blank argument to the trace for making the exit phase.
+        if(trace_.front().first != NULL) {
+          trace_.push_front(std::pair<TreeType *, TreeType *>(NULL, NULL));
+        }
+
+        // Pop the next item to visit in the list.
+        std::pair<TreeType *, TreeType *> args = trace_.back();
+        trace_.pop_back();
+
         while(
-          trace_.empty() == false && args.second != NULL &&
-          hashed_essential_reference_subtrees_to_send->size() <
-          max_hashed_subtrees_to_queue) {
+          trace_.empty() == false && args.second != NULL) {
 
           // Get the arguments.
           TreeType *global_query_node = args.first;
@@ -145,7 +159,7 @@ class ReferenceTreeWalker {
               metric_in, local_reference_node->bound());
 
           // Determine if the pair can be pruned.
-          if(global().ConsiderExtrinsicPrune(squared_distance_range)) {
+          if(global_in.ConsiderExtrinsicPrune(squared_distance_range)) {
             typename TableType::TreeIterator qnode_it =
               query_table_->get_node_iterator(global_query_node);
             while(qnode_it.HasNext()) {
@@ -154,104 +168,126 @@ class ReferenceTreeWalker {
               extrinsic_prunes_[query_process_id] +=
                 local_reference_node->count();
             }
-            continue;
-          }
 
-          if(global_query_node->is_leaf()) {
+          } // end of prunable case.
 
-            // If the reference node size is within the size limit, then for
-            // each query process in the set, add the reference node to the
-            // list.
-            if(local_reference_node->count() <= max_reference_subtree_size_ ||
-                local_reference_node->is_leaf()) {
-              typename TableType::TreeIterator qnode_it =
-                query_table_->get_node_iterator(global_query_node);
-              std::pair<int, int> local_rnode_id(
-                local_reference_node->begin(), local_reference_node->count());
-              while(qnode_it.HasNext()) {
-                int query_process_id;
-                qnode_it.Next(&query_process_id);
+          // Non-prunable-case.
+          else {
 
-                // Only add if, weak scaling measuring is disabled or the
-                // query process rank differs from the reference process rank
-                // by at most 2 bits.
-                if((! weak_scaling_measuring_mode_)  ||
-                    (core::math::BitCount(
-                       query_process_id, world_->rank()) <= 2 &&
-                     num_reference_points_assigned_per_process_[
-                       query_process_id ] <
-                     max_num_reference_points_to_pack_per_process_)) {
-                  essential_reference_subtrees_[
-                    query_process_id ].push_back(local_rnode_id);
-                  num_reference_points_assigned_per_process_[
-                    query_process_id ] += local_rnode_id.second;
+            if(global_query_node->is_leaf()) {
 
-                  // Add the query process ID to the list of query processes
-                  // that this reference subtree needs to be sent to.
-                  if(query_process_id != world_->rank()) {
-                    HashSendList_(
-                      local_rnode_id, query_process_id,
-                      hashed_essential_reference_subtrees);
+              // If the reference node size is within the size limit, then for
+              // each query process in the set, add the reference node to the
+              // list.
+              if(local_reference_node->count() <= max_reference_subtree_size_ ||
+                  local_reference_node->is_leaf()) {
+                typename TableType::TreeIterator qnode_it =
+                  query_table_->get_node_iterator(global_query_node);
+                std::pair<int, int> local_rnode_id(
+                  local_reference_node->begin(), local_reference_node->count());
+                while(qnode_it.HasNext()) {
+                  int query_process_id;
+                  qnode_it.Next(&query_process_id);
+
+                  // Only add if, weak scaling measuring is disabled or the
+                  // query process rank differs from the reference process rank
+                  // by at most 2 bits.
+                  if((! weak_scaling_measuring_mode_)  ||
+                      (core::math::BitCount(
+                         query_process_id, world.rank()) <= 2 &&
+                       num_reference_points_assigned_per_process_[
+                         query_process_id ] <
+                       max_num_reference_points_to_pack_per_process_)) {
+                    essential_reference_subtrees_[
+                      query_process_id ].push_back(local_rnode_id);
+                    num_reference_points_assigned_per_process_[
+                      query_process_id ] += local_rnode_id.second;
+
+                    // Add the query process ID to the list of query processes
+                    // that this reference subtree needs to be sent to.
+                    if(query_process_id != world.rank()) {
+                      HashSendList_(
+                        world, local_rnode_id, query_process_id,
+                        hashed_essential_reference_subtrees_to_send);
+                    }
+                  } // non-prune case.
+
+                  // Otherwise, consider it as pruned.
+                  else {
+                    extrinsic_prunes_ [
+                      query_process_id] += local_reference_node->count();
                   }
                 }
+              } // qnode leaf, rnode leaf.
+              else {
+                trace_.push_back(
+                  std::pair <
+                  TreeType *, TreeType * > (
+                    global_query_node, local_reference_node->left()));
+                trace_.push_front(
+                  std::pair <
+                  TreeType *, TreeType * > (
+                    global_query_node, local_reference_node->right()));
+              } // qnode leaf, rnode non-leaf.
+            } // end of query node being a leaf.
 
-                // Otherwise, consider it as pruned.
-                else {
-                  extrinsic_prunes_ [
-                    query_process_id] += local_reference_node->count();
-                }
-              }
-            }
             else {
-              trace_.push_back(
-                std::pair <
-                TreeType *, TreeType * > (
-                  global_query_node, local_reference_node->left()));
-              trace_.push_front(
-                std::pair <
-                TreeType *, TreeType * > (
-                  global_query_node, local_reference_node->right()));
-            }
-            continue;
-          }
 
-          // Here, we know that the global query node is a non-leaf, so we
-          // need to split both ways.
-          if(local_reference_node->count() <= max_reference_subtree_size_ ||
-              local_reference_node->is_leaf()) {
-            trace_.push_back(
-              std::pair <
-              TreeType *, TreeType * > (
-                global_query_node->left(), local_reference_node));
-            trace_.push_front(
-              std::pair <
-              TreeType *, TreeType * > (
-                global_query_node->right(), local_reference_node));
+              // Here, we know that the global query node is a non-leaf, so we
+              // need to split both ways.
+              if(local_reference_node->count() <= max_reference_subtree_size_ ||
+                  local_reference_node->is_leaf()) {
+                trace_.push_back(
+                  std::pair <
+                  TreeType *, TreeType * > (
+                    global_query_node->left(), local_reference_node));
+                trace_.push_front(
+                  std::pair <
+                  TreeType *, TreeType * > (
+                    global_query_node->right(), local_reference_node));
+              } // qnode non-leaf, rnode leaf.
+              else {
+                trace_.push_back(
+                  std::pair <
+                  TreeType *, TreeType * > (
+                    global_query_node->left(), local_reference_node->left()));
+                trace_.push_front(
+                  std::pair <
+                  TreeType *, TreeType * > (
+                    global_query_node->left(), local_reference_node->right()));
+                trace_.push_back(
+                  std::pair <
+                  TreeType *, TreeType * > (
+                    global_query_node->right(), local_reference_node->left()));
+                trace_.push_front(
+                  std::pair <
+                  TreeType *, TreeType * > (
+                    global_query_node->right(), local_reference_node->right()));
+              } // qnode, rnode non-leaves.
+            } // end of non-leaf qnode case.
+          } // end of non-prunable case.
+
+          // Pop the next item in the list, if we should continue.
+          should_terminate =
+            ((world.size() == 1 && essential_reference_subtrees_[world.rank()].size() >= 1) ||
+             (world.size() > 1 && static_cast<int>(hashed_essential_reference_subtrees_to_send->size()) >= max_hashed_subtrees_to_queue)  || trace_.empty());
+
+          if(! should_terminate) {
+            args = trace_.back();
+            trace_.pop_back();
           }
           else {
-            trace_.push_back(
-              std::pair <
-              TreeType *, TreeType * > (
-                global_query_node->left(), local_reference_node->left()));
-            trace_.push_front(
-              std::pair <
-              TreeType *, TreeType * > (
-                global_query_node->left(), local_reference_node->right()));
-            trace_.push_back(
-              std::pair <
-              TreeType *, TreeType * > (
-                global_query_node->right(), local_reference_node->left()));
-            trace_.push_front(
-              std::pair <
-              TreeType *, TreeType * > (
-                global_query_node->right(), local_reference_node->right()));
+            break;
           }
+
         } // end of the inner loop.
 
         // The termination condition.
-        if(trace_.empty() ||
-            hashed_essential_reference_subtrees_to_send->size() >=
-            max_hashed_subtrees_to_queue) {
+        should_terminate =
+          ((world.size() == 1 && essential_reference_subtrees_[world.rank()].size() >= 1) ||
+           (world.size() > 1 && static_cast<int>(hashed_essential_reference_subtrees_to_send->size()) >= max_hashed_subtrees_to_queue) ||
+           trace_.empty());
+        if(should_terminate) {
           break;
         }
       }
@@ -260,27 +296,39 @@ class ReferenceTreeWalker {
       // Generate tasks on the self.
       std::vector< boost::tuple<int, int, int, int> > received_subtable_ids;
       for(unsigned int j = 0;
-          j < essential_reference_subtrees_to_send_[world.rank()].size(); j++) {
+          j < essential_reference_subtrees_[world.rank()].size(); j++) {
 
         int reference_begin =
-          essential_reference_subtrees_to_send_[ world.rank()][j].first;
+          essential_reference_subtrees_[ world.rank()][j].first;
         int reference_count =
-          essential_reference_subtrees_to_send_[ world.rank()][j].second;
+          essential_reference_subtrees_[ world.rank()][j].second;
 
         // Reference subtables on the self are already available.
         received_subtable_ids.push_back(
-          boost::make_tuple(i, reference_begin, reference_count, -1));
+          boost::make_tuple(
+            world.rank() , reference_begin, reference_count, -1));
       }
 
       // Fill out the initial task consisting of the reference trees on
       // the same process.
       distributed_tasks_in->GenerateTasks(
-        world, metric, received_subtable_ids);
-      essential_reference_subtrees_to_send_.resize(0);
-      essential_reference_subtrees_to_send_.resize(world.size());
+        world, metric_in, received_subtable_ids);
+      essential_reference_subtrees_.resize(0);
+      essential_reference_subtrees_.resize(world.size());
 
+      // For each query process, queue up how much computation is
+      // pruned.
+      for(int i = 0; i < world.size(); i++) {
+        unsigned long int completed_work =
+          static_cast<unsigned long int>(
+            query_table_->local_n_entries(i)) *
+          extrinsic_prunes_[ i ];
+        distributed_tasks_in->push_completed_computation(
+          world, i, extrinsic_prunes_[i], completed_work);
 
-      // Queue prune counts.
+        // Clear the extrinsic prunes.
+        extrinsic_prunes_[i] = 0;
+      }
     }
 
     /** @brief Initializes the reference tree walker.
@@ -290,7 +338,13 @@ class ReferenceTreeWalker {
       DistributedTableType *query_table_in,
       TableType *local_reference_table_in,
       int max_reference_subtree_size_in,
-      bool weak_scaling_mode_in) {
+      bool weak_scaling_mode_in,
+      unsigned long int max_num_reference_points_to_pack_per_process_in) {
+
+      // Set the maximum number of reference points to pack per
+      // process.
+      max_num_reference_points_to_pack_per_process_ =
+        max_num_reference_points_to_pack_per_process_in;
 
       // Set the weak scaling mode.
       weak_scaling_measuring_mode_ = weak_scaling_mode_in;
