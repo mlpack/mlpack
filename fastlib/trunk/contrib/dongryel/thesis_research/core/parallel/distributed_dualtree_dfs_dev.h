35bff937103e769e171e1cbc406ae1a807b57f4b
Dongryeol Lee <drselee@gmail.com>
2011-10-09 16:02:15 +0000

    Another tweak.

--- fastlib/trunk/contrib/dongryel/thesis_research/core/parallel/distributed_dualtree_dfs_dev.h
+++ fastlib/trunk/contrib/dongryel/thesis_research/core/parallel/distributed_dualtree_dfs_dev.h
@@ -115,12 +115,12 @@ void DistributedDualtreeDfs<DistributedProblemType>::AllToAllIReduce_(
         // Determine if we need to walk the reference tree to make
         // more messages.
         if((world_->size() == 1 &&
-            distributed_tasks.num_remaining_tasks() < 4 * omp_get_num_threads()) ||
+            distributed_tasks.num_remaining_tasks() < 2 * omp_get_num_threads()) ||
             (world_->size() > 1 &&
              static_cast<int>(hashed_essential_reference_subtrees_to_send.size()) <
-             4 * omp_get_num_threads())) {
+             2 * omp_get_num_threads())) {
           distributed_tasks.WalkReferenceTree(
-            metric, problem_->global(), *world_, 8 * omp_get_num_threads(),
+            metric, problem_->global(), *world_, 2 * omp_get_num_threads(),
             query_table_->get_tree(),
             & hashed_essential_reference_subtrees_to_send);
         }
