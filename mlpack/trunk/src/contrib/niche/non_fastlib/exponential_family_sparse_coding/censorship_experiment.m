a968dc7a0a06861b1266b46094d8d8bc10bbf110
tekhnofiend <nishantmehta.x@gmail.com>
2011-10-21 20:38:35 +0000

    lots of changes, including loading of bush half censored dataset

--- mlpack/trunk/src/contrib/niche/non_fastlib/exponential_family_sparse_coding/censorship_experiment.m
+++ mlpack/trunk/src/contrib/niche/non_fastlib/exponential_family_sparse_coding/censorship_experiment.m
@@ -1,6 +1,7 @@
 load ap_small;
 tf_orig = tf;
 load ap_small_dukakis_censored;
+%load ap_small_bush_half_censored;
 tf_cens = tf;
 
 tf_orig = tf_orig(:,[1:952 954:end]);
@@ -15,16 +16,18 @@ clear normalization_matrix;
 
 
 
-
 n_atoms = 10;
 
-opts = statset('Display','iter', 'MaxIter', 3);
+opts = statset('Display','iter', 'MaxIter', 10);
 disp(opts);
 [IDX C] = kmeans(tf_orig_normalized', n_atoms, 'Options', opts);
 C_normalized = full(normcols(C'));
 
 lambda = 0.2;
-max_iter = 10;
+max_iter = 100;
+
+return;
+
 [D S] = ExpFamSparseCoding('g', tf_orig_normalized, n_atoms, lambda, max_iter, ...
 			   false,  C_normalized);
 
@@ -33,9 +36,12 @@ for j=1:n_atoms
   disp(vocab(i(end-9:end))');
 end
 
-max_iter_perturbed = 10;
-lambda_Q = 0.3;
+%max_iter_perturbed = 100;
+%lambda_Q = 0.3;
 
-[Q1, S1] = ExpFamSparseCodingPerturbed('g', tf_cens_normalized, ...
-				       n_atoms, lambda, lambda_Q, ...
-				       max_iter_perturbed, false, D);
+% settings which show recognition of dukakis censoring
+max_iter_perturbed = 20;
+lambda_Q = 0.2;
+
+[Q_orig, S_orig] = ExpFamSparseCodingPerturbed('g', tf_orig_normalized, n_atoms, lambda, lambda_Q, max_iter_perturbed, false, D);
+[Q_cens, S_cens] = ExpFamSparseCodingPerturbed('g', tf_cens_normalized, n_atoms, lambda, lambda_Q, max_iter_perturbed, false, D);
