2cb78a3e99d18d95e813ca8544e98fe5e9e416a7
Ryan Curtin <ryan@ratml.org>
2011-10-31 18:51:24 +0000

    Don't include textfile.h because it doesn't exist.

--- src/mlpack/core.h
+++ src/mlpack/core.h
@@ -93,6 +93,5 @@
 #include <mlpack/core/math/math_misc.hpp>
 #include <mlpack/core/math/range.hpp>
 #include <mlpack/core/utilities/save_restore_utility.hpp>
-#include <mlpack/core/file/textfile.hpp>
 
 #endif
