e9381c97103e3f6e46650ea15d6ceb473f700b6b
Ajinkya Kale <kaleajinkya@gmail.com>
2011-11-11 21:29:55 +0000

    added comments and made some formatting changes

--- src/mlpack/methods/pca/pca.hpp
+++ src/mlpack/methods/pca/pca.hpp
@@ -1,13 +1,33 @@
+/**
+ * @file pca.hpp
+ *
+ * Defines the PCA class to perform Principal Components Analysis on the
+ * specified data set.
+ */
 #include <mlpack/core.h>
 namespace mlpack {
 namespace pca {
 
-class PCA {
+class PCA
+{
+ public:
+  PCA();
 
-	public:
-		PCA();
-		void Apply(arma::mat& coeff, arma::mat& score, arma::mat& data);
+  /**
+   * Apply Armadillo's Principal Component Analysis to the provided data set.
+   *
+   * @param data - Data matrix
+   * @param coeff - PCA Loadings
+   * @param score - contains the coordinates of the original data in the new coordinate system defined by the principal components
+   */
+  void Apply(const arma::mat& data, arma::mat& coeff, arma::mat& score);
 
-};
-};
-};
+  /**
+   * Delete PCA object
+   */
+  ~PCA();
+
+}; // class PCA
+
+}; // namespace pca
+}; // namespace mlpack
