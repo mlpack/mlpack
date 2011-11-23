ed2a46a7b8362db4b5f180cf61c9c405687a1d1a
Ryan Curtin <ryan@ratml.org>
2011-11-23 23:43:22 +0000

    Update formatting of linear_regression to reflect decisions made in #153.

--- src/mlpack/methods/linear_regression/linear_regression.hpp
+++ src/mlpack/methods/linear_regression/linear_regression.hpp
@@ -1,57 +1,78 @@
-#ifndef __MLPACK_METHODS_LINEAR_REGRESSCLIN_HPP
-#define __MLPACK_METHODS_LINEAR_REGRESSCLIN_HPP
+/**
+ * @file linear_regression.hpp
+ * @author James Cline
+ *
+ * Simple least-squares linear regression.
+ */
+#ifndef __MLPACK_METHODS_LINEAR_REGRESSION_LINEAR_REGRESSION_HPP
+#define __MLPACK_METHODS_LINEAR_REGRESSION_LINEAR_REGRESSION_HPP
 
 #include <mlpack/core.h>
+
 namespace mlpack {
 namespace linear_regression {
 
-/** 
- *  A simple linear regresion algorithm using ordinary least squares.
+/**
+ * A simple linear regresion algorithm using ordinary least squares.
  */
 class LinearRegression
 {
-  public:
-    /** Creates the model.
-     *  @param predictors X, matrix of data points to create B with.
-     *  @param responses y, the measured data for each point in X
-     */ 
-    LinearRegression(arma::mat& predictors, const arma::colvec& responses);
+ public:
+  /**
+   * Creates the model.
+   *
+   * @param predictors X, matrix of data points to create B with.
+   * @param responses y, the measured data for each point in X
+   */
+  LinearRegression(arma::mat& predictors, const arma::colvec& responses);
 
-    /** Initialize the model from a file.
-     *  @param filename the name of the file to load the model from.
-     */
-    LinearRegression(const std::string& filename);
+  /**
+   * Initialize the model from a file.
+   *
+   * @param filename the name of the file to load the model from.
+   */
+  LinearRegression(const std::string& filename);
 
-    /** Destructor - no work done. */
-    ~LinearRegression();
+  /**
+   * Destructor - no work done.
+   */
+  ~LinearRegression();
 
-    /** Calculate y_i for each data point in points.
-     *  @param predictions y, will contain calculated values on completion.
-     *  @param points the data points to calculate with.
-     */
-    void predict(arma::rowvec& predictions, const arma::mat& points);
+  /**
+   * Calculate y_i for each data point in points.
+   *
+   * @param predictions y, will contain calculated values on completion.
+   * @param points the data points to calculate with.
+   */
+  void predict(arma::rowvec& predictions, const arma::mat& points);
 
-    /** Returns the model.
-     * @return the parameters which describe the least squares solution.
-     */
-    arma::vec getParameters();
+  /**
+   * Returns the model.
+   *
+   * @return the parameters which describe the least squares solution.
+   */
+  arma::vec getParameters();
 
-    /** Saves the model.
-     *  @param filename the name of the file to load the model from.
-     */
-    bool save(const std::string& filename);
+  /**
+   * Saves the model.
+   *
+   * @param filename the name of the file to load the model from.
+   */
+  bool save(const std::string& filename);
 
-    /** Loads the model.
-     *  @param filename the name of the file to load the model from.
-     */
-    bool load(const std::string& filename);
+  /**
+   * Loads the model.
+   *
+   * @param filename the name of the file to load the model from.
+   */
+  bool load(const std::string& filename);
 
-  private:
-    /** The calculated B.
-     * Initialized and filled by constructor to hold the least squares solution.
-     */
-    arma::vec parameters;
-
+ private:
+  /**
+   * The calculated B.
+   * Initialized and filled by constructor to hold the least squares solution.
+   */
+  arma::vec parameters;
 };
 
 }; // namespace linear_regression
