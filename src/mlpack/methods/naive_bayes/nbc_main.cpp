5ff598a0beca0a6c5da378ab03052df9ab5ef360
Ryan Curtin <ryan@ratml.org>
2011-11-24 01:39:41 +0000

    Move filenames to .hpp and .cpp to help finish #152, and fix style in accordance with #153.  Removed phi() since it is a copy of the version found in methods/gmm/ (which will also have to be moved).

---
+++ src/mlpack/methods/naive_bayes/nbc_main.cpp
@@ -0,0 +1,75 @@
+/**
+ * @author Parikshit Ram (pram@cc.gatech.edu)
+ * @file nbc_main.cpp
+ *
+ * This program runs the Simple Naive Bayes Classifier.
+ *
+ * This classifier does parametric naive bayes classification assuming that the
+ * features are sampled from a Gaussian distribution.
+ *
+ * PARAMETERS TO BE INPUT:
+ *
+ * --train
+ * This is the file that contains the training data.
+ *
+ * --nbc/classes
+ * This is the number of classes present in the training data.
+ *
+ * --test
+ * This file contains the data points which the trained classifier would
+ * classify.
+ *
+ * --output
+ * This file will contain the classes to which the corresponding data points in
+ * the testing data.
+ */
+#include <mlpack/core.h>
+
+#include "simple_nbc.hpp"
+
+PARAM_STRING_REQ("train", "A file containing the training set", "nbc");
+PARAM_STRING_REQ("test", "A file containing the test set", "nbc");
+PARAM_STRING("output", "The file in which the output of the test would "
+    "be written, defaults to 'output.csv')", "nbc", "output.csv");
+
+PARAM_MODULE("nbc", "Trains on a given set and number of classes and tests "
+    "them on a given set");
+
+PROGRAM_INFO("Parametric Naive Bayes", "This program test drives the Parametric"
+    " Naive Bayes Classifier assuming that the features are sampled from a "
+    "Gaussian distribution.", "nbc");
+
+using namespace mlpack;
+using namespace naive_bayes;
+
+int main(int argc, char* argv[])
+{
+  CLI::ParseCommandLine(argc, argv);
+
+  const char *training_data_filename =
+      CLI::GetParam<std::string>("nbc/train").c_str();
+  arma::mat training_data;
+  data::Load(training_data_filename, training_data, true);
+
+  const char *testing_data_filename =
+      CLI::GetParam<std::string>("nbc/test").c_str();
+  arma::mat testing_data;
+  data::Load(testing_data_filename, testing_data, true);
+
+  // Create and train the classifier.
+  Timers::StartTimer("nbc/training");
+  SimpleNaiveBayesClassifier nbc = SimpleNaiveBayesClassifier(training_data);
+  Timers::StopTimer("nbc/training");
+
+  // Timing the running of the Naive Bayes Classifier.
+  arma::vec results;
+  Timers::StartTimer("nbc/testing");
+  nbc.Classify(testing_data, results);
+  Timers::StopTimer("nbc/testing");
+
+  // Output results.
+  std::string output_filename = CLI::GetParam<std::string>("nbc/output");
+  data::Save(output_filename.c_str(), results, true);
+
+  return 0;
+}
---
+++ src/mlpack/methods/naive_bayes/simple_nbc.hpp
@@ -0,0 +1,109 @@
+/**
+ * @file simple_nbc.hpp
+ * @author Parikshit Ram (pram@cc.gatech.edu)
+ *
+ * A Naive Bayes Classifier which parametrically estimates the distribution of
+ * the features.  It is assumed that the features have been sampled from a
+ * Gaussian PDF.
+ */
+#ifndef __MLPACK_METHODS_NBC_SIMPLE_NBC_HPP
+#define __MLPACK_METHODS_NBC_SIMPLE_NBC_HPP
+
+#include <mlpack/core.h>
+#include <mlpack/methods/gmm/phi.hpp>
+
+namespace mlpack {
+namespace naive_bayes {
+
+PARAM_INT_REQ("classes", "The number of classes present in the data.", "nbc");
+PARAM_INT("features", "The number of features in the data.", "nbc", 0);
+PARAM_INT("examples", "The number of examples in the training set.", "nbc", 0);
+PARAM_INT("tests", "The number of data points in the test set.", "nbc", 0);
+
+PARAM_MODULE("nbc", "Trains the classifier using the training set "
+    "and outputs the results for the test set.");
+
+/**
+ * A classification class. The class labels are assumed
+ * to be positive integers - 0,1,2,....
+ *
+ * This class trains on the data by calculating the
+ * sample mean and variance of the features with
+ * respect to each of the labels, and also the class
+ * probabilities.
+ *
+ * Mathematically, it computes P(X_i = x_i | Y = y_j)
+ * for each feature X_i for each of the labels y_j.
+ * Alongwith this, it also computes the classs probabilities
+ * P( Y = y_j)
+ *
+ * For classifying a data point (x_1, x_2, ..., x_n),
+ * it computes the following:
+ * arg max_y(P(Y = y)*P(X_1 = x_1 | Y = y) * ... * P(X_n = x_n | Y = y))
+ *
+ * Example use:
+ *
+ * @code
+ * SimpleNaiveBayesClassifier nbc;
+ * arma::mat training_data, testing_data;
+ * datanode *nbc_module = fx_submodule(NULL,"nbc","nbc");
+ * arma::vec results;
+ *
+ * nbc.InitTrain(training_data, nbc_module);
+ * nbc.Classify(testing_data, &results);
+ * @endcode
+ */
+class SimpleNaiveBayesClassifier
+{
+ public:
+  //! Sample mean for each class.
+  arma::mat means_;
+
+  //! Sample variances for each class.
+  arma::mat variances_;
+
+  //! Class probabilities.
+  arma::vec class_probabilities_;
+
+  //! The number of classes present.
+  size_t number_of_classes_;
+
+  /**
+   * Initializes the classifier as per the input and then trains it
+   * by calculating the sample mean and variances
+   *
+   * Example use:
+   * @code
+   * arma::mat training_data, testing_data;
+   * datanode nbc_module = fx_submodule(NULL,"nbc","nbc");
+   * ....
+   * SimpleNaiveBayesClassifier nbc(training_data, nbc_module);
+   * @endcode
+   */
+  SimpleNaiveBayesClassifier(const arma::mat& data);
+
+  /**
+   * Default constructor, you need to use the other one.
+   */
+  SimpleNaiveBayesClassifier();
+
+  ~SimpleNaiveBayesClassifier() { }
+
+  /**
+   * Given a bunch of data points, this function evaluates the class
+   * of each of those data points, and puts it in the vector 'results'
+   *
+   * @code
+   * arma::mat test_data; // each column is a test point
+   * arma::vec results;
+   * ...
+   * nbc.Classify(test_data, &results);
+   * @endcode
+   */
+  void Classify(const arma::mat& test_data, arma::vec& results);
+};
+
+}; // namespace naive_bayes
+}; // namespace mlpack
+
+#endif
