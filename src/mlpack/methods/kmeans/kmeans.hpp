9bbaefe5a0ae6f5c0c61a7610d274c71ec77183c
Ryan Curtin <ryan@ratml.org>
2011-11-24 21:53:08 +0000

    Move K-Means to be its own method.

--- src/mlpack/methods/kmeans/kmeans.hpp
+++ src/mlpack/methods/kmeans/kmeans.hpp
@@ -1,17 +1,16 @@
 /**
- * @author Parikshit Ram (pram@cc.gatech.edu)
  * @file kmeans.hpp
+ * @author Parikshit Ram (pram@cc.gatech.edu)
  *
- * Defines a Gaussian Mixture model and
- * estimates the parameters of the model
+ * K-Means clustering.
  */
-#ifndef __MLPACK_METHODS_MOG_KMEANS_HPP
-#define __MLPACK_METHODS_MOG_KMEANS_HPP
+#ifndef __MLPACK_METHODS_KMEANS_KMEANS_HPP
+#define __MLPACK_METHODS_KMEANS_KMEANS_HPP
 
 #include <mlpack/core.hpp>
 
 namespace mlpack {
-namespace gmm {
+namespace kmeans {
 
 /**
  * This function computes the k-means of the data and stores the calculated
@@ -25,6 +24,7 @@ void KMeans(const arma::mat& data,
             std::vector<arma::vec>& means,
             std::vector<arma::mat>& covars,
             arma::vec& weights);
+
 }; // namespace gmm
 }; // namespace mlpack
 
