f59bd5de771bbec8115ccdf38a3041660392f59e
Ryan Curtin <ryan@ratml.org>
2011-11-24 02:42:17 +0000

    neighbor_search.h is now neighbor_search.hpp.

--- src/mlpack/tests/allkfn_test.cpp
+++ src/mlpack/tests/allkfn_test.cpp
@@ -4,7 +4,7 @@
  * Tests for AllkFN (all-k-furthest-neighbors).
  */
 #include <mlpack/core.h>
-#include <mlpack/methods/neighbor_search/neighbor_search.h>
+#include <mlpack/methods/neighbor_search/neighbor_search.hpp>
 #include <boost/test/unit_test.hpp>
 
 using namespace mlpack;
