fcae3eb8368b36b295daa65824241dc42450401e
Ajinkya Kale <kaleajinkya@gmail.com>
2011-11-26 22:55:37 +0000

    <#126: Implement simple PCA> removing pca_test from pca dir (Added it in tests dir)

---
+++ src/mlpack/methods/pca/CMakeFiles/pca.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ajinkya/Projects/MLPACK/fastlab/mlpack/trunk/src/mlpack/methods/pca/pca_test.cpp" "/home/ajinkya/Projects/MLPACK/fastlab/mlpack/trunk/src/mlpack/methods/pca/CMakeFiles/pca.dir/pca_test.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "DEBUG"
+  "BOOST_TEST_DYN_LINK"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ajinkya/Projects/MLPACK/fastlab/mlpack/trunk/src/mlpack/CMakeFiles/mlpack.dir/DependInfo.cmake"
+  )
