04c87d8d4fa5a7efe6c32cfd3c5631bcefa3b423
Ajinkya Kale <kaleajinkya@gmail.com>
2011-11-26 22:55:37 +0000

    <#126: Implement simple PCA> removing pca_test from pca dir (Added it in tests dir)

---
+++ src/mlpack/methods/pca/CMakeFiles/pca.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pca.dir/pca_test.cpp.o"
+  "../../../../bin/pca.pdb"
+  "../../../../bin/pca"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
