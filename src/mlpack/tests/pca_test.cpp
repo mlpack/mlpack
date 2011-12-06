b9c6fa3a59f4830096c931fd4dfd084aaa9d5cad
Ajinkya Kale <kaleajinkya@gmail.com>
2011-12-06 09:48:19 +0000

    changed the test to accomodate pricipal axes in opp direction ie when eigenvectors returned are flipped in sign compared to the eignvec returned by matlab for the same dataset

--- src/mlpack/tests/pca_test.cpp
+++ src/mlpack/tests/pca_test.cpp
@@ -41,7 +41,7 @@ BOOST_AUTO_TEST_CASE(ArmaComparisonPCATest)
 /**
  * Test that dimensionality reduction with PCA works the same way MATLAB does
  * (which should be correct!).
- *
+ */
 BOOST_AUTO_TEST_CASE(PCADimensionalityReductionTest)
 {
   // Fake, simple dataset.  The results we will compare against are from MATLAB.
@@ -57,10 +57,21 @@ BOOST_AUTO_TEST_CASE(PCADimensionalityReductionTest)
   mat correct("-1.53781086 -3.51358020 -0.16139887 -1.87706634  7.08985628;"
               " 1.29937798  3.45762685 -2.69910005 -3.15620704  1.09830225");
 
+  // If the eigenvectors are pointed opposite directions, they will cancel
+// each other out in this summation.
+  for(size_t i = 0; i < data.n_rows; i++)
+  {
+    if (fabs(correct(i, 1) + data(i,1)) < 0.001 /* arbitrary */)
+    {
+         // Flip Armadillo coefficients for this column.
+         data.row(i) *= -1;
+    }
+  }
+
   for (size_t row = 0; row < 2; row++)
     for (size_t col = 0; col < 5; col++)
       BOOST_REQUIRE_CLOSE(data(row, col), correct(row, col), 1e-3);
 }
-*/
+
 
 BOOST_AUTO_TEST_SUITE_END();
