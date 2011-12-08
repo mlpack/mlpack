9fe64c4794fec7fbb96309325276510f42b83bd1
tekhnofiend <nishantmehta.x@gmail.com>
2011-12-08 06:07:01 +0000

    I think I fixed two bugs. need to check tomorrow

--- src/mlpack/methods/radical/radical.cpp
+++ src/mlpack/methods/radical/radical.cpp
@@ -10,17 +10,7 @@
 //using namespace arma;
 using namespace std;
 
-/*
-Radical::Radical(double noiseStdDev, size_t nReplicates, size_t nAngles, size_t nSweeps,
-		 const arma::mat& matX) {
-  this->noiseStdDev = noiseStdDev;
-  this->nReplicates = nReplicates;
-  this->nAngles = nAngles;
-  this->nSweeps = nSweeps;
-  this->matX = arma::mat(matX); // is this the same as this.X = X ?
-  m = floor(sqrt(matX.n_rows));
-}
-*/
+
 Radical::Radical(double noiseStdDev, size_t nReplicates, size_t nAngles, size_t nSweeps,
 		 const arma::mat& matX) :
   noiseStdDev(noiseStdDev),
@@ -33,18 +23,11 @@ Radical::Radical(double noiseStdDev, size_t nReplicates, size_t nAngles, size_t 
 }
 
 
-void Radical::WhitenX(arma::mat& matWhitening) {
-  arma::mat matU, matV;
-  arma::vec s;
-  arma::svd(matU, s, matV, cov(matX));
-  matWhitening = matU * arma::diagmat(pow(s, -0.5)) * arma::trans(matV);
-  matX = matX * matWhitening;
-}
-
 void Radical::CopyAndPerturb(arma::mat& matXNew, const arma::mat& matX) {
   matXNew = arma::repmat(matX, nReplicates, 1);
   matXNew = matXNew + noiseStdDev * arma::randn(matXNew.n_rows, matXNew.n_cols);
 }
+
 
 double Radical::Vasicek(const arma::vec& z) {
   arma::vec v = sort(z);
@@ -52,6 +35,7 @@ double Radical::Vasicek(const arma::vec& z) {
   arma::vec logs = arma::log(v.subvec(m, nPoints - 1) - v.subvec(0, nPoints - 1 - m));
   return (double) arma::sum(logs);
 }
+
 
 double Radical::DoRadical2D(const arma::mat& matX) {
   arma::mat matXMod;
@@ -87,7 +71,7 @@ double Radical::DoRadical2D(const arma::mat& matX) {
 void Radical::DoRadical(arma::mat& matY, arma::mat& matW) {
 
   // matX is nPoints by nDims (although less intuitive than columns being points,
-  // and although this is the transpose of the ICA literature,this choice is 
+  // and although this is the transpose of the ICA literature, this choice is 
   // for computational efficiency)
   
   size_t nDims = matX.n_cols;
@@ -95,7 +79,7 @@ void Radical::DoRadical(arma::mat& matY, arma::mat& matW) {
   arma::mat matWhitening;
   WhitenX(matWhitening);
   
-  // in the RADICAL code, they do not copy and perturb initial, although the
+  // in the RADICAL code, they do not copy and perturb initially, although the
   // paper does. we follow the code as it should match their reported results
   // and likely does a better job bouncing out of local optima
   //GeneratePerturbedX(X, X);
@@ -118,11 +102,20 @@ void Radical::DoRadical(arma::mat& matY, arma::mat& matW) {
 	matJ(j,i) = -sin(thetaOpt);
 	matJ(j,j) = cos(thetaOpt);
 	matW = matW * matJ;
-	matY = matY * matW;
+	matY = matX * matW;
       }
     }
   }
   
   // the final transpose provides W in the typical form from the ICA literature
-  matW = arma::trans(matW * matWhitening);
+  matW = arma::trans(matWhitening * matW);
+}
+
+
+void Radical::WhitenX(arma::mat& matWhitening) {
+  arma::mat matU, matV;
+  arma::vec s;
+  arma::svd(matU, s, matV, cov(matX));
+  matWhitening = matU * arma::diagmat(pow(s, -0.5)) * arma::trans(matV);
+  matX = matX * matWhitening;
 }
