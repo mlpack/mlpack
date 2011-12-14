a1068f988360c22aff1d8bc71396c678e81e1569
Ryan Curtin <ryan@ratml.org>
2011-12-14 10:53:18 +0000

    Refactoring of EMST/UnionFind API.

--- src/mlpack/tests/union_find_test.cpp
+++ src/mlpack/tests/union_find_test.cpp
@@ -17,8 +17,7 @@ BOOST_AUTO_TEST_SUITE(UnionFindTest);
 BOOST_AUTO_TEST_CASE(TestFind)
 {
   static const size_t test_size_ = 10;
-  UnionFind test_union_find_;
-  test_union_find_.Init(test_size_);
+  UnionFind test_union_find_(test_size_);
 
   for (size_t i = 0; i < test_size_; i++)
     BOOST_REQUIRE(test_union_find_.Find(i) == i);
@@ -32,8 +31,7 @@ BOOST_AUTO_TEST_CASE(TestFind)
 BOOST_AUTO_TEST_CASE(TestUnion)
 {
   static const size_t test_size_ = 10;
-  UnionFind test_union_find_;
-  test_union_find_.Init(test_size_);
+  UnionFind test_union_find_(test_size_);
 
   test_union_find_.Union(0, 1);
   test_union_find_.Union(2, 3);
