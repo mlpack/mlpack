cf4db79006e1379e89d6d6a196172d8ce191cd4d
Ryan Curtin <ryan@ratml.org>
2011-12-16 06:34:38 +0000

    Add some useful documentation.

---
+++ doc/guide/matrices.hpp
@@ -0,0 +1,70 @@
+/*! @page matrices Matrices in MLPACK
+
+@section matintro Introduction
+
+MLPACK uses Armadillo matrices for matrix support.  Armadillo is a fast C++
+matrix library which makes use of advanced template techniques to provide the
+fastest possible matrix operations.
+
+Documentation on Armadillo can be found on their website:
+
+http://arma.sourceforge.net/docs.html
+
+Nonetheless, there are a few further caveats for MLPACK Armadillo usage.
+
+@section format Column-wise Matrices
+
+Armadillo matrices are stored in a column-major format; this means that on disk,
+each column is located in contiguous memory.
+
+This means that, for the vast majority of machine learning methods, it is faster
+to store observations as columns and dimensions as rows.  This is counter to
+most standard machine learning texts!
+
+Major implications of this are for linear algebra.  For instance, the covariance
+of a matrix is typically
+
+@f$ C = X^T X @f$
+
+but for a column-wise matrix, it is
+
+@f$ C = X X^T @f$
+
+and this is very important to keep in mind!  If your MLPACK code is not working,
+this may be a factor in why.
+
+@section loading Loading Matrices
+
+MLPACK provides a data::Load() and data::Save() function, which should be used
+instead of Armadillo's loading and saving functions.
+
+Most machine learning data is stored in row-major format; a CSV, for example,
+will generally have one observation per line and each column will correspond to
+a dimension.
+
+The data::Load() and data::Save() functions transpose the matrix upon loading,
+meaning that the following CSV:
+
+@code
+$ cat data.csv
+3,3,3,3,0
+3,4,4,3,0
+3,4,4,3,0
+3,3,4,3,0
+3,6,4,3,0
+2,4,4,3,0
+2,4,4,1,0
+3,3,3,2,0
+3,4,4,2,0
+3,4,4,2,0
+3,3,4,2,0
+3,6,4,2,0
+2,4,4,2,0
+@endcode
+
+is actually loaded with 5 rows and 13 columns, not 13 rows and 5 columns like
+the CSV is written.
+
+This is important to remember!
+
+*/
---
+++ doc/guide/sample.hpp
@@ -0,0 +1,93 @@
+/*! @page sample Simple Sample MLPACK Programs
+
+@section sampleintro Introduction
+
+On this page, several simple MLPACK examples are contained, in increasing order
+of complexity.
+
+@section covariance Covariance Computation
+
+A simple program to compute the covariance of a data matrix ("data.csv"),
+assuming that the data is already centered, and save it to file.
+
+@code
+// Includes all relevant components of MLPACK.
+#include <mlpack/core.hpp>
+
+// Convenience.
+using namespace mlpack;
+
+int main()
+{
+  // First, load the data.
+  arma::mat data;
+  // Use data::Load() which transposes the matrix.
+  data::Load("data.csv", data, true);
+
+  // Now compute the covariance.  We assume that the data is already centered.
+  // Remember, because the matrix is column-major, the covariance operation is
+  // transposed.
+  arma::mat cov = data * trans(data) / data.n_cols;
+
+  // Save the output.
+  data::Save("cov.csv", cov, true);
+}
+@endcode
+
+@section nn Nearest Neighbor
+
+This simple program uses the mlpack::neighbor::NeighborSearch object to find the
+nearest neighbor of each point in a dataset using the L1 metric, and then print
+the index of the neighbor and the distance of it to stdout.
+
+@code
+@include <mlpack/core.hpp>
+
+using namespace mlpack;
+using namespace mlpack::neighbor; // NeighborSearch and NearestNeighborSort
+using namespace mlpack::metric; // ManhattanDistance
+
+int main()
+{
+  // Load the data from data.csv (hard-coded).  Use CLI for simple command-line
+  // parameter handling.
+  arma::mat data;
+  data::Load("data.csv", data, true);
+
+  // Use templates to specify that we want a NeighborSearch object which uses
+  // the Manhattan distance.
+  NeighborSearch<NearestNeighborSort, ManhattanDistance> nn(data);
+
+  // Create the object we will store the nearest neighbors in.
+  arma::Col<size_t> neighbors;
+  arma::vec distances; // We need to store the distance too.
+
+  // Compute the neighbors.
+  nn.Search(1, neighbors, distances);
+
+  // Write each neighbor and distance using Log.
+  for (size_t i = 0; i < neighbors.n_elem; ++i)
+  {
+    Log::Info << "Nearest neighbor of point " << i << " is point "
+        << neighbors[i] << " and the distance is " << distances[i] << ".\n";
+  }
+}
+@endcode
+
+@section other Other examples
+
+For more complex examples, it is useful to refer to the main executables:
+
+ - methods/neighbor_search/allknn_main.cpp
+ - methods/neighbor_search/allkfn_main.cpp
+ - methods/emst/emst_main.cpp
+ - methods/radical/radical_main.cpp
+ - methods/nca/nca_main.cpp
+ - methods/naive_bayes/nbc_main.cpp
+ - methods/pca/pca_main.cpp
+ - methods/lars/lars_main.cpp
+ - methods/linear_regression/linear_regression_main.cpp
+ - methods/gmm/gmm_main.cpp
+ - methods/kmeans/kmeans_main.cpp
+
+*/
