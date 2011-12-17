d5e96bfb236fb7fb88d213d50d5adb143614ef70
Ryan Curtin <ryan@ratml.org>
2011-12-17 07:06:02 +0000

    We have a few more executables now.

--- src/mlpack/core.hpp
+++ src/mlpack/core.hpp
@@ -49,8 +49,8 @@
  *
  * A full list of executables is given below:
  *
- * allkfn, allknn, emst, gmm, kernel_pca, kmeans, lars, linear_regression, nbc,
- * nca, pca, radical
+ * allkfn, allknn, emst, gmm, hmm_train, hmm_loglik, hmm_viterbi, hmm_generate,
+ * kernel_pca, kmeans, lars, linear_regression, nbc, nca, pca, radical
  *
  * @section tutorial Tutorials
  *
