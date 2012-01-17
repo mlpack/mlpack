d74e64947f87d219c30a806e4051f6b4264e3dd0
James Cline <jamesclinedev@gmail.com>
2012-01-17 20:31:20 +0000

    Comment out unused parameter names (suprisingly valid C++), -Wunused-parameter

--- src/mlpack/core/util/nulloutstream.hpp
+++ src/mlpack/core/util/nulloutstream.hpp
@@ -30,48 +30,48 @@ class NullOutStream
   /**
    * Does nothing.
    */
-  NullOutStream(const NullOutStream& other) { }
+  NullOutStream(const NullOutStream& /* other */) { }
 
   //! Does nothing.
-  NullOutStream& operator<<(bool val) { return *this; }
+  NullOutStream& operator<<(bool /* val */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(short val) { return *this; }
+  NullOutStream& operator<<(short /* val */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(unsigned short val) { return *this; }
+  NullOutStream& operator<<(unsigned short /* val */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(int val) { return *this; }
+  NullOutStream& operator<<(int /* val */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(unsigned int val) { return *this; }
+  NullOutStream& operator<<(unsigned int /* val */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(long val) { return *this; }
+  NullOutStream& operator<<(long /* val */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(unsigned long val) { return *this; }
+  NullOutStream& operator<<(unsigned long /* val */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(float val) { return *this; }
+  NullOutStream& operator<<(float /* val */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(double val) { return *this; }
+  NullOutStream& operator<<(double /* val */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(long double val) { return *this; }
+  NullOutStream& operator<<(long double /* val */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(void* val) { return *this; }
+  NullOutStream& operator<<(void* /* val */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(const char* str) { return *this; }
+  NullOutStream& operator<<(const char* /* str */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(std::string& str) { return *this; }
+  NullOutStream& operator<<(std::string& /* str */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(std::streambuf* sb) { return *this; }
+  NullOutStream& operator<<(std::streambuf* /* sb */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(std::ostream& (*pf) (std::ostream&))
+  NullOutStream& operator<<(std::ostream& /* (*pf) (std::ostream&) */)
   { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(std::ios& (*pf) (std::ios&)) { return *this; }
+  NullOutStream& operator<<(std::ios& /* (*pf) (std::ios&) */) { return *this; }
   //! Does nothing.
-  NullOutStream& operator<<(std::ios_base& (*pf) (std::ios_base&))
+  NullOutStream& operator<<(std::ios_base& /* (*pf) (std::ios_base&) */)
   { return *this; }
 
   //! Does nothing.
   template<typename T>
-  NullOutStream& operator<<(T s)
+  NullOutStream& operator<<(T /* s */)
   { return *this; }
 };
 
