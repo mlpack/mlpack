859ed231a5c8b2cfbe1dbf7effb3f41c5caf7769
Ryan Curtin <ryan@ratml.org>
2012-02-21 02:39:14 +0000

    Initialize StatisticType objects in the root node too.

--- src/mlpack/core/tree/binary_space_tree_impl.hpp
+++ src/mlpack/core/tree/binary_space_tree_impl.hpp
@@ -31,6 +31,12 @@ BinarySpaceTree<BoundType, StatisticType, MatType>::BinarySpaceTree(
 {
   // Do the actual splitting of this node.
   SplitNode(data);
+
+  // Create the statistic depending on if we are a leaf or not.
+  if (IsLeaf())
+    stat = StatisticType(data, begin, count);
+  else
+    stat = StatisticType(data, begin, count, left->Stat(), right->Stat());
 }
 
 template<typename BoundType, typename StatisticType, typename MatType>
@@ -53,6 +59,12 @@ BinarySpaceTree<BoundType, StatisticType, MatType>::BinarySpaceTree(
 
   // Now do the actual splitting.
   SplitNode(data, oldFromNew);
+
+  // Create the statistic depending on if we are a leaf or not.
+  if (IsLeaf())
+    stat = StatisticType(data, begin, count);
+  else
+    stat = StatisticType(data, begin, count, left->Stat(), right->Stat());
 }
 
 template<typename BoundType, typename StatisticType, typename MatType>
@@ -76,6 +88,12 @@ BinarySpaceTree<BoundType, StatisticType, MatType>::BinarySpaceTree(
 
   // Now do the actual splitting.
   SplitNode(data, oldFromNew);
+
+  // Create the statistic depending on if we are a leaf or not.
+  if (IsLeaf())
+    stat = StatisticType(data, begin, count);
+  else
+    stat = StatisticType(data, begin, count, left->Stat(), right->Stat());
 
   // Map the newFromOld indices correctly.
   newFromOld.resize(data.n_cols);
@@ -159,16 +177,16 @@ BinarySpaceTree<BoundType, StatisticType, MatType>::BinarySpaceTree(
   // Perform the actual splitting.
   SplitNode(data, oldFromNew);
 
-  // Map the newFromOld indices correctly.
-  newFromOld.resize(data.n_cols);
-  for (size_t i = 0; i < data.n_cols; i++)
-    newFromOld[oldFromNew[i]] = i;
-
   // Create the statistic depending on if we are a leaf or not.
   if (IsLeaf())
     stat = StatisticType(data, begin, count);
   else
     stat = StatisticType(data, begin, count, left->Stat(), right->Stat());
+
+  // Map the newFromOld indices correctly.
+  newFromOld.resize(data.n_cols);
+  for (size_t i = 0; i < data.n_cols; i++)
+    newFromOld[oldFromNew[i]] = i;
 }
 
 template<typename BoundType, typename StatisticType, typename MatType>
@@ -309,7 +327,7 @@ size_t BinarySpaceTree<BoundType, StatisticType, MatType>::TreeDepth() const
 }
 
 template<typename BoundType, typename StatisticType, typename MatType>
-inline const 
+inline const
     BoundType& BinarySpaceTree<BoundType, StatisticType, MatType>::Bound() const
 {
   return bound;
@@ -322,7 +340,7 @@ inline BoundType& BinarySpaceTree<BoundType, StatisticType, MatType>::Bound()
 }
 
 template<typename BoundType, typename StatisticType, typename MatType>
-inline const StatisticType& 
+inline const StatisticType&
     BinarySpaceTree<BoundType, StatisticType, MatType>::Stat() const
 {
   return stat;
@@ -388,7 +406,7 @@ inline size_t BinarySpaceTree<BoundType, StatisticType, MatType>::Count() const
 }
 
 template<typename BoundType, typename StatisticType, typename MatType>
-void 
+void
     BinarySpaceTree<BoundType, StatisticType, MatType>::SplitNode(MatType& data)
 {
   // We need to expand the bounds of this node properly.
