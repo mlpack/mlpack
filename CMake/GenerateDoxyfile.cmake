1d89ea2cd8ceeb79d9728edf2220548eb8d9a980
Ryan Curtin <ryan@ratml.org>
2012-03-14 17:27:10 +0000

    Generate Doxygen documentation with 'make doc' only in out-of-source build directory -- don't clutter source directory.  (I'm proud of this)

---
+++ CMake/GenerateDoxyfile.cmake
@@ -0,0 +1,35 @@
+cmake_minimum_required(VERSION 2.8)
+
+# We need to modify the Doxyfile slightly.  We'll copy the Doxyfile into the
+# build directory, update the location of the source, and then run Doxygen and
+# it will generate the documentation into the build directory.
+
+message(STATUS "Hurble ${CMAKE_CURRENT_SOURCE_DIR}")
+
+# First, read the Doxyfile in as a variable.
+file(READ "${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile" DOXYFILE_CONTENTS)
+
+# Now, modify all the "INPUT" paths.  I've written each of the three out by
+# hand.  If more are added, they'll need to be added here too.
+string(REPLACE
+    "./src/mlpack"
+    "${CMAKE_CURRENT_SOURCE_DIR}/src/mlpack"
+    DOXYFILE_AUXVAR "${DOXYFILE_CONTENTS}"
+)
+
+string(REPLACE
+    "./doc/guide"
+    "${CMAKE_CURRENT_SOURCE_DIR}/doc/guide"
+    DOXYFILE_CONTENTS "${DOXYFILE_AUXVAR}"
+)
+
+string(REPLACE
+    "./doc/tutorials"
+    "${CMAKE_CURRENT_SOURCE_DIR}/doc/tutorials"
+    DOXYFILE_AUXVAR "${DOXYFILE_CONTENTS}"
+)
+
+message(STATUS "Ready to go ${DESTDIR}")
+
+# Save the Doxyfile to its new location.
+file(WRITE "${DESTDIR}/Doxyfile" "${DOXYFILE_AUXVAR}")
