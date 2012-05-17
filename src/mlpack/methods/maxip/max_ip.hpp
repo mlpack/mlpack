10788af881d3360b93ebf1938c143b170be18004
Ryan Curtin <ryan@ratml.org>
2012-05-17 16:55:26 +0000

    Add MaxIP class.

---
+++ src/mlpack/methods/maxip/max_ip.hpp
@@ -0,0 +1,64 @@
+/**
+ * @file max_ip.hpp
+ * @author Ryan Curtin
+ *
+ * Definition of the MaxIP class, which is the maximum inner product search.
+ */
+#ifndef __MLPACK_METHODS_MAXIP_MAX_IP_HPP
+#define __MLPACK_METHODS_MAXIP_MAX_IP_HPP
+
+#include <mlpack/core.hpp>
+#include "ip_metric.hpp"
+#include <mlpack/core/tree/cover_tree.hpp>
+
+namespace mlpack {
+namespace maxip {
+
+template<typename KernelType>
+class MaxIP
+{
+ public:
+  MaxIP(const arma::mat& referenceSet,
+        bool single = false,
+        bool naive = false);
+
+  MaxIP(const arma::mat& referenceSet,
+        const arma::mat& querySet,
+        bool single = false,
+        bool naive = false);
+
+  ~MaxIP();
+
+  void Search(const size_t k,
+              arma::Mat<size_t>& indices,
+              arma::mat& products);
+
+ private:
+  const arma::mat& referenceSet;
+
+  const arma::mat& querySet;
+
+  tree::CoverTree<IPMetric<KernelType> >* referenceTree;
+
+  tree::CoverTree<IPMetric<KernelType> >* queryTree;
+
+  bool single;
+
+  bool naive;
+
+  // Utility function.  Copied too many times from too many places.
+  void InsertNeighbor(arma::Mat<size_t>& indices,
+                      arma::mat& products,
+                      const size_t queryIndex,
+                      const size_t pos,
+                      const size_t neighbor,
+                      const double distance);
+};
+
+}; // namespace maxip
+}; // namespace mlpack
+
+// Include implementation.
+#include "max_ip_impl.hpp"
+
+#endif
