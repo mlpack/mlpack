3f5319a33d97cfaed96f5db9f38272808404e6ef
Ryan Curtin <ryan@ratml.org>
2012-08-05 03:00:55 +0000

    Add a few files we forgot, including the convenience include files which include both trees and traversers.

---
+++ src/mlpack/core/tree/cover_tree.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file cover_tree.hpp
+ * @author Ryan Curtin
+ *
+ * Includes all the necessary files to use the CoverTree class.
+ */
+#ifndef __MLPACK_CORE_TREE_COVER_TREE_HPP
+#define __MLPACK_CORE_TREE_COVER_TREE_HPP
+
+#include "bounds.hpp"
+#include "cover_tree/cover_tree.hpp"
+#include "cover_tree/single_tree_traverser.hpp"
+#include "cover_tree/dual_tree_traverser.hpp"
+
+#endif
