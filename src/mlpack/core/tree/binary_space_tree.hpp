e8bc8f42ad462759cebdce36bdb40e4db7ea0f64
Ryan Curtin <ryan@ratml.org>
2012-08-05 03:00:55 +0000

    Add a few files we forgot, including the convenience include files which include both trees and traversers.

---
+++ src/mlpack/core/tree/binary_space_tree.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file binary_space_tree.hpp
+ * @author Ryan Curtin
+ *
+ * Include all the necessary files to use the BinarySpaceTree class.
+ */
+#ifndef __MLPACK_CORE_TREE_BINARY_SPACE_TREE_HPP
+#define __MLPACK_CORE_TREE_BINARY_SPACE_TREE_HPP
+
+#include "bounds.hpp"
+#include "binary_space_tree/binary_space_tree.hpp"
+#include "binary_space_tree/single_tree_traverser.hpp"
+#include "binary_space_tree/dual_tree_traverser.hpp"
+
+#endif
---
+++ src/mlpack/core/tree/binary_space_tree/single_tree_traverser.hpp
@@ -0,0 +1,57 @@
+/**
+ * @file single_tree_traverser.hpp
+ * @author Ryan Curtin
+ *
+ * A nested class of BinarySpaceTree which traverses the entire tree with a
+ * given set of rules which indicate the branches which can be pruned and the
+ * order in which to recurse.  This traverser is a depth-first traverser.
+ */
+#ifndef __MLPACK_CORE_TREE_BINARY_SPACE_TREE_SINGLE_TREE_TRAVERSER_HPP
+#define __MLPACK_CORE_TREE_BINARY_SPACE_TREE_SINGLE_TREE_TRAVERSER_HPP
+
+#include <mlpack/core.hpp>
+
+#include "binary_space_tree.hpp"
+
+namespace mlpack {
+namespace tree {
+
+template<typename BoundType, typename StatisticType, typename MatType>
+template<typename RuleType>
+class BinarySpaceTree<BoundType, StatisticType, MatType>::SingleTreeTraverser
+{
+ public:
+  /**
+   * Instantiate the single tree traverser with the given rule set.
+   */
+  SingleTreeTraverser(RuleType& rule);
+
+  /**
+   * Traverse the tree with the given point.
+   *
+   * @param queryIndex The index of the point in the query set which is being
+   *     used as the query point.
+   * @param referenceNode The tree node to be traversed.
+   */
+  void Traverse(const size_t queryIndex, BinarySpaceTree& referenceNode);
+
+  //! Get the number of prunes.
+  size_t NumPrunes() const { return numPrunes; }
+  //! Modify the number of prunes.
+  size_t& NumPrunes() { return numPrunes; }
+
+ private:
+  //! Reference to the rules with which the tree will be traversed.
+  RuleType& rule;
+
+  //! The number of nodes which have been pruned during traversal.
+  size_t numPrunes;
+};
+
+}; // namespace tree
+}; // namespace mlpack
+
+// Include implementation.
+#include "single_tree_traverser_impl.hpp"
+
+#endif
