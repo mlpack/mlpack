00fbc33d456ce2ee0e9e2dde6903d0fa97065432
TrironkKiatkungwanglai <trironk@gmail.com>
2012-08-24 16:53:46 +0000

    #164: Started documenting sfinae_utilities.hpp.
    Moved implementations of CallBaseLogic into prefixedoutstream_impl.hpp

--- src/mlpack/core/util/sfinae_utility.hpp
+++ src/mlpack/core/util/sfinae_utility.hpp
@@ -2,10 +2,10 @@
  * @file sfinae_utility.hpp
  * @author Trironk Kiatkungwanglai
  *
- * Utilities for the SFINAE Paradigm.
- *
- * Taken from http://stackoverflow.com/a/6324863/391618
- *
+ * This file contains macro utilities for the SFINAE Paradigm. These utilities
+ * determine if classes passed in as template parameters contain members at
+ * compile time, which is useful for changing functionality depending on what
+ * operations an object is capable of performing.
  */
 #ifndef __MLPACK_CORE_SFINAE_UTILITY
 #define __MLPACK_CORE_SFINAE_UTILITY
@@ -13,15 +13,23 @@
 #include <boost/utility/enable_if.hpp>
 #include <boost/type_traits.hpp>
 
-// Taken from: http://stackoverflow.com/a/264088/391618
-
-#define HAS_MEM_FUNC(func, name)                                               \
+/*
+ * Note: This macro is taken from http://stackoverflow.com/a/264088/391618
+ *
+ * This macro generates a template struct that is useful for enabling/disabling
+ * a method if the template class passed in contains a member function matching
+ * a given signature with a specified name.
+ * 
+ * @param NAME the name of the struct to construct. For example: HasToString
+ * @param FUNC the name of the function to check for. For example: ToString
+ */
+#define HAS_MEM_FUNC(FUNC, NAME)                                               \
     template<typename T, typename sig>                                         \
-    struct name {                                                              \
+    struct NAME {                                                              \
         typedef char yes;                                                      \
         typedef long no;                                                       \
         template <typename U, U> struct type_check;                            \
-        template <typename _1> static yes &chk(type_check<sig, &_1::func> *);  \
+        template <typename _1> static yes &chk(type_check<sig, &_1::FUNC> *);  \
         template <typename   > static no  &chk(...);                           \
         static bool const value = sizeof(chk<T>(0)) == sizeof(yes);            \
     };
