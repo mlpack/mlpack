85a4204d9e69d48152821e14597a3972308cae52
Ryan Curtin <ryan@ratml.org>
2012-09-10 19:28:20 +0000

    Add tests for new MahalanobisDistance constructor.

--- src/mlpack/tests/kernel_test.cpp
+++ src/mlpack/tests/kernel_test.cpp
@@ -108,11 +108,13 @@ BOOST_AUTO_TEST_CASE(lmetric_zeros)
 }
 
 /**
- * Simple test of Mahalanobis distance with unset covariance matrix.
+ * Simple test of Mahalanobis distance with unset covariance matrix in
+ * constructor.
  */
 BOOST_AUTO_TEST_CASE(md_unset_covariance)
 {
   MahalanobisDistance<false> md;
+  md.Covariance() = arma::eye<arma::mat>(4, 4);
   arma::vec a = "1.0 2.0 2.0 3.0";
   arma::vec b = "0.0 0.0 1.0 3.0";
 
@@ -121,12 +123,41 @@ BOOST_AUTO_TEST_CASE(md_unset_covariance)
 }
 
 /**
- * Simple test of Mahalanobis distance with unset covariance matrix and
- * t_take_root set to true.
+ * Simple test of Mahalanobis distance with unset covariance matrix in
+ * constructor and t_take_root set to true.
  */
 BOOST_AUTO_TEST_CASE(md_root_unset_covariance)
 {
   MahalanobisDistance<true> md;
+  md.Covariance() = arma::eye<arma::mat>(4, 4);
+  arma::vec a = "1.0 2.0 2.5 5.0";
+  arma::vec b = "0.0 2.0 0.5 8.0";
+
+  BOOST_REQUIRE_CLOSE(md.Evaluate(a, b), sqrt(14.0), 1e-5);
+  BOOST_REQUIRE_CLOSE(md.Evaluate(b, a), sqrt(14.0), 1e-5);
+}
+
+/**
+ * Simple test of Mahalanobis distance setting identity covariance in
+ * constructor.
+ */
+BOOST_AUTO_TEST_CASE(md_eye_covariance)
+{
+  MahalanobisDistance<false> md(4);
+  arma::vec a = "1.0 2.0 2.0 3.0";
+  arma::vec b = "0.0 0.0 1.0 3.0";
+
+  BOOST_REQUIRE_CLOSE(md.Evaluate(a, b), 6.0, 1e-5);
+  BOOST_REQUIRE_CLOSE(md.Evaluate(b, a), 6.0, 1e-5);
+}
+
+/**
+ * Simple test of Mahalanobis distance setting identity covariance in
+ * constructor and t_take_root set to true.
+ */
+BOOST_AUTO_TEST_CASE(md_root_eye_covariance)
+{
+  MahalanobisDistance<true> md(4);
   arma::vec a = "1.0 2.0 2.5 5.0";
   arma::vec b = "0.0 2.0 0.5 8.0";
 
