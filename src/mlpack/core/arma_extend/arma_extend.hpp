62ee170f07f6007c49048a10f12de5467ef62d9a
Ryan Curtin <ryan@ratml.org>
2012-09-16 22:56:05 +0000

    Remove internal sparse support; if sparse support is desired, use MLPACK with Armadillo 3.4.0 (or newer).

--- src/mlpack/core/arma_extend/arma_extend.hpp
+++ src/mlpack/core/arma_extend/arma_extend.hpp
@@ -12,66 +12,12 @@
 #ifndef __MLPACK_CORE_ARMA_EXTEND_ARMA_EXTEND_HPP
 #define __MLPACK_CORE_ARMA_EXTEND_ARMA_EXTEND_HPP
 
-// Figure out if we have sparse matrix support yet.
-#include <armadillo_bits/config.hpp>
-
-#ifndef ARMA_HAS_SPMAT
-  // Include forward declarations of SpMat, SpRow, and SpCol.
-  namespace arma {
-    #include "sparse/forward_bones.hpp"
-  };
-
-  // Now set the extra things to be included for regular Mat, Row, and Col.
-  #define ARMA_EXTRA_MAT_PROTO mlpack/core/arma_extend/sparse/Mat_extra_bones.hpp
-  #define ARMA_EXTRA_MAT_MEAT  mlpack/core/arma_extend/sparse/Mat_extra_meat.hpp
-#endif
-
 #include <armadillo>
 
 // To get CSV support on versions of Armadillo prior to 2.0.0, we'll do this.  I
 // feel dirty, but I think it's the best we can do.
 #if (ARMA_VERSION_MAJOR < 2)
   #define csv_ascii (ppm_binary + 1) // ppm_binary is the last in the old enums.
-#endif
-
-// Include all the things we might need for sparse matrix support.
-#ifndef ARMA_HAS_SPMAT
-
-namespace arma {
-  #include "sparse/traits.hpp"
-
-  #include "sparse/Proxy.hpp"
-
-  #include "sparse/SpValProxy_bones.hpp"
-  #include "sparse/SpMat_bones.hpp"
-  #include "sparse/SpCol_bones.hpp"
-  #include "sparse/SpRow_bones.hpp"
-  #include "sparse/SpSubview_bones.hpp"
-
-  #include "sparse/arma_ostream_bones.hpp"
-
-  #include "sparse/restrictors.hpp"
-
-  #include "sparse/fn_accu.hpp"
-  #include "sparse/fn_eye.hpp"
-  #include "sparse/fn_ones.hpp"
-//  #include "sparse/fn_qr.hpp"
-  #include "sparse/fn_randn.hpp"
-  #include "sparse/fn_randu.hpp"
-  #include "sparse/fn_zeros.hpp"
-  #include "sparse/fn_min.hpp"
-  #include "sparse/fn_max.hpp"
-
-  #include "sparse/SpValProxy_meat.hpp"
-  #include "sparse/SpMat_meat.hpp"
-  #include "sparse/SpCol_meat.hpp"
-  #include "sparse/SpRow_meat.hpp"
-  #include "sparse/SpSubview_meat.hpp"
-
-  #include "sparse/arma_ostream_meat.hpp"
-//  #include "sparse/op_dot_meat.hpp"
-};
-
 #endif
 
 namespace arma {
