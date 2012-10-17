571a3496e7f5cb1d3155d1803054562555876ba1
Ryan Curtin <ryan@ratml.org>
2012-10-17 18:58:30 +0000

    Change name of output and update MATLAB binding accordingly.

--- src/mlpack/bindings/matlab/emst/emst.m
+++ src/mlpack/bindings/matlab/emst/emst.m
@@ -1,25 +1,25 @@
 function result = emst(dataPoints, varargin)
 % Fast Euclidean Minimum Spanning Tree. This script can compute
-% the Euclidean minimum spanning tree of a set of input points using the 
+% the Euclidean minimum spanning tree of a set of input points using the
 % dual-tree Boruvka algorithm.
-% 
+%
 % The output is saved in a three-column matrix, where each row indicates an
 % edge.  The first column corresponds to the lesser index of the edge; the
 % second column corresponds to the greater index of the edge; and the third
 % column corresponds to the distance between the two points.
 %
 % Parameters:
-% dataPoints - the matrix of data points. Columns are assumed to represent dimensions, 
-%              with rows representing seperate points. 
-% method     - the algorithm for computing the tree. 'naive' or 'boruvka', with 
+% dataPoints - the matrix of data points. Columns are assumed to represent dimensions,
+%              with rows representing seperate points.
+% method     - the algorithm for computing the tree. 'naive' or 'boruvka', with
 %              'boruvka' being the default algorithm.
-% leafSize   - Leaf size in the kd-tree.  One-element leaves give the 
+% leafSize   - Leaf size in the kd-tree.  One-element leaves give the
 %              empirically best performance, but at the cost of greater memory
-%              requirements. One is default. 
-% 
+%              requirements. One is default.
+%
 % Examples:
 % result = emst(dataPoints);
-% or 
+% or
 % esult = emst(dataPoints,'method','naive');
 
 % a parser for the inputs
@@ -31,13 +31,13 @@ p.addOptional('leafSize', 1, @isscalar);
 p.parse(varargin{:});
 parsed = p.Results;
 
-% interfacing with mlpack. transposing to machine learning standards. 
+% interfacing with mlpack. transposing to machine learning standards.
 if strcmpi(parsed.method, 'boruvka')
-  result = mex_emst(dataPoints', 1, parsed.leafSize);
+  result = emst_mex(dataPoints', 1, parsed.leafSize);
 	result = result';
   return;
 else
-  result = mex_emst(dataPoints', 0, 1);
+  result = emst_mex(dataPoints', 0, 1);
 	result = result';
   return;
 end
