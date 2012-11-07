569a937e8e0ff792f54b5aa7f8f2d1839b9ab264
Ryan Curtin <ryan@ratml.org>
2012-11-07 20:48:28 +0000

    Add explicit constructors for sparse vectors, which are applied only when Armadillo 3.4 is used (this is a backport of new functionality).

---
+++ src/mlpack/core/arma_extend/Col_extra_bones.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file Col_extra_bones.hpp
+ * @author Ryan Curtin
+ *
+ * Add an extra explicit constructor for sparse vectors, but only if it doesn't
+ * already exist (Armadillo 3.4 specific).
+ */
+#ifdef ARMA_HAS_SPMAT
+  #if ARMA_VERSION_MAJOR == 3 && ARMA_VERSION_MINOR == 4
+    inline explicit Col(const SpCol<eT>& X);
+  #endif
+#endif
---
+++ src/mlpack/core/arma_extend/Row_extra_bones.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file Row_extra_bones.hpp
+ * @author Ryan Curtin
+ *
+ * Add an extra explicit constructor for sparse vectors, but only if it doesn't
+ * already exist (Armadillo 3.4 specific).
+ */
+#ifdef ARMA_HAS_SPMAT
+  #if ARMA_VERSION_MAJOR == 3 && ARMA_VERSION_MINOR == 4
+    inline explicit Row(const SpRow<eT>& X);
+  #endif
+#endif
