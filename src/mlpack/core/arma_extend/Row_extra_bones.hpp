1961e7e9826c5531d1afd711a9be8a869e02fdab
Ryan Curtin <ryan@ratml.org>
2012-11-07 22:39:08 +0000

    No need to typedef u64 anymore; set size_t == uword by defining ARMA_64BIT_WORD on systems where size_t is 64 bits; clean up preprocessor #ifs so that Row_ and Col_extra_bones work correctly.

--- src/mlpack/core/arma_extend/Row_extra_bones.hpp
+++ src/mlpack/core/arma_extend/Row_extra_bones.hpp
@@ -5,8 +5,6 @@
  * Add an extra explicit constructor for sparse vectors, but only if it doesn't
  * already exist (Armadillo 3.4 specific).
  */
-#ifdef ARMA_HAS_SPMAT
-  #if ARMA_VERSION_MAJOR == 3 && ARMA_VERSION_MINOR == 4
-    inline explicit Row(const SpRow<eT>& X);
-  #endif
+#if ARMA_VERSION_MAJOR == 3 && ARMA_VERSION_MINOR == 4
+  inline explicit Row(const SpRow<eT>& X);
 #endif
--- src/mlpack/core/arma_extend/arma_extend.hpp
+++ src/mlpack/core/arma_extend/arma_extend.hpp
@@ -12,6 +12,20 @@
 #ifndef __MLPACK_CORE_ARMA_EXTEND_ARMA_EXTEND_HPP
 #define __MLPACK_CORE_ARMA_EXTEND_ARMA_EXTEND_HPP
 
+// Use 64-bit indices (define uword as u64), but only if size_t is that size.
+// Basically, this will use 64-bit indices on 64-bit systems and 32-bit indices
+// on 32-bit systems (yes, there are exceptions).
+#if (ULONG_MAX > 0xffffffff)
+  #define ARMA_64BIT_WORD
+#endif
+
+// Add constructors for sparse vectors (these are only added if sparse support
+// is enabled).
+#define ARMA_EXTRA_COL_PROTO mlpack/core/arma_extend/Col_extra_bones.hpp
+#define ARMA_EXTRA_COL_MEAT  mlpack/core/arma_extend/Col_extra_meat.hpp
+#define ARMA_EXTRA_ROW_PROTO mlpack/core/arma_extend/Row_extra_bones.hpp
+#define ARMA_EXTRA_ROW_MEAT  mlpack/core/arma_extend/Row_extra_meat.hpp
+
 #include <armadillo>
 
 // To get CSV support on versions of Armadillo prior to 2.0.0, we'll do this.  I
@@ -20,12 +34,8 @@
   #define csv_ascii (ppm_binary + 1) // ppm_binary is the last in the old enums.
 #endif
 
-namespace arma {
-  // u64
-  #include "typedef.hpp"
-  #include "traits.hpp"
-  #include "promote_type.hpp"
 
+namespace arma {
   // ccov()
   #include "op_ccov_proto.hpp"
   #include "op_ccov_meat.hpp"
