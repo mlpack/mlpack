18688ca9d52f05aafcd4c71410ca63bd72fb4e07
Ryan Curtin <ryan@ratml.org>
2012-11-16 19:04:48 +0000

    Remove unnecessary Makefile; add header comment, remove unnecessary headers, and change tabs to spaces.

--- src/mlpack/bindings/matlab/kmeans/kmeans.cpp
+++ src/mlpack/bindings/matlab/kmeans/kmeans.cpp
@@ -1,52 +1,29 @@
+/**
+ * @file kmeans.cpp
+ * @author Patrick Mason
+ *
+ * MEX function for MATLAB k-means binding.
+ */
 #include "mex.h"
 
 #include <mlpack/core.hpp>
-
-#include "kmeans.hpp"
-#include "allow_empty_clusters.hpp"
+#include <mlpack/methods/kmeans/kmeans.hpp>
+#include <mlpack/methods/kmeans/allow_empty_clusters.hpp>
 
 using namespace mlpack;
 using namespace mlpack::kmeans;
 using namespace std;
 
-/*
-// Define parameters for the executable.
-PROGRAM_INFO("K-Means Clustering", "This program performs K-Means clustering "
-    "on the given dataset, storing the learned cluster assignments either as "
-    "a column of labels in the file containing the input dataset or in a "
-    "separate file.  Empty clusters are not allowed by default; when a cluster "
-    "becomes empty, the point furthest from the centroid of the cluster with "
-    "maximum variance is taken to fill that cluster.");
-
-PARAM_STRING_REQ("inputFile", "Input dataset to perform clustering on.", "i");
-PARAM_INT_REQ("clusters", "Number of clusters to find.", "c");
-
-PARAM_FLAG("in_place", "If specified, a column of the learned cluster "
-    "assignments will be added to the input dataset file.  In this case, "
-    "--outputFile is not necessary.", "p");
-PARAM_STRING("outputFile", "File to write output labels or labeled data to.",
-    "o", "output.csv");
-PARAM_FLAG("allow_empty_clusters", "Allow empty clusters to be created.", "e");
-PARAM_FLAG("labels_only", "Only output labels into output file.", "l");
-PARAM_DOUBLE("overclustering", "Finds (overclustering * clusters) clusters, "
-    "then merges them together until only the desired number of clusters are "
-    "left.", "O", 1.0);
-PARAM_INT("max_iterations", "Maximum number of iterations before K-Means "
-    "terminates.", "m", 1000);
-PARAM_INT("seed", "Random seed.  If 0, 'std::time(NULL)' is used.", "s", 0);
-PARAM_FLAG("fast_kmeans", "Use the experimental fast k-means algorithm by Pelleg and Moore", "f")
-*/
-
 void mexFunction(int nlhs, mxArray *plhs[],
                  int nrhs, const mxArray *prhs[])
 {
   // argument checks
-  if (nrhs != 7) 
+  if (nrhs != 7)
   {
     mexErrMsgTxt("Expecting seven arguments.");
   }
 
-  if (nlhs != 1) 
+  if (nlhs != 1)
   {
     mexErrMsgTxt("Output required.");
   }
@@ -109,10 +86,10 @@ void mexFunction(int nlhs, mxArray *plhs[],
   const size_t numPoints = mxGetN(prhs[0]);
   const size_t numDimensions = mxGetM(prhs[0]);
   arma::mat dataset(numDimensions, numPoints);
-  
-  // setting the values. 
+
+  // setting the values.
   double * mexDataPoints = mxGetPr(prhs[0]);
-  for (int i = 0, n = numPoints * numDimensions; i < n; ++i) 
+  for (int i = 0, n = numPoints * numDimensions; i < n; ++i)
   {
     dataset(i) = mexDataPoints[i];
   }
@@ -189,7 +166,7 @@ void mexFunction(int nlhs, mxArray *plhs[],
 
   // setting the values
   double * out = mxGetPr(plhs[0]);
-  for (int i = 0, n = assignments.n_elem; i < n; ++i) 
+  for (int i = 0, n = assignments.n_elem; i < n; ++i)
   {
     out[i] = assignments(i);
   }
--- src/mlpack/bindings/matlab/kmeans/test.m
+++
@@ -1,6 +0,0 @@
-dataPoints = [0, 0; 1, 1; 3, 3; 0.5, 0; 1000, 0; 1001, 0];
-
-% running the emst computation with mlpack
-result = kmeans(dataPoints, 2)
-
-
