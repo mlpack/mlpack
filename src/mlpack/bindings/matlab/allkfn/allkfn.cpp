1442b8378bbb422dcaba2da60ed25ed7f7cda550
Ryan Curtin <ryan@ratml.org>
2012-11-16 18:25:01 +0000

    Update AllkFN binding documentation to how it should eventually be.

--- src/mlpack/bindings/matlab/allkfn/allkfn.cpp
+++ src/mlpack/bindings/matlab/allkfn/allkfn.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file allkfn.cpp
+ * @author Patrick Mason
+ *
+ * MEX function for MATLAB All-kFN binding.
+ */
 #include "mex.h"
 
 #include <mlpack/core.hpp>
@@ -13,83 +19,28 @@ using namespace mlpack;
 using namespace mlpack::neighbor;
 using namespace mlpack::tree;
 
-/*
-// Information about the program itself.
-PROGRAM_INFO("All K-Furthest-Neighbors",
-    "This program will calculate the all k-furthest-neighbors of a set of "
-    "points. You may specify a separate set of reference points and query "
-    "points, or just a reference set which will be used as both the reference "
-    "and query set."
-    "\n\n"
-    "For example, the following will calculate the 5 furthest neighbors of each"
-    "point in 'input.csv' and store the distances in 'distances.csv' and the "
-    "neighbors in the file 'neighbors.csv':"
-    "\n\n"
-    "$ allkfn --k=5 --reference_file=input.csv --distances_file=distances.csv\n"
-    "  --neighbors_file=neighbors.csv"
-    "\n\n"
-    "The output files are organized such that row i and column j in the "
-    "neighbors output file corresponds to the index of the point in the "
-    "reference set which is the i'th furthest neighbor from the point in the "
-    "query set with index j.  Row i and column j in the distances output file "
-    "corresponds to the distance between those two points.");
-
-// Define our input parameters that this program will take.
-PARAM_STRING_REQ("reference_file", "File containing the reference dataset.",
-    "r");
-PARAM_INT_REQ("k", "Number of furthest neighbors to find.", "k");
-PARAM_STRING_REQ("distances_file", "File to output distances into.", "d");
-PARAM_STRING_REQ("neighbors_file", "File to output neighbors into.", "n");
-
-PARAM_STRING("query_file", "File containing query points (optional).", "q", "");
-
-PARAM_INT("leaf_size", "Leaf size for tree building.", "l", 20);
-PARAM_FLAG("naive", "If true, O(n^2) naive mode is used for computation.", "N");
-PARAM_FLAG("single_mode", "If true, single-tree search is used (as opposed to "
-    "dual-tree search.", "s");
-*/
-
 void mexFunction(int nlhs, mxArray *plhs[],
                  int nrhs, const mxArray *prhs[])
 {
-  // checking inputs
-	if (nrhs != 6) 
+  // Check the inputs.
+  if (nrhs != 6)
   {
     mexErrMsgTxt("Expecting seven arguments.");
   }
 
-  if (nlhs != 2) 
+  if (nlhs != 2)
   {
     mexErrMsgTxt("Two outputs required.");
   }
 
-	/*
-  // Give CLI the command line parameters the user passed in.
-  CLI::ParseCommandLine(argc, argv);
-
-  // Get all the parameters.
-  string referenceFile = CLI::GetParam<string>("reference_file");
-
-  string distancesFile = CLI::GetParam<string>("distances_file");
-  string neighborsFile = CLI::GetParam<string>("neighbors_file");
-
-  int lsInt = CLI::GetParam<int>("leaf_size");
-
-  size_t k = CLI::GetParam<int>("k");
-
-  bool naive = CLI::HasParam("naive");
-  bool singleMode = CLI::HasParam("single_mode");
-	*/
-
-	// getting the dimensions of the reference matrix
   size_t numPoints = mxGetN(prhs[0]);
   size_t numDimensions = mxGetM(prhs[0]);
 
-	// feeding the referenceData matrix
+  // Create the reference matrix.
 	arma::mat referenceData(numDimensions, numPoints);
-	// setting the values. 
+	// setting the values.
   double * mexDataPoints = mxGetPr(prhs[0]);
-  for (int i = 0, n = numPoints * numDimensions; i < n; ++i) 
+  for (int i = 0, n = numPoints * numDimensions; i < n; ++i)
   {
     referenceData(i) = mexDataPoints[i];
   }
@@ -105,11 +56,11 @@ void mexFunction(int nlhs, mxArray *plhs[],
 
 	// single mode?
 	bool singleMode = (mxGetScalar(prhs[5]) == 1.0);
-	
+
 	// the query matrix
 	double * mexQueryPoints = mxGetPr(prhs[2]);
-	arma::mat queryData; 
-	bool hasQueryData = ((mxGetM(prhs[2]) != 0) && (mxGetN(prhs[2]) != 0)); 
+	arma::mat queryData;
+	bool hasQueryData = ((mxGetM(prhs[2]) != 0) && (mxGetN(prhs[2]) != 0));
 
   //arma::mat referenceData;
   //arma::mat queryData; // So it doesn't go out of scope.
@@ -164,12 +115,12 @@ void mexFunction(int nlhs, mxArray *plhs[],
   //f (CLI::GetParam<string>("query_file") != "")
 	if (hasQueryData)
   {
-		// setting the values. 
+		// setting the values.
   	mexDataPoints = mxGetPr(prhs[2]);
   	numPoints = mxGetN(prhs[2]);
   	numDimensions = mxGetM(prhs[2]);
 		queryData = arma::mat(numDimensions, numPoints);
-  	for (int i = 0, n = numPoints * numDimensions; i < n; ++i) 
+  	for (int i = 0, n = numPoints * numDimensions; i < n; ++i)
   	{
    		queryData(i) = mexDataPoints[i];
   	}
@@ -239,12 +190,12 @@ void mexFunction(int nlhs, mxArray *plhs[],
 
   // setting the values
   double * out = mxGetPr(plhs[0]);
-  for (int i = 0, n = distances.n_rows * distances.n_cols; i < n; ++i) 
+  for (int i = 0, n = distances.n_rows * distances.n_cols; i < n; ++i)
   {
     out[i] = distances(i);
   }
   out = mxGetPr(plhs[1]);
-  for (int i = 0, n = neighbors.n_rows * neighbors.n_cols; i < n; ++i) 
+  for (int i = 0, n = neighbors.n_rows * neighbors.n_cols; i < n; ++i)
   {
     out[i] = neighbors(i);
   }
