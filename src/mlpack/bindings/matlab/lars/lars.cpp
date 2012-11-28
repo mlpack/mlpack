8f0fff4ff7b78d5a4c1bc4ffc82270d8837f9c46
Ryan Curtin <ryan@ratml.org>
2012-11-28 23:10:05 +0000

    Remove unnecessary Makefile, tabs to spaces, use correct includes.

--- src/mlpack/bindings/matlab/lars/lars.cpp
+++ src/mlpack/bindings/matlab/lars/lars.cpp
@@ -2,7 +2,7 @@
 
 #include <mlpack/core.hpp>
 
-#include "lars.hpp"
+#include <mlpack/methods/lars/lars.hpp>
 
 using namespace arma;
 using namespace std;
@@ -13,31 +13,31 @@ void mexFunction(int nlhs, mxArray *plhs[],
                  int nrhs, const mxArray *prhs[])
 {
   // argument checks
-  if (nrhs != 4) 
+  if (nrhs != 4)
   {
     mexErrMsgTxt("Expecting four inputs.");
   }
 
-  if (nlhs != 1) 
+  if (nlhs != 1)
   {
     mexErrMsgTxt("Output required.");
   }
 
-	double lambda1 = mxGetScalar(prhs[2]);
-	double lambda2 = mxGetScalar(prhs[3]);
-	bool useCholesky = (mxGetScalar(prhs[3]) == 1.0);
+  double lambda1 = mxGetScalar(prhs[2]);
+  double lambda2 = mxGetScalar(prhs[3]);
+  bool useCholesky = (mxGetScalar(prhs[3]) == 1.0);
 
-	// loading covariates
-	mat matX(mxGetM(prhs[0]), mxGetN(prhs[0]));
-	double * values = mxGetPr(prhs[0]);
-	for (int i=0, num=mxGetNumberOfElements(prhs[0]); i<num; ++i)
-		matX(i) = values[i];
+  // loading covariates
+  mat matX(mxGetM(prhs[0]), mxGetN(prhs[0]));
+  double * values = mxGetPr(prhs[0]);
+  for (int i=0, num=mxGetNumberOfElements(prhs[0]); i<num; ++i)
+    matX(i) = values[i];
 
-	// loading responses
-	mat matY(mxGetM(prhs[1]), mxGetN(prhs[1]));
-	values = mxGetPr(prhs[1]);
-	for (int i=0, num=mxGetNumberOfElements(prhs[1]); i<num; ++i)
-		matY(i) = values[i];
+  // loading responses
+  mat matY(mxGetM(prhs[1]), mxGetN(prhs[1]));
+  values = mxGetPr(prhs[1]);
+  for (int i=0, num=mxGetNumberOfElements(prhs[1]); i<num; ++i)
+    matY(i) = values[i];
 
   if (matY.n_cols > 1)
     mexErrMsgTxt("Only one column or row allowed in responses file!");
@@ -50,9 +50,9 @@ void mexFunction(int nlhs, mxArray *plhs[],
   vec beta;
   lars.Regress(matX, matY.unsafe_col(0), beta, false /* do not transpose */);
 
-	// return to matlab
-	plhs[0] = mxCreateDoubleMatrix(beta.n_elem, 1, mxREAL);
-	values = mxGetPr(plhs[0]);
-	for (int i = 0; i < beta.n_elem; ++i)
-		values[i] = beta(i);
+  // return to matlab
+  plhs[0] = mxCreateDoubleMatrix(beta.n_elem, 1, mxREAL);
+  values = mxGetPr(plhs[0]);
+  for (int i = 0; i < beta.n_elem; ++i)
+    values[i] = beta(i);
 }
