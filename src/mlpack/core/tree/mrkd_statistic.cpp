517014812a3e6191cb9a79c8ee396d5684f9d742
Ryan Curtin <ryan@ratml.org>
2012-12-21 20:52:04 +0000

    Refactor MRKDStatistic into three files (maybe a little overkill, I realize now).  Add accessors and mutators after making internal variables private.

---
+++ src/mlpack/core/tree/mrkd_statistic.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file mrkd_statistic.cpp
+ * @author James Cline
+ *
+ * Definition of the statistic for multi-resolution kd-trees.
+ */
+#include "mrkd_statistic.hpp"
+
+using namespace mlpack;
+using namespace mlpack::tree;
+
+MRKDStatistic::MRKDStatistic() :
+    dataset(NULL),
+    begin(0),
+    count(0),
+    leftStat(NULL),
+    rightStat(NULL),
+    parentStat(NULL)
+{ }
+
+/**
+ * Returns a string representation of this object.
+ */
+std::string MRKDStatistic::ToString() const
+{
+  std::ostringstream convert;
+
+  convert << "MRKDStatistic [" << this << std::endl;
+  convert << "begin: " << begin << std::endl;
+  convert << "count: " << count << std::endl;
+  convert << "sumOfSquaredNorms: " << sumOfSquaredNorms << std::endl;
+  if (leftStat != NULL)
+  {
+    convert << "leftStat:" << std::endl;
+    convert << mlpack::util::Indent(leftStat->ToString());
+  }
+  if (rightStat != NULL)
+  {
+    convert << "rightStat:" << std::endl;
+    convert << mlpack::util::Indent(rightStat->ToString());
+  }
+  return convert.str();
+}
