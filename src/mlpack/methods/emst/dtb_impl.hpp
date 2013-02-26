1896e4f31238abbe33607c49dc63d04a5042eeeb
Ryan Curtin <ryan@ratml.org>
2013-02-26 22:50:34 +0000

    Adapt to modified statistic API.

--- src/mlpack/methods/emst/dtb_impl.hpp
+++ src/mlpack/methods/emst/dtb_impl.hpp
@@ -26,27 +26,11 @@ DTBStat::DTBStat() : maxNeighborDistance(DBL_MAX), componentMembership(-1)
 /**
  * An initializer for leaves.
  */
-template<typename MatType>
-DTBStat::DTBStat(const MatType& /* dataset */,
-                 const size_t start,
-                 const size_t count) :
+template<typename TreeType>
+DTBStat::DTBStat(const TreeType& node) :
     maxNeighborDistance(DBL_MAX),
-    componentMembership((count == 1) ? start : -1)
-{
-  // Nothing to do.
-}
-
-/**
- * An initializer for non-leaves.
- */
-template<typename MatType>
-DTBStat::DTBStat(const MatType& /* dataset */,
-                 const size_t start,
-                 const size_t count,
-                 const DTBStat& /* leftStat */,
-                 const DTBStat& /* rightStat */) :
-    maxNeighborDistance(DBL_MAX),
-    componentMembership((count == 1) ? start : -1)
+    componentMembership(
+        ((node.NumPoints() == 1) && (node.NumChildren() == 0)) ? start : -1)
 {
   // Nothing to do.
 }
@@ -140,11 +124,11 @@ void DualTreeBoruvka<MetricType, TreeType>::ComputeMST(arma::mat& results)
 
   while (edges.size() < (data.n_cols - 1))
   {
-    
+
     typename TreeType::template DualTreeTraverser<RuleType> traverser(rules);
-    
+
     traverser.Traverse(*tree, *tree);
-    
+
     AddAllEdges();
 
     Cleanup();
