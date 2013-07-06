2581da949c661d17ee64132f3ca92d835c6993ab
Parikshit Ram <p.ram@gatech.edu>
2013-07-06 01:57:46 +0000

    Chebyshev distance (L-infinity distance) added to lmetric.hpp

--- src/mlpack/core/metrics/lmetric_impl.hpp
+++ src/mlpack/core/metrics/lmetric_impl.hpp
@@ -78,6 +78,14 @@ double LMetric<3, false>::Evaluate(const VecType1& a, const VecType2& b)
   return accu(pow(abs(a - b), 3.0));
 }
 
+// L-infinity (Chebyshev distance) specialization
+template<>
+template<typename VecType1, typename VecType2>
+double LMetric<INT_MAX, false>::Evaluate(const VecType1& a, const VecType2& b)
+{
+  return max(abs(a - b));
+}
+
 }; // namespace metric
 }; // namespace mlpack
 
