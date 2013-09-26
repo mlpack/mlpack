b5d7f18cb22ea59b6b2fd054807a7cbab479b674
Marcus Edel <marcus.edel@fu-berlin.de>
2013-09-26 19:39:58 +0000

    Update the timer example call and output.

--- doc/guide/timer.hpp
+++ doc/guide/timer.hpp
@@ -7,12 +7,14 @@ methods.  The results of any timers used during the program are displayed at
 output by the mlpack::CLI object, when --verbose is given:
 
 @code
-$ allknn -i=data.csv -k 5
+$ allknn -r dataset.csv -n neighbors_out.csv -d distances_out.csv -k 5 -v
 <...>
 [INFO ] Program timers:
-[INFO ]   computing_neighbors: 0.044764s
-[INFO ]   total_time: 0.061249s
-[INFO ]   tree_building: 0.003075s
+[INFO ]   computing_neighbors: 0.010650s
+[INFO ]   loading_data: 0.002567s
+[INFO ]   saving_data: 0.001115s
+[INFO ]   total_time: 0.149816s
+[INFO ]   tree_building: 0.000534s
 @endcode
 
 @section usingtimer Timer API
