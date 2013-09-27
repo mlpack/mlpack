55d31236f50c2a8e99a2d49362c530338a66ca71
Ryan Curtin <ryan@ratml.org>
2013-09-27 01:47:32 +0000

    Update NMF to work with sparse matrices also.

--- src/mlpack/methods/nmf/random_init.hpp
+++ src/mlpack/methods/nmf/random_init.hpp
@@ -19,7 +19,8 @@ class RandomInitialization
   // Empty constructor required for the InitializeRule template
   RandomInitialization() { }
 
-  inline static void Initialize(const arma::mat& V,
+  template<typename MatType>
+  inline static void Initialize(const MatType& V,
                                 const size_t r,
                                 arma::mat& W,
                                 arma::mat& H)
