b2c914841e6e938ba9c9d3718f5959b3d4671e39
Ryan Curtin <ryan@ratml.org>
2013-10-01 16:04:45 +0000

    Clean up CFTest, and fix some comments that were wrong.

--- src/mlpack/tests/cf_test.cpp
+++ src/mlpack/tests/cf_test.cpp
@@ -19,113 +19,102 @@ using namespace mlpack::cf;
 using namespace std;
 
 /**
- * Make sure that the rated item is not recommended
+ * Make sure that the constructor works okay.
  */
 BOOST_AUTO_TEST_CASE(CFConstructorTest)
 {
-  //Matrix to save data
-  arma::mat dataset, retDataset; 
-
-  //Loading GroupLens data 
+  // Load GroupLens data.
+  arma::mat dataset;
   data::Load("GroupLens100k.csv", dataset);
 
-  //Number of Recommendations
-  size_t numRecs = 10;
-  
-  //Number of users for similarity
-  size_t numUsersForSimilarity = 5;
+  // Number of recommendations (not the default).
+  const size_t numRecs = 15;
 
-  //Creating a CF object
+  // Number of users for similarity (not the default).
+  const size_t numUsersForSimilarity = 8;
+
   CF c(numRecs, numUsersForSimilarity, dataset);
-  
-  //Getter
-  retDataset = c.Data();
-  //Checking for parameters
+
+  // Check parameters.
   BOOST_REQUIRE_EQUAL(c.NumRecs(), numRecs);
   BOOST_REQUIRE_EQUAL(c.NumUsersForSimilarity(), numUsersForSimilarity);
-  
-  //Checking Data
-  BOOST_REQUIRE_EQUAL(retDataset.n_rows, dataset.n_rows);  
-  BOOST_REQUIRE_EQUAL(retDataset.n_cols, dataset.n_cols);    
 
-  //Checking Values
-  for (size_t i=0;i<dataset.n_rows;i++)
-    for (size_t j=0;j<dataset.n_cols;j++)
-      BOOST_REQUIRE_EQUAL(retDataset(i,j),dataset(i,j));  
+  // Check data.
+  BOOST_REQUIRE_EQUAL(c.Data().n_rows, dataset.n_rows);
+  BOOST_REQUIRE_EQUAL(c.Data().n_cols, dataset.n_cols);
+
+  // Check values (this should be superfluous...).
+  for (size_t i = 0; i < dataset.n_rows; i++)
+    for (size_t j = 0; j < dataset.n_cols; j++)
+      BOOST_REQUIRE_EQUAL(c.Data()(i, j), dataset(i, j));
 }
 
-/*
- * Make sure that correct number of recommendations 
- * are generated when query set. Default Case.
+/**
+ * Make sure that correct number of recommendations are generated when query
+ * set. Default case.
  */
 BOOST_AUTO_TEST_CASE(CFGetRecommendationsAllUsersTest)
 {
-  //Dummy number of recommendations
+  // Dummy number of recommendations.
   size_t numRecs = 3;
-  //GroupLens100k.csv dataset has 943 users
+  // GroupLens100k.csv dataset has 943 users.
   size_t numUsers = 943;
- 
-  //Matrix to save recommednations
-  arma::Mat<size_t> recommendations;
-  
-  //Matrix to save data
-  arma::mat dataset; 
 
-  //Loading GroupLens data 
+  // Matrix to save recommendations into.
+  arma::Mat<size_t> recommendations;
+
+  // Load GroupLens data.
+  arma::mat dataset;
   data::Load("GroupLens100k.csv", dataset);
 
-  //Creating a CF object
+  // Creat a CF object
   CF c(dataset);
 
-  //Setting Number of Recommendations
+  // Set number of recommendations.
   c.NumRecs(numRecs);
-  
-  //Generating Recommendations when query set is not specified 
+
+  // Generate recommendations when query set is not specified.
   c.GetRecommendations(recommendations);
 
-  //Checking if correct number of Recommendations are generated
+  // Check if correct number of recommendations are generated.
   BOOST_REQUIRE_EQUAL(recommendations.n_rows, numRecs);
 
-  //Checking if recommendations are generated for all users 
+  // Check if recommendations are generated for all users.
   BOOST_REQUIRE_EQUAL(recommendations.n_cols, numUsers);
 }
 
-/*
- * Make sure that the recommendations are genrated 
- * for queried users only
- */ 
+/**
+ * Make sure that the recommendations are generated for queried users only.
+ */
 BOOST_AUTO_TEST_CASE(CFGetRecommendationsQueriedUserTest)
 {
-  //Number of users for which recommendations are seeked
+  // Number of users that we will search for recommendations for.
   size_t numUsers = 10;
-  
-  //Default number of recommendations
-  size_t numRecsDefault = 5;
- 
-  //Creaating dummy query set
-  arma::Col<size_t> users = arma::zeros<arma::Col<size_t> >(numUsers,1);
-  for (size_t i=0;i<numUsers;i++)
-    users(i) = i+1;
-  
-  //Matrix to save recommednations
-  arma::Mat<size_t> recommendations;
-  
-  //Matrix to save data
-  arma::mat dataset; 
 
-  //Loading GroupLens data 
+  // Default number of recommendations.
+  size_t numRecsDefault = 5;
+
+  // Creaate dummy query set.
+  arma::Col<size_t> users = arma::zeros<arma::Col<size_t> >(numUsers, 1);
+  for (size_t i = 0; i < numUsers; i++)
+    users(i) = i + 1;
+
+  // Matrix to save recommendations into.
+  arma::Mat<size_t> recommendations;
+
+  // Load GroupLens data.
+  arma::mat dataset;
   data::Load("GroupLens100k.csv", dataset);
 
-  //Creating a CF object
   CF c(dataset);
-  
-  //Generating Recommendations when query set is not specified 
+
+  // Generate recommendations when query set is specified.
   c.GetRecommendations(recommendations, users);
 
-  //Checking if correct number of Recommendations are generated
+  // Check if correct number of recommendations are generated.
   BOOST_REQUIRE_EQUAL(recommendations.n_rows, numRecsDefault);
 
-  //Checking if recommendations are generated for all users 
+  // Check if recommendations are generated for the right number of users.
   BOOST_REQUIRE_EQUAL(recommendations.n_cols, numUsers);
 }
 
