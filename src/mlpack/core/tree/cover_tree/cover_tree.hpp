69d2a71e4a90f830aa31792762fc03bf04782d5c
Ryan Curtin <ryan@ratml.org>
2013-10-03 17:24:23 +0000

    This is premature; wait for this until later.

--- src/mlpack/core/tree/cover_tree/cover_tree.hpp
+++ src/mlpack/core/tree/cover_tree/cover_tree.hpp
@@ -217,6 +217,11 @@ class CoverTree
   size_t Point(const size_t) const { return point; }
 
   // Fake
+  CoverTree* Left() const { return NULL; }
+  CoverTree* Right() const { return NULL; }
+  size_t Begin() const { return 0; }
+  size_t Count() const { return 0; }
+  size_t End() const { return 0; }
   bool IsLeaf() const { return (children.size() == 0); }
   size_t NumPoints() const { return 1; }
 
