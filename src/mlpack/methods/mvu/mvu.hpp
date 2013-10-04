fbbc133a81bc07eb58f107fa543cb7e3c8de42b2
Ryan Curtin <ryan@ratml.org>
2013-10-04 04:33:03 +0000

    Add a note that this does not work.

--- src/mlpack/methods/mvu/mvu.hpp
+++ src/mlpack/methods/mvu/mvu.hpp
@@ -6,6 +6,8 @@
  * class as well as a class representing the objective function (a semidefinite
  * program) which MVU seeks to minimize.  Minimization is performed by the
  * Augmented Lagrangian optimizer (which in turn uses the L-BFGS optimizer).
+ *
+ * Note: this implementation of MVU does not work.  See #189.
  */
 #ifndef __MLPACK_METHODS_MVU_MVU_HPP
 #define __MLPACK_METHODS_MVU_MVU_HPP
