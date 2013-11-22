86dcd5c93b760ee4deb33b22d6c50f9b449f6bdc
Ryan Curtin <ryan@ratml.org>
2013-11-22 18:49:34 +0000

    Implement FurthestPointDistance() for cover trees.

--- src/mlpack/core/tree/cover_tree/cover_tree.hpp
+++ src/mlpack/core/tree/cover_tree/cover_tree.hpp
@@ -310,10 +310,14 @@ class CoverTree
   //! Modify the distance to the parent.
   double& ParentDistance() { return parentDistance; }
 
-  //! Get the distance to the furthest descendant.
+  //! Get the distance to the furthest point.  This is always 0 for cover trees.
+  double FurthestPointDistance() const { return 0.0; }
+
+  //! Get the distance from the center of the node to the furthest descendant.
   double FurthestDescendantDistance() const
   { return furthestDescendantDistance; }
-  //! Modify the distance to the furthest descendant.
+  //! Modify the distance from the center of the node to the furthest
+  //! descendant.
   double& FurthestDescendantDistance() { return furthestDescendantDistance; }
 
   //! Get the centroid of the node and store it in the given vector.
