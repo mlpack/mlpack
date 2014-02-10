896e8a409f8db3237aaf12fda7724304d9c9b858
Ryan Curtin <ryan@ratml.org>
2014-02-10 13:31:54 +0000

    Remove license text.

--- src/mlpack/tests/hmm_test.cpp
+++ src/mlpack/tests/hmm_test.cpp
@@ -2,21 +2,6 @@
  * @file hmm_test.cpp
  *
  * Test file for HMMs.
- *
- * This file is part of MLPACK 1.0.7.
- *
- * MLPACK is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * MLPACK is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
- * details (LICENSE.txt).
- *
- * You should have received a copy of the GNU General Public License along with
- * MLPACK.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <mlpack/core.hpp>
 #include <mlpack/methods/hmm/hmm.hpp>
@@ -61,7 +46,7 @@ BOOST_AUTO_TEST_CASE(SimpleDiscreteHMMTestViterbi)
   arma::mat observation = "0 0 1 0 0";
   arma::Col<size_t> states;
   hmm.Predict(observation, states);
-    
+
   // Check each state.
   BOOST_REQUIRE_EQUAL(states[0], 0); // Rain.
   BOOST_REQUIRE_EQUAL(states[1], 0); // Rain.
