087192ae5c5f7960280291ad18e7e595b5277d42
Ryan Curtin <ryan@ratml.org>
2014-03-28 15:38:26 +0000

    Refactor LRSDP into the main class and a separate function (LRSDPFunction). This is a patch from Abhishek Laddha for #318.

--- src/mlpack/core/optimizers/lrsdp/lrsdp.hpp
+++ src/mlpack/core/optimizers/lrsdp/lrsdp.hpp
@@ -11,17 +11,15 @@
 #include <mlpack/core.hpp>
 #include <mlpack/core/optimizers/aug_lagrangian/aug_lagrangian.hpp>
 
+#include "lrsdp_function.hpp"
+
 namespace mlpack {
 namespace optimization {
 
 /**
- * An implementation of a low-rank semidefinite program solver, based on
- * Monteiro and Burer's formulation.  The optimizer is the augmented Lagrangian
- * algorithm, and thus this class has Evaluate(), Gradient(),
- * EvaluateConstraint(), and GradientConstraint() functions for that purpose.
- * However, you should not call the Gradient() or GradientConstraint() functions
- * by hand, because they are only implemented as part of the augmented
- * Lagrangian optimizer.
+ * LRSDP is the implementation of Monteiro and Burer's formulation of low-rank
+ * semidefinite programs (LR-SDP).  This solver uses the augmented Lagrangian
+ * optimizer to solve low-rank semidefinite programs.
  */
 class LRSDP
 {
@@ -49,7 +47,7 @@ class LRSDP
    */
   LRSDP(const size_t numConstraints,
         const arma::mat& initialPoint,
-        AugLagrangian<LRSDP>& augLagrangian);
+        AugLagrangian<LRSDPFunction>& augLagrangian);
 
   /**
    * Optimize the LRSDP and return the final objective value.  The given
@@ -59,87 +57,45 @@ class LRSDP
    */
   double Optimize(arma::mat& coordinates);
 
-  /**
-   * Evaluate the objective function of the LRSDP (no constraints) at the given
-   * coordinates.  This is used by AugLagrangian<LRSDP>.
-   */
-  double Evaluate(const arma::mat& coordinates) const;
-
-  /**
-   * Evaluate the gradient of the LRSDP (no constraints) at the given
-   * coordinates.  This is used by AugLagrangian<LRSDP>.
-   */
-  void Gradient(const arma::mat& coordinates, arma::mat& gradient) const;
-
-  /**
-   * Evaluate a particular constraint of the LRSDP at the given coordinates.
-   */
-  double EvaluateConstraint(const size_t index,
-                            const arma::mat& coordinates) const;
-
-  /**
-   * Evaluate the gradient of a particular constraint of the LRSDP at the given
-   * coordinates.
-   */
-  void GradientConstraint(const size_t index,
-                          const arma::mat& coordinates,
-                          arma::mat& gradient) const;
-
-  //! Get the number of constraints in the LRSDP.
-  size_t NumConstraints() const { return b.n_elem; }
-
-  //! Get the initial point of the LRSDP.
-  const arma::mat& GetInitialPoint();
-
   //! Return the objective function matrix (C).
-  const arma::mat& C() const { return c; }
+  const arma::mat& C() const { return function.C(); }
   //! Modify the objective function matrix (C).
-  arma::mat& C() { return c; }
+  arma::mat& C() { return function.C(); }
 
   //! Return the vector of A matrices (which correspond to the constraints).
-  const std::vector<arma::mat>& A() const { return a; }
+  const std::vector<arma::mat>& A() const { return function.A(); }
   //! Modify the veector of A matrices (which correspond to the constraints).
-  std::vector<arma::mat>& A() { return a; }
+  std::vector<arma::mat>& A() { return function.A(); }
 
   //! Return the vector of modes for the A matrices.
-  const arma::uvec& AModes() const { return aModes; }
+  const arma::uvec& AModes() const { return function.AModes(); }
   //! Modify the vector of modes for the A matrices.
-  arma::uvec& AModes() { return aModes; }
+  arma::uvec& AModes() { return function.AModes(); }
 
   //! Return the vector of B values.
-  const arma::vec& B() const { return b; }
+  const arma::vec& B() const { return function.B(); }
   //! Modify the vector of B values.
-  arma::vec& B() { return b; }
+  arma::vec& B() { return function.B(); }
+
+  //! Return the function to be optimized.
+  const LRSDPFunction& Function() const { return function; }
+  //! Modify the function to be optimized.
+  LRSDPFunction& Function() { return function; }
 
   //! Return the augmented Lagrangian object.
-  const AugLagrangian<LRSDP>& AugLag() const { return augLag; }
+  const AugLagrangian<LRSDPFunction>& AugLag() const { return augLag; }
   //! Modify the augmented Lagrangian object.
-  AugLagrangian<LRSDP>& AugLag() { return augLag; }
+  AugLagrangian<LRSDPFunction>& AugLag() { return augLag; }
 
-  // convert the obkect into a string
+  //! Return a string representation of the object.
   std::string ToString() const;
 
  private:
-  // Should probably use sparse matrices for some of these.
-
-  //! For objective function.
-  arma::mat c;
-  //! A_i for each constraint.
-  std::vector<arma::mat> a;
-  //! b_i for each constraint.
-  arma::vec b;
-
-  //! 1 if entries in matrix, 0 for normal.
-  arma::uvec aModes;
-
-  //! Initial point.
-  arma::mat initialPoint;
-
-  //! Internal AugLagrangian object, if one was not passed at construction time.
-  AugLagrangian<LRSDP> augLagInternal;
+  //! Function to optimize, which the AugLagrangian object holds.
+  LRSDPFunction function;
 
   //! The AugLagrangian object which will be used for optimization.
-  AugLagrangian<LRSDP>& augLag;
+  AugLagrangian<LRSDPFunction> augLag;
 };
 
 }; // namespace optimization
