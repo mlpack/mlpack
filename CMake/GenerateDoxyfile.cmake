0d106c9efdbc8bbfacbf5889f35226b2ca3ac4e3
Ryan Curtin <ryan@ratml.org>
2014-04-07 14:20:21 +0000

    Doxygen fix so that local paths aren't included in the built documentation.

--- CMake/GenerateDoxyfile.cmake
+++ CMake/GenerateDoxyfile.cmake
@@ -25,5 +25,12 @@ string(REPLACE
     DOXYFILE_AUXVAR "${DOXYFILE_CONTENTS}"
 )
 
+# Change the STRIP_FROM_PATH so that it works right even in the build directory;
+# otherwise, every file will have the full path in it.
+string(REGEX REPLACE
+    "(STRIP_FROM_PATH[ ]*=) ./"
+    "\\1 ${CMAKE_CURRENT_SOURCE_DIR}/"
+    DOXYFILE_CONTENTS ${DOXYFILE_AUXVAR})
+
 # Save the Doxyfile to its new location.
-file(WRITE "${DESTDIR}/Doxyfile" "${DOXYFILE_AUXVAR}")
+file(WRITE "${DESTDIR}/Doxyfile" "${DOXYFILE_CONTENTS}")
