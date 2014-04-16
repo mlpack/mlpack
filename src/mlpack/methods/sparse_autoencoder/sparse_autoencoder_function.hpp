f8c663a52da2b5c37712be5f0e9fc2c99d1f70d6
Ryan Curtin <ryan@ratml.org>
2014-04-16 18:35:16 +0000

    First pass: add file headers, include guards, and then my vimrc auto-stripped all trailing spaces.

--- src/mlpack/methods/sparse_autoencoder/sparse_autoencoder_function.hpp
+++ src/mlpack/methods/sparse_autoencoder/sparse_autoencoder_function.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file sparse_autoencoder_function.hpp
+ * @author Siddharth Agrawal
+ *
+ * The function to be optimized for sparse autoencoders.  Any mlpack optimizer
+ * can be used.
+ */
+#ifndef __MLPACK_METHODS_SPARSE_AUTOENCODER_SPARSE_AUTOENCODER_FUNCTION_HPP
+#define __MLPACK_METHODS_SPARSE_AUTOENCODER_SPARSE_AUTOENCODER_FUNCTION_HPP
+
 #include <mlpack/core.hpp>
 
 namespace mlpack {
@@ -11,17 +21,17 @@ namespace nn {
 class SparseAutoencoderFunction
 {
  public:
- 
+
   SparseAutoencoderFunction(const arma::mat& data,
                             const size_t visibleSize,
                             const size_t hiddenSize,
                             const double lambda = 0.0001,
                             const double beta = 3,
                             const double rho = 0.01);
-  
+
   //Initializes the parameters of the model to suitable values.
   const arma::mat InitializeWeights();
-  
+
   /**
    * Evaluates the objective function of the Sparse Autoencoder model using the
    * given parameters. The cost function has terms for the reconstruction
@@ -33,29 +43,29 @@ class SparseAutoencoderFunction
    * @param parameters Current values of the model parameters.
    */
   double Evaluate(const arma::mat& parameters) const;
-  
+
   /**
    * Evaluates the gradient values of the parameters given the current set of
-   * parameters. The function performs a feedforward pass and computes the error 
-   * in reconstructing the data points. It then uses the backpropagation 
+   * parameters. The function performs a feedforward pass and computes the error
+   * in reconstructing the data points. It then uses the backpropagation
    * algorithm to compute the gradient values.
    *
    * @param parameters Current values of the model parameters.
    * @param gradient Pointer to matrix where gradient values are stored.
    */
   void Gradient(const arma::mat& parameters, arma::mat& gradient) const;
-  
+
   /**
    * Returns the elementwise sigmoid of the passed matrix, where the sigmoid
    * function of a real number 'x' is [1 / (1 + exp(-x))].
-   * 
+   *
    * @param x Matrix of real values for which we require the sigmoid activation.
    */
   arma::mat Sigmoid(const arma::mat& x) const
   {
     return (1.0 / (1 + arma::exp(-x)));
   }
-  
+
   //! Return the initial point for the optimization.
   const arma::mat& GetInitialPoint() const { return initialPoint; }
 
@@ -64,7 +74,7 @@ class SparseAutoencoderFunction
   {
     this->visibleSize = visible;
   }
-  
+
   //! Gets size of the visible layer.
   size_t VisibleSize() const
   {
@@ -77,42 +87,42 @@ class SparseAutoencoderFunction
     this->hiddenSize = hidden;
   }
 
-  //! Gets the size of the hidden layer.  
+  //! Gets the size of the hidden layer.
   size_t HiddenSize() const
   {
     return hiddenSize;
   }
-  
+
   //! Sets the L2-regularization parameter.
   void Lambda(const double l)
   {
     this->lambda = l;
   }
-  
+
   //! Gets the L2-regularization parameter.
   double Lambda() const
   {
     return lambda;
   }
-  
+
   //! Sets the KL divergence parameter.
   void Beta(const double b)
   {
     this->beta = b;
   }
-  
+
   //! Gets the KL divergence parameter.
   double Beta() const
   {
     return beta;
   }
-  
+
   //! Sets the sparsity parameter.
   void Rho(const double r)
   {
     this->rho = r;
   }
-  
+
   //! Gets the sparsity parameter.
   double Rho() const
   {
@@ -120,7 +130,7 @@ class SparseAutoencoderFunction
   }
 
  private:
- 
+
   //! The matrix of data points.
   const arma::mat& data;
   //! Intial parameter vector.
@@ -139,3 +149,5 @@ class SparseAutoencoderFunction
 
 }; // namespace nn
 }; // namespace mlpack
+
+#endif
