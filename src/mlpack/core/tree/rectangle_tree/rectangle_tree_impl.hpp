ed1b2463c86b1169484421f270550ce65adcd66d
andrewmw94 <andrewmw94@gmail.com>
2014-05-19 17:32:08 +0000

    Add files and some preliminary code for R tree

---
+++ src/mlpack/core/tree/rectangle_tree/rectangle_tree_impl.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file rectangle_tree_impl.hpp
+ *
+ * Implementation of generalized rectangle tree.
+ */
+#ifndef __MLPACK_CORE_TREE_RECTANGLE_TREE_RECTANGLE_TREE_IMPL_HPP
+#define __MLPACK_CORE_TREE_RECTANGLE_TREE_RECTANGLE_TREE_IMPL_HPP
+
+// In case it wasn't included already for sem reason.
+#include "rectangle_tree.hpp"
+
+namespace mlpack {
+namespace tree {
+
+template<typename StatisticType,
+	   typename MatType>
+RectangleTree<StatisticType, MatType>::RectangleTree()
+
+
+}; //namespace tree
+}; //namespace mlpack
