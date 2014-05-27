82adf9e946af4a735757402e2f0e8cc76ae73e34
andrewmw94 <andrewmw94@gmail.com>
2014-05-27 12:17:52 +0000

    more R tree stuff.

--- src/mlpack/core/tree/rectangle_tree.hpp
+++ src/mlpack/core/tree/rectangle_tree.hpp
@@ -5,8 +5,8 @@
  * Include all the necessary filse to use the Rectangle Type Trees (RTree, RStarTree, XTree,
  * and HilbertRTree.)
  */
-#ifndef __MLPACK_CORE_TREE_RECTINGLINEAR_TREE_RECTANGLINEAR_TREE_HPP
-#define __MLPACK_CORE_TREE_RECTINGLINEAR_TREE_RECTANGLINEAR_TREE_HPP
+#ifndef __MLPACK_CORE_TREE_RECTINGLE_TREE_RECTANGLE_TREE_HPP
+#define __MLPACK_CORE_TREE_RECTINGLE_TREE_RECTANGLE_TREE_HPP
 
 /* we include bounds.hpp since it gives us the necessary files.
  * However, we will not use the "ballbounds" option.
