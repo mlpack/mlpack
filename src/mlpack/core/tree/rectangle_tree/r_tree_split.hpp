dc5dda18051caf650ba2002fad89226579a95aac
andrewmw94 <andrewmw94@9d5b8971-822b-0410-80eb-d18c1038ef23>
2014-05-27 20:49:20 +0000

    more stuff for R Trees

--- src/mlpack/core/tree/rectangle_tree/r_tree_split.hpp
+++ src/mlpack/core/tree/rectangle_tree/r_tree_split.hpp
@@ -1,1 +1,49 @@
- 
+/**
+ * @file r_tree_split.hpp
+ * @author Andrew Wells
+ *
+ * Defintion of the RTreeSplit class, a class that splits the nodes of an R tree, starting
+ * at a leaf node and moving upwards if necessary.
+ */
+#ifndef __MLPACK_CORE_TREE_RECTANGLE_TREE_R_TREE_SPLIT_HPP
+#define __MLPACK_CORE_TREE_RECTANGLE_TREE_R_TREE_SPLIT_HPP
+
+#include <mlpack/core.hpp>
+
+namespace mlpack {
+namespace tree /** Trees and tree-building procedures. */ {
+
+/**
+ * A Rectangle Tree has new points inserted at the bottom.  When these
+ * nodes overflow, we split them, moving up the tree and splitting nodes
+ * as necessary.
+ */
+template<typename MatType = arma::mat>
+class RTreeSplit
+{
+public:
+
+/**
+ * Split a leaf node using the "default" algorithm.  The methods for splitting non-leaf
+ * nodes are private since they should only be called if a leaf node overflows.
+ */
+static bool SplitLeafNode(const RectangleTree& tree);
+
+private:
+
+/**
+ * Split a non-leaf node using the "default" algorithm.
+ */
+static bool SplitNonLeafNode(const RectangleTree& tree);
+
+};
+
+}; // namespace tree
+}; // namespace mlpack
+
+// Include implementation
+#include "r_tree_split_impl.hpp"
+
+#endif
+
+
