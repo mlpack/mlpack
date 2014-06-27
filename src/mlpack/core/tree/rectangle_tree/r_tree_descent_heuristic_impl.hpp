e62a8c663c64026216241e54c0db398ae42f5327
andrewmw94 <andrewmw94@9d5b8971-822b-0410-80eb-d18c1038ef23>
2014-06-27 15:30:54 +0000

    Rectangle tree and tests.  Construction seems to work.

--- src/mlpack/core/tree/rectangle_tree/r_tree_descent_heuristic_impl.hpp
+++ src/mlpack/core/tree/rectangle_tree/r_tree_descent_heuristic_impl.hpp
@@ -15,7 +15,6 @@ namespace tree {
 
 inline double RTreeDescentHeuristic::EvalNode(const HRectBound<>& bound, const arma::vec& point)
 {
-  std::cout << "eval node called" << std::endl;
   return bound.Contains(point) ? 0 : bound.MinDistance(point);
 }
 
