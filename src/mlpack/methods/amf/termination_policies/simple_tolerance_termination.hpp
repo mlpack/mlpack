339486fa03d4932d013060c50f1d6ab66fc4595d
sumedhghaisas <sumedhghaisas@gmail.com>
2014-07-02 21:07:42 +0000

    * faster implementation of SVDBatchWithMomentum * tolerance termination policy is modified according to new policy * test point selection in validation RMSE termination is shifted to constructor

--- src/mlpack/methods/amf/termination_policies/simple_tolerance_termination.hpp
+++ src/mlpack/methods/amf/termination_policies/simple_tolerance_termination.hpp
@@ -10,6 +10,7 @@
 namespace mlpack {
 namespace amf {
 
+template <class MatType>
 class SimpleToleranceTermination
 {
  public:
@@ -17,44 +18,50 @@ class SimpleToleranceTermination
                              const size_t maxIterations = 10000)
             : tolerance(tolerance), maxIterations(maxIterations) {}
 
-  template<typename MatType>
-  void Initialize(MatType& V)
+  void Initialize(const MatType& V)
   {
     residueOld = DBL_MAX;
     iteration = 1;
-    normOld = 0;
     residue = DBL_MIN;
 
-    const size_t n = V.n_rows;
-    const size_t m = V.n_cols;
-
-    nm = n * m;
+    this->V = &V;
   }
 
   bool IsConverged()
   {
-    if(((residueOld - residue) / residueOld < tolerance && iteration > 4) 
+    if(((residueOld - residue) / residueOld < tolerance && iteration > 4)
         || iteration > maxIterations) return true;
     else return false;
   }
 
-  template<typename MatType>
-  void Step(const MatType& W, const MatType& H)
+  void Step(const arma::mat& W, const arma::mat& H)
   {
     // Calculate norm of WH after each iteration.
     arma::mat WH;
 
     WH = W * H;
-    double norm = sqrt(accu(WH % WH) / nm);
 
-    if (iteration != 0)
+    residueOld = residue;
+    size_t n = V->n_rows;
+    size_t m = V->n_cols;
+    double sum = 0;
+    size_t count = 0;
+    for(size_t i = 0;i < n;i++)
     {
-      residueOld = residue;
-      residue = fabs(normOld - norm);
-      residue /= normOld;
+      for(size_t j = 0;j < m;j++)
+      {
+        double temp = 0;
+        if((temp = (*V)(i,j)) != 0)
+        {
+          temp = (temp - WH(i, j));
+          temp = temp * temp;
+          sum += temp;
+          count++;
+        }
+      }
     }
-
-    normOld = norm;
+    residue = sum / count;
+    residue = sqrt(residue);
 
     iteration++;
   }
@@ -66,15 +73,16 @@ class SimpleToleranceTermination
   double tolerance;
   size_t maxIterations;
 
+  const MatType* V;
+
   size_t iteration;
   double residueOld;
   double residue;
   double normOld;
-
-  size_t nm;
 }; // class SimpleToleranceTermination
 
 }; // namespace amf
 }; // namespace mlpack
 
 #endif // _MLPACK_METHODS_AMF_SIMPLE_TOLERANCE_TERMINATION_HPP_INCLUDED
+
--- src/mlpack/methods/amf/update_rules/svd_batchlearning.hpp
+++ src/mlpack/methods/amf/update_rules/svd_batchlearning.hpp
@@ -1,3 +1,7 @@
+/**
+ * @file simple_residue_termination.hpp
+ * @author Sumedh Ghaisas
+ */
 #ifndef __MLPACK_METHODS_AMF_UPDATE_RULES_SVD_BATCHLEARNING_HPP
 #define __MLPACK_METHODS_AMF_UPDATE_RULES_SVD_BATCHLEARNING_HPP
 
@@ -9,117 +13,128 @@ namespace amf
 {
 class SVDBatchLearning
 {
-public:
-    SVDBatchLearning(double u = 0.000001,
-                     double kw = 0,
-                     double kh = 0,
-                     double momentum = 0.2,
-                     double min = -DBL_MIN,
-                     double max = DBL_MAX)
+ public:
+  SVDBatchLearning(double u = 0.0002,
+                   double kw = 0,
+                   double kh = 0,
+                   double momentum = 0.5,
+                   double min = -DBL_MIN,
+                   double max = DBL_MAX)
         : u(u), kw(kw), kh(kh), min(min), max(max), momentum(momentum)
     {}
 
-    template<typename MatType>
-    void Initialize(const MatType& dataset, const size_t rank)
-    {
-        const size_t n = dataset.n_rows;
-        const size_t m = dataset.n_cols;
+  template<typename MatType>
+  void Initialize(const MatType& dataset, const size_t rank)
+  {
+    const size_t n = dataset.n_rows;
+    const size_t m = dataset.n_cols;
 
-        mW.zeros(n, rank);
-        mH.zeros(rank, m);
+    mW.zeros(n, rank);
+    mH.zeros(rank, m);
+  }
+
+  /**
+   * The update rule for the basis matrix W.
+   * The function takes in all the matrices and only changes the
+   * value of the W matrix.
+   *
+   * @param V Input matrix to be factorized.
+   * @param W Basis matrix to be updated.
+   * @param H Encoding matrix.
+   */
+  template<typename MatType>
+  inline void WUpdate(const MatType& V,
+                      arma::mat& W,
+                      const arma::mat& H)
+  {
+    size_t n = V.n_rows;
+
+    size_t r = W.n_cols;
+
+    mW = momentum * mW;
+
+    arma::mat deltaW(n, r);
+    deltaW.zeros();
+
+    for(typename MatType::const_iterator it = V.begin();it != V.end();it++)
+    {
+      size_t row = it.row();
+      size_t col = it.col();
+      deltaW.row(it.row()) += (*it - arma::dot(W.row(row), H.col(col))) * 
+                                                    arma::trans(H.col(col));
     }
 
-    /**
-    * The update rule for the basis matrix W.
-    * The function takes in all the matrices and only changes the
-    * value of the W matrix.
-    *
-    * @param V Input matrix to be factorized.
-    * @param W Basis matrix to be updated.
-    * @param H Encoding matrix.
-    */
-    template<typename MatType>
-    inline void WUpdate(const MatType& V,
-                               arma::mat& W,
-                               const arma::mat& H)
+    if(kw != 0) for(size_t i = 0; i < n; i++)
     {
-        size_t n = V.n_rows;
-        size_t m = V.n_cols;
-
-        size_t r = W.n_cols;
-
-        mW = momentum * mW;
-
-        arma::mat deltaW(n, r);
-        deltaW.zeros();
-
-        for(size_t i = 0; i < n; i++)
-        {
-            for(size_t j = 0; j < m; j++)
-                if(V(i,j) != 0) deltaW.row(i) += (V(i,j) - Predict(W.row(i), H.col(j))) * arma::trans(H.col(j));
-            deltaW.row(i) -= kw * W.row(i);
-        }
-
-        mW += u * deltaW;
-        W += mW;
+      deltaW.row(i) -= kw * W.row(i);
     }
 
-    /**
-    * The update rule for the encoding matrix H.
-    * The function takes in all the matrices and only changes the
-    * value of the H matrix.
-    *
-    * @param V Input matrix to be factorized.
-    * @param W Basis matrix.
-    * @param H Encoding matrix to be updated.
-    */
-    template<typename MatType>
-    inline void HUpdate(const MatType& V,
-                               const arma::mat& W,
-                               arma::mat& H)
+    mW += u * deltaW;
+    W += mW;
+  }
+
+  /**
+   * The update rule for the encoding matrix H.
+   * The function takes in all the matrices and only changes the
+   * value of the H matrix.
+   *
+   * @param V Input matrix to be factorized.
+   * @param W Basis matrix.
+   * @param H Encoding matrix to be updated.
+   */
+  template<typename MatType>
+  inline void HUpdate(const MatType& V,
+                      const arma::mat& W,
+                      arma::mat& H)
+  {
+    size_t m = V.n_cols;
+
+    size_t r = W.n_cols;
+
+    mH = momentum * mH;
+
+    arma::mat deltaH(r, m);
+    deltaH.zeros();
+
+    for(typename MatType::const_iterator it = V.begin();it != V.end();it++)
     {
-        size_t n = V.n_rows;
-        size_t m = V.n_cols;
-
-        size_t r = W.n_cols;
-
-        mH = momentum * mH;
-
-        arma::mat deltaH(r, m);
-        deltaH.zeros();
-
-        for(size_t j = 0; j < m; j++)
-        {
-            for(size_t i = 0; i < n; i++)
-                if(V(i,j) != 0) deltaH.col(j) += (V(i,j) - Predict(W.row(i), H.col(j))) * arma::trans(W.row(i));
-            deltaH.col(j) -= kh * H.col(j);
-        }
-
-        mH += u*deltaH;
-        H += mH;
-    }
-private:
-
-    double Predict(const arma::mat& wi, const arma::mat& hj) const
-    {
-        arma::mat temp = (wi * hj);
-        double out = temp(0,0);
-        return out;
+      size_t row = it.row();
+      size_t col = it.col();
+      deltaH.col(col) += (*it - arma::dot(W.row(row), H.col(col))) * 
+                                                    arma::trans(W.row(row));
     }
 
-    double u;
-    double kw;
-    double kh;
-    double min;
-    double max;
-    double momentum;
+    if(kh != 0) for(size_t j = 0; j < m; j++)
+    {
+      deltaH.col(j) -= kh * H.col(j);
+    }
 
-    arma::mat mW;
-    arma::mat mH;
+    mH += u*deltaH;
+    H += mH;
+  }
+  
+ private:
+  double Predict(const arma::mat& wi, const arma::mat& hj) const
+  {
+    arma::mat temp = (wi * hj);
+    double out = temp(0,0);
+    return out;
+  }
+
+  double u;
+  double kw;
+  double kh;
+  double min;
+  double max;
+  double momentum;
+
+  arma::mat mW;
+  arma::mat mH;
 };
 } // namespace amf
 } // namespace mlpack
 
 
 #endif
+
 
