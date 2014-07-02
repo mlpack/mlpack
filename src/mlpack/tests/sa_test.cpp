e5649066b03d8622b8a4b18bc88677172ee273fd
Ryan Curtin <ryan@ratml.org>
2014-07-02 19:21:33 +0000

    Remove now-unnecessary include.

--- src/mlpack/tests/sa_test.cpp
+++ src/mlpack/tests/sa_test.cpp
@@ -7,7 +7,6 @@
 #include <mlpack/core.hpp>
 #include <mlpack/core/optimizers/sa/sa.hpp>
 #include <mlpack/core/optimizers/sa/exponential_schedule.hpp>
-#include <mlpack/core/optimizers/sa/laplace_distribution.hpp>
 #include <mlpack/core/optimizers/lbfgs/test_functions.hpp>
 
 #include <mlpack/core/metrics/ip_metric.hpp>
@@ -31,10 +30,9 @@ BOOST_AUTO_TEST_CASE(GeneralizedRosenbrockTest)
   size_t dim = 50;
   GeneralizedRosenbrockFunction f(dim);
 
-  LaplaceDistribution moveDist;
   ExponentialSchedule schedule(1e-5);
-  SA<GeneralizedRosenbrockFunction, LaplaceDistribution, ExponentialSchedule> 
-      sa(f, moveDist, schedule, 1000.,1000, 100, 1e-9, 3, 20, 0.3, 0.3, 10000000);
+  SA<GeneralizedRosenbrockFunction, ExponentialSchedule>
+      sa(f, schedule, 1000.,1000, 100, 1e-9, 3, 20, 0.3, 0.3, 10000000);
   arma::mat coordinates = f.GetInitialPoint();
   double result = sa.Optimize(coordinates);
 
