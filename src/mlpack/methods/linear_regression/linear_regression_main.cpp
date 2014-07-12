c13aa9397233eefa3bbc1f2e8aec9821f86d9d03
Marcus Edel <marcus.edel@fu-berlin.de>
2014-07-12 17:09:45 +0000

    Don't save a rowvector into a colvector. Use the build in option of the save function to transpose the vector.

--- src/mlpack/methods/linear_regression/linear_regression_main.cpp
+++ src/mlpack/methods/linear_regression/linear_regression_main.cpp
@@ -170,7 +170,6 @@ int main(int argc, char* argv[])
     Timer::Stop("prediction");
 
     // Save predictions.
-    predictions = arma::trans(predictions);
-    data::Save(outputPredictions, predictions, true);
+    data::Save(outputPredictions, predictions, true, false);
   }
 }
