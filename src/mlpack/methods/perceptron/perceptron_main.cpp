fdbcc58c681f501fcd320f750a9fc4335af59503
Udit Saxena <saxena.udit@gmail.com>
2014-07-16 15:12:44 +0000

    Minor changes to the macros in the *main.cpp files.

--- src/mlpack/methods/perceptron/perceptron_main.cpp
+++ src/mlpack/methods/perceptron/perceptron_main.cpp
@@ -2,7 +2,7 @@
  * @file: perceptron_main.cpp
  * @author: Udit Saxena
  *
- *
+ * Main executable for the Perceptron.
  */
 
 #include <mlpack/core.hpp>
@@ -13,15 +13,36 @@ using namespace mlpack::perceptron;
 using namespace std;
 using namespace arma;
 
-PROGRAM_INFO("Perceptron","");
+PROGRAM_INFO("Perceptron",
+    "This program implements a perceptron, which is a single level "
+    "Neural Network. The perceptron makes its predictions based on "
+    "a linear predictor function combining a set of weights with the feature "
+    "vector.\n"
+    "The perceptron learning rule is able to converge, given enough iterations "
+    "using the --iterations (-i) parameter, if the data supplied is "
+    "linearly separable. "
+    "\n"
+    "The Perceptron is parameterized by a matrix of weight vectors which "
+    "denotes the numerical weights of the Neural Network."
+    "\n"
+    "This program allows training of a perceptron, and then application of "
+    "the learned perceptron to a test dataset.  To train a perceptron, "
+    "a training dataset must be passed to --train_file (-t).  Labels can either "
+    "be present as the last dimension of the training dataset, or given "
+    "explicitly with the --labels_file (-l) parameter."
+    "\n"
+    "A test file is given through the --test_file (-T) parameter.  The "
+    "predicted labels for the test set will be stored in the file specified by "
+    "the --output_file (-o) parameter."
+    );
 
-//necessary parameters
-PARAM_STRING_REQ("train_file", "A file containing the training set.", "tr");
+// Necessary parameters
+PARAM_STRING_REQ("train_file", "A file containing the training set.", "t");
 PARAM_STRING_REQ("labels_file", "A file containing labels for the training set.",
   "l");
-PARAM_STRING_REQ("test_file", "A file containing the test set.", "te");
+PARAM_STRING_REQ("test_file", "A file containing the test set.", "T");
 
-//optional parameters.
+// Optional parameters.
 PARAM_STRING("output", "The file in which the predicted labels for the test set"
     " will be written.", "o", "output.csv");
 PARAM_INT("iterations","The maximum number of iterations the perceptron is "
@@ -38,8 +59,25 @@ int main(int argc, char *argv[])
   const string labelsFilename = CLI::GetParam<string>("labels_file");
   // Load labels.
   mat labelsIn;
-  data::Load(labelsFilename, labelsIn, true);
 
+  if (CLI::HasParam("labels_file"))
+  {
+    const string labelsFilename = CLI::GetParam<string>("labels_file");
+    // Load labels.
+    data::Load(labelsFilename, labelsIn, true);
+
+    // Do the labels need to be transposed?
+    if (labelsIn.n_rows == 1)
+      labelsIn = labelsIn.t();
+  }
+  else
+  {
+    // Extract the labels as the last
+    Log::Info << "Using the last dimension of training set as labels." << endl;
+
+    labelsIn = trainingData.row(trainingData.n_rows - 1).t();
+    trainingData.shed_row(trainingData.n_rows - 1);
+  }
   // helpers for normalizing the labels
   Col<size_t> labels;
   vec mappings;
