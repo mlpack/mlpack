2a739560b2970c0938cb868209957bc948c5fc4e
Marcus Edel <marcus.edel@fu-berlin.de>
2014-07-18 21:56:30 +0000

    Add nystroem method for kernel approximation.

---
+++ src/mlpack/methods/nystroem_method/random_selection.hpp
@@ -0,0 +1,40 @@
+/**
+ * @file random_selection.hpp
+ * @author Ryan Curtin
+ *
+ * Randomly select some points (with replacement) to use for the Nystroem
+ * method.  Replacement is suboptimal, but for rank << number of points, this is
+ * unlikely.
+ */
+#ifndef __MLPACK_METHODS_NYSTROEM_METHOD_RANDOM_SELECTION_HPP
+#define __MLPACK_METHODS_NYSTROEM_METHOD_RANDOM_SELECTION_HPP
+
+#include <mlpack/core.hpp>
+
+namespace mlpack {
+namespace kernel {
+
+class RandomSelection
+{
+ public:
+  /**
+   * Randomly select the specified number of points in the dataset.
+   *
+   * @param data Dataset to sample from.
+   * @param m Number of points to select.
+   * @return Indices of selected points from the dataset.
+   */
+  const static arma::vec Select(const arma::mat& data, const size_t m)
+  {
+    arma::vec selectedPoints(m);
+    for (size_t i = 0; i < m; ++i)
+      selectedPoints(i) = math::RandInt(0, data.n_cols);
+
+    return selectedPoints;
+  }
+};
+
+}; // namespace kernel
+}; // namespace mlpack
+
+#endif
