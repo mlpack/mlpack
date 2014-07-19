5ff5b38efbd322251ff3f8d5fd6302fc12f53b00
Marcus Edel <marcus.edel@fu-berlin.de>
2014-07-19 20:37:35 +0000

    Outsource the matrix multipilcation.

--- src/mlpack/methods/nystroem_method/nystroem_method_impl.hpp
+++ src/mlpack/methods/nystroem_method/nystroem_method_impl.hpp
@@ -81,7 +81,8 @@ void NystroemMethod<KernelType, PointSelectionPolicy>::Apply(arma::mat& output)
   arma::svd(U, s, V, miniKernel);
 
   // Construct the output matrix.
-  output = semiKernel * (U * arma::diagmat(1.0 / sqrt(s))) * V;
+  arma::mat normalization = (U * arma::diagmat(1.0 / sqrt(s)));
+  output = semiKernel * normalization * V;
 }
 
 }; // namespace kernel
