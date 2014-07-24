5ce1ac2bd8216fccb504ad56ca9f92f013f5d1ef
Udit Saxena <saxena.udit@gmail.com>
2014-07-24 20:34:38 +0000

    Adaboost test improved and now works. Improved adaboost.

--- src/mlpack/methods/adaboost/adaboost.hpp
+++ src/mlpack/methods/adaboost/adaboost.hpp
@@ -19,14 +19,45 @@ template <typename MatType = arma::mat, typename WeakLearner =
 class Adaboost 
 {
 public:
-  arma::Row<size_t> finalHypothesis;
-
+  /**
+   *  Constructor. Currently runs the Adaboost.mh algorithm
+   *  
+   *  @param data Input data
+   *  @param labels Corresponding labels
+   *  @param iterations Number of boosting rounds 
+   *  @param other Weak Learner, which has been initialized already
+   */
   Adaboost(const MatType& data, const arma::Row<size_t>& labels,
-           int iterations, size_t classes, const WeakLearner& other);
+           int iterations, const WeakLearner& other);
 
+  /**
+   *  This function helps in building a classification Matrix which is of 
+   *  form: 
+   *  -1 if l is not the correct label
+   *  1 if l is the correct label
+   *
+   *  @param t The classification matrix to be built
+   *  @param l The labels from which the classification matrix is to be built.
+   */
   void buildClassificationMatrix(arma::mat& t, const arma::Row<size_t>& l);
 
+  /**
+   *  This function helps in building the Weight Distribution matrix
+   *  which is updated during every iteration. It calculates the 
+   *  "difficulty" in classifying a point by adding the weights for all 
+   *  instances, using D.
+   *  
+   *  @param D The 2 Dimensional weight matrix from which the weights are
+   *            to be calculated.
+   *  @param weights The output weight vector.
+   */
   void buildWeightMatrix(const arma::mat& D, arma::rowvec& weights);
+
+  // Stores the final classification of the Labels.
+  arma::Row<size_t> finalHypothesis;
+
+  // To check for the bound for the hammingLoss.
+  double ztAccumulator;
 
 }; // class Adaboost
 
