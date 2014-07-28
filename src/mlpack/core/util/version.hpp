aa3ffce50570b3485a20f436535197bd5263bc67
Ryan Curtin <ryan@ratml.org>
2014-07-28 14:29:34 +0000

    Update version number.

--- src/mlpack/core/util/version.hpp
+++ src/mlpack/core/util/version.hpp
@@ -13,7 +13,7 @@
 // higher number than the most recent release.
 #define __MLPACK_VERSION_MAJOR 1
 #define __MLPACK_VERSION_MINOR 0
-#define __MLPACK_VERSION_PATCH 8
+#define __MLPACK_VERSION_PATCH 9
 
 // The name of the version (for use by --version).
 namespace mlpack {
