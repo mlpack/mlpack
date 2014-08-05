3c8b5d3e5d24e3f01e0958187b5596d720ee7cc9
michaelfox99 <michaelfox99@gmail.com>
2014-08-05 13:34:32 +0000

    Added legacy file conversion

--- src/mlpack/methods/hmm/hmm_util.hpp
+++ src/mlpack/methods/hmm/hmm_util.hpp
@@ -1,9 +1,9 @@
 /**
  * @file hmm_util.hpp
  * @author Ryan Curtin
+ * @author Michael Fox
  *
- * Save/load utilities for HMMs.  This should be eventually merged into the HMM
- * class itself.
+ * Deprecated Save/load utilities for HMMs. See HMM::Save, HMM::Load.  
  */
 #ifndef __MLPACK_METHODS_HMM_HMM_UTIL_HPP
 #define __MLPACK_METHODS_HMM_HMM_UTIL_HPP
@@ -14,8 +14,7 @@ namespace mlpack {
 namespace hmm {
 
 /**
- * Save an HMM to file.  This only works for GMMs, DiscreteDistributions, and
- * GaussianDistributions.
+ * Save an HMM to file (deprecated).
  *
  * @tparam Distribution Distribution type of HMM.
  * @param sr SaveRestoreUtility to use.
@@ -24,14 +23,22 @@ template<typename Distribution>
 void SaveHMM(const HMM<Distribution>& hmm, util::SaveRestoreUtility& sr);
 
 /**
- * Load an HMM from file.  This only works for GMMs, DiscreteDistributions, and
- * GaussianDistributions.
+ * Load an HMM from file (deprecated).
  *
  * @tparam Distribution Distribution type of HMM.
  * @param sr SaveRestoreUtility to use.
  */
 template<typename Distribution>
 void LoadHMM(HMM<Distribution>& hmm, util::SaveRestoreUtility& sr);
+
+/**
+ * Converter for HMMs saved using older MLPACK versions.
+ *
+ * @tparam Distribution Distribution type of HMM.
+ * @param sr SaveRestoreUtility to use.
+ */
+template<typename Distribution>
+void ConvertHMM(HMM<Distribution>& hmm, const util::SaveRestoreUtility& sr);
 
 }; // namespace hmm
 }; // namespace mlpack
