6594b028a9ffe2348d04f8f91f2e6bb9009bd2a5
sumedhghaisas <sumedhghaisas@9d5b8971-822b-0410-80eb-d18c1038ef23>
2014-08-06 21:38:07 +0000

    * minor changes

--- src/mlpack/methods/cf/plain_svd.hpp
+++ src/mlpack/methods/cf/plain_svd.hpp
@@ -1,3 +1,9 @@
+/**
+ * @file plain_svd.hpp
+ * @author Sumedh Ghaisas
+ *
+ * Wrapper class for Armadillo's SVD.
+ */
 #ifndef __MLPACK_METHODS_PLAIN_SVD_HPP
 #define __MLPACK_METHODS_PLAIN_SVD_HPP
 
@@ -8,23 +14,50 @@ namespace mlpack
 namespace svd
 {
 
+/**
+ * This class acts as a wrapper class for Armadillo's SVD implementation to be 
+ * used by Collaborative Filteraing module.
+ *
+ * @see CF
+ */
 class PlainSVD
 {
  public:
+  // empty constructor
   PlainSVD() {};
 
+  /**
+   * Factorizer function which takes SVD of the given matrix and returns the 
+   * frobenius norm of error.
+   *
+   * @param V input matrix
+   * @param W first unitary matrix
+   * @param sigma eigenvalue matrix
+   * @param H second unitary matrix
+   *
+   * @note V = W * sigma * arma::trans(H)
+   */
   double Apply(const arma::mat& V,
                arma::mat& W,
                arma::mat& sigma,
                arma::mat& H) const;
-
+  /**
+   * Factorizer function which computes SVD and returns matrices as required by 
+   * CF module.
+   * 
+   * @param V input matrix
+   * @param W first unitary matrix
+   * @param H second unitary matrix
+   *
+   * @note V = W * H
+   */
   double Apply(const arma::mat& V,
                size_t r,
                arma::mat& W,
                arma::mat& H) const;
-};
+}; // class PlainSVD
 
-};
-};
+}; // namespace svd
+}; // namespace mlpack
 
 #endif
