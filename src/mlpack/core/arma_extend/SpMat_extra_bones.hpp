e083f952110d370fa173bba90ac4517afee4e67c
Ryan Curtin <ryan@ratml.org>
2014-08-30 04:06:08 +0000

    Backport another sparse matrix constructor for softmax regression.

--- src/mlpack/core/arma_extend/SpMat_extra_bones.hpp
+++ src/mlpack/core/arma_extend/SpMat_extra_bones.hpp
@@ -5,17 +5,29 @@
  * Add a batch constructor for SpMat, if the version is older than 3.810.0.
  */
 #if ARMA_VERSION_MAJOR == 3 && ARMA_VERSION_MINOR < 810
-template<typename T1, typename T2> inline SpMat(
+template<typename T1, typename T2>
+inline SpMat(
     const Base<uword, T1>& locations,
     const Base<eT, T2>& values,
     const bool sort_locations = true);
 
-template<typename T1, typename T2> inline SpMat(
+template<typename T1, typename T2>
+inline SpMat(
     const Base<uword, T1>& locations,
     const Base<eT, T2>& values,
     const uword n_rows,
     const uword n_cols,
     const bool sort_locations = true);
+#endif
+
+#if ARMA_VERSION_MAJOR == 3 && ARMA_VERSION_MINOR < 920
+template<typename T1, typename T2, typename T3>
+inline SpMat(
+    const Base<uword, T1>& rowind,
+    const Base<uword, T2>& colptr,
+    const Base<eT, T3>& values,
+    const uword n_rows,
+    const uword n_cols);
 #endif
 
 /*
