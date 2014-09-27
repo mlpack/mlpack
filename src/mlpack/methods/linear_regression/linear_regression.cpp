10b7e1b4a5d4f99c05fa14458260f432dd596ef0
Ryan Curtin <ryan@ratml.org>
2014-09-27 16:35:43 +0000

    Spacing and line length fixes.

--- src/mlpack/methods/linear_regression/linear_regression.cpp
+++ src/mlpack/methods/linear_regression/linear_regression.cpp
@@ -96,10 +96,13 @@ void LinearRegression::Predict(const arma::mat& points, arma::vec& predictions)
 {
   if (intercept)
   {
-    // We want to be sure we have the correct number of dimensions in the dataset.
-    Log::Assert(points.n_rows == parameters.n_rows-1);
-    // Get the predictions, but this ignores the intercept value (parameters[0]).
-    predictions = arma::trans(arma::trans(parameters.subvec(1, parameters.n_elem - 1)) * points);
+    // We want to be sure we have the correct number of dimensions in the
+    // dataset.
+    Log::Assert(points.n_rows == parameters.n_rows - 1);
+    // Get the predictions, but this ignores the intercept value
+    // (parameters[0]).
+    predictions = arma::trans(arma::trans(parameters.subvec(1,
+        parameters.n_elem - 1)) * points);
     // Now add the intercept.
     predictions += parameters(0);
   }
