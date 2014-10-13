521bc9e51a90bb9dd33f4a5097126bddc28b16d3
Ryan Curtin <ryan@ratml.org>
2014-10-13 21:07:28 +0000

    A test for the DTNN k-means algorithm.

--- src/mlpack/tests/kmeans_test.cpp
+++ src/mlpack/tests/kmeans_test.cpp
@@ -10,6 +10,7 @@
 #include <mlpack/methods/kmeans/elkan_kmeans.hpp>
 #include <mlpack/methods/kmeans/hamerly_kmeans.hpp>
 #include <mlpack/methods/kmeans/pelleg_moore_kmeans.hpp>
+#include <mlpack/methods/kmeans/dtnn_kmeans.hpp>
 
 #include <boost/test/unit_test.hpp>
 #include "old_boost_test_definitions.hpp"
@@ -587,6 +588,38 @@ BOOST_AUTO_TEST_CASE(PellegMooreTest)
 
     for (size_t i = 0; i < centroids.n_elem; ++i)
       BOOST_REQUIRE_CLOSE(naiveCentroids[i], pmCentroids[i], 1e-5);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(DTNNTest)
+{
+  const size_t trials = 5;
+
+  for (size_t t = 0; t < trials; ++t)
+  {
+    arma::mat dataset(10, 1000);
+    dataset.randu();
+
+    const size_t k = 5 * (t + 1);
+    arma::mat centroids(10, k);
+    centroids.randu();
+
+    arma::mat naiveCentroids(centroids);
+    KMeans<> km;
+    arma::Col<size_t> assignments;
+    km.Cluster(dataset, k, assignments, naiveCentroids, false, true);
+
+    KMeans<metric::EuclideanDistance, RandomPartition, MaxVarianceNewCluster,
+        DefaultDTNNKMeans> dtnn;
+    arma::Col<size_t> dtnnAssignments;
+    arma::mat dtnnCentroids(centroids);
+    dtnn.Cluster(dataset, k, dtnnAssignments, dtnnCentroids, false, true);
+
+    for (size_t i = 0; i < dataset.n_cols; ++i)
+      BOOST_REQUIRE_EQUAL(assignments[i], dtnnAssignments[i]);
+
+    for (size_t i = 0; i < centroids.n_cols; ++i)
+      BOOST_REQUIRE_CLOSE(naiveCentroids[i], dtnnCentroids[i], 1e-5);
   }
 }
 
