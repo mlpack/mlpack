b35e8c2f6b8f96376b84aab85f2ded67fb0a831d
Ryan Curtin <ryan@ratml.org>
2014-11-05 19:36:49 +0000

    This is an experimental method that I am working on.  Right now it is not very useful as I have not implemented all of the pruning strategies that I intend to.

---
+++ src/mlpack/methods/kmeans/dual_tree_kmeans.hpp
@@ -0,0 +1,71 @@
+/**
+ * @file dual_tree_kmeans.hpp
+ * @author Ryan Curtin
+ *
+ * A dual-tree algorithm for a single k-means iteration.
+ */
+#ifndef __MLPACK_METHODS_KMEANS_DUAL_TREE_KMEANS_HPP
+#define __MLPACK_METHODS_KMEANS_DUAL_TREE_KMEANS_HPP
+
+#include "dual_tree_kmeans_statistic.hpp"
+
+namespace mlpack {
+namespace kmeans {
+
+template<
+    typename MetricType,
+    typename MatType,
+    typename TreeType = tree::BinarySpaceTree<bound::HRectBound<2>,
+        DualTreeKMeansStatistic>
+>
+class DualTreeKMeans
+{
+ public:
+  DualTreeKMeans(const MatType& dataset, MetricType& metric);
+
+  ~DualTreeKMeans();
+
+  double Iterate(const arma::mat& centroids,
+                 arma::mat& newCentroids,
+                 arma::Col<size_t>& counts);
+
+  //! Return the number of distance calculations.
+  size_t DistanceCalculations() const { return distanceCalculations; }
+  //! Modify the number of distance calculations.
+  size_t& DistanceCalculations() { return distanceCalculations; }
+
+ private:
+  //! The original dataset reference.
+  const MatType& datasetOrig;
+  //! The dataset we are using.
+  const MatType& dataset;
+  //! A copy of the dataset, if necessary.
+  MatType datasetCopy;
+  //! The metric.
+  MetricType metric;
+
+  //! The tree built on the points.
+  TreeType* tree;
+
+  arma::vec clusterDistances;
+  arma::Col<size_t> assignments;
+  arma::vec distances;
+  arma::Col<size_t> distanceIteration;
+
+  //! The current iteration.
+  size_t iteration;
+
+  //! Track distance calculations.
+  size_t distanceCalculations;
+};
+
+template<typename MetricType, typename MatType>
+using DefaultDualTreeKMeans = DualTreeKMeans<MetricType, MatType>;
+
+} // namespace kmeans
+} // namespace mlpack
+
+// Include implementation.
+#include "dual_tree_kmeans_impl.hpp"
+
+#endif
---
+++ src/mlpack/methods/kmeans/dual_tree_kmeans_rules.hpp
@@ -0,0 +1,80 @@
+/**
+ * @file dual_tree_kmeans_rules.hpp
+ * @author Ryan Curtin
+ *
+ * A set of tree traversal rules for dual-tree k-means clustering.
+ */
+#ifndef __MLPACK_METHODS_KMEANS_DUAL_TREE_KMEANS_RULES_HPP
+#define __MLPACK_METHODS_KMEANS_DUAL_TREE_KMEANS_RULES_HPP
+
+namespace mlpack {
+namespace kmeans {
+
+template<typename MetricType, typename TreeType>
+class DualTreeKMeansRules
+{
+ public:
+  DualTreeKMeansRules(const typename TreeType::Mat& dataset,
+                      const arma::mat& centroids,
+                      arma::mat& newCentroids,
+                      arma::Col<size_t>& counts,
+                      const std::vector<size_t>& mappings,
+                      const size_t iteration,
+                      const arma::vec& clusterDistances,
+                      arma::vec& distances,
+                      arma::Col<size_t>& assignments,
+                      arma::Col<size_t>& distanceIteration,
+                      MetricType& metric);
+
+  double BaseCase(const size_t queryIndex, const size_t referenceIndex);
+
+  double Score(const size_t queryIndex, TreeType& referenceNode);
+
+  double Score(TreeType& queryNode, TreeType& referenceNode);
+
+  double Rescore(const size_t queryIndex,
+                 TreeType& referenceNode,
+                 const double oldScore) const;
+
+  double Rescore(TreeType& queryNode,
+                 TreeType& referenceNode,
+                 const double oldScore) const;
+
+  size_t DistanceCalculations() const { return distanceCalculations; }
+  size_t& DistanceCalculations() { return distanceCalculations; }
+
+  typedef neighbor::NeighborSearchTraversalInfo<TreeType> TraversalInfoType;
+
+  const TraversalInfoType& TraversalInfo() const { return traversalInfo; }
+  TraversalInfoType& TraversalInfo() { return traversalInfo; }
+
+ private:
+  const typename TreeType::Mat& dataset;
+  const arma::mat& centroids;
+  arma::mat& newCentroids;
+  arma::Col<size_t>& counts;
+  const std::vector<size_t>& mappings;
+  const size_t iteration;
+  const arma::vec& clusterDistances;
+  arma::vec& distances;
+  arma::Col<size_t>& assignments;
+  arma::Col<size_t> visited;
+  arma::Col<size_t>& distanceIteration;
+  MetricType& metric;
+
+  size_t distanceCalculations;
+
+  TraversalInfoType traversalInfo;
+
+  size_t IterationUpdate(TreeType& referenceNode) const;
+
+  bool IsDescendantOf(const TreeType& potentialParent, const TreeType&
+      potentialChild) const;
+};
+
+} // namespace kmeans
+} // namespace mlpack
+
+#include "dual_tree_kmeans_rules_impl.hpp"
+
+#endif
