fed7bf245c0acd3061fb0703a4f041869146274e
Ryan Curtin <ryan@ratml.org>
2014-11-18 22:55:49 +0000

    Issue a runtime error if the user is using Armadillo 4.300.x through 4.400.x and tries to load or save HDF5 files, since that is a bug in Armadillo.

--- src/mlpack/core/data/load_impl.hpp
+++ src/mlpack/core/data/load_impl.hpp
@@ -142,6 +142,22 @@ bool Load(const std::string& filename,
 #ifdef ARMA_USE_HDF5
     loadType = arma::hdf5_binary;
     stringType = "HDF5 data";
+  #if ARMA_VERSION_MAJOR == 4 && \
+      (ARMA_VERSION_MINOR >= 300 && ARMA_VERSION_MINOR <= 400)
+    Timer::Stop("loading_data");
+    if (fatal)
+      Log::Fatal << "Attempted to load '" << filename << "' as HDF5 data, but "
+          << "Armadillo 4.300.0 through Armadillo 4.400.1 are known to have "
+          << "bugs and one of these versions is in use.  Load failed."
+          << std::endl;
+    else
+      Log::Warn << "Attempted to load '" << filename << "' as HDF5 data, but "
+          << "Armadillo 4.300.0 through Armadillo 4.400.1 are known to have "
+          << "bugs and one of these versions is in use.  Load failed."
+          << std::endl;
+
+    return false;
+  #endif
 #else
     Timer::Stop("loading_data");
     if (fatal)
@@ -184,6 +200,8 @@ bool Load(const std::string& filename,
   else
     Log::Info << "Loading '" << filename << "' as " << stringType << ".  "
         << std::flush;
+
+  Log::Debug << "load type " << loadType << "\n";
 
   const bool success = matrix.load(stream, loadType);
 
