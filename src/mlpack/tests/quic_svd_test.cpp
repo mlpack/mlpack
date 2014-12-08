4d6ed991ea256beebde9916c99745b3575a93e54
Ryan Curtin <ryan@ratml.org>
2014-12-08 18:46:52 +0000

    Refactor test to handle random failures.

--- src/mlpack/tests/quic_svd_test.cpp
+++ src/mlpack/tests/quic_svd_test.cpp
@@ -40,18 +40,31 @@ BOOST_AUTO_TEST_CASE(QUICSVDReconstructionError)
   arma::mat dataset;
   data::Load("test_data_3_1000.csv", dataset);
 
-  // Obtain the SVD using default parameters.
-  arma::mat u, v, sigma;
-  QUIC_SVD quicsvd(dataset, u, v, sigma);
-  
-  // Reconstruct the matrix using the SVD.
-  arma::mat reconstruct;
-  reconstruct = u * sigma * v.t();
-  
-  // The relative reconstruction error should be small.
-  double relativeError = arma::norm(dataset - reconstruct, "frob") /
-                         arma::norm(dataset, "frob");                         
-  BOOST_REQUIRE_SMALL(relativeError, 1e-5);
+  // Since QUIC-SVD may have random errors, run up to three trials to get a good
+  // results.
+  size_t successes = 0;
+  size_t trial = 0;
+
+  while (trial < 3 && successes < 1)
+  {
+    // Obtain the SVD using default parameters.
+    arma::mat u, v, sigma;
+    QUIC_SVD quicsvd(dataset, u, v, sigma);
+
+    // Reconstruct the matrix using the SVD.
+    arma::mat reconstruct;
+    reconstruct = u * sigma * v.t();
+
+    // The relative reconstruction error should be small.
+    double relativeError = arma::norm(dataset - reconstruct, "frob") /
+                           arma::norm(dataset, "frob");
+    if (relativeError < 1e-5)
+      ++successes;
+
+    ++trial;
+  }
+
+  BOOST_REQUIRE_GE(successes, 1);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
