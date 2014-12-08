72e90adb7a5c187dd7725e8cdc462f040f956a7d
Ryan Curtin <ryan@ratml.org>
2014-12-08 05:00:29 +0000

    Backport row_col_iterator.

---
+++ src/mlpack/core/arma_extend/Mat_extra_bones.hpp
@@ -0,0 +1,141 @@
+/*
+ * Add row_col_iterator and row_col_const_iterator to arma::Mat.
+ */
+
+/*
+ * row_col_iterator for Mat<eT>. This iterator can return row and column index
+ * of the entry its pointing too. The functionality of this iterator is similar
+ * to sparse matrix iterators.
+ */
+
+#if ARMA_VERSION_MAJOR < 4 || \
+    (ARMA_VERSION_MAJOR == 4 && ARMA_VERSION_MINOR < 349)
+class row_col_iterator;
+
+class const_row_col_iterator
+  {
+  public:
+
+  // empty constructor
+  inline const_row_col_iterator();
+  // constructs const iterator from other iterators
+  inline const_row_col_iterator(const row_col_iterator& it);
+  inline const_row_col_iterator(const const_row_iterator& it);
+  inline const_row_col_iterator(const row_iterator& it);
+  // constructs iterator with given row and col index
+  inline const_row_col_iterator(const Mat<eT>& in_M, const uword row = 0, const uword col = 0);
+
+  /*
+   * Returns the value of the current position.
+   */
+  inline arma_hot const eT& operator*() const { return *current_pos; }
+
+  /*
+   * Increment and decrement operators for this iterator.
+   */
+  inline arma_hot const_row_col_iterator& operator++();
+  inline arma_hot const_row_col_iterator  operator++(int);
+  inline arma_hot const_row_col_iterator& operator--();
+  inline arma_hot const_row_col_iterator  operator--(int);
+
+  /*
+   * Comparison operator with itself and other relevant iterators.
+   */
+  inline arma_hot bool operator==(const const_row_col_iterator& rhs) const;
+  inline arma_hot bool operator!=(const const_row_col_iterator& rhs) const;
+  inline arma_hot bool operator==(const row_col_iterator& rhs) const;
+  inline arma_hot bool operator!=(const row_col_iterator& rhs) const;
+  inline arma_hot bool operator==(const const_iterator& rhs) const;
+  inline arma_hot bool operator!=(const const_iterator& rhs) const;
+  inline arma_hot bool operator==(const iterator& rhs) const;
+  inline arma_hot bool operator!=(const iterator& rhs) const;
+  inline arma_hot bool operator==(const const_row_iterator& rhs) const;
+  inline arma_hot bool operator!=(const const_row_iterator& rhs) const;
+  inline arma_hot bool operator==(const row_iterator& rhs) const;
+  inline arma_hot bool operator!=(const row_iterator& rhs) const;
+
+  arma_inline uword row() const { return internal_row; }
+  arma_inline uword col() const { return internal_col; }
+
+  // So that we satisfy the STL iterator types.
+  typedef std::bidirectional_iterator_tag iterator_category;
+  typedef eT                              value_type;
+  typedef uword                           difference_type; // not certain on this one
+  typedef const eT*                       pointer;
+  typedef const eT&                       reference;
+
+  arma_aligned const Mat<eT>* M;
+
+  arma_aligned const eT* current_pos;
+  arma_aligned       uword  internal_col;
+  arma_aligned       uword  internal_row;
+  };
+
+class row_col_iterator
+  {
+  public:
+
+  // empty constructor
+  inline row_col_iterator();
+  // constructs const iterator from other iterators
+  inline row_col_iterator(const row_iterator& it);
+  // constructs iterator with given row and col index
+  inline row_col_iterator(Mat<eT>& in_M, const uword row = 0, const uword col = 0);
+
+  /*
+   * Returns the value of the current position.
+   */
+  inline arma_hot eT& operator*() const { return *current_pos; }
+
+  /*
+   * Increment and decrement operators for this iterator.
+   */
+  inline arma_hot row_col_iterator& operator++();
+  inline arma_hot row_col_iterator  operator++(int);
+  inline arma_hot row_col_iterator& operator--();
+  inline arma_hot row_col_iterator  operator--(int);
+
+  /*
+   * Comparison operator with itself and other relevant iterators.
+   */
+  inline arma_hot bool operator==(const const_row_col_iterator& rhs) const;
+  inline arma_hot bool operator!=(const const_row_col_iterator& rhs) const;
+  inline arma_hot bool operator==(const row_col_iterator& rhs) const;
+  inline arma_hot bool operator!=(const row_col_iterator& rhs) const;
+  inline arma_hot bool operator==(const const_iterator& rhs) const;
+  inline arma_hot bool operator!=(const const_iterator& rhs) const;
+  inline arma_hot bool operator==(const iterator& rhs) const;
+  inline arma_hot bool operator!=(const iterator& rhs) const;
+  inline arma_hot bool operator==(const const_row_iterator& rhs) const;
+  inline arma_hot bool operator!=(const const_row_iterator& rhs) const;
+  inline arma_hot bool operator==(const row_iterator& rhs) const;
+  inline arma_hot bool operator!=(const row_iterator& rhs) const;
+
+  arma_inline uword row() const { return internal_row; }
+  arma_inline uword col() const { return internal_col; }
+
+  // So that we satisfy the STL iterator types.
+  typedef std::bidirectional_iterator_tag iterator_category;
+  typedef eT                              value_type;
+  typedef uword                           difference_type; // not certain on this one
+  typedef const eT*                       pointer;
+  typedef const eT&                       reference;
+
+  arma_aligned const Mat<eT>* M;
+
+  arma_aligned       eT* current_pos;
+  arma_aligned       uword  internal_col;
+  arma_aligned       uword  internal_row;
+  };
+
+/*
+ * Extra functions for Mat<eT>
+ */
+// begin for iterator row_col_iterator
+inline const_row_col_iterator begin_row_col() const;
+inline row_col_iterator begin_row_col();
+
+// end for iterator row_col_iterator
+inline const_row_col_iterator end_row_col() const;
+inline row_col_iterator end_row_col();
+#endif
--- src/mlpack/core/arma_extend/SpMat_extra_bones.hpp
+++ src/mlpack/core/arma_extend/SpMat_extra_bones.hpp
@@ -5,15 +5,45 @@
  * Add a batch constructor for SpMat, if the version is older than 3.810.0.
  */
 #if ARMA_VERSION_MAJOR == 3 && ARMA_VERSION_MINOR < 810
-template<typename T1, typename T2> inline SpMat(
+template<typename T1, typename T2>
+inline SpMat(
     const Base<uword, T1>& locations,
     const Base<eT, T2>& values,
     const bool sort_locations = true);
 
-template<typename T1, typename T2> inline SpMat(
+template<typename T1, typename T2>
+inline SpMat(
     const Base<uword, T1>& locations,
     const Base<eT, T2>& values,
     const uword n_rows,
     const uword n_cols,
     const bool sort_locations = true);
+#endif
+
+#if ARMA_VERSION_MAJOR == 3 && ARMA_VERSION_MINOR < 920
+template<typename T1, typename T2, typename T3>
+inline SpMat(
+    const Base<uword, T1>& rowind,
+    const Base<uword, T2>& colptr,
+    const Base<eT, T3>& values,
+    const uword n_rows,
+    const uword n_cols);
+#endif
+
+/*
+ * Extra functions for SpMat<eT>
+ * Adding definition of row_col_iterator to generalize with Mat<eT>::row_col_iterator
+ */
+#if ARMA_VERSION_MAJOR < 4 || \
+    (ARMA_VERSION_MAJOR == 4 && ARMA_VERSION_MINOR < 349)
+typedef iterator row_col_iterator;
+typedef const_iterator const_row_col_iterator;
+
+// begin for iterator row_col_iterator
+inline const_row_col_iterator begin_row_col() const;
+inline row_col_iterator begin_row_col();
+
+// end for iterator row_col_iterator
+inline const_row_col_iterator end_row_col() const;
+inline row_col_iterator end_row_col();
 #endif
