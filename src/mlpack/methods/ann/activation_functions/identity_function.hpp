7dad33e2c2fd30e87e125ef826ae7ab652df8cf6
Marcus Edel <marcus.edel@fu-berlin.de>
2014-12-31 21:49:45 +0100

    Add implementation of identity activation function.

---
+++ src/mlpack/methods/ann/activation_functions/identity_function.hpp
@@ -0,0 +1,76 @@
+/**
+ * @file identity_function.hpp
+ * @author Marcus Edel
+ *
+ * Definition and implementation of the identity function.
+ */
+#ifndef __MLPACK_METHODS_ANN_ACTIVATION_FUNCTIONS_IDENTITY_FUNCTION_HPP
+#define __MLPACK_METHODS_ANN_ACTIVATION_FUNCTIONS_IDENTITY_FUNCTION_HPP
+
+#include <mlpack/core.hpp>
+
+namespace mlpack {
+namespace ann /** Artificial Neural Network. */ {
+
+/**
+ * The identity function, defined by
+ *
+ * @f[
+ * f(x) &=& x \\
+ * f'(x) &=& 1
+ * @f]
+ */
+class IdentityFunction
+{
+ public:
+  /**
+   * Computes the identity function.
+   *
+   * @param x Input data.
+   * @return f(x).
+   */
+  static double fn(const double x)
+  {
+    return x;
+  }
+
+  /**
+   * Computes the identity function.
+   *
+   * @param x Input data.
+   * @param y The resulting output activation.
+   */
+  template<typename InputVecType, typename OutputVecType>
+  static void fn(const InputVecType& x, OutputVecType& y)
+  {
+    y = x;
+  }
+
+  /**
+   * Computes the first derivative of the identity function.
+   *
+   * @param x Input data.
+   * @return f'(x)
+   */
+  static double deriv(const double /* unused */)
+  {
+    return 1.0;
+  }
+
+  /**
+   * Computes the first derivatives of the identity function.
+   *
+   * @param y Input activations.
+   * @param x The resulting derivatives.
+   */
+  template<typename InputVecType, typename OutputVecType>
+  static void deriv(const InputVecType& y, OutputVecType& x)
+  {
+    x.ones(y.n_elem);
+  }
+}; // class IdentityFunction
+
+}; // namespace ann
+}; // namespace mlpack
+
+#endif
