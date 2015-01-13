2d23dbe88c954a6b756a94110e7130f7cb38c3eb
Ryan Curtin <ryan@ratml.org>
2015-01-13 11:16:41 -0500

    Asleep at the wheel?

--- src/mlpack/methods/matrix_completion/matrix_completion_impl.hpp
+++ src/mlpack/methods/matrix_completion/matrix_completion_impl.hpp
@@ -99,7 +99,7 @@ size_t MatrixCompletion::DefaultRank(const size_t m,
   //   Pablo Moscato, Michael Norman, and Gabor Pataki.
   //   Math Oper. Res., 23(2). 1998.
   const size_t mpn = m + n;
-  const float r = 0.5 + sqrt(0.25 + 2 * p);
+  float r = 0.5 + sqrt(0.25 + 2 * p);
   if (ceil(r) > mpn)
     r = mpn; // An upper bound on the dimension.
   return ceil(r);
