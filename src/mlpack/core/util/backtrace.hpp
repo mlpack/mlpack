cbccdc78493a4db6cf6ebbf6442674bab8d9c22c
Ryan Curtin <ryan@ratml.org>
2015-03-24 18:38:05 -0400

    Include default unconfigured backtrace.hpp file.

---
+++ src/mlpack/core/util/backtrace.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file backtrace.hpp
+ *
+ * Default configuration file to include backtrace() functionality.  Here we
+ * assume it is not available.
+ */
+#ifndef __MLPACK_CORE_UTIL_BACKTRACE_HPP
+#define __MLPACK_CORE_UTIL_BACKTRACE_HPP
+
+#define Backtrace_FOUND 0
+
+#endif
