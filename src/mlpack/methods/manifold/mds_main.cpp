7f40487702f4394a39cba95e58d8fcb1709a69c4
Dhawal Arora <d.p.arora1@gmail.com>
2015-04-08 05:21:23 +0530

    fixed some syntax errors

--- src/mlpack/methods/manifold/mds_main.cpp
+++ src/mlpack/methods/manifold/mds_main.cpp
@@ -12,9 +12,10 @@ using namespace mlpack::manifold;
 using namespace arma;
 
 // Document program.
-PROGRAM_INFO("Multi Dimensional Scaling", "Multidimensional Scaling is a classical approach that maps the original high dimensional
- space to a lower dimensional space, but does so in an attempt to preserve pairwise dis-
- tances between all the data points in both the dimensions.");
+PROGRAM_INFO("Multi Dimensional Scaling", "Multidimensional Scaling is a classical approach"
+ 	     "that maps the original high dimensional space to a lower dimensional space,"
+             "but does so in an attempt to preserve pairwise distances"
+             "between all the data points in both the dimensions.");
 
 // Parameters for program.
 PARAM_STRING_REQ("input", "Input dataset to perform MDS on.", "i");
@@ -23,26 +24,27 @@ PARAM_INT("dimensionality", "Desired dimensionality of output dataset." , "d", 2
 
 
 int main(int argc,char **argv){
-	// Parse commandline.
-  	CLI::ParseCommandLine(argc, argv);
+    
+    // Parse commandline.
+    CLI::ParseCommandLine(argc, argv);
 	
-	// Load input dataset.
-	string inputFile = CLI::GetParam<string>("input");
-  	arma::mat dataset;
+    // Load input dataset.
+    string inputFile = CLI::GetParam<string>("input");
+    arma::mat dataset;
   	
-  	Timer::Start("Load dataset");
-  	data::Load(inputFile, dataset);
-  	dataset=trans(dataset);
-  	Timer::Stop("Load dataset");
+    Timer::Start("Load dataset");
+    data::Load(inputFile, dataset);
+    dataset=trans(dataset);
+    Timer::Stop("Load dataset");
 
-  	size_t dimensions = CLI::GetParam<int>("dimensionality")
+    size_t dimensions = CLI::GetParam<int>("dimensionality");
 
     if (dimensions >= dataset.n_rows){
       	Log::Warn << "For cases of dimensionality greater than the number of observations,"
       	"the result for dimensionality same as number of observations will be computed" << std::endl;
     }
   	
-  	//Perform MDS
+    //Perform MDS
     Timer::Start("Perform MDS");
     MDS m(dataset,dimensions,"euclidean","metric");
     dataset=m.transformedData();
@@ -50,10 +52,11 @@ int main(int argc,char **argv){
 
     Timer::Start("Save output");
     // Now save the results.
-  	string outputFile = CLI::GetParam<string>("output");
-  	data::Save(outputFile, dataset);
-  	Timer::Stop("Save output");
+    string outputFile = CLI::GetParam<string>("output");
+    dataset=trans(dataset);
+    data::Save(outputFile, dataset);
+    Timer::Stop("Save output");
 
 
-	return 0;
+    return 0;
 }
