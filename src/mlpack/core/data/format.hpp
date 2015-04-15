666e7fe7f17c94089cb13d39ba1b6dca21883a1d
ryan <ryan@ratml.org>
2015-04-15 11:57:41 -0400

    Add list of formats we can load.

---
+++ src/mlpack/core/data/format.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file formats.hpp
+ * @author Ryan Curtin
+ *
+ * Define the formats that can be used by mlpack's Load() and Save() mechanisms
+ * via boost::serialization.
+ */
+#ifndef __MLPACK_CORE_DATA_FORMATS_HPP
+#define __MLPACK_CORE_DATA_FORMATS_HPP
+
+namespace mlpack {
+namespace data {
+
+//! Define the formats we can read through boost::serialization.
+enum format
+{
+  autodetect,
+  text,
+  xml,
+  binary
+};
+
+} // namespace data
+} // namespace mlpack
+
+#endif
