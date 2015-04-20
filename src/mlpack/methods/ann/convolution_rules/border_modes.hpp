003ff8917587b2a47a6a192eb2b9b3b717143ca9
Marcus Edel <marcus.edel@fu-berlin.de>
2015-04-20 20:16:00 +0200

    Add border modes (valid convolution, full convolution).

---
+++ src/mlpack/methods/ann/convolution_rules/border_modes.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file border_modes.hpp
+ * @author Marcus Edel
+ *
+ * This file provides the border modes that can be used to compute different
+ * convolutions.
+ */
+#ifndef __MLPACK_METHOS_ANN_CONVOLUTION_RULES_BORDER_MODES_HPP
+#define __MLPACK_METHOS_ANN_CONVOLUTION_RULES_BORDER_MODES_HPP
+
+namespace mlpack {
+namespace ann {
+
+/*
+ * The FullConvolution class represents the full two-dimensional convolution.
+ */
+class FullConvolution { /* Nothing to do here */ };
+
+/*
+ * The ValidConvolution represents only those parts of the convolution that are
+ * computed without the zero-padded edges.
+ */
+class ValidConvolution { /* Nothing to do here */ };
+
+}; // namespace ann
+}; // namespace mlpack
+
+#endif
