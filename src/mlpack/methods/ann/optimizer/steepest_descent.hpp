f84079f191adef76a60e66ee8a27b2846571b9a2
Marcus Edel <marcus.edel@fu-berlin.de>
2015-05-02 21:53:37 +0200

    Clean the steepest descent optimizer class and add tensor support.

--- src/mlpack/methods/ann/optimizer/steepest_descent.hpp
+++ src/mlpack/methods/ann/optimizer/steepest_descent.hpp
@@ -2,8 +2,10 @@
  * @file steepest_descent.hpp
  * @author Marcus Edel
  *
- * Intialization rule for the neural networks. This simple initialization is
- * performed by assigning a random matrix to the weight matrix.
+ * Implmentation of the steepest descent optimizer. The method of steepest
+ * descent, also called the gradient descent method, is used to find the
+ * nearest local minimum of a function which the assumtion that the gradient of
+ * the function can be computed.
  */
 #ifndef __MLPACK_METHOS_ANN_OPTIMIZER_STEEPEST_DESCENT_HPP
 #define __MLPACK_METHOS_ANN_OPTIMIZER_STEEPEST_DESCENT_HPP
@@ -14,20 +16,32 @@ namespace mlpack {
 namespace ann /** Artificial Neural Network. */ {
 
 /**
- * This class is used to initialize randomly the weight matrix.
+ * This class is used to update the weights using steepest descent.
  *
- * @tparam MatType Type of matrix (should be arma::mat or arma::spmat).
+ * @tparam DataType Type of input data (should be arma::mat,
+ * arma::spmat or arma::cube).
  */
-template<typename MatType = arma::mat, typename VecType = arma::colvec>
+template<typename DataType = arma::mat>
 class SteepestDescent
 {
  public:
-  /**
-   * Initialize the random initialization rule with the given lower bound and
-   * upper bound.
+  /*
+   * Construct the optimizer object, which will be used to update the weights.
    *
-   * @param lowerBound The number used as lower bound.
-   * @param upperBound The number used as upper bound.
+   * @param lr The value used as learning rate (Default: 1).
+   */
+  SteepestDescent(const double lr = 1) : lr(lr), mom(0)
+  {
+    // Nothing to do here.
+  }
+
+  /**
+   * Construct the optimizer object, which will be used to update the weights.
+   *
+   * @param cols The number of cols to initilize the momentum matrix.
+   * @param rows The number of rows to initilize the momentum matrix.
+   * @param lr The value used as learning rate (Default: 1).
+   * @param mom The value used as momentum (Default: 0.1).
    */
   SteepestDescent(const size_t cols,
                   const size_t rows,
@@ -36,11 +50,38 @@ class SteepestDescent
       lr(lr), mom(mom)
   {
     if (mom > 0)
-      momWeights = arma::zeros<MatType>(rows, cols);
+      momWeights = arma::zeros<DataType>(rows, cols);
   }
 
-  void UpdateWeights(MatType& weights,
-                     const MatType& gradient,
+  /**
+   * Construct the optimizer object, which will be used to update the weights.
+   *
+   * @param cols The number of cols used to initilize the momentum matrix.
+   * @param rows The number of rows used to initilize the momentum matrix.
+   * @param slices The number of slices used to initilize the momentum matrix.
+   * @param lr The value used as learning rate (Default: 1).
+   * @param mom The value used as momentum (Default: 0.1).
+   */
+  SteepestDescent(const size_t cols,
+                  const size_t rows,
+                  const size_t slices,
+                  const double lr = 1,
+                  const double mom = 0.1) :
+      lr(lr), mom(mom)
+  {
+    if (mom > 0)
+      momWeights = arma::zeros<DataType>(rows, cols, slices);
+  }
+
+  /*
+   * Update the specified weights using steepest descent.
+   *
+   * @param weights The weights that should be updated.
+   * @param gradient The gradient used to update the weights.
+   */
+  template<typename WeightType, typename GradientType>
+  void UpdateWeights(WeightType& weights,
+                     const GradientType& gradient,
                      const double /* unused */)
   {
     if (mom > 0)
@@ -53,16 +94,15 @@ class SteepestDescent
       weights -= lr * gradient;
   }
 
-
  private:
-  //! The number used as learning rate.
+  //! The value used as learning rate.
   const double lr;
 
-  //! The number used as momentum.
+  //! The value used as momentum.
   const double mom;
 
-  //! weight momentum
-  MatType momWeights;
+  //! Momentum matrix.
+  DataType momWeights;
 }; // class SteepestDescent
 
 }; // namespace ann
