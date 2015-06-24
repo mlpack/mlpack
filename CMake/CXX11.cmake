15c86b861642ee25deb1659a81b50b894ef707e2
ryan <ryan@ratml.org>
2015-06-24 16:59:17 -0400

    Allow the Intel compiler too... not that it works. "internal error: null pointer"

--- CMake/CXX11.cmake
+++ CMake/CXX11.cmake
@@ -28,7 +28,8 @@ macro(check_for_cxx11_compiler _VAR)
     set(${_VAR})
     if((MSVC AND (MSVC10 OR MSVC11 OR MSVC12)) OR
        (CMAKE_COMPILER_IS_GNUCXX AND NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 4.6) OR
-       (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 3.1))
+       (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 3.1) OR
+       (CMAKE_CXX_COMPILER_ID STREQUAL "Intel" AND NOT ${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 12.0))
         set(${_VAR} 1)
         message(STATUS "Checking for C++11 compiler - available")
     else()
@@ -39,7 +40,9 @@ endmacro()
 # Sets the appropriate flag to enable C++11 support
 macro(enable_cxx11)
     if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
     endif()
 endmacro()
 
