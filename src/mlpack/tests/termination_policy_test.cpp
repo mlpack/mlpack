651e22447c406242349cfcd5c7865156a529be2d
Ryan Curtin <ryan@ratml.org>
2015-07-07 16:03:55 +0000

    Add tests for MaxIterationTermination.

---
+++ src/mlpack/tests/termination_policy_test.cpp
@@ -0,0 +1,61 @@
+/**
+ * @file termination_policy_test.cpp
+ * @author Ryan Curtin
+ *
+ * Tests for AMF termination policies.
+ */
+#include <mlpack/core.hpp>
+#include <mlpack/methods/amf/amf.hpp>
+#include <mlpack/methods/amf/termination_policies/max_iteration_termination.hpp>
+#include <mlpack/methods/amf/update_rules/nmf_mult_div.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include "old_boost_test_definitions.hpp"
+
+BOOST_AUTO_TEST_SUITE(TerminationPolicyTest);
+
+using namespace std;
+using namespace arma;
+using namespace mlpack;
+using namespace mlpack::amf;
+
+/**
+ * Simple test -- make sure termination happens after the right number of
+ * iterations.
+ */
+BOOST_AUTO_TEST_CASE(MaxIterationTerminationTest)
+{
+  MaxIterationTermination mit(500);
+
+  arma::mat x; // Just an argument to pass.
+  for (size_t i = 0; i < 499; ++i)
+    BOOST_REQUIRE_EQUAL(mit.IsConverged(x, x), false);
+
+  // Should keep returning true once maximum iterations are reached.
+  BOOST_REQUIRE_EQUAL(mit.IsConverged(x, x), true);
+  BOOST_REQUIRE_EQUAL(mit.Iteration(), 500);
+  BOOST_REQUIRE_EQUAL(mit.IsConverged(x, x), true);
+  BOOST_REQUIRE_EQUAL(mit.IsConverged(x, x), true);
+}
+
+/**
+ * Make sure that AMF properly terminates.
+ */
+BOOST_AUTO_TEST_CASE(AMFMaxIterationTerminationTest)
+{
+  mat w = randu<mat>(20, 12);
+  mat h = randu<mat>(12, 20);
+  mat v = w * h;
+  size_t r = 12;
+
+  // Custom tighter tolerance.
+  MaxIterationTermination mit(10); // Only 10 iterations.
+  AMF<MaxIterationTermination,
+      RandomInitialization,
+      NMFMultiplicativeDivergenceUpdate> nmf(mit);
+  nmf.Apply(v, r, w, h);
+
+  BOOST_REQUIRE_EQUAL(nmf.TerminationPolicy().Iteration(), 10);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
