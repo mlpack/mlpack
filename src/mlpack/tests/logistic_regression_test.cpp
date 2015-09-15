ebfe0c0b9d6125e429d0631766b2357e66574a7b
Ryan Curtin <ryan@ratml.org>
2015-09-15 01:01:14 +0000

    Wow, this was an incredibly stupid bug.

--- src/mlpack/tests/logistic_regression_test.cpp
+++ src/mlpack/tests/logistic_regression_test.cpp
@@ -577,7 +577,7 @@ BOOST_AUTO_TEST_CASE(LogisticRegressionLBFGSGaussianTest)
     data.col(i) = g1.Random();
     responses[i] = 0;
   }
-  for (size_t i = 501; i < 1000; ++i)
+  for (size_t i = 500; i < 1000; ++i)
   {
     data.col(i) = g2.Random();
     responses[i] = 1;
