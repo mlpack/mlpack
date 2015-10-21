f22e2d552f807fea5b12c607cba6994eaaca2097
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-10-21 09:00:55 +0800

    first commit

---
+++ src/mlpack/methods/sparse_autoencoder/activation_functions/logistic_function.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file logistic_function.hpp
+ * @author Marcus Edel
+ *
+ * Definition and implementation of the logistic function.
+ */
+#ifndef __MLPACK_METHODS_NN_ACTIVATION_FUNCTIONS_LOGISTIC_FUNCTION_HPP
+#define __MLPACK_METHODS_NN_ACTIVATION_FUNCTIONS_LOGISTIC_FUNCTION_HPP
+
+#include <mlpack/core.hpp>
+#include <mlpack/methods/ann/activation_functions/logistic_function.hpp>
+
+namespace mlpack {
+namespace nn /** Neural Network. */ {
+	
+using LogisticFunction = mlpack::ann::LogisticFunction;
+
+}; // namespace nn
+}; // namespace mlpack
+
+#endif
