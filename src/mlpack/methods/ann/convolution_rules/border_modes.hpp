69b969be75362bf93c222f9cd3da77d4f6694352
marcus <marcus.edel@fu-berlin.de>
2015-11-14 17:18:53 +0100

    Fix the include guards to avoid the problem ofdouble inclusion.

--- src/mlpack/methods/ann/convolution_rules/border_modes.hpp
+++ src/mlpack/methods/ann/convolution_rules/border_modes.hpp
@@ -5,8 +5,8 @@
  * This file provides the border modes that can be used to compute different
  * convolutions.
  */
-#ifndef __MLPACK_METHOS_ANN_CONVOLUTION_RULES_BORDER_MODES_HPP
-#define __MLPACK_METHOS_ANN_CONVOLUTION_RULES_BORDER_MODES_HPP
+#ifndef __MLPACK_METHODS_ANN_CONVOLUTION_RULES_BORDER_MODES_HPP
+#define __MLPACK_METHODS_ANN_CONVOLUTION_RULES_BORDER_MODES_HPP
 
 namespace mlpack {
 namespace ann {
