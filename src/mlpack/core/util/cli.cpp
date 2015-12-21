3ae72c567b7db7ed2b834bd2ec21ac44c48607dd
ryan <ryan@ratml.org>
2015-12-21 12:39:24 -0500

    Don't print "Required options:" unless we need to.
    The same for "Options:", but I don't think there are any mlpack programs that
    have only required options.

--- src/mlpack/core/util/cli.cpp
+++ src/mlpack/core/util/cli.cpp
@@ -602,10 +602,7 @@ void CLI::PrintHelp(const std::string& param)
 
   for (size_t pass = 0; pass < 2; ++pass)
   {
-    if (pass == 0)
-      std::cout << "Required options:" << std::endl << std::endl;
-    else
-      std::cout << "Options: " << std::endl << std::endl;
+    bool printedHeader = false;
 
     // Print out the descriptions of everything else.
     for (iter = gmap.begin(); iter != gmap.end(); ++iter)
@@ -628,6 +625,15 @@ void CLI::PrintHelp(const std::string& param)
         continue; // Don't print this one.
       if ((pass == 1) && required)
         continue; // Don't print this one.
+
+      if (!printedHeader)
+      {
+        printedHeader = true;
+        if (pass == 0)
+          std::cout << "Required options:" << std::endl << std::endl;
+        else
+          std::cout << "Options: " << std::endl << std::endl;
+      }
 
       if (pass == 1) // Append default value to description.
       {
