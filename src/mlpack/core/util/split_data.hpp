e99632f750912a714063505135f56e2c4f1da259
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-02-24 17:10:00 +0800

    split train test support arma cube

--- src/mlpack/core/util/split_data.hpp
+++ src/mlpack/core/util/split_data.hpp
@@ -12,8 +12,58 @@
 namespace mlpack {
 namespace util {
 
+namespace details{
+
+template<typename T>
+inline
+arma::Mat<T> createData(arma::Mat<T> const &input,
+                        size_t dataSize)
+{
+  return arma::Mat<T>(input.n_rows, dataSize);
+}
+
+template<typename T>
+inline
+arma::Cube<T> createData(arma::Cube<T> const &input,
+                         size_t dataSize)
+{
+  return arma::Cube<T>(input.n_rows, input.n_cols, dataSize);
+}
+
+template<typename T>
+inline
+void extractData(arma::Mat<T> const &input, arma::Mat<T> &output,
+                 size_t inputIndex, size_t outputIndex)
+{
+  output.col(outputIndex) = input.col(inputIndex);
+}
+
+template<typename T>
+inline
+void extractData(arma::Cube<T> const &input, arma::Cube<T> &output,
+                 size_t inputIndex, size_t outputIndex)
+{
+  output.slice(outputIndex) = input.slice(inputIndex);
+}
+
+template<typename T>
+inline
+size_t extractSize(arma::Mat<T> const &input)
+{
+  return input.n_cols;
+}
+
+template<typename T>
+inline
+size_t extractSize(arma::Cube<T> const &input)
+{
+  return input.n_slices;
+}
+
+}
+
 /**
- *Split training data and test data, please define 
+ *Split training data and test data, please define
  *ARMA_USE_CXX11 to enable move of c++11
  *@param input input data want to split
  *@param label input label want to split
@@ -27,38 +77,39 @@ namespace util {
  *@endcode
  */
 template<typename T>
-std::tuple<arma::Mat<T>, arma::Mat<T>,
+std::tuple<T, T,
 arma::Row<size_t>, arma::Row<size_t>>
-TrainTestSplit(arma::Mat<T> const &input,
+TrainTestSplit(T const &input,
                arma::Row<size_t> const &label,
                double testRatio,
                unsigned int seed = 0)
 {
   size_t const testSize =
-      static_cast<size_t>(input.n_cols * testRatio);
-  size_t const trainSize = input.n_cols - testSize;
+      static_cast<size_t>(details::extractSize(input) * testRatio);
+  size_t const trainSize = details::extractSize(input) - testSize;
 
-  arma::Mat<T> trainData(input.n_rows, trainSize);
-  arma::Mat<T> testData(input.n_rows, testSize);
+  T trainData = details::createData(input, trainSize);
+  T testData = details::createData(input, testSize);
   arma::Row<size_t> trainLabel(trainSize);
   arma::Row<size_t> testLabel(testSize);
 
-  std::vector<size_t> permutation(input.n_cols);
+  std::vector<size_t> permutation(details::extractSize(input));
   std::iota(std::begin(permutation), std::end(permutation), 0);
 
   std::mt19937 gen(seed);
   std::shuffle(std::begin(permutation), std::end(permutation), gen);
 
-  for(size_t i = 0; i != trainData.n_cols; ++i)
+  for(size_t i = 0; i != trainSize; ++i)
   {
-     trainData.col(i) = input.col(permutation[i]);
-     trainLabel(i) = label(permutation[i]);
+    details::extractData(input, trainData, permutation[i], i);
+    trainLabel(i) = label(permutation[i]);
   }
 
-  for(size_t i = 0; i != testData.n_cols; ++i)
+  for(size_t i = 0; i != testSize; ++i)
   {
-     testData.col(i) = input.col(permutation[i + trainSize]);
-     testLabel(i) = label(permutation[i + trainSize]);
+    details::extractData(input, testData,
+                         permutation[i + trainSize], i);
+    testLabel(i) = label(permutation[i + trainSize]);
   }
 
   return std::make_tuple(trainData, testData, trainLabel, testLabel);
