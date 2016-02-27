07833dd4046e9c9fdc8897bc07271b1a4c87e445
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-02-28 00:46:49 +0800

    first commit

---
+++ src/mlpack/core/util/tuple_serialize.hpp
@@ -0,0 +1,36 @@
+#ifndef MLPACK_CORE_SERIALIZE_TUPLE
+#define MLPACK_CORE_SERIALIZE_TUPLE
+
+#include <mlpack/core.hpp>
+
+#include <string>
+
+namespace boost {
+namespace serialization {  
+ 
+  template<
+      size_t I, 
+      size_t Max,
+      typename Archive, 
+      typename... Args
+  >
+  typename std::enable_if<I < Max, void>::type
+  Serialize(Archive& ar, std::tuple<Args...>& t, const unsigned int /* version */)
+  {
+    ar & data::CreateNVP(std::get<I>(t), "tuple" + std::to_string(I));
+	Serialize<I+1, Max>(ar, t, 0);
+  }
+  
+  template<
+      size_t I, 
+      size_t Max,
+      typename Archive, 
+      typename... Args
+  >
+  typename std::enable_if<I == Max, void>::type 
+  Serialize(Archive&, std::tuple<Args...>&, const unsigned int /* version */)
+  {    
+  }       
+}
+}
+#endif
