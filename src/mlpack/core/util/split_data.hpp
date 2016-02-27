2fb7ee4610f82c15d64e0d6fc5bdcce118ffd727
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-02-27 11:30:45 +0800

    support multiple slice

--- src/mlpack/core/util/split_data.hpp
+++ src/mlpack/core/util/split_data.hpp
@@ -25,6 +25,7 @@ public:
    * @param slice indicate how many slice(depth) per image,
    * this parameter only work on arma::Cube<T>
    * @param seed seed of the random device
+   * @warning slice should not less than 1
    */
   TrainTestSplit(double testRatio,
                  size_t slice = 1,
@@ -32,7 +33,11 @@ public:
     seed(seed),
     slice(slice),
     testRatio(testRatio)
-  {}
+  {
+    if(slice < 1){
+      throw std::out_of_range("The range of slice should not less than 1");
+    }
+  }
 
   /**
    *Split training data and test data, please define
@@ -132,6 +137,9 @@ public:
   }
   void Slice(size_t value)
   {
+    if(value < 1){
+      throw std::out_of_range("The range of slice should not less than 1");
+    }
     slice = value;
   }
 
@@ -157,7 +165,10 @@ private:
   void ExtractData(arma::Cube<T> const &input, arma::Cube<T> &output,
                    size_t inputIndex, size_t outputIndex) const
   {
-    output.slice(outputIndex) = input.slice(inputIndex);
+    outputIndex *= slice;
+    inputIndex *= slice;
+    output.slices(outputIndex, outputIndex + slice - 1) =
+        input.slices(inputIndex, inputIndex + slice - 1);
   }
 
   template<typename T>
@@ -192,59 +203,6 @@ private:
   size_t slice;
   double testRatio;
 };
-
-/**
- *Split training data and test data, please define
- *ARMA_USE_CXX11 to enable move of c++11
- *@param input input data want to split
- *@param label input label want to split
- *@param testRatio the ratio of test data
- *@param seed seed of the random device
- *@code
- *arma::mat trainData = loadData();
- *arma::Row<size_t> label = loadLabel();
- *std::random_device rd;
- *auto trainTest = TrainTestSplit(trainData, label, 0.25, rd());
- *@endcode
- */
-/*template<typename T>
-std::tuple<T, T,
-arma::Row<size_t>, arma::Row<size_t>>
-TrainTestSplit(T const &input,
-               arma::Row<size_t> const &label,
-               double testRatio,
-               unsigned int seed = 0)
-{
-  size_t const testSize =
-      static_cast<size_t>(details::extractSize(input) * testRatio);
-  size_t const trainSize = details::extractSize(input) - testSize;
-
-  T trainData = details::createData(input, trainSize);
-  T testData = details::createData(input, testSize);
-  arma::Row<size_t> trainLabel(trainSize);
-  arma::Row<size_t> testLabel(testSize);
-
-  std::vector<size_t> permutation(details::extractSize(input));
-  std::iota(std::begin(permutation), std::end(permutation), 0);
-
-  std::mt19937 gen(seed);
-  std::shuffle(std::begin(permutation), std::end(permutation), gen);
-
-  for(size_t i = 0; i != trainSize; ++i)
-  {
-    details::extractData(input, trainData, permutation[i], i);
-    trainLabel(i) = label(permutation[i]);
-  }
-
-  for(size_t i = 0; i != testSize; ++i)
-  {
-    details::extractData(input, testData,
-                         permutation[i + trainSize], i);
-    testLabel(i) = label(permutation[i + trainSize]);
-  }
-
-  return std::make_tuple(trainData, testData, trainLabel, testLabel);
-}*/
 
 } // namespace util
 } // namespace mlpack
