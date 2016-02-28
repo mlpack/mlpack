11e039001ceacf0440ea2e728f601a06a763ef93
stereomatchingkiss <stereomatchingkiss@gmail.com>
2016-02-28 09:58:04 +0800

    test multi slice and use new api to do the test

--- src/mlpack/tests/split_data_test.cpp
+++ src/mlpack/tests/split_data_test.cpp
@@ -40,42 +40,81 @@ void compareData(arma::cube const &inputData, arma::cube const &compareData,
     }
 }
 
-BOOST_AUTO_TEST_CASE(SplitDataSplitResultMat)
+void compareData(arma::cube const &inputData, arma::cube const &compareData,
+                 arma::Row<size_t> const &inputLabel, size_t slice)
 {
-    arma::mat trainData(2,10);
-    trainData.randu();
-    arma::Row<size_t> labels(trainData.n_cols);
-    for(size_t i = 0; i != labels.n_cols; ++i){
-        labels(i) = i;
+    for(size_t i = 0; i != inputLabel.size(); ++i){
+        size_t const inputIndex = inputLabel(i)*slice;
+        arma::cube const &lhs = inputData.slices(inputIndex,
+                                                 inputIndex+slice-1);
+        arma::cube const &rhs = compareData.slices(i*slice, i+slice-1);
+        for(size_t j = 0; j != lhs.size(); ++j){
+            BOOST_REQUIRE_CLOSE(lhs(j), rhs(j), 1e-5);
+        }
     }
+}
 
-    auto const value = util::TrainTestSplit(trainData, labels, 0.2);
+BOOST_AUTO_TEST_CASE(SplitDataSplitResultMat)
+{    
+    arma::mat input(2,10);
+    input.randu();
+    using Labels = arma::Row<size_t>;
+    Labels const labels =
+            arma::linspace<Labels>(0, input.n_cols-1,
+                                   input.n_cols);
+
+    util::TrainTestSplit tts(0.2, 1);
+    auto const value = tts.Split(input, labels);
     BOOST_REQUIRE(std::get<0>(value).n_cols == 8);
     BOOST_REQUIRE(std::get<1>(value).n_cols == 2);
     BOOST_REQUIRE(std::get<2>(value).n_cols == 8);
     BOOST_REQUIRE(std::get<3>(value).n_cols == 2);
 
-    compareData(trainData, std::get<0>(value), std::get<2>(value));
-    compareData(trainData, std::get<1>(value), std::get<3>(value));
+    compareData(input, std::get<0>(value), std::get<2>(value));
+    compareData(input, std::get<1>(value), std::get<3>(value));
 }
 
 BOOST_AUTO_TEST_CASE(SplitDataSplitResultCube)
 {
-    arma::cube trainData(2,2,10);
-    trainData.randu();
-    arma::Row<size_t> labels(trainData.n_slices);
-    for(size_t i = 0; i != labels.n_cols; ++i){
-        labels(i) = i;
-    }
+    arma::cube input(2,2,10);
+    input.randu();
+    using Labels = arma::Row<size_t>;
+    Labels const labels =
+            arma::linspace<Labels>(0, input.n_slices-1,
+                                   input.n_slices);
 
-    auto const value = util::TrainTestSplit(trainData, labels, 0.2);
+    util::TrainTestSplit tts(0.2, 1);
+    auto const value = tts.Split(input, labels);
     BOOST_REQUIRE(std::get<0>(value).n_slices == 8);
     BOOST_REQUIRE(std::get<1>(value).n_slices == 2);
     BOOST_REQUIRE(std::get<2>(value).n_cols == 8);
     BOOST_REQUIRE(std::get<3>(value).n_cols == 2);
 
-    compareData(trainData, std::get<0>(value), std::get<2>(value));
-    compareData(trainData, std::get<1>(value), std::get<3>(value));
+    compareData(input, std::get<0>(value), std::get<2>(value));
+    compareData(input, std::get<1>(value), std::get<3>(value));
+}
+
+BOOST_AUTO_TEST_CASE(SplitDataSplitResultCubeMultiSlice)
+{
+    size_t const slice = 3;
+    arma::cube input(2,2,slice*2);
+    input.randu();
+    using Labels = arma::Row<size_t>;
+    Labels const labels =
+            arma::linspace<Labels>(0, input.n_slices/slice-1,
+                                   input.n_slices/slice);
+
+    util::TrainTestSplit tts(0.5, slice);
+    auto const value = tts.Split(input, labels);
+    BOOST_REQUIRE(std::get<0>(value).n_slices == 3);
+    BOOST_REQUIRE(std::get<1>(value).n_slices == 3);
+    BOOST_REQUIRE(std::get<2>(value).n_cols == 1);
+    BOOST_REQUIRE(std::get<3>(value).n_cols == 1);
+
+    compareData(input, std::get<0>(value),
+                std::get<2>(value), slice);
+    compareData(input, std::get<1>(value),
+                std::get<3>(value), slice);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
