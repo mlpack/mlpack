34dbb9446d41f000d7dc28c902f35a9a5df940ca
ranjan1990 <ranjan.rbc@gmail.com>
2016-03-18 23:56:13 +0530

    scd added

---
+++ src/mlpack/core/optimizers/scd/scd.h
@@ -0,0 +1,28 @@
+/**
+ * @file scd.hpp
+ * @author Ranjan Mondal
+ *
+ * Stochastic Coordinate Descent (SGD).
+ *      */
+#ifndef __MLPACK_CORE_OPTIMIZERS_SCD_SCD_HPP
+#define __MLPACK_CORE_OPTIMIZERS_SCD_SCD_HPP
+
+#include <mlpack/core.hpp>
+
+namespace mlpack {
+namespace optimization {
+
+
+  template<typename DecomposableFunctionType>
+  class SCD
+  {
+
+  };
+
+}// namespace optimization
+}// namespace mlpack
+
+#endif
+
+
+
