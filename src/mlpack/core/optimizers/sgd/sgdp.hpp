2a6791c4fc74e6d06fbc9faa161b2128e880c552
ranjan1990 <ranjan.rbc@gmail.com>
2016-03-18 14:01:30 +0530

    change

---
+++ src/mlpack/core/optimizers/sgd/sgdp.hpp
@@ -0,0 +1,85 @@
+/**
+ * @file sgdp.hpp
+ * @author Ranjan Mondal
+ *
+ * Parallel Stochastic Gradient Descent (SGD).
+ */
+#ifndef __MLPACK_CORE_OPTIMIZERS_SGD_SGDP_HPP
+#define __MLPACK_CORE_OPTIMIZERS_SGD_SGDP_HPP
+
+#include <mlpack/core.hpp>
+#include<omp.h>
+#include<vector>
+
+namespace mlpack {
+namespace optimization {
+
+template<typename DecomposableFunctionType>
+class SGD_P
+{
+  public:
+  /**
+  * @param function Function to be optimized (minimized).
+  * @param stepSize Step size for each iteration.
+  * @param maxIterations Maximum number of iterations allowed (0 means no
+  *     limit).
+  * @param tolerance Maximum absolute tolerance to terminate algorithm.
+  *
+  **/
+  SGD_P(DecomposableFunctionType& function,
+      const double stepSize = 0.01,
+      const size_t maxIterations = 100000,
+      const double tolerance = 1e-5);
+
+  /**
+   * Optimize the given function using  parallel stochastic gradient descent.
+   *  The given starting point will be modified to store the finishing point of the
+   * algorithm, and the final objective value is returned.
+   * @param iterate Starting point (will be modified).
+   * @return Objective value of the final point.
+   */
+  double Optimize(arma::mat& iterate);
+
+  //! Get the instantiated function to be optimized.
+  const DecomposableFunctionType& Function() const { return function; }
+  //! Modify the instantiated function.
+  DecomposableFunctionType& Function() { return function; }
+
+  //! Get the step size.
+  double StepSize() const { return stepSize; }
+  //! Modify the step size.
+  double& StepSize() { return stepSize; }
+
+  //! Get the maximum number of iterations (0 indicates no limit).
+  size_t MaxIterations() const { return maxIterations; }
+  //! Modify the maximum number of iterations (0 indicates no limit).
+  size_t& MaxIterations() { return maxIterations; }
+
+  //! Get the tolerance for termination.
+  double Tolerance() const { return tolerance; }
+  //! Modify the tolerance for termination.
+  double& Tolerance() { return tolerance; }
+
+
+ private:
+  //! The instantiated function.
+  DecomposableFunctionType& function;
+
+  //! The step size for each example.
+  double stepSize;
+
+  //! The maximum number of allowed iterations.
+  size_t maxIterations;
+
+  //! The tolerance for termination.
+  double tolerance;
+
+};
+
+} // namespace optimization
+} // namespace mlpack
+
+// Include implementation.
+#include "sgd_impl_P.hpp"
+
+#endif
---
+++ src/mlpack/tests/sgd_test_P.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file sgd_test_P.cpp
+ * @author Ranjan Mondal
+ *
+ * Test file for SGD (stochastic gradient descent).
+ */
+#include <mlpack/core.hpp>
+#include <mlpack/core/optimizers/sgd/sgd_P.hpp>
+#include <mlpack/core/optimizers/lbfgs/test_functions.hpp>
+#include <mlpack/core/optimizers/sgd/test_function.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include "old_boost_test_definitions.hpp"
+
+using namespace std;
+using namespace arma;
+using namespace mlpack;
+using namespace mlpack::optimization;
+using namespace mlpack::optimization::test;
+
+BOOST_AUTO_TEST_SUITE(SGDTest);
+
+BOOST_AUTO_TEST_CASE(SimpleSGDTestFunction)
+{
+  SGDTestFunction f;
+  SGD_P<SGDTestFunction> s(f, 0.0003, 5000000, 1e-9, true);
+
+  arma::mat coordinates = f.GetInitialPoint();
+  double result = s.Optimize(coordinates);
+
+  BOOST_REQUIRE_CLOSE(result, -1.0, 0.05);
+  BOOST_REQUIRE_SMALL(coordinates[0], 1e-3);
+  BOOST_REQUIRE_SMALL(coordinates[1], 1e-7);
+  BOOST_REQUIRE_SMALL(coordinates[2], 1e-7);
+}
+
+BOOST_AUTO_TEST_CASE(GeneralizedRosenbrockTest)
+{
+  // Loop over several variants.
+  for (size_t i = 10; i < 50; i += 5)
+  {
+    // Create the generalized Rosenbrock function.
+    GeneralizedRosenbrockFunction f(i);
+
+    SGD_P<GeneralizedRosenbrockFunction> s(f, 0.001, 50000, 1e-15, true);
+
+    arma::mat coordinates = f.GetInitialPoint();
+    double result = s.Optimize(coordinates);
+
+    BOOST_REQUIRE_SMALL(result, 1e-10);
+    for (size_t j = 0; j < i; ++j)
+      BOOST_REQUIRE_CLOSE(coordinates[j], (double) 1.0, 1e-3);
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
