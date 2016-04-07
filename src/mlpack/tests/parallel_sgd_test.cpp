a981f8322e84ec06a349b80d261639a282f4f7c5
ranjan1990 <ranjan.rbc@gmail.com>
2016-04-07 20:52:54 +0530

    LogisticRegressionPSGDSimpleTest  added

--- src/mlpack/tests/parallel_sgd_test.cpp
+++ src/mlpack/tests/parallel_sgd_test.cpp
@@ -9,7 +9,10 @@
 #include <mlpack/core/optimizers/parallel_sgd/sgdp.hpp>
 #include <mlpack/core/optimizers/lbfgs/test_functions.hpp>
 #include <mlpack/core/optimizers/parallel_sgd/test_function.hpp>
-//#include <mlpack/core/optimizers/sgd/test_function.hpp>
+
+#include <mlpack/core/optimizers/sgd/sgd.hpp>
+#include <mlpack/methods/logistic_regression/logistic_regression.hpp>
+
 
 #include <boost/test/unit_test.hpp>
 #include "old_boost_test_definitions.hpp"
@@ -17,32 +20,36 @@
 using namespace std;
 using namespace arma;
 using namespace mlpack;
+using namespace mlpack::regression;
 using namespace mlpack::optimization;
 using namespace mlpack::optimization::test;
 
+  
 BOOST_AUTO_TEST_SUITE(PSGDTest);
-/*
-BOOST_AUTO_TEST_CASE(SimpleSGDTestFunction)
-{
-  PSGDTestFunction f;
-  ParallelSGD<PSGDTestFunction> s(f, 0.0003,   5000000, 1e-9);
 
-  arma::mat coordinates = f.GetInitialPoint();
-  double result = s.Optimize(coordinates);
-  BOOST_REQUIRE_CLOSE(result, -1.0, 0.05);
-  BOOST_REQUIRE_SMALL(coordinates[0], 1e-3);
-  BOOST_REQUIRE_SMALL(coordinates[1], 1e-7);
-  BOOST_REQUIRE_SMALL(coordinates[2], 1e-7);
+BOOST_AUTO_TEST_CASE(LogisticRegressionPSGDSimpleTest)
+{
+  arma::mat data("1 2 3;""1 2 3;");
+  arma::Row<size_t> responses("1 1 0");
+  
+  LogisticRegressionFunction<> lrf(data, responses, 0.001);
+  ParallelSGD<LogisticRegressionFunction<>> psgd(lrf, 0.005, 500000, 1e-10);
+
+  LogisticRegression<> lr(psgd);
+  arma::vec sigmoids = 1 / (1 + arma::exp(-lr.Parameters()[0] - data.t() * lr.Parameters().subvec(1, lr.Parameters().n_elem - 1)));
+
+    BOOST_REQUIRE_CLOSE(sigmoids[0], 1.0, 7.0);
+    BOOST_REQUIRE_CLOSE(sigmoids[1], 1.0, 14.0);
+    BOOST_REQUIRE_SMALL(sigmoids[2], 0.1);
 }
 
-*/
+
 BOOST_AUTO_TEST_CASE(BoothsFunctionTest)
 {
   BoothsFunction f;
   ParallelSGD<BoothsFunction> s(f, 0.0003, 5000000, 1e-9);
   arma::mat coordinates = f.GetInitialPoint();
   double result = s.Optimize(coordinates);
-
   BOOST_REQUIRE_SMALL(result, 1e-3);
   BOOST_REQUIRE_CLOSE(coordinates[0], (double) 1.0, 0.1);
   BOOST_REQUIRE_CLOSE(coordinates[1], (double) 3.0, 0.1);
@@ -50,24 +57,22 @@ BOOST_AUTO_TEST_CASE(BoothsFunctionTest)
 }
 
 
-
-
 BOOST_AUTO_TEST_CASE(GeneralizedRosenbrockTest)
 {
   // Loop over several variants.
-  for (size_t i = 10; i < 14; i += 5)
+  for (size_t i = 10; i <50; i += 5)
   {
     // Create the generalized Rosenbrock function.
     GeneralizedRosenbrockFunction f(i);
 
-    ParallelSGD<GeneralizedRosenbrockFunction> s(f, 0.001,999999, 1e-15);
+    ParallelSGD<GeneralizedRosenbrockFunction> s(f, 0.001,0, 1e-15);
 
     arma::mat coordinates = f.GetInitialPoint();
     double result = s.Optimize(coordinates);
     BOOST_REQUIRE_SMALL(result, 1e-4);
     for (size_t j = 0; j < i; ++j)
     {
-      BOOST_REQUIRE_CLOSE(coordinates[j], (double) 1.0, 02);
+      BOOST_REQUIRE_CLOSE(coordinates[j], (double) 1.0, 2);
     }
   }
 }
