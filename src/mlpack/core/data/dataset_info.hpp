db78f394f13f0f58df8344ddc91b40bb599762eb
Keon Kim <kwk236@gmail.com>
2016-06-15 09:55:34 +0900

    remove datatype in dataset_info

--- src/mlpack/core/data/dataset_info.hpp
+++ src/mlpack/core/data/dataset_info.hpp
@@ -17,19 +17,6 @@
 
 namespace mlpack {
 namespace data {
-
-/**
- * The Datatype enum specifies the types of data mlpack algorithms can use.
- * The vast majority of mlpack algorithms can only use numeric data (i.e.
- * float/double/etc.), but some algorithms can use categorical data, specified
- * via this Datatype enum and the DatasetMapper class.
- */
-/*enum Datatype : bool [> bool is all the precision we need for two types <]*/
-/*{*/
-  /*numeric = 0,*/
-  /*categorical = 1*/
-/*};*/
-
 /**
  * Auxiliary information for a dataset, including mappings to/from strings and
  * the datatype of each dimension.  DatasetMapper objects are optionally
