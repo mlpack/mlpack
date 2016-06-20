40636498694b74803125b4ba6b545c14f43a87be
Ryan Curtin <ryan@ratml.org>
2016-06-20 09:01:17 -0400

    Update license information for backported Armadillo code.
    Also remove backports that we no longer need.

--- src/mlpack/core/arma_extend/hdf5_misc.hpp
+++ src/mlpack/core/arma_extend/hdf5_misc.hpp
@@ -1,3 +1,14 @@
+// Copyright (C) 2012-2013 National ICT Australia (NICTA)
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// -------------------------------------------------------------------
+//
+// Written by Conrad Sanderson - http://conradsanderson.id.au
+// Written by Ryan Curtin
+// Written by Szabolcs Horvat
+
 // To hack in u64/s64 support to Armadillo when it is not compiled with
 // ARMA_64BIT_WORD.
 namespace hdf5_misc {
