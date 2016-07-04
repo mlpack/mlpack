260a48e3ab93b97586806e10ac52572f1b9486e6
Marcus Edel <marcus.edel@fu-berlin.de>
2016-07-04 14:56:18 +0200

    Update boost test header.

--- src/mlpack/tests/randomized_svd_test.cpp
+++ src/mlpack/tests/randomized_svd_test.cpp
@@ -9,7 +9,7 @@
 #include <mlpack/methods/randomized_svd/randomized_svd.hpp>
 
 #include <boost/test/unit_test.hpp>
-#include "old_boost_test_definitions.hpp"
+#include "test_tools.hpp"
 
 BOOST_AUTO_TEST_SUITE(RandomizedSVDTest);
 
