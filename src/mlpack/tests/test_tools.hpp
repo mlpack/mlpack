d7287ac8851d399a1e94bf1b66ead70800c87cf4
Marcus Edel <marcus.edel@fu-berlin.de>
2016-07-08 00:21:37 +0200

    Fix clang warning when using integer absolute value function 'abs' when argument is of floating point type.

--- src/mlpack/tests/test_tools.hpp
+++ src/mlpack/tests/test_tools.hpp
@@ -37,6 +37,6 @@
 // Require the approximation L to be within a relative error of E respect to the
 // actual value R.
 #define REQUIRE_RELATIVE_ERR( L, R, E ) \
-    BOOST_REQUIRE_LE( abs((R) - (L)), (E) * abs(R))
+    BOOST_REQUIRE_LE( std::abs((R) - (L)), (E) * std::abs(R))
 
 #endif
