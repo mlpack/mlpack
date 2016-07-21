a46270e79991ab712d036bef3d6049a1884ac5e2
Yannis Mentekidis <mentekid@gmail.com>
2016-07-21 13:21:53 +0100

    Correctly removes _BACKPORT

--- src/mlpack/core/boost_backport/policy.hpp
+++ src/mlpack/core/boost_backport/policy.hpp
@@ -3,8 +3,8 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef BOOST_MATH_POLICY_HPP_
-#define BOOST_MATH_POLICY_HPP_
+#ifndef BOOST_MATH_POLICY_HPP
+#define BOOST_MATH_POLICY_HPP
 
 #include <boost/mpl/list.hpp>
 #include <boost/mpl/contains.hpp>
--- src/mlpack/core/boost_backport/trigamma.hpp
+++ src/mlpack/core/boost_backport/trigamma.hpp
@@ -3,8 +3,8 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef BOOST_MATH_SF_TRIGAMMA_HPP_
-#define BOOST_MATH_SF_TRIGAMMA_HPP_
+#ifndef BOOST_MATH_SF_TRIGAMMA_HPP
+#define BOOST_MATH_SF_TRIGAMMA_HPP
 
 #ifdef _MSC_VER
 #pragma once
