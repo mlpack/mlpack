256d77dfc80c49b9136055536d6c58985e02a8de
Ryan Curtin <ryan@ratml.org>
2016-07-27 10:10:09 -0400

    Update to latest version number.

--- doc/guide/build.hpp
+++ doc/guide/build.hpp
@@ -11,14 +11,14 @@ href="http://keon.io/mlpack-on-windows.html">Keon's excellent tutorial</a>.
 
 @section Download latest mlpack build
 Download latest mlpack build from here:
-<a href="http://www.mlpack.org/files/mlpack-2.0.2.tar.gz">mlpack-2.0.2</a>
+<a href="http://www.mlpack.org/files/mlpack-2.0.3.tar.gz">mlpack-2.0.3</a>
 
 @section builddir Creating Build Directory
 
 Once the mlpack source is unpacked, you should create a build directory.
 
 @code
-$ cd mlpack-2.0.2
+$ cd mlpack-2.0.3
 $ mkdir build
 @endcode
 
