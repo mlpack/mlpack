4469c6ee19223fbccbddbeae714a330ebe4af78c
Anuraj Kanodia <akanuraj200@gmail.com>
2016-09-25 11:17:31 +0530

    Update core.hpp

--- src/mlpack/core.hpp
+++ src/mlpack/core.hpp
@@ -194,7 +194,7 @@
  *   - Keon Kim <kwk236@gmail.com>
  *   - Nilay Jain <nilayjain13@gmail.com>
  *   - Peter Lehner <peter.lehner@dlr.de>
- *	 - Anuraj Kanodia <akanuraj200@gmail.com>
+ *  - Anuraj Kanodia <akanuraj200@gmail.com>
  */
 
 // First, include all of the prerequisites.
