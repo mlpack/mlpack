24748b08488955e8747dce8be7bd2ceb636618d5
Marcus Edel <marcus.edel@fu-berlin.de>
2016-12-12 13:52:50 +0100

    Minor style fixes.

--- src/mlpack/methods/ann/layer/convolution.hpp
+++ src/mlpack/methods/ann/layer/convolution.hpp
@@ -110,7 +110,7 @@ public:
   void Reset()
   {
     weight = arma::cube(weights.memptr(), kW, kH,
-        outSize * inSize, false,false);
+        outSize * inSize, false, false);
     bias = arma::mat(weights.memptr() + weight.n_elem,
         outSize, 1, false, false);
   }
@@ -215,8 +215,6 @@ public:
         {
           gTemp.slice(inMap) += output;
         }
-
-
       }
     }
 
