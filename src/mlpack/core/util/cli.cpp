c029cc0b3d615b7fa9d5f2b26bd441623127457a
Ryan Curtin <ryan@ratml.org>
2017-01-06 16:13:52 -0500

    Split HyphenateString() into a separate file.

--- src/mlpack/core/util/cli.cpp
+++ src/mlpack/core/util/cli.cpp
@@ -16,6 +16,7 @@
 
 #include "cli.hpp"
 #include "log.hpp"
+#include "hyphenate_string.hpp"
 
 #include <mlpack/core/data/load.hpp>
 #include <mlpack/core/data/save.hpp>
@@ -152,54 +153,6 @@ bool CLI::HasParam(const std::string& key)
   const std::string& checkKey = usedKey;
 
   return (vmap.count(parameters.at(checkKey).boostName) > 0);
-}
-
-/**
- * Hyphenate a string or split it onto multiple 80-character lines, with some
- * amount of padding on each line.  This is used for option output.
- *
- * @param str String to hyphenate (splits are on ' ').
- * @param padding Amount of padding on the left for each new line.
- */
-std::string CLI::HyphenateString(const std::string& str, int padding)
-{
-  size_t margin = 80 - padding;
-  if (str.length() < margin)
-    return str;
-  std::string out("");
-  unsigned int pos = 0;
-  // First try to look as far as possible.
-  while (pos < str.length())
-  {
-    size_t splitpos;
-    // Check that we don't have a newline first.
-    splitpos = str.find('\n', pos);
-    if (splitpos == std::string::npos || splitpos > (pos + margin))
-    {
-      // We did not find a newline.
-      if (str.length() - pos < margin)
-      {
-        splitpos = str.length(); // The rest fits on one line.
-      }
-      else
-      {
-        splitpos = str.rfind(' ', margin + pos); // Find nearest space.
-        if (splitpos <= pos || splitpos == std::string::npos) // Not found.
-          splitpos = pos + margin;
-      }
-    }
-    out += str.substr(pos, (splitpos - pos));
-    if (splitpos < str.length())
-    {
-      out += '\n';
-      out += std::string(padding, ' ');
-    }
-
-    pos = splitpos;
-    if (str[pos] == ' ' || str[pos] == '\n')
-      pos++;
-  }
-  return out;
 }
 
 // Returns the sole instance of this class.
