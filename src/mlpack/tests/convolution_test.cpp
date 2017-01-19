826a399a610d46de34f2cbaf8c0030e7cf19be78
Marcus Edel <marcus.edel@fu-berlin.de>
2017-01-19 15:47:23 +0100

    Minor style fix; move up comment to avoid potential licence parsing problems.

--- src/mlpack/tests/convolution_test.cpp
+++ src/mlpack/tests/convolution_test.cpp
@@ -3,11 +3,12 @@
  * @author Shangtong Zhang
  * @author Marcus Edel
  *
+ * Tests for various convolution strategies.
+ *
  * mlpack is free software; you may redistribute it and/or modify it under the
  * terms of the 3-clause BSD license.  You should have received a copy of the
  * 3-clause BSD license along with mlpack.  If not, see
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
- * Tests for various convolution strategies.
  */
 #include <mlpack/core.hpp>
 
