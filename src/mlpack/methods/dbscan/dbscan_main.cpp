e4d8a4cb8eff303909b7aa2450e47dbcde46cfd1
Ryan Curtin <ryan@ratml.org>
2017-02-23 09:47:47 -0500

    Add program documentation.

--- src/mlpack/methods/dbscan/dbscan_main.cpp
+++ src/mlpack/methods/dbscan/dbscan_main.cpp
@@ -18,7 +18,32 @@ using namespace mlpack::tree;
 using namespace std;
 
 PROGRAM_INFO("DBSCAN clustering",
-    "This program implements the DBSCAN algorithm for clustering.");
+    "This program implements the DBSCAN algorithm for clustering using "
+    "accelerated tree-based range search.  The type of tree that is used "
+    "may be parameterized, or brute-force range search may also be used."
+    "\n\n"
+    "The input dataset to be clustered may be specified with the --input_file "
+    "option, the radius of each range search may be specified with the "
+    "--epsilon option, and the minimum number of points in a cluster may be "
+    "specified with the --min_size option."
+    "\n\n"
+    "The output of the clustering may be saved as --assignments_file or "
+    "--centroids_file; --assignments_file will save the cluster assignments of "
+    "each point, and --centroids_file will save the centroids of each cluster."
+    "\n\n"
+    "The range search may be controlled with the --tree_type, --single_mode, "
+    "and --naive parameters.  The --tree_type parameter can control the type of"
+    " tree used for range search; this can take a variety of values: 'kd', 'r',"
+    " 'r-star', 'x', 'hilbert-r', 'r-plus', 'r-plus-plus', 'cover', 'ball'. "
+    "The --single_mode option will force single-tree search (as opposed to the "
+    "default dual-tree search), and --naive will force brute-force range "
+    "search."
+    "\n\n"
+    "An example usage to run DBSCAN on the dataset in input.csv with a radius "
+    "of 0.5 and a minimum cluster size of 5 is given below:"
+    "\n\n"
+    "  $ mlpack_dbscan -i input.csv -e 0.5 -m 5");
+
 
 PARAM_STRING_IN_REQ("input_file", "Input dataset to cluster.", "i");
 PARAM_STRING_OUT("assignments_file", "Output file for assignments of each "
