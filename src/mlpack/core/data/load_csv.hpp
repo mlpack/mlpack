fd3b4b16da2a755a1041a6866a1293deb7e8be64
stereomatchingkiss <thamngapwei@gmail.com>
2017-02-26 07:25:51 +0800

    add license

--- src/mlpack/core/data/load_csv.hpp
+++ src/mlpack/core/data/load_csv.hpp
@@ -3,6 +3,11 @@
  * @author ThamNgapWei
  *
  * This is a csv parsers which use to parse the csv file format
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
 #ifndef MLPACK_CORE_DATA_LOAD_CSV_HPP
 #define MLPACK_CORE_DATA_LOAD_CSV_HPP
