4e07f4fed08ffbe849faf7d9c40b82be70635b3b
shikhar <shikharbhardwaj68@gmail.com>
2017-03-01 21:06:52 +0530

    Fix include order to include armadillo before mlpack

--- src/mlpack/core/util/singletons.hpp
+++ src/mlpack/core/util/singletons.hpp
@@ -12,8 +12,8 @@
 #ifndef MLPACK_CORE_UTIL_SINGLETONS_HPP
 #define MLPACK_CORE_UTIL_SINGLETONS_HPP
 
-#include "cli_deleter.hpp"
 #include <mlpack/mlpack_export.hpp>
+#include "cli_deleter.hpp"
 
 namespace mlpack {
 namespace util {
