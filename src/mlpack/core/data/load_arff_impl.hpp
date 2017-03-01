80c450425b7ac6d21b8d8298a5735e777783cf88
Praveen Ch <chvsp972911@gmail.com>
2017-03-02 05:26:42 +1100

    Case insensitive condition added

--- src/mlpack/core/data/load_arff_impl.hpp
+++ src/mlpack/core/data/load_arff_impl.hpp
@@ -56,13 +56,15 @@ void LoadARFF(const std::string& filename,
 
       // Get the annotation we are looking at.
       std::string annotation(*it);
+      std::transform(annotation.begin(), annotation.end(), annotation.begin(),
+            ::tolower);
 
-      if (*tok.begin() == "@relation")
+      if (annotation == "@relation")
       {
         // We don't actually have anything to do with the name of the dataset.
         continue;
       }
-      else if (*tok.begin() == "@attribute")
+      else if (annotation == "@attribute")
       {
         ++dimensionality;
         // We need to mark this dimension with its according type.
@@ -84,7 +86,7 @@ void LoadARFF(const std::string& filename,
           throw std::logic_error("list of ARFF values not yet supported");
         }
       }
-      else if (*tok.begin() == "@data")
+      else if (annotation == "@data")
       {
         // We are in the data section.  So we can move out of this loop.
         break;
