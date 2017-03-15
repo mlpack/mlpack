4b08d9abd85282fe8bda3663b738635910402096
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2017-03-15 22:09:34 +0530

    Update AdaGrad test

--- src/mlpack/tests/ada_grad_test.cpp
+++ src/mlpack/tests/ada_grad_test.cpp
@@ -12,7 +12,7 @@
 #include <mlpack/core.hpp>
 #include <mlpack/core/optimizers/sgd/sgd.hpp>
 #include <mlpack/core/optimizers/sgd/update_policies/ada_grad_update.hpp>
-#include <mlpack/core/optimizers/lbfgs/test_functions.hpp>
+#include <mlpack/methods/logistic_regression/logistic_regression.hpp>
 #include <mlpack/core/optimizers/sgd/test_function.hpp>
 
 #include <boost/test/unit_test.hpp>
@@ -26,54 +26,82 @@ using namespace mlpack::optimization::test;
 
 BOOST_AUTO_TEST_SUITE(AdaGradTest);
 
-BOOST_AUTO_TEST_CASE(AdaGradSpeedUpTestFunction)
+/**
+ *Tests the Adagrad optimizer using a simple test function.
+ */
+BOOST_AUTO_TEST_CASE(SimpleAdaGradTestFunction)
 {
   SGDTestFunction f;
   AdaGradUpdate adagradUpdate(1e-8);
-  AdaGrad<SGDTestFunction> s(f, 0.0003, 2500000, 1e-9, true, adagradUpdate);
+  AdaGrad<SGDTestFunction> optimizer(f, 0.99, 5000000, 1e-9, true, adagradUpdate);
 
   arma::mat coordinates = f.GetInitialPoint();
-  double result = s.Optimize(coordinates);
+  optimizer.Optimize(coordinates);
 
-  BOOST_REQUIRE_CLOSE(result, -1.0, 0.05);
-  BOOST_REQUIRE_SMALL(coordinates[0], 1e-3);
-  BOOST_REQUIRE_SMALL(coordinates[1], 1e-7);
-  BOOST_REQUIRE_SMALL(coordinates[2], 1e-7);
-
-  // Compare with SGD with vanilla update.
-  SGDTestFunction f1;
-  StandardSGD<SGDTestFunction> s1(f1, 0.0003, 2500000, 1e-9, true);
-
-  arma::mat coordinates1 = f.GetInitialPoint();
-  double result1 = s1.Optimize(coordinates1);
-
-  // Result doesn't converge in 2500000 iterations.
-  BOOST_REQUIRE_GT(result1 + 1.0, 0.05);
-  BOOST_REQUIRE_GE(coordinates1[0], 1e-3);
-  BOOST_REQUIRE_SMALL(coordinates1[1], 1e-7);
-  BOOST_REQUIRE_SMALL(coordinates1[2], 1e-7);
-
-
-  BOOST_REQUIRE_LE(result,result1);
+  BOOST_REQUIRE_SMALL(coordinates[0], 0.003);
+  BOOST_REQUIRE_SMALL(coordinates[1], 0.003);
+  BOOST_REQUIRE_SMALL(coordinates[2], 0.003);
 }
 
-BOOST_AUTO_TEST_CASE(AdaGradGeneralizedRosenbrockTest)
+/**
+ * Run AdaGrad on logistic regression and make sure the results are acceptable.
+ */
+BOOST_AUTO_TEST_CASE(AdaGradLogisticRegressionTest)
 {
-  // Loop over several variants.
-  for (size_t i = 10; i < 50; i += 5)
+  // Generate a two-Gaussian dataset.
+  GaussianDistribution g1(arma::vec("1.0 1.0 1.0"), arma::eye<arma::mat>(3, 3));
+  GaussianDistribution g2(arma::vec("9.0 9.0 9.0"), arma::eye<arma::mat>(3, 3));
+
+  arma::mat data(3, 1000);
+  arma::Row<size_t> responses(1000);
+  for (size_t i = 0; i < 500; ++i)
   {
-    // Create the generalized Rosenbrock function.
-    GeneralizedRosenbrockFunction f(i);
-    AdaGradUpdate adagradUpdate(1e-8);
-    AdaGrad<GeneralizedRosenbrockFunction> s(f, 0.001, 0, 1e-15, true, adagradUpdate);
-
-    arma::mat coordinates = f.GetInitialPoint();
-    double result = s.Optimize(coordinates);
-
-    BOOST_REQUIRE_SMALL(result, 1e-10);
-    for (size_t j = 0; j < i; ++j)
-      BOOST_REQUIRE_CLOSE(coordinates[j], (double) 1.0, 1e-3);
+    data.col(i) = g1.Random();
+    responses[i] = 0;
   }
-}
+  for (size_t i = 500; i < 1000; ++i)
+  {
+    data.col(i) = g2.Random();
+    responses[i] = 1;
+  }
 
+  // Shuffle the dataset.
+  arma::uvec indices = arma::shuffle(arma::linspace<arma::uvec>(0,
+      data.n_cols - 1, data.n_cols));
+  arma::mat shuffledData(3, 1000);
+  arma::Row<size_t> shuffledResponses(1000);
+  for (size_t i = 0; i < data.n_cols; ++i)
+  {
+    shuffledData.col(i) = data.col(indices[i]);
+    shuffledResponses[i] = responses[indices[i]];
+  }
+
+  // Create a test set.
+  arma::mat testData(3, 1000);
+  arma::Row<size_t> testResponses(1000);
+  for (size_t i = 0; i < 500; ++i)
+  {
+    testData.col(i) = g1.Random();
+    testResponses[i] = 0;
+  }
+  for (size_t i = 500; i < 1000; ++i)
+  {
+    testData.col(i) = g2.Random();
+    testResponses[i] = 1;
+  }
+
+  LogisticRegression<> lr(shuffledData.n_rows, 0.5);
+
+  LogisticRegressionFunction<> lrf(shuffledData, shuffledResponses, 0.5);
+  AdaGradUpdate adagradUpdate(1e-8);
+  AdaGrad<LogisticRegressionFunction<> > adagrad(lrf, 0.99, 5000000, 1e-9, true, adagradUpdate);
+  lr.Train(adagrad);
+
+  // Ensure that the error is close to zero.
+  const double acc = lr.ComputeAccuracy(data, responses);
+  BOOST_REQUIRE_CLOSE(acc, 100.0, 0.3); // 0.3% error tolerance.
+
+  const double testAcc = lr.ComputeAccuracy(testData, testResponses);
+  BOOST_REQUIRE_CLOSE(testAcc, 100.0, 0.6); // 0.6% error tolerance.
+}
 BOOST_AUTO_TEST_SUITE_END();
