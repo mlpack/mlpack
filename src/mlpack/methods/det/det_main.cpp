b78d0642ad2e01f50114c985486dfbb233bd9534
Skand <pvskand@gmail.com>
2017-03-18 10:26:10 +0530

    removed unwanted headers

--- src/mlpack/methods/det/det_main.cpp
+++ src/mlpack/methods/det/det_main.cpp
@@ -10,7 +10,7 @@
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
 #include <mlpack/prereqs.hpp>
-#include <mlpack/core/util/cli.hpp>
+
 #include "dt_utils.hpp"
 
 using namespace mlpack;
