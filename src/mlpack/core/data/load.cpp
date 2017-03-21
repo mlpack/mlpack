98477eacc7eb8fa4c348008d401e9905f39aed4a
Ryan Curtin <ryan@ratml.org>
2017-03-21 15:43:35 -0400

    Add license.

--- src/mlpack/core/data/load.cpp
+++ src/mlpack/core/data/load.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file load.cpp
+ * @author Tham Ngap Wei
+ *
+ * Explicit instantiations of load functions.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
 #include "load.hpp"
 #include "load_impl.hpp"
 
