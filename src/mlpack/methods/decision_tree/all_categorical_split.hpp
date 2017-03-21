45a048ba7dcead660c7c6640cd2bfbd5d09212f7
Ryan Curtin <ryan@ratml.org>
2017-03-21 15:43:46 -0400

    Add license to DBSCAN and decision tree files.

--- src/mlpack/methods/decision_tree/all_categorical_split.hpp
+++ src/mlpack/methods/decision_tree/all_categorical_split.hpp
@@ -4,6 +4,11 @@
  *
  * This file defines a tree splitter that split a categorical feature into all
  * of the possible categories.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
 #ifndef MLPACK_METHODS_DECISION_TREE_ALL_CATEGORICAL_SPLIT_HPP
 #define MLPACK_METHODS_DECISION_TREE_ALL_CATEGORICAL_SPLIT_HPP
