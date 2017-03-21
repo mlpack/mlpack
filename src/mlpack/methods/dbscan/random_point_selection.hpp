d9164ab4fb2085507c9f4abd3e33562c418eee2f
Ryan Curtin <ryan@ratml.org>
2017-03-21 15:43:46 -0400

    Add license to DBSCAN and decision tree files.

--- src/mlpack/methods/dbscan/random_point_selection.hpp
+++ src/mlpack/methods/dbscan/random_point_selection.hpp
@@ -3,6 +3,11 @@
  * @author Ryan Curtin
  *
  * Randomly select the next point for DBSCAN.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
 #ifndef MLPACK_METHODS_DBSCAN_RANDOM_POINT_SELECTION_HPP
 #define MLPACK_METHODS_DBSCAN_RANDOM_POINT_SELECTION_HPP
