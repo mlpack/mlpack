16a27fc242de1d493b18805333e2bc8c72fe8d73
Vivek Pal <vivekpal.dtu@gmail.com>
2017-04-02 19:55:11 +0530

    Fix macros

--- src/mlpack/core/optimizers/smorms3/smorms3_update.hpp
+++ src/mlpack/core/optimizers/smorms3/smorms3_update.hpp
@@ -9,8 +9,8 @@
  * 3-clause BSD license along with mlpack.  If not, see
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
-#ifndef MLPACK_CORE_OPTIMIZERS_SMORMS3_UPDATE_HPP
-#define MLPACK_CORE_OPTIMIZERS_SMORMS3_UPDATE_HPP
+#ifndef MLPACK_CORE_OPTIMIZERS_SMORMS3_SMORMS3_UPDATE_HPP
+#define MLPACK_CORE_OPTIMIZERS_SMORMS3_SMORMS3_UPDATE_HPP
 
 #include <mlpack/prereqs.hpp>
 
