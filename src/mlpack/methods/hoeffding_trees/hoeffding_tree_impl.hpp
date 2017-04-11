e9901c5a882e9f0b5df788620de4b89504487abe
Samikshya Chand <samikshya.chand.ece15@itbhu.ac.in>
2017-04-11 11:27:49 +0530

    Update hoeffding_tree_impl.hpp

--- src/mlpack/methods/hoeffding_trees/hoeffding_tree_impl.hpp
+++ src/mlpack/methods/hoeffding_trees/hoeffding_tree_impl.hpp
@@ -500,7 +500,21 @@ size_t HoeffdingTree<
   else
     return 0; // Not sure what to do here...
 }
-
+size_t NumDescendants()
+{
+size_t nodes = 0;
+std::stack<HoeffdingTree<>*> stack;
+stack.push(&tree); // 'tree' is the root of the tree
+while (!stack.empty())
+{  
+   HoeffdingTree<>* node = stack.top();
+   stack.pop();
+   nodes += node.NumChildren();
+   for (size_t i = 0; i < node.NumChildren(); ++i)
+            stack.push(&node.Child(i));
+}
+return nodes;
+}
 template<
     typename FitnessFunction,
     template<typename> class NumericSplitType,
