2f9d37d71c8ed73dde749341a484f3364a9f3a17
Ryan Curtin <ryan@ratml.org>
2017-05-12 22:46:15 -0400

    Add tests for all other parameter types.

--- src/mlpack/bindings/python/tests/test_python_binding.py
+++ src/mlpack/bindings/python/tests/test_python_binding.py
@@ -102,6 +102,7 @@ class TestPythonBinding(unittest.TestCase):
 
     self.assertEqual(output['matrix_out'].shape[0], 100)
     self.assertEqual(output['matrix_out'].shape[1], 4)
+    self.assertEqual(output['matrix_out'].dtype, np.double)
     for i in [0, 1, 3]:
       for j in range(100):
         self.assertEqual(x[j, i], output['matrix_out'][j, i])
@@ -124,6 +125,7 @@ class TestPythonBinding(unittest.TestCase):
 
     self.assertEqual(output['matrix_out'].shape[0], 3)
     self.assertEqual(output['matrix_out'].shape[1], 4)
+    self.assertEqual(output['matrix_out'].dtype, np.double)
     self.assertEqual(output['matrix_out'][0, 0], 1)
     self.assertEqual(output['matrix_out'][0, 1], 2)
     self.assertEqual(output['matrix_out'][0, 2], 6)
@@ -136,6 +138,195 @@ class TestPythonBinding(unittest.TestCase):
     self.assertEqual(output['matrix_out'][2, 1], 12)
     self.assertEqual(output['matrix_out'][2, 2], 26)
     self.assertEqual(output['matrix_out'][2, 3], 14)
+
+  def testNumpyUmatrix(self):
+    """
+    Same as testNumpyMatrix() but with an unsigned matrix.
+    """
+    x = np.random.randint(0, high=500, size=[100, 5])
+
+    output = test_python_binding(string_in='hello',
+                                 int_in=12,
+                                 double_in=4.0,
+                                 umatrix_in=x)
+
+    self.assertEqual(output['umatrix_out'].shape[0], 100)
+    self.assertEqual(output['umatrix_out'].shape[1], 4)
+    self.assertEqual(output['umatrix_out'].dtype, np.long)
+    for i in [0, 1, 3]:
+      for j in range(100):
+        self.assertEqual(x[j, i], output['umatrix_out'][j, i])
+
+    for j in range(100):
+      self.assertEqual(2 * x[j, 2], output['umatrix_out'][j, 2])
+
+  def testArraylikeUmatrix(self):
+    """
+    Test that we can pass an arraylike unsigned matrix.
+    """
+    x = [[1, 2, 3, 4, 5],
+         [6, 7, 8, 9, 10],
+         [11, 12, 13, 14, 15]]
+
+    output = test_python_binding(string_in='hello',
+                                 int_in=12,
+                                 double_in=4.0,
+                                 umatrix_in=x)
+
+    self.assertEqual(output['umatrix_out'].shape[0], 3)
+    self.assertEqual(output['umatrix_out'].shape[1], 4)
+    self.assertEqual(output['umatrix_out'].dtype, np.long)
+    self.assertEqual(output['umatrix_out'][0, 0], 1)
+    self.assertEqual(output['umatrix_out'][0, 1], 2)
+    self.assertEqual(output['umatrix_out'][0, 2], 6)
+    self.assertEqual(output['umatrix_out'][0, 3], 4)
+    self.assertEqual(output['umatrix_out'][1, 0], 6)
+    self.assertEqual(output['umatrix_out'][1, 1], 7)
+    self.assertEqual(output['umatrix_out'][1, 2], 16)
+    self.assertEqual(output['umatrix_out'][1, 3], 9)
+    self.assertEqual(output['umatrix_out'][2, 0], 11)
+    self.assertEqual(output['umatrix_out'][2, 1], 12)
+    self.assertEqual(output['umatrix_out'][2, 2], 26)
+    self.assertEqual(output['umatrix_out'][2, 3], 14)
+
+  def testCol(self):
+    """
+    Test a column vector input parameter.
+    """
+    x = np.random.rand(100)
+
+    output = test_python_binding(string_in='hello',
+                                 int_in=12,
+                                 double_in=4.0,
+                                 col_in=x)
+
+    self.assertEqual(output['col_out'].shape[0], 100)
+    self.assertEqual(output['col_out'].dtype, np.double)
+
+    for i in range(100):
+      self.assertEqual(output['col_out'][i], x[i] * 2)
+
+  def testUcol(self):
+    """
+    Test an unsigned column vector input parameter.
+    """
+    x = np.random.randint(0, high=500, size=100)
+
+    output = test_python_binding(string_in='hello',
+                                 int_in=12,
+                                 double_in=4.0,
+                                 ucol_in=x)
+
+    self.assertEqual(output['ucol_out'].shape[0], 100)
+    self.assertEqual(output['ucol_out'].dtype, np.long)
+    for i in range(100):
+      self.assertEqual(output['ucol_out'][i], x[i] * 2)
+
+  def testRow(self):
+    """
+    Test a row vector input parameter.
+    """
+    x = np.random.rand(100)
+
+    output = test_python_binding(string_in='hello',
+                                 int_in=12,
+                                 double_in=4.0,
+                                 row_in=x)
+
+    self.assertEqual(output['row_out'].shape[0], 100)
+    self.assertEqual(output['row_out'].dtype, np.double)
+
+    for i in range(100):
+      self.assertEqual(output['row_out'][i], x[i] * 2)
+
+  def testUrow(self):
+    """
+    Test an unsigned row vector input parameter.
+    """
+    x = np.random.randint(0, high=500, size=100)
+
+    output = test_python_binding(string_in='hello',
+                                 int_in=12,
+                                 double_in=4.0,
+                                 urow_in=x)
+
+    self.assertEqual(output['urow_out'].shape[0], 100)
+    self.assertEqual(output['urow_out'].dtype, np.long)
+
+    for i in range(100):
+      self.assertEqual(output['urow_out'][i], x[i] * 2)
+
+  def testMatrixAndInfoNumpy(self):
+    """
+    Test that we can pass a matrix with all numeric features.
+    """
+    x = np.random.rand(100, 10)
+
+    output = test_python_binding(string_in='hello',
+                                 int_in=12,
+                                 double_in=4.0,
+                                 matrix_and_info_in=x)
+
+    self.assertEqual(output['matrix_and_info_out'].shape[0], 100)
+    self.assertEqual(output['matrix_and_info_out'].shape[1], 10)
+
+    for i in range(10):
+      for j in range(100):
+        self.assertEqual(output['matrix_and_info_out'][j, i], x[j, i] * 2.0)
+
+  def testMatrixAndInfoPandas(self):
+    """
+    Test that we can pass a matrix with some categorical features.
+    """
+    x = pd.DataFrame(np.random.rand(10, 4), columns=list('abcd'))
+    x['e'] = pd.Series(['a', 'b', 'c', 'd', 'a', 'b', 'e', 'c', 'a', 'b'],
+        dtype='category')
+
+    output = test_python_binding(string_in='hello',
+                                 int_in=12,
+                                 double_in=4.0,
+                                 matrix_and_info_in=x)
+
+    self.assertEqual(output['matrix_and_info_out'].shape[0], 10)
+    self.assertEqual(output['matrix_and_info_out'].shape[1], 5)
+
+    cols = list('abcde')
+
+    for i in range(4):
+      for j in range(10):
+        self.assertEqual(output['matrix_and_info_out'][j, i], x[cols[i]][j] * 2)
+
+    for j in range(10):
+      self.assertEqual(output['matrix_and_info_out'][j, 4], x[cols[4]][j])
+
+  def testIntVector(self):
+    """
+    Test that we can pass a vector of ints and get back that same vector but
+    with the last element removed.
+    """
+    x = [1, 2, 3, 4, 5]
+
+    output = test_python_binding(string_in='hello',
+                                 int_in=12,
+                                 double_in=4.0,
+                                 vector_in=x)
+
+    self.assertEqual(output['vector_out'], [1, 2, 3, 4])
+
+  def testStringVector(self):
+    """
+    Test that we can pass a vector of strings and get back that same vector but
+    with the last element removed.
+    """
+    x = ['one', 'two', 'three', 'four', 'five']
+
+    output = test_python_binding(string_in='hello',
+                                 int_in=12,
+                                 double_in=4.0,
+                                 str_vector_in=x)
+
+    self.assertEqual(output['str_vector_out'],
+        ['one', 'two', 'three', 'four'])
 
 if __name__ == '__main__':
   unittest.main()
