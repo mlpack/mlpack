bb165eb1073d6b83d665be80f2d2b3564ab24f4e
Abhinav Moudgil <abhinavmoudgil95@gmail.com>
2017-05-25 23:28:34 +0530

    Lines should be <= 80 characters long

--- src/mlpack/core/dists/discrete_distribution.hpp
+++ src/mlpack/core/dists/discrete_distribution.hpp
@@ -144,10 +144,10 @@ class DiscreteDistribution
       // Ensure that the observation is within the bounds.
       if (obs >= probabilities[dimension].n_elem)
       {
-        Log::Debug << "DiscreteDistribution::Probability(): received observation "
-             << obs << "; observation must be in [0, "
-             << probabilities[dimension].n_elem << "] for this distribution."
-             << std::endl;
+        Log::Debug << "DiscreteDistribution::Probability(): "
+             << "received observation " << obs << "; observation must be"
+             << "in [0, " << probabilities[dimension].n_elem << "] for this "
+             << "distribution." << std::endl;
       }
       probability *= probabilities[dimension][obs];
     }
