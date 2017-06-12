e637b2290ad67b99f7a63d7fff20e62772b63f6b
Konstantin Sidorov <kost2906@yandex.ru>
2017-06-12 13:39:09 +0400

    Code refactoring + some rewrite towards arma's native methods

--- src/mlpack/methods/ann/augmented/tasks/score_impl.hpp
+++ src/mlpack/methods/ann/augmented/tasks/score_impl.hpp
@@ -20,72 +20,23 @@ namespace ann /* Artificial Neural Network */ {
 namespace augmented /* Augmented neural network */ {
 namespace scorers /* Scoring utilities for augmented */ {
 
-double SequencePrecision(arma::field<arma::colvec> trueOutputs,
-                         arma::field<arma::colvec> predOutputs)
+template<typename eT>
+double SequencePrecision(arma::field<eT> trueOutputs,
+                         arma::field<eT> predOutputs)
 {
   double score = 0;
-  auto testSize = trueOutputs.n_elem;
+  size_t testSize = trueOutputs.n_elem;
   assert(testSize == predOutputs.n_elem);
 
   for (size_t i = 0; i < testSize; i++)
   {
-    auto prediction = trueOutputs.at(i);
-    auto output = predOutputs.at(i);
 
-    bool ok = true;
-
-    if (output.n_elem != prediction.n_elem)
+    if (arma::approx_equal(
+          trueOutputs.at(i), predOutputs.at(i),
+          "absdiff", 1e-4))
     {
-      ok = false;
+      score++;
     }
-    else
-    {
-      for (size_t j = 0; j < prediction.n_elem; ++j) {
-        if (output.at(j) != prediction.at(j)) {
-          ok = false;
-          break;
-        }
-      }
-    }
-
-    if (ok) score++;
-  }
-  score /= testSize;
-  return score;
-}
-
-double SequencePrecision(arma::field<arma::mat> trueOutputs,
-                         arma::field<arma::mat> predOutputs)
-{
-  double score = 0;
-  auto testSize = trueOutputs.n_elem;
-  assert(testSize == predOutputs.n_elem);
-
-  for (size_t i = 0; i < testSize; i++)
-  {
-    auto prediction = trueOutputs.at(i);
-    auto output = predOutputs.at(i);
-
-    bool ok = true;
-
-    if (output.n_rows != prediction.n_rows ||
-        output.n_cols != prediction.n_cols)
-    {
-      ok = false;
-    }
-    else
-    {
-      for (size_t j = 0; j < prediction.n_rows; ++j) {
-        for (size_t k = 0; k < prediction.n_cols; ++k) {
-          if (output.at(j, k) != prediction.at(j, k)) {
-            ok = false;
-            break;
-          }
-        }
-      }
-    }
-
-    if (ok) score++;
   }
   score /= testSize;
   return score;
