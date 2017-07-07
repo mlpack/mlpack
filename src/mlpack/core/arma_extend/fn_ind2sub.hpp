0efd256912855b1bfb650e8c83f9f1ae31c82095
rajiv2605 <rajivvaidyanathan4@gmail.com>
2017-07-07 12:35:41 +0530

    Updated src/mlpack/core/arma_extend/CMakeLists.txt after deleting fn_ind2sub.hpp and operator_minus.hpp

--- src/mlpack/core/arma_extend/fn_ind2sub.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2008-2016 National ICT Australia (NICTA)
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-// -------------------------------------------------------------------
-//
-// Written by Conrad Sanderson - http://conradsanderson.id.au
