f596ee1ce6b894c218a9dcfa2e573a91589e0c7c
kartik nighania <kkstrack@gmail.com>
2017-07-08 03:56:09 +0530

    test case added and fixed

--- src/mlpack/tests/cmaes_test.cpp
+++ src/mlpack/tests/cmaes_test.cpp
@@ -1,40 +1,41 @@
 /**
- * @file cmaes_test.cpp
- * @author Kartik Nighania (Mentor Marcus Edel)
+ * @file sgd_test.cpp
+ * @author Ryan Curtin
  *
- * Test file for CMAES (Covariance Matrix Adaptation Evolution Strategy).
+ * Test file for SGD (stochastic gradient descent).
  *
  * mlpack is free software; you may redistribute it and/or modify it under the
  * terms of the 3-clause BSD license.  You should have received a copy of the
  * 3-clause BSD license along with mlpack.  If not, see
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
-
 #include <mlpack/core.hpp>
 #include <mlpack/core/optimizers/cmaes/cmaes.hpp>
-#include <mlpack/core/optimizers/lbfgs/test_functions.hpp>
 #include <mlpack/core/optimizers/cmaes/test_function.hpp>
-#include <armadillo>
 
 #include <boost/test/unit_test.hpp>
 #include "test_tools.hpp"
 
+using namespace std;
+using namespace arma;
 using namespace mlpack;
 using namespace mlpack::optimization;
 using namespace mlpack::optimization::test;
 
-BOOST_AUTO_TEST_SUITE(CMAEStest);
+BOOST_AUTO_TEST_SUITE(CMAESTest);
 
-BOOST_AUTO_TEST_CASE(SimpleCMAEStestFunction)
+BOOST_AUTO_TEST_CASE(SimpleCMAESTestFunction)
 {
   cmaesTestFunction test;
+  
+  int N = test.NumFunctions();
 
-  arma::mat initialStart("0.5; 0.5; 0.5");
-  arma::mat initialStandardDeviation("2; 2; 2");
+  arma::mat start(N,1); start.fill(0.5); 
+  arma::mat initialStdDeviations(N,1); initialStdDeviations.fill(1.5);
 
-  CMAES<cmaesTestFunction> s(test, initialStart, initialStandardDeviation);
+  CMAES<cmaesTestFunction> s(test,start,initialStdDeviations);
 
-  arma::mat coordinates(test.NumFunctions(), 1);
+  arma::mat coordinates(N,1);
   double result = s.Optimize(coordinates);
 
   BOOST_REQUIRE_CLOSE(result, -1.0, 0.05);
@@ -43,26 +44,5 @@ BOOST_AUTO_TEST_CASE(SimpleCMAEStestFunction)
   BOOST_REQUIRE_SMALL(coordinates[2], 1e-7);
 }
 
-/* CANT IMPLEMENT DUE TO CHANGE IN EVALUATE FUNCTION IN CMAES
-
-BOOST_AUTO_TEST_CASE(GeneralizedRosenbrockTest)
-{
-  // Loop over several variants.
-  for (size_t i = 10; i < 50; i += 5)
-  {
-    // Create the generalized Rosenbrock function.
-    GeneralizedRosenbrockFunction f(i);
-
-    SGD<GeneralizedRosenbrockFunction> s(f, 0.001, 0, 1e-15, true);
-
-    arma::mat coordinates = f.GetInitialPoint();
-    double result = s.Optimize(coordinates);
-
-    BOOST_REQUIRE_SMALL(result, 1e-10);
-    for (size_t j = 0; j < i; ++j)
-      BOOST_REQUIRE_CLOSE(coordinates[j], (double) 1.0, 1e-3);
-  }
-}
-*/
-
 BOOST_AUTO_TEST_SUITE_END();
+
