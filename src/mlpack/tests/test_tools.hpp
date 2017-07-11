8822c4aee31a397818eb9b5f7f42ec9ecb658dcd
Shikhar Bhardwaj <shikharbhardwaj68@gmail.com>
2017-07-12 02:58:47 +0530

    Add comment to describe FilterFileName

--- src/mlpack/tests/test_tools.hpp
+++ src/mlpack/tests/test_tools.hpp
@@ -48,6 +48,7 @@ inline void CheckMatrices(const arma::Mat<size_t>& a,
     BOOST_REQUIRE_EQUAL(a[i], b[i]);
 }
 
+// Filter typeinfo string to generate unique filenames for serialization tests.
 inline std::string FilterFileName(const std::string& inputString)
 {
   // Take the last valid 32 characters for the filename.
