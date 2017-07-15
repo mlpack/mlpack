aa51bfd1f9e9c370fd84fedc0a24a08baf1b0fb2
kartik nighania <kkstrack@gmail.com>
2017-07-15 23:44:41 +0530

    test error and algo is fine

--- src/mlpack/tests/cmaes_test.cpp
+++ src/mlpack/tests/cmaes_test.cpp
@@ -1,6 +1,6 @@
 /**
  * @file cmaes_test.cpp
- * @author Kartik Nighania (Mentor Marcus Edel)
+ * @author Kartik Nighania Mentor Marcus Edel
  *
  * Test file for CMAES (Covariance Matrix Adaptation Evolution Strategy).
  *
@@ -9,33 +9,41 @@
  * 3-clause BSD license along with mlpack.  If not, see
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
-
 #include <mlpack/core.hpp>
+
 #include <mlpack/core/optimizers/cmaes/cmaes.hpp>
 #include <mlpack/core/optimizers/lbfgs/test_functions.hpp>
-#include <mlpack/core/optimizers/cmaes/test_function.hpp>
-#include <armadillo>
+#include <mlpack/core/optimizers/sgd/test_function.hpp>
+
+#include <mlpack/methods/logistic_regression/logistic_regression.hpp>
 
 #include <boost/test/unit_test.hpp>
 #include "test_tools.hpp"
 
+using namespace std;
+using namespace arma;
 using namespace mlpack;
 using namespace mlpack::optimization;
 using namespace mlpack::optimization::test;
 
-BOOST_AUTO_TEST_SUITE(CMAEStest);
+using namespace mlpack::distribution;
+using namespace mlpack::regression;
 
-BOOST_AUTO_TEST_CASE(SimpleCMAEStestFunction)
+BOOST_AUTO_TEST_SUITE(CMAESTest);
+
+BOOST_AUTO_TEST_CASE(SimpleCMAESTestFunction)
 {
-  cmaesTestFunction test;
+  SGDTestFunction test;
 
-  arma::mat initialStart("0.5; 0.5; 0.5");
-  arma::mat initialStandardDeviation("2; 2; 2");
+  size_t N = test.NumFunctions();
 
-  CMAES<cmaesTestFunction> s(test, initialStart, initialStandardDeviation);
+  arma::mat start(N, 1); start.fill(0.5);
+  arma::mat initialStdDeviations(N, 1); initialStdDeviations.fill(1.5);
 
-  arma::mat coordinates(test.NumFunctions(), 1);
-  double result = s.Optimize(coordinates);
+  CMAES s(N, start, initialStdDeviations, 10000, 1e-18);
+
+  arma::mat coordinates(N, 1);
+  double result = s.Optimize(test, coordinates);
 
   BOOST_REQUIRE_CLOSE(result, -1.0, 0.05);
   BOOST_REQUIRE_SMALL(coordinates[0], 1e-3);
@@ -43,26 +51,67 @@ BOOST_AUTO_TEST_CASE(SimpleCMAEStestFunction)
   BOOST_REQUIRE_SMALL(coordinates[2], 1e-7);
 }
 
-/* CANT IMPLEMENT DUE TO CHANGE IN EVALUATE FUNCTION IN CMAES
+// written by Marcus Edel and modified for CMAES by Kartik Nighania
+// to test CMAES as an optimizer for logistic regression optimization
 
-BOOST_AUTO_TEST_CASE(GeneralizedRosenbrockTest)
+BOOST_AUTO_TEST_CASE(LogisticRegressionTestWithCMAES)
 {
-  // Loop over several variants.
-  for (size_t i = 10; i < 50; i += 5)
+  // Generate a two-Gaussian dataset.
+  GaussianDistribution g1(arma::vec("1.0 1.0 1.0"), arma::eye<arma::mat>(3, 3));
+  GaussianDistribution g2(arma::vec("9.0 9.0 9.0"), arma::eye<arma::mat>(3, 3));
+
+  arma::mat data(3, 1000);
+  arma::Row<size_t> responses(1000);
+  for (size_t i = 0; i < 500; ++i)
   {
-    // Create the generalized Rosenbrock function.
-    GeneralizedRosenbrockFunction f(i);
-
-    SGD<GeneralizedRosenbrockFunction> s(f, 0.001, 0, 1e-15, true);
-
-    arma::mat coordinates = f.GetInitialPoint();
-    double result = s.Optimize(coordinates);
-
-    BOOST_REQUIRE_SMALL(result, 1e-10);
-    for (size_t j = 0; j < i; ++j)
-      BOOST_REQUIRE_CLOSE(coordinates[j], (double) 1.0, 1e-3);
+    data.col(i) = g1.Random();
+    responses[i] = 0;
   }
+  for (size_t i = 500; i < 1000; ++i)
+  {
+    data.col(i) = g2.Random();
+    responses[i] = 1;
+  }
+
+  // Shuffle the dataset.
+  arma::uvec indices = arma::shuffle(arma::linspace<arma::uvec>(0,
+      data.n_cols - 1, data.n_cols));
+  arma::mat shuffledData(3, 1000);
+  arma::Row<size_t> shuffledResponses(1000);
+  for (size_t i = 0; i < data.n_cols; ++i)
+  {
+    shuffledData.col(i) = data.col(indices[i]);
+    shuffledResponses[i] = responses[indices[i]];
+  }
+
+  // Create a test set.
+  arma::mat testData(3, 1000);
+  arma::Row<size_t> testResponses(1000);
+  for (size_t i = 0; i < 500; ++i)
+  {
+    testData.col(i) = g1.Random();
+    testResponses[i] = 0;
+  }
+  for (size_t i = 500; i < 1000; ++i)
+  {
+    testData.col(i) = g2.Random();
+    testResponses[i] = 1;
+  }
+
+  int dim = shuffledData.n_rows + 1;
+  arma::mat start1(dim,1); start1.fill(0.5); 
+  arma::mat initialStdDeviations1(dim,1); initialStdDeviations1.fill(1.5);
+
+  CMAES test1(dim, start1, initialStdDeviations1, 50000, 1e-7);
+
+  LogisticRegression<> lr(shuffledData, shuffledResponses, test1, 0.5);
+
+  // Ensure that the error is close to zero.
+  const double acc = lr.ComputeAccuracy(data, responses);
+  BOOST_REQUIRE_CLOSE(acc, 100.0, 0.3); // 0.3% error tolerance.
+
+  const double testAcc = lr.ComputeAccuracy(testData, testResponses);
+  BOOST_REQUIRE_CLOSE(testAcc, 100.0, 0.6); // 0.6% error tolerance.
 }
-*/
 
 BOOST_AUTO_TEST_SUITE_END();
