7fe0c384fe8141ae66bca3a687a8317ce1febdbc
kartik nighania <kkstrack@gmail.com>
2017-07-26 11:14:59 +0530

    neural network test not working

--- src/mlpack/tests/cmaes_test.cpp
+++ src/mlpack/tests/cmaes_test.cpp
@@ -17,12 +17,17 @@
 
 #include <mlpack/methods/logistic_regression/logistic_regression.hpp>
 
+#include <mlpack/core/optimizers/sgd/update_policies/vanilla_update.hpp>
+#include <mlpack/methods/ann/layer/layer.hpp>
+#include <mlpack/methods/ann/ffn.hpp>
+
 #include <boost/test/unit_test.hpp>
 #include "test_tools.hpp"
 
 using namespace std;
 using namespace arma;
 using namespace mlpack;
+using namespace mlpack::ann;
 using namespace mlpack::optimization;
 using namespace mlpack::optimization::test;
 
@@ -30,6 +35,79 @@ using namespace mlpack::distribution;
 using namespace mlpack::regression;
 
 BOOST_AUTO_TEST_SUITE(CMAESTest);
+
+/**
+ * Train and evaluate a vanilla network with the specified structure.
+ */
+template<typename MatType = arma::mat>
+void BuildVanillaNetwork(MatType& trainData,
+                         MatType& trainLabels,
+                         MatType& testData,
+                         MatType& testLabels,
+                         const size_t outputSize,
+                         const size_t hiddenLayerSize,
+                         const size_t maxEpochs,
+                         const double classificationErrorThreshold)
+{
+  /*
+   * Construct a feed forward network with trainData.n_rows input nodes,
+   * hiddenLayerSize hidden nodes and trainLabels.n_rows output nodes. The
+   * network structure looks like:
+   *
+   *  Input         Hidden        Output
+   *  Layer         Layer         Layer
+   * +-----+       +-----+       +-----+
+   * |     |       |     |       |     |
+   * |     +------>|     +------>|     |
+   * |     |     +>|     |     +>|     |
+   * +-----+     | +--+--+     | +-----+
+   *             |             |
+   *  Bias       |  Bias       |
+   *  Layer      |  Layer      |
+   * +-----+     | +-----+     |
+   * |     |     | |     |     |
+   * |     +-----+ |     +-----+
+   * |     |       |     |
+   * +-----+       +-----+
+   */
+
+  FFN<NegativeLogLikelihood<> > model;
+  model.Add<Linear<> >(trainData.n_rows, hiddenLayerSize);
+  model.Add<SigmoidLayer<> >();
+  model.Add<Linear<> >(hiddenLayerSize, outputSize);
+  model.Add<LogSoftMax<> >();
+
+  int dim = trainData.n_rows * hiddenLayerSize +  hiddenLayerSize * outputSize;
+  arma::mat start1(dim, 1); start1.fill(0.5);
+  arma::mat initialStdDeviations1(dim, 1); initialStdDeviations1.fill(0.3);
+
+  CMAES opt(dim, start1, initialStdDeviations1, 50000, 1e-8);
+
+  model.Train(trainData, trainLabels, opt);
+
+  MatType predictionTemp;
+  model.Predict(testData, predictionTemp);
+  MatType prediction = arma::zeros<MatType>(1, predictionTemp.n_cols);
+
+  for (size_t i = 0; i < predictionTemp.n_cols; ++i)
+  {
+    prediction(i) = arma::as_scalar(arma::find(
+        arma::max(predictionTemp.col(i)) == predictionTemp.col(i), 1)) + 1;
+  }
+
+  size_t error = 0;
+  for (size_t i = 0; i < testData.n_cols; i++)
+  {
+    if (int(arma::as_scalar(prediction.col(i))) ==
+        int(arma::as_scalar(testLabels.col(i))))
+    {
+      error++;
+    }
+  }
+
+  double classificationError = 1 - double(error) / testData.n_cols;
+  BOOST_REQUIRE_LE(classificationError, classificationErrorThreshold);
+}
 
 BOOST_AUTO_TEST_CASE(SimpleCMAESTestFunction)
 {
@@ -102,9 +180,9 @@ BOOST_AUTO_TEST_CASE(LogisticRegressionTestWithCMAES)
   arma::mat start1(dim, 1); start1.fill(0.5);
   arma::mat initialStdDeviations1(dim, 1); initialStdDeviations1.fill(1.5);
 
-  CMAES test1(dim, start1, initialStdDeviations1, 50000, 1e-8);
+  CMAES test1(dim, start1, initialStdDeviations1, 50000, 1e-7);
 
-  LogisticRegression<arma::mat> lr(shuffledData, shuffledResponses, test1, 0.5);
+  LogisticRegression<> lr(shuffledData, shuffledResponses, test1, 0.5);
 
   // Ensure that the error is close to zero.
   const double acc = lr.ComputeAccuracy(data, responses);
@@ -112,5 +190,40 @@ BOOST_AUTO_TEST_CASE(LogisticRegressionTestWithCMAES)
 
   const double testAcc = lr.ComputeAccuracy(testData, testResponses);
   BOOST_REQUIRE_CLOSE(testAcc, 100.0, 0.6); // 0.6% error tolerance.
+}
+
+BOOST_AUTO_TEST_CASE(feedForwardNetworkCMAES)
+{
+  arma::mat irisTrainData;
+  data::Load("iris_train.csv", irisTrainData, true);
+ 
+ //normalize train data
+ double minVal=0,range=1;
+ for(int i=0; i<irisTrainData.n_rows; i++)
+ {
+   minVal = irisTrainData.row(i).min();
+   range  = irisTrainData.row(i).max() - minVal;
+   irisTrainData.row(i) =  (irisTrainData.row(i) - minVal)/range;
+ }
+
+ arma::mat irisTrainLabels;
+ data::Load("iris_train_labels.csv", irisTrainLabels, true);
+
+ arma::mat irisTestData;
+ data::Load("iris_test.csv", irisTestData, true);
+
+  //normalize test data
+ for(int i=0; i<irisTestData.n_rows; i++)
+ {
+  minVal = irisTestData.row(i).min();
+  range  = irisTestData.row(i).max() - minVal;
+  irisTestData.row(i) =  (irisTestData.row(i) - minVal)/range;
+ }
+
+   arma::mat irisTestLabels;
+    data::Load("iris_test_labels.csv", irisTestLabels, true);
+
+ BuildVanillaNetwork<>
+(irisTrainData, irisTrainLabels, irisTestData, irisTestLabels, 3, 8, 70, 0.1);
 }
 BOOST_AUTO_TEST_SUITE_END();
