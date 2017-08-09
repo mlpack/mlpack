0f90149c048e46fa8eb72019391ef3ad1212d371
Chenzhe <diao@ualberta.ca>
2017-08-09 11:53:47 -0600

    Changed the code for structured group constraint solver.

--- src/mlpack/core/optimizers/fw/constr_structure_group.hpp
+++ src/mlpack/core/optimizers/fw/constr_structure_group.hpp
@@ -35,22 +35,30 @@ namespace optimization {
  * class. Now, the norm defined for the original vector is:
  * \f[
  * ||v||_G := \min_{v_g} \sum_{g\in G} ||v_g||_g, \qquad
- * s.t. v = \sum_{g\in G} v_g
+ * s.t. \quad v = \sum_{g\in G} v_g
  * \f]
  * This norm is an atom norm, and the dual norm is given by
  * \f[
  * ||y||^*_G := \max_{g\in G} ||y_g||_g^*
  * \f]
  *
+ * See Jaggi's paper:
+ * @code
+ * @inproceedings{Jag:2013Revisiting,
+ *  Author = {Jaggi, Martin},
+ *  Booktitle = {ICML (1)},
+ *  Pages = {427--435},
+ *  Title = {Revisiting Frank-Wolfe: Projection-Free Sparse Convex Optimization.},
+ *  Year = {2013}}
+ * @endcode
  *
  *  For ConstrStrctGroupSolver to work, we need to use template class GroupType, 
  *  which gives functions:
  *
- *      int NumGroups();
- *      double DualNorm(const arma::vec& yk, const int group_ind)
- *	    arma::vec OptimalFromGroup(const arma::vec& yk, const size_t GroupId)
- *      arma::vec ProjectToGroup(const arma::vec& v, const size_t GroupId);
- *
+ *    size_t NumGroups();
+ *    double DualNorm(const arma::vec& yk, const int group_ind);
+ *    ProjectToGroup(const arma::mat& v, const size_t groupId, arma::vec& y);
+ *    void OptimalFromGroup(const arma::mat& v, const size_t groupId, arma::mat& s);
  *
  * @tparam GroupType Class that implements functions to map original vectors to
  *                   each group, and to solve linear optimization problem in the
@@ -63,113 +71,142 @@ class ConstrStructGroupSolver
   /**
    * Construct the structure group optimization solver.
    *
-   * @param group_extractor Class used to project to a group, recovery from a
-   * group, and compute norm in each group.
+   * @param groupExtractor Class used to project to a group, recovery from a
+   *                       group, and compute norm in each group.
    */
-  ConstrStructGroupSolver(GroupType& group_extractor) :
-    group_extractor(group_extractor)
+  ConstrStructGroupSolver(GroupType& groupExtractor) :
+    groupExtractor(groupExtractor)
   { /* Nothing to do */ }
 
   /**
    * Optimizer of structure group ball constrained Problem for FrankWolfe.
    *
    * @param v Input local gradient.
-   * @param s Output optimal solution in the constrained domain.
+   * @param s Output optimal solution in the constrained atom domain.
    */
   void Optimize(const arma::mat& v, arma::mat& s)
   {
-    int nGroups = group_extractor.NumGroups();
-    double dualnorm = 0;
-    int optimal_group = 1;
+    size_t nGroups = groupExtractor.NumGroups();
+    double dualNorm = 0;
+    size_t optimalGroup = 1;
 
-    // Find the optimal group
-    for (size_t i=1; i<= nGroups; ++i)
+    // Find the optimal group.
+    for (size_t i = 1; i <= nGroups; ++i)
     {
-      arma::mat y = group_extractor.ProjectToGroup(v, i);
-      double newnorm = group_extractor.DualNorm(y, i);
+      arma::vec y;
+      groupExtractor.ProjectToGroup(v, i, y);
+      double newNorm = groupExtractor.DualNorm(y, i);
 
       // Find the group with largest dual norm.
-      if (newnorm > dualnorm)
+      if (newNorm > dualNorm)
       {
-        optimal_group = i;
-        dualnorm = newnorm;
+        optimalGroup = i;
+        dualNorm = newNorm;
       }
     }
 
-    arma::mat y_opt = group_extractor.ProjectToGroup(optimal_group);
-    s = group_extractor.OptimalFromGroup(y_opt, optimal_group);
-
+    groupExtractor.OptimalFromGroup(v, optimalGroup, s);
   }
 
  private:
-  GroupType& group_extractor;
+  //! Information and methods for groups.
+  GroupType& groupExtractor;
 };
 
 /**
  * Implementation of Structured Group. The projection to each group is using
  * restriction of vector support here, and the norm in each group is using lp
  * norm.
- *
  */
 class GroupLpBall
 {
  public:
+  /**
+   * Construct the lp ball group extractor class.
+   *
+   * @param p lp ball.
+   * @param dimOrig dimension of the original vector.
+   * @param groupIndicesList vector of support indices lists of each group.
+   */
   GroupLpBall(const double p,
-              const int dimOrig,
-              std::vector<arma::uvec> groupIndList):
-    p(p), numGroups(groupIndList.size()),
+              const size_t dimOrig,
+              std::vector<arma::uvec> groupIndicesList):
+    p(p), numGroups(groupIndicesList.size()),
     dimOrig(dimOrig),
-    groupIndList(groupIndList),
-    lpBallGroup(p)
+    groupIndicesList(groupIndicesList),
+    lpBallSolver(p)
   {/* Nothing to do. */}
 
-  arma::vec ProjectToGroup(const arma::vec& v, const size_t groupId)
+  /**
+   * Projection to specific group.
+   *
+   * @param v input vector to be projected.
+   * @param groupId input ID number of the group, start from 1.
+   * @param y output projection of the vector to specific group.
+   */
+  void ProjectToGroup(const arma::mat& v, const size_t groupId, arma::vec& y)
   {
-    arma::uvec& indList = groupIndList[groupId-1];
+    arma::uvec& indList = groupIndicesList[groupId - 1];
     size_t dim = indList.n_elem;
-    arma::vec y(dim);
+    y.set_size(dim);
 
     for (size_t i = 0; i < dim; ++i)
-    {
       y(i) = v(indList(i));
-    }
-
-    return y;
   }
 
-  arma::vec OptimalFromGroup(const arma::vec& yk, const size_t groupId)
+  /**
+   * Get optimal atom, which belongs to specific group.
+   * See Jaggi's paper for details.
+   *
+   * @param v input gradient vector.
+   * @param groupId optimal atom belongs to this group.
+   * @param s output optimal atom.
+   */
+  void OptimalFromGroup(const arma::mat& v, const size_t groupId, arma::mat& s)
   {
-    arma::vec sProj;
-    lpBallGroup.Optimize(yk, sProj);
+    // Project v to group.
+    arma::vec yk;
+    ProjectToGroup(v, groupId, yk);
 
-    arma::uvec& indList = groupIndList[groupId-1];
-    size_t dim = indList.n_elem;
-    arma::vec s = arma::zeros<arma::vec>(dimOrig);
+    // Optimize in this group.
+    arma::vec sProj(yk.n_elem);
+    lpBallSolver.Optimize(yk, sProj);
+
+    // Recover s to the original dimension.
+    arma::uvec& indList = groupIndicesList[groupId - 1];
+    size_t dim = indList.n_elem;  // dimension of the group.
+    s.zeros(dimOrig, 1);
 
     for (size_t i = 0; i < dim; ++i)
-    {
       s(indList(i)) = sProj(i);
-    }
-    return s;
   }
-    
-  int NumGroups() const {return numGroups;}
-  int& NumGroups() {return numGroups;}
-    
-  // group_ind start from 1
-  double DualNorm(const arma::vec& yk, const int groupInd)
+
+  //! Get the number of groups.
+  size_t NumGroups() const {return numGroups;}
+  //! Modify the number of groups.
+  size_t& NumGroups() {return numGroups;}
+
+  /**
+   * Compute the q-norm of yk, 1/p+1/q=1.
+   *
+   * @param yk compute the q-norm of yk.
+   * @param groupId group ID number.
+   */
+  double DualNorm(const arma::vec& yk, const int groupId)
   {
-    if (p == std::numeric_limits<double>::infinity()) {
+    if (p == std::numeric_limits<double>::infinity())
+    {
       // inf-norm, return 1-norm
-      return sum(abs(yk));
+      return arma::norm(yk, 1);
     }
     else if (p > 1.0) {
       // p norm, return q-norm
-      double q = 1 / (1 - 1/p);
-      return  std::pow(sum(pow(abs(yk), q)), 1/q);
+      double q = 1.0 / (1.0 - 1.0/p);
+      return  arma::norm(yk, q);
     }
     else if (p == 1.0) {
-      return max(abs(yk));
+      // 1-norm, return inf-norm
+      return arma::norm(yk, "inf");
     }
     else {
       Log::Fatal << "Wrong norm p!" << std::endl;
@@ -183,16 +220,16 @@ class GroupLpBall
   double p;
 
   //! Number of groups.
-  int numGroups;
+  size_t numGroups;
 
   //! Original Problem Dimension.
-  int dimOrig;
+  size_t dimOrig;
 
   //! Indices list of each group, indices start from 0.
-  std::vector<arma::uvec> groupIndList;
+  std::vector<arma::uvec> groupIndicesList;
 
-  //! Each group uses a lp norm
-  ConstrLpBallSolver lpBallGroup;
+  //! Each group uses lp norm
+  ConstrLpBallSolver lpBallSolver;
 };
 
 
