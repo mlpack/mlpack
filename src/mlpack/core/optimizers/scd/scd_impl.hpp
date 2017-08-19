204a80dfb7a85b64f080ac4547441f27448c53e5
Shikhar Bhardwaj <shikharbhardwaj68@gmail.com>
2017-08-19 19:47:08 +0530

    Modify SCD to update only relavant part of the decision variable.

--- src/mlpack/core/optimizers/scd/scd_impl.hpp
+++ src/mlpack/core/optimizers/scd/scd_impl.hpp
@@ -53,7 +53,7 @@ double SCD<DescentPolicyType>::Optimize(ResolvableFunctionType& function,
     function.FeatureGradient(iterate, featureIdx, gradient);
 
     // Update the decision variable with the partial gradient.
-    iterate -= stepSize * gradient;
+    iterate.col(featureIdx) -= stepSize * gradient.col(featureIdx);
 
     // Check for convergence.
     if (i % updateInterval == 0)
