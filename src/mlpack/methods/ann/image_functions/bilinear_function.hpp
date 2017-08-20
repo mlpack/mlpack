18d81513c75444d1d03009d3521935d52d86fdab
kris-singh <krishnakant.singh@suiit.ac.in>
2017-08-20 21:05:21 +0530

    Fix indexing in image

--- src/mlpack/methods/ann/image_functions/bilinear_function.hpp
+++ src/mlpack/methods/ann/image_functions/bilinear_function.hpp
@@ -47,8 +47,45 @@ class BiLinearFunction
     if (output.is_empty() && (output.n_cols != outColSize || output.n_rows != outRowSize))
       output = arma::resize(output, outRowSize, outColSize);
 
-    scaleRow = (double)input.n_rows / output.n_rows;
-    scaleCol = (double)input.n_cols / output.n_cols;
+    scaleRow = (double)input.n_rows / (double) output.n_rows;
+    scaleCol = (double)input.n_cols / (double) output.n_cols;
+
+    for (size_t i = 0; i < output.n_rows; i++)
+    {
+      for (size_t j = 0; j < output.n_cols; j++)
+      {
+        r_origin = std::floor(i * scaleRow);
+        c_origin = std::floor(j * scaleCol);
+
+        if (r_origin > input.n_rows - 2)
+          r_origin = input.n_rows - 2;
+        if (c_origin > input.n_cols - 2)
+          c_origin = input.n_cols - 2;
+
+        double deltaR = i * scaleRow - r_origin;
+        double deltaC = j * scaleCol - c_origin;
+        coff1 = (1 - deltaC) * (1 - deltaC);
+        coff2 = deltaR * (1 - deltaC);
+        coff3 = (1 - deltaR) * deltaC;
+        coff4 = deltaR * deltaC;
+
+        output(i, j) =  input(c_origin * input.n_rows + r_origin) * coff1 +
+                        input(c_origin * input.n_rows + r_origin + 1) * coff2 +
+                        input((c_origin + 1) * input.n_rows + r_origin) * coff3 +
+                        input((c_origin + 1) * input.n_rows + r_origin+1) * coff4;
+      }
+    }
+  }
+
+  template<typename eT>
+  void DownSample(const arma::Mat<eT>& input, arma::Mat<eT>& output)
+  {
+
+    if (output.is_empty() && (output.n_cols != inColSize || output.n_rows != inRowSize))
+      output = arma::resize(output, inRowSize, inColSize);
+
+    scaleRow = (double)(input.n_rows - 1) / output.n_rows;
+    scaleCol = (double)(input.n_cols - 1) / output.n_cols;
 
     for (size_t i = 0; i < output.n_rows; i++)
       for (size_t j = 0; j < output.n_cols; j++)
@@ -67,45 +104,11 @@ class BiLinearFunction
         coff2 = deltaR * (1 - deltaC);
         coff3 = (1 - deltaR) * deltaC;
         coff4 = deltaR * deltaC;
-        eT temp = input(c_origin * input.n_rows + r_origin );
-        output(i, j) =  temp * coff1 + temp * coff2 + temp * coff3 +
-                        temp * coff4;
-      }
-  }
 
-  template<typename eT>
-  void DownSample(const arma::Mat<eT>& input, arma::Mat<eT>& output)
-  {
-    arma::Mat<eT> tempInput = input;
-    if (output.is_empty() && (output.n_cols != inColSize || output.n_rows != inRowSize))
-      output = arma::resize(output, inRowSize, inColSize);
-    // Get dimensions.
-    if (input.n_rows != outRowSize || input.n_cols != outColSize)
-      tempInput.reshape(outRowSize, outColSize);
-
-    scaleRow = (double)(input.n_rows - 1) / output.n_rows;
-    scaleCol = (double)(input.n_cols - 1) / output.n_cols;
-
-    for (size_t i = 0; i < output.n_rows; i++)
-      for (size_t j = 0; j < output.n_cols; j++)
-      {
-        r_origin = std::floor(i * scaleRow);
-        c_origin = std::floor(j * scaleCol);
-
-        if (r_origin > tempInput.n_rows - 2)
-          r_origin = tempInput.n_rows - 2;
-        if (c_origin > tempInput.n_cols - 2)
-          c_origin = tempInput.n_cols - 2;
-
-        double deltaR = i * scaleRow - r_origin;
-        double deltaC = j * scaleCol - c_origin;
-        coff1 = (1 - deltaC) * (1 - deltaC);
-        coff2 = deltaR * (1 - deltaC);
-        coff3 = (1 - deltaR) * deltaC;
-        coff4 = deltaR * deltaC;
-        eT temp = input(c_origin * input.n_rows + r_origin );
-        output(i, j) =  temp * coff1 + temp * coff2 + temp * coff3 +
-                        temp * coff4;
+        output(i, j) =  input(c_origin * input.n_rows + r_origin) * coff1 +
+                        input(c_origin * input.n_rows + r_origin + 1) * coff2 +
+                        input((c_origin + 1) * input.n_rows + r_origin) * coff3 +
+                        input((c_origin + 1) * input.n_rows + r_origin+1) * coff4;
       }
   }
 
