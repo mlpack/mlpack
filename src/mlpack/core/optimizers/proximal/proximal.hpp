dc8f3b768af53c12dbca485680e7dc797c7e9285
Chenzhe Diao <diao@ualberta.ca>
2017-08-27 20:57:59 -0600

    Fixed regspace and static proximal methods.

--- src/mlpack/core/optimizers/proximal/proximal.hpp
+++ src/mlpack/core/optimizers/proximal/proximal.hpp
@@ -26,18 +26,6 @@ namespace optimization {
  */
 class Proximal {
  public:
-  /** 
-   * Constructor of the proximal class.
-   *
-   * @param tau norm of lp ball.
-   */
-  Proximal(double tau);
-
-  //! Get the norm of the lp ball.
-  double Tau() const { return tau; }
-  //! Modify the norm of the lp ball.
-  double& Tau() { return tau; }
-
   /**
    * Project the vector onto the l1 ball with norm tau. That is, we will solve
    * for:
@@ -48,7 +36,7 @@ class Proximal {
    * @param v Input vector to be approxmated, the output optimal vector is
    *          also saved in v.
    */
-  void ProjectToL1Ball(arma::vec& v);
+  static void ProjectToL1Ball(arma::vec& v, double tau);
 
   /**
    * Project the vector onto the l0 ball with norm tau. That is, we try to
@@ -60,11 +48,7 @@ class Proximal {
    * @param v Input vector to be approxmated, the output optimal vector is
    *          also saved in v.
    */
-  void ProjectToL0Ball(arma::vec& v);
-
- private:
-  //! Norm of lp ball. Input an integer if you are using l0 ball.
-  double tau;
+  static void ProjectToL0Ball(arma::vec& v, int tau);
 };  // class Proximal
 
 } // namespace optimization
