8b5765bb3532f70778629425751cfa0927bb2838
Ryan Curtin <ryan@ratml.org>
2017-10-09 11:00:35 -0400

    Fix style and static analysis issues.

--- src/mlpack/methods/rann/ra_util.cpp
+++ src/mlpack/methods/rann/ra_util.cpp
@@ -30,11 +30,6 @@ size_t mlpack::neighbor::RAUtil::MinimumSamplesReqd(const size_t n,
   double prob;
   Log::Assert(alpha <= 1.0);
 
-  // going through all values of sample sizes
-  // to find the minimum samples required to satisfy the
-  // desired bound
-  bool done = false;
-
   // This performs a binary search on the integer values between 'lb = k'
   // and 'ub = n' to find the minimum number of samples 'm' required to obtain
   // the desired success probability 'alpha'.
@@ -69,7 +64,7 @@ size_t mlpack::neighbor::RAUtil::MinimumSamplesReqd(const size_t n,
       }
     }
     m = (ub + lb) / 2;
-  } while (!done);
+  } while (true);
 
   return (std::min(m + 1, n));
 }
