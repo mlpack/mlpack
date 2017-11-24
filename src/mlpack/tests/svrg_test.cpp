a12f3f5dec9db27d40942cea54e0ffc7161ae715
Marcus Edel <marcus.edel@fu-berlin.de>
2017-11-24 22:37:53 +0100

    Add SVRG and SVRG-BB test case.

---
+++ src/mlpack/tests/svrg_test.cpp
@@ -0,0 +1,141 @@
+/**
+ * @file svrg_test.cpp
+ * @author Marcus Edel
+ *
+ * Test file for SVRG.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#include <mlpack/core.hpp>
+#include <mlpack/core/optimizers/svrg/svrg.hpp>
+#include <mlpack/core/optimizers/katyusha/katyusha.hpp>
+
+#include <mlpack/core/optimizers/lbfgs/test_functions.hpp>
+#include <mlpack/core/optimizers/sgd/test_function.hpp>
+
+#include <mlpack/methods/logistic_regression/logistic_regression.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include "test_tools.hpp"
+
+using namespace std;
+using namespace arma;
+using namespace mlpack;
+using namespace mlpack::optimization;
+using namespace mlpack::optimization::test;
+
+using namespace mlpack::distribution;
+using namespace mlpack::regression;
+
+BOOST_AUTO_TEST_SUITE(SVRGTest);
+
+/**
+ * Create the data for the logistic regression test case.
+ */
+void CreateLogisticRegressionTestData(arma::mat& data,
+                                      arma::mat& testData,
+                                      arma::mat& shuffledData,
+                                      arma::Row<size_t>& responses,
+                                      arma::Row<size_t>& testResponses,
+                                      arma::Row<size_t>& shuffledResponses)
+{
+  // Generate a two-Gaussian dataset.
+  GaussianDistribution g1(arma::vec("1.0 1.0 1.0"), arma::eye<arma::mat>(3, 3));
+  GaussianDistribution g2(arma::vec("9.0 9.0 9.0"), arma::eye<arma::mat>(3, 3));
+
+  data = arma::mat(3, 1000);
+  responses = arma::Row<size_t>(1000);
+  for (size_t i = 0; i < 500; ++i)
+  {
+    data.col(i) = g1.Random();
+    responses[i] = 0;
+  }
+  for (size_t i = 500; i < 1000; ++i)
+  {
+    data.col(i) = g2.Random();
+    responses[i] = 1;
+  }
+
+  // Shuffle the dataset.
+  arma::uvec indices = arma::shuffle(arma::linspace<arma::uvec>(0,
+      data.n_cols - 1, data.n_cols));
+  shuffledData = arma::mat(3, 1000);
+  shuffledResponses = arma::Row<size_t>(1000);
+  for (size_t i = 0; i < data.n_cols; ++i)
+  {
+    shuffledData.col(i) = data.col(indices[i]);
+    shuffledResponses[i] = responses[indices[i]];
+  }
+
+  // Create a test set.
+  testData = arma::mat(3, 1000);
+  testResponses = arma::Row<size_t>(1000);
+  for (size_t i = 0; i < 500; ++i)
+  {
+    testData.col(i) = g1.Random();
+    testResponses[i] = 0;
+  }
+  for (size_t i = 500; i < 1000; ++i)
+  {
+    testData.col(i) = g2.Random();
+    testResponses[i] = 1;
+  }
+}
+
+/**
+ * Run SVRG on logistic regression and make sure the results are acceptable.
+ */
+BOOST_AUTO_TEST_CASE(SVRGLogisticRegressionTest)
+{
+  arma::mat data, testData, shuffledData;
+  arma::Row<size_t> responses, testResponses, shuffledResponses;
+
+  CreateLogisticRegressionTestData(data, testData, shuffledData,
+      responses, testResponses, shuffledResponses);
+
+  // Now run big-batch SGD with a couple of batch sizes.
+  for (size_t batchSize = 35; batchSize < 50; batchSize += 5)
+  {
+    SVRG optimizer(0.001, batchSize, 15000, 1e-3, true);
+    LogisticRegression<> lr(shuffledData, shuffledResponses, optimizer, 0.5);
+
+    // Ensure that the error is close to zero.
+    const double acc = lr.ComputeAccuracy(data, responses);
+    BOOST_REQUIRE_CLOSE(acc, 100.0, 1.5); // 1.5% error tolerance.
+
+    const double testAcc = lr.ComputeAccuracy(testData, testResponses);
+    BOOST_REQUIRE_CLOSE(testAcc, 100.0, 1.5); // 1.5% error tolerance.
+  }
+}
+
+/**
+ * Run SVRG_BB on logistic regression and make sure the results are acceptable.
+ */
+BOOST_AUTO_TEST_CASE(SVRGBBLogisticRegressionTest)
+{
+  arma::mat data, testData, shuffledData;
+  arma::Row<size_t> responses, testResponses, shuffledResponses;
+
+  CreateLogisticRegressionTestData(data, testData, shuffledData,
+      responses, testResponses, shuffledResponses);
+
+  // Now run big-batch SGD with a couple of batch sizes.
+  for (size_t batchSize = 35; batchSize < 50; batchSize += 5)
+  {
+    SVRG_BB optimizer(0.001, batchSize, 1500, 1e-5, true,
+        SVRGUpdate(), BarzilaiBorwein(0.1));
+    LogisticRegression<> lr(shuffledData, shuffledResponses, optimizer, 0.5);
+
+    // Ensure that the error is close to zero.
+    const double acc = lr.ComputeAccuracy(data, responses);
+    BOOST_REQUIRE_CLOSE(acc, 100.0, 1.5); // 1.5% error tolerance.
+
+    const double testAcc = lr.ComputeAccuracy(testData, testResponses);
+    BOOST_REQUIRE_CLOSE(testAcc, 100.0, 1.5); // 1.5% error tolerance.
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END();
