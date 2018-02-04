01e487aad554110787a5a0d5a3b83c56e1b3dac6
KARTHEEKCIC <bkartheekreddy@gmail.com>
2018-02-04 23:39:50 +0530

    adding more style fixes

--- src/mlpack/methods/logistic_regression/logistic_regression_main.cpp
+++ src/mlpack/methods/logistic_regression/logistic_regression_main.cpp
@@ -237,7 +237,8 @@ static void mlpackMain()
   else if (CLI::HasParam("training"))
   {
     // Checking the size of training data if no labels are passed 
-    if (regressors.n_rows < 2) {
+    if (regressors.n_rows < 2) 
+    {
       Log::Fatal << "Can't get responses from training data "
             "since it has less than 2 rows." << endl;
     }
@@ -286,7 +287,8 @@ static void mlpackMain()
     testSet = std::move(CLI::GetParam<arma::mat>("test"));
 
     // checking the dimensionality of the test data
-    if (testSet.n_rows != model.Parameters().n_cols-1) {
+    if (testSet.n_rows != model.Parameters().n_cols-1)
+    {
       Log::Fatal << "Test data dimensionality (" << testSet.n_rows << ") must " 
           << "be the same as the dimensionality of the Training Data (" << model.Parameters().n_cols-1 << ")!" << endl;
     }
