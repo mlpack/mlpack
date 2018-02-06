7195837c3120d0ebbe520bda666ad1842d179406
Rajiv Vaidyanathan <rajiv.vaidyanathan4@gmail.com>
2018-02-07 00:39:51 +0530

    Deported all codes from spsa_update.hpp to spsa.hpp

--- src/mlpack/core/optimizers/spsa/spsa.hpp
+++ src/mlpack/core/optimizers/spsa/spsa.hpp
@@ -2,7 +2,7 @@
  * @file spsa_update.hpp
  * @author N Rajiv Vaidyanathan
  *
- * SPSA update for Stochastic Gradient Descent.
+ * SPSA update for faster convergence.
  *
  * mlpack is free software; you may redistribute it and/or modify it under the
  * terms of the 3-clause BSD license.  You should have received a copy of the
@@ -14,7 +14,7 @@
 
 #include <mlpack/prereqs.hpp>
 #include <mlpack/core/optimizers/sgd/sgd.hpp>
-#include "spsa_update.hpp"
+//#include "spsa_update.hpp"
 
 namespace mlpack {
 namespace optimization {
@@ -42,30 +42,59 @@ namespace optimization {
 class SPSA
 {
  public:
+  float a;
+  float A;
+  float alpha;
+  float gamma;
+  float c;
+  float ak;
+  float ck;
+  size_t max_iter;
+  arma::vec sp_vector;
   SPSA(const float& alpha = 0.602,
        const float& gamma = 0.101,
        const float& a = 1e-6,
        const float& c = 0.01,
-       const size_t& maxIterations = 100000)
+       const size_t& maxIterations = 100000):
+    alpha(alpha),
+    gamma(gamma),
+    a(a),
+    c(c),
+    A(0.1*a),
+    max_iter(maxIterations)
   {
-    spsaUpdate = new SPSAUpdate(alpha, gamma, a, c,
-                      maxIterations);
+/*    spsaUpdate = new SPSAUpdate(alpha, gamma, a, c,
+                      maxIterations);*/
   }
 
-  ~SPSA()
+/*  ~SPSA()
   {
     delete spsaUpdate;
-  }
+  }*/
 
   template<typename DecomposableFunctionType>
   void Optimize(DecomposableFunctionType& function, arma::mat& iterate)
   {
-    spsaUpdate->Update(iterate, function);
+    //spsaUpdate->Update(iterate, function);
+    const int s = iterate.n_elem;
+
+    for (size_t i = 0; i < max_iter; i++)
+    {
+      sp_vector = arma::conv_to<arma::vec>::from(
+                  randi(s, arma::distr_param(-1, 1)));
+      ak = a/pow((max_iter + 1 + A), alpha);
+      ck = c/pow((max_iter + 1), gamma);
+      arma::vec f_plus = function.Evaluate(iterate + ck*sp_vector, s);
+      arma::vec f_minus = function.Evaluate(iterate - ck*sp_vector, s);
+      arma::mat gradient = ((f_plus - f_minus)/2)*ck*sp_vector.i();
+
+      iterate -= ak*gradient;
+    }
   }
 
- private:
+// private:
   //! The SPSA Descent object pointer.
-  SPSAUpdate *spsaUpdate;
+  //SPSAUpdate *spsaUpdate;
 };
 
 } // namespace optimization
--- src/mlpack/core/optimizers/spsa/spsa_update.hpp
+++ src/mlpack/core/optimizers/spsa/spsa_update.hpp
@@ -2,7 +2,7 @@
  * @file spsa_update.hpp
  * @author N Rajiv Vaidyanathan
  *
- * SPSA update for Stochastic Gradient Descent.
+ * SPSA update function.
  *
  * mlpack is free software; you may redistribute it and/or modify it under the
  * terms of the 3-clause BSD license.  You should have received a copy of the
@@ -44,51 +44,36 @@ class SPSAUpdate
        const float& gamma,
        const float& a,
        const float& c,
-       const size_t& maxIterations):
-    alpha(alpha),
-    gamma(gamma),
-    a(a),
-    c(c),
-    max_iter(maxIterations)
+       const size_t& maxIterations)
   {
     Initialize();
   }
 
   void Initialize()
   {
-    A = 0.1*a;
-    ak = a/pow((max_iter + 1 + A), alpha);
-    ck = c/pow((max_iter + 1), gamma);
   }
 
   template<typename DecomposableFunctionType>
   void Update(arma::mat& iterate,
           DecomposableFunctionType& function)
   {
-    const int s = iterate.n_elem;
+/*    const int s = iterate.n_elem;
 
     for (size_t i = 0; i < max_iter; i++)
     {
       sp_vector = arma::conv_to<arma::vec>::from(
                   randi(s, arma::distr_param(-1, 1)));
+      ak = a/pow((max_iter + 1 + A), alpha);
+      ck = c/pow((max_iter + 1), gamma);
       arma::vec f_plus = function.Evaluate(iterate + ck*sp_vector, s);
       arma::vec f_minus = function.Evaluate(iterate - ck*sp_vector, s);
       arma::mat gradient = ((f_plus - f_minus)/2)*ck*sp_vector.i();
 
       iterate -= ak*gradient;
-    }
+    }*/
   }
 
  private:
-  float a;
-  float A;
-  float alpha;
-  float gamma;
-  float c;
-  float ak;
-  float ck;
-  size_t max_iter;
-  arma::vec sp_vector;
 };
 
 } // namespace optimization
