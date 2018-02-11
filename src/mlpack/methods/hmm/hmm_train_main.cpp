ece3e0dcce5f3099dd0b1ed5ae15f49924191fd8
daivik <daivnema@in.ibm.com>
2018-02-11 19:03:23 +0000

    Add check for tolerance >= 0 in hmm_train_main.cpp

--- src/mlpack/methods/hmm/hmm_train_main.cpp
+++ src/mlpack/methods/hmm/hmm_train_main.cpp
@@ -379,6 +379,12 @@ static void mlpackMain()
         "unknown HMM type");
   }
 
+  if (CLI::HasParam("tolerance"))
+  {
+    RequireParamValue<int>("tolerance", [](int x) { return x >= 0; }, true,
+        "tolerance must be non-negative");
+  }
+
   // Load the input data.
   vector<mat> trainSeq;
   if (batch)
