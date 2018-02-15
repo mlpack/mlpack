5359e4f9c764a66435ce517b4361b58dd76d4068
Manish <manish887kr@gmail.com>
2018-02-15 21:36:12 +0530

    Set leaf_size to 1

--- src/mlpack/tests/knn_test.cpp
+++ src/mlpack/tests/knn_test.cpp
@@ -1443,8 +1443,9 @@ BOOST_AUTO_TEST_CASE(GreedyTreeSearch)
   // Initalize dataset.
   arma::mat dataset = arma::randu<arma::mat>(3, 100);
 
+  // Build tree with a leaf_size of 1.
   KDTree<EuclideanDistance, NeighborSearchStat<NearestNeighborSort>,
-      arma::mat> tree(dataset);
+      arma::mat> tree(dataset, 1);
 
   NeighborSearch<NearestNeighborSort, LMetric<2>, arma::mat, KDTree>
       greedyTreeSearch(std::move(tree), GREEDY_SINGLE_TREE_MODE);
@@ -1455,15 +1456,13 @@ BOOST_AUTO_TEST_CASE(GreedyTreeSearch)
   // Search for 5 nearest neighbours.
   greedyTreeSearch.Search(5, neighbors, distances);
 
-  // Check that all neighbour values are between 0 and 100 as only 100 points
-  // are present in dataset, hence in total we would be having 500 values.
-  BOOST_REQUIRE_EQUAL(arma::accu(neighbors >= 0 && neighbors <100),
-                      500);
+  // Check that all neighbour values are between 0 and 100, as only 100 points
+  // are present in dataset.
+  BOOST_REQUIRE_EQUAL(arma::accu(neighbors < 0 || neighbors >= 100), 0);
 
-  // Check that all distances values are between 0 and 1.0 as arma::randu
+  // Check that all distances values are between 0.0 and 1.0 as arma::randu
   // generates a uniform distribution in [0, 1].
-  BOOST_REQUIRE_EQUAL(arma::accu(distances >= 0 && distances <= 1.0),
-                      500);
+  BOOST_REQUIRE_EQUAL(arma::accu(distances < 0.0 || distances > 1.0), 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
