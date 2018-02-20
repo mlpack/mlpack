11877f90ee594d7a9946c44c3f4a48ca3bed5f9d
Adeel Ahmad <adeelahmad14@hotmail.com>
2018-02-20 19:44:18 +0500

    Test the PSO method with Matyas, Booth, McCormick, and Eggholder function

--- src/mlpack/tests/pso_test.cpp
+++ src/mlpack/tests/pso_test.cpp
@@ -13,6 +13,11 @@
 #include <mlpack/core/optimizers/pso/pso.hpp>
 #include <mlpack/core/optimizers/pso/test_function.hpp>
 #include <mlpack/core/optimizers/problems/rosenbrock_function.cpp>
+#include <mlpack/core/optimizers/problems/matyas_function.cpp>
+#include <mlpack/core/optimizers/problems/booth_function.cpp>
+#include <mlpack/core/optimizers/problems/wood_function.cpp>
+#include <mlpack/core/optimizers/problems/mc_cormick_function.cpp>
+#include <mlpack/core/optimizers/problems/eggholder_function.cpp>
 
 #include <boost/test/unit_test.hpp>
 
@@ -41,7 +46,7 @@ BOOST_AUTO_TEST_CASE(SimpleTest)
 /**
  * Test for minimizing the Rosenbrock function.
  */
-BOOST_AUTO_TEST_CASE(RosenbrockTest)
+BOOST_AUTO_TEST_CASE(RosenbrockFunctionTest)
 {
   PSO optimizer;
   RosenbrockFunction f;
@@ -53,6 +58,73 @@ BOOST_AUTO_TEST_CASE(RosenbrockTest)
   double result = optimizer.Optimize(f, iterate);
 
   BOOST_REQUIRE_SMALL(result, 1e-5);
+}
+
+/**
+ * Test for minimizing Matyas function.
+ */
+BOOST_AUTO_TEST_CASE(MatyasFunctionTest)
+{
+  PSO optimizer;
+  MatyasFunction f;
+
+  arma::mat iterate;
+  iterate << 0.5828 << 0.0654 << 0.8817 << 0.3461 << arma::endr
+          << 0.2351 << 0.0034 << 0.1641 << 0.7621;
+
+  double result = optimizer.Optimize(f, iterate);
+
+  BOOST_REQUIRE_SMALL(result, 1e-5);
+}
+
+/**
+ * Test for minimizing the Booth function.
+ */
+BOOST_AUTO_TEST_CASE(BoothFunctionTest)
+{
+  PSO optimizer(60, 0.9, 0.5, 0.3, 200, 1e-3);
+  BoothFunction f;
+
+  arma::mat iterate;
+
+  iterate << 0.5828 << 0.0654 << 0.8817 << 0.3461 << arma::endr
+          << 0.2351 << 0.0034 << 0.1641 << 0.7621;
+
+  double result = optimizer.Optimize(f, iterate);
+
+  BOOST_REQUIRE_SMALL(result, 1e-3);
+}
+
+/**
+ * Test for the McCormick function.
+ */
+BOOST_AUTO_TEST_CASE(McCormickTest)
+{
+  PSO optimizer;
+  McCormickFunction f;
+
+  arma::mat iterate;
+  iterate << -0.54719 << -1.54719;
+
+  double result = optimizer.Optimize(f, iterate);
+
+  BOOST_TEST(result == -1.9133, boost::test_tools::tolerance(1e-3));
+}
+
+/**
+ * Test for the Eggholder function.
+ */
+BOOST_AUTO_TEST_CASE(EggholderFunctionTest)
+{
+  PSO optimizer;
+  EggholderFunction f;
+
+  arma::mat iterate;
+  iterate << 512 << 404.2319;
+
+  double result = optimizer.Optimize(f, iterate);
+
+  BOOST_TEST(result == -959.6407, boost::test_tools::tolerance(1e-3));
 }
 
 BOOST_AUTO_TEST_SUITE_END();
