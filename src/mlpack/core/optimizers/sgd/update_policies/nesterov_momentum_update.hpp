b624518249b4f19e5e8adf447ce0883f41e43d48
Sourabh Varshney <sourabhvarshney111@gmail.com>
2018-02-25 22:06:12 +0530

    Added base declaration

---
+++ src/mlpack/core/optimizers/sgd/update_policies/nesterov_momentum_update.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file nesterov_momentum_update.hpp
+ * @author Sourabh Varshney
+ *
+ * Nesterov Momentum Update for Stochastic Gradient Descent.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_CORE_OPTIMIZERS_SGD_NESTEROV_MOMENTUM_UPDATE_HPP
+#define MLPACK_CORE_OPTIMIZERS_SGD_NESTEROV_MOMENTUM_UPDATE_HPP
+
+#include <mlpack/prereqs.hpp>
+
+namespace mlpack {
+namespace optimization {
+
+/*
+ */
+
+
+} // namespace optimization
+} // namespace mlpack
+
+#endif
