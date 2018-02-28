a4f2ed529ebb4a9e2201e4c6661e6db05e28d12a
dakshitagrawal97 <neelagrawal08@gmail.com>
2018-02-28 22:33:21 +0530

    add lecun normal initialization tests

--- src/mlpack/tests/init_rules_test.cpp
+++ src/mlpack/tests/init_rules_test.cpp
@@ -24,6 +24,7 @@
 #include <mlpack/methods/ann/init_rules/const_init.hpp>
 #include <mlpack/methods/ann/init_rules/gaussian_init.hpp>
 #include <mlpack/methods/ann/init_rules/he_init.hpp>
+#include <mlpack/methods/ann/init_rules/lecun_normal_init.hpp>
 
 #include <boost/test/unit_test.hpp>
 #include "test_tools.hpp"
@@ -210,10 +211,35 @@ BOOST_AUTO_TEST_CASE(HeInitTest)
   arma::mat weights;
   arma::cube weights3d;
 
-  HeInitialization initialization;
+  HeInitialization initializer;
 
-  initialization.Initialize(weights, rows, cols);
-  initialization.Initialize(weights3d, rows, cols, slices);
+  initializer.Initialize(weights, rows, cols);
+  initializer.Initialize(weights3d, rows, cols, slices);
+
+  BOOST_REQUIRE_EQUAL(weights.n_rows, rows);
+  BOOST_REQUIRE_EQUAL(weights.n_cols, cols);
+
+  BOOST_REQUIRE_EQUAL(weights3d.n_rows, rows);
+  BOOST_REQUIRE_EQUAL(weights3d.n_cols, cols);
+  BOOST_REQUIRE_EQUAL(weights3d.n_slices, slices);
+}
+
+/**
+* Simple test of the LecunNormalInitialization class.
+*/
+BOOST_AUTO_TEST_CASE(LecunNormalInitTest)
+{
+  const size_t rows = 4;
+  const size_t cols = 4;
+  const size_t slices = 2;
+
+  arma::mat weights;
+  arma::cube weights3d;
+
+  LecunNormalInitialization initializer;
+
+  initializer.Initialize(weights, rows, cols);
+  initializer.Initialize(weights3d, rows, cols, slices);
 
   BOOST_REQUIRE_EQUAL(weights.n_rows, rows);
   BOOST_REQUIRE_EQUAL(weights.n_cols, cols);
