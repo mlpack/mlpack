184931a9586cd98b548f20118edba664da3154c7
akhandait <akhandait45@gmail.com>
2018-03-06 14:32:18 +0530

    wasn't in sync with master, some manual adjustments

--- src/mlpack/methods/neighbor_search/kfn_main.cpp
+++ src/mlpack/methods/neighbor_search/kfn_main.cpp
@@ -261,14 +261,23 @@ static void mlpackMain()
           << queryData.n_rows << "x" << queryData.n_cols << ")." << endl;
     }
 
-    // Sanity check on k value: must be greater than 0, must be less than the
-    // number of reference points.  Since it is unsigned, we only test the upper
-    // bound.
+    // Sanity check on k value: must be greater than 0, must be less than or
+    // equal to the number of reference points.  Since it is unsigned,
+    // we only test the upper bound.
     if (k > kfn->Dataset().n_cols)
     {
       Log::Fatal << "Invalid k: " << k << "; must be greater than 0 and less "
           << "than or equal to the number of reference points ("
           << kfn->Dataset().n_cols << ")." << endl;
+    }
+    
+    // Sanity check on k value: must not be equal to the number of reference
+    // points when query data has not been provided.
+    if (!CLI::HasParam("query") && k == kfn->Dataset().n_cols)
+    {
+      Log::Fatal << "Invalid k: " << k << "; must be less than the number of "
+          << "reference points (" << kfn->Dataset().n_cols << ") "
+          << "if query data has not been provided." << endl;
     }
 
     // Now run the search.
