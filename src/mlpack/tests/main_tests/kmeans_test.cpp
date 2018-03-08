47f5a2d0682975b77c5c213e09708a3ac231510a
Prabhat <prabhatsharma7298@gmail.com>
2018-03-09 02:24:59 +0530

    Reverted to CheckMatrices

--- src/mlpack/tests/main_tests/kmeans_test.cpp
+++ src/mlpack/tests/main_tests/kmeans_test.cpp
@@ -390,23 +390,17 @@ BOOST_AUTO_TEST_CASE(AlgorithmsSimilarTest)
   dualCoverTreeOutput = std::move(CLI::GetParam<arma::mat>("output"));
   dualCoverTreeCentroid = std::move(CLI::GetParam<arma::mat>("centroid"));
 
-  for (size_t i = 0; i < inputData.n_cols; ++i)
-    BOOST_REQUIRE_EQUAL(naiveOutput(0, i), elkanOutput(0, i));
-  for (size_t i = 0; i < inputData.n_cols; ++i)
-    BOOST_REQUIRE_EQUAL(naiveOutput(0, i), hamerlyOutput(0, i));
-  for (size_t i = 0; i < inputData.n_cols; ++i)
-    BOOST_REQUIRE_EQUAL(naiveOutput(0, i), dualTreeOutput(0, i));
-  for (size_t i = 0; i < inputData.n_cols; ++i)
-    BOOST_REQUIRE_EQUAL(naiveOutput(0, i), dualCoverTreeOutput(0, i));
+  // Checking all the algorithms return same assignments
+  CheckMatrices(naiveOutput, hamerlyOutput);
+  CheckMatrices(naiveOutput, elkanOutput);
+  CheckMatrices(naiveOutput, dualTreeOutput);
+  CheckMatrices(naiveOutput, dualCoverTreeOutput);
 
-  for (size_t i = 0; i < initCentroid.n_elem; ++i)
-    BOOST_REQUIRE_CLOSE(naiveCentroid(0, i), elkanCentroid(0, i), 1e-5);
-  for (size_t i = 0; i < initCentroid.n_elem; ++i)
-    BOOST_REQUIRE_CLOSE(naiveCentroid(0, i), hamerlyCentroid(0, i), 1e-5);
-  for (size_t i = 0; i < initCentroid.n_elem; ++i)
-    BOOST_REQUIRE_CLOSE(naiveCentroid(0, i), dualTreeCentroid(0, i), 1e-5);
-  for (size_t i = 0; i < initCentroid.n_elem; ++i)
-    BOOST_REQUIRE_CLOSE(naiveCentroid(0, i), dualCoverTreeCentroid(0, i), 1e-5);
+  // Checking all the algorithms return almost same centroid
+  CheckMatrices(naiveCentroid, hamerlyCentroid);
+  CheckMatrices(naiveCentroid, elkanCentroid);
+  CheckMatrices(naiveCentroid, dualTreeCentroid);
+  CheckMatrices(naiveCentroid, dualCoverTreeCentroid);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
