5c72999fc283a2ce91d3e24b3ba4bc238a52e069
Sourabh Varshney <sourabhvarshney111@gmail.com>
2018-03-10 15:10:25 +0530

    Tried tests for constant momentum

--- src/mlpack/core/optimizers/sgd/update_policies/nesterov_momentum_update.hpp
+++ src/mlpack/core/optimizers/sgd/update_policies/nesterov_momentum_update.hpp
@@ -80,8 +80,7 @@ class NesterovMomentumUpdate
   {
     iteration++;
 
-    double momentumT = std::min((1 - std::pow(2 , (- 1 - ((log(floor(iteration
-        / 250)) +1) / log(2))))) , maxMomentum);
+    double momentumT = 0.9;
 
     velocity = momentumT * velocity - stepSize * gradient;
 
