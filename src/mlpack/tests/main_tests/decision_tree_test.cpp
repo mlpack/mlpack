7668ea4ea75e25e8ac465ff0816456a17387d5ed
manthan-r-sheth <manthanrsheth96@gmail.com>
2018-03-12 00:31:46 +0530

    Fix main_tests

--- src/mlpack/tests/main_tests/decision_tree_test.cpp
+++ src/mlpack/tests/main_tests/decision_tree_test.cpp
@@ -201,36 +201,40 @@ BOOST_AUTO_TEST_CASE(DecisionMinimumGainSplitTest)
  */
 BOOST_AUTO_TEST_CASE(DecisionRegularisationTest)
 {
-  // Completely random dataset with no structure.
-  arma::mat dataset(10, 1000, arma::fill::randu);
-  arma::Row<size_t> labels(1000);
-  for (size_t i = 0; i < 1000; ++i)
-    labels[i] = i % 3; // 3 classes.
-  arma::rowvec weights(labels.n_elem);
-  weights.ones();
+  arma::mat inputData;
+  DatasetInfo info;
+  if (!data::Load("braziltourism.arff", inputData, info))
+    BOOST_FAIL("Cannot load train dataset braziltourism.arff!");
+
+  arma::Row<size_t> labels;
+  if (!data::Load("braziltourism_labels.txt", labels))
+    BOOST_FAIL("Cannot load labels for braziltourism_labels.txt");
+
+  // Initialize an all-ones weight matrix.
+  arma::mat weights(1, labels.n_cols, arma::fill::ones);
 
   // Input training data.
-  SetInputParam("training", dataset);
+  SetInputParam("training", std::make_tuple(info, inputData));
   SetInputParam("labels", labels);
   SetInputParam("weights", weights);
 
   SetInputParam("minimum_gain_split", 1e-7);
 
   // Input test data.
-  SetInputParam("test", dataset);
+  SetInputParam("test", std::make_tuple(info, inputData));
   arma::Row<size_t> pred;
   mlpackMain();
   pred = std::move(CLI::GetParam<arma::Row<size_t>>("predictions"));
 
   // Input training data.
-  SetInputParam("training", dataset);
+  SetInputParam("training", std::make_tuple(info, inputData));
   SetInputParam("labels", std::move(labels));
   SetInputParam("weights", std::move(weights));
 
   SetInputParam("minimum_gain_split", 0.01);
 
   // Input test data.
-  SetInputParam("test", std::move(dataset));
+  SetInputParam("test", std::move(std::make_tuple(info, inputData)));
   arma::Row<size_t> predRegularised;
   mlpackMain();
   predRegularised = std::move(CLI::GetParam<arma::Row<size_t>>("predictions"));
