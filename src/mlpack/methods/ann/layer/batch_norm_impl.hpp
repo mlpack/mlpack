f598498559bbdd59b8039cd56725a5054b3a2cc2
manthan-r-sheth <manthanrsheth96@gmail.com>
2018-03-14 14:31:27 +0530

    Minor fix

--- src/mlpack/methods/ann/layer/batch_norm_impl.hpp
+++ src/mlpack/methods/ann/layer/batch_norm_impl.hpp
@@ -89,7 +89,7 @@ void BatchNorm<InputDataType, OutputDataType>::Backward(
   variance = arma::var(input, 1, 1);
 
   arma::mat m = arma::sum(gy % (input.each_col() - mean), 1);
-  g = arma::repmat(m, 1, input.n_cols) % (-input.each_col() + mean);
+  g = arma::repmat(m, 1, input.n_cols) % (mean - input.each_col());
   g.each_col() %= 1.0 / (variance + eps);
   g += (gy.each_col() - arma::sum(gy, 1));
   g += (input.n_cols - 1) * gy;
