ba1addcc5c05ee880d1cae40949cfb945f6b88e4
Chintan <chintan.soni4@gmail.com>
2018-03-21 02:55:41 +0530

    Added more tests, provided reasonable defaults to PSO constructor.

--- src/mlpack/tests/pso_test.cpp
+++ src/mlpack/tests/pso_test.cpp
@@ -12,7 +12,9 @@
 #include <mlpack/core.hpp>
 #include <mlpack/core/optimizers/pso/pso.hpp>
 
+#include <mlpack/core/optimizers/problems/rastrigin_function.hpp>
 #include <mlpack/core/optimizers/problems/rosenbrock_function.hpp>
+#include <mlpack/core/optimizers/problems/sphere_function.hpp>
 
 #include <boost/test/unit_test.hpp>
 #include "test_tools.hpp"
@@ -25,7 +27,20 @@ using namespace mlpack::optimization::test;
 
 BOOST_AUTO_TEST_SUITE(PSOTest);
 
-BOOST_AUTO_TEST_CASE(RosenbrockTest)
+BOOST_AUTO_TEST_CASE(PSORastriginTest)
+{
+  RastriginFunction f(4);
+  LBestPSO s;
+
+  arma::vec coordinates = f.GetInitialPoint();
+  double result = s.Optimize(f, coordinates);
+
+  BOOST_REQUIRE_SMALL(result, 1e-5);
+  for (size_t j = 0; j < 4; ++j)
+    BOOST_REQUIRE_SMALL(coordinates[j], 1e-3);
+}
+
+BOOST_AUTO_TEST_CASE(PSORosenbrockTest)
 {
   RosenbrockFunction f;
   LBestPSO s;
@@ -33,9 +48,22 @@ BOOST_AUTO_TEST_CASE(RosenbrockTest)
   arma::vec coordinates = f.GetInitialPoint();
   double result = s.Optimize(f, coordinates);
 
-  BOOST_REQUIRE_SMALL(result, 1e-10);
+  BOOST_REQUIRE_SMALL(result, 1e-5);
   for (size_t j = 0; j < 2; ++j)
     BOOST_REQUIRE_CLOSE(coordinates[j], (double) 1.0, 1e-3);
+}
+
+BOOST_AUTO_TEST_CASE(PSOSphereTest)
+{
+  SphereFunction f(4);
+  LBestPSO s;
+
+  arma::vec coordinates = f.GetInitialPoint();
+  double result = s.Optimize(f, coordinates);
+
+  BOOST_REQUIRE_SMALL(result, 1e-5);
+  for (size_t j = 0; j < 4; ++j)
+    BOOST_REQUIRE_SMALL(coordinates[j], 1e-3);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
