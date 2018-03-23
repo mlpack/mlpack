4a1dc046ce852e4ae49881c0ed4955de4916ff3b
luffy <rajrohan1108@gmail.com>
2018-03-23 14:50:00 +0530

    style fixes

--- src/mlpack/methods/reinforcement_learning/environment/acrobat.hpp
+++ src/mlpack/methods/reinforcement_learning/environment/acrobat.hpp
@@ -35,60 +35,60 @@ class Acrobat
    /*
     * Implementation of Acrobat State
     */    
-   class State
-   {
-     public :
-      /**
-      * Construct a state instance.
-      */
-      State(): data(dimension)
-      { /* nothing to do here */ }
-      /**
-      * Construct a state instance from given data.
-      *
-      * @param data Data for the acrobat.
-      * Consists of sin() and cos() for two rotational joint and angular velocity.
-      * 
-      */
-      State(const arma::colvec& data): data(data)
-      { /* nothing to do here */ }
-
-      //! Modify the state representation
-      arma::colvec& Data() {return data;}
-
-      //! Get value of theta1
-      double Theta1() const {return data[0];}
-      //! Modify value of theta1
-      double& Theta1() {return data[0];}
-
-      //! Get value of theta2
-      double Theta2() const {return data[1];}
-      //! Modify value of theta2
-      double& Theta2() {return data[1];}
-
-      //! Get value of Angular velocity 1
-      double AngularVelocity1() const { return data[2]; }
-      //! Modify the angular velocity 1.
-      double& AngularVelocity1() { return data[2]; }
-
-      //! Get value of Angular velocity 2
-      double AngularVelocity2() const { return data[3]; }
-      //! Modify the angular velocity 2.
-      double& AngularVelocity2() { return data[3]; }
-
-      //! Encode the state to a column vector.
-      const arma::colvec& Encode() const { return data; }
-
-      //! Dimension of the encoded state.
-      static constexpr size_t dimension = 4;
-
-     private :
-      arma::colvec data;
-   }; // state 
-   /* 
-    * Implementation of action for cartpole
+  class State
+  {
+   public :
+    /**
+    * Construct a state instance.
     */
-   enum Action
+    State(): data(dimension)
+    { /* nothing to do here */ }
+    /**
+    * Construct a state instance from given data.
+    *
+    * @param data Data for the acrobat.
+    * Consists of sin() and cos() for two rotational joint and angular velocity.
+    * 
+    */
+    State(const arma::colvec& data): data(data)
+    { /* nothing to do here */ }
+
+    //! Modify the state representation
+    arma::colvec& Data() {return data;}
+
+    //! Get value of theta1
+    double Theta1() const {return data[0];}
+    //! Modify value of theta1
+    double& Theta1() {return data[0];}
+
+    //! Get value of theta2
+    double Theta2() const {return data[1];}
+    //! Modify value of theta2
+    double& Theta2() {return data[1];}
+
+    //! Get value of Angular velocity 1
+    double AngularVelocity1() const { return data[2]; }
+    //! Modify the angular velocity 1.
+    double& AngularVelocity1() { return data[2]; }
+
+    //! Get value of Angular velocity 2
+    double AngularVelocity2() const { return data[3]; }
+    //! Modify the angular velocity 2.
+    double& AngularVelocity2() { return data[3]; }
+
+    //! Encode the state to a column vector.
+    const arma::colvec& Encode() const { return data; }
+
+    //! Dimension of the encoded state.
+    static constexpr size_t dimension = 4;
+
+   private :
+    arma::colvec data;
+  }; // state 
+  /* 
+   * Implementation of action for cartpole
+   */
+  enum Action
    {
    	negativeTorque,
    	zeroTorque,
@@ -122,18 +122,18 @@ class Acrobat
            const double max_vel1 = 4*PIE,
            const double max_vel2 = 9*PIE,
            const double dt = 0.2) :
-      gravity(gravity),
-      link_length1(link_length1),
-      link_length2(link_length2),
-      link_mass1(link_mass1),
-      link_mass2(link_mass2),
-      link_com1(link_com1),
-      link_com2(link_com2),
-      link_moi(link_moi),
-      max_vel1(max_vel1),
-      max_vel2(max_vel2),
-      dt(dt)
-   { /* Nothing to do here */ }
+    gravity(gravity),
+    link_length1(link_length1),
+    link_length2(link_length2),
+    link_mass1(link_mass1),
+    link_mass2(link_mass2),
+    link_com1(link_com1),
+    link_com2(link_com2),
+    link_moi(link_moi),
+    max_vel1(max_vel1),
+    max_vel2(max_vel2),
+    dt(dt)
+    { /* Nothing to do here */ }
    /**
     * Dynamics of the Acrobat System.
     * To get reward and next state based on current
@@ -148,84 +148,82 @@ class Acrobat
    double Sample(const State& state,
                  const Action& action,
                  State& nextState)
-          {
-            rk4(state,action,nextState);
-            double pie = PIE;
+    {
+     rk4(state,action,nextState);
+     double pie = PIE;
 
-            // double theta1 = ;
-            nextState.Theta1() = wrap(nextState.Theta1(),-pie,pie);
+     nextState.Theta1() = wrap(nextState.Theta1(),-pie,pie);
 
-            // double theta2 = nextState.Theta2();
-            nextState.Theta2() = wrap(nextState.Theta2(),-pie,pie);
-            nextState.AngularVelocity1() = bound(nextState.AngularVelocity1(),
+     nextState.Theta2() = wrap(nextState.Theta2(),-pie,pie);
+     nextState.AngularVelocity1() = bound(nextState.AngularVelocity1(),
             	                                 -max_vel1 , max_vel1);
-            nextState.AngularVelocity2() = bound(nextState.AngularVelocity2(),
+     nextState.AngularVelocity2() = bound(nextState.AngularVelocity2(),
             	                                 -max_vel2 , max_vel2);
-            return -1;
-          };
-  double Sample(const State& state, const Action& action)
-          {
-            State nextState;
-            return Sample(state, action, nextState);
-          }
-  State InitialSample() const
-          {
-            return State((arma::randu<arma::colvec>(4) - 0.5) / 5.0);
-          }
+     return -1;
+    };
+   double Sample(const State& state, const Action& action)
+    {
+     State nextState;
+     return Sample(state, action, nextState);
+    }
+   State InitialSample() const
+    {
+     return State((arma::randu<arma::colvec>(4) - 0.5) / 5.0);
+    }
   /**
    *
    * @param state The current State
    */        
   bool IsTerminal(const State& state) const
-          {
-          	double theta1 = state.Theta1();
-          	double theta2 = state.Theta2();
-            return bool ((-cos(theta1)-cos(theta1+theta2)) > 1.0);
-          }
+    {
+     double theta1 = state.Theta1();
+     double theta2 = state.Theta2();
+     return bool ((-cos(theta1)-cos(theta1+theta2)) > 1.0);
+    }
   /**
    * @param state Current State
    * @param torque Torque Applied 
    */
-  arma::colvec dsdt(arma::colvec state,
+   arma::colvec dsdt(arma::colvec state,
    	           const double torque)
-   	      {
-            double m1 = link_mass1;
-            double m2 = link_mass2;
-            double l1 = link_length1;
-            double l2 = link_length2;
-            double lc1 = link_com1;
-            double lc2 = link_com2;
-            double I1 = link_moi;
-            double I2 = link_moi;
-            double g = gravity;
-            double a = torque;
-            double theta1 = state[0];
-            double theta2 = state[1];
-            double dtheta1 = state[2];
-            double dtheta2 = state[3];
-            double d1 = m1 * pow(lc1,2) + m2 * 
-                       (pow(l1,2) + pow(lc2,2) + 2 * l1 * lc2 * cos(theta2)) 
-                        + I1 + I2 ;
-            double d2 = m2 * (pow(lc2,2) + l1 * lc2 * cos(theta2)) + I2;
-            double phi2 = m2 * lc2 * g * cos(theta1 + theta2 - PIE / 2.);
-            
-            double phi1 = - m2 * l1 * lc2 * pow(dtheta2,2) * sin(theta2)
-               - 2 * m2 * l1 * lc2 * dtheta2 * dtheta1 * sin(theta2)
-               + (m1 * lc1 + m2 * l1) * g * cos(theta1 - PIE / 2)
-               + phi2;
-            
-            double ddtheta2 = (a + d2 / d1 * phi1 - m2 * l1 * lc2 * pow(dtheta1,2) * 
-            	sin(theta2) - phi2) / (m2 * pow(lc2,2) + I2 - pow(d2,2) / d1);
+    {
+     double m1 = link_mass1;
+     double m2 = link_mass2;
+     double l1 = link_length1;
+     double l2 = link_length2;
+     double lc1 = link_com1;
+     double lc2 = link_com2;
+     double I1 = link_moi;
+     double I2 = link_moi;
+     double g = gravity;
+     double a = torque;
+     double theta1 = state[0];
+     double theta2 = state[1];
+     double dtheta1 = state[2];
+     double dtheta2 = state[3];
+     double d1 = m1 * pow(lc1,2) + m2 * 
+               (pow(l1,2) + pow(lc2,2) + 2 * l1 * lc2 * cos(theta2)) 
+                + I1 + I2 ;
+     double d2 = m2 * (pow(lc2,2) + l1 * lc2 * cos(theta2)) + I2;
+     double phi2 = m2 * lc2 * g * cos(theta1 + theta2 - PIE / 2.);
+    
+     double phi1 = - m2 * l1 * lc2 * pow(dtheta2,2) * sin(theta2)
+       - 2 * m2 * l1 * lc2 * dtheta2 * dtheta1 * sin(theta2)
+       + (m1 * lc1 + m2 * l1) * g * cos(theta1 - PIE / 2)
+       + phi2;
+    
+     double ddtheta2 = (a + d2 / d1 * phi1 - m2 * l1 * lc2 * pow(dtheta1,2) * 
+    	sin(theta2) - phi2) / (m2 * pow(lc2,2) + I2 - pow(d2,2) / d1);
 
-            double ddtheta1 = -(d2 * ddtheta2 + phi1) / d1;
-            arma::colvec returnValues = {dtheta1, dtheta2, ddtheta1, ddtheta2};
-            return returnValues;
-   	      };
-   	/**
-   	* @param value scalar value to wrap
-   	* @param minimum minimum range of wrap
-   	* @param maximum maximum range of wrap
-   	*/      
+     double ddtheta1 = -(d2 * ddtheta2 + phi1) / d1;
+     arma::colvec returnValues = {dtheta1, dtheta2, ddtheta1, ddtheta2};
+     return returnValues;
+   	};
+   /**
+   * @param value scalar value to wrap
+   * @param minimum minimum range of wrap
+   * @param maximum maximum range of wrap
+   */      
    double wrap(double value,
                double minimum,
                double maximum)
@@ -320,3 +318,5 @@ class Acrobat
 }; // class Acrobat
 } // namespace rl
 } // namespace mlpack
+
+#endif
