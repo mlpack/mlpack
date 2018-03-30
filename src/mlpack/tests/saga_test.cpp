83bffa4211881af10a516780da8fbacd24e85687
Prabhat <prabhatsharma7298@gmail.com>
2018-03-31 01:05:55 +0530

    Order of tests changed

--- src/mlpack/tests/saga_test.cpp
+++ src/mlpack/tests/saga_test.cpp
@@ -24,6 +24,52 @@ using namespace mlpack::optimization::test;
 BOOST_AUTO_TEST_SUITE(SAGATypeTest);
 
 /**
+* Run SAGA on Sphere function and make sure the results are acceptable.
+*/
+BOOST_AUTO_TEST_CASE(SphereFunctionSAGATest)
+{
+  // Loop over several variants.
+  for (size_t i = 50; i < 100; i += 10)
+  {
+    SphereFunction f(i);
+
+    double stepSize = 0.00001 * i;
+
+    SAGA optimizer(stepSize, 5, 500000, 1e-7, false);
+
+    arma::mat coordinates = f.GetInitialPoint();
+    double result = optimizer.Optimize(f, coordinates);
+
+    BOOST_REQUIRE_SMALL(result, 0.1);
+    //for (size_t j = 0; j < i; ++j)
+      //BOOST_REQUIRE_SMALL(coordinates[j], (double) 0.001);
+  }
+}
+
+/**
+* Run SAG on Sphere function and make sure the results are acceptable.
+*/
+BOOST_AUTO_TEST_CASE(SphereFunctionSAGTest)
+{
+  // Loop over several variants.
+  for (size_t i = 50; i < 100; i += 10)
+  {
+    SphereFunction f(i);
+
+    double stepSize = 0.00001 * i;
+
+    SAG optimizer(stepSize, 5, 500000, 1e-7, false);
+
+    arma::mat coordinates = f.GetInitialPoint();
+    double result = optimizer.Optimize(f, coordinates);
+
+    BOOST_REQUIRE_SMALL(result, 0.1);
+    //for (size_t j = 0; j < i; ++j)
+      //BOOST_REQUIRE_SMALL(coordinates[j], (double) 0.001);
+  }
+}
+
+/**
  * Run SAGA on logistic regression and make sure the results are acceptable.
  */
 BOOST_AUTO_TEST_CASE(SAGALogisticRegressionTest)
@@ -50,73 +96,29 @@ BOOST_AUTO_TEST_CASE(SAGALogisticRegressionTest)
 }
 
 /**
-* Run SAGA on Sphere function and make sure the results are acceptable.
-*/
-BOOST_AUTO_TEST_CASE(SphereFunctionSAGATest)
-{
-  // Loop over several variants.
-  for (size_t i = 50; i < 100; i += 10)
-  {
-    SphereFunction f(i);
-
-    double stepSize = 0.00001 * i;
-
-    SAGA optimizer(stepSize, 5, 500000, 1e-7, false);
-
-    arma::mat coordinates = f.GetInitialPoint();
-    double result = optimizer.Optimize(f, coordinates);
-
-    BOOST_REQUIRE_SMALL(result, 0.1);
-    for (size_t j = 0; j < i; ++j)
-      BOOST_REQUIRE_SMALL(coordinates[j], (double) 0.001);
-  }
-}
-/**
 * Run SAG on logistic regression and make sure the results are acceptable.
 */
-BOOST_AUTO_TEST_CASE(SAGLogisticRegressionTest)
-{
-  arma::mat data, testData, shuffledData;
-  arma::Row<size_t> responses, testResponses, shuffledResponses;
-
-  LogisticRegressionTestData(data, testData, shuffledData,
-                             responses, testResponses, shuffledResponses);
-
-  // Now run mini-batch SAGA with a couple of batch sizes.
-  for (size_t batchSize = 35; batchSize < 50; batchSize += 5)
+  BOOST_AUTO_TEST_CASE(SAGLogisticRegressionTest)
   {
-    SAG optimizer(0.001, batchSize, 250, 1e-3, false);
-    LogisticRegression<> lr(shuffledData, shuffledResponses, optimizer, 0.5);
+    arma::mat data, testData, shuffledData;
+    arma::Row<size_t> responses, testResponses, shuffledResponses;
 
-    // Ensure that the error is close to zero.
-    const double acc = lr.ComputeAccuracy(data, responses);
-    BOOST_REQUIRE_CLOSE(acc, 100.0, 1.5); // 1.5% error tolerance.
+    LogisticRegressionTestData(data, testData, shuffledData,
+                               responses, testResponses, shuffledResponses);
 
-    const double testAcc = lr.ComputeAccuracy(testData, testResponses);
-    BOOST_REQUIRE_CLOSE(testAcc, 100.0, 1.5); // 1.5% error tolerance.
+    // Now run mini-batch SAGA with a couple of batch sizes.
+    for (size_t batchSize = 35; batchSize < 50; batchSize += 5)
+    {
+      SAG optimizer(0.001, batchSize, 250, 1e-3, false);
+      LogisticRegression<> lr(shuffledData, shuffledResponses, optimizer, 0.5);
+
+      // Ensure that the error is close to zero.
+      const double acc = lr.ComputeAccuracy(data, responses);
+      BOOST_REQUIRE_CLOSE(acc, 100.0, 1.5); // 1.5% error tolerance.
+
+      const double testAcc = lr.ComputeAccuracy(testData, testResponses);
+      BOOST_REQUIRE_CLOSE(testAcc, 100.0, 1.5); // 1.5% error tolerance.
+    }
   }
-}
 
-/**
-* Run SAG on Sphere function and make sure the results are acceptable.
-*/
-BOOST_AUTO_TEST_CASE(SphereFunctionSAGTest)
-{
-  // Loop over several variants.
-  for (size_t i = 50; i < 100; i += 10)
-  {
-    SphereFunction f(i);
-
-    double stepSize = 0.00001 * i;
-
-    SAG optimizer(stepSize, 5, 500000, 1e-7, false);
-
-    arma::mat coordinates = f.GetInitialPoint();
-    double result = optimizer.Optimize(f, coordinates);
-
-    BOOST_REQUIRE_SMALL(result, 0.1);
-    for (size_t j = 0; j < i; ++j)
-      BOOST_REQUIRE_SMALL(coordinates[j], (double) 0.001);
-  }
-}
 BOOST_AUTO_TEST_SUITE_END();
