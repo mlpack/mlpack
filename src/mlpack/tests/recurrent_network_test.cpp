c1d7a5864d27ced0ac01445b38900837b33c65a5
Projyal Dev <projyal@gmail.com>
2018-04-09 08:45:24 +0530

    Style correction.

--- src/mlpack/tests/recurrent_network_test.cpp
+++ src/mlpack/tests/recurrent_network_test.cpp
@@ -1207,7 +1207,7 @@ double RNNSineTest(size_t hiddenUnits, size_t rho, size_t numEpochs = 100)
   // data and the pediction to vectors and compare the two.
   arma::colvec testVector = arma::vectorise(testData);
   arma::colvec predVector = arma::vectorise(prediction);
-  //Adjust the vectors for comparison, as the prediction is one step ahead.
+  // Adjust the vectors for comparison, as the prediction is one step ahead.
   testVector = testVector.rows(1, testVector.n_rows - 1);
   predVector = predVector.rows(0, predVector.n_rows - 2);
   double error = std::sqrt(arma::sum(arma::square(testVector - predVector))) /
