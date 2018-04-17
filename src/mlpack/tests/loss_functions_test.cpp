d6a4e518debc2be1880872800eb495e2d80f4771
Sourabh Varshney <sourabhvarshney111@gmail.com>
2018-04-18 03:07:14 +0530

    Add test_tools header

--- src/mlpack/tests/loss_functions_test.cpp
+++ src/mlpack/tests/loss_functions_test.cpp
@@ -17,6 +17,7 @@
 #include <mlpack/methods/ann/loss_functions/cross_entropy_error.hpp>
 
 #include <boost/test/unit_test.hpp>
+#include "test_tools.hpp"
 
 using namespace mlpack;
 using namespace mlpack::ann;
