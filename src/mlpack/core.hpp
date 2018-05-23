757fe1c697a638189b3f9b7b245893ec4d9c53e3
yaswagner <yasmine.dumouchel@gmail.com>
2018-05-22 21:34:15 -0400

    Added test for NCA binding

--- src/mlpack/core.hpp
+++ src/mlpack/core.hpp
@@ -241,6 +241,7 @@
  *   - Tan Jun An <yamidarkxxx@gmail.com>
  *   - Moksh Jain <mokshjn00@gmail.com>
  *   - Manthan-R-Sheth <manthanrsheth96@gmail.com>
+ *   - Yasmine Dumouchel <yasmine.dumouchel@gmail.com>
  */
 
 // First, include all of the prerequisites.
