ed3ee74d1b556ab2a1a13a81677ede342e88b5d2
akhandait <akhandait45@gmail.com>
2018-08-14 20:55:11 +0530

    Add Concatenate layer.

--- src/mlpack/tests/ann_layer_test.cpp
+++ src/mlpack/tests/ann_layer_test.cpp
@@ -1084,6 +1084,56 @@ BOOST_AUTO_TEST_CASE(GradientConcatLayerTest)
 }
 
 /**
+ * Concatenate layer numerical gradient test.
+ */
+BOOST_AUTO_TEST_CASE(GradientConcatenateLayerTest)
+{
+  // Concatenate function gradient instantiation.
+  struct GradientFunction
+  {
+    GradientFunction()
+    {
+      input = arma::randu(10, 1);
+      target = arma::mat("1");
+
+      model = new FFN<NegativeLogLikelihood<>, NguyenWidrowInitialization>();
+      model->Predictors() = input;
+      model->Responses() = target;
+      model->Add<IdentityLayer<> >();
+      model->Add<Linear<> >(10, 5);
+
+      arma::mat concat = arma::ones(5, 1);
+      concatenate = new Concatenate<>();
+      concatenate->Concat() = concat;
+      model->Add(concatenate);
+
+      model->Add<Linear<> >(10, 5);
+      model->Add<LogSoftMax<> >();
+    }
+
+    ~GradientFunction()
+    {
+      delete model;
+    }
+
+    double Gradient(arma::mat& gradient) const
+    {
+      double error = model->Evaluate(model->Parameters(), 0, 1);
+      model->Gradient(model->Parameters(), 0, gradient, 1);
+      return error;
+    }
+
+    arma::mat& Parameters() { return model->Parameters(); }
+
+    FFN<NegativeLogLikelihood<>, NguyenWidrowInitialization>* model;
+    Concatenate<>* concatenate;
+    arma::mat input, target;
+  } function;
+
+  BOOST_REQUIRE_LE(CheckGradient(function), 1e-4);
+}
+
+/**
  * Simple lookup module test.
  */
 BOOST_AUTO_TEST_CASE(SimpleLookupLayerTest)
