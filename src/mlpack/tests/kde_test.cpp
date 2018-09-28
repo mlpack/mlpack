a26dedd9796ad0a9430a12a6c76fd04e0e0a44ec
Roberto Hueso Gomez <robertohueso96@gmail.com>
2018-09-28 18:38:25 +0200

    Add KDE RTree gaussian test

--- src/mlpack/tests/kde_test.cpp
+++ src/mlpack/tests/kde_test.cpp
@@ -239,6 +239,40 @@ BOOST_AUTO_TEST_CASE(OctreeGaussianKDETest)
 }
 
 /**
+ * Test RTree dual-tree implementation results against brute force results.
+ */
+BOOST_AUTO_TEST_CASE(RTreeGaussianKDETest)
+{
+  arma::mat reference = arma::randu(2, 500);
+  arma::mat query = arma::randu(2, 200);
+  arma::vec bfEstimations = arma::vec(query.n_cols, arma::fill::zeros);
+  arma::vec treeEstimations = arma::vec(query.n_cols, arma::fill::zeros);
+  const double kernelBandwidth = 0.3;
+  const double relError = 0.01;
+
+  // Brute force KDE
+  GaussianKernel kernel(kernelBandwidth);
+  BruteForceKDE<GaussianKernel>(reference,
+                                query,
+                                bfEstimations,
+                                kernel);
+
+  // Optimized KDE
+  metric::EuclideanDistance metric;
+  KDE<metric::EuclideanDistance,
+      arma::mat,
+      kernel::GaussianKernel,
+      tree::RTree>
+    kde(metric, kernel, relError, 0.0);
+  kde.Train(reference);
+  kde.Evaluate(std::move(query), treeEstimations);
+
+  // Check whether results are equal.
+  for (size_t i = 0; i < query.n_cols; ++i)
+    BOOST_REQUIRE_CLOSE(bfEstimations[i], treeEstimations[i], relError);
+}
+
+/**
  * Test duplicated value in reference matrix.
  */
 BOOST_AUTO_TEST_CASE(DuplicatedReferenceSampleKDETest)
