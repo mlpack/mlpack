a3c6d590587a424f8fff5cf50519a9c8af646f81
robotcator <problemset@163.com>
2019-01-14 15:37:28 +0800

    update the sumtree implementation

--- src/mlpack/methods/reinforcement_learning/replay/sumtree.hpp
+++ src/mlpack/methods/reinforcement_learning/replay/sumtree.hpp
@@ -19,93 +19,101 @@
 
 namespace mlpack {
 namespace rl {
-  template<typename T>
-  class SumTree {
-    SumTree(size_t capacity):
-        capacity(capacity)
-    {
-      element(2 * capacity);
-    }
 
-    void set(size_t idx, T value)
+/**
+ * Implementation of SumTree.
+ */
+
+template<typename T>
+class SumTree
+{
+ public:
+  SumTree(size_t capacity):
+      capacity(capacity)
+  {
+    element = std::vector<T> (2 * capacity);
+  }
+
+  void set(size_t idx, T value)
+  {
+    idx += capacity;
+    element[idx] = value;
+    idx /= 2;
+    while (idx >= 1)
     {
-      idx += capacity;
-      element[idx] = value;
+      element[idx] = element[2 * idx] + element[2 * idx + 1];
       idx /= 2;
-      while (idx >= 1)
-      {
-        element[idx] = element[2 * idx] + element[2 * idx + 1];
-        idx /= 2;
-      }
     }
+  }
 
-    T get(size_t idx)
+  T get(size_t idx)
+  {
+    idx += capacity;
+    return element[idx];
+  }
+
+  T sumHelper(size_t _start, size_t _end, size_t node, size_t node_start, size_t node_end)
+  {
+    if (_start == node_start && _end == node_end)
     {
-      idx += capacity;
-      return element[idx];
+      return element[node];
     }
-
-    T sumHelper(size_t _start, size_t _end, size_t node, size_t node_start, size_t node_end)
+    size_t mid = (node_start + node_end) / 2;
+    if (_end <= mid)
     {
-      if (_start == node_start && _end == node_end)
+      return sumHelper(_start, _end, 2 * node, node_start, mid);
+    }
+    else
+    {
+      if (mid + 1 <= _start)
       {
-        return element[node];
-      }
-      size_t mid = (node_start + node_end) / 2;
-      if (_end <= mid)
-      {
-        return sumHelper(_start, _end, 2 * node, node_start, mid);
+        return sumHelper(_start, _end, 2 * node + 1, mid + 1 , node_end);
       }
       else
       {
-        if (mid + 1 <= _start)
-        {
-          return sumHelper(_start, _end, 2 * node + 1, mid + 1 , node_end);
-        }
-        else
-        {
-          return sumHelper(_start, mid, 2 * node, node_start, mid) +
-                  sumHelper(mid+1, _end, 2 * node + 1, mid + 1 , node_end);
-        }
+        return sumHelper(_start, mid, 2 * node, node_start, mid) +
+                sumHelper(mid+1, _end, 2 * node + 1, mid + 1 , node_end);
       }
     }
+  }
 
-    T sum(size_t _start, size_t _end)
-    {
-//    caculate the sum of contiguous subsequence of the array.
-      _end -= 1;
-      return sumHelper(_start, _end, 1, 0, capacity-1);
-    }
+  T sum(size_t _start, size_t _end)
+  {
+//  caculate the sum of contiguous subsequence of the array.
+    _end -= 1;
+    return sumHelper(_start, _end, 1, 0, capacity-1);
+  }
 
-    T sum()
-    {
-      return sum(0, capacity);
-    }
+  T sum()
+  {
+    return sum(0, capacity);
+  }
 
-    size_t findPrefixSum(T mass)
-    {
+  size_t findPrefixSum(T mass)
+  {
 //    Find the highest index `idx` in the array such that
-//            sum(arr[0] + arr[1] + ... + arr[i - i]) <= prefixsum
-      int idx = 1;
-      while (idx < capacity)
+//            sum(arr[0] + arr[1] + ... + arr[i]) <= mass
+    int idx = 1;
+    while (idx < capacity)
+    {
+      if (element[2 * idx] > mass)
       {
-        if (element[2 * idx] > mass)
-        {
-          idx = 2 * idx;
-        }
-        else
-        {
-          mass -= element[2 * idx];
-          idx = 2 * idx + 1;
-        }
+        idx = 2 * idx;
       }
-      return idx - capacity;
+      else
+      {
+        mass -= element[2 * idx];
+        idx = 2 * idx + 1;
+      }
     }
+    return idx - capacity;
+  }
 
-   private:
-    capacity;
-    std::vector<T> element;
-  };
+ private:
+  size_t capacity;
+  std::vector<T> element;
+};
+
 } // namespace rl
 } // namespace mlpack
 
