32d65495d7e0b024b0102bc28550fd7cd796052b
rajiv2605 <rajiv.vaidyanathan4@gmail.com>
2019-01-18 11:37:16 +0530

    Created the basic files for dice loss function

---
+++ src/mlpack/methods/ann/loss_functions/dice_loss.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file dice_loss.hpp
+ * @author N Rajiv Vaidyanathan
+ *
+ * Definition of the dice loss function.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_METHODS_ANN_LOSS_FUNCTIONS_DICE_LOSS_HPP
+#define MLPACK_METHODS_ANN_LOSS_FUNCTIONS_DICE_LOSS_HPP
+
+#include <mlpack/prereqs.hpp>
+
+namespace mlpack {
+namespace ann /** Artificial Neural Network. */ {
+
+} // namespace ann
+} // namespace mlpack
+
+// Include implementation.
+#include "dice_loss_impl.hpp"
+
+#endif
---
+++ src/mlpack/methods/ann/loss_functions/dice_loss_impl.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file dice_loss_impl.hpp
+ * @author N Rajiv Vaidyanathan
+ *
+ * Implementation of the dice loss function.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_METHODS_ANN_LOSS_FUNCTIONS_DICE_LOSS_IMPL_HPP
+#define MLPACK_METHODS_ANN_LOSS_FUNCTIONS_DICE_LOSS_IMPL_HPP
+
+// In case it hasn't yet been included.
+#include "dice_loss.hpp"
+
+namespace mlpack {
+namespace ann /** Artificial Neural Network. */ {
+
+} // namespace ann
+} // namespace mlpack
+
+#endif
