808fd922d18b2856e37b1c3371b290b5a349c189
gauravcr7rm <gauravcr7rm@gmail.com>
2019-01-23 16:14:51 +0530

    Updated gmm_probability_test.cpp

---
+++ src/mlpack/tests/main_tests/gmm_probability_test.cpp
@@ -0,0 +1,97 @@
+#define BINDING_TYPE BINDING_TYPE_TEST
+
+#include <mlpack/core.hpp>
+static const std::string testName = "Gmm_Probability"
+
+#include <mlpack/core/util/mlpack_main.hpp>
+#include <mlpack/methods/gmm/gmm_probabiity_main.cpp>
+#include <mlpack/methods/gmm/gmm_train_main.cpp>
+#include "test_helper.hpp"
+
+#include <boost/test/unit_tests.hpp>
+#include "../test_tools.hpp"
+
+using namespace mlpack;
+
+struct GmmProbabilityTestFixture
+{
+  public:
+    GmmProbabilityTestFixture()
+    {
+      CLI::RestoreSettings(testname);
+    }
+
+    ~GmmProbabilityTestFixture()
+    {
+      bindings::tests::CleanMemory();
+      CLI::ClearSettings();
+    }
+};
+
+
+BOOST_FIXTURE_TEST_SUITE(GmmProbabilityMainTest,
+                         GmmProbabilityTestFixture);
+
+//Make sure that input model is provided
+BOOST_AUTO_TEST_CASE(GmmProbabilityInputModelTest)
+{
+  arma::mat InputData;
+
+  InputData << 0 << 1 << 2<< 3 << 4 << 5 << 6 << 7 << endr
+            << 0 << 4 << 3 << 4 << 8 << 9 << 2 << 5 <<endr;
+
+  SetInputParam("input" , std::move(InputData));
+
+  Log::Fatal.ignoreInput = true;
+  BOOST_REQUIRE_THROW(mlpackMain(), std::runtime_error);
+  Log::Fatal.ignoreInput = false;
+}
+  
+//Make sure that input points are provided
+BOOST_AUTO_TEST_CASE()
+{
+  int g = 3;
+  int trials = 2;
+
+
+  arma::mat inputData;
+  if (!data::Load("vc2.csv", inputData))
+  BOOST_FAIL("Unable to load train dataset vc2.csv!");
+  
+  gmm = new GMM((size_t)g , inputdata.n_rows);
+
+  SetInputParam("input_model" , std::move(gmm));
+
+  Log::Fatal.ignoreInput = true;
+  BOOST_REQUIRE_THROW(mlpackMain(), std::runtime_error);
+  Log::Fatal.ignoreInput = false;
+}
+
+//checking the input and output dimensionality
+BOOST_AUTO_TEST_CASE()
+{
+  int g= 3;
+  int trials = 2;
+
+  arma::mat inputData;
+  if (!data::Load("vc2.csv", inputData))
+  BOOST_FAIL("Unable to load train dataset vc2.csv!");
+  
+  gmm = new GMM((size_t)g , inputdata.n_rows);
+  
+  arma::mat InputData;
+  InputData << 0 << 1 << 2<< 3 << 4 << 5 << 6 << 7 << endr
+            << 0 << 4 << 3 << 4 << 8 << 9 << 2 << 5 <<endr;
+
+  SetInputParam("input_model", std::move(gmm));
+  SetInputParam("input", std::move(InputData));
+
+  mlpackMain();
+
+  BOOST_REQUIRE_EQUAL(CLI::GetParam<arma::mat>("output").n_cols,
+                      8);
+  BOOST_REQUIRE_EQUAL(CLI::GetParam<arma::mat>("output").n_rows,
+                      2);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
