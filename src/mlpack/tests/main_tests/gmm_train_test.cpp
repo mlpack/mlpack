afc2dfe67b32f38e317120fb850f2420d3c8b48f
gauravcr7rm <gauravcr7rm@gmail.com>
2019-02-07 18:37:55 +0530

    gmm_train_test.cpp update

--- src/mlpack/tests/main_tests/gmm_train_test.cpp
+++ src/mlpack/tests/main_tests/gmm_train_test.cpp
@@ -9,49 +9,35 @@
  * 3-clause BSD license along with mlpack.  If not, see
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
- #include<string>
-
+#include<string>
 
 #define BINDING_TYPE BINDING_TYPE_TEST
-static const std:: string testname = "gmm_train"
-
+static const std:: string testname = "GmmTrain"
 
 #include <mlpack/core.hpp>
 #include <mlpack/core/util/mlpack_main.hpp>
 #include "test_helper.hpp"
 #include <mlpack/methods/gmm/gmm_train_main.cpp>
-#include <mlpack/methods/kmeans/kmeans_main.cpp>
-#include <mlpack/methods/gmm/no_constraint.hpp>
-#include <mlpack/methods/gmm/positive_definite_constraint.hpp>
-#include <mlpack/methods/gmm/diagonal_constraint.hpp>
-#include <mlpack/methods/gmm/eigenvalue_ratio_constraint.hpp>
-
-
-#include "no_constraint.hpp"
-#include "diagonal_constraint.hpp"
-
 
 #include <boost/test/unit_test.hpp>
 #include "../test_tools.hpp"
-
 
 using namespace mlpack;
 
 struct GmmTrainTestFixture
 {
 public:
-  GmmTrainTestFixture()
-  {
-    // Cache in the options for this program.
-    CLI::RestoreSettings(testname);
-  }
+ GmmTrainTestFixture()
+ {
+   // Cache in the options for this program.
+   CLI::RestoreSettings(testname);
+ }
 
-
-  ~GmmTrainTestFixture()
-  {
-    // Clear the settings.
-    CLI::ClearSettings();
-  }
+ ~GmmTrainTestFixture()
+ {
+   // Clear the settings.
+   CLI::ClearSettings();
+ }
 };
 
 void ResetGmmTrainSetting()
@@ -60,27 +46,22 @@ void ResetGmmTrainSetting()
   CLI::RestoreSettings(testname);
 }
 
-
 BOOST_FIXTURE_TEST_SUITE(GmmTrainMainTest , GmmTrainTestFixture);
-
 
 // To check if the gaussian is positive or not.
 BOOST_AUTO_TEST_CASE(GmmTrainValidGaussian)
 {
   arma::mat inputData;
   if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
+    BOOST_FAIL("Unable to load train dataset vc2.csv!");
 
-
-  SetInputParam("input" , std::move(inputdata));
+  SetInputParam("input" , std::move(inputData));
   SetInputParam("gaussian" , (int) -1); //invalid
 
   Log::Fatal.ignoreInput = true;
   BOOST_REQUIRE_THROW(mlpackMain(), std::runtime_error);
   Log::Fatal.ignoreInput = false;
-
 }
-
 
 /* To check if the number of gaussians in the output model is same as
 that of input gaussian parameter or not. */
@@ -91,9 +72,9 @@ BOOST_AUTO_TEST_CASE(GmmTrainOutputModelGaussian)
 
   arma::mat inputData;
   if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
+    BOOST_FAIL("Unable to load train dataset vc2.csv!");
 
-  SetInputParam("input" , std::move(inputdata));
+  SetInputParam("input" , std::move(inputData));
   SetInputParam("gaussians" , g);
   SetInputParam("trials" , trials);
   
@@ -103,7 +84,6 @@ BOOST_AUTO_TEST_CASE(GmmTrainOutputModelGaussian)
   BOOST_REQUIRE_EQUAL(gmm->gaussians() , g);
 }
 
-
 // Number of trials is provided or not.
 BOOST_AUTO_TEST_CASE(GmmTrainValidTrials)
 {
@@ -112,18 +92,16 @@ BOOST_AUTO_TEST_CASE(GmmTrainValidTrials)
 
   arma::mat inputData;
   if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
-
+    BOOST_FAIL("Unable to load train dataset vc2.csv!");
 
   SetInputParam("input" , std::move(inputdata));
   SetInputParam("gaussians" , g);
-  SetInputParam("trials" , trials) // Invalid
+  SetInputParam("trials" , trials);// Invalid.
 
   Log::Fatal.ignoreInput = true;
   BOOST_REQUIRE_THROW(mlpackMain(), std::runtime_error);
   Log::Fatal.ignoreInput = false;
 }
-
 
 // If covariance flag is false then no_force_positive parameter is not specified.
 BOOST_AUTO_TEST_CASE(GmmTrainDiagonalCovarianceCheck)
@@ -134,9 +112,9 @@ BOOST_AUTO_TEST_CASE(GmmTrainDiagonalCovarianceCheck)
 
   arma::mat inputData;
   if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
+    BOOST_FAIL("Unable to load train dataset vc2.csv!");
 
-  SetInputParam("input" , std::move(inputdata));
+  SetInputParam("input" , std::move(inputData));
   SetInputParam("gaussians" , g);
   SetInputParam("trials" , t);
   SetInputParam("Diagonal_covariance" , false);
@@ -146,11 +124,9 @@ BOOST_AUTO_TEST_CASE(GmmTrainDiagonalCovarianceCheck)
   Log::Fatal.ignoreInput = true;
   BOOST_REQUIRE_THROW(mlpackMain() , runtime_error);
   Log::Fatal.ignoreInput = false;
-
 }
 
-
-//max iterations must be positive
+// Max iterations must be positive.
 BOOST_AUTO_TEST_CASE(GmmTrainMaxIterations)
 {
   int g = 3;
@@ -159,19 +135,17 @@ BOOST_AUTO_TEST_CASE(GmmTrainMaxIterations)
 
   arma::mat inputData;
   if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
+    BOOST_FAIL("Unable to load train dataset vc2.csv!");
 
-
-  SetinputParam("input" , std::move(inputdata));
-  SetinputParam("gaussians", g);
-  SetinputParam("trials" , t);
-  SetinputParam("max_iterations" ,mi);// Invalid
+  SetInputParam("input" , std::move(inputData));
+  SetInputParam("gaussians", g);
+  SetInputParam("trials" , t);
+  SetInputParam("max_iterations" ,mi);// Invalid.
 
   Log::Fatal.ignoreInput = true;
   BOOST_REQUIRE_THROW(mlpackMain() , runtime_error);
   Log::Fatal.ignoreInput = false;
 }
-
 
 // Tolerence must be positive. 
 BOOST_AUTO_TEST_CASE(GmmTrainTolerence)
@@ -183,22 +157,20 @@ BOOST_AUTO_TEST_CASE(GmmTrainTolerence)
 
   arma::mat inputData;
   if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
+    BOOST_FAIL("Unable to load train dataset vc2.csv!");
 
-  SetinputParam("input" , std::move(inputdata));
-  SetinputParam("gaussians", g);
-  SetinputParam("trials" , t);
-  SetinputParam("max_iterations" ,mi);
-  SetinputParam("tolerence" , tol);//invalid
-
+  SetInputParam("input" , std::move(inputData));
+  SetInputParam("gaussians", g);
+  SetInputParam("trials" , t);
+  SetInputParam("max_iterations" ,mi);
+  SetInputParam("tolerence" , tol);// Invalid
+  
   Log::Fatal.ignoreInput = true;
   BOOST_REQUIRE_THROW(mlpackMain() , runtime_error);
   Log::Fatal.ignoreInput = false;
-
 }
 
-
- /**
+/**
  *  Checking that percentage is between 0 and 1 when --refined_start is specified.
 */
 BOOST_AUTO_TEST_CASE(RefinedStartPercentageTest)
@@ -211,7 +183,7 @@ BOOST_AUTO_TEST_CASE(RefinedStartPercentageTest)
   double P = 2.0;
   arma::mat inputData;
   if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
+    BOOST_FAIL("Unable to load train dataset vc2.csv!");
 
   SetInputParam("input", std::move(inputData));
   SetInputParam("refined_start", true);
@@ -223,42 +195,6 @@ BOOST_AUTO_TEST_CASE(RefinedStartPercentageTest)
   Log::Fatal.ignoreInput = false;
 }
 
-
-// Make sure that output model can be reused.
-BOOST_AUTO_TEST_CASE(GmmTrainReuseGmmModel)
-{
-  int g = 3;
-  int t = 2;
-  int mi = 5;
-
-  arma::mat inputData;
-  if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
-
-
-  SetinputParam("input" , std::move(inputdata));
-  SetinputParam("gaussians", g);
-  SetinputParam("trials" , t);
-  SetinputParam("max_iterations" ,mi);
-
-  mlpackMain();
-  
-  GMM* gmm = CLI::GetParam<GMM*>("output_model");
-
-  SetinputParam("input_model" , gmm);
-
-  CLI::GetSingleton().Parameters()["gaussians"].wasPassed = false;
-  CLI::GetSingleton().Parameters()["trials"].wasPassed = false;
-  CLI::GetSingleton().Parameters()["max_iterations"].wasPassed = false;
-
-  mlpackMain();
-
-  GMM* gmm1 = CLI::GetParam<GMM*>("output_model");
-
-  ApproximatelyEqual(gmm, gmm1);
-
-}
-
 // Number of gaussians in the model trained from input model.
 BOOST_AUTO_TEST_CASE(GmmTrainNumberOfGaussian)
 {
@@ -268,19 +204,18 @@ BOOST_AUTO_TEST_CASE(GmmTrainNumberOfGaussian)
 
   arma::mat inputData;
   if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
+    BOOST_FAIL("Unable to load train dataset vc2.csv!");
 
+  SetInputParam("input" , std::move(inputData));
+  SetInputParam("gaussians", g);
+  SetInputParam("trials" , t);
+  SetInputParam("max_iterations" ,mi);
 
-  SetinputParam("input" , std::move(inputdata));
-  SetinputParam("gaussians", g);
-  SetinputParam("trials" , t);
-  SetinputParam("max_iterations" ,mi)
-
-  mlpakMain();
+  mlpackMain();
   
   GMM* gmm = CLI::GetParam<GMM*>("output_model");
 
-  SetinputParam("input_model" , gmm);
+  SetInputParam("input_model" , gmm);
 
   CLI::GetSingleton().Parameters()["gaussians"].wasPassed = false;
   CLI::GetSingleton().Parameters()["trials"].wasPassed = false;
@@ -291,32 +226,6 @@ BOOST_AUTO_TEST_CASE(GmmTrainNumberOfGaussian)
   GMM* gmm1 = CLI::GetParam<GMM*>("output_model");
 
   BOOST_REQUIRE_EQUAL(gmm1->gaussians(), g);
-
-}
-
-// Checking that after noise addition matrix is invertible.
-BOOST_AUTO_TEST_CASE(GmmTrainNoiseAddition)
-{
-  int g = 3;
-  int trials = 2;
-  int maxi = 5;
-  double noise = 0.68;
-
-  arma::mat inputData;
-  if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
-
-  SetinputParam("input" , std::move(inputdata));
-  SetinputParam("gaussians", g);
-  SetinputParam("trials" , trials);
-  SetinputParam("max_iterations" ,maxi);
-  SetInputParam("noise" , noise);
-
-  mlpackMain();
-
-  GMM* gmm = CLI::GeParam<GMM*>("output_model");
-
-  BOOST_REQUIRE_EQUAL(gmm->Diagonal_covariance(), true);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
