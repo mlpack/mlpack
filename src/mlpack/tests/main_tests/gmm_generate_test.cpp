8b07d2a293315aac46b114455a2677ffea965c9a
gauravcr7rm <gauravcr7rm@gmail.com>
2019-02-13 16:02:21 +0530

    Final Updation

--- src/mlpack/tests/main_tests/gmm_generate_test.cpp
+++ src/mlpack/tests/main_tests/gmm_generate_test.cpp
@@ -10,20 +10,14 @@
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
 #define BINDING_TYPE BINDING_TYPE_TEST
+static const std::string testName = "GmmGenerate";
 
 #include <mlpack/core.hpp>
-static const std::string testName = "gmm_generate"
-
 #include <mlpack/core/util/mlpack_main.hpp>
 #include <mlpack/methods/gmm/gmm_generate_main.cpp>
-#include <mlpack/methods/gmm/gmm_train_main.cpp>
-#include <mlpack/methods/gmm/no_constraint.hpp>
-#include <mlpack/methods/gmm/positive_definite_constraint.hpp>
-#include <mlpack/methods/gmm/diagonal_constraint.hpp>
-#include <mlpack/methods/gmm/eigenvalue_ratio_constraint.hpp>
-#include "test_helper.hpp"
 
-#include <boost/test/unit_tests.hpp>
+#include "test_helper.hpp"
+#include <boost/test/unit_test.hpp>
 #include "../test_tools.hpp"
 
 using namespace mlpack;
@@ -33,7 +27,7 @@ struct GmmGenerateTestFixture
   public:
    GmmGenerateTestFixture()
    {
-     CLI::RestoreSettings(testname);
+     CLI::RestoreSettings(testName);
    }
 
    ~GmmGenerateTestFixture()
@@ -43,18 +37,17 @@ struct GmmGenerateTestFixture
    }
 };
 
-BOOST_FIXTURE_TEST_SUITE(GmmGenerateMainTest,
-                         GmmGenerateTestFixture);
+BOOST_FIXTURE_TEST_SUITE(GmmGenerateMainTest, GmmGenerateTestFixture);
 
 // Making sure samples are provided.
 BOOST_AUTO_TEST_CASE(GmmGenerateSamples)
 {
   arma::mat inputData;
   if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
+    BOOST_FAIL("Unable to load train dataset vc2.csv!");
   
   GMM gmm(1, 2);
-  gmm.Train(inputData, 10);
+  gmm.Train(inputData, 2);
   SetInputParam("input_model", std::move(gmm));
 
   Log::Fatal.ignoreInput = true;
@@ -64,14 +57,15 @@ BOOST_AUTO_TEST_CASE(GmmGenerateSamples)
 
 // Checking dimensionality of output.
 BOOST_AUTO_TEST_CASE(GmmGenerateDimensionality)
-{ int samples =10;
+{ 
+  int samples = 10;
 
   arma::mat inputData;
   if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
+    BOOST_FAIL("Unable to load train dataset vc2.csv!");
   
   GMM gmm(1, 2);
-  gmm.Train(data, 10);
+  gmm.Train(inputData, 2);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
 
   SetInputParam("input_model", std::move(gmm));
   SetInputParam("samples", samples);
@@ -80,30 +74,8 @@ BOOST_AUTO_TEST_CASE(GmmGenerateDimensionality)
 
   arma::mat output = CLI::GetParam<arma::mat>("output");
 
-  BOOST_REQUIRE_EQUAL(output.n_rows, gmm->Dimensionality());
+  BOOST_REQUIRE_EQUAL(output.n_rows, gmm.Dimensionality());
   BOOST_REQUIRE_EQUAL(output.n_cols, samples);
- 
-}
-
-// Samples passed should be positive.
-BOOST_AUTO_TEST_CASE(GmmGeneratePositiveSample)
-{
-  int samples = -1;
-
-  arma::mat inputData;
-  if (!data::Load("vc2.csv", inputData))
-  BOOST_FAIL("Unable to load train dataset vc2.csv!");
-  
-  GMM gmm(1, 2);
-  gmm.Train(data, 10);
-
-  SetInputParam("input_model", std::move(gmm));
-  SetInputParam("samples", samples); // Invalid
-
-
-  Log::Fatal.ignoreInput = true;
-  BOOST_REQUIRE_THROW(mlpackMain(), std::runtime_error);
-  Log::Fatal.ignoreInput = false;
-}
+ }
 
 BOOST_AUTO_TEST_SUITE_END();
