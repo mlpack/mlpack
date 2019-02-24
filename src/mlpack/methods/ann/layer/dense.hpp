14b72e7955e4f807bf19085889809ff7ff5aa5b2
rajiv2605 <rajiv.vaidyanathan4@gmail.com>
2019-02-24 22:18:47 +0530

    Added Dense block class definition

--- src/mlpack/methods/ann/layer/dense.hpp
+++ src/mlpack/methods/ann/layer/dense.hpp
@@ -49,6 +49,110 @@ template<typename InputDataType = arma::mat,
          typename OutputDataType = arma::mat>
 class Dense
 {
+ public:
+  //! Create the Dense object.
+  Dense();
+
+  /**
+   * Create the Dense layer object for a specific configuration.
+   * 
+   * @param nb_layers The number of convolution blocks to append to the block.
+   * @param growth_rate Growth rate of the dense block.
+   * @param bottleneck Bottleneck convolution block is added to each convolution
+   *        block if true.
+   * @param dropout_rate Dropout rate for the dropout layer.
+   * @param weight_decay Weight decay factor.
+   */
+  Dense(const size_t nb_layers, const size_t growth_rate,
+    const bool bottleneck = false, const double dropout_rate,
+    const double weight_decay = 1e-4);
+
+  /**
+   * Ordinary feed forward pass of the dense layer.
+   *
+   * @param input Input data used for evaluating the specified function.
+   * @param output Resulting output activation.
+   */
+  template<typename eT>
+  void Forward(const arma::Mat<eT>&& input, arma::Mat<eT>&& output);
+
+  /**
+   * Ordinary feed backward pass of the dense layer.
+   *
+   * @param input The propagated input activation.
+   * @param gy The backpropagated error.
+   * @param g The calculated gradient.
+   */
+  template<typename eT>
+  void Backward(const arma::Mat<eT>&& /* input */,
+                arma::Mat<eT>&& gy,
+                arma::Mat<eT>&& g);
+
+  /**
+   * Calculate the gradient using the output delta and the input activations.
+   *
+   * @param input The input activations
+   * @param error The calculated error
+   * @param gradient The calculated gradient.
+   */
+  template<typename eT>
+  void Gradient(const arma::Mat<eT>&& input,
+                arma::Mat<eT>&& error,
+                arma::Mat<eT>&& gradient);
+
+  //! Get the input parameter.
+  InputDataType const& InputParameter() const { return inputParameter; }
+  //! Modify the input parameter.
+  InputDataType& InputParameter() { return inputParameter; }
+
+  //! Get the output parameter.
+  OutputDataType const& OutputParameter() const { return outputParameter; }
+  //! Modify the output parameter.
+  OutputDataType& OutputParameter() { return outputParameter; }
+
+  //! Get the delta.
+  OutputDataType const& Delta() const { return delta; }
+  //! Modify the delta.
+  OutputDataType& Delta() { return delta; }
+
+  //! Get the gradient.
+  OutputDataType const& Gradient() const { return gradient; }
+  //! Modify the gradient.
+  OutputDataType& Gradient() { return gradient; }
+
+  /**
+   * Serialize the layer
+   */
+  template<typename Archive>
+  void serialize(Archive& ar, const unsigned int /* version */);
+
+ private:
+  //! Locally-stored number of convolution blocks to append to the block.
+  size_t nb_layers;
+
+  //! Locally-stored growth rate of the dense block.
+  size_t growth_rate;
+
+  //! Locally-stored value to decide if bottleneck has to be added.
+  bool bottleneck;
+
+  //! Locally-stored dropout rate for the dropout layer.
+  double dropout_rate;
+
+  //! Locally-stored weight decay factor.
+  double weight_decay;
+
+  //! Locally-stored input parameter object.
+  InputDataType inputParameter;
+
+  //! Locally-stored output parameter object.
+  OutputDataType outputParameter;
+
+  //! Locally-stored delta object.
+  OutputDataType delta;
+
+  //! Locally-stored gradient object.
+  OutputDataType gradient;
 }; // class Dense
 
 } // namespace ann
