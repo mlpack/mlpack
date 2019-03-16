8e500be96c4196f93fbd2f9b892d4c9faa58b36d
jeffinsam <jeffinsam@karunya.edu.in>
2019-03-15 20:16:36 -0400

    mlpack style of coding

--- src/mlpack/core/data/normalize_labels_impl.hpp
+++ src/mlpack/core/data/normalize_labels_impl.hpp
@@ -40,33 +40,30 @@ void NormalizeLabels(const RowType& labelsIn,
   // we'll resize it back down to its actual size.
   mapping.set_size(labelsIn.n_elem);
   labels.set_size(labelsIn.n_elem);
-  
   // Map for mapping labelIn to their label 
-  std::map<eT,size_t> hastTable;
+  std::map<eT, size_t> hastTable;
   size_t curLabel = 0;
   for (size_t i = 0; i < labelsIn.n_elem; ++i)
   {
     // If labelsIn[i] aldeardy there in Map then just its label
-    if (hastTable[labelsIn[i]]!=0)
+    if (hastTable[labelsIn[i]] != 0)
     {
-  	  labels[i]=hastTable[labelsIn[i]]-1;
-  	}
-  	else
-  	{
-  	  // If labelsIn[i] not there then add it to Map
-  	  hastTable[labelsIn[i]]=curLabel+1;
-  	  labels[i]=curLabel;
+      labels[i] = hastTable[labelsIn[i]]-1;
+    }
+    else
+    {
+      // If labelsIn[i] not there then add it to Map
+      hastTable[labelsIn[i]] = curLabel+1;
+      labels[i] = curLabel;
       ++curLabel;
-  	}
-  
+    }
   }
-
   // Resize mapping back down to necessary size.
   mapping.resize(curLabel);
   // Mapping array created with encoded labels
   for (auto it=hastTable.begin(); it!=hastTable.end(); ++it)
   {
-    mapping[(it->second)-1]=it->first;
+    mapping[(it->second)-1] = it->first;
   }
   hastTable.clear();
 }
