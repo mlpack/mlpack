dd5fa6ea7e263ea8a520203b9602f97179be5aad
Gaurav Sharma <gsharma81999@gmail.com>
2019-03-19 16:29:18 +0530

    Add r_squared.hpp and r_squared_impl.hpp
    This commit will add the two mentioned files, r_squared.hpp contains RSquared class which contain the prototype of Evaluate method to calculate and return the r2 value. R squared is one of the very important metric from statistics point of view and tell how much our model is depending on input values, also known as coeff of determination, r_squared_impl.hpp contains the implemention of this function.

---
+++ src/mlpack/core/cv/metrics/r_squared.hpp
@@ -0,0 +1,56 @@
+/**
+ * @file r_squared.hpp
+ * @author Gaurav Sharma
+ *
+ * The R_Squared metric.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_CORE_CV_METRICS_R_SQUARED_HPP
+#define MLPACK_CORE_CV_METRICS_R_SQUARED_HPP
+
+#include <mlpack/core.hpp>
+
+namespace mlpack {
+namespace cv {
+
+/**
+ * The R-Squared also known as coefficient of determination is a metric of
+ * performance for regression algorithms. It is the proportion of the
+ * variance in Y being explained by the variation in X, and is given
+ * by the ratio SSR/SST, where SSR is sum of squares regression and
+ * SST is total sum of squares.
+ */
+class RSquared
+{
+ public:
+  /**
+   * Run prediction and calculate the r-squared .
+   *
+   * @param model A regression model.
+   * @param data Column-major data containing test items.
+   * @param responses Ground truth (correct) target values for the test items,
+   *     should be either a row vector or a column-major matrix.
+   */
+  template<typename MLAlgorithm, typename DataType, typename ResponsesType>
+  static double Evaluate(MLAlgorithm& model,
+                         const DataType& data,
+                         const ResponsesType& responses);
+
+  /**
+   * Information for hyper-parameter tuning code. It indicates that we want
+   * to minimize the measurement.
+   */
+  static const bool NeedsMinimization = false;
+};
+
+} // namespace cv
+} // namespace mlpack
+
+// Include implementation.
+#include "r_squared_impl.hpp"
+
+#endif
---
+++ src/mlpack/core/cv/metrics/r_squared_impl.hpp
@@ -0,0 +1,44 @@
+/**
+ * @file r_squared_impl.hpp
+ * @author Gaurav Sharma
+ *
+ * The implementation of the class RSquared.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_CORE_CV_METRICS_R_SQUARED_IMPL_HPP
+#define MLPACK_CORE_CV_METRICS_R_SQUARED_IMPL_HPP
+
+#include <mlpack/core/cv/metrics/facilities.hpp>
+
+namespace mlpack {
+namespace cv {
+
+template<typename MLAlgorithm, typename DataType, typename ResponsesType>
+double RSquared::Evaluate(MLAlgorithm& model,
+                     const DataType& data,
+                     const ResponsesType& responses)
+{
+  AssertColumnSizes(data, responses, "RSquared::Evaluate()");  
+
+  ResponsesType predictedResponses;
+  model.Predict(data, predictedResponses);
+  
+  double mean_responses = arma::mean(responses);
+
+  //calculate SSR
+  double SSR = arma::accu(arma::square(predictedResponses - mean_responses));
+
+  //calculate SST
+  double SST = arma::accu(arma::square(responses - mean_responses));
+
+  return SSR / SST;
+}
+
+} // namespace cv
+} // namespace mlpack
+
+#endif
