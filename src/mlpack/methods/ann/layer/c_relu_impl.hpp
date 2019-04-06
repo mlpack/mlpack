0e81e5bea6b66ef1ce441d9b985523fa8bb646c6
jeffinsam <jeffinsam@karunya.edu.in>
2019-04-06 10:26:04 -0400

    styling issues

--- src/mlpack/methods/ann/layer/c_relu_impl.hpp
+++ src/mlpack/methods/ann/layer/c_relu_impl.hpp
@@ -37,7 +37,7 @@ void CReLU<InputDataType, OutputDataType>::Forward(
   OutputType temp1;
   OutputType temp2;
   Fn(input, temp1);
-  InputType inptemp=-1*input;
+  InputType inptemp = -1 * input;
   Fn(inptemp, temp2);
   // Concat Neg and Pos Relu
   output = arma::join_cols(temp1,temp2);
@@ -52,7 +52,7 @@ void CReLU<InputDataType, OutputDataType>::Backward(
   Deriv(input,derivative);
   DataType temp;
   temp = gy % derivative;
-  g= temp.rows(0, (input.n_rows/2-1) )-temp.rows(input.n_rows/2,(input.n_rows-1));
+  g= temp.rows(0, (input.n_rows / 2 - 1)) - temp.rows(input.n_rows / 2, (input.n_rows - 1));
 
   /**
   * Below implementation was a different varient but couldn't manage to implement it.
