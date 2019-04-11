0077a0dcdb38d69fd4ee5dc1eb21e18a7dba62bb
jeffinsam <jeffinsam@karunya.edu.in>
2019-04-11 19:10:03 -0400

    added neccessary includes

--- src/mlpack/core/math/ccov.hpp
+++ src/mlpack/core/math/ccov.hpp
@@ -3,7 +3,7 @@
  * @author Ryan Curtin
  * @author Conrad Sanderson
  *
- * ccov(X) is same as cov(trans(X)) but without the cost of computing trans(X)
+ * ColumnCovariance(X) is same as cov(trans(X)) but without the cost of computing trans(X)
  *
  * mlpack is free software; you may redistribute it and/or modify it under the
  * terms of the 3-clause BSD license.  You should have received a copy of the
@@ -13,17 +13,20 @@
 #ifndef MLPACK_CORE_MATH_CCOV_HPP
 #define MLPACK_CORE_MATH_CCOV_HPP
 
+#include <mlpack/core.hpp>
+#include <mlpack/core/util/log.hpp>
+
 namespace mlpack {
 namespace math /** Miscellaneous math routines. */ {
 
 template<typename eT>
 inline
 arma::Mat<eT>
-ccov(const arma::Mat<eT>& A, const arma::uword norm_type = 0)
+ColumnCovariance(const arma::Mat<eT>& A, const arma::size_t norm_type = 0)
 {
   if (norm_type > 1)
   {
-    Log::Fatal << "ccov(): norm_type must be 0 or 1" << std::endl;
+    Log::Fatal << "ColumnCovariance(): norm_type must be 0 or 1" << std::endl;
   }
 
   arma::Mat<eT> out;
@@ -41,7 +44,7 @@ ccov(const arma::Mat<eT>& A, const arma::uword norm_type = 0)
   }
   else
   {
-    const arma::uword N = A.n_cols;
+    const arma::size_t N = A.n_cols;
     const eT norm_val = (norm_type == 0) ?
         ( (N > 1) ? eT(N-1) : eT(1) ) : eT(N);
 
@@ -55,17 +58,15 @@ ccov(const arma::Mat<eT>& A, const arma::uword norm_type = 0)
   return out;
 }
 
-
-
 template<typename T>
 inline
 arma::Mat< std::complex<T> >
-ccov(const arma::Mat< std::complex<T> >& A,
-     const arma::uword norm_type = 0)
+ColumnCovariance(const arma::Mat< std::complex<T> >& A,
+     const arma::size_t norm_type = 0)
 {
   if (norm_type > 1)
   {
-    Log::Fatal << "ccov(): norm_type must be 0 or 1" << std::endl;
+    Log::Fatal << "ColumnCovariance(): norm_type must be 0 or 1" << std::endl;
   }
 
   typedef typename std::complex<T> eT;
@@ -89,7 +90,7 @@ ccov(const arma::Mat< std::complex<T> >& A,
   }
   else
   {
-    const arma::uword N = A.n_cols;
+    const arma::size_t N = A.n_cols;
     const eT norm_val = (norm_type == 0) ?
         ( (N > 1) ? eT(N-1) : eT(1) ) : eT(N);
 
@@ -102,151 +103,6 @@ ccov(const arma::Mat< std::complex<T> >& A,
 
   return out;
 }
-
-
-
-template<typename eT>
-inline
-arma::Mat<eT>
-ccov(const arma::Mat<eT>& A,
-     const arma::Mat<eT>& B,
-     const arma::uword norm_type = 0)
-{
-  if (norm_type > 1)
-  {
-    Log::Fatal << "ccov(): norm_type must be 0 or 1" << std::endl;
-  }
-
-  arma::Mat<eT> out;
-
-  if (A.is_vec() && B.is_vec())
-  {
-    if (A.n_elem != B.n_elem)
-    {
-      Log::Fatal << "ccov(): the number of elements in A and B must match"
-          << std::endl;
-    }
-
-    const eT* A_ptr = A.memptr();
-    const eT* B_ptr = B.memptr();
-
-    eT A_acc   = eT(0);
-    eT B_acc   = eT(0);
-    eT out_acc = eT(0);
-
-    const arma::uword N = A.n_elem;
-
-    for (arma::uword i = 0; i < N; ++i)
-    {
-      const eT A_tmp = A_ptr[i];
-      const eT B_tmp = B_ptr[i];
-
-      A_acc += A_tmp;
-      B_acc += B_tmp;
-
-      out_acc += A_tmp * B_tmp;
-    }
-
-    out_acc -= (A_acc * B_acc) / eT(N);
-
-    const eT norm_val = (norm_type == 0) ?
-        ( (N > 1) ? eT(N-1) : eT(1) ) : eT(N);
-
-    out.set_size(1, 1);
-    out[0] = out_acc/norm_val;
-  }
-  else
-  {
-    if ( (A.n_rows != B.n_rows) || (A.n_cols != B.n_cols) )
-    {
-      Log::Fatal << "ccov(): size of A and B must match" << std::endl;
-    }
-
-    const arma::uword N = A.n_cols;
-    const eT norm_val = (norm_type == 0) ?
-        ( (N > 1) ? eT(N-1) : eT(1) ) : eT(N);
-
-    out = A * arma::trans(B);
-    out -= (sum(A) * arma::trans(sum(B))) / eT(N);
-    out /= norm_val;
-  }
-
-  return out;
-}
-
-
-
-template<typename T>
-inline
-arma::Mat< std::complex<T> >
-ccov(const arma::Mat< std::complex<T> >& A,
-     const arma::Mat< std::complex<T> >& B,
-     const arma::uword norm_type = 0)
-{
-  if (norm_type > 1)
-  {
-    Log::Fatal << "ccov(): norm_type must be 0 or 1" << std::endl;
-  }
-
-  typedef typename std::complex<T> eT;
-
-  arma::Mat<eT> out;
-
-  if (A.is_vec() && B.is_vec())
-  {
-    if (A.n_elem != B.n_elem)
-    {
-      Log::Fatal << "ccov(): the number of elements in A and B must match"
-          << std::endl;
-    }
-
-    const eT* A_ptr = A.memptr();
-    const eT* B_ptr = B.memptr();
-
-    eT A_acc   = eT(0);
-    eT B_acc   = eT(0);
-    eT out_acc = eT(0);
-
-    const arma::uword N = A.n_elem;
-
-    for (arma::uword i = 0; i < N; ++i)
-    {
-      const eT A_tmp = A_ptr[i];
-      const eT B_tmp = B_ptr[i];
-
-      A_acc += A_tmp;
-      B_acc += B_tmp;
-
-      out_acc += std::conj(A_tmp) * B_tmp;
-    }
-
-    out_acc -= (std::conj(A_acc) * B_acc) / eT(N);
-
-    const eT norm_val = (norm_type == 0) ?
-        ( (N > 1) ? eT(N-1) : eT(1) ) : eT(N);
-
-    out.set_size(1, 1);
-    out[0] = out_acc/norm_val;
-  }
-  else
-  {
-    if ( (A.n_rows != B.n_rows) || (A.n_cols != B.n_cols) )
-    {
-      Log::Fatal << "ccov(): size of A and B must match" << std::endl;
-    }
-
-    const arma::uword N = A.n_cols;
-    const eT norm_val = (norm_type == 0) ?
-        ( (N > 1) ? eT(N-1) : eT(1) ) : eT(N);
-
-    out = A * arma::trans(arma::conj(B));
-    out -= (sum(A) * arma::trans(arma::conj(arma::sum(B)))) / eT(N);
-    out /= norm_val;
-  }
-
-  return out;
-}
-
 
 } // namespace math
 } // namespace mlpack
