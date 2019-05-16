267eb51d45dc833b1b52caa488e54fc32f866cd7
Ryan Curtin <ryan@ratml.org>
2019-05-16 19:34:23 -0400

    Another attempt to make the Windows build work.

--- src/mlpack/bindings/python/ConfigureSetup.cmake
+++ src/mlpack/bindings/python/ConfigureSetup.cmake
@@ -12,6 +12,18 @@
 # - OpenMP_CXX_FLAGS: OpenMP C++ compilation flags
 # - DISABLE_CFLAGS: list of CFLAGS or CXXFLAGS to be disabled
 # - CYTHON_INCLUDE_DIRECTORIES: include directories for Cython
+# - MLPACK_LIBDIR: path to mlpack libraries
+# - BOOST_LIBDIRS: paths to Boost libraries
 # - OUTPUT_DIR: binary output directory for CMake
-message(STATUS "OUTPUT_DIR is ${OUTPUT_DIR}")
+
+get_filename_component(Boost_SERIALIZATION_LIB_NAME_IN
+    ${Boost_SERIALIZATION_LIBRARY} NAME_WE)
+get_filename_component(MLPACK_LIB_NAME_IN
+    ${MLPACK_LIBRARY} NAME_WE)
+
+# Strip 'lib' off the front of the name, if it's there.
+string(REGEX REPLACE "^lib" "" Boost_SERIALIZATION_LIB_NAME
+    "${Boost_SERIALIZATION_LIB_NAME_IN}")
+string(REGEX REPLACE "^lib" "" MLPACK_LIB_NAME "${MLPACK_LIB_NAME_IN}")
+
 configure_file(${SETUP_PY_IN} ${SETUP_PY_OUT})
