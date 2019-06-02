a3b3362f3b7be4c5f0c2842326eaace883c1dd12
Saksham Bansal <7020962+saksham189@users.noreply.github.com>
2019-06-02 20:01:01 +0700

    Add Inception Score

---
+++ src/mlpack/methods/ann/gan/metrics/inception_score.hpp
@@ -0,0 +1,39 @@
+/**
+ * @file inception_score.hpp
+ * @author Saksham Bansal
+ *
+ * Definition of Inception Score for Generative Adversarial Networks.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+
+#ifndef MLPACK_METHODS_METRICS_INCEPTION_SCORE_HPP
+#define MLPACK_METHODS_METRICS_INCEPTION_SCORE_HPP
+
+#include <mlpack/prereqs.hpp>
+
+namespace mlpack {
+namespace ann /* Artificial Neural Network */ {
+
+/**
+* Function that computes Inception Score for
+*
+* @param trueOutputs Ground truth sequences.
+* @param predOutputs Sequences predicted by model.
+* @param tol Minimum absolute difference value
+*            which is considered as a model failure.
+*/
+template<typename ModelType>
+double InceptionScore(ModelType Model,
+                      arma::mat images);
+
+
+} // namespace ann
+} // namespace mlpack
+
+#include "inception_score_impl.hpp"
+
+#endif
