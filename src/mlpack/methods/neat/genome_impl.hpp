3e43fd48f78f62e05b54e461039cbb3bc1bf4e81
Rahul Prabhu <cupertinorp@gmail.com>
2019-06-06 00:02:41 +0530

    Fixed typo in description.

--- src/mlpack/methods/neat/genome_impl.hpp
+++ src/mlpack/methods/neat/genome_impl.hpp
@@ -6,8 +6,7 @@
  *
  * mlpack is free software; you may redistribute it and/or modify it under the
  * terms of the 3-clause BSD license.  You should have received a copy of the
- * 3-clause BSD license along with mlpack.  If n
- ot, see
+ * 3-clause BSD license along with mlpack.  If not, see
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
 #ifndef MLPACK_METHODS_NEAT_GENOME_IMPL_HPP
