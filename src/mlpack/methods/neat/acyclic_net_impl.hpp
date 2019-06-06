589f946d0bf6e8cf71488e2fd0f7b7453af78182
Rahul Prabhu <cupertinorp@gmail.com>
2019-06-06 20:37:09 +0530

    Fixed acyclic net evaluation.

--- src/mlpack/methods/neat/acyclic_net_impl.hpp
+++ src/mlpack/methods/neat/acyclic_net_impl.hpp
@@ -77,8 +77,8 @@ arma::vec AcyclicNet<ActivationFunction>::Evaluate(arma::vec input)
   std::map<size_t, double> nodeValues;
   // Add all the nodes to the map.
   for (size_t i = 0; i < nodeGeneList.size(); i++)
-    if (nodeGeneList[i] <= inputNodeCount)
-      nodeValues[nodeGeneList[i]] = input[nodeGeneList[i]-1];
+    if (nodeGeneList[i] <= inputNodeCount && i != 0)
+      nodeValues[nodeGeneList[i]] = input[i-1];
     else
       nodeValues[nodeGeneList[i]] = 0;
 
@@ -90,14 +90,14 @@ arma::vec AcyclicNet<ActivationFunction>::Evaluate(arma::vec input)
       int nodeID = layers[i][j];
       if (nodeID == 0)
       {
-        for (auto const& x : directedGraph)
-          nodeValues[x.first] += bias;
+        for (auto const& x : directedGraph[nodeID])
+          nodeValues[x.first] += bias * x.second.getWeight();
       }
       else
       {
         double result = actFn.Fn(nodeValues[nodeID]);
         for (auto const& x : directedGraph)
-          nodeValues[x.first] += result;
+          nodeValues[x.first] += result * x.second.getWeight();
       }
     }
   }
