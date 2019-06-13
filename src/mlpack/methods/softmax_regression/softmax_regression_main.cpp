e08b0c09fe9ba6c570919e000f6777912527dbcd
Yashwant Singh Parihar <Yashwantsingh.sngh@gmail.com>
2019-06-13 21:15:36 +0530

    Update softmax_regression_main.cpp

--- src/mlpack/methods/softmax_regression/softmax_regression_main.cpp
+++ src/mlpack/methods/softmax_regression/softmax_regression_main.cpp
@@ -200,10 +200,6 @@ void TestClassifyAcc(size_t numClasses, const Model& model)
   arma::Row<size_t> predictLabels;
   model.Classify(testData, predictLabels);
 
-  // Save predictions, if desired.
-  if (CLI::HasParam("predictions"))
-    CLI::GetParam<arma::Row<size_t>>("predictions") = predictLabels;
-
   // Calculate accuracy, if desired.
   if (CLI::HasParam("test_labels"))
   {
@@ -242,6 +238,9 @@ void TestClassifyAcc(size_t numClasses, const Model& model)
         << (totalBingo) / static_cast<double>(predictLabels.n_elem) << " ("
         << totalBingo << " of " << predictLabels.n_elem << ")." << endl;
   }
+  // Save predictions, if desired.
+  if (CLI::HasParam("predictions"))
+    CLI::GetParam<arma::Row<size_t>>("predictions") = std::move(predictLabels);
 }
 
 template<typename Model>
