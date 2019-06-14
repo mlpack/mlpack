cd689ba8a439a8635d653a8e28d2242345c11f6f
Rahul Prabhu <cupertinorp@gmail.com>
2019-06-14 19:05:03 +0530

    Commented out tests besides XOR for debugging.

--- src/mlpack/tests/neat_test.cpp
+++ src/mlpack/tests/neat_test.cpp
@@ -12,13 +12,21 @@
 
 #include <mlpack/core.hpp>
 
+// NEAT resources.
 #include <mlpack/methods/neat/neat.hpp>
+#include <mlpack/methods/neat/selection_strategies/rank_selection.hpp>
+
+// RL environments.
 #include <mlpack/methods/reinforcement_learning/environment/mountain_car.hpp>
 #include <mlpack/methods/reinforcement_learning/environment/continuous_mountain_car.hpp>
 #include <mlpack/methods/reinforcement_learning/environment/cart_pole.hpp>
 #include <mlpack/methods/reinforcement_learning/environment/continuous_multiple_pole_cart.hpp>
 #include <mlpack/methods/reinforcement_learning/environment/acrobot.hpp>
 #include <mlpack/methods/reinforcement_learning/environment/pendulum.hpp>
+
+// Activation functions.
+#include <mlpack/methods/ann/activation_functions/logistic_function.hpp>
+
 #include <mlpack/tests/neat_test_tools.hpp>
 
 #include <boost/test/unit_test.hpp>
@@ -26,6 +34,7 @@
 
 using namespace mlpack;
 using namespace mlpack::rl;
+using namespace mlpack::ann;
 using namespace mlpack::neat;
 
 BOOST_AUTO_TEST_SUITE(NEATTest)
@@ -35,140 +44,142 @@ BOOST_AUTO_TEST_SUITE(NEATTest)
  */
 BOOST_AUTO_TEST_CASE(NEATXORTest)
 {
-  XORTask task();
-  NEAT<XORTask> model(task, 2, 1);
+  XORTask task;
+  NEAT<XORTask, LogisticFunction, RankSelection> model(task, 2, 1, 50, 50, 5, 1, 0.8, 0.5, 0.8, 0.5, 0.2, 0.5, 0.2, 0.1, true);
 
   // Find the best genome and it's fitness.
-  Genome bestGenome = model.Train();
+  Genome<LogisticFunction> bestGenome = model.Train();
+  std::cout << bestGenome.Fitness() << std::endl;
+  bestGenome.Parameters().print();
   double finalFitness = task.Evaluate(bestGenome);
 
   // Check if the final fitness is acceptable.
   BOOST_REQUIRE(finalFitness >= 3.9);
 }
 
-/**
- * Test NEAT on the Pendulum environment.
- */
-BOOST_AUTO_TEST_CASE(NEATPendulumTest)
-{
-  const Pendulum env = Pendulum();
-  ContinuousRLTask<Pendulum> task(env);
-  NEAT<ContinuousRLTask<Pendulum>> model(task, 2, 1);
+// /**
+//  * Test NEAT on the Pendulum environment.
+//  */
+// BOOST_AUTO_TEST_CASE(NEATPendulumTest)
+// {
+//   const Pendulum env = Pendulum();
+//   ContinuousRLTask<Pendulum> task(env);
+//   NEAT<ContinuousRLTask<Pendulum>> model(task, 2, 1);
 
-  // Find the best genome and it's fitness.
-  Genome bestGenome = model.Train();
-  double finalFitness = task.Evaluate(bestGenome);
+//   // Find the best genome and it's fitness.
+//   Genome bestGenome = model.Train();
+//   double finalFitness = task.Evaluate(bestGenome);
 
-  // Check if the final fitness is acceptable (Placeholder).
-  BOOST_REQUIRE(finalFitness >= 90);
-}
+//   // Check if the final fitness is acceptable (Placeholder).
+//   BOOST_REQUIRE(finalFitness >= 90);
+// }
 
-/**
- * Test NEAT on the ContinuousMountainCar environment.
- */
-BOOST_AUTO_TEST_CASE(NEATContinuousMountainCarTest)
-{
-  const ContinuousMountainCar task = ContinuousMountainCar();
-  ContinuousRLTask<ContinuousMountainCar> task(env);
-  NEAT<ContinuousRLTask<ContinuousMountainCar>> model(task, 2, 1);
+// /**
+//  * Test NEAT on the ContinuousMountainCar environment.
+//  */
+// BOOST_AUTO_TEST_CASE(NEATContinuousMountainCarTest)
+// {
+//   const ContinuousMountainCar task = ContinuousMountainCar();
+//   ContinuousRLTask<ContinuousMountainCar> task(env);
+//   NEAT<ContinuousRLTask<ContinuousMountainCar>> model(task, 2, 1);
 
-  // Find the best genome and it's fitness.
-  Genome bestGenome = model.Train();
-  double finalFitness = task.Evaluate(bestGenome);
+//   // Find the best genome and it's fitness.
+//   Genome bestGenome = model.Train();
+//   double finalFitness = task.Evaluate(bestGenome);
 
-  // Check if the final fitness is acceptable (Placeholder).
-  BOOST_REQUIRE(finalFitness >= 90);
-}
+//   // Check if the final fitness is acceptable (Placeholder).
+//   BOOST_REQUIRE(finalFitness >= 90);
+// }
 
-/**
- * Test NEAT on the MountainCar environment.
- */
-BOOST_AUTO_TEST_CASE(NEATMountainCarTest)
-{
-  const MountainCar env = MountainCar();
-  DiscreteRLTask<MountainCar> task(env);
-  NEAT<DiscreteRLTask<MountainCar>> model(task, 2, 1);
+// /**
+//  * Test NEAT on the MountainCar environment.
+//  */
+// BOOST_AUTO_TEST_CASE(NEATMountainCarTest)
+// {
+//   const MountainCar env = MountainCar();
+//   DiscreteRLTask<MountainCar> task(env);
+//   NEAT<DiscreteRLTask<MountainCar>> model(task, 2, 1);
 
-  // Find the best genome and it's fitness.
-  Genome bestGenome = model.Train();
-  double finalFitness = task.Evaluate(bestGenome);
+//   // Find the best genome and it's fitness.
+//   Genome bestGenome = model.Train();
+//   double finalFitness = task.Evaluate(bestGenome);
 
-  // Check if the final fitness is acceptable (Placeholder).
-  BOOST_REQUIRE(finalFitness >= 90);
-}
+//   // Check if the final fitness is acceptable (Placeholder).
+//   BOOST_REQUIRE(finalFitness >= 90);
+// }
 
-/**
- * Test NEAT on the Acrobot environment.
- */
-BOOST_AUTO_TEST_CASE(NEATAcrobotTest)
-{
-  const Acrobot env = Acrobot();
-  DiscreteRLTask<Acrobot> task(env);
-  NEAT<DiscreteRLTask<Acrobot>> model(task, 2, 1);
+// /**
+//  * Test NEAT on the Acrobot environment.
+//  */
+// BOOST_AUTO_TEST_CASE(NEATAcrobotTest)
+// {
+//   const Acrobot env = Acrobot();
+//   DiscreteRLTask<Acrobot> task(env);
+//   NEAT<DiscreteRLTask<Acrobot>> model(task, 2, 1);
 
-  // Find the best genome and it's fitness.
-  Genome bestGenome = model.Train();
-  double finalFitness = task.Evaluate(bestGenome);
+//   // Find the best genome and it's fitness.
+//   Genome bestGenome = model.Train();
+//   double finalFitness = task.Evaluate(bestGenome);
 
-  // Check if the final fitness is acceptable (Placeholder).
-  BOOST_REQUIRE(finalFitness >= 90);
-}
+//   // Check if the final fitness is acceptable (Placeholder).
+//   BOOST_REQUIRE(finalFitness >= 90);
+// }
 
-/**
- * Test NEAT on the CartPole environment.
- */
-BOOST_AUTO_TEST_CASE(NEATCartPoleTest)
-{
-  const CartPole env = CartPole();
-  DiscreteRLTask<CartPole> task(env);
-  NEAT<DiscreteRLTask<CartPole>> model(task, 2, 1);
+// /**
+//  * Test NEAT on the CartPole environment.
+//  */
+// BOOST_AUTO_TEST_CASE(NEATCartPoleTest)
+// {
+//   const CartPole env = CartPole();
+//   DiscreteRLTask<CartPole> task(env);
+//   NEAT<DiscreteRLTask<CartPole>> model(task, 2, 1);
 
-  // Find the best genome and it's fitness.
-  Genome bestGenome = model.Train();
-  double finalFitness = task.Evaluate(bestGenome);
+//   // Find the best genome and it's fitness.
+//   Genome bestGenome = model.Train();
+//   double finalFitness = task.Evaluate(bestGenome);
 
-  // Check if the final fitness is acceptable (Placeholder).
-  BOOST_REQUIRE(finalFitness >= 90);
-}
+//   // Check if the final fitness is acceptable (Placeholder).
+//   BOOST_REQUIRE(finalFitness >= 90);
+// }
 
-/**
- * Test NEAT on the Double Pole Cart Balancing environment.
- */
-BOOST_AUTO_TEST_CASE(NEATDoublePoleCartTest)
-{
-  arma::vec poleLengths = {1, 0.1};
-  arma::vec poleMasses = {1, 0.1};
-  const ContinuousMultiplePoleCart env = ContinuousMultiplePoleCart(2,
-      poleLengths, poleMasses);
-  DPVTask task(env);
-  NEAT<DPVTask> model(task, 6 /* Input nodes */, 1 /* Output nodes */);
+// /**
+//  * Test NEAT on the Double Pole Cart Balancing environment.
+//  */
+// BOOST_AUTO_TEST_CASE(NEATDoublePoleCartTest)
+// {
+//   arma::vec poleLengths = {1, 0.1};
+//   arma::vec poleMasses = {1, 0.1};
+//   const ContinuousMultiplePoleCart env = ContinuousMultiplePoleCart(2,
+//       poleLengths, poleMasses);
+//   DPVTask task(env);
+//   NEAT<DPVTask> model(task, 6 /* Input nodes */, 1 /* Output nodes */);
 
-  // Find the best genome and it's fitness.
-  Genome bestGenome = model.Train();
-  double finalFitness = task.Evaluate(bestGenome);
+//   // Find the best genome and it's fitness.
+//   Genome bestGenome = model.Train();
+//   double finalFitness = task.Evaluate(bestGenome);
 
-  // Check if the final fitness is acceptable (Placeholder).
-  BOOST_REQUIRE(finalFitness >= 90);
-}
+//   // Check if the final fitness is acceptable (Placeholder).
+//   BOOST_REQUIRE(finalFitness >= 90);
+// }
 
-/**
- * Test NEAT on the Double Pole Cart Balancing environment without velocities.
- */
-BOOST_AUTO_TEST_CASE(NEATDoublePoleCartNoVelocitiesTest)
-{
-  arma::vec poleLengths = {1, 0.1};
-  arma::vec poleMasses = {1, 0.1};
-  const ContinuousMultiplePoleCart env = ContinuousMultiplePoleCart(2,
-      poleLengths, poleMasses);
-  DPNVTask task(env);
-  NEAT<DPNVTask> model(task, 3 /* Input nodes */, 1 /* Output nodes */);
+// /**
+//  * Test NEAT on the Double Pole Cart Balancing environment without velocities.
+//  */
+// BOOST_AUTO_TEST_CASE(NEATDoublePoleCartNoVelocitiesTest)
+// {
+//   arma::vec poleLengths = {1, 0.1};
+//   arma::vec poleMasses = {1, 0.1};
+//   const ContinuousMultiplePoleCart env = ContinuousMultiplePoleCart(2,
+//       poleLengths, poleMasses);
+//   DPNVTask task(env);
+//   NEAT<DPNVTask> model(task, 3 /* Input nodes */, 1 /* Output nodes */);
 
-  // Find the best genome and it's fitness.
-  Genome bestGenome = model.Train();
-  double finalFitness = task.Evaluate(bestGenome);
+//   // Find the best genome and it's fitness.
+//   Genome bestGenome = model.Train();
+//   double finalFitness = task.Evaluate(bestGenome);
 
-  // Check if the final fitness is acceptable (Placeholder).
-  BOOST_REQUIRE(finalFitness >= 90);
-}
+//   // Check if the final fitness is acceptable (Placeholder).
+//   BOOST_REQUIRE(finalFitness >= 90);
+// }
 
 BOOST_AUTO_TEST_SUITE_END()
--- src/mlpack/tests/neat_test_tools.hpp
+++ src/mlpack/tests/neat_test_tools.hpp
@@ -14,19 +14,21 @@
 
 #include <mlpack/core.hpp>
 #include <mlpack/methods/reinforcement_learning/environment/continuous_multiple_pole_cart.hpp>
+#include <mlpack/methods/ann/activation_functions/logistic_function.hpp>
 
 using namespace mlpack;
 using namespace mlpack::neat;
+using namespace mlpack::ann;
 
 // A class that defines the XOR task.
 class XORTask
 {
-  double Evaluate(const Genome& genome)
+ public:
+  double Evaluate(Genome<LogisticFunction>& genome)
   {
     // Create a random input of 0s and 1s.
     arma::vec input = arma::randi<arma::vec>(2, arma::distr_param(0, 1));
-    genome.Input(input);
-    arma::vec output = genome.Output();
+    arma::vec output = genome.Evaluate(input);
 
     // The expected output of the XOR gate.
     arma::vec answer = {(input[0] + input[1]) * (!input[0] + !input[1])};
@@ -36,186 +38,186 @@ class XORTask
   }
 };
 
-/**
- * A Task class that wraps a continuous RL environment.
- */
-template<class EnvironmentType>
-class ContinuousRLTask
-{
-  ContinuousRLTask(const EnvironmentType& env) : environment(env)
-  { /* Nothing to do here */ }
+// /**
+//  * A Task class that wraps a continuous RL environment.
+//  */
+// template<class EnvironmentType>
+// class ContinuousRLTask
+// {
+//   ContinuousRLTask(const EnvironmentType& env) : environment(env)
+//   { /* Nothing to do here */ }
 
-  double Evaluate(Genome& genome)
-  {
-    // Set the initial state.
-    typename EnvironmentType::State state = environment.InitialSample();
-    genome.Input(state.Data());
+//   double Evaluate(Genome& genome)
+//   {
+//     // Set the initial state.
+//     typename EnvironmentType::State state = environment.InitialSample();
+//     genome.Input(state.Data());
 
-    double fitness = 0;
-    while (!environment.IsTerminal())
-    {
-      // Choose the action to perform.
-      typename EnvironmentType::Action action;
-      action.action[0] = genome.Output()[0];
+//     double fitness = 0;
+//     while (!environment.IsTerminal())
+//     {
+//       // Choose the action to perform.
+//       typename EnvironmentType::Action action;
+//       action.action[0] = genome.Output()[0];
 
-      // Use the current action to get the next state.
-      fitness += environment.Sample(state, action, state);
+//       // Use the current action to get the next state.
+//       fitness += environment.Sample(state, action, state);
 
-      // Update the state of the genome for the next step.
-      genome.Input(state.Data());
-    }
-    return fitness;
-  }
+//       // Update the state of the genome for the next step.
+//       genome.Input(state.Data());
+//     }
+//     return fitness;
+//   }
 
- private:
-  EnvironmentType environment;
-};
+//  private:
+//   EnvironmentType environment;
+// };
 
-/**
- * A Task class that wraps a discrete RL environment.
- */
-template<class EnvironmentType>
-class DiscreteRLTask
-{
-  DiscreteRLTask(const EnvironmentType& env) : environment(env)
-  { /* Nothing to do here */ }
+// /**
+//  * A Task class that wraps a discrete RL environment.
+//  */
+// template<class EnvironmentType>
+// class DiscreteRLTask
+// {
+//   DiscreteRLTask(const EnvironmentType& env) : environment(env)
+//   { /* Nothing to do here */ }
 
-  double Evaluate(Genome& genome)
-  {
-    // Set the initial state.
-    typename EnvironmentType::State state = environment.InitialSample();
-    genome.Input(state.Data());
+//   double Evaluate(Genome& genome)
+//   {
+//     // Set the initial state.
+//     typename EnvironmentType::State state = environment.InitialSample();
+//     genome.Input(state.Data());
 
-    double fitness = 0;
-    while (!environment.IsTerminal())
-    {
-      // Choose the action to perform.
-      const int size = EnvironmentType::Action::size;
-      int actionInt = std::round(arma::clamp(genome.Output(), 0, size - 1)[0];
-      typename EnvironmentType::Action action = static_cast<typename
-          EnvironmentType::Action>(actionInt);
+//     double fitness = 0;
+//     while (!environment.IsTerminal())
+//     {
+//       // Choose the action to perform.
+//       const int size = EnvironmentType::Action::size;
+//       int actionInt = std::round(arma::clamp(genome.Output(), 0, size - 1)[0];
+//       typename EnvironmentType::Action action = static_cast<typename
+//           EnvironmentType::Action>(actionInt);
 
-      // Use the current action to get the next state.
-      fitness += environment.Sample(state, action, state);
+//       // Use the current action to get the next state.
+//       fitness += environment.Sample(state, action, state);
 
-      // Update the state of the genome for the next step.
-      genome.Input(state.Data());
-    }
-    return fitness;
-  }
+//       // Update the state of the genome for the next step.
+//       genome.Input(state.Data());
+//     }
+//     return fitness;
+//   }
 
- private:
-  EnvironmentType environment;
-};
+//  private:
+//   EnvironmentType environment;
+// };
 
-// A class that defines the Double Pole Balancing Task with velocities.
-class DPVTask
-{
-  DPVTask(const ContinuousMultiplePoleCart env)::environment(env)
-  { /* Nothing to do here */ }
+// // A class that defines the Double Pole Balancing Task with velocities.
+// class DPVTask
+// {
+//   DPVTask(const ContinuousMultiplePoleCart env)::environment(env)
+//   { /* Nothing to do here */ }
 
-  double Evaluate(Genome& genome)
-  {
-    ContinuousMultiplePoleCart::State state = environment.InitialSample();
-    arma::mat inputMatrix = state.Data();
-    double angleThreshold = 12 * 2 * 3.1416 / 360;
+//   double Evaluate(Genome& genome)
+//   {
+//     ContinuousMultiplePoleCart::State state = environment.InitialSample();
+//     arma::mat inputMatrix = state.Data();
+//     double angleThreshold = 12 * 2 * 3.1416 / 360;
 
-    // Input a vector scaled down features.
-    arma::vec input = {inputMatrix[0, 0] / 2.4, inputMatrix[1, 0] / 100,
-        inputMatrix[0, 1] / angleThreshold, inputMatrix[1, 1] / 100,
-        inputMatrix[0, 2] / angleThreshold, inputMatrix[1, 2] / 100};
-    genome.Input(input);
+//     // Input a vector scaled down features.
+//     arma::vec input = {inputMatrix[0, 0] / 2.4, inputMatrix[1, 0] / 100,
+//         inputMatrix[0, 1] / angleThreshold, inputMatrix[1, 1] / 100,
+//         inputMatrix[0, 2] / angleThreshold, inputMatrix[1, 2] / 100};
+//     genome.Input(input);
 
-    double fitnessDenom = 0;
-    int timeStep = 0;
-    while (!environment.IsTerminal())
-    {
-      timeStep++;
+//     double fitnessDenom = 0;
+//     int timeStep = 0;
+//     while (!environment.IsTerminal())
+//     {
+//       timeStep++;
 
-      // Choose an action.
-      ContinuousMultiplePoleCart::Action action;
-      action.action[0] = genome.Output()[0];
+//       // Choose an action.
+//       ContinuousMultiplePoleCart::Action action;
+//       action.action[0] = genome.Output()[0];
 
-      // Use the current action to get the next state.
-      environment.Sample(state, action, state);
+//       // Use the current action to get the next state.
+//       environment.Sample(state, action, state);
 
-      // Update the state of the genome for the next step.
-      inputMatrix = state.Data();
-      arma::vec input = {inputMatrix[0, 0] / 2.4, inputMatrix[1, 0] / 100,
-          inputMatrix[0, 1] / angleThreshold, inputMatrix[1, 1] / 100,
-          inputMatrix[0, 2] / angleThreshold, inputMatrix[1, 2] / 100};
-      genome.Input(input);
+//       // Update the state of the genome for the next step.
+//       inputMatrix = state.Data();
+//       arma::vec input = {inputMatrix[0, 0] / 2.4, inputMatrix[1, 0] / 100,
+//           inputMatrix[0, 1] / angleThreshold, inputMatrix[1, 1] / 100,
+//           inputMatrix[0, 2] / angleThreshold, inputMatrix[1, 2] / 100};
+//       genome.Input(input);
 
-      if (timeStep >= 1000)
-        continue;
+//       if (timeStep >= 1000)
+//         continue;
 
-      if (timeStep >= 100)
-      {
-        int pow = timeStep - 100;
-        arma::vec temp = {inputMatrix[0, 0], inputMatrix[1, 0],
-            inputMatrix[0, 1], inputMatrix[1, 1]};
-        fitnessDenom += arma::accu(arma::pow(temp, pow));
-      }
-    }
-    return timeStep / 10000 + 0.675 / fitnessDenom;
-  }
+//       if (timeStep >= 100)
+//       {
+//         int pow = timeStep - 100;
+//         arma::vec temp = {inputMatrix[0, 0], inputMatrix[1, 0],
+//             inputMatrix[0, 1], inputMatrix[1, 1]};
+//         fitnessDenom += arma::accu(arma::pow(temp, pow));
+//       }
+//     }
+//     return timeStep / 10000 + 0.675 / fitnessDenom;
+//   }
 
- private:
-  ContinuousMultiplePoleCart environment;
-};
+//  private:
+//   ContinuousMultiplePoleCart environment;
+// };
 
-// A class that defines the Double Pole Balancing Task without velocities.
-class DPNVTask
-{
-  DPNVTask(const ContinuousMultiplePoleCart env)::environment(env)
-  { /* Nothing to do here */ }
+// // A class that defines the Double Pole Balancing Task without velocities.
+// class DPNVTask
+// {
+//   DPNVTask(const ContinuousMultiplePoleCart env)::environment(env)
+//   { /* Nothing to do here */ }
 
-  double Evaluate(Genome& genome)
-  {
-    ContinuousMultiplePoleCart::State state = environment.InitialSample();
-    arma::mat inputMatrix = state.Data();
-    arma::vec input = inputMatrix.row(0);
-    genome.Input(input);
+//   double Evaluate(Genome& genome)
+//   {
+//     ContinuousMultiplePoleCart::State state = environment.InitialSample();
+//     arma::mat inputMatrix = state.Data();
+//     arma::vec input = inputMatrix.row(0);
+//     genome.Input(input);
 
-    double fitnessDenom = 0;
-    int timeStep = 0;
-    while (!environment.IsTerminal())
-    {
-      timeStep++;
+//     double fitnessDenom = 0;
+//     int timeStep = 0;
+//     while (!environment.IsTerminal())
+//     {
+//       timeStep++;
 
-      // Choose an action.
-      ContinuousMultiplePoleCart::Action action;
-      action.action[0] = genome.Output()[0];
+//       // Choose an action.
+//       ContinuousMultiplePoleCart::Action action;
+//       action.action[0] = genome.Output()[0];
 
-      // Use the current action to get the next state.
-      environment.Sample(state, action, state);
+//       // Use the current action to get the next state.
+//       environment.Sample(state, action, state);
 
-      // Update the state of the genome for the next step.
-      inputMatrix = state.Data();
-      input = inputMatrix.row(0);
+//       // Update the state of the genome for the next step.
+//       inputMatrix = state.Data();
+//       input = inputMatrix.row(0);
 
-      // Scale the input between -1 and 1.
-      input[0] /= 2.4;
-      input[1] /= 12 * 2 * 3.1416 / 360;
-      input[2] /= 12 * 2 * 3.1416 / 360;
-      genome.Input(input);
+//       // Scale the input between -1 and 1.
+//       input[0] /= 2.4;
+//       input[1] /= 12 * 2 * 3.1416 / 360;
+//       input[2] /= 12 * 2 * 3.1416 / 360;
+//       genome.Input(input);
 
-      if (timeStep >= 1000)
-        continue;
+//       if (timeStep >= 1000)
+//         continue;
 
-      if (timeStep >= 100)
-      {
-        int pow = timeStep - 100;
-        arma::vec temp = {inputMatrix[0, 0], inputMatrix[1, 0],
-            inputMatrix[0, 1], inputMatrix[1, 1]};
-        fitnessDenom += arma::accu(arma::pow(temp, pow));
-      }
-    }
-    return timeStep / 10000 + 0.675 / fitnessDenom;
-  }
+//       if (timeStep >= 100)
+//       {
+//         int pow = timeStep - 100;
+//         arma::vec temp = {inputMatrix[0, 0], inputMatrix[1, 0],
+//             inputMatrix[0, 1], inputMatrix[1, 1]};
+//         fitnessDenom += arma::accu(arma::pow(temp, pow));
+//       }
+//     }
+//     return timeStep / 10000 + 0.675 / fitnessDenom;
+//   }
 
- private:
-  ContinuousMultiplePoleCart environment;
-};
+//  private:
+//   ContinuousMultiplePoleCart environment;
+// };
 
 #endif
