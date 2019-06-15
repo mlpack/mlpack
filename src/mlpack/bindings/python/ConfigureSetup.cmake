1bb542c70e4932f5b3df93f3f6f7c82c0315d946
Ryan Curtin <ryan@ratml.org>
2019-06-15 16:02:29 -0400

    Link against Armadillo dependencies.

--- src/mlpack/bindings/python/ConfigureSetup.cmake
+++ src/mlpack/bindings/python/ConfigureSetup.cmake
@@ -7,6 +7,7 @@
 # - SETUP_PY_OUT: location of output file
 # - PACKAGE_VERSION: version of package
 # - Boost_SERIALIZATION_LIBRARY: location of Boost serialization library
+# - ARMADILLO_LIBRARIES: space-separated list of Armadillo dependencies
 # - MLPACK_LIBRARY: location of mlpack library
 # - MLPACK_PYXS: list of pyx files
 # - OpenMP_CXX_FLAGS: OpenMP C++ compilation flags
@@ -15,7 +16,6 @@
 # - MLPACK_LIBDIR: path to mlpack libraries
 # - BOOST_LIBDIRS: paths to Boost libraries
 # - OUTPUT_DIR: binary output directory for CMake
-
 get_filename_component(Boost_SERIALIZATION_LIB_NAME_IN
     ${Boost_SERIALIZATION_LIBRARY} NAME_WE)
 get_filename_component(MLPACK_LIB_NAME_IN
@@ -25,5 +25,24 @@ get_filename_component(MLPACK_LIB_NAME_IN
 string(REGEX REPLACE "^lib" "" Boost_SERIALIZATION_LIB_NAME
     "${Boost_SERIALIZATION_LIB_NAME_IN}")
 string(REGEX REPLACE "^lib" "" MLPACK_LIB_NAME "${MLPACK_LIB_NAME_IN}")
+
+# There may be multiple Armadillo libraries, so first convert the string we got
+# to a list.
+string(REPLACE " " ";" ARMADILLO_LIBRARIES_LIST "${ARMADILLO_LIBRARIES}")
+set(ARMADILLO_LIBRARIES "")
+set(ARMADILLO_LIBDIRS "")
+foreach(l ${ARMADILLO_LIBRARIES_LIST})
+  get_filename_component(l_in "${l}" NAME_WE)
+  get_filename_component(l_dir "${l}" DIRECTORY)
+  string(REGEX REPLACE "^[ ]*lib" "" l_out "${l_in}")
+  string(STRIP "${l_dir}" l_dir_out)
+  set(ARMADILLO_LIBRARIES "${ARMADILLO_LIBRARIES} ${l_out}")
+  set(ARMADILLO_LIBDIRS "${ARMADILLO_LIBDIRS} ${l_dir_out}")
+endforeach()
+
+string(STRIP "${ARMADILLO_LIBRARIES}" tmp)
+set(ARMADILLO_LIBRARIES "${tmp}")
+string(STRIP "${ARMADILLO_LIBDIRS}" tmp)
+set(ARMADILLO_LIBDIRS "${tmp}")
 
 configure_file(${SETUP_PY_IN} ${SETUP_PY_OUT})
