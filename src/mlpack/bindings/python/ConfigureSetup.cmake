9ec264dbb22dd7346af6ebde8970a3a2144ac486
Ryan Curtin <ryan@ratml.org>
2019-06-18 22:38:12 -0400

    Try specifying libraries directly via extra_link_args.
    I'm not yet sure if this will work on Windows.

--- src/mlpack/bindings/python/ConfigureSetup.cmake
+++ src/mlpack/bindings/python/ConfigureSetup.cmake
@@ -16,33 +16,5 @@
 # - MLPACK_LIBDIR: path to mlpack libraries
 # - BOOST_LIBDIRS: paths to Boost libraries
 # - OUTPUT_DIR: binary output directory for CMake
-get_filename_component(Boost_SERIALIZATION_LIB_NAME_IN
-    ${Boost_SERIALIZATION_LIBRARY} NAME_WE)
-get_filename_component(MLPACK_LIB_NAME_IN
-    ${MLPACK_LIBRARY} NAME_WE)
-
-# Strip 'lib' off the front of the name, if it's there.
-string(REGEX REPLACE "^lib" "" Boost_SERIALIZATION_LIB_NAME
-    "${Boost_SERIALIZATION_LIB_NAME_IN}")
-string(REGEX REPLACE "^lib" "" MLPACK_LIB_NAME "${MLPACK_LIB_NAME_IN}")
-
-# There may be multiple Armadillo libraries, so first convert the string we got
-# to a list.
-string(REPLACE " " ";" ARMADILLO_LIBRARIES_LIST "${ARMADILLO_LIBRARIES}")
-set(ARMADILLO_LIBRARIES "")
-set(ARMADILLO_LIBDIRS "")
-foreach(l ${ARMADILLO_LIBRARIES_LIST})
-  get_filename_component(l_in "${l}" NAME_WE)
-  get_filename_component(l_dir "${l}" DIRECTORY)
-  string(REGEX REPLACE "^[ ]*lib" "" l_out "${l_in}")
-  string(STRIP "${l_dir}" l_dir_out)
-  set(ARMADILLO_LIBRARIES "${ARMADILLO_LIBRARIES} ${l_out}")
-  set(ARMADILLO_LIBDIRS "${ARMADILLO_LIBDIRS} ${l_dir_out}")
-endforeach()
-
-string(STRIP "${ARMADILLO_LIBRARIES}" tmp)
-set(ARMADILLO_LIBRARIES "${tmp}")
-string(STRIP "${ARMADILLO_LIBDIRS}" tmp)
-set(ARMADILLO_LIBDIRS "${tmp}")
 
 configure_file(${SETUP_PY_IN} ${SETUP_PY_OUT})
