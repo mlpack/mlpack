20dd60b2df4bd0c5d793c10202d068b88dffa9ae
Rahul Prabhu <cupertinorp@gmail.com>
2019-06-26 10:07:21 +0530

    Fixed misspelling.

--- src/mlpack/tests/rl_components_test.cpp
+++ src/mlpack/tests/rl_components_test.cpp
@@ -47,7 +47,7 @@ BOOST_AUTO_TEST_CASE(SimplePendulumTest)
   // The reward is always negative. Check if not lower than lowest possible.
   BOOST_REQUIRE(reward >= -(pow(M_PI, 2) + 6.404));
 
-  BOOST_REQUIRE(!task.isTerminal(state));
+  BOOST_REQUIRE(!task.IsTerminal(state));
 
   // The action is simply the torque. Check if dimension is 1.
   BOOST_REQUIRE_EQUAL(1, action.size);
