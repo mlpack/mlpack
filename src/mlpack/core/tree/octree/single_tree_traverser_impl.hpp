1dc6df50e21e7d9c95b9a7712d16b24346a9b5bb
Roberto Hueso Gomez <robertohueso96@gmail.com>
2019-07-13 18:44:56 +0200

    Fix scoring in octree traversals
    Scoring function is now also called for the root nodes.

--- src/mlpack/core/tree/octree/single_tree_traverser_impl.hpp
+++ src/mlpack/core/tree/octree/single_tree_traverser_impl.hpp
@@ -42,6 +42,18 @@ void Octree<MetricType, StatisticType, MatType>::SingleTreeTraverser<RuleType>::
   }
   else
   {
+    // If it's the root node, just score it.
+    if (referenceNode.Parent() == NULL)
+    {
+      const double rootScore = rule.Score(queryIndex, referenceNode);
+      // If root score is DBL_MAX, don't recurse into that node.
+      if (rootScore == DBL_MAX)
+      {
+        ++numPrunes;
+        return;
+      }
+    }
+
     // Do a prioritized recursion, by scoring all candidates and then sorting
     // them.
     arma::vec scores(referenceNode.NumChildren());
