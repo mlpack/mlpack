58d6a8e2392ab477af5c44e0d31ec30cf240733a
mulx10 <mehulkumarnirala@iitkgp.ac.in>
2019-07-26 21:38:27 +0530

    Remove unused Warnings

--- src/mlpack/core/data/image_info.hpp
+++ src/mlpack/core/data/image_info.hpp
@@ -10,6 +10,10 @@
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
 
+// To remove unused functions warnings.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+
 #ifndef MLPACK_CORE_DATA_IMAGE_INFO_HPP
 #define MLPACK_CORE_DATA_IMAGE_INFO_HPP
 
@@ -103,3 +107,5 @@ class ImageInfo
 #endif // HAS_STB.
 
 #endif
+
+#pragma GCC diagnostic pop
