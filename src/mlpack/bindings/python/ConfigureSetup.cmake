8154e4f6f47657afcf0f46fc1303bcf902186aa1
Ryan Curtin <ryan@ratml.org>
2019-08-29 21:10:18 -0400

    Update configuration to set right library dirs.

--- src/mlpack/bindings/python/ConfigureSetup.cmake
+++ src/mlpack/bindings/python/ConfigureSetup.cmake
@@ -7,6 +7,7 @@
 # - SETUP_PY_OUT: location of output file
 # - PACKAGE_VERSION: version of package
 # - Boost_SERIALIZATION_LIBRARY: location of Boost serialization library
+# - Boost_LIBRARY_DIRS: paths to boost libraries
 # - ARMADILLO_LIBRARIES: space-separated list of Armadillo dependencies
 # - MLPACK_LIBRARY: location of mlpack library
 # - MLPACK_PYXS: list of pyx files
@@ -14,23 +15,27 @@
 # - DISABLE_CFLAGS: list of CFLAGS or CXXFLAGS to be disabled
 # - CYTHON_INCLUDE_DIRECTORIES: include directories for Cython
 # - MLPACK_LIBDIR: path to mlpack libraries
-# - BOOST_LIBDIRS: paths to Boost libraries
 # - OUTPUT_DIR: binary output directory for CMake
 
 # It's possible that the FindBoost CMake script may have returned a Boost
 # library with "lib" improperly prepended to it.  So we need to see if the file
 # exists, and if it doesn't, but it has a "lib" in it, then we will try
 # stripping the "lib" off the front.
-if (NOT EXISTS "$(Boost_SERIALIZATION_LIBRARY}")
+message(STATUS "Run with ${Boost_SERIALIZATION_LIBRARY}.")
+if (NOT EXISTS "${Boost_SERIALIZATION_LIBRARY}")
+  message(STATUS "Did not find serialization library ${Boost_SERIALIZATION_LIBRARY}!")
   # Split the filename to see if it starts with lib.
   set(Boost_SERIALIZATION_LIBRARY_ORIG "${Boost_SERIALIZATION_LIBRARY}")
   get_filename_component(SER_LIB_DIRECTORY "${Boost_SERIALIZATION_LIBRARY}"
       DIRECTORY)
   get_filename_component(SER_LIB_FILENAME "${Boost_SERIALIZATION_LIBRARY}" NAME)
+  message(STATUS "Name component is ${SER_LIB_FILENAME}, and directory is ${SER_LIB_DIRECTORY}.")
 
   # Strip any preceding "lib/".
   string(REGEX REPLACE "^lib" "" STRIPPED_FILENAME "${SER_LIB_FILENAME}")
+  message(STATUS "Regex gave us ${STRIPPED_FILENAME}.")
   set(Boost_SERIALIZATION_LIBRARY "${SER_LIB_DIRECTORY}/${STRIPPED_FILENAME}")
+  message(STATUS "New library ${Boost_SERIALIZATION_LIBRARY}.")
 
   if (NOT EXISTS "${Boost_SERIALIZATION_LIBRARY}")
     # We didn't find it, so for ease of debugging just revert to the original.
