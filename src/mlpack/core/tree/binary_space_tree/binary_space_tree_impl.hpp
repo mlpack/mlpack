8bef7cc56a2db10775759186664f23c7f2cd79ac
Sriram <sriramsk1999@gmail.com>
2019-09-14 19:48:04 +0530

    Implemened Move Assignment for Binary Space Tree and made progress on Copy Assignment

--- src/mlpack/core/tree/binary_space_tree/binary_space_tree_impl.hpp
+++ src/mlpack/core/tree/binary_space_tree/binary_space_tree_impl.hpp
@@ -383,89 +383,54 @@ BinarySpaceTree(
 template<typename MetricType,
          typename StatisticType,
          typename MatType,
-         typename SplitType,
-         typename DescentType,
-         template<typename> class AuxiliaryInformationType>
-BinarySpaceTree<MetricType, StatisticType, MatType, SplitType, DescentType,
-              AuxiliaryInformationType>&
-BinarySpaceTree<MetricType, StatisticType, MatType, SplitType, DescentType,
-              AuxiliaryInformationType>::
+         template<typename BoundMetricType, typename...> class BoundType,
+         template<typename SplitBoundType, typename SplitMatType>
+             class SplitType>
+BinarySpaceTree<MetricType, StatisticType, MatType, BoundType, SplitType>&
+BinarySpaceTree<MetricType, StatisticType, MatType, BoundType, SplitType>::
 operator=(const BinarySpaceTree& other)
 {
   // Return if it's the same tree.
   if (this == &other)
     return *this;
 
-  for (size_t i = 0; i < numChildren; i++)
-    delete children[i];
-
-  if (ownsDataset)
-    delete dataset;
-
-  maxNumChildren = other.MaxNumChildren();
-  minNumChildren = other.MinNumChildren();
-  numChildren = other.NumChildren();
-  children.resize(maxNumChildren + 1, NULL);
-  parent = NULL;
+  left = NULL;
+  right = NULL;
+  parent = other.Parent();
   begin = other.Begin();
   count = other.Count();
-  numDescendants = other.numDescendants;
-  maxLeafSize = other.MaxLeafSize();
-  minLeafSize = other.MinLeafSize();
   bound = other.bound;
+  stat = other.stat;
   parentDistance = other.ParentDistance();
-  dataset = new MatType(*other.dataset);
-  ownsDataset = true;
-  points = other.points;
-  auxiliaryInfo = AuxiliaryInfoType(other.auxiliaryInfo, this, true);
-
-  if (numChildren > 0)
-  {
-    for (size_t i = 0; i < numChildren; i++)
-      children[i] = new BinarySpaceTree(other.Child(i), true, this);
-  }
-
+  furthestDescendantDistance = other.FurthestDescendantDistance();
   return *this;
 }
 
 template<typename MetricType,
          typename StatisticType,
          typename MatType,
-         typename SplitType,
-         typename DescentType,
-         template<typename> class AuxiliaryInformationType>
-RectangleTree<MetricType, StatisticType, MatType, SplitType, DescentType,
-              AuxiliaryInformationType>&
-RectangleTree<MetricType, StatisticType, MatType, SplitType, DescentType,
-              AuxiliaryInformationType>::
-operator=(RectangleTree&& other)
+         template<typename BoundMetricType, typename...> class BoundType,
+         template<typename SplitBoundType, typename SplitMatType>
+             class SplitType>
+BinarySpaceTree<MetricType, StatisticType, MatType, BoundType, SplitType>&
+BinarySpaceTree<MetricType, StatisticType, MatType, BoundType, SplitType>::
+operator=(BinarySpaceTree&& other)
 {
   // Return if it's the same tree.
   if (this == &other)
     return *this;
 
-  for (size_t i = 0; i < numChildren; i++)
-    delete children[i];
-
-  if (ownsDataset)
-    delete dataset;
-
-  maxNumChildren = other.MaxNumChildren();
-  minNumChildren = other.MinNumChildren();
-  numChildren = other.NumChildren();
-  children = std::move(other.children);
   parent = other.Parent();
+  left = other.Left();
+  right = other.Right();
   begin = other.Begin();
   count = other.Count();
-  numDescendants = other.numDescendants;
-  maxLeafSize = other.MaxLeafSize();
-  minLeafSize = other.MinLeafSize();
   bound = std::move(other.bound);
+  stat = std::move(other.stat);
   parentDistance = other.ParentDistance();
+  furthestDescendantDistance = other.FurthestDescendantDistance();
+  minimumBoundDistance = other.MinimumBoundDistance();
   dataset = other.dataset;
-  ownsDataset = other.ownsDataset;
-  points = std::move(other.points);
-  auxiliaryInfo = std::move(other.auxiliaryInfo);
 
   return *this;
 }
