2ba7043b3c9d33f014182a49d6c3d4f3c554cd9f
cmercier <clement.mercier@hrenoble-inp.org>
2019-09-22 22:39:38 +0200

    Add tests on randomly generated examples.

--- src/mlpack/tests/bayesian_ridge_test.cpp
+++ src/mlpack/tests/bayesian_ridge_test.cpp
@@ -1,21 +1,23 @@
-#include <iostream>
-// Includes all relevant components of mlpack.
+/**
+ * @file bayesian_ridge_test.cpp
+ * @author Clement Mercier
+ *
+ * Test for BayesianRidge.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
 
-#include <math.h>
-#include <ctime>
 
 #include <mlpack/core/data/load.hpp>
-
-// #define BOOST_TEST_DYN_LINK 
-// #define BOOST_TEST_MODULE BayesianRidgeTest
-#include <boost/test/unit_test.hpp>
-
 #include <mlpack/methods/bayesian_ridge/bayesian_ridge.hpp>
+
+#include <boost/test/unit_test.hpp>
 
 using namespace mlpack::regression;
 using namespace mlpack::data;
-using namespace std;
-using namespace arma;
 
 BOOST_AUTO_TEST_SUITE(BayesianRidgeTest);
 
@@ -25,7 +27,7 @@ void GenerateProblem(arma::mat& X,
 		     size_t nDims,
 		     float sigma=0.0)
 {
-  arma_rng::set_seed(4);
+  arma::arma_rng::set_seed(4);
     
   X = arma::randn(nDims, nPoints);
   arma::colvec omega = arma::randn(nDims);
@@ -34,35 +36,31 @@ void GenerateProblem(arma::mat& X,
   y += noise;
 }
 
+// Ensure that predictions are close enough to the target
+// for a free noise dataset.
 BOOST_AUTO_TEST_CASE(BayesianRidgeRegressionTest)
 {
-  // First, load the data.
-  mat Xtrain, Xtest;
-  rowvec ytrain, ytest;
-
-  // The RMSE are set according to the results obtained by the current
-  // implementation on the following dataset.
-  // y = Xw + noise, noise->Normal(0,1/beta), where beta=40 is the
-  // precision and w vector/solution to recover with the seven first elements
-  // non zero.
-  const double RMSETRAIN = 0.14507, RMSETEST = 0.17961;
+  arma::mat X;
+  arma::rowvec y, predictions;
   
-  Load("reg_x_train.csv", Xtrain, false, true);  
-  Load("reg_x_test.csv", Xtest, false, true);  
-  Load("reg_y_train.csv", ytrain, false, true);
-  Load("reg_y_test.csv", ytest, false, true);  
+  GenerateProblem(X, y, 200, 10);
 
-  // Instanciate and train the estimator
-  BayesianRidge estimator(true, false);
-  estimator.Train(Xtrain, ytrain);
-
-  // Check if the RMSE are still equal to the previously fixed values
-  BOOST_REQUIRE_SMALL(estimator.Rmse(Xtrain,ytrain) - RMSETRAIN, 0.05);
-  BOOST_REQUIRE_SMALL(estimator.Rmse(Xtest,ytest) - RMSETEST, 0.05);
-
+  // Instanciate and train the estimator.
+  BayesianRidge estimator(true);
+  estimator.Train(X,y);
+  estimator.Predict(X, predictions);
+  
+  for (size_t i = 0; i < y.size(); i++)
+    {
+      BOOST_REQUIRE_CLOSE(predictions[i], y[i], 1e-6);
+    }
+  // Check that the estimated variance is zero.
+  BOOST_REQUIRE_SMALL(estimator.getVariance(), 1e-6);
 }
 
-BOOST_AUTO_TEST_CASE(TestCenterNormalize)
+
+// Verify fitIntercept and normalize equal false do not affect the solution.
+BOOST_AUTO_TEST_CASE(TestCenter0Normalize0)
 {
   arma::mat X;
   arma::rowvec y;
@@ -75,9 +73,36 @@ BOOST_AUTO_TEST_CASE(TestCenterNormalize)
   // To be neutral data_offset must be all 0.
   BOOST_TEST(sum(estimator.getdata_offset()) == 0);
 
+  // To be neutral responses_offset must be 0.
+  BOOST_TEST(estimator.getresponses_offset() == 0);
+
   // To be neutral data_scale must be all 1.
   BOOST_TEST(sum(estimator.getdata_scale()) == nDims);
 }
+
+// Verify that centering and normalization are correct.
+BOOST_AUTO_TEST_CASE(TestCenter1Normalize1)
+{
+  arma::mat X;
+  arma::rowvec y;
+  size_t nDims = 30, nPoints = 100;
+  GenerateProblem(X, y, nPoints, nDims, 0.5);
+
+  BayesianRidge estimator(true, true);
+  estimator.Train(X, y);
+
+  arma::colvec x_mean = arma::mean(X, 1);
+  arma::colvec x_std = arma::stddev(X, 0, 1);
+  double y_mean = arma::mean(y);
+
+  BOOST_REQUIRE_SMALL(sum(estimator.getdata_offset() - x_mean), 1e-6);
+    
+  BOOST_REQUIRE_SMALL(abs(estimator.getresponses_offset() - y_mean), 1e-6);
+
+  BOOST_REQUIRE_SMALL(sum(estimator.getdata_scale() - x_std), 1e-6);
+}
+
+
 
 BOOST_AUTO_TEST_CASE(ColinearTest)
 {
@@ -98,11 +123,10 @@ BOOST_AUTO_TEST_CASE(OnePointTest)
   arma::rowvec predictions, std;
   double y_i, std_i;
 
-  Load("reg_x_train.csv", X, false, true);  
-  Load("reg_y_train.csv", y, false, true);
-  
+  GenerateProblem(X, y, 100, 10, 2.0);
+    
   BayesianRidge estimator(false, false);
-  estimator.Train(X,y);
+  estimator.Train(X, y);
 
   // Predict on all the points.
   estimator.Predict(X, predictions);
