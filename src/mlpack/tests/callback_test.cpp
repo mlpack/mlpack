f5c4e31e95b54ce15e955480d5a25661e16d94c3
Khizir Siddiqui <khizirsiddiqui@gmail.com>
2019-11-07 11:57:40 +0530

    change callback tests location

--- src/mlpack/tests/callback_test.cpp
+++ src/mlpack/tests/callback_test.cpp
@@ -4,11 +4,13 @@
 #include <mlpack/core.hpp>
 #include <mlpack/methods/ann/ffn.hpp>
 #include <mlpack/methods/ann/rnn.hpp>
+#include <mlpack/methods/ann/rbm/rbm.hpp>
 #include <mlpack/methods/ann/loss_functions/mean_squared_error.hpp>
 #include <mlpack/methods/logistic_regression/logistic_regression.hpp>
 #include <mlpack/methods/lmnn/lmnn.hpp>
 #include <mlpack/methods/nca/nca.hpp>
 #include <mlpack/core/metrics/lmetric.hpp>
+#include <mlpack/methods/ann/init_rules/gaussian_init.hpp>
 
 #include <boost/test/unit_test.hpp>
 
@@ -176,6 +178,57 @@ BOOST_AUTO_TEST_CASE(NCAWithOptimizerCallback)
   std::stringstream stream;
 
   nca.LearnDistance(outputMatrix, ens::ProgressBar(70, stream));
+  BOOST_REQUIRE_GT(stream.str().length(), 0);
+}
+
+
+/*
+ * Tests the RBM Implementation with PrintLoss callback.
+ */
+BOOST_AUTO_TEST_CASE(RBMCallbackTest)
+{
+  // Normalised dataset.
+  int hiddenLayerSize = 100;
+  size_t batchSize = 10;
+  size_t numEpoches = 30;
+  arma::mat trainData, testData, dataset;
+  arma::mat trainLabelsTemp, testLabelsTemp;
+  trainData.load("digits_train.arm");
+  testData.load("digits_test.arm");
+  trainLabelsTemp.load("digits_train_label.arm");
+  testLabelsTemp.load("digits_test_label.arm");
+
+  arma::Row<size_t> trainLabels = arma::zeros<arma::Row<size_t>>(1,
+      trainLabelsTemp.n_cols);
+  arma::Row<size_t> testLabels = arma::zeros<arma::Row<size_t>>(1,
+      testLabelsTemp.n_cols);
+
+  for (size_t i = 0; i < trainLabelsTemp.n_cols; ++i)
+    trainLabels(i) = arma::as_scalar(trainLabelsTemp.col(i));
+
+  for (size_t i = 0; i < testLabelsTemp.n_cols; ++i)
+    testLabels(i) = arma::as_scalar(testLabelsTemp.col(i));
+
+  arma::mat output, XRbm(hiddenLayerSize, trainData.n_cols),
+      YRbm(hiddenLayerSize, testLabels.n_cols);
+
+  XRbm.zeros();
+  YRbm.zeros();
+
+  GaussianInitialization gaussian(0, 0.1);
+  RBM<GaussianInitialization> model(trainData,
+      gaussian, trainData.n_rows, hiddenLayerSize, batchSize);
+
+  size_t numRBMIterations = trainData.n_cols * numEpoches;
+  numRBMIterations /= batchSize;
+  ens::StandardSGD msgd(0.03, batchSize, numRBMIterations, 0, true);
+  model.Reset();
+  model.VisibleBias().ones();
+  model.HiddenBias().ones();
+  std::stringstream stream;
+  // Call the train function with printloss callback.
+  double objVal = model.Train(msgd, ens::PrintLoss(stream));
+
   BOOST_REQUIRE_GT(stream.str().length(), 0);
 }
 
