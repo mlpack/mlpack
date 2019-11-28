a042098734cf4e70543c17aefb8ed6ab4b7c7b1c
Vasyl Teliman <vasniktel@gmail.com>
2019-11-28 21:53:53 +0200

    Implement ProgramCall

--- src/mlpack/bindings/java/print_doc_functions_impl.hpp
+++ src/mlpack/bindings/java/print_doc_functions_impl.hpp
@@ -1,9 +1,67 @@
 #ifndef MLPACK_BINDINGS_JAVA_PRINT_DOC_FUNCTIONS_IMPL_HPP
-#define MLPACK_BINDINGS_JULIA_PRINT_DOC_FUNCTIONS_IMPL_HPP
+#define MLPACK_BINDINGS_JAVA_PRINT_DOC_FUNCTIONS_IMPL_HPP
+
+#include <sstream>
 
 namespace mlpack {
 namespace bindings {
 namespace java {
+
+inline void PrintInputCallParam(std::ostream&)
+{}
+
+template <typename T, typename... Args>
+void PrintInputCallParam(std::ostream& os, const std::string& name, const T& value, const Args&... args)
+{
+  const auto& param = CLI::Parameters().at(name);
+  if (param.input)
+  {
+    os << std::endl;
+    if (param.cppType == "bool")
+    {
+      os << "    .put(\"" << name << "\", true)";
+    }
+    else if (param.cppType == "arma::mat" ||
+        param.cppType == "arma::vec" ||
+        param.cppType == "arma::rowvec" ||
+        param.cppType == "std::tuple<mlpack::data::DatasetInfo, arma::mat>" ||
+        param.cppType == "arma::Mat<size_t>" ||
+        param.cppType == "arma::Row<size_t>" ||
+        param.cppType == "arma::Col<size_t>")
+    {
+      os << "    .put(\"" << name << "\", Nd4j.createFromNpyFile(new File(\"" << value << ".npy\"))";
+    }
+    else if (param.cppType == "std::string")
+    {
+      os << "    .put(\"" << name << "\", \"" << value << "\")";
+    }
+    else
+    {
+      os << "    .put(\"" << name << "\", " << value << ")";
+    }
+  }
+  
+  PrintInputCallParam(os, args...);
+}
+
+inline void PrintOutputCallParam(std::ostream&)
+{}
+
+template <typename T, typename... Args>
+void PrintOutputCallParam(std::ostream& os, const std::string& name, const T& /* value */, const Args&... args)
+{
+  const auto& param = CLI::Parameters().at(name);
+  if (!param.input)
+  {
+    os << std::endl;
+    std::string type;
+    CLI::GetSingleton().functionMap[param.tname]["GetJavaType"](param, nullptr, (void*) &type);
+
+    os << type << " " << name << " = params.get(\"" << name << "\", " << type << ".class);";
+  }
+   
+  PrintOutputCallParam(os, args...);
+}
 
 inline std::string ParamString(const std::string& paramName)
 {
@@ -14,40 +72,60 @@ template<typename T>
 inline std::string PrintValue(const T& value, bool quotes) 
 {
   std::ostringstream oss;
-  if (quotes) oss << "`";
-  oss << value;
-  if (quotes) oss << "`";
+  oss << std::boolalpha;
+  const char* quote = quotes ? "'" : "";
+  oss << "{@code " << quote << value << quote << "}";
   return oss.str();
-}
-
-template<>
-inline std::string PrintValue(const bool& value, bool quotes) 
-{
-  std::string result = value ? "true" : "false";
-  if (quotes) result = "`" + std::move(result) + "`";
-  return result;
 }
 
 inline std::string PrintDataset(const std::string& datasetName) 
 {
-  return "`" + datasetName + "`";
+  return "{@code " + datasetName + '}';
 }
 
 inline std::string PrintModel(const std::string& modelName)
 {
-  return "`" + modelName + "`";
+  return "{@code " + modelName + '}';
 }
 
 template<typename... Args>
-std::string ProgramCall(const std::string& programName, Args... args)
+std::string ProgramCall(const std::string& /* programName */, Args... args)
 {
-  // TODO: finish it
-  return "`" + programName + "(<args>)`";
+  std::ostringstream oss;
+  oss << std::boolalpha;
+
+  oss << "<pre>" << std::endl
+      << "{@code" << std::endl;
+  
+  oss << "Params params = new Params()";
+
+  PrintInputCallParam(oss, args...);
+
+  oss << ";" << std::endl
+      << std::endl
+      << "run(params);" << std::endl;
+  
+  PrintOutputCallParam(oss, args...);
+
+  oss << std::endl
+      << "}" << std::endl
+      << "</pre>" << std::endl;
+
+  return oss.str();
 }
 
-inline std::string ProgramCall(const std::string& programName)
+inline std::string ProgramCall(const std::string& /* programName */)
 {
-  return "`" + programName + "()`";
+  std::ostringstream oss;
+
+  oss << "<pre>" << std::endl
+      << "{@code" << std::endl
+      << "Params params = new Params();" << std::endl
+      << "run(params);" << std::endl
+      << "}" << std::endl
+      << "</pre>" << std::endl;
+  
+  return oss.str();
 }
 
 inline bool IgnoreCheck(const std::string& paramName)
