f9edd34978461662ece6d0b7b74c97eb1a604cef
Ryan Curtin <ryan@ratml.org>
2019-12-03 23:36:17 -0500

    Expand search directory set.

--- CMake/FindStbImage.cmake
+++ CMake/FindStbImage.cmake
@@ -6,13 +6,23 @@
 #  STB_IMAGE_INCLUDE_DIR - list of required include directories
 
 file(GLOB STB_IMAGE_SEARCH_PATHS
+    ${CMAKE_BINARY_DIR}/deps/
     ${CMAKE_BINARY_DIR}/deps/stb)
 find_path(STB_IMAGE_INCLUDE_DIR
-  NAMES stb_image.h stb_image_write.h
-  PATHS ${STB_IMAGE_SEARCH_PATHS})
+    NAMES stb/stb_image.h stb/stb_image_write.h
+    PATHS ${STB_IMAGE_SEARCH_PATHS})
 
 if(STB_IMAGE_INCLUDE_DIR)
   set(STB_IMAGE_FOUND YES)
+  set(STB_IMAGE_INCLUDE_DIR "${STB_IMAGE_INCLUDE_DIR}/stb/")
+else ()
+  find_path(STB_IMAGE_INCLUDE_DIR
+        NAMES stb_image.h stb_image_write.h
+        PATHS ${STB_IMAGE_SEARCH_PATHS})
+
+  if(STB_IMAGE_INCLUDE_DIR)
+    set(STB_IMAGE_FOUND YES)
+  endif ()
 endif ()
 
 # Checks 'REQUIRED'.
