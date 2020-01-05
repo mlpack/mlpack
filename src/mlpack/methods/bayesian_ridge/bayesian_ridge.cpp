9f8d0c4dbe33a4b631f675cd00f2e7dafb874aa5
cmercier <clement.mercier@hrenoble-inp.org>
2020-01-05 22:46:55 +0100

    Modifications according to the zoq's comments.

--- src/mlpack/methods/bayesian_ridge/bayesian_ridge.cpp
+++ src/mlpack/methods/bayesian_ridge/bayesian_ridge.cpp
@@ -17,42 +17,42 @@ using namespace mlpack;
 using namespace mlpack::regression;
 
 
-BayesianRidge::BayesianRidge(const bool fitIntercept,
-                             const bool normalize) :
-  fitIntercept(fitIntercept),
-  normalize(normalize)
+BayesianRidge::BayesianRidge(const bool centerData,
+                             const bool scaleData) :
+  centerData(centerData),
+  scaleData(scaleData)
 {/* Nothing to do */}
 
-float BayesianRidge::Train(const arma::mat& data,
-                           const arma::rowvec& responses)
+double BayesianRidge::Train(const arma::mat& data,
+                            const arma::rowvec& responses)
 {
   Timer::Start("bayesian_ridge_regression");
 
   arma::mat phi;
   arma::rowvec t;
-  arma::colvec vecphitT;
-  arma::mat phiphiT;
   arma::colvec eigval;
   arma::mat eigvec;
   arma::colvec eigvali;
 
-  // Preprocess the data. Center and normalize.
-  CenterNormalize(data,
+  // Preprocess the data. Center and scale.
+  CenterScaleData(data,
                   responses,
-                  fitIntercept,
-                  normalize,
+                  centerData,
+                  scaleData,
                   phi,
                   t,
-                  data_offset,
-                  data_scale,
-                  responses_offset);
-  vecphitT = phi * t.t();
-  phiphiT =  phi * phi.t();
+                  dataOffset,
+                  dataScale,
+                  responsesOffset);
+
+  // Compute this quantities once and for all.
+  const arma::colvec vecphitT = phi * t.t();
+  const arma::mat phiphiT =  phi * phi.t();
 
   arma::eig_sym(eigval, eigvec, phiphiT);
 
   // Detect singular matrix.
-  if (eigval[0] == 0)
+  if (eigval[0] < 1e-8)
   {
     Log::Warn << "Singular matrix. Two lines or more are colinear."
               <<  std::endl;
@@ -70,41 +70,40 @@ float BayesianRidge::Train(const arma::mat& data,
   unsigned short i = 0;
   double deltaAlpha = 1, deltaBeta = 1, crit = 1;
   arma::mat matA = arma::eye<arma::mat>(p, p);
-  arma::rowvec temp;
 
   while ((crit > tol) && (i < nIterMax))
-    {
-      deltaAlpha = -alpha;
-      deltaBeta = -beta;
+  {
+    deltaAlpha = -alpha;
+    deltaBeta = -beta;
 
-      // Compute the posterior statistics.
-      // with inv()
-      for (size_t k = 0; k < p; k++) {matA(k, k) = alpha;}
-      // inv is used instead of solve beacause we need matCovariance to
-      // compute the prediction uncertainties. If solve is used, matCovariance
-      // must be comptuted at the end of the loop.
-      matCovariance = inv_sympd(matA + phiphiT * beta);
-      omega = (matCovariance * vecphitT) * beta;
+    // Compute the posterior statistics.
+    // with inv()
+    matA.diag().fill(alpha);
+    // inv is used instead of solve because we need the covariance matrix to
+    // compute the prediction uncertainties. If solve is used, matCovariance
+    // must be comptuted at the end of the loop.
+    matCovariance = inv_sympd(matA + phiphiT * beta);
+    omega = (matCovariance * vecphitT) * beta;
 
-      // // with solve()
-      // for (size_t k = 0; k < p; k++) {matA(k,k) = alpha / beta;}
-      // omega = solve(matA + phiphiT, vecphitT);
+    // // with solve()
+    // matA.diag().fill(alpha/ beta);
+    // omega = solve(matA + phiphiT, vecphitT);
 
-      // Update alpha.
-      eigvali = eigval * beta;
-      gamma = sum(eigvali / (alpha + eigvali));
-      alpha = gamma / dot(omega.t(), omega);
+    // Update alpha.
+    eigvali = eigval * beta;
+    gamma = sum(eigvali / (alpha + eigvali));
+    alpha = gamma / dot(omega.t(), omega);
 
-      // Update beta.
-      temp = t - omega.t() * phi;
-      beta = (n - gamma) / dot(temp, temp);
+    // Update beta.
+    const arma::rowvec temp = t - omega.t() * phi;
+    beta = (n - gamma) / dot(temp, temp);
 
-      // Comptute the stopping criterion.
-      deltaAlpha += alpha;
-      deltaBeta += beta;
-      crit = abs(deltaAlpha/alpha + deltaBeta/beta);
-      i++;
-    }
+    // Comptute the stopping criterion.
+    deltaAlpha += alpha;
+    deltaBeta += beta;
+    crit = abs(deltaAlpha/alpha + deltaBeta/beta);
+    i++;
+  }
   Timer::Stop("bayesian_ridge_regression");
   return Rmse(data, responses);
 }
@@ -114,32 +113,10 @@ void BayesianRidge::Predict(const arma::mat& points,
 {
   arma::mat X = points;
 
-  // Center and normalize the points before applying the model
-  X.each_col() -= data_offset;
-  X.each_col() /= data_scale;
-  predictions = omega.t() * X + responses_offset;
-}
-
-void BayesianRidge::Predict(const arma::colvec& point,
-                            double& prediction) const
-{
-  arma::mat point_mat = arma::conv_to<arma::mat>::from(point);
-  arma::rowvec prediction_vec(1);
-  Predict(point_mat, prediction_vec);
-  prediction = prediction_vec[0];
-}
-
-
-void BayesianRidge::Predict(const arma::colvec& point,
-                            double& prediction,
-                            double& std) const
-{
-  arma::mat point_mat = arma::conv_to<arma::mat>::from(point);
-  arma::rowvec prediction_vec(1);
-  arma::rowvec std_vec(1);
-  Predict(point_mat, prediction_vec, std_vec);
-  prediction = prediction_vec[0];
-  std = std_vec[0];
+  // Center and scaleData the points before applying the model
+  X.each_col() -= dataOffset;
+  X.each_col() /= dataScale;
+  predictions = omega.t() * X + responsesOffset;
 }
 
 void BayesianRidge::Predict(const arma::mat& points,
@@ -148,20 +125,20 @@ void BayesianRidge::Predict(const arma::mat& points,
 {
   arma::mat X = points;
 
-  // Center and normalize the points before applying the model.
-  X.each_col() -= data_offset;
-  X.each_col() /= data_scale;
-  predictions = omega.t() * X + responses_offset;
+  // Center and scaleData the points before applying the model.
+  X.each_col() -= dataOffset;
+  X.each_col() /= dataScale;
+  predictions = omega.t() * X + responsesOffset;
 
   // Compute the standard deviation of each prediction.
   std = arma::zeros<arma::rowvec>(X.n_cols);
   arma::colvec phi(X.n_rows);
   for (size_t i = 0; i < X.n_cols; i++)
-    {
-      phi = X.col(i);
-      std[i] = sqrt(Variance()
+  {
+    phi = X.col(i);
+    std[i] = sqrt(Variance()
                + dot(phi.t() * matCovariance, phi));
-    }
+  }
 }
 
 double BayesianRidge::Rmse(const arma::mat& data,
@@ -172,48 +149,48 @@ double BayesianRidge::Rmse(const arma::mat& data,
   return sqrt(mean(square(responses - predictions)));
 }
 
-void BayesianRidge::CenterNormalize(const arma::mat& data,
+void BayesianRidge::CenterScaleData(const arma::mat& data,
                                     const arma::rowvec& responses,
-                                    bool fit_intercept,
-                                    bool normalize,
-                                    arma::mat& data_proc,
-                                    arma::rowvec& responses_proc,
-                                    arma::colvec& data_offset,
-                                    arma::colvec& data_scale,
-                                    double& responses_offset)
+                                    bool centerData,
+                                    bool scaleData,
+                                    arma::mat& dataProc,
+                                    arma::rowvec& responsesProc,
+                                    arma::colvec& dataOffset,
+                                    arma::colvec& dataScale,
+                                    double& responsesOffset)
 {
   // Initialize the offsets to their neutral forms.
-  data_offset = arma::zeros<arma::colvec>(data.n_rows);
-  data_scale = arma::ones<arma::colvec>(data.n_rows);
-  responses_offset = 0.0;
+  dataOffset = arma::zeros<arma::colvec>(data.n_rows);
+  dataScale = arma::ones<arma::colvec>(data.n_rows);
+  responsesOffset = 0.0;
 
-  if (fit_intercept)
-    {
-      data_offset = mean(data, 1);
-      responses_offset = mean(responses);
-    }
-  if (normalize)
-      data_scale = stddev(data, 0, 1);
+  if (centerData)
+  {
+    dataOffset = mean(data, 1);
+    responsesOffset = mean(responses);
+  }
+
+  if (scaleData)
+    dataScale = stddev(data, 0, 1);
 
   // Copy data and response before the processing.
-  data_proc = data;
-  responses_proc = responses;
+  dataProc = data;
   // Center the data.
-  data_proc.each_col() -= data_offset;
+  dataProc.each_col() -= dataOffset;
   // Scale the data.
-  data_proc.each_col() /= data_scale;
+  dataProc.each_col() /= dataScale;
   // Center the responses.
-  responses_proc -= responses_offset;
+  responsesProc = responses - responsesOffset;
 }
 
 
 // Copy construcor
 BayesianRidge::BayesianRidge(const BayesianRidge& other):
-  fitIntercept(other.fitIntercept),
-  normalize(other.normalize),
-  data_offset(other.data_offset),
-  data_scale(other.data_scale),
-  responses_offset(other.responses_offset),
+  centerData(other.centerData),
+  scaleData(other.scaleData),
+  dataOffset(other.dataOffset),
+  dataScale(other.dataScale),
+  responsesOffset(other.responsesOffset),
   alpha(other.alpha),
   beta(other.beta),
   gamma(other.gamma),
@@ -223,11 +200,11 @@ BayesianRidge::BayesianRidge(const BayesianRidge& other):
 
 // Move construcor
 BayesianRidge::BayesianRidge(BayesianRidge&& other):
-  fitIntercept(other.fitIntercept),
-  normalize(other.normalize),
-  data_offset(std::move(other.data_offset)),
-  data_scale(std::move(other.data_scale)),
-  responses_offset(other.responses_offset),
+  centerData(other.centerData),
+  scaleData(other.scaleData),
+  dataOffset(std::move(other.dataOffset)),
+  dataScale(std::move(other.dataScale)),
+  responsesOffset(other.responsesOffset),
   alpha(other.alpha),
   beta(other.beta),
   gamma(other.gamma),
@@ -236,18 +213,18 @@ BayesianRidge::BayesianRidge(BayesianRidge&& other):
 {
   // Clear the other object
   if (this != &other)
-    {
-      other.fitIntercept = false;
-      other.normalize = false;
-      other.data_offset.reset();
-      other.data_scale.reset();
-      other.responses_offset = 0.0;
-      other.alpha = 0.0;
-      other.gamma = 0.0;
-      other.beta = 0.0;
-      other.omega.reset();
-      other.matCovariance.reset();
-    }
+  {
+    other.centerData = false;
+    other.scaleData = false;
+    other.dataOffset.reset();
+    other.dataScale.reset();
+    other.responsesOffset = 0.0;
+    other.alpha = 0.0;
+    other.gamma = 0.0;
+    other.beta = 0.0;
+    other.omega.reset();
+    other.matCovariance.reset();
+  }
 }
 
 BayesianRidge& BayesianRidge::operator=(const BayesianRidge& other)
@@ -255,11 +232,11 @@ BayesianRidge& BayesianRidge::operator=(const BayesianRidge& other)
   if (this == &other)
     return *this;
 
-  fitIntercept = other.fitIntercept;
-  normalize = other.normalize;
-  data_offset = other.data_offset;
-  data_scale = other.data_scale;
-  responses_offset = other.responses_offset;
+  centerData = other.centerData;
+  scaleData = other.scaleData;
+  dataOffset = other.dataOffset;
+  dataScale = other.dataScale;
+  responsesOffset = other.responsesOffset;
   alpha = other.alpha;
   gamma = other.gamma;
   beta = other.beta;
@@ -271,29 +248,29 @@ BayesianRidge& BayesianRidge::operator=(const BayesianRidge& other)
 BayesianRidge& BayesianRidge::operator=(BayesianRidge&& other)
 {
   if (this != &other)
-    {
-      fitIntercept = other.fitIntercept;
-      normalize = other.normalize;
-      data_offset = other.data_offset;
-      data_scale = other.data_scale;
-      responses_offset = other.responses_offset;
-      alpha = other.alpha;
-      gamma = other.gamma;
-      beta = other.beta;
-      omega = other.omega;
-      matCovariance = other.matCovariance;
+  {
+    centerData = other.centerData;
+    scaleData = other.scaleData;
+    dataOffset = other.dataOffset;
+    dataScale = other.dataScale;
+    responsesOffset = other.responsesOffset;
+    alpha = other.alpha;
+    gamma = other.gamma;
+    beta = other.beta;
+    omega = other.omega;
+    matCovariance = other.matCovariance;
 
-      // Clear the other object.
-      other.fitIntercept = false;
-      other.normalize = false;
-      other.data_offset.reset();
-      other.data_scale.reset();
-      other.responses_offset = 0.0;
-      other.alpha = 0.0;
-      other.gamma = 0.0;
-      other.beta = 0.0;
-      other.omega.reset();
-      other.matCovariance.reset();
-    }
+    // Clear the other object.
+    other.centerData = false;
+    other.scaleData = false;
+    other.dataOffset.reset();
+    other.dataScale.reset();
+    other.responsesOffset = 0.0;
+    other.alpha = 0.0;
+    other.gamma = 0.0;
+    other.beta = 0.0;
+    other.omega.reset();
+    other.matCovariance.reset();
+  }
   return *this;
 }
