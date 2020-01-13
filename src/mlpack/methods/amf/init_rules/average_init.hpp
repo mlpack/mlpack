658819df4a183167e61c9650d46deb8d0c4669d5
jzy95310 <45862046+jzy95310@users.noreply.github.com>
2020-01-12 21:56:26 -0800

    Update average_init.hpp
    Add member function InitializeOne(V, r, W, H) for MergeInitialization Rule Template

--- src/mlpack/methods/amf/init_rules/average_init.hpp
+++ src/mlpack/methods/amf/init_rules/average_init.hpp
@@ -74,6 +74,82 @@ class AverageInitialization
     H = H + avgV;
   }
 
+  /**
+   * Initialize the matrix W or H to the average value of V with uniform
+   * random noise added.
+   *
+   * @param V Input matrix.
+   * @param r Rank of matrix.
+   * @param whichMatrix Specify which matrix to initialize.
+   * @param M W or H matrix, to be initialized to the average value of V
+   * with uniform random noise added.
+   */
+  template<typename MatType>
+  inline static void InitializeOne(const MatType& V, 
+                                   const size_t r, 
+                                   const char whichMatrix, 
+                                   arma::mat& M)
+  {
+    const size_t n = V.n_rows;
+    const size_t m = V.n_cols;
+
+    if (whichMatrix == 'W' || whichMatrix == 'w') 
+    {
+      double avgV = 0;
+      size_t count = 0;
+      double min = DBL_MAX;
+
+      // Iterate over all elements in the matrix (for sparse matrices, this only
+      // iterates over nonzeros).
+      for (typename MatType::const_row_col_iterator it = V.begin();
+          it != V.end(); ++it)
+      {
+        ++count;
+        avgV += *it;
+        // Track the minimum value.
+        if (*it < min)
+          min = *it;
+      }
+
+      avgV = sqrt(((avgV / (n * m)) - min) / r);
+
+      // Initialize W to random values
+      M.randu(n, r);
+
+      M = M + avgV;
+    }
+    else if (whichMatrix == 'H' || whichMatrix == 'h') 
+    {
+      double avgV = 0;
+      size_t count = 0;
+      double min = DBL_MAX;
+
+      // Iterate over all elements in the matrix (for sparse matrices, this only
+      // iterates over nonzeros).
+      for (typename MatType::const_row_col_iterator it = V.begin();
+          it != V.end(); ++it)
+      {
+        ++count;
+        avgV += *it;
+        // Track the minimum value.
+        if (*it < min)
+          min = *it;
+      }
+
+      avgV = sqrt(((avgV / (n * m)) - min) / r);
+
+      // Initialize H to random values
+      M.randu(r, m);
+
+      M = M + avgV;
+    }
+    else 
+    {
+      Log::Fatal << "Specify either 'H' or 'W' when initializing "
+          "one of W and H matrices!" << std::endl;
+    }
+  }
+
   //! Serialize the object (in this case, there is nothing to do).
   template<typename Archive>
   void serialize(Archive& /* ar */, const unsigned int /* version */) { }
