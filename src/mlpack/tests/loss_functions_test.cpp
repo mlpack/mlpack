b6103ba2805feaf17d7203f3af094fcbf792b2be
Mrityunjay Tripathi <mrityunjay2668@gmail.com>
2020-01-31 23:40:00 +0530

    corrected writing style, added test for poisson_nll_loss and vectorized the forward and backward propagation when (full == true)

--- src/mlpack/tests/loss_functions_test.cpp
+++ src/mlpack/tests/loss_functions_test.cpp
@@ -14,6 +14,7 @@
 #include <mlpack/core.hpp>
 
 #include <mlpack/methods/ann/layer/layer.hpp>
+#include <mlpack/methods/ann/loss_functions/poisson_nll_loss.hpp>
 #include <mlpack/methods/ann/loss_functions/kl_divergence.hpp>
 #include <mlpack/methods/ann/loss_functions/earth_mover_distance.hpp>
 #include <mlpack/methods/ann/loss_functions/mean_squared_error.hpp>
@@ -32,6 +33,44 @@ using namespace mlpack;
 using namespace mlpack::ann;
 
 BOOST_AUTO_TEST_SUITE(LossFunctionsTest);
+
+BOOST_AUTO_TEST_CASE(PoissonNegativeLogLikelihoodTest)
+{
+  arma::mat log_input, output, target;
+  PoissonNegativeLogLikelihoodLoss<> module;
+
+  // Test the Forward function on a user generated input and compare it against
+  // the manually calculated result.
+  log_input = arma::mat("1.0 1.0 3.0 2.0 -1.0 1.0 -1.0 0.0");
+  target = arma::mat("1.0 1.0 1.0 2.0 2.0 1.0 2.0 0.0");
+  double loss = module.Forward(std::move(log_input), std::move(target));
+  BOOST_REQUIRE_CLOSE_FRACTION(loss, 3.9206, 0.0001);
+
+  // Test the Backward function.
+  module.Backward(std::move(input), std::move(target), std::move(output));
+  
+  // if(full == false) output = 1 - target / (input + eps)
+  arma::mat expected_out = arma::mat("0.2148 0.2148 2.3857 0.6736 -0.2040 0.2148 -0.2040 0.1250");
+  checkMatrices(output, std::move(expected_out));
+
+  BOOST_REQUIRE_EQUAL(output.n_rows, input.n_rows);
+  BOOST_REQUIRE_EQUAL(output.n_cols, input.n_cols);
+
+  // Test the error function on a single input and (full == true).
+  log_input = arma::mat("4");
+  target = arma::mat("3");
+  module.full = true;
+  loss = module.Forward(std::move(input), std::move(target));
+  // since target > 1, so the extra term log(n!) is added
+  // Therefore, loss = e^4 - 3*4 + 3*log(3) - 3 + log(2*pi*3) = 44.3622316
+  BOOST_REQUIRE_CLOSE_FRACTION(loss, 44.3622316 , 0.0001);
+
+  // Test the Backward function on a single input and (full == true).
+  module.Backward(std::move(input), std::move(target), std::move(output));
+  
+  BOOST_REQUIRE_CLOSE_FRACTION(arma::accu(output), 53.3406, 0.0001);
+  BOOST_REQUIRE_EQUAL(output.n_elem, 1);
+}
 
 /**
  * Simple KL Divergence test.  The loss should be zero if input = target.
