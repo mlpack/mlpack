5330dc863e7d9d2c762159bfde19b02f2881292d
ojhalakshya <ojhalakshya@gmail.com>
2020-02-02 20:14:54 +0530

    changes

--- src/mlpack/methods/ann/activation_functions/soft_shrinkage.hpp
+++ src/mlpack/methods/ann/activation_functions/soft_shrinkage.hpp
@@ -8,8 +8,8 @@
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
 
-#ifndef MLPACK_METHODS_ANN_ACTIVATION_FUNCTIONS_MISH_FUNCTION_HPP
-#define MLPACK_METHODS_ANN_ACTIVATION_FUNCTIONS_MISH_FUNCTION_HPP
+#ifndef MLPACK_METHODS_ANN_ACTIVATION_FUNCTIONS_SOFT_SHRINKAGE_FUNCTION_HPP
+#define MLPACK_METHODS_ANN_ACTIVATION_FUNCTIONS_SOFT_SHRINKAGE_FUNCTION_HPP
 
 #include <mlpack/prereqs.hpp>
 #include <algorithm>
@@ -19,39 +19,36 @@ namespace ann /** Artificial Neural Network. */ {
 
 /**
  *SoftShrinkage operator is defined as
- *         ⎧ x - lambda, if x >  lambda
- *  f(x) = ⎨ x + lambda, if x < -lambda
- *         ⎩ 0, otherwise
+ *         | x - lambda, if x >  lambda
+ *  f(x) = | x + lambda, if x < -lambda
+ *         | 0, otherwise
  *
  * lambda is set to 0.5 by default
  *
- *          ⎧ 1 , if x >  lambda
- *  f'(x) = ⎨ 1 , if x < -lambda
- *          ⎩ 0 , otherwise
+ *          | 1 , if x >  lambda
+ *  f'(x) = | 1 , if x < -lambda
+ *          | 0 , otherwise
  */
 class SoftShrinkage
 {
   public:
   static double Fn(const double x)
   {
-    if(x>lambda)
-    return (x-lambda);
+    if(x > lambda)
+    {
+      return (x - lambda);
+    }
     
-    else if(x<lambda)
-    return (x+lambda);
+    else if(x < -lambda)
+    {
+      return (x + lambda);
+    }
 
     return 0;
   }
   template <typename InputVecType,typename OutputVecType>
   static void Fn(const InputVecType &x, OutputVecType &y)
   {
-    if(arma::x>arma::lambda)
-    return (arma::x-arma::lambda);
-
-    else if(arma::x<arma::lambda)
-    return (arma::x+arma::lambda);
-
-    else
-    return 0;
+    
   }
 }
