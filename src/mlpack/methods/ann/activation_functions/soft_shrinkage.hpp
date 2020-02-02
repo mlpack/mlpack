615cb54fe5806b76a795cba37dd251c33eb625df
ojhalakshya <ojhalakshya@gmail.com>
2020-02-02 19:58:30 +0530

    soft shrinkage activation function

---
+++ src/mlpack/methods/ann/activation_functions/soft_shrinkage.hpp
@@ -0,0 +1,57 @@
+/**
+ * @file soft_shrinkage_function.hpp
+ * @author Lakshya Ojha
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+
+#ifndef MLPACK_METHODS_ANN_ACTIVATION_FUNCTIONS_MISH_FUNCTION_HPP
+#define MLPACK_METHODS_ANN_ACTIVATION_FUNCTIONS_MISH_FUNCTION_HPP
+
+#include <mlpack/prereqs.hpp>
+#include <algorithm>
+
+namespace mlpack {
+namespace ann /** Artificial Neural Network. */ {
+
+/**
+ *SoftShrinkage operator is defined as
+ *         ⎧ x - lambda, if x >  lambda
+ *  f(x) = ⎨ x + lambda, if x < -lambda
+ *         ⎩ 0, otherwise
+ *
+ * lambda is set to 0.5 by default
+ *
+ *          ⎧ 1 , if x >  lambda
+ *  f'(x) = ⎨ 1 , if x < -lambda
+ *          ⎩ 0 , otherwise
+ */
+class SoftShrinkage
+{
+  public:
+  static double Fn(const double x)
+  {
+    if(x>lambda)
+    return (x-lambda);
+    
+    else if(x<lambda)
+    return (x+lambda);
+
+    return 0;
+  }
+  template <typename InputVecType,typename OutputVecType>
+  static void Fn(const InputVecType &x, OutputVecType &y)
+  {
+    if(arma::x>arma::lambda)
+    return (arma::x-arma::lambda);
+
+    else if(arma::x<arma::lambda)
+    return (arma::x+arma::lambda);
+
+    else
+    return 0;
+  }
+}
