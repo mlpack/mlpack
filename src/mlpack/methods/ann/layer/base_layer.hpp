7754ff71b3fcc19cc2a4f674dd2485bd25e97146
ojhalakshya <ojhalakshya@gmail.com>
2020-02-06 22:37:17 +0530

    base layer changes

--- src/mlpack/methods/ann/layer/base_layer.hpp
+++ src/mlpack/methods/ann/layer/base_layer.hpp
@@ -22,6 +22,7 @@
 #include <mlpack/methods/ann/activation_functions/hard_sigmoid_function.hpp>
 #include <mlpack/methods/ann/activation_functions/swish_function.hpp>
 #include <mlpack/methods/ann/activation_functions/mish_function.hpp>
+#include <mlpack/methods/ann/activation_functions/softshrink_function.hpp>
 
 namespace mlpack {
 namespace ann /** Artificial Neural Network. */ {
@@ -206,6 +207,17 @@ template <
     typename OutputDataType = arma::mat
 >
 using MishFunctionLayer = BaseLayer<
+    ActivationFunction, InputDataType, OutputDataType>;
+
+/**
+ * Standard SoftShrink-Layer using the Soft Shrink activation function.
+ */
+template <
+    class ActivationFunction = SoftShrinkFunction,
+    typename InputDataType = arma::mat,
+    typename OutputDataType = arma::mat
+>
+using SoftShrinkFunctionLayer = BaseLayer<
     ActivationFunction, InputDataType, OutputDataType>;
 
 } // namespace ann
