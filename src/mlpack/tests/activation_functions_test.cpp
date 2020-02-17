4a9f5d152076da90b6850518239e2a995ffe1d69
ojhalakshya <ojhalakshya@gmail.com>
2020-02-17 19:32:35 +0530

    final checks

--- src/mlpack/tests/activation_functions_test.cpp
+++ src/mlpack/tests/activation_functions_test.cpp
@@ -740,5 +740,5 @@ BOOST_AUTO_TEST_CASE(SoftShrinkFunctionTest)
                                    desiredActivations);
   CheckSoftShrinkDerivativeCorrect(desiredActivations,
                                    desiredDerivatives);
-} 
+}
 BOOST_AUTO_TEST_SUITE_END();
