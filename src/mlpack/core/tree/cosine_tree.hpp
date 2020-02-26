3ceec617165b44e09348343596b0649ea7021b7f
Sriram <sriramsk1999@gmail.com>
2020-02-26 13:17:20 +0530

    Added serialization and renamed file

---
+++ src/mlpack/core/tree/cosine_tree.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file cosine_tree.hpp
+ * @author Sriram S K
+ *
+ * Includes all the necessary files to use the CosineTree class.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_CORE_TREE_COSINE_TREE_HPP
+#define MLPACK_CORE_TREE_COSINE_TREE_HPP
+
+#include <mlpack/prereqs.hpp>
+#include "cosine_tree/cosine_tree.hpp"
+
+#endif
