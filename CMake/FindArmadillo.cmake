65796c8b705d32112c3ece94b9ecfc5f6f872459
Ryan Birmingham <birm@rbirm.us>
2020-03-04 11:43:57 -0500

    minor cleanup of findArma

--- CMake/FindArmadillo.cmake
+++ CMake/FindArmadillo.cmake
@@ -6,7 +6,7 @@ FindArmadillo
 -------------
 
 Find the Armadillo C++ library.
-Armadillo is library for linear algebra & scientific computing.
+Armadillo is a library for linear algebra & scientific computing.
 
 Using Armadillo:
 
@@ -29,11 +29,6 @@ This module sets the following variables:
   ARMADILLO_VERSION_PATCH - patch version number
   ARMADILLO_VERSION_STRING - version number as a string (ex: "1.0.4")
   ARMADILLO_VERSION_NAME - name of the version (ex: "Antipodean Antileech")
-  ARMA_USE_WRAPPER - set to true if armadillo has been configured as a wrapper
-  ARMA_USE_LAPACK - set to true if armadillo has been configured to use LAPACK
-  ARMA_USE_BLAS - set to true if armadillo has been configured to use BLAS
-  ARMA_USE_ARPACK - set to true if armadillo has been configured to use ARPACK
-  ARMA_USE_HDF5 - set to true if armadillo has been configured to use HDF5
 #]=======================================================================]
 
 find_path(ARMADILLO_INCLUDE_DIR
@@ -57,7 +52,7 @@ if(ARMADILLO_INCLUDE_DIR)
   if(EXISTS "${ARMADILLO_INCLUDE_DIR}/armadillo_bits/arma_version.hpp")
 
     # Read and parse armdillo version header file for version number
-    file(READ "${ARMADILLO_INCLUDE_DIR}/armadillo_bits/arma_version.hpp" _armadillo_HEADER_CONTENTS)
+    file(STRINGS "${ARMADILLO_INCLUDE_DIR}/armadillo_bits/arma_version.hpp" _armadillo_HEADER_CONTENTS REGEX "#define ARMA_VERSION_[A-Z]+ ")
     string(REGEX REPLACE ".*#define ARMA_VERSION_MAJOR ([0-9]+).*" "\\1" ARMADILLO_VERSION_MAJOR "${_armadillo_HEADER_CONTENTS}")
     string(REGEX REPLACE ".*#define ARMA_VERSION_MINOR ([0-9]+).*" "\\1" ARMADILLO_VERSION_MINOR "${_armadillo_HEADER_CONTENTS}")
     string(REGEX REPLACE ".*#define ARMA_VERSION_PATCH ([0-9]+).*" "\\1" ARMADILLO_VERSION_PATCH "${_armadillo_HEADER_CONTENTS}")
@@ -70,27 +65,28 @@ if(ARMADILLO_INCLUDE_DIR)
   set(ARMADILLO_VERSION_STRING "${ARMADILLO_VERSION_MAJOR}.${ARMADILLO_VERSION_MINOR}.${ARMADILLO_VERSION_PATCH}")
 endif ()
 
-# read relevant variables from header
+# Read relevant variables from header
 if(EXISTS "${ARMADILLO_INCLUDE_DIR}/armadillo_bits/config.hpp")
-  file(READ "${ARMADILLO_INCLUDE_DIR}/armadillo_bits/config.hpp" _armadillo_CONFIG_CONTENTS)
-  # ARMA_USE_WRAPPER
-  string(REGEX MATCH "\r?\n[\t ]*#define[ \t]+ARMA_USE_WRAPPER[ \t]*\r?\n" ARMA_USE_WRAPPER "${_armadillo_CONFIG_CONTENTS}")
-  # ARMA_USE_LAPACK
-  string(REGEX MATCH "\r?\n[\t ]*#if[\t ]+!defined[(]ARMA_USE_LAPACK[)][\t ]*\r?\n[\t ]*#define[ \t]+ARMA_USE_LAPACK[ \t]*\r?\n" ARMA_USE_LAPACK "${_armadillo_CONFIG_CONTENTS}")
-  # ARMA_USE_BLAS
-  string(REGEX MATCH "\r?\n[\t ]*#if[\t ]+!defined[(]ARMA_USE_BLAS[)][\t ]*\r?\n[\t ]*#define[ \t]+ARMA_USE_BLAS[ \t]*\r?\n" ARMA_USE_BLAS "${_armadillo_CONFIG_CONTENTS}")
-  # ARMA_USE_ARPACK
-  string(REGEX MATCH "\r?\n[\t ]*#if[\t ]+!defined[(]ARMA_USE_ARPACK[)][\t ]*\r?\n[\t ]*#define[ \t]+ARMA_USE_ARPACK[ \t]*\r?\n" ARMA_USE_ARPACK "${_armadillo_CONFIG_CONTENTS}")
-  # ARMA_USE_HDF5
-  string(REGEX MATCH "\r?\n[\t ]*#if[\t ]+!defined[(]ARMA_USE_HDF5[)][\t ]*\r?\n[\t ]*#define[ \t]+ARMA_USE_HDF5[ \t]*\r?\n" ARMA_USE_HDF5 "${_armadillo_CONFIG_CONTENTS}")
+  file(STRINGS "${ARMADILLO_INCLUDE_DIR}/armadillo_bits/config.hpp" _armadillo_CONFIG_CONTENTS REGEX "#define ARMA_USE_[A-Z]+ ")
+  # _ARMA_USE_WRAPPER
+  string(REGEX MATCH "\r?\n[\t ]*#define[ \t]+ARMA_USE_WRAPPER[ \t]*\r?\n" _ARMA_USE_WRAPPER "${_armadillo_CONFIG_CONTENTS}")
+  # _ARMA_USE_LAPACK
+  string(REGEX MATCH "\r?\n[\t ]*#if[\t ]+!defined[(]ARMA_USE_LAPACK[)][\t ]*\r?\n[\t ]*#define[ \t]+_ARMA_USE_LAPACK[ \t]*\r?\n" _ARMA_USE_LAPACK "${_armadillo_CONFIG_CONTENTS}")
+  # _ARMA_USE_BLAS
+  string(REGEX MATCH "\r?\n[\t ]*#if[\t ]+!defined[(]ARMA_USE_BLAS[)][\t ]*\r?\n[\t ]*#define[ \t]+_ARMA_USE_BLAS[ \t]*\r?\n" _ARMA_USE_BLAS "${_armadillo_CONFIG_CONTENTS}")
+  # _ARMA_USE_ARPACK
+  string(REGEX MATCH "\r?\n[\t ]*#if[\t ]+!defined[(]ARMA_USE_ARPACK[)][\t ]*\r?\n[\t ]*#define[ \t]+_ARMA_USE_ARPACK[ \t]*\r?\n" _ARMA_USE_ARPACK "${_armadillo_CONFIG_CONTENTS}")
+  # _ARMA_USE_HDF5
+  string(REGEX MATCH "\r?\n[\t ]*#if[\t ]+!defined[(]ARMA_USE_HDF5[)][\t ]*\r?\n[\t ]*#define[ \t]+_ARMA_USE_HDF5[ \t]*\r?\n" _ARMA_USE_HDF5 "${_armadillo_CONFIG_CONTENTS}")
+  unset(_armadillo_HEADER_CONTENTS)
 endif()
 
 include(FindPackageHandleStandardArgs)
 
-# if ARMA_USE_WRAPPER is set, then we just link to armadillo, but if it's not then we need support libraries instead
-set(ARMA_SUPPORT_LIBRARIES)
+# If _ARMA_USE_WRAPPER is set, then we just link to armadillo, but if it's not then we need support libraries instead
+set(_ARMA_SUPPORT_LIBRARIES)
 
-if(ARMA_USE_WRAPPER)
+if(_ARMA_USE_WRAPPER)
   # UNIX paths are standard, no need to write.
   find_library(ARMADILLO_LIBRARY
     NAMES armadillo
@@ -99,34 +95,46 @@ if(ARMA_USE_WRAPPER)
   find_package_handle_standard_args(Armadillo
     REQUIRED_VARS ARMADILLO_LIBRARY ARMADILLO_INCLUDE_DIR
     VERSION_VAR ARMADILLO_VERSION_STRING)
-else(ARMA_USE_WRAPPER)
+else(_ARMA_USE_WRAPPER)
   # don't link to armadillo in this case
   set(ARMADILLO_LIBRARY "")
-  if(ARMA_USE_LAPACK)
+  if(_ARMA_USE_LAPACK)
     find_package(LAPACK REQUIRED)
-    set(ARMA_SUPPORT_LIBRARIES "${ARMA_SUPPORT_LIBRARIES}" "${LAPACK_LIBRARIES}")
-  endif(ARMA_USE_LAPACK)
-  if(ARMA_USE_BLAS)
+    set(_ARMA_SUPPORT_LIBRARIES "${_ARMA_SUPPORT_LIBRARIES}" "${LAPACK_LIBRARIES}")
+  endif(_ARMA_USE_LAPACK)
+  if(_ARMA_USE_BLAS)
     find_package(BLAS REQUIRED)
-    set(ARMA_SUPPORT_LIBRARIES "${ARMA_SUPPORT_LIBRARIES}" "${BLAS_LIBRARIES}")
-  endif(ARMA_USE_BLAS)
-  if(ARMA_USE_ARPACK)
+    set(_ARMA_SUPPORT_LIBRARIES "${_ARMA_SUPPORT_LIBRARIES}" "${BLAS_LIBRARIES}")
+  endif(_ARMA_USE_BLAS)
+  if(_ARMA_USE_ARPACK)
     find_package(ARPACK REQUIRED)
-    set(ARMA_SUPPORT_LIBRARIES "${ARMA_SUPPORT_LIBRARIES}" "${ARPACK_LIBRARIES}")
-  endif(ARMA_USE_ARPACK)
+    set(_ARMA_SUPPORT_LIBRARIES "${_ARMA_SUPPORT_LIBRARIES}" "${ARPACK_LIBRARIES}")
+  endif(_ARMA_USE_ARPACK)
+  if(_ARMA_USE_HDF5)
+    find_package(HDF5 REQUIRED)
+    set(_ARMA_SUPPORT_LIBRARIES "${_ARMA_SUPPORT_LIBRARIES}" "${HDF5_LIBRARIES}")
+  endif(_ARMA_USE_HDF5)
   set(ARMADILLO_FOUND true)
   find_package_handle_standard_args(Armadillo
     REQUIRED_VARS ARMADILLO_INCLUDE_DIR
     VERSION_VAR ARMADILLO_VERSION_STRING)
-endif(ARMA_USE_WRAPPER)
+endif(_ARMA_USE_WRAPPER)
 
 if (ARMADILLO_FOUND)
   set(ARMADILLO_INCLUDE_DIRS ${ARMADILLO_INCLUDE_DIR})
-  set(ARMADILLO_LIBRARIES ${ARMADILLO_LIBRARY}  ${ARMA_SUPPORT_LIBRARIES})
+  set(ARMADILLO_LIBRARIES ${ARMADILLO_LIBRARY} ${_ARMA_SUPPORT_LIBRARIES})
 endif ()
+
+# Clean up internal variables
+unset(_ARMA_SUPPORT_LIBRARIES)
+unset(_ARMA_USE_WRAPPER)
+unset(_ARMA_USE_LAPACK)
+unset(_ARMA_USE_BLAS)
+unset(_ARMA_USE_ARPACK)
+unset(_ARMA_USE_HDF5)
 
 # Hide internal variables
 mark_as_advanced(
-  ARMA_SUPPORT_LIBRARIES
+  _ARMA_SUPPORT_LIBRARIES
   ARMADILLO_INCLUDE_DIR
   ARMADILLO_LIBRARY)
