aad257912b3288fb6856178eec2ecee96e88d8c9
Gaurav Singh <gs8763076@gmail.com>
2020-03-08 00:04:38 +0530

    Changing std::endl to n.

--- src/mlpack/methods/ann/rnn_impl.hpp
+++ src/mlpack/methods/ann/rnn_impl.hpp
@@ -79,7 +79,7 @@ WarnMessage(OptimizerType& optimizer, size_t no_of_datapoints) const
               << "dataset. To fix this, modify the maximum "
               << "number of iterations to be at least equal "
               << "to the number of points of your dataset "
-              << "(" << no_of_datapoints << ")." <<std::endl;
+              << "(" << no_of_datapoints << ")." << "\n";
   }
 }
 
