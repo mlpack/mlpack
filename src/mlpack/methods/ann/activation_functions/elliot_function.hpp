2c9fd3d168263afb6b062fb858b5f058d23ceff1
Bisakh Mondal <bisakhmondal00@gmail.com>
2020-03-09 00:51:03 +0530

    styles added

---
+++ src/mlpack/methods/ann/activation_functions/elliot_function.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file elliot_function.hpp
+ * @author Bisakh Mondal
+ *
+ * Definition and implementation of the Elliot Activation function as described 
+ * by D.L. Elliott
+ *
+ * For more information, see the following paper.
+ *
+ * @code
+ * @techreport{elliott1993better,
+ *   title={A better activation function for artificial neural networks},
+ *   author={Elliott, David L},
+ *   year={1993}
+ * }
+ * @endcode
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
