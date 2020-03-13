17c33ba615ac236fc9ec84f745000e88023fa585
Gaurav Singh <gs8763076@gmail.com>
2020-03-14 01:36:57 +0530

    Update src/mlpack/methods/ann/rnn.hpp
    Co-Authored-By: Marcus Edel <marcus.edel@fu-berlin.de>

--- src/mlpack/methods/ann/rnn.hpp
+++ src/mlpack/methods/ann/rnn.hpp
@@ -88,7 +88,7 @@ class RNN
       ::value, void>::type
   WarnMessage(OptimizerType& optimizer, size_t no_of_datapoints) const;
 
-/**
+ /**
    * Check if the optimizer has MaxIterations() parameter, if it
    * doesn't then simply return from the function.
    *
