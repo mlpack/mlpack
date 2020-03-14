fb39a4e443e903247d0f463c2fab6a1509486a8e
himanshupathak21061998 <hpathak336@gmail.com>
2020-03-14 21:53:30 +0530

    Adding some changes in implementation

--- src/mlpack/methods/ann/layer/radial_basis_function_impl.hpp
+++ src/mlpack/methods/ann/layer/radial_basis_function_impl.hpp
@@ -41,13 +41,11 @@ void RBF<InputDataType, OutputDataType>::Forward(
     const arma::Mat<eT>& input,
     arma::Mat<eT>& output)
 {
-  centres = arma::randu(outSize, inSize);
+  centres = arma::mat(outSize, input.n_cols, arma::fill::randu);
   centres = arma::normcdf(centres, 0, 1);
-  sigmas = arma::ones(outSize);
-  arma::cube x = arma::cube(outSize, input.n_rows, input.n_rows);
-  distances = arma::mat(input.n_rows, outSize);
-  arma::cube c = arma::cube(outSize, input.n_rows, input.n_rows);
-
+  sigmas = arma::ones(1,outSize);
+  arma::cube x = arma::cube(outSize, input.n_cols, input.n_rows);
+  
   for(size_t i=0;i < input.n_rows;i++)
   {
     for (size_t j = 0;j < outSize; j++)
@@ -55,13 +53,14 @@ void RBF<InputDataType, OutputDataType>::Forward(
       x.slice(i).row(j) = input.row(i);
     }
   }
-
+  arma::cube c = arma::cube(outSize, input.n_cols, input.n_rows);
   for(size_t i=0; i < input.n_rows; i++)
   {
     c.slice(i)= centres;
   }
-
-  for(size_t i=0;i < input.n_rows;i++)
+  distances = arma::mat(input.n_rows, outSize);
+  
+  for(size_t i=0;i < outSize;i++)
   { 
   distances.row(i) = arma::pow(arma::sum (arma::pow ((x.slice(i) - c.slice(i)), 2), 1), 0.5).t() * sigmas(i);
   }
--- src/mlpack/tests/feedforward_network_test.cpp
+++ src/mlpack/tests/feedforward_network_test.cpp
@@ -633,10 +633,12 @@ BOOST_AUTO_TEST_CASE(RBFNetworkTest)
   
 
   FFN<NegativeLogLikelihood<> > model;
-  model.Add<RBF<> >(trainData.n_rows, 8);
+  model.Add<RBF<> >(trainData.n_cols, 8);
   model.Add<GaussianFunctionLayer<> >();
+  model.Add<Linear<> >(trainData.n_rows, 8);
   model.Add<Linear<> >(8, 3);
   model.Add<LogSoftMax<> >();
+  std::cout<<trainData.n_cols;
 
   // Vanilla neural net with logistic activation function.
   // Because 92% of the patients are not hyperthyroid the neural
@@ -656,9 +658,10 @@ BOOST_AUTO_TEST_CASE(RBFNetworkTest)
   labels += 1;
 
   FFN<NegativeLogLikelihood<> > model1;
-  model1.Add<RBF<> >(dataset.n_rows, 10);
+  model1.Add<RBF<> >(dataset.n_cols, 10);
   model1.Add<GaussianFunctionLayer<> >();
-  model1.Add<Linear<> >(10, 2);
+  model1.Add<Linear<> >(dataset.n_rows, 10);
+  model.Add<Linear<> >(10, 2);
   model1.Add<LogSoftMax<> >();
   // Vanilla neural net with logistic activation function.
   TestNetwork<>(model1, dataset, labels, dataset, labels, 10, 0.2);
