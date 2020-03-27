4e826370ee9facdbbfa2314d9d766477ede0b378
nishantkr18 <nishantkr.ece18@itbhu.ac.in>
2020-03-27 15:21:07 +0530

    added comments

--- src/mlpack/methods/reinforcement_learning/q_networks/vanilla_dqn.hpp
+++ src/mlpack/methods/reinforcement_learning/q_networks/vanilla_dqn.hpp
@@ -28,10 +28,24 @@ class VanillaDQN
 {
  public:
 
+  /**
+   * Default constructor.
+   */
   VanillaDQN() : network() 
   { /* Nothing to do here. */ }
 
-  VanillaDQN(const int inputDim, const int hiddenDim1, const int hiddenDim2, const int outputDim) : network()
+  /**
+   * Construct an instance of VanillaDQN class.
+   *
+   * @param inputDim number of inputs.
+   * @param hiddenDim1 number of neurons in hiddenlayer-1.
+   * @param hiddenDim2 number of neurons in hiddenlayer-2.
+   * @param outputDim number of neurons in output layer.
+   */
+  VanillaDQN(const int inputDim, 
+            const int hiddenDim1, 
+            const int hiddenDim2, 
+            const int outputDim) : network()
   {
     FFN<MeanSquaredError<>, GaussianInitialization> model(MeanSquaredError<>(),
         GaussianInitialization(0, 0.001));
@@ -42,21 +56,42 @@ class VanillaDQN
     model.Add<Linear<>>(hiddenDim2, outputDim);
     network = model;
   }
+
   VanillaDQN(NetworkType network) : network(std::move(network))
   { /* Nothing to do here. */ }
 
+  /**
+   * Predict the responses to a given set of predictors. The responses will
+   * reflect the output of the given output layer as returned by the
+   * output layer function.
+   *
+   * If you want to pass in a parameter and discard the original parameter
+   * object, be sure to use std::move to avoid unnecessary copy.
+   *
+   * @param state Input state.
+   * @param actionValue Matrix to put output action values of states input.
+   */
   void Predict(const arma::mat state,
              arma::mat& actionValue)
   {
     network.Predict(state, actionValue);
   }
 
+  /**
+   * Perform the forward pass of the states in real batch mode.
+   *
+   * @param state The input state.
+   * @param target The predicted target.
+   */
   void Forward(const arma::mat state,
              arma::mat& target)
   {
     network.Forward(state, target);
   }
 
+  /**
+   * Resets the parameters of the network when empty.
+   */
   void ResetParametersIfEmpty()
   {
     if (network.Parameters().is_empty())
@@ -68,6 +103,13 @@ class VanillaDQN
   //! Modify the Parameters.
   arma::mat& Parameters() { return network.Parameters(); }
 
+  /**
+   * Perform the backward pass of the state in real batch mode.
+   *
+   * @param state The input state.
+   * @param target The training target.
+   * @return gradient The gradient.
+   */
   void Backward(const arma::mat state,
              arma::mat& target,
              arma::mat& gradient)
