f098b3ac35db5a2b726156326860dad52b62e737
Param-29 <param.mirani17@vit.edu>
2020-03-28 13:32:28 +0530

    Style Fix-3

--- src/mlpack/methods/naive_bayes/naive_bayes_classifier_impl.hpp
+++ src/mlpack/methods/naive_bayes/naive_bayes_classifier_impl.hpp
@@ -241,7 +241,7 @@ void NaiveBayesClassifier<ModelMatType>::LogLikelihood(
     ModelMatType rhs = -0.5 * arma::diagmat(invVar.col(i)) * diffs;
     arma::Mat<ElemType> exponents = arma::sum(diffs % rhs, 0);
     logLikelihoods.row(i) += (data.n_rows / -2.0 * log(2 * M_PI) - 0.5 *
-        arma::accu(arma::log(variances.col(i))) + exponents);    
+        arma::accu(arma::log(variances.col(i))) + exponents);
   }
 }
 
