b77d4b7d523fa1391e23222b23f7d57ba2ae2495
Gaurav Singh <gs8763076@gmail.com>
2020-04-01 01:28:55 +0530

    Add CELU activation function for the issue #2181 (#2191)
    * add-celu
    
    * add-celu
    
    * celu activation function added.
    
    * Add CELU activation function
    
    * minor style changes
    
    * Minor style changes.
    
    * Removing deterministic parameter and correcting typos
    
    * Solving syntax issue.
    
    * HISTORY.md and documentation correction.
    
    * Reintroducing the deterministic parameter and minor style changes in other files.
    
    * Minor changes.
    
    * Minor changes in elu.hpp
    
    * Changes in layer_types.hpp.
    
    * Style changes in celu.hpp and celu_impl.hpp.
    
    * Minor changes in celu_impl.hpp.
    
    * Changes in activation_functions_test.cpp.
    
    * Removing r-values references from celu.
    
    * Adding deterministic method and check for alpha parameter to avoid division by zero error.
    
    * Minor style changes
    
    * Minor changes.
    
    * Solving merge conflicts.
    
    * Minor changes.
    
    * Adding the url to the paper and correcting identations in activation_functions_test.cpp.
    
    * Minor changes.
    
    * Update src/mlpack/methods/ann/layer/celu.hpp
    
    Co-Authored-By: Marcus Edel <marcus.edel@fu-berlin.de>
    
    * Final changes.
    
    * Minor changes.
    
    * Initial commit.
    
    * Update src/mlpack/tests/activation_functions_test.cpp
    
    Co-Authored-By: Marcus Edel <marcus.edel@fu-berlin.de>
    
    * Update src/mlpack/tests/activation_functions_test.cpp
    
    Co-Authored-By: Marcus Edel <marcus.edel@fu-berlin.de>
    
    * Remove if from implementation.
    
    * Update src/mlpack/tests/activation_functions_test.cpp
    
    Co-Authored-By: Marcus Edel <marcus.edel@fu-berlin.de>
    
    * Update src/mlpack/tests/activation_functions_test.cpp
    
    Co-Authored-By: Marcus Edel <marcus.edel@fu-berlin.de>
    
    * Update src/mlpack/tests/activation_functions_test.cpp
    
    Co-Authored-By: Marcus Edel <marcus.edel@fu-berlin.de>
    
    Co-authored-by: Marcus Edel <marcus.edel@fu-berlin.de>

--- src/mlpack/methods/ann/layer/elu_impl.hpp
+++ src/mlpack/methods/ann/layer/elu_impl.hpp
@@ -3,7 +3,7 @@
  * @author Vivek Pal
  * @author Dakshit Agrawal
  *
- * Implementation of the ELU activation function as descibed by Djork-Arne
+ * Implementation of the ELU activation function as described by Djork-Arne
  * Clevert, Thomas Unterthiner and Sepp Hochreiter.
  *
  * Implementation of the SELU function as introduced by Klambauer et. al. in
@@ -51,7 +51,7 @@ template<typename InputType, typename OutputType>
 void ELU<InputDataType, OutputDataType>::Forward(
     const InputType& input, OutputType& output)
 {
-  output.set_size(arma::size(input));
+  output = arma::ones<OutputDataType>(arma::size(input));
   for (size_t i = 0; i < input.n_elem; i++)
   {
     if (input(i) < DBL_MAX)
@@ -59,8 +59,6 @@ void ELU<InputDataType, OutputDataType>::Forward(
       output(i) = (input(i) > 0) ? lambda * input(i) : lambda *
           alpha * (std::exp(input(i)) - 1);
     }
-    else
-      output(i) = 1.0;
   }
 
     if (!deterministic)
