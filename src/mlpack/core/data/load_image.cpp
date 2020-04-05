674737eb190a01adae7e000364a3b0d0224a1adf
Ryan Curtin <ryan@ratml.org>
2020-04-04 23:14:47 -0400

    Workaround Debian buster STB include bug.

--- src/mlpack/core/data/load_image.cpp
+++ src/mlpack/core/data/load_image.cpp
@@ -9,13 +9,12 @@
 
 #ifdef HAS_STB
 
+// The definition of STB_IMAGE_IMPLEMENTATION means that the implementation will
+// be included here directly.
 #define STB_IMAGE_STATIC
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
 
-#define STB_IMAGE_WRITE_STATIC
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb_image_write.h>
+#include <stb_image.h>
 
 namespace mlpack {
 namespace data {
