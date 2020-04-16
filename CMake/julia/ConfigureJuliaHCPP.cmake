506bc4593c478434a9cebada2fdc8e1987fa7008
Ryan Curtin <ryan@ratml.org>
2020-04-16 13:36:40 -0400

    Accept model types with <> in the name.

--- CMake/julia/ConfigureJuliaHCPP.cmake
+++ CMake/julia/ConfigureJuliaHCPP.cmake
@@ -15,56 +15,72 @@ file(READ "${PROGRAM_MAIN_FILE}" MAIN_FILE)
 
 # Grab all "PARAM_MODEL_IN(Model,", "PARAM_MODEL_IN_REQ(Model,",
 # "PARAM_MODEL_OUT(Model,".
-string(REGEX MATCHALL "PARAM_MODEL_IN\\([A-Za-z_]*," MODELS_IN "${MAIN_FILE}")
-string(REGEX MATCHALL "PARAM_MODEL_IN_REQ\\([A-Za-z_]*," MODELS_IN_REQ
+string(REGEX MATCHALL "PARAM_MODEL_IN\\([A-Za-z_<>]*," MODELS_IN
+    "${MAIN_FILE}")
+string(REGEX MATCHALL "PARAM_MODEL_IN_REQ\\([A-Za-z_<>]*," MODELS_IN_REQ
     "${MAIN_FILE}")
 string(REGEX MATCHALL "PARAM_MODEL_OUT\\([A-Za-z_]*," MODELS_OUT "${MAIN_FILE}")
 
 string(REGEX REPLACE "PARAM_MODEL_IN\\(" "" MODELS_IN_STRIP1 "${MODELS_IN}")
 string(REGEX REPLACE "," "" MODELS_IN_STRIP2 "${MODELS_IN_STRIP1}")
+string(REGEX REPLACE "[<>,]" "" MODELS_IN_SAFE_STRIP2 "${MODELS_IN_STRIP1}")
 
 string(REGEX REPLACE "PARAM_MODEL_IN_REQ\\(" "" MODELS_IN_REQ_STRIP1
     "${MODELS_IN_REQ}")
 string(REGEX REPLACE "," "" MODELS_IN_REQ_STRIP2 "${MODELS_IN_REQ_STRIP1}")
+string(REGEX REPLACE "[<>,]" "" MODELS_IN_REQ_SAFE_STRIP2
+    "${MODELS_IN_REQ_STRIP1}")
 
 string(REGEX REPLACE "PARAM_MODEL_OUT\\(" "" MODELS_OUT_STRIP1 "${MODELS_OUT}")
 string(REGEX REPLACE "," "" MODELS_OUT_STRIP2 "${MODELS_OUT_STRIP1}")
+string(REGEX REPLACE "[<>,]" "" MODELS_OUT_SAFE_STRIP2 "${MODELS_OUT_STRIP1}")
 
-set(MODEL_TYPES ${MODELS_IN_STRIP2} ${MODELS_IN_REQ_STRIP2} ${MODELS_OUT_STRIP2})
+set(MODEL_TYPES ${MODELS_IN_STRIP2} ${MODELS_IN_REQ_STRIP2}
+    ${MODELS_OUT_STRIP2})
+set(MODEL_SAFE_TYPES ${MODELS_IN_SAFE_STRIP2} ${MODELS_IN_REQ_SAFE_STRIP2}
+    ${MODELS_OUT_SAFE_STRIP2})
 if (MODEL_TYPES)
   list(REMOVE_DUPLICATES MODEL_TYPES)
+endif ()
+if (MODEL_SAFE_TYPES)
+  list(REMOVE_DUPLICATES MODEL_SAFE_TYPES)
 endif ()
 
 # Now, generate the definitions of the functions we need.
 set(MODEL_PTR_DEFNS "")
 set(MODEL_PTR_IMPLS "")
-foreach (MODEL_TYPE ${MODEL_TYPES})
-  # Generate the definition.
-  set(MODEL_PTR_DEFNS "${MODEL_PTR_DEFNS}
-// Get the pointer to a ${MODEL_TYPE} parameter.
-void* CLI_GetParam${MODEL_TYPE}Ptr(const char* paramName);
-// Set the pointer to a ${MODEL_TYPE} parameter.
-void CLI_SetParam${MODEL_TYPE}Ptr(const char* paramName, void* ptr);
+list(LENGTH MODEL_TYPES NUM_MODEL_TYPES)
+if (${NUM_MODEL_TYPES} GREATER 0)
+  foreach (INDEX RANGE NUM_MODEL_TYPES)
+    list(GET MODEL_TYPES ${INDEX} MODEL_TYPE)
+    list(GET MODEL_SAFE_TYPES ${INDEX} MODEL_SAFE_TYPE)
 
+    # Generate the definition.
+    set(MODEL_PTR_DEFNS "${MODEL_PTR_DEFNS}
+// Get the pointer to a ${MODEL_TYPE} parameter.
+void* CLI_GetParam${MODEL_SAFE_TYPE}Ptr(const char* paramName);
+// Set the pointer to a ${MODEL_TYPE} parameter.
+void CLI_SetParam${MODEL_SAFE_TYPE}Ptr(const char* paramName, void* ptr);
 ")
 
-  # Generate the implementation.
-  set(MODEL_PTR_IMPLS "${MODEL_PTR_IMPLS}
+    # Generate the implementation.
+    set(MODEL_PTR_IMPLS "${MODEL_PTR_IMPLS}
 // Get the pointer to a ${MODEL_TYPE} parameter.
-void* CLI_GetParam${MODEL_TYPE}Ptr(const char* paramName)
+void* CLI_GetParam${MODEL_SAFE_TYPE}Ptr(const char* paramName)
 {
   return (void*) CLI::GetParam<${MODEL_TYPE}*>(paramName);
 }
 
 // Set the pointer to a ${MODEL_TYPE} parameter.
-void CLI_SetParam${MODEL_TYPE}Ptr(const char* paramName, void* ptr)
+void CLI_SetParam${MODEL_SAFE_TYPE}Ptr(const char* paramName, void* ptr)
 {
   CLI::GetParam<${MODEL_TYPE}*>(paramName) = (${MODEL_TYPE}*) ptr;
   CLI::SetPassed(paramName);
 }
 
 ")
-endforeach ()
+  endforeach ()
+endif()
 
 # Now configure both of the files.
 configure_file("${JULIA_H_IN}" "${JULIA_H_OUT}")
