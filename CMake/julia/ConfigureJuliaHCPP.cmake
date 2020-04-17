72885f678a32183844b4bb3f265dd1eb1fc59c04
Ryan Curtin <ryan@ratml.org>
2020-04-17 13:48:02 -0400

    Fix missing parenthesis.

--- CMake/julia/ConfigureJuliaHCPP.cmake
+++ CMake/julia/ConfigureJuliaHCPP.cmake
@@ -88,7 +88,7 @@ char* Serialize${MODEL_SAFE_TYPE}Ptr(void* ptr, size_t* length)
   std::ostringstream oss;
   {
     boost::archive::binary_oarchive oa(oss);
-    oa << boost::serialization::make_nvp(\"${MODEL_SAFE_TYPE}\", ((${MODEL_TYPE}*) ptr);
+    oa << boost::serialization::make_nvp(\"${MODEL_SAFE_TYPE}\", ((${MODEL_TYPE}*) ptr));
   }
 
   *length = oss.str().length();
