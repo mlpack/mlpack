e8b2c5089a3d1db1d6acdeb0f14f6924c4255c78
Omar Shrit <shrit@lri.fr>
2020-05-13 21:47:52 +0200

    Create a fuction that wrap rpi config
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- board/rpi-config.cmake
+++ board/rpi-config.cmake
@@ -1,21 +1,22 @@
 ## A small sketch to see where we can go with RPI configurations
 
-if(rpi0, 1)
+function(RPI BOARD_NAME)
+if(BOARD_NAME STREQUAL "rpi0" OR BOARD_NAME STREQUAL "rpi1")
   set(CMAKE_SYSTEM_PROCESSOR BCM2835)
-elif(rpi 2)
+elseif(BOARD_NAME STREQUAL "rpi2")
   set(CMAKE_SYSTEM_PROCESSOR BCM2836)
-elif(rpi 3)
+elseif(BOARD_NAME STREQUAL "rpi3")
   set(CMAKE_SYSTEM_PROCESSOR BCM2837)
-elif(rpi 4)
+elseif(BOARD_NAME STREQUAL "rpi4")
   set(CMAKE_SYSTEM_PROCESSOR BCM2711)
 endif()
 
 # Set a toolchain path.
 set(TOOLCHAIN_PATH "")
 
-if(rpi0, 1, 2)
+if(BOARD_NAME STREQUAL "rpi0" OR BOARD_NAME STREQUAL "rpi1" OR BOARD_NAME STREQUAL "rpi2")
   set(CMAKE_CXX_COMPILER ${TC_PATH}arm-linux-gnueabihf-g++)
-elif(rpi 3 4)
+elseif(BOARD_NAME STREQUAL "rpi3" OR BOARD_NAME STREQUAL "rpi4")
   set(CMAKE_CXX_COMPILER ${TC_PATH}aarch64-linux-gnu-g++)
 endif()
 
@@ -23,12 +24,13 @@ endif()
 set(CMAKE_CX_FLAGS "${CMAKE_CXX_FLAGS} -0s -flto ")
 
 # Set the CMAKE CXX flags
-if(rpi0, 1)
+if(BOARD_NAME STREQUAL "rpi0" OR BOARD_NAME STREQUAL "rpi1")
   set(CMAKE_CX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mcpu=arm1176jzf-s")
-elif(rpi 2)
+elseif(BOARD_NAME STREQUAL "rpi2")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-vfpv4 -mfloat-abi=hard̈́ -march=armv7-a -mtune=cortex-a7")
-elif(rpi 3)
+elseif(BOARD_NAME STREQUAL "rpi3")
   set(CMAKE_CX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-fp-armv8 -mfloat-abi=hard -mcpu=cortex-a53 -mneon-for-64bits")
-elif(rpi 4)
+elseif(BOARD_NAME STREQUAL "rpi4")
   set(CMAKE_CX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-fp-armv8 -mfloat-abi=hard -mcpu=cortex-a72 -mneon-for-64bits")
 endif()
+endfunction()
