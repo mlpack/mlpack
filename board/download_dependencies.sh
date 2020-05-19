2b2b476ca1b256df3f945d13454380bf1d4eb0d1
Omar Shrit <shrit@lri.fr>
2020-05-19 20:03:49 +0200

    Compile boost + OpenBLAS for arm64
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- board/download_dependencies.sh
+++ board/download_dependencies.sh
@@ -6,17 +6,37 @@
 
 version=$1
 version_underscore=$2
+arch=$3
+binary=$4
 
-
-echo "Let us download boost as dependecies..."
+echo "Download and build boost as a static lbrary"
 wget --no-check-certificate \
       "https://dl.bintray.com/boostorg/release/$version/source/boost_$version_underscore.tar.gz" && \
     tar xvzf boost_$version_underscore.tar.gz && \
     rm -f boost_$version_underscore.tar.gz && \
     cd boost_$version_underscore && \
     cp tools/build/example/user-config.jam project-config.jam && \
-    echo 'using gcc : arm : arm-linux-gnueabihf-g++ ;' >> project-config.jam &&\
+    echo 'using gcc : arm : aarch64-linux-gnu-g++ ;' >> project-config.jam &&\
     b2 --build=$PWD/build toolset=gcc-arm link=static cxxflags=-fPIC && \
     -with-libraries=math,program_options,serialization,test && \
-    cd ../ && \
-    rm -rf boost_$version_underscore
+    cd ../../ && \
+
+
+echo "Downlaod and build OpenBLAS as a static library"
+wget --no-check-certificate \
+      "https://github.com/xianyi/OpenBLAS/archive/v0.3.9.tar.gz" && \
+      tar xvzf v0.3.9.tar.gz && \
+      rm -f v0.3.9.tar.gz && \
+      cd OpenBLAS-0.3.9 && \
+      make TARGET=ARMV8 BINARY=64 HOSTCC=gcc CC=aarch64-linux-gnu-gcc NOFORTRAN=1 libs &&\
+      cd ../ &&\
+
+# echo "Download and build armadillo as a header"
+# wget --no-check-certificate \
+#     http://masterblaster.mlpack.org:5005/$arma_version.tar.gz && \
+#     tar xvzf $arma_version.tar.gz && \
+#     rm -f $arma_version.tar.gz && \
+#     cd $arma_version && \
+#     make -j32 && \
+#     cd .. && \
+#     rm -rf $arma_version
