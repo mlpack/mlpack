f3ebf5c8f4ab75cbd1c1355b67127d5a6b99afdb
Omar Shrit <shrit@lri.fr>
2020-05-19 22:02:57 +0200

    Download armadillo, not finished yet
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- board/download_dependencies.sh
+++ board/download_dependencies.sh
@@ -9,34 +9,34 @@ version_underscore=$2
 arch=$3
 binary=$4
 
-echo "Download and build boost as a static lbrary"
-wget --no-check-certificate \
-      "https://dl.bintray.com/boostorg/release/$version/source/boost_$version_underscore.tar.gz" && \
-    tar xvzf boost_$version_underscore.tar.gz && \
-    rm -f boost_$version_underscore.tar.gz && \
-    cd boost_$version_underscore && \
-    cp tools/build/example/user-config.jam project-config.jam && \
-    echo 'using gcc : arm : aarch64-linux-gnu-g++ ;' >> project-config.jam &&\
-    b2 --build=$PWD/build toolset=gcc-arm link=static cxxflags=-fPIC && \
-    -with-libraries=math,program_options,serialization,test && \
-    cd ../../ && \
-
-
-echo "Downlaod and build OpenBLAS as a static library"
-wget --no-check-certificate \
-      "https://github.com/xianyi/OpenBLAS/archive/v0.3.9.tar.gz" && \
-      tar xvzf v0.3.9.tar.gz && \
-      rm -f v0.3.9.tar.gz && \
-      cd OpenBLAS-0.3.9 && \
-      make TARGET=ARMV8 BINARY=64 HOSTCC=gcc CC=aarch64-linux-gnu-gcc NOFORTRAN=1 libs &&\
-      cd ../ &&\
-
-# echo "Download and build armadillo as a header"
+# echo "Download and build boost as a static lbrary"
 # wget --no-check-certificate \
-#     http://masterblaster.mlpack.org:5005/$arma_version.tar.gz && \
-#     tar xvzf $arma_version.tar.gz && \
-#     rm -f $arma_version.tar.gz && \
-#     cd $arma_version && \
-#     make -j32 && \
-#     cd .. && \
-#     rm -rf $arma_version
+#       "https://dl.bintray.com/boostorg/release/$version/source/boost_$version_underscore.tar.gz" && \
+#     tar xvzf boost_$version_underscore.tar.gz && \
+#     rm -f boost_$version_underscore.tar.gz && \
+#     cd boost_$version_underscore && \
+#     cp tools/build/example/user-config.jam project-config.jam && \
+#     echo 'using gcc : arm : aarch64-linux-gnu-g++ ;' >> project-config.jam &&\
+#     b2 --build=$PWD/build toolset=gcc-arm link=static cxxflags=-fPIC && \
+#     -with-libraries=math,program_options,serialization,test && \
+#     cd ../../ && \
+
+
+# echo "Downlaod and build OpenBLAS as a static library"
+# wget --no-check-certificate \
+#       "https://github.com/xianyi/OpenBLAS/archive/v0.3.9.tar.gz" && \
+#       tar xvzf v0.3.9.tar.gz && \
+#       rm -f v0.3.9.tar.gz && \
+#       cd OpenBLAS-0.3.9 && \
+#       make TARGET=ARMV8 BINARY=64 HOSTCC=gcc CC=aarch64-linux-gnu-gcc NOFORTRAN=1 libs &&\
+#       cd ../ &&\
+
+echo "Download and build armadillo as a header"
+wget --no-check-certificate \
+    http://sourceforge.net/projects/arma/files/armadillo-9.880.1.tar.xz && \
+    tar xvf armadillo-9.880.1.tar.xz && \
+    rm -f armadillo-9.880.1.tar.xz && \
+    cd  armadillo-9.880.1
+    mkdir build && cd build
+    cmake -DCMAKE_TOOLCHAIN_FILE=../../arm-64bit-toolchain.cmake ../
+    
