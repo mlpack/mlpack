c10e887f8e62399f36db5a04aea34bf5c5bf834c
nishantkr18 <nniishantkumar@gmail.com>
2020-05-20 19:11:45 +0530

    style-issues in empty-loss

--- src/mlpack/methods/ann/loss_functions/empty_loss_impl.hpp
+++ src/mlpack/methods/ann/loss_functions/empty_loss_impl.hpp
@@ -28,21 +28,20 @@ EmptyLoss<InputDataType, OutputDataType>::EmptyLoss()
 template<typename InputDataType, typename OutputDataType>
 template<typename InputType, typename TargetType>
 double EmptyLoss<InputDataType, OutputDataType>::Forward(
-  const InputType&& input, const TargetType&& target)
+    const InputType&& input, const TargetType&& target)
 {
-    return 0;
+  return 0;
 }
 
- template<typename InputDataType, typename OutputDataType>
- template<typename InputType, typename TargetType, typename OutputType>
- void EmptyLoss<InputDataType, OutputDataType>::Backward(
-     const InputType&& input,
-     const TargetType&& target,
-     OutputType&& output)
- {
-   // todo: check correctness
-   output = input;
- }
+template<typename InputDataType, typename OutputDataType>
+template<typename InputType, typename TargetType, typename OutputType>
+void EmptyLoss<InputDataType, OutputDataType>::Backward(
+    const InputType&& input,
+    const TargetType&& target,
+    OutputType&& output)
+{
+  output = input;
+}
 
 template<typename InputDataType, typename OutputDataType>
 template<typename Archive>
