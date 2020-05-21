66430727b62c0756f8395494d2f879e6e2765f42
Omar Shrit <shrit@lri.fr>
2020-05-21 13:43:14 +0200

    Adding parent scope inside the function.
    This will make flags visible in the global scope CMakeLists
    
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- board/flags-config.cmake
+++ board/flags-config.cmake
@@ -16,16 +16,16 @@
 
 function(flags BOARD_NAME)
   # Set all minimization flags for all platforms
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -0s -flto")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Os -flto")
 
   # Set the CMAKE CXX flags
   if(BOARD_NAME STREQUAL "rpi0" OR BOARD_NAME STREQUAL "rpi1")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mcpu=arm1176jzf-s")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mcpu=arm1176jzf-s" PARENT_SCOPE)
   elseif(BOARD_NAME STREQUAL "rpi2")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-vfpv4 -mfloat-abi=hard̈́ -march=armv7-a -mtune=cortex-a7")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-vfpv4 -mfloat-abi=hard̈́ -march=armv7-a -mtune=cortex-a7" PARENT_SCOPE)
   elseif(BOARD_NAME STREQUAL "rpi3")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-fp-armv8 -mfloat-abi=hard -mcpu=cortex-a53 -mneon-for-64bits")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}  -mcpu=cortex-a53 " PARENT_SCOPE)
   elseif(BOARD_NAME STREQUAL "rpi4")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-fp-armv8 -mfloat-abi=hard -mcpu=cortex-a72 -mneon-for-64bits")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-fp-armv8 -mfloat-abi=hard -mcpu=cortex-a72 -mneon-for-64bits" PARENT_SCOPE)
   endif()
 endfunction()
