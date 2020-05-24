e2677c91edb7398b70e5b37a2f12fe9e5e5bb298
UtR491 <64355352+UtR491@users.noreply.github.com>
2020-05-25 00:29:01 +0530

    Implementation of the SSIM metric

---
+++ src/mlpack/core/cv/metrics/ssim_index.hpp
@@ -0,0 +1,34 @@
+/**
+ * @file ssim_index.hpp
+ * @author Utkarsh Rai
+ *
+ * The SSIM (Structural Similarity) metric.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_CORE_CV_METRICS_SSIM_HPP
+#define MLPACK_CORE_CV_METRICS_SSIM_HPP
+
+#include <mlpack/core.hpp>
+
+namespace mlpack {
+	namespace cv {
+
+		class SSIMIndex {
+			public:
+
+				template<typename DataType>
+					static double Evaluate(DataType const& image,
+							DataType const& reference);
+
+				static const bool NeedsMinimization = false;
+		}; //class SSIMIndex
+	} //namespace cv
+} //namespace mlpack
+
+#include "ssim_index_impl.hpp"
+
+#endif
---
+++ src/mlpack/core/cv/metrics/ssim_index_impl.hpp
@@ -0,0 +1,53 @@
+#ifndef MLPACK_CORE_CV_METRICS_SSIMINDEX_IMPL_HPP
+#define MLPACK_CORE_CV_METRICS_SSIMINDEX_IMPL_HPP
+
+#include <mlpack/core.hpp>
+
+namespace mlpack {
+	namespace cv {
+
+		template<typename DataType>
+			double SSIMIndex::Evaluate(DataType const& image,
+					DataType const& reference)
+			{
+				if (image.n_cols != reference.n_cols || image.n_rows != reference.n_rows)
+				{
+					std::ostringstream oss;
+					oss << "SSIMIndex::Evaluate(): size of reference (" 
+						<<reference.n_cols<<"X"<<reference.n_rows
+						<<") does not match size of image ("
+						<<image.n_cols<<"X"<<image.n_rows<<")!"
+						<<std::endl;
+					throw std::invalid_argument(oss.str());
+				}
+
+				kernel::GaussianKernel gaussianKernel = kernel::GaussianKernel(1.5);
+				DataType gaussianWindow = gaussianKernel.Evaluate(arma::regspace(-5, 5), arma::regspace(-5, 5));
+
+
+				arma::mat meanReference = arma::conv2(reference, gaussianWindow, "same"); 
+
+				arma::mat meanImage =  arma::conv2(image, gaussianWindow, "same");
+
+				arma::mat varianceReference = arma::conv2(arma::square(reference), gaussianWindow, "same") - arma::square(meanReference);
+
+				arma::mat varianceImage = arma::conv2(arma::square(image), gaussianWindow, "same") - arma::square(meanImage);
+
+				arma::mat covarianceReferenceImage = arma::conv2(reference%image, gaussianWindow, "same") - meanReference%meanImage;
+
+				double dynamicRange = image.max - image.min;
+
+				double regularisationConstant1 = 0.0001*dynamicRange*dynamicRange;
+
+				double regularisationConstant2 = 0.0009*dynamicRange*dynamicRange;
+
+				arma::mat localSSIM = ((2*meanReference%meanImage + regularisationConstant1)%(2*covarianceReferenceImage + regularisationConstant2))/((arma::square(meanReference)+arma::square(meanImage)+regularisationConstant1)%(varianceReference + varianceImage + regularisationConstant2));
+
+				double meanSSIM = arma::mean(localSSIM);
+				return meanSSIM;
+
+			}
+
+	}//namespace cv
+}//namespace mlpack
+#endif
