ea265ba77bf2427fc669eb06bd93cff7f0b206ac
Omar Shrit <omar@shrit.me>
2020-05-25 17:04:03 +0200

    Adding fdata sections, and gc-sections.
    We are not gaining even 1KB by adding these flags.
    Just keep them here for now
    
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- board/flags-config.cmake
+++ board/flags-config.cmake
@@ -16,7 +16,7 @@
 
 function(flags BOARD_NAME)
   # Set all minimization flags for all platforms
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fuse-ld=gold -Os -fomit-frame-pointer -fno-unwind-tables -fno-asynchronous-unwind-tables -flto")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fuse-ld=gold -Os -fdata-sections -ffunction-sections -fomit-frame-pointer -fno-unwind-tables -fno-asynchronous-unwind-tables -flto")
 
   # Set the CMAKE CXX flags
   if(BOARD_NAME STREQUAL "rpi0" OR BOARD_NAME STREQUAL "rpi1")
@@ -25,6 +25,7 @@ function(flags BOARD_NAME)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-vfpv4 -mfloat-abi=hard̈́ -march=armv7-a -mtune=cortex-a7" PARENT_SCOPE)
   elseif(BOARD_NAME STREQUAL "rpi3")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}  -mcpu=cortex-a53 " PARENT_SCOPE)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
   elseif(BOARD_NAME STREQUAL "rpi4")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-fp-armv8 -mfloat-abi=hard -mcpu=cortex-a72 -mneon-for-64bits" PARENT_SCOPE)
   endif()
