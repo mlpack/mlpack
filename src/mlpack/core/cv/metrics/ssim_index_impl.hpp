79b49c0ba911fa62d42954686d11ff3a5bf80f97
UtR491 <64355352+UtR491@users.noreply.github.com>
2020-05-27 22:11:12 +0530

    Update ssim_index_impl.hpp
    style issues

--- src/mlpack/core/cv/metrics/ssim_index_impl.hpp
+++ src/mlpack/core/cv/metrics/ssim_index_impl.hpp
@@ -13,14 +13,12 @@ double SSIMIndex::Evaluate(DataType const& image,
  if (arma::size(reference) != arma::size(image))
  {
   std::ostringstream oss;
-  oss << "SSIMIndex::Evaluate(): size of reference (" 
-    <<reference.n_cols<<"X"<<reference.n_rows
+  oss << "SSIMIndex::Evaluate(): size of reference ("
     <<") does not match size of image ("
     <<image.n_cols<<"X"<<image.n_rows<<")!"
     <<std::endl;
   throw std::invalid_argument(oss.str());
  }
-
  kernel::GaussianKernel gaussianKernel = kernel::GaussianKernel(1.5);
  arma::mat gaussianWindow(11, 11);
  for ( int row = 0; row < 11; row++)
@@ -31,37 +29,27 @@ double SSIMIndex::Evaluate(DataType const& image,
   }
  }
  arma::mat meanReference = arma::conv2(reference, gaussianWindow, "same"); 
-
  arma::mat meanImage =  arma::conv2(image, gaussianWindow, "same");
-
  arma::mat varianceReference = arma::conv2(arma::square(reference),
                                            gaussianWindow, "same") -
                                           arma::square(meanReference);
-
  arma::mat varianceImage = arma::conv2(arma::square(image),
                                        gaussianWindow, "same") -
                                       arma::square(meanImage);
-
  arma::mat covarianceReferenceImage = arma::conv2(reference%image,
                                                   gaussianWindow, "same") -
                                                  meanReference%meanImage;
-
  double dynamicRange = image.max() - image.min();
-
  double regularisationConstant1 = 0.0001 * dynamicRange * dynamicRange;
-
  double regularisationConstant2 = 0.0009 * dynamicRange * dynamicRange;
-
  arma::mat localSSIM = ((2*meanReference%meanImage + regularisationConstant1)%
                         (2*covarianceReferenceImage + regularisationConstant2))/
                        ((arma::square(meanReference)+arma::square(meanImage)+
                          regularisationConstant1)%
                         (varianceReference + varianceImage +
                          regularisationConstant2));
-
  double meanSSIM = arma::mean(arma::mean(localSSIM));
  return meanSSIM;
-
 }
 
 }//namespace cv
