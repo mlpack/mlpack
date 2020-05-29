b95745fb3e8ba34a5570dbebbe41d3643f64c619
Omar Shrit <omar@shrit.me>
2020-05-29 15:34:28 +0200

    Adding parallella board name
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- board/flags-config.cmake
+++ board/flags-config.cmake
@@ -1,18 +1,4 @@
-## A small sketch to see where we can go with RPI configurations
-
-## Not sure if we merge these with the arm files
-## TODO:: VERIFY later
-
-# if(BOARD_NAME STREQUAL "rpi0" OR BOARD_NAME STREQUAL "rpi1")
-#   set(CMAKE_SYSTEM_PROCESSOR BCM2835)
-# elseif(BOARD_NAME STREQUAL "rpi2")
-#   set(CMAKE_SYSTEM_PROCESSOR BCM2836)
-# elseif(BOARD_NAME STREQUAL "rpi3")
-#   set(CMAKE_SYSTEM_NAME Linux)
-#   set(CMAKE_SYSTEM_PROCESSOR BCM2837)
-# elseif(BOARD_NAME STREQUAL "rpi4")
-#   set(CMAKE_SYSTEM_PROCESSOR BCM2711)
-# endif()
+# Flags for each supported board
 
 function(flags BOARD_NAME)
   # Set all minimization flags for all platforms
@@ -21,12 +7,18 @@ function(flags BOARD_NAME)
   # Set the CMAKE CXX flags
   if(BOARD_NAME STREQUAL "rpi0" OR BOARD_NAME STREQUAL "rpi1")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mcpu=arm1176jzf-s" PARENT_SCOPE)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
   elseif(BOARD_NAME STREQUAL "rpi2")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-vfpv4 -mfloat-abi=hard̈́ -march=armv7-a -mtune=cortex-a7" PARENT_SCOPE)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")  
   elseif(BOARD_NAME STREQUAL "rpi3")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}  -mcpu=cortex-a53 " PARENT_SCOPE)
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
   elseif(BOARD_NAME STREQUAL "rpi4")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon-fp-armv8 -mfloat-abi=hard -mcpu=cortex-a72 -mneon-for-64bits" PARENT_SCOPE)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
+  elseif(BOARD_NAME STREQUAL "parallella")
+   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} " PARENT_SCOPE)
+   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")  
   endif()
 endfunction()
