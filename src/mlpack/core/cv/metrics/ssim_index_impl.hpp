5e7e7deb60279152f07753d51ee2c157d0644be3
UtR491 <64355352+UtR491@users.noreply.github.com>
2020-05-30 15:40:00 +0530

    Update ssim_index_impl.hpp
    Style changes.

--- src/mlpack/core/cv/metrics/ssim_index_impl.hpp
+++ src/mlpack/core/cv/metrics/ssim_index_impl.hpp
@@ -25,13 +25,13 @@ double SSIMIndex::Evaluate(DataType const& image,
   {
     std::ostringstream oss;
     oss << "SSIMIndex::Evaluate(): size of reference ("
-      <<reference.n_cols<<"X"<<reference.n_rows
-      <<") does not match size of image ("
-      <<image.n_cols<<"X"<<image.n_rows<<")!"
-      <<std::endl;
+      << reference.n_cols << "X" << reference.n_rows
+      << ") does not match size of image ("
+      << image.n_cols << "X" << image.n_rows << ")!"
+      << std::endl;
     throw std::invalid_argument(oss.str());
   }
-  // Instantitating a Gaussian Kernel with bandwidth 1.5
+  // Instantitating a Gaussian Kernel with bandwidth 1.5.
   kernel::GaussianKernel gaussianKernel = kernel::GaussianKernel(1.5);
   
   // Gaussian Window of size 11X11.
@@ -45,49 +45,45 @@ double SSIMIndex::Evaluate(DataType const& image,
                                     arma::linspace(-6, column - 5, 1));
     }
   }
-  // Calculate local mean of reference
+  // Calculate local mean of reference.
   arma::mat meanReference = arma::conv2(reference, gaussianWindow, "same");
   
-  // Calculate local mean of image
+  // Calculate local mean of image.
   arma::mat meanImage =  arma::conv2(image, gaussianWindow, "same");
 
-  // Calculate local variance of reference
+  // Calculate local variance of reference.
   arma::mat varianceReference = arma::conv2(arma::square(reference),
-                                            gaussianWindow, "same") -
-                                            arma::square(meanReference);
+      gaussianWindow, "same") - arma::square(meanReference);
 
-  // Calculate local variance of image
+  // Calculate local variance of image.
   arma::mat varianceImage = arma::conv2(arma::square(image),
-                                        gaussianWindow, "same") -
-                                        arma::square(meanImage);
+      gaussianWindow, "same") - arma::square(meanImage);
   
-  // Calulate covariance of image and reference
+  // Calulate covariance of image and reference.
   arma::mat covarianceReferenceImage = arma::conv2(reference%image,
-                                                   gaussianWindow, "same") -
-                                                   meanReference%meanImage;
+      gaussianWindow, "same") - meanReference%meanImage;
 
-  // Calculate Dynamic Range
+  // Calculate Dynamic Range.
   double dynamicRange = image.max() - image.min();
 
-  // Calculate C1
+  // Calculate C1.
   double regularisationConstant1 = 0.0001 * dynamicRange * dynamicRange;
 
-  // Calculate C2
+  // Calculate C2.
   double regularisationConstant2 = 0.0009 * dynamicRange * dynamicRange;
 
   // Calculate local SSIM index.
-  arma::mat localSSIM = ((2*meanReference%meanImage + regularisationConstant1)%
-                        (2*covarianceReferenceImage + regularisationConstant2))/
-                        ((arma::square(meanReference)+arma::square(meanImage)+
-                         regularisationConstant1)%
-                        (varianceReference + varianceImage +
-                         regularisationConstant2));
+  arma::mat localSSIM = ((2*meanReference%meanImage + regularisationConstant1) %
+      (2*covarianceReferenceImage + regularisationConstant2)) /
+      ((arma::square(meanReference) + arma::square(meanImage) +
+       regularisationConstant1) % (varianceReference + varianceImage +
+      regularisationConstant2));
 
-  // Calculate average of local SSIM index
+  // Calculate average of local SSIM index.
   double meanSSIM = arma::mean(arma::mean(localSSIM));
   return meanSSIM;
 }
 
-} // namespace cv
-} // namespace mlpack
+} // namespace cv.
+} // namespace mlpack.
 #endif
