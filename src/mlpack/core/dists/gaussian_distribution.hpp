16a8ba6b0df38a6890dbdd9c3c1cb3aa0908091d
Omar Shrit <omar@shrit.me>
2020-06-09 22:38:25 +0200

    BOOST::SERIALIZATION --> CEREAL core/dists
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/mlpack/core/dists/gaussian_distribution.hpp
+++ src/mlpack/core/dists/gaussian_distribution.hpp
@@ -181,11 +181,11 @@ class GaussianDistribution
   void serialize(Archive& ar, const unsigned int /* version */)
   {
     // We just need to serialize each of the members.
-    ar & BOOST_SERIALIZATION_NVP(mean);
-    ar & BOOST_SERIALIZATION_NVP(covariance);
-    ar & BOOST_SERIALIZATION_NVP(covLower);
-    ar & BOOST_SERIALIZATION_NVP(invCov);
-    ar & BOOST_SERIALIZATION_NVP(logDetCov);
+    ar & CEREAL_NVP(mean);
+    ar & CEREAL_NVP(covariance);
+    ar & CEREAL_NVP(covLower);
+    ar & CEREAL_NVP(invCov);
+    ar & CEREAL_NVP(logDetCov);
   }
 
  private:
