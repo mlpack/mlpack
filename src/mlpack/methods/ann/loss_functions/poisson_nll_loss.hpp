911e09b672f98b60b14716eecc3a89aa984bff72
Mrityunjay Tripathi <35535378+mrityunjay-tripathi@users.noreply.github.com>
2020-06-10 06:36:12 +0530

    use class name in class description
    Co-authored-by: Mikhail Lozhnikov <lozhnikovma@gmail.com>

--- src/mlpack/methods/ann/loss_functions/poisson_nll_loss.hpp
+++ src/mlpack/methods/ann/loss_functions/poisson_nll_loss.hpp
@@ -2,7 +2,7 @@
  * @file methods/ann/loss_functions/poisson_nll_loss.hpp
  * @author Mrityunjay Tripathi
  *
- * Definition of the Poisson Negative Log Likelihood class.
+ * Definition of the PoissonNLLLoss class.
  *
  * mlpack is free software; you may redistribute it and/or modify it under the
  * terms of the 3-clause BSD license.  You should have received a copy of the
