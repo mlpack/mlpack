fed89b403f32604c037f5f7e1699a0fe77f9a63e
UtR491 <64355352+UtR491@users.noreply.github.com>
2020-06-12 15:38:49 +0530

    Update ssim_index_impl.hpp

--- src/mlpack/core/cv/metrics/ssim_index_impl.hpp
+++ src/mlpack/core/cv/metrics/ssim_index_impl.hpp
@@ -45,16 +45,16 @@ double SSIMIndex::Evaluate(DataType const& image,
   arma::vec point;
   // Gaussian Window of size 11x11.
   arma::mat gaussianWindow(11, 11);
-  for ( size_t row = 0; row < 11; row+=1)
+  for ( size_t row = 0; row < 11; row += 1)
   {
-    for ( size_t column = 0; column<11; column+=1)
+    for ( size_t column = 0; column<11; column += 1)
     {
       gaussianWindow(row, column) = gaussianKernel.Evaluate(
-      				     std::sqrt(std::pow(std::abs((int)row-5),2) +
-      				     std::pow(std::abs((int)column-5),2)));
+      				     std::sqrt(std::pow(std::abs((int) row - 5), 2) +
+      				     std::pow(std::abs((int) column - 5), 2)));
     }
   }
-  gaussianWindow = gaussianWindow/arma::accu(gaussianWindow);
+  gaussianWindow = gaussianWindow / arma::accu(gaussianWindow);
   
   // Calculate local mean of reference.
   arma::mat meanReference = arma::conv2(reference, gaussianWindow, "same");
@@ -62,36 +62,36 @@ double SSIMIndex::Evaluate(DataType const& image,
   arma::mat meanImage =  arma::conv2(image, gaussianWindow, "same");
   // Calculate local variance of reference.
   arma::mat varianceReference = arma::conv2(arma::square(reference),
-      gaussianWindow, "same") - meanReference%meanReference;
+      gaussianWindow, "same") - arma::square(meanReference);
   // Calculate local variance of image.
   arma::mat varianceImage = arma::conv2(arma::square(image),
-      gaussianWindow, "same") - meanImage%meanImage;
+      gaussianWindow, "same") - arma::square(meanImage);
   // Calulate covariance of image and reference.
   arma::mat covarianceReferenceImage = arma::conv2(reference % image,
-      gaussianWindow, "same") - meanReference%meanImage;
+      gaussianWindow, "same") - meanReference % meanImage;
   // Dropping the zero-padded edges.
   int nCol = meanReference.n_cols;
   int nRow = meanReference.n_rows;
-  meanReference.shed_rows(nRow-5, nRow-1);
-  meanReference.shed_rows(0,4);
-  meanReference.shed_cols(nCol-5, nCol-1);
-  meanReference.shed_cols(0,4);
-  meanImage.shed_rows(nRow-5, nRow-1);
-  meanImage.shed_rows(0,4);
-  meanImage.shed_cols(nCol-5, nCol-1);
-  meanImage.shed_cols(0,4);
-  varianceReference.shed_rows(nRow-5, nRow-1);
-  varianceReference.shed_rows(0,4);
-  varianceReference.shed_cols(nCol-5, nCol-1);
-  varianceReference.shed_cols(0,4);
-  varianceImage.shed_rows(nRow-5, nRow-1);
-  varianceImage.shed_rows(0,4);
-  varianceImage.shed_cols(nCol-5, nCol-1);
-  varianceImage.shed_cols(0,4);
-  covarianceReferenceImage.shed_rows(nRow-5, nRow-1);
-  covarianceReferenceImage.shed_rows(0,4);
-  covarianceReferenceImage.shed_cols(nCol-5, nCol-1);
-  covarianceReferenceImage.shed_cols(0,4);
+  meanReference.shed_rows(nRow - 5, nRow - 1);
+  meanReference.shed_rows(0, 4);
+  meanReference.shed_cols(nCol - 5, nCol - 1);
+  meanReference.shed_cols(0, 4);
+  meanImage.shed_rows(nRow - 5, nRow - 1);
+  meanImage.shed_rows(0, 4);
+  meanImage.shed_cols(nCol - 5, nCol - 1);
+  meanImage.shed_cols(0, 4);
+  varianceReference.shed_rows(nRow - 5, nRow - 1);
+  varianceReference.shed_rows(0, 4);
+  varianceReference.shed_cols(nCol - 5, nCol - 1);
+  varianceReference.shed_cols(0, 4);
+  varianceImage.shed_rows(nRow - 5, nRow - 1);
+  varianceImage.shed_rows(0, 4);
+  varianceImage.shed_cols(nCol - 5, nCol - 1);
+  varianceImage.shed_cols(0, 4);
+  covarianceReferenceImage.shed_rows(nRow - 5, nRow - 1);
+  covarianceReferenceImage.shed_rows(0, 4);
+  covarianceReferenceImage.shed_cols(nCol - 5, nCol - 1);
+  covarianceReferenceImage.shed_cols(0, 4);
   
   // Calculate Dynamic Range.
   const double dynamicRange = 255;
@@ -103,7 +103,7 @@ double SSIMIndex::Evaluate(DataType const& image,
   const double regularisationConstant2 = 0.0009 * dynamicRange * dynamicRange;
 
   // Calculate local SSIM index.
-  arma::mat localSSIM = ((2 * meanReference%meanImage + regularisationConstant1) %
+  arma::mat localSSIM = ((2 * meanReference % meanImage + regularisationConstant1) %
       (2 * covarianceReferenceImage + regularisationConstant2)) /
       ((arma::square(meanReference) + arma::square(meanImage) +
        regularisationConstant1) % (varianceReference + varianceImage +
