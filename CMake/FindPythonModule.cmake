8fc3eca7f3759a8a8a1636103d9c61e80ef7d0f0
kartikdutt18 <kartikdutt@live.in>
2020-06-12 10:01:41 +0530

    Revert find python

--- CMake/FindPythonModule.cmake
+++ CMake/FindPythonModule.cmake
@@ -1,67 +1,58 @@
-# - Macro to find a python module
+# FindPythonModule.cmake: find a specific Python module.
 #
-# Usage:
-#  include (FindPythonModule)
-#  find_python_module (module [VERSION] [REQUIRED])
+# The source here is Mark Moll from a post on the CMake mailing list:
+# https://cmake.org/pipermail/cmake/2011-January/041666.html
 #
-# The following variables are defined:
-#  MODULE_FOUND - true if found
-#  MODULE_LOCATION - directory of the module, or it's library file if binary module
-#  MODULE_VERSION_STRING - module version, if available through __version__
-#
-
-macro (find_python_module module)
-
-  string (TOUPPER ${module} module_upper)
-  if (NOT ${module_upper}_FOUND)
-
-    # parse arguments
-    set (${module}_FIND_OPTIONAL TRUE)
-    if (${ARGC} EQUAL 2)
-      if (${ARGV1} MATCHES REQUIRED)
-        set (${module}_FIND_OPTIONAL FALSE)
-      else ()
-        set (${module}_FIND_VERSION ${ARGV1})
+# It has been modified to also check a minimum version if given.
+function(find_python_module module)
+  string(TOUPPER ${module} module_upper)
+  if (NOT PY_${module_upper})
+    if (ARGC GREATER 1 AND ARGV1 STREQUAL "REQUIRED")
+      set(${module}_FIND_REQUIRED TRUE)
+      if (ARGC GREATER 2)
+        set(VERSION_REQ ${ARGV2})
       endif ()
-    elseif (${ARGC} EQUAL 3)
-      if (${ARGV2} MATCHES REQUIRED)
-        set (${module}_FIND_OPTIONAL FALSE)
+    else ()
+      if (ARGC GREATER 1)
+        # Not required but we have version constraints.
+        set(VERSION_REQ ${ARGV1})
       endif ()
-      set (${module}_FIND_VERSION ${ARGV1})
     endif ()
-
-    # A module's location is usually a directory, but for binary modules it's a .so file.
-    execute_process (COMMAND "${PYTHON_EXECUTABLE}" "-c" 
-                      "import re, ${module}; print(re.compile('/__init__.py.*').sub('',${module}.__file__))"
-                      RESULT_VARIABLE _${module}_status
-                      OUTPUT_VARIABLE _${module}_location
-                      ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+    # A module's location is usually a directory, but for binary modules
+    # it's a .so file.
+    execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c" 
+      "import re, ${module}; print(re.compile('/__init__.py.*').sub('',${module}.__file__))"
+      RESULT_VARIABLE _${module}_status
+      OUTPUT_VARIABLE _${module}_location
+      ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
     if (NOT _${module}_status)
-      set (${module_upper}_LOCATION ${_${module}_location}
-            CACHE STRING "Location of Python module ${module}")
-      # retrieve version
-      execute_process (COMMAND "${PYTHON_EXECUTABLE}" "-c" "import ${module}; print(${module}.__version__)"
-                        RESULT_VARIABLE _${module}_status
-                        OUTPUT_VARIABLE _${module}_version
-                        ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+      # Now we have to check the version.
+      if (VERSION_REQ)
+        execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
+            "import ${module}; from distutils.version import StrictVersion; print(StrictVersion(${module}.__version__) >= StrictVersion('${VERSION_REQ}'));"
+            RESULT_VARIABLE _version_status
+            OUTPUT_VARIABLE _version_compare
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-      set (_${module_upper}_VERSION_MATCH TRUE)
-      if (NOT _${module}_status)
-        set (${module_upper}_VERSION_STRING ${_${module}_version})
-        if (${module}_FIND_VERSION)
-          if (${module}_FIND_VERSION VERSION_GREATER ${module_upper}_VERSION_STRING)
-            set (_${module_upper}_VERSION_MATCH FALSE)
+        if ("${_version_compare}" STREQUAL "True")
+          set(PY_${module_upper} ${_${module}_location} CACHE STRING
+            "Location of Python module ${module}")
+        else ()
+          if (${module_upper}_FIND_REQUIRED)
+            message(FATAL_ERROR "Could not find suitable version >= ${VERSION_REQ} of Python module ${module}!")
+          else ()
+            message(WARNING "Unsuitable version of Python module ${module} (${VERSION_REQ} or greater required).")
           endif ()
         endif ()
-        mark_as_advanced (${module_upper}_VERSION_STRING)
+      else ()
+        # No version requirement so we are done.
+        set(PY_${module_upper} ${_${module}_location} CACHE STRING
+            "Location of Python module ${module}")
       endif ()
     endif ()
-
-    find_package_handle_standard_args (${module} REQUIRED_VARS ${module_upper}_LOCATION
-                                        ${module}_FIND_OPTIONAL
-                                        _${module_upper}_VERSION_MATCH
-                                        VERSION_VAR ${module_upper}_VERSION_STRING
-                                     )
-    mark_as_advanced (${module_upper}_LOCATION)
-  endif (NOT ${module_upper}_FOUND)
-endmacro (find_python_module)
+  endif ()
+  find_package_handle_standard_args(PY_${module} DEFAULT_MSG PY_${module_upper})
+  if (NOT PY_${module_upper}_FOUND AND ${module_upper}_FIND_REQUIRED)
+    message(FATAL_ERROR "Could not find Python module ${module}!")
+  endif ()
+endfunction ()
