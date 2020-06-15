ec7f0093e0a0048420d8ee4850217d509ebee0f1
jeffin143 <jeffinsam@karunya.edu.in>
2020-06-15 21:03:57 +0530

    Re write test using catch

---
+++ src/mlpack/tests/main.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file tests/main.cpp
+ *
+ * Main file for Catch testing.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#include <iostream>
+#include <mlpack/core.hpp>
+
+//#define CATCH_CONFIG_MAIN  // catch.hpp will define main()
+#define CATCH_CONFIG_RUNNER  // we will define main()
+#include "catch.hpp"
+
+int main(int argc, char** argv)
+{
+  /**
+   * Uncomment these three lines if you want to test with different random seeds
+   * each run.  This is good for ensuring that a test's tolerance is sufficient
+   * across many different runs.
+   */
+  //size_t seed = std::time(NULL);
+  //srand((unsigned int) seed);
+  //arma::arma_rng::set_seed(seed);
+
+  std::cout << "run test using Catch2"  << std::endl;
+
+
+  return Catch::Session().run(argc, argv);
+}
