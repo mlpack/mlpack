7d1a6fe959ba20a50fda71be310bab3c7bbe7471
Omar Shrit <omar@shrit.me>
2020-06-15 15:58:21 +0200

    Uncommented version of mlpack_low_resource
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/mlpack/methods/neighbor_search/knn_low_resource_main.cpp
+++ src/mlpack/methods/neighbor_search/knn_low_resource_main.cpp
@@ -98,91 +98,91 @@ PROGRAM_INFO(
 static void
 mlpackMain()
 {
-  // ReportIgnoredParam({ { "input_model", true } }, "tree_type");
-  // ReportIgnoredParam({ { "input_model", true } }, "random_basis");
+  ReportIgnoredParam({ { "input_model", true } }, "tree_type");
+  ReportIgnoredParam({ { "input_model", true } }, "random_basis");
 
-  // // The user should give something to do...
-  // RequireAtLeastOnePassed(
-  //   { "k", "output_model" }, false, "no results will be saved");
+  // The user should give something to do...
+  RequireAtLeastOnePassed(
+    { "k", "output_model" }, false, "no results will be saved");
 
-  // // If the user specifies k but no output files, they should be warned.
-  // if (CMD::HasParam("k")) {
-  //   RequireAtLeastOnePassed(
-  //     { "neighbors", "distances" },
-  //     false,
-  //     "nearest neighbor search results will not be saved");
-  // }
+  // If the user specifies k but no output files, they should be warned.
+  if (CMD::HasParam("k")) {
+    RequireAtLeastOnePassed(
+      { "neighbors", "distances" },
+      false,
+      "nearest neighbor search results will not be saved");
+  }
 
-  // // If the user specifies output files but no k, they should be warned.
-  // ReportIgnoredParam({ { "k", false } }, "neighbors");
-  // ReportIgnoredParam({ { "k", false } }, "distances");
-  // ReportIgnoredParam({ { "k", false } }, "query");
+  // If the user specifies output files but no k, they should be warned.
+  ReportIgnoredParam({ { "k", false } }, "neighbors");
+  ReportIgnoredParam({ { "k", false } }, "distances");
+  ReportIgnoredParam({ { "k", false } }, "query");
   KNNModel* knn;
 
   // Load the model from file.
-//  knn = CMD::GetParam<KNNModel*>("input_model");
+ knn = CMD::GetParam<KNNModel*>("input_model");
 
-  // // Adjust search mode.
-  // // Use the dual tree mode as a default mode  
-  // knn->SearchMode() = DUAL_TREE_MODE;
+  // Adjust search mode.
+  // Use the dual tree mode as a default mode  
+  knn->SearchMode() = DUAL_TREE_MODE;
 
-  // Log::Info << "Loaded kNN model from '"
-  //           << CMD::GetPrintableParam<KNNModel*>("input_model")
-  //           << "' (trained on " << knn->Dataset().n_rows << "x"
-  //           << knn->Dataset().n_cols << " dataset)." << endl;
+  Log::Info << "Loaded kNN model from '"
+            << CMD::GetPrintableParam<KNNModel*>("input_model")
+            << "' (trained on " << knn->Dataset().n_rows << "x"
+            << knn->Dataset().n_cols << " dataset)." << endl;
 
-  // // Perform search, if desired.
-  // if (CMD::HasParam("k")) {
-  //   const size_t k = (size_t)CMD::GetParam<int>("k");
+  // Perform search, if desired.
+  if (CMD::HasParam("k")) {
+    const size_t k = (size_t)CMD::GetParam<int>("k");
 
-  //   arma::mat queryData;
-  //   if (CMD::HasParam("query")) {
-  //     Log::Info << "Using query data from "
-  //               << CMD::GetPrintableParam<arma::mat>("query") << "." << endl;
-  //     queryData = std::move(CMD::GetParam<arma::mat>("query"));
-  //     if (queryData.n_rows != knn->Dataset().n_rows) {
-  //       // Clean memory if needed before crashing.
-  //       const size_t dimensions = knn->Dataset().n_rows;
-  //       if (CMD::HasParam("reference"))
-  //         delete knn;
-  //       Log::Fatal << "Query has invalid dimensions(" << queryData.n_rows
-  //                  << "); should be " << dimensions << "!" << endl;
-  //     }
-  //   }
+    arma::mat queryData;
+    if (CMD::HasParam("query")) {
+      Log::Info << "Using query data from "
+                << CMD::GetPrintableParam<arma::mat>("query") << "." << endl;
+      queryData = std::move(CMD::GetParam<arma::mat>("query"));
+      if (queryData.n_rows != knn->Dataset().n_rows) {
+        // Clean memory if needed before crashing.
+        const size_t dimensions = knn->Dataset().n_rows;
+        if (CMD::HasParam("reference"))
+          delete knn;
+        Log::Fatal << "Query has invalid dimensions(" << queryData.n_rows
+                   << "); should be " << dimensions << "!" << endl;
+      }
+    }
 
-  //   // Sanity check on k value: must be greater than 0, must be less than or
-  //   // equal to the number of reference points.  Since it is unsigned,
-  //   // we only test the upper bound.
-  //   if (k > knn->Dataset().n_cols) {
-  //     // Clean memory if needed before crashing.
-  //     const size_t referencePoints = knn->Dataset().n_cols;
+    // Sanity check on k value: must be greater than 0, must be less than or
+    // equal to the number of reference points.  Since it is unsigned,
+    // we only test the upper bound.
+    if (k > knn->Dataset().n_cols) {
+      // Clean memory if needed before crashing.
+      const size_t referencePoints = knn->Dataset().n_cols;
 
-  //     // Sanity check on k value: must not be equal to the number of reference
-  //     // points when query data has not been provided.
-  //     if (!CMD::HasParam("query") && k == knn->Dataset().n_cols) {
-  //       // Clean memory if needed before crashing.
-  //       const size_t referencePoints = knn->Dataset().n_cols;
-  //       if (CMD::HasParam("reference"))
-  //         delete knn;
-  //       Log::Fatal << "Invalid k: " << k << "; must be less than the number of "
-  //                  << "reference points (" << referencePoints
-  //                  << ") if query data has "
-  //                  << "not been provided." << endl;
-  //     }
+      // Sanity check on k value: must not be equal to the number of reference
+      // points when query data has not been provided.
+      if (!CMD::HasParam("query") && k == knn->Dataset().n_cols) {
+        // Clean memory if needed before crashing.
+        const size_t referencePoints = knn->Dataset().n_cols;
+        if (CMD::HasParam("reference"))
+          delete knn;
+        Log::Fatal << "Invalid k: " << k << "; must be less than the number of "
+                   << "reference points (" << referencePoints
+                   << ") if query data has "
+                   << "not been provided." << endl;
+      }
 
-  //     // Now run the search.
-  //     arma::Mat<size_t> neighbors;
-  //     arma::mat distances;
+      // Now run the search.
+      arma::Mat<size_t> neighbors;
+      arma::mat distances;
 
-  //     if (CMD::HasParam("query"))
-  //       knn->Search(std::move(queryData), k, neighbors, distances);
-  //     else
-  //       knn->Search(k, neighbors, distances);
-  //     Log::Info << "Search complete." << endl;
+      if (CMD::HasParam("query"))
+        knn->Search(std::move(queryData), k, neighbors, distances);
+      else
+        knn->Search(k, neighbors, distances);
+      Log::Info << "Search complete." << endl;
 
-  //     // Save output.
-  //     CMD::GetParam<arma::Mat<size_t>>("neighbors") = std::move(neighbors);
-  //     CMD::GetParam<arma::mat>("distances") = std::move(distances);
-  //   }
-  // }
+      // Save output.
+      CMD::GetParam<arma::Mat<size_t>>("neighbors") = std::move(neighbors);
+      CMD::GetParam<arma::mat>("distances") = std::move(distances);
+    }
+  }
 }
