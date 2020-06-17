fa3a5d352d962d0d55e99405940a0fc40231386b
Omar Shrit <omar@shrit.me>
2020-06-17 20:41:41 +0200

    Adding a pointer wrapper.
    Signed-off-by: Omar Shrit <omar@shrit.me>

---
+++ src/mlpack/core/cereal/pointer_wrapper.hpp
@@ -0,0 +1,64 @@
+#ifndef CEREAL_PONTER_WRAPPER_HPP
+#define CEREAL_PONTER_WRAPPER_HPP
+
+/*
+ * The objective of this class is to create a wrapper for
+ * raw pointer by encapsulating them in a smart pointer unique_ptr
+ * This will allow to serialize raw pointer in cereal as a smart pointer
+ * because it will be difficult to change all pointer type in mlpack
+ */
+
+#include <cereal/archives/binary.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/archives/portable_binary.hpp>
+#include <cereal/archives/xml.hpp>
+#include <cereal/types/memory.hpp>
+
+namespace cereal {
+
+template<class T>
+class pointer_wrapper
+{
+public:
+
+   pointer_wrapper(T * pointer)
+    {  
+        localPointer = std::move(pointer);
+        std::cout << "address of local ptr" << localPointer << std::endl;
+        std::cout << "address of  pointer" << pointer << std::endl;
+    }
+    
+  template<class Archive>
+  void save(Archive& ar, const unsigned int /*version*/) const
+  {
+    std::unique_ptr<T> smartPointer = std::make_unique<T>(*this->localPointer);
+    ar(CEREAL_NVP(smartPointer));
+  }
+
+  template<class Archive>
+  void load(Archive& ar, const unsigned int /*version*/)
+  {
+      std::unique_ptr<T> smartPointer;
+      ar(CEREAL_NVP(smartPointer));
+      this->localPointer = smartPointer.release();
+      std::cout << "The value of localPointer is : " << *localPointer << std::endl;
+      std::cout << "The addresss of localPointer is : " << &localPointer << std::endl;
+  }
+
+private:
+  T* localPointer;
+
+
+};
+
+template<class T>
+inline pointer_wrapper<T>
+make_pointer(T* t)
+{
+  pointer_wrapper<T> a(t);
+  return a;
+}
+
+} // end namespace cereal
+
+#endif // CEREAL_POINTER_WRAPPER_HPP
