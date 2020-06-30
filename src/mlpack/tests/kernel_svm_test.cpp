136df806e345722bc23f4dbd65f1551d30ea453f
himanshupathak21061998 <hpathak336@gmail.com>
2020-07-01 00:20:47 +0530

    Testing with only one dataset

--- src/mlpack/tests/kernel_svm_test.cpp
+++ src/mlpack/tests/kernel_svm_test.cpp
@@ -35,8 +35,8 @@ BOOST_AUTO_TEST_CASE(LinearSVMFitIntercept)
   const double delta = 1.0;
 
   // Generate a two-Gaussian dataset,
-  GaussianDistribution g1(arma::vec("1.0 9.0 1.0"), arma::eye<arma::mat>(3, 3));
-  GaussianDistribution g2(arma::vec("4.0 3.0 4.0"), arma::eye<arma::mat>(3, 3));
+  GaussianDistribution g1(arma::vec("1.0 1.0 1.0"), arma::eye<arma::mat>(3, 3));
+  GaussianDistribution g2(arma::vec("9.0 9.0 9.0"), arma::eye<arma::mat>(3, 3));
 
   bool success = false;
   for (size_t trial = 0; trial < 5; ++trial)
@@ -88,53 +88,6 @@ BOOST_AUTO_TEST_CASE(LinearSVMFitIntercept)
   }
 
   BOOST_REQUIRE_EQUAL(success, true);
-}
-
-// Test training of linear svm on two Gaussians and ensure it's
-// properly separable.
-BOOST_AUTO_TEST_CASE(LinearSVMGaussianTest)
-{
-  // Generate a two-Gaussian dataset.
-  GaussianDistribution g1(arma::vec("1.0 1.0 1.0"), arma::eye<arma::mat>(3, 3));
-  GaussianDistribution g2(arma::vec("9.0 9.0 9.0"), arma::eye<arma::mat>(3, 3));
-
-  arma::mat data(3, 1000);
-  arma::Row<size_t> responses(1000);
-  for (size_t i = 0; i < 500; ++i)
-  {
-    data.col(i) = g1.Random();
-    responses[i] = 2;
-  }
-  for (size_t i = 500; i < 1000; ++i)
-  {
-    data.col(i) = g2.Random();
-    responses[i] = 1;
-  }
-
-  // Now train a svm object on it.
-  const std::string l = "linear";
-  KernelSVM<> svm(data, responses, 2, 1.0, 10.0, l, false, 10);
-
-  // Ensure that the error is close to zero.
-  const double acc = svm.ComputeAccuracy(data, responses);
-  BOOST_REQUIRE_CLOSE(acc, 1.0, 0.03); // 0.3% error tolerance.
-
-  // Create a test set.
-  for (size_t i = 0; i < 500; ++i)
-  {
-    data.col(i) = g1.Random();
-    responses[i] = 2;
-  }
-  for (size_t i = 500; i < 1000; ++i)
-  {
-    data.col(i) = g2.Random();
-    responses[i] = 1;
-  }
-
-  // Ensure that the error is close to zero.
-  const double testAcc = svm.ComputeAccuracy(data, responses);
-
-  BOOST_REQUIRE_CLOSE(testAcc, 1.0, 0.06); // 0.6% error tolerance.
 }
 
 BOOST_AUTO_TEST_SUITE_END();
