ea3bd26460ca54428b7ea7037ffde6dca8051b0e
himanshupathak21061998 <hpathak336@gmail.com>
2020-07-04 08:33:34 +0530

    Adding Gaussian kernel test

--- src/mlpack/tests/kernel_svm_test.cpp
+++ src/mlpack/tests/kernel_svm_test.cpp
@@ -19,6 +19,7 @@
 using namespace mlpack;
 using namespace mlpack::svm;
 using namespace mlpack::distribution;
+using namespace mlpack::kernel;
 
 BOOST_AUTO_TEST_SUITE(KernelSVMTest);
 
@@ -53,6 +54,72 @@ BOOST_AUTO_TEST_CASE(LinearSVMFitIntercept)
 
     // Now train a svm object on it.
     KernelSVM<> svm(data, labels, 1.0, false, 10);
+
+    // Ensure that the error is close to zero.
+    const double acc = svm.ComputeAccuracy(data, labels);
+    if (acc <= 0.98)
+      continue;
+
+    arma::mat testData(inputSize, points);
+    arma::Row<size_t> testLabels(inputSize);
+
+    // Create a test set.
+    for (size_t i = 0; i < points / 2; ++i)
+    {
+      data.col(i) = g1.Random();
+      labels[i] = 0;
+    }
+    for (size_t i = points / 2; i < points; ++i)
+    {
+      data.col(i) = g2.Random();
+      labels[i] = 1;
+    }
+
+    // Ensure that the error is close to zero.
+    const double testAcc = svm.ComputeAccuracy(data, labels);
+    if (testAcc >= 0.95)
+    {
+      success = true;
+      break;
+    }
+  }
+
+  BOOST_REQUIRE_EQUAL(success, true);
+}
+
+/**
+ * Test training of kernel svm for two classes on a complex gaussian dataset
+ * using smo optimizer.
+ */
+BOOST_AUTO_TEST_CASE(GaussianKernelSVMFitIntercept)
+{
+  const size_t points = 1000;
+  const size_t inputSize = 3;
+
+  // Generate a two-Gaussian dataset,
+  GaussianDistribution g1(arma::vec("1.0 1.0 1.0"), arma::eye<arma::mat>(3, 3));
+  GaussianDistribution g2(arma::vec("9.0 9.0 9.0"), arma::eye<arma::mat>(3, 3));
+
+  bool success = false;
+  for (size_t trial = 0; trial < 5; ++trial)
+  {
+    arma::mat data(inputSize, points);
+    arma::Row<size_t> labels(points);
+    for (size_t i = 0; i < points / 2; ++i)
+    {
+      data.col(i) = g1.Random();
+      labels[i] = 0;
+    }
+    for (size_t i = points / 2; i < points; ++i)
+    {
+      data.col(i) = g2.Random();
+      labels[i] = 1;
+    }
+
+    // Now train a svm object on it.
+    KernelSVM<arma::mat,
+              kernel::GaussianKernel> svm(data,
+                                labels, 1.0, false, 10);
 
     // Ensure that the error is close to zero.
     const double acc = svm.ComputeAccuracy(data, labels);
