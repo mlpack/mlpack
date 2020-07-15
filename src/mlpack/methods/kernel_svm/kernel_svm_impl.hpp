918a1ad3d7ff23327573a8d193db50764bc3baf6
himanshupathak21061998 <hpathak336@gmail.com>
2020-07-15 23:52:17 +0530

    Making less intialization of memory

--- src/mlpack/methods/kernel_svm/kernel_svm_impl.hpp
+++ src/mlpack/methods/kernel_svm/kernel_svm_impl.hpp
@@ -191,11 +191,6 @@ double KernelSVM<MatType, KernelType>::Train(
     {
       saved_labels(i) = label(i);
     }
-
-    else
-    {
-      saved_data.col(i) = arma::zeros(data.n_rows);
-    }
   }
 }
 
@@ -243,9 +238,12 @@ void KernelSVM<MatType, KernelType>::Classify(
       double  prediction = 0;
       for (size_t j = 0; j < saved_data.n_cols; j++)
       {
-        prediction = prediction + alpha(j) *
+        if(saved_labels(j) != 0)
+        {
+          prediction = prediction + alpha(j) *
                      saved_labels(j) * kernel.Evaluate(data.col(i),
                      saved_data.col(j));
+        }
       }
       scores(i) = prediction + intercept;
     }
