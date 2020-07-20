9466a5d42c4dd6568bd6e169db96473e9a17c0de
Omar Shrit <omar@shrit.me>
2020-07-20 18:51:09 +0200

    Add a pointer vector wrapper.
    This will serialize vector that have pointers inside.
    
    Signed-off-by: Omar Shrit <omar@shrit.me>

---
+++ src/mlpack/core/cereal/pointer_vector_wrapper.hpp
@@ -0,0 +1,74 @@
+/**
+ * @file core/cereal/pointer_vector_wrapper.hpp
+ * @author Omar Shrit
+ *
+ * Implementation of a vector wrapper to enable the serialization of
+ * the size of the vector in cereal
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_CORE_CEREAL_POINTER_VECTOR_WRAPPER_HPP
+#define MLPACK_CORE_CEREAL_POINTER_VECTOR_WRAPPER_HPP
+
+#include <cereal/archives/json.hpp>
+#include <cereal/archives/portable_binary.hpp>
+#include <cereal/archives/xml.hpp>
+#include <cereal/types/vector.hpp>
+
+#include "pointer_wrapper.hpp"
+
+namespace cereal {
+
+template<class T>
+class pointer_vector_wrapper
+{
+/*
+ * The objective of this class is to create a wrapper for
+ * std::vector that hold pointers by adding also the size of the vector.
+ * Cereal supports the serialization of the std vector, but 
+ * we need to add the size of the vector if it holds a raw pointers.
+ */
+ public:
+   pointer_vector_wrapper(std::vector<T*>& PointerVec);
+    : PointerVector(PointerVec)
+  {}
+
+  template<class Archive>
+  void save(Archive& ar, const unsigned int /*version*/) const
+  { 
+    ar & CEREAL_NVP(PointerVector.size()); 
+    for (size_t i = 0; i < PointerVector.size(); ++i) 
+    {
+      ar & CEREAL_POINTER(PointerVector.at(i));
+    }
+  }
+
+  template<class Archive>
+  void load(Archive& ar, const unsigned int /*version*/)
+  {
+    ar & CEREAL_NVP(PointerVector.size()); 
+    for (size_t i = 0; i < PointerVector.size(); ++i) 
+    {
+      ar & CEREAL_POINTER(PointerVector.at(i));
+    }
+  }
+
+private:
+  std::vector<T*>& PointerVector;
+};
+
+template<class T>
+inline pointer_vector_wrapper<T>
+make_pointer_vector(std::vector<T*>& t)
+{
+  return pointer_vector_wrapper<T>(t);
+}
+
+#define CEREAL_VECTOR_POINTER(T) cereal::make_pointer_vector(T) \
+
+} // end namespace cereal
+
+#endif // CEREAL_POINTER_VECTPR_WRAPPER_HPP
