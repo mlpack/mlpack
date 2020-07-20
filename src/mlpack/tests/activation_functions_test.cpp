0d651e29cf952f6e2a7276b598418c8f42336c3b
Ryan Curtin <ryan@ratml.org>
2020-07-20 17:55:52 -0400

    Add missing header.

--- src/mlpack/tests/activation_functions_test.cpp
+++ src/mlpack/tests/activation_functions_test.cpp
@@ -34,7 +34,7 @@
 #include <mlpack/methods/ann/activation_functions/gaussian_function.hpp>
 
 #include "catch.hpp"
-
+#include "test_catch_tools.hpp"
 
 using namespace mlpack;
 using namespace mlpack::ann;
