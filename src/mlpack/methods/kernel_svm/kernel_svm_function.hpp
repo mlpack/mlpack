21c2a6207312c052fc6fe96fecf061bcdb4a03c4
himanshupathak21061998 <hpathak336@gmail.com>
2020-07-28 05:47:09 +0530

    Changing whole implementation without style fixes

--- src/mlpack/methods/kernel_svm/kernel_svm_function.hpp
+++ src/mlpack/methods/kernel_svm/kernel_svm_function.hpp
@@ -9,20 +9,18 @@
  * 3-clause BSD license along with mlpack.  If not, see
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
-#ifndef MLPACK_METHODS_KERNEL_SVM_KERNEL_SVM_HPP
-#define MLPACK_METHODS_KERNEL_SVM_KERNEL_SVM_HPP
+#ifndef MLPACK_METHODS_KERNEL_SVM_KERNEL_FUNCTION_SVM_HPP
+#define MLPACK_METHODS_KERNEL_SVM_KERNEL_FUNCTION_SVM_HPP
 
 #include <mlpack/prereqs.hpp>
 #include <ensmallen.hpp>
 #include <string>
 
-#include "kernel_svm_function.hpp"
-
 namespace mlpack {
 namespace svm {
 
 /**
- * The KernelSVM class implements an smo algorithm for support vector machine
+ * The KernelSVMFunction class implements an smo algorithm for support vector machine
  * model, and supports training with multiple non-linear and linear kenel.
  * The class supports different observation types via the MatType template
  * parameter; for instance, support vector classification can be performed
@@ -51,7 +49,7 @@ namespace svm {
  * const size_t inputSize = 1000; // Size of input feature vector.
  *
  * // Train the model using default options.
- * KernelSVM<> svm(train_data, C, kernel_flag, max_iter, tol,
+ * KernelSVMFunction<> svm(train_data, C, kernel_flag, max_iter, tol,
  *     kernel::Gaussian());
  *
  * arma::mat test_data;
@@ -64,7 +62,7 @@ namespace svm {
  */
 template <typename MatType = arma::mat,
           typename KernelType = kernel::LinearKernel>
-class KernelSVM
+class KernelSVMFunction
 {
  public:
   /**
@@ -74,18 +72,17 @@ class KernelSVM
    * @param labels Labels associated with the feature data.
    * @param regularization standard svm regularization parameter.
    * @param fitIntercept add intercept term or not.
-   * @param numClass Number of classes for classification.
    * @param max_iter maximum number of iteration for training.
    * @param tol tolerance value.
    * @param kernel kernel type of kernel used with svm.
    */
-  KernelSVM(const MatType& data,
-            const arma::Row<size_t>& labels,
+  KernelSVMFunction(const MatType& data,
+            const arma::rowvec& labels,
             const double regularization = 1.0,
             const bool fitIntercept = false,
-            const size_t numClass = 2,
             const size_t max_iter = 10,
-            const double tol = 1e-3);
+            const double tol = 1e-3,
+            const KernelType kernel = KernelType());
 
   /**
    * Initialize the Kernel SVM without performing training.  Default  Be sure 
@@ -94,39 +91,11 @@ class KernelSVM
    *
    * @param regularization standard svm regularization parameter.
    * @param fitIntercept add intercept term or not.
-   * @param numClasses Number of classes for classification.
    * @param kernel kernel type of kernel used with svm.
    */
-  KernelSVM(const double regularization = 1.0,
+  KernelSVMFunction(const double regularization = 1.0,
             const bool fitIntercept = false,
-            const size_t numClass = 2);
-
-  /**
-   * Classify the given points, returning the predicted labels for each point.
-   * The function calculates the probabilities for every class, given a data
-   * point. It then chooses the class which has the highest probability among
-   * all.
-   *
-   * @param data Set of points to classify.
-   * @param labels Predicted labels for each point.
-   */
-  void Classify(const MatType& data,
-                arma::Row<size_t>& labels) const;
-
-  /**
-   * Classify the given points, returning class scores and predicted
-   * class label for each point.
-   * The function calculates the scores for every class, given a data
-   * point. It then chooses the class which has the highest probability among
-   * all.
-   *
-   * @param data Matrix of data points to be classified.
-   * @param labels Predicted labels for each point.
-   * @param scores Class probabilities for each point.
-   */
-  void Classify(const MatType& data,
-                arma::Row<size_t>& labels,
-                arma::mat& scores) const;
+            const KernelType kernel = KernelType());
 
   /**
    * Classify the given points, returning class scores for each point.
@@ -134,31 +103,7 @@ class KernelSVM
    * @param data Matrix of data points to be classified.
    * @param scores Class scores for each point.
    */
-  void Classify(const MatType& data,
-                arma::mat& scores) const;
-
-  /**
-   * Classify the given point. The predicted class label is returned.
-   * The function calculates the scores for every class, given the point.
-   * It then chooses the class which has the highest probability among all.
-   *
-   * @param point Point to be classified.
-   * @return Predicted class label of the point.
-   */
-  template<typename VecType>
-  size_t Classify(const VecType& point) const;
-
-  /**
-   * Computes accuracy of the learned model given the feature data and the
-   * labels associated with each data point. Predictions are made using the
-   * provided data and are compared with the actual labels.
-   *
-   * @param testData Matrix of data points using which predictions are made.
-   * @param testLabels Vector of labels associated with the data.
-   * @return Accuracy of the model.
-   */
-  double ComputeAccuracy(const MatType& testData,
-                         const arma::Row<size_t>& testLabels) const;
+  arma::rowvec Classify(const MatType& data) const;
 
   /**
    * Train the Kernel SVM with the given training data.
@@ -171,7 +116,7 @@ class KernelSVM
    * @return Objective value of the final point.
    */
   double Train(const MatType& data,
-               const arma::Row<size_t>& labels,
+               const arma::rowvec& labels,
                const size_t max_iter = 5,
                const double tol = 1e-3);
 
@@ -179,30 +124,39 @@ class KernelSVM
   bool& FitIntercept() { return fitIntercept; }
 
   /**
-   * Serialize the KernelSVM model.
+   * Serialize the KernelSVMFunction model.
    */
   template<typename Archive>
   void serialize(Archive& ar, const unsigned int /* version */)
   {
     ar & BOOST_SERIALIZATION_NVP(fitIntercept);
+    ar & BOOST_SERIALIZATION_NVP(trainingData);
+    ar & BOOST_SERIALIZATION_NVP(trainCoefficients);
+    ar & BOOST_SERIALIZATION_NVP(alpha);
+    ar & BOOST_SERIALIZATION_NVP(intercept);
   }
 
  private:
-  size_t numClass;
-  double numClassifier;
-  arma::mat classesClassifier;
-  KernelSVMFunction<MatType , KernelType> svms;
-  std::vector<KernelSVMFunction<MatType , KernelType> > network;
   //! Locally saved standard svm regularization parameter.
   double regularization;
   //! Kernel flag.
   bool fitIntercept;
+  //! Locally saved interce value of kernel.
+  double intercept;
+  //! Locally saved alpha values.
+  arma::rowvec alpha;
+  //! Locally saved KernelType values.
+  KernelType kernel;
+    //! Locally saved training labels.
+  arma::rowvec trainCoefficients;
+  //! Locally saved input data of classes for non-linear kernel.
+  arma::mat trainingData;
 };
 
 } // namespace svm
 } // namespace mlpack
 
 // Include implementation.
-#include "kernel_svm_impl.hpp"
+#include "kernel_svm_function_impl.hpp"
 
 #endif // MLPACK_METHODS_KERNEL_SVM_KERNEL_SVM_HPP
