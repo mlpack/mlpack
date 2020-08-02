b9cb3019c1b6a50579d61a12940d66d7314ea1d3
Omar Shrit <omar@shrit.me>
2020-08-02 14:20:05 +0200

    Re-include armadillo, after the extending
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/mlpack/core/arma_extend/arma_extend.hpp
+++ src/mlpack/core/arma_extend/arma_extend.hpp
@@ -1,4 +1,19 @@
+/***
+ * @file core/arma_extend/arma_extend.hpp
+ * @author Ryan Curtin
+ *
+ * Include Armadillo extensions which currently are not part of the main
+ * Armadillo codebase.
+ *
+ */
+#ifndef MLPACK_CORE_ARMA_EXTEND_ARMA_EXTEND_HPP
+#define MLPACK_CORE_ARMA_EXTEND_ARMA_EXTEND_HPP
+
 // Add vec_type, col_type, and row_type to Mat and SpMat.
 // TODO: refactor and remove these!
 #define ARMA_EXTRA_MAT_PROTO mlpack/core/arma_extend/Mat_extra_bones.hpp
 #define ARMA_EXTRA_SPMAT_PROTO mlpack/core/arma_extend/SpMat_extra_bones.hpp
+
+#include <armadillo>
+
+#endif
