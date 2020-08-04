8f143b4a61e86544e416bec2f91f0d84ae0b956f
Omar Shrit <omar@shrit.me>
2020-08-04 14:49:11 +0200

    Add the implementation proposed by ryan
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/mlpack/core/arma_extend/serialize_armadillo.hpp
+++ src/mlpack/core/arma_extend/serialize_armadillo.hpp
@@ -20,61 +20,58 @@
 /**
  * Add an external serialization function for SpMat.
  */
+namespace cereal {
+
 template<typename Archive, typename eT>
-void serialize(Archive& ar, arma::SpMat<eT>& SpMat)
+void serialize(Archive& ar, arma::SpMat<eT>& mat)
 {
   // This is accurate from Armadillo 3.6.0 onwards.
   // We can't use CEREAL_NVP() because of the access::rw() call.
   // We need to check on this access::rw
-  ar(cereal::make_nvp("n_rows", arma::access::rw(SpMat.n_rows)));
-  ar(cereal::make_nvp("n_cols", arma::access::rw(SpMat.n_cols)));
-  ar(cereal::make_nvp("n_elem", arma::access::rw(SpMat.n_elem)));
-  ar(cereal::make_nvp("n_nonzero", arma::access::rw(SpMat.n_nonzero)));
-  ar(cereal::make_nvp("vec_state", arma::access::rw(SpMat.vec_state)));
+  arma::uword n_rows = mat.n_rows;
+  arma::uword n_cols = mat.n_cols;
+  arma::uword n_nonzero = mat.n_nonzero;
+  arma::uword vec_state = mat.vec_state;
+
+  ar(CEREAL_NVP(n_rows));
+  ar(CEREAL_NVP(n_cols));
+  ar(CEREAL_NVP(n_nonzero));
+  ar(CEREAL_NVP(vec_state));
 
   // Now we have to serialize the values, row indices, and column pointers.
   // If we are loading, we need to initialize space for these things.
   if (Archive::is_loading::value)
   {
-    const arma::uword new_n_nonzero = SpMat.n_nonzero; // Save this; we're about to nuke it.
-    SpMat.init(SpMat.n_rows,SpMat.n_cols); // Allocate column pointers.
-    SpMat.mem_resize(new_n_nonzero); // Allocate storage.
-    // These calls will set the sentinel values at the end of the storamat
+    mat.zeros(n_rows, n_cols);
+    arma::access::rw(mat.vec_state) = vec_state;
+    mat.mem_resize(n_nonzero); // Allocate storage.
+    // These calls will set the sentinel values at the end of the stored
     // column pointers, if necessary, so we don't need to worry about them.
   }
 
-  ar(cereal::make_array(arma::access::rwp(SpMat.values), SpMat.n_nonzero));
-  ar(cereal::make_array(arma::access::rwp(SpMat.row_indices), SpMat.n_nonzero));
-  ar(cereal::make_array(arma::access::rwp(SpMat.col_ptrs), SpMat.n_cols + 1));
+  ar(cereal::make_array(arma::access::rwp(mat.values), mat.n_nonzero));
+  ar(cereal::make_array(arma::access::rwp(mat.row_indices), mat.n_nonzero));
+  ar(cereal::make_array(arma::access::rwp(mat.col_ptrs), mat.n_cols + 1));
 }
 
 // Add an external serialization function for Mat.
 template<typename Archive, typename eT>
 void serialize(Archive& ar, arma::Mat<eT>& mat)
 {
-  const arma::uword old_n_elem = mat.n_elem;
-
   // This is accurate from Armadillo 3.6.0 onwards.
   // We can't use CEREAL_NVP() because of the arma::access::rw() call.
-  ar(cereal::make_nvp("n_rows", arma::access::rw(mat.n_rows)));
-  ar(cereal::make_nvp("n_cols", arma::access::rw(mat.n_cols)));
-  ar(cereal::make_nvp("n_elem", arma::access::rw(mat.n_elem)));
-  ar(cereal::make_nvp("vec_state", arma::access::rw(mat.vec_state)));
+  arma::uword n_rows = mat.n_rows;
+  arma::uword n_cols = mat.n_cols;
+  arma::uword vec_state = mat.vec_state;
 
-  // mem_state will always be 0 on load, so we don't need to save it.
+  ar(CEREAL_NVP(n_rows));
+  ar(CEREAL_NVP(n_cols));
+  ar(CEREAL_NVP(vec_state));
+
   if (Archive::is_loading::value)
   {
-    // Don't free if local memory is being used.
-    if (mat.mem_state == 0 && mat.mem != NULL && 
-            old_n_elem > arma::arma_config::mat_prealloc)
-    {
-      arma::memory::release(arma::access::rw(mat.mem));
-    }
-
-    arma::access::rw(mat.mem_state) = 0;
-
-    // We also need to allocate the memory we're using.
-    mat.init_cold();
+    mat.set_size(n_rows, n_cols);
+    arma::access::rw(mat.vec_state) = vec_state;
   }
 
   ar & cereal::make_array(arma::access::rwp(mat.mem), mat.n_elem);
@@ -84,33 +81,20 @@ void serialize(Archive& ar, arma::Mat<eT>& mat)
 template<typename Archive, typename eT>
 void serialize(Archive& ar, arma::Cube<eT>& cube)
 {
-  const arma::uword old_n_elem = cube.n_elem;
-
   // This is accurate from Armadillo 3.6.0 onwards.
-  ar(cereal::make_nvp("n_rows",arma::access::rw(cube.n_rows)));
-  ar(cereal::make_nvp("n_cols",arma::access::rw(cube.n_cols)));
-  ar(cereal::make_nvp("n_elem_slice",arma::access::rw(cube.n_elem_slice)));
-  ar(cereal::make_nvp("n_slices",arma::access::rw(cube.n_slices)));
-  ar(cereal::make_nvp("n_elem",arma::access::rw(cube.n_elem)));
+  arma::uword n_rows = cube.n_rows;
+  arma::uword n_cols = cube.n_cols;
+  arma::uword n_slices = cube.n_slices;
 
-  // mem_state will always be 0 on load, so we don't need to save it.
+  ar(CEREAL_NVP(n_rows));
+  ar(CEREAL_NVP(n_cols));
+  ar(CEREAL_NVP(n_slices));
+
   if (Archive::is_loading::value)
   {
-    // Clean any mat pointers.
-    cube.delete_mat();
-
-    // Don't free if local memory is being used.
-    if (cube.mem_state == 0 && cube.mem != NULL && 
-            old_n_elem > arma::arma_config::mat_prealloc)
-    {
-      arma::memory::release(arma::access::rw(cube.mem));
-    }
-
-    arma::access::rw(cube.mem_state) = 0;
-
-    // We also need to allocate the memory we're using.
-    cube.init_cold();
+    cube.set_size(n_rows, n_cols, n_slices);
   }
 
   ar & cereal::make_array(arma::access::rwp(cube.mem), cube.n_elem);
 }
+} // end namespace cereal
