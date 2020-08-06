dd0882d1fe3c3077526cd8ea9242168adcddb430
jeffin sam <jeffinsam@karunya.edu.in>
2020-08-07 02:52:02 +0530

    Apply suggestions from code review
    Co-authored-by: Ryan Curtin <ryan@ratml.org>

--- src/mlpack/tests/binarize_test.cpp
+++ src/mlpack/tests/binarize_test.cpp
@@ -20,7 +20,7 @@ using namespace mlpack;
 using namespace arma;
 using namespace mlpack::data;
 
-TEST_CASE("BinerizeOneDimension", "[BinarizeTest]")
+TEST_CASE("BinarizeOneDimension", "[BinarizeTest]")
 {
   mat input;
   input << 1 << 2 << 3 << endr
@@ -32,15 +32,15 @@ TEST_CASE("BinerizeOneDimension", "[BinarizeTest]")
   const size_t dimension = 1;
   Binarize<double>(input, output, threshold, dimension);
 
-  REQUIRE(output(0, 0)== Approx(1.0).epsilon(1e-7)); // 1
-  REQUIRE(output(0, 1)== Approx(2.0).epsilon(1e-7)); // 2
-  REQUIRE(output(0, 2)== Approx(3.0).epsilon(1e-7)); // 3
+  REQUIRE(output(0, 0) == Approx(1.0).epsilon(1e-7)); // 1
+  REQUIRE(output(0, 1) == Approx(2.0).epsilon(1e-7)); // 2
+  REQUIRE(output(0, 2) == Approx(3.0).epsilon(1e-7)); // 3
   REQUIRE(output(1, 0) == Approx(0.0).margin(1e-5)); // 4 target
   REQUIRE(output(1, 1) == Approx(0.0).margin(1e-5)); // 5 target
-  REQUIRE(output(1, 2)== Approx(1.0).epsilon(1e-7)); // 6 target
-  REQUIRE(output(2, 0)== Approx(7.0).epsilon(1e-7)); // 7
-  REQUIRE(output(2, 1)== Approx(8.0).epsilon(1e-7)); // 8
-  REQUIRE(output(2, 2)== Approx(9.0).epsilon(1e-7)); // 9
+  REQUIRE(output(1, 2) == Approx(1.0).epsilon(1e-7)); // 6 target
+  REQUIRE(output(2, 0) == Approx(7.0).epsilon(1e-7)); // 7
+  REQUIRE(output(2, 1) == Approx(8.0).epsilon(1e-7)); // 8
+  REQUIRE(output(2, 2) == Approx(9.0).epsilon(1e-7)); // 9
 }
 
 TEST_CASE("BinerizeAll", "[BinarizeTest]")
@@ -60,8 +60,8 @@ TEST_CASE("BinerizeAll", "[BinarizeTest]")
   REQUIRE(output(0, 2) == Approx(0.0).margin(1e-5)); // 3
   REQUIRE(output(1, 0) == Approx(0.0).margin(1e-5)); // 4
   REQUIRE(output(1, 1) == Approx(0.0).margin(1e-5)); // 5
-  REQUIRE(output(1, 2)== Approx(1.0).epsilon(1e-7)); // 6
-  REQUIRE(output(2, 0)== Approx(1.0).epsilon(1e-7)); // 7
-  REQUIRE(output(2, 1)== Approx(1.0).epsilon(1e-7)); // 8
-  REQUIRE(output(2, 2)== Approx(1.0).epsilon(1e-7)); // 9
+  REQUIRE(output(1, 2) == Approx(1.0).epsilon(1e-7)); // 6
+  REQUIRE(output(2, 0) == Approx(1.0).epsilon(1e-7)); // 7
+  REQUIRE(output(2, 1) == Approx(1.0).epsilon(1e-7)); // 8
+  REQUIRE(output(2, 2) == Approx(1.0).epsilon(1e-7)); // 9
 }
