547923b0dcf6e4e46f52622350f8e74fe30ece24
Yashwant <yashwantsingh.sngh@gmail.com>
2020-08-08 09:59:25 +0530

    More debugging.

--- CMake/R/ConfigureRCPP.cmake
+++ CMake/R/ConfigureRCPP.cmake
@@ -12,10 +12,17 @@ strip_type("${PROGRAM_MAIN_FILE}")
 
 # Extract the requried part from *main.cpp.
 # Example: mlpack/methods/adaboost/adaboost_main.cpp
-string(REGEX MATCHALL "mlpack\\/src\\/[A-Za-z\\/._]*" INCLUDE_MODEL_SAFE "${PROGRAM_MAIN_FILE}")
+set(INCLUDE_FILE "")
+string(REGEX MATCHALL "mlpack\\/src\\/[A-Za-z\\/._]*" INCLUDE_MODEL_SAFE 
+    "${PROGRAM_MAIN_FILE}")
 string(REGEX REPLACE "mlpack\\/src\\/" "" INCLUDE_FILE "${INCLUDE_MODEL_SAFE}")
-set(INCLUDE_FILE "${INCLUDE_FILE}")
-message(${INCLUDE_FILE})
+
+if (INCLUDE_FILE)
+  list(REMOVE_DUPLICATES INCLUDE_FILE)
+endif ()
+
+set(INCLUDE_FILE ${INCLUDE_FILE})
+message("${INCLUDE_FILE}")
 file(READ "${MODEL_FILE}" MODEL_FILE_TYPE)
 if (NOT (MODEL_FILE_TYPE MATCHES "\"${MODEL_SAFE_TYPES}\""))
   file(APPEND "${MODEL_FILE}" "\"${MODEL_SAFE_TYPES}\"\n")
