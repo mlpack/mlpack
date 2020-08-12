9de6bf3062b8f2b9c80f29c4aac99d6ce33acabf
Ryan Curtin <ryan@ratml.org>
2020-08-12 16:14:21 -0400

    I thought I already removed this?

--- src/mlpack/methods/neighbor_search/neighbor_search_impl.hpp
+++ src/mlpack/methods/neighbor_search/neighbor_search_impl.hpp
@@ -816,9 +816,6 @@ DualTreeTraversalType, SingleTreeTraversalType>::Search(
       // Create the traverser.
       tree::GreedySingleTreeTraverser<Tree, RuleType> traverser(rules);
 
-      // Set the value of minBaseCases.
-      traverser.MinBaseCases() = k;
-
       // Now have it traverse for each point.
       for (size_t i = 0; i < referenceSet->n_cols; ++i)
         traverser.Traverse(i, *referenceTree);
