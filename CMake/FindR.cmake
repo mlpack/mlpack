9261418500c82c549303aeb6c523185d5b570eaa
Yashwant <yashwantsingh.sngh@gmail.com>
2020-08-19 16:40:36 +0530

    brew gfortran.

--- CMake/FindR.cmake
+++ CMake/FindR.cmake
@@ -1,13 +1,21 @@
-# FindR.cmake
-if (R_FOUND)
-  return()
-endif()
+# FindR.cmkae
+# Make sure find package macros are included
+set(TEMP_CMAKE_FIND_APPBUNDLE ${CMAKE_FIND_APPBUNDLE})
+set(CMAKE_FIND_APPBUNDLE "NEVER")
 
-# Find the R program.
-find_program (R_EXECUTABLE R DOC "R executable.")
+# Find R.
+find_program(R_EXECUTABLE R DOC "R executable.")
 
-# Get the R version.
-if (R_EXECUTABLE)
+if(R_EXECUTABLE)
+  # Get the location of R.
+  execute_process(
+      WORKING_DIRECTORY .
+      COMMAND ${R_EXECUTABLE} RHOME
+      OUTPUT_VARIABLE R_BASE_DIR
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
+  # Get the R version.
   execute_process(
       COMMAND ${R_EXECUTABLE} --version
       OUTPUT_VARIABLE R_VERSION_STRING
@@ -17,17 +25,23 @@ if (R_EXECUTABLE)
     string(REGEX REPLACE ".*([0-9]+\\.[0-9]+\\.[0-9]+).*" "\\1"
         R_VERSION_STRING ${R_VERSION_STRING})
   endif ()
-endif ()
+
+  set(R_HOME ${R_BASE_DIR} CACHE PATH "R home directory obtained from R RHOME")
+  mark_as_advanced(R_HOME)
+endif()
 
 # Find the Rscript program.
-find_program (RSCRIPT_EXECUTABLE Rscript DOC "Rscript executable.")
+find_program(RSCRIPT_EXECUTABLE Rscript DOC "Rscript executable.")
 
-include(FindPackageHandleStandardArgs)
+set(CMAKE_FIND_APPBUNDLE ${TEMP_CMAKE_FIND_APPBUNDLE})
+
+mark_as_advanced(RSCRIPT_EXECUTABLE R_EXECUTABLE)
+set( _REQUIRED_R_VARIABLES R_EXECUTABLE )
+
+include( FindPackageHandleStandardArgs )
 find_package_handle_standard_args(
     R
-    REQUIRED_VARS R_EXECUTABLE RSCRIPT_EXECUTABLE
+    REQUIRED_VARS ${_REQUIRED_R_VARIABLES}
     VERSION_VAR R_VERSION_STRING
     FAIL_MESSAGE "R not found"
 )
-
-mark_as_advanced(RSCRIPT_EXECUTABLE R_EXECUTABLE)
