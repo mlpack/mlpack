1155bdf8f656c8e70cc7a979ffc7454b0939e41c
Omar Shrit <omar@shrit.me>
2020-08-23 20:30:02 +0200

    Fix style
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/mlpack/core/cereal/pointer_wrapper.hpp
+++ src/mlpack/core/cereal/pointer_wrapper.hpp
@@ -23,9 +23,8 @@
 namespace std {
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args)
-
 {
-      return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 } // namepace std
 #endif
