d1cfaa30b458c757be794e40180b6a9483e17783
Omar Shrit <omar@shrit.me>
2020-09-17 17:07:38 +0200

    Remove old comments
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/mlpack/core/arma_extend/serialize_armadillo.hpp
+++ src/mlpack/core/arma_extend/serialize_armadillo.hpp
@@ -29,8 +29,6 @@ template<typename Archive, typename eT>
 void serialize(Archive& ar, arma::SpMat<eT>& mat)
 {
   // This is accurate from Armadillo 3.6.0 onwards.
-  // We can't use CEREAL_NVP() because of the access::rw() call.
-  // We need to check on this access::rw
   arma::uword n_rows = mat.n_rows;
   arma::uword n_cols = mat.n_cols;
   arma::uword n_nonzero = mat.n_nonzero;
