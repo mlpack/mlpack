0481b92ce7ba838a5c46b77915e5e9c04c4565b2
Omar Shrit <omar@shrit.me>
2020-09-17 17:40:27 +0200

    Reverse the order to deduce correctly the version
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- CMake/Findcereal.cmake
+++ CMake/Findcereal.cmake
@@ -25,17 +25,18 @@ if(CEREAL_INCLUDE_DIRS)
     string(REGEX REPLACE ".*#define CEREAL_VERSION_PATCH ([0-9]+).*" "\\1"
         CEREAL_VERSION_PATCH "${_CEREAL_HEADER_CONTENTS}")
 
-  elseif(EXISTS "${CEREAL_INCLUDE_DIRS}/cereal/types/valarray.hpp")
-    set(CEREAL_VERSION_MAJOR 1)
-    set(CEREAL_VERSION_MINOR 1)
-    set(CEREAL_VERSION_PATCH 2)
-
   elseif(EXISTS "${CEREAL_INCLUDE_DIRS}/cereal/details/polymorphic_impl_fwd.hpp")
 
     set(CEREAL_VERSION_MAJOR 1)
     set(CEREAL_VERSION_MINOR 2)
     set(CEREAL_VERSION_PATCH 0)
+  elseif(EXISTS "${CEREAL_INCLUDE_DIRS}/cereal/types/valarray.hpp")
+
+    set(CEREAL_VERSION_MAJOR 1)
+    set(CEREAL_VERSION_MINOR 1)
+    set(CEREAL_VERSION_PATCH 2)
   else()
+
     set(CEREAL_VERSION_MAJOR 1)
     set(CEREAL_VERSION_MINOR 1)
     set(CEREAL_VERSION_PATCH 1) 
