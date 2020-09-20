62f19a90731890af82ce1156c00f40117e0a3660
Omar Shrit <omar@shrit.me>
2020-09-20 19:31:45 +0200

    Adjust cereal path in windows and adjust name for CEREAL_INCLUDE_DIR
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- CMake/Findcereal.cmake
+++ CMake/Findcereal.cmake
@@ -1,10 +1,10 @@
 #Findcereal.cmake
-find_path(CEREAL_INCLUDE_DIRS
+find_path(CEREAL_INCLUDE_DIR
   NAMES cereal
   PATHS "$ENV{ProgramFiles}/cereal/include"
   )
 
-if(CEREAL_INCLUDE_DIRS)
+if(CEREAL_INCLUDE_DIR)
   # ------------------------------------------------------------------------
   #  Extract version information from <CEREAL>
   # ------------------------------------------------------------------------
@@ -13,10 +13,10 @@ if(CEREAL_INCLUDE_DIRS)
   set(CEREAL_VERSION_MINOR 0)
   set(CEREAL_VERSION_PATCH 0)
 
-  if(EXISTS "${CEREAL_INCLUDE_DIRS}/cereal/version.hpp")
+  if(EXISTS "${CEREAL_INCLUDE_DIR}/cereal/version.hpp")
 
     # Read and parse cereal version header file for version number
-    file(READ "${CEREAL_INCLUDE_DIRS}/cereal/version.hpp"
+    file(READ "${CEREAL_INCLUDE_DIR}/cereal/version.hpp"
         _CEREAL_HEADER_CONTENTS)
     string(REGEX REPLACE ".*#define CEREAL_VERSION_MAJOR ([0-9]+).*" "\\1"
         CEREAL_VERSION_MAJOR "${_CEREAL_HEADER_CONTENTS}")
@@ -25,12 +25,12 @@ if(CEREAL_INCLUDE_DIRS)
     string(REGEX REPLACE ".*#define CEREAL_VERSION_PATCH ([0-9]+).*" "\\1"
         CEREAL_VERSION_PATCH "${_CEREAL_HEADER_CONTENTS}")
 
-  elseif(EXISTS "${CEREAL_INCLUDE_DIRS}/cereal/details/polymorphic_impl_fwd.hpp")
+  elseif(EXISTS "${CEREAL_INCLUDE_DIR}/cereal/details/polymorphic_impl_fwd.hpp")
 
     set(CEREAL_VERSION_MAJOR 1)
     set(CEREAL_VERSION_MINOR 2)
     set(CEREAL_VERSION_PATCH 0)
-  elseif(EXISTS "${CEREAL_INCLUDE_DIRS}/cereal/types/valarray.hpp")
+  elseif(EXISTS "${CEREAL_INCLUDE_DIR}/cereal/types/valarray.hpp")
 
     set(CEREAL_VERSION_MAJOR 1)
     set(CEREAL_VERSION_MINOR 1)
@@ -46,8 +46,8 @@ endif ()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(cereal
-  REQUIRED_VARS CEREAL_INCLUDE_DIRS
+  REQUIRED_VARS CEREAL_INCLUDE_DIR
   VERSION_VAR CEREAL_VERSION_STRING
   )
 
-mark_as_advanced(CEREAL_INCLUDE_DIRS)
+mark_as_advanced(CEREAL_INCLUDE_DIR)
