a4321a5d558b5e1a3cef4e89022de7d3036dd480
Marcus Edel <marcus.edel@fu-berlin.de>
2020-10-02 13:59:53 +0200

    Trigger build to test the Actions cancel feature - Test.

--- src/mlpack/tests/ann_layer_test.cpp
+++ src/mlpack/tests/ann_layer_test.cpp
@@ -9,7 +9,7 @@
  * terms of the 3-clause BSD license.  You should have received a copy of the
  * 3-clause BSD license along with mlpack.  If not, see
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
- */ 
+ */
 #include <mlpack/core.hpp>
 
 #include <mlpack/methods/ann/layer/layer.hpp>
