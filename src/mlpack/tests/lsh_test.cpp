03e6c086c7c13c21df445e02f6a5f7fee22b7aa7
RishabhGarg108 <rishabhgarg108@gmail.com>
2020-11-11 18:29:27 +0530

    Fixed styles in lsh_test.cpp

--- src/mlpack/tests/lsh_test.cpp
+++ src/mlpack/tests/lsh_test.cpp
@@ -589,12 +589,12 @@ BOOST_AUTO_TEST_CASE(MultiprobeDeterministicTest)
 
   // Construct q1 so it is hashed directly under C2.
   arma::mat q1;
-  q1 = arma::mat( { 3.9, 2.99 } ).t();
+  q1 = arma::mat({ 3.9, 2.99 }).t();
   q1 -= offsets;
 
   // Construct q2 so it is hashed near the center of C2.
   arma::mat q2;
-  q2 = arma::mat( { 3.6, 3.6 } );
+  q2 = arma::mat({ 3.6, 3.6 }).t();
   q2 -= offsets;
 
   arma::Mat<size_t> neighbors;
