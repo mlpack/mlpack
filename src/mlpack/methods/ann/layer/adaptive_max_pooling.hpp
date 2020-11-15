a3d7499b5dd2dd5f828aa36e871bbd89c0c4d25e
Gopi M Tatiraju <deathcoderx@gmail.com>
2020-11-15 21:44:33 +0530

    Update adaptive_max_pooling.hpp

--- src/mlpack/methods/ann/layer/adaptive_max_pooling.hpp
+++ src/mlpack/methods/ann/layer/adaptive_max_pooling.hpp
@@ -51,7 +51,7 @@ class AdaptiveMaxPooling
    * @param outputShape A two-value tuple indicating width and height of the output.
    */
   AdaptiveMaxPooling(const std::tuple<size_t, size_t>& outputShape);
-    
+
   /**
    * Ordinary feed forward pass of a neural network, evaluating the function
    * f(x) by propagating the activity forward through f.
