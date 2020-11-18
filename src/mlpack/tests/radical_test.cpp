3fbfc838f138f83a7ae0c380b5143ad3606fe214
antonio29398 <56562351+antonio29398@users.noreply.github.com>
2020-11-18 14:18:17 +0100

    If clause applied

--- src/mlpack/tests/radical_test.cpp
+++ src/mlpack/tests/radical_test.cpp
@@ -21,8 +21,12 @@ using namespace arma;
 TEST_CASE("Radical_Test_Radical3D", "[RadicalTest]")
 {
   mat matX;
-  data::Load("data_3d_mixed.txt", matX);
-
+  
+  if (!data::Load("data_3d_mixed.txt", matX)){
+    
+  FAIL("Unable to load data_3d_mixed.txt!");
+  }
+  
   Radical rad(0.175, 5, 100, matX.n_rows - 1);
 
   mat matY;
@@ -39,7 +43,11 @@ TEST_CASE("Radical_Test_Radical3D", "[RadicalTest]")
   }
 
   mat matS;
-  data::Load("data_3d_ind.txt", matS);
+  
+  if (!data::Load("data_3d_ind.txt", matS)){
+  FAIL("Unable to load data_3d_ind.txt!");
+  }
+  
   rad.DoRadical(matS, matY, matW);
 
   matYT = trans(matY);
