d27dd9d10c4d4a589a356d5af42dd5b932c78bff
Marcus Edel <marcus.edel@fu-berlin.de>
2020-12-01 13:50:06 +0100

    Include some more tests.

--- src/mlpack/bindings/python/tests/test_python_binding.py
+++ src/mlpack/bindings/python/tests/test_python_binding.py
@@ -21,130 +21,105 @@ class TestPythonBinding(unittest.TestCase):
   This class tests the basic functionality of the Python bindings.
   """
 
-  def testRunBindingCorrectly(self):
-    """
-    Test that when we run the binding correctly (with correct input parameters),
-    we get the expected output.
-    """
-    output = test_python_binding(string_in='hello',
-                                 int_in=12,
-                                 double_in=4.0,
-                                 mat_req_in=[[1.0]],
-                                 col_req_in=[1.0],
-                                 flag1=True)
+  # def testRunBindingCorrectly(self):
+  #   """
+  #   Test that when we run the binding correctly (with correct input parameters),
+  #   we get the expected output.
+  #   """
+  #   output = test_python_binding(string_in='hello',
+  #                                int_in=12,
+  #                                double_in=4.0,
+  #                                mat_req_in=[[1.0]],
+  #                                col_req_in=[1.0],
+  #                                flag1=True)
 
-    self.assertEqual(output['string_out'], 'hello2')
-    self.assertEqual(output['int_out'], 13)
-    self.assertEqual(output['double_out'], 5.0)
+  #   self.assertEqual(output['string_out'], 'hello2')
+  #   self.assertEqual(output['int_out'], 13)
+  #   self.assertEqual(output['double_out'], 5.0)
 
-  def testRunBindingNoFlag(self):
-    """
-    If we forget the mandatory flag, we should get wrong results.
-    """
-    output = test_python_binding(string_in='hello',
-                                 int_in=12,
-                                 double_in=4.0,
-                                 mat_req_in=[[1.0]],
-                                 col_req_in=[1.0])
+  # def testRunBindingNoFlag(self):
+  #   """
+  #   If we forget the mandatory flag, we should get wrong results.
+  #   """
+  #   output = test_python_binding(string_in='hello',
+  #                                int_in=12,
+  #                                double_in=4.0,
+  #                                mat_req_in=[[1.0]],
+  #                                col_req_in=[1.0])
 
-    self.assertNotEqual(output['string_out'], 'hello2')
-    self.assertNotEqual(output['int_out'], 13)
-    self.assertNotEqual(output['double_out'], 5.0)
+  #   self.assertNotEqual(output['string_out'], 'hello2')
+  #   self.assertNotEqual(output['int_out'], 13)
+  #   self.assertNotEqual(output['double_out'], 5.0)
 
-  def testRunBindingWrongString(self):
-    """
-    If we give the wrong string, we should get wrong results.
-    """
-    output = test_python_binding(string_in='goodbye',
-                                 int_in=12,
-                                 double_in=4.0,
-                                 mat_req_in=[[1.0]],
-                                 col_req_in=[1.0],
-                                 flag1=True)
+  # def testRunBindingWrongString(self):
+  #   """
+  #   If we give the wrong string, we should get wrong results.
+  #   """
+  #   output = test_python_binding(string_in='goodbye',
+  #                                int_in=12,
+  #                                double_in=4.0,
+  #                                mat_req_in=[[1.0]],
+  #                                col_req_in=[1.0],
+  #                                flag1=True)
 
-    self.assertNotEqual(output['string_out'], 'hello2')
+  #   self.assertNotEqual(output['string_out'], 'hello2')
 
-  def testRunBindingWrongInt(self):
-    """
-    If we give the wrong int, we should get wrong results.
-    """
-    output = test_python_binding(string_in='hello',
-                                 int_in=15,
-                                 double_in=4.0,
-                                 mat_req_in=[[1.0]],
-                                 col_req_in=[1.0],
-                                 flag1=True)
+  # def testRunBindingWrongInt(self):
+  #   """
+  #   If we give the wrong int, we should get wrong results.
+  #   """
+  #   output = test_python_binding(string_in='hello',
+  #                                int_in=15,
+  #                                double_in=4.0,
+  #                                mat_req_in=[[1.0]],
+  #                                col_req_in=[1.0],
+  #                                flag1=True)
 
-    self.assertNotEqual(output['int_out'], 13)
+  #   self.assertNotEqual(output['int_out'], 13)
 
-  def testRunBindingWrongDouble(self):
-    """
-    If we give the wrong double, we should get wrong results.
-    """
-    output = test_python_binding(string_in='hello',
-                                 int_in=12,
-                                 double_in=2.0,
-                                 mat_req_in=[[1.0]],
-                                 col_req_in=[1.0],
-                                 flag1=True)
+  # def testRunBindingWrongDouble(self):
+  #   """
+  #   If we give the wrong double, we should get wrong results.
+  #   """
+  #   output = test_python_binding(string_in='hello',
+  #                                int_in=12,
+  #                                double_in=2.0,
+  #                                mat_req_in=[[1.0]],
+  #                                col_req_in=[1.0],
+  #                                flag1=True)
 
-    self.assertNotEqual(output['double_out'], 5.0)
+  #   self.assertNotEqual(output['double_out'], 5.0)
 
-  def testRunBadFlag(self):
-    """
-    If we give the second flag, this should fail.
-    """
-    output = test_python_binding(string_in='hello',
-                                 int_in=12,
-                                 double_in=4.0,
-                                 mat_req_in=[[1.0]],
-                                 col_req_in=[1.0],
-                                 flag1=True,
-                                 flag2=True)
+  # def testRunBadFlag(self):
+  #   """
+  #   If we give the second flag, this should fail.
+  #   """
+  #   output = test_python_binding(string_in='hello',
+  #                                int_in=12,
+  #                                double_in=4.0,
+  #                                mat_req_in=[[1.0]],
+  #                                col_req_in=[1.0],
+  #                                flag1=True,
+  #                                flag2=True)
 
-    self.assertNotEqual(output['string_out'], 'hello2')
-    self.assertNotEqual(output['int_out'], 13)
-    self.assertNotEqual(output['double_out'], 5.0)
+  #   self.assertNotEqual(output['string_out'], 'hello2')
+  #   self.assertNotEqual(output['int_out'], 13)
+  #   self.assertNotEqual(output['double_out'], 5.0)
 
-  def testNumpyMatrix(self):
-    """
-    The matrix we pass in, we should get back with the third dimension doubled
-    and the fifth forgotten.
-    """
-    x = np.random.rand(100, 5);
-    z = copy.deepcopy(x)
-
-    output = test_python_binding(string_in='hello',
-                                 int_in=12,
-                                 double_in=4.0,
-                                 mat_req_in=[[1.0]],
-                                 col_req_in=[1.0],
-                                 matrix_in=z)
-
-    self.assertEqual(output['matrix_out'].shape[0], 100)
-    self.assertEqual(output['matrix_out'].shape[1], 4)
-    self.assertEqual(output['matrix_out'].dtype, np.double)
-    for i in [0, 1, 3]:
-      for j in range(100):
-        self.assertEqual(x[j, i], output['matrix_out'][j, i])
-
-    for j in range(100):
-      self.assertEqual(2 * x[j, 2], output['matrix_out'][j, 2])
-
-  # def testNumpyMatrixForceCopy(self):
+  # def testNumpyMatrix(self):
   #   """
   #   The matrix we pass in, we should get back with the third dimension doubled
   #   and the fifth forgotten.
   #   """
   #   x = np.random.rand(100, 5);
+  #   z = copy.deepcopy(x)
 
   #   output = test_python_binding(string_in='hello',
   #                                int_in=12,
   #                                double_in=4.0,
   #                                mat_req_in=[[1.0]],
   #                                col_req_in=[1.0],
-  #                                matrix_in=x,
-  #                                copy_all_inputs=True)
+  #                                matrix_in=z)
 
   #   self.assertEqual(output['matrix_out'].shape[0], 100)
   #   self.assertEqual(output['matrix_out'].shape[1], 4)
@@ -155,6 +130,31 @@ class TestPythonBinding(unittest.TestCase):
 
   #   for j in range(100):
   #     self.assertEqual(2 * x[j, 2], output['matrix_out'][j, 2])
+
+  def testNumpyMatrixForceCopy(self):
+    """
+    The matrix we pass in, we should get back with the third dimension doubled
+    and the fifth forgotten.
+    """
+    x = np.random.rand(100, 5);
+
+    output = test_python_binding(string_in='hello',
+                                 int_in=12,
+                                 double_in=4.0,
+                                 mat_req_in=[[1.0]],
+                                 col_req_in=[1.0],
+                                 matrix_in=x,
+                                 copy_all_inputs=True)
+
+    self.assertEqual(output['matrix_out'].shape[0], 100)
+    self.assertEqual(output['matrix_out'].shape[1], 4)
+    self.assertEqual(output['matrix_out'].dtype, np.double)
+    for i in [0, 1, 3]:
+      for j in range(100):
+        self.assertEqual(x[j, i], output['matrix_out'][j, i])
+
+    for j in range(100):
+      self.assertEqual(2 * x[j, 2], output['matrix_out'][j, 2])
 
 
   # def testNumpyFContiguousMatrix(self):
