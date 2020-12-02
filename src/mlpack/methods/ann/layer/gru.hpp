f03464aa2cf46ffc34b18e36253836898456c5b5
NippunSharma <inbox.nippun@gmail.com>
2020-12-02 17:39:10 +0530

    added to bilinear, glimpse, gru, highway, layer_norm, linear3d

--- src/mlpack/methods/ann/layer/gru.hpp
+++ src/mlpack/methods/ann/layer/gru.hpp
@@ -155,6 +155,12 @@ class GRU
   //! Get the number of output units.
   size_t OutSize() const { return outSize; }
 
+  //! Get the shape of the input.
+  size_t InputShape() const 
+  {
+    return inSize;
+  }
+
   /**
    * Serialize the layer
    */
