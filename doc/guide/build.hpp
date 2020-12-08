e75d7fdce75a11aec774d0114405927a5c95d08b
iamarchit123 <73429785+iamarchit123@users.noreply.github.com>
2020-12-08 18:31:35 +0530

    Update doc/guide/build.hpp
    Co-authored-by: Omar Shrit <omar@shrit.me>

--- doc/guide/build.hpp
+++ doc/guide/build.hpp
@@ -6,7 +6,7 @@ This document discusses how to build mlpack from source. These build directions
 will work for any Linux-like shell environment (for example Ubuntu, macOS,
 FreeBSD etc). However, mlpack is in the repositories of many Linux distributions
 and so it may be easier to use the package manager for your system.  For example,
-on Ubuntu, you can install mlpack library and command line executables(eg. mlpack_pca, mlpack_kmeans etc.) with the following command:
+on Ubuntu, you can install mlpack library and command-line executables (eg. mlpack_pca, mlpack_kmeans, etc.) with the following command:
 
 @code
 $ sudo apt-get install libmlpack-dev mlpack-bin
