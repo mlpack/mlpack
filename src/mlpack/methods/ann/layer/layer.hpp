a3f746580b2ab350c404e1c1a9f7f49a9c9c3a1b
Marcus Edel <marcus.edel@fu-berlin.de>
2020-12-23 17:12:32 +0100

    Clean-up abstract layer class and add some further documentation.

--- src/mlpack/methods/ann/layer/layer.hpp
+++ src/mlpack/methods/ann/layer/layer.hpp
@@ -2,7 +2,7 @@
  * @file methods/ann/layer/layer.hpp
  * @author Marcus Edel
  *
- * This includes various layers to construct a model.
+ * Base class for neural network layers.
  *
  * mlpack is free software; you may redistribute it and/or modify it under the
  * terms of the 3-clause BSD license.  You should have received a copy of the
@@ -12,72 +12,160 @@
 #ifndef MLPACK_METHODS_ANN_LAYER_LAYER_HPP
 #define MLPACK_METHODS_ANN_LAYER_LAYER_HPP
 
-template<typename InputDataType, typename OutputDataType>
+/**
+ * A layer is an abstract class implementing common neural networks operations,
+ * such as convolution, batch norm, etc. These operations require managing
+ * weights, losses, updates, and inter-layer connectivity.
+ *
+ * Users will just instantiate a layer by inherited from the abstract class and
+ * implement the layer specific methods. It is recommend that descendants of
+ * Layer implement the following methods:
+ *
+ *  - Constructor: Defines custom layer attributes, and creates layer state
+ *    variables.
+ *
+ *  - Forward(input, output): Performs the forward logic of applying the layer
+ *    to the input object and storing the result in the output object.
+ *
+ *  - Backward(input, gy, g): Performs a backpropagation step through the layer,
+ *    with respect to the given input.
+ *
+ *  - Gradient(input, error, gradient): Computing the gradient of the layer with
+ *    respect to its own input.
+ *
+ * The memory for the layer's parameters (weights and biases) is not allocated
+ * by the layer itself, instead it is allocated by the network that the layer
+ * belongs to, and passed to the layer when it needs to use it.
+ *
+ * See the linear layer implementation for a basic example. It's a layer with
+ * two variables, w and b, that returns y = w * x + b. It shows how to implement
+ * Forward(), Backward() and Gradient().  The weights of the layers are tracked
+ * in layer.Parameters().
+ *
+ * @tparam InputType The type of the layer's inputs. Layers automatically cast
+ *     inputs to this type (default: arma::mat).
+ * @tparam OutputType The type of the layer's computation which also causes the
+ *     computations and output to also be in this type. The type also allows the
+ *     computation and weight type to differ from the input type
+ *     (default: arma::mat).
+ */
+template<typename InputType = arma::mat,
+         typename OutputType = arma::mat>
 class Layer
 {
  public:
+   /**
+    * Default constructor.
+    */
+   Layer() { /* Nothing to do here */ }
+
+   /**
+    * Default deconstructor.
+    */
+   ~Layer() { /* Nothing to do here */ }
+
   /**
-   * Ordinary feed forward pass of a neural network, evaluating the function
-   * f(x) by propagating the activity forward through f.
+   * Takes an input object, and computes the corresponding output of the layer.
+   * In general input and output are matrices. However, some special layers like
+   * table layers might expect something else. Please, refer to each layer
+   * specification for further information.
    *
-   * @param input Input data used for evaluating the specified function.
-   * @param output Resulting output activation.
+   * @param * (input) Input data used for evaluating the specified layer.
+   * @param * (output) Resulting output.
    */
-  virtual void Forward(const arma::mat&,
-                       arma::mat&)
+  virtual void Forward(const InputType& /* input */,
+                       OutputType& /* output */)
   { /* Nothing to do here */ }
 
   /**
-   * Ordinary feed forward pass of a neural network, evaluating the function
-   * f(x) by propagating the activity forward through f.
+   * Takes an input and output object, and computes the corresponding loss of
+   * the layer.  In general input and output are matrices. However, some special
+   * layers like table layers might expect something else. Please, refer to each
+   * layer specification for further information.
    *
-   * @param input Input data used for evaluating the specified function.
-   * @param output Resulting output activation.
+   * @param * (input) Input data used for evaluating the specified layer.
+   * @param * (output) Resulting output.
    */
-  virtual double Forward(const arma::mat&,
-                         const arma::mat&)
+  virtual void Forward(const InputType& /* input */,
+                       const OutputType& /* output */)
   { /* Nothing to do here */ }
 
   /**
-   * Ordinary feed backward pass of a neural network, calculating the function
-   * f(x) by propagating x backwards trough f. Using the results from the feed
-   * forward pass.
+   * Performs a backpropagation step through the layer, with respect to the
+   * given input. In general this method makes the assumption Forward(input,
+   * output) has been called before, with the same input. If you do not respect
+   * this rule, Backward(input, gy, g) might compute incorrect results.
+   *
+   * In general input and gy and g are matrices. However, some special
+   * sub-classes like table layers might expect something else. Please, refer to
+   * each module specification for further information.
+   *
+   * A backpropagation step consist of computing of computing the gradient
+   * output input with respect to the output of the layer and given error.
+   *
+   * During the backward pass our goal is to use 'gy' in order to compute the
+   * downstream gradients (g). We assume that the upstream gradient (gy) has
+   * already been computed and is passed to the layer.
    *
    * @param * (input) The propagated input activation.
-   * @param gy The backpropagated error.
-   * @param g The calculated gradient.
+   * @param * (gy) The backpropagated error.
+   * @param * (g) The calculated gradient.
    */
-  virtual void Backward(const arma::mat& input,
-                        const arma::mat& gy,
-                        arma::mat& g)
+  virtual void Backward(const InputType& /* input */,
+                        const OutputType& /* gy */,
+                        OutputType& /* g */)
   { /* Nothing to do here */ }
 
   /**
-   * Calculate the gradient using the output delta and the input activation.
+   * Computing the gradient of the layer with respect to its own input. This is
+   * returned in gradient.
    *
-   * @param input The input parameter used for calculating the gradient.
-   * @param error The calculated error.
-   * @param gradient The calculated gradient.
+   * The layer parameters (weights and biases) are updated accordingly using the
+   * computed gradient not by the layer itself, instead they are updated by the
+   * network that holds the instantiated layer.
+   *
+   * @param * (input) The input parameter used for calculating the gradient.
+   * @param * (error) The calculated error.
+   * @param * (gradient) The calculated gradient.
    */
-  virtual void Gradient(const arma::mat& input,
-                        const arma::mat& error,
-                        arma::mat& gradient)
+  virtual void Gradient(const InputType& /* input */,
+                        const OutputType& /* error */,
+                        OutputType& /* gradient */)
   { /* Nothing to do here */ }
 
   /**
-   * Reset the layer parameter.
+   * Reset the layer parameter. The method is called to assigned the allocated
+   * memory to the internal layer parameters like weights and biases. The method
+   * should be called before the first call of Forward(input, output). If you
+   * do not respect this rule, Forward(input, output) and Backward(input, gy, g)
+   * might compute incorrect results.
    */
   virtual void Reset() {}
 
-  //! Get the gradient.
-  virtual OutputDataType const& Gradient() const { return gradient; }
-  //! Modify the gradient.
-  virtual OutputDataType& Gradient() { return gradient; }
+  //! Get the parameters.
+  virtual OutputType const& Parameters() const { return weights; }
+  //! Modify the parameters.
+  virtual OutputType& Parameters() { return weights; }
+
+  //! Get the input parameter.
+  virtual InputType const& InputParameter() const { return inputParameter; }
+  //! Modify the input parameter.
+  virtual InputType& InputParameter() { return inputParameter; }
+
+  //! Get the output parameter.
+  virtual OutputType const& OutputParameter() const { return outputParameter; }
+  //! Modify the output parameter.
+  virtual OutputType& OutputParameter() { return outputParameter; }
 
   //! Get the delta.
-  virtual arma::mat const& Delta() const { return delta; }
+  virtual OutputType const& Delta() const { return delta; }
   //! Modify the delta.
-  virtual arma::mat& Delta() { return delta; }
+  virtual OutputType& Delta() { return delta; }
+
+  //! Get the gradient.
+  virtual OutputType const& Gradient() const { return gradient; }
+  //! Modify the gradient.
+  virtual OutputType& Gradient() { return gradient; }
 
   //! Get the layer loss.
   virtual double Loss() { return 0; }
@@ -85,30 +173,21 @@ class Layer
   //! Get the size of the weights.
   virtual size_t WeightSize() const { return 0; }
 
-  //! Get the parameters.
-  virtual OutputDataType const& Parameters() const { return weights; }
-  //! Modify the parameters.
-  virtual OutputDataType& Parameters() { return weights; }
-
-  //! Get the input parameter.
-  virtual InputDataType const& InputParameter() const { return inputParameter; }
-  //! Modify the input parameter.
-  virtual InputDataType& InputParameter() { return inputParameter; }
-
-  virtual OutputDataType const& OutputParameter() const { return outputParameter; }
-  //! Modify the output parameter.
-  virtual OutputDataType& OutputParameter() { return outputParameter; }
-
  private:
-  arma::mat weights;
+  //! Locally-stored weight object.
+  OutputType weights;
 
-  arma::mat outputParameter;
+  //! Locally-stored input parameter object.
+  InputType inputParameter;
 
-  arma::mat inputParameter;
+  //! Locally-stored output parameter object.
+  OutputType outputParameter;
 
-  arma::mat delta;
+  //! Locally-stored delta object.
+  OutputType delta;
 
-  arma::mat gradient;
+  //! Locally-stored gradient object.
+  OutputType gradient;
 };
 
 #endif
