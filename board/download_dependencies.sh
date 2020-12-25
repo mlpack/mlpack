6f30bda1ab6224164bdf725a6ad667fea28dafd8
Omar Shrit <omar@shrit.me>
2020-12-25 21:59:39 +0100

    Remove boost
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- board/download_dependencies.sh
+++ board/download_dependencies.sh
@@ -4,23 +4,6 @@
 # required architecture.
 # 
 
-version=$1
-version_underscore=$2
-arch=$3
-binary=$4
-
-echo "Download and build boost as a static lbrary"
-wget --no-check-certificate \
-      "https://dl.bintray.com/boostorg/release/$version/source/boost_$version_underscore.tar.gz" && \
-    tar xvzf boost_$version_underscore.tar.gz && \
-    rm -f boost_$version_underscore.tar.gz && \
-    cd boost_$version_underscore && \
-    cp tools/build/example/user-config.jam project-config.jam && \
-    echo 'using gcc : arm : aarch64-linux-gnu-g++ ;' >> project-config.jam &&\
-    b2 --build=$PWD/build toolset=gcc-arm link=static cxxflags=-fPIC && \
-    -with-libraries=math,program_options,serialization,test && \
-    cd ../../ && \
-
 echo "Downlaod and build OpenBLAS as a static library"
 wget --no-check-certificate \
       "https://github.com/xianyi/OpenBLAS/archive/v0.3.9.tar.gz" && \
