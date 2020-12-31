7a825f60bf3c8d0e858923a9acc21a1036ec12c9
Omar Shrit <omar@shrit.me>
2020-12-31 21:44:23 +0100

    Update OpenBLAS version
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- board/download_openblas.sh
+++ board/download_openblas.sh
@@ -12,19 +12,20 @@ wget --no-check-certificate \
       tar xvzf boost_$version_underscore.tar.gz && \
       rm -f boost_$version_underscore.tar.gz && \
 
-echo "Download and build OpenBLAS as a static library"
-wget --no-check-certificate \
-      "https://github.com/xianyi/OpenBLAS/archive/v0.3.9.tar.gz" && \
-       tar xvzf v0.3.9.tar.gz && \
-       rm -f v0.3.9.tar.gz && \
-       cd OpenBLAS-0.3.9 && \
-       make TARGET=ARMV8 BINARY=64 HOSTCC=gcc CC=aarch64-linux-gnu-gcc NOFORTRAN=1 libs &&\
-       cd ../ &&\
-
 echo "Download the Cereal library"
 wget --no-check-certificate \
       "https://github.com/USCiLab/cereal/archive/v1.3.0.tar.gz" && \
        tar xvzf v1.3.0.tar.gz && \
        rm -f v1.3.0.tar.gz && \
+
+echo "Download and build OpenBLAS as a static library"
+wget --no-check-certificate \
+      "https://github.com/xianyi/OpenBLAS/archive/v0.3.13.tar.gz" && \
+       tar xvzf v0.3.13.tar.gz && \
+       rm -f v0.3.13.tar.gz && \
+       cd OpenBLAS-0.3.13 && \
+       make TARGET=ARMV8 BINARY=64 HOSTCC=gcc CC=aarch64-linux-gnu-gcc FC=aarch64-linux-gnu-gfortran NO_SHARED=1 &&\
+       make *.a libopenblas.a
+       cd ../ &&\
 
 exit 0
