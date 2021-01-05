d0a28cf70919fab97d12d1192b1de3de58643c5b
Anmolpreet Singh <54476451+Anmol2001@users.noreply.github.com>
2021-01-06 00:12:09 +0530

    Update src/mlpack/tests/feedforward_network_test.cpp
    Co-authored-by: Marcus Edel <marcus.edel@fu-berlin.de>

--- src/mlpack/tests/feedforward_network_test.cpp
+++ src/mlpack/tests/feedforward_network_test.cpp
@@ -172,7 +172,7 @@ TEST_CASE("CheckCopyMovingReparametrizationNetworkTest", "[FeedForwardNetworkTes
 
   FFN<NegativeLogLikelihood<> > *model = new FFN<NegativeLogLikelihood<> >;
   model->Add<Linear<> >(trainData.n_rows, 8);
-  model->Add<Reparametrization<> >(4,false,true,1);
+  model->Add<Reparametrization<> >(4, false, true, 1);
   model->Add<LogSoftMax<> >();
 
   FFN<NegativeLogLikelihood<> > *model1 = new FFN<NegativeLogLikelihood<> >;
