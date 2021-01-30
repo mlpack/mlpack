bb4829fe891679acb7ddf66ff4b703c979952f3f
Omar Shrit <omar@shrit.me>
2021-01-30 20:03:30 +0100

    Do not install armadillo, not required
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- board/download_dependencies.sh
+++ board/download_dependencies.sh
@@ -3,22 +3,22 @@
 # This script download OpenBLAS dependecy for Armadillo and cross-compile them against
 # required architecture.
 #
-version=1.75.0
-version_underscore=1_75_0
-
 gcc_version=$1
 
 echo "Download and build boost as a static lbrary"
-	wget --no-check-certificate \
-	"https://dl.bintray.com/boostorg/release/$version/source/boost_$version_underscore.tar.gz" && \
-	tar xvzf boost_$version_underscore.tar.gz && \
-	rm -f boost_$version_underscore.tar.gz && \
+wget --no-check-certificate \
+    "https://dl.bintray.com/boostorg/release/$version/source/boost_$version_underscore.tar.gz" && \
+    tar xvzf boost_1_75_0.tar.gz && \
+    rm -f boost_1.75.0.tar.gz && \
 
 echo "Download the Cereal library"
-	wget --no-check-certificate \
-	"https://github.com/USCiLab/cereal/archive/v1.3.0.tar.gz" && \
-	tar xvzf v1.3.0.tar.gz && \
-	rm -f v1.3.0.tar.gz && \
+wget --no-check-certificate \
+    "https://github.com/USCiLab/cereal/archive/v1.3.0.tar.gz" && \
+    tar xvzf v1.3.0.tar.gz && \
+    rm -f v1.3.0.tar.gz && \
+
+echo "Download the Armadillo library"
+    curl https://data.kurg.org/armadillo-8.400.0.tar.xz | tar -xvJ
 
 echo "Download and build OpenBLAS as a static library"
 wget --no-check-certificate \
