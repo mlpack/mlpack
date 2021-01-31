10187640dfd8167f1896924cec35a64d028ebaa7
Omar Shrit <omar@shrit.me>
2021-01-31 23:14:09 +0100

    Fix the hf addition
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- board/arm-32bit-toolchain.cmake
+++ board/arm-32bit-toolchain.cmake
@@ -7,7 +7,7 @@ elseif(UNIX OR APPLE)
     set(UTIL_SEARCH_CMD which)
 endif()
 
-set(TOOLCHAIN_PREFIX arm-linux-gnueabihf-)
+set(TOOLCHAIN_PREFIX arm-linux-gnueabi-)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE   STATIC_LIBRARY)
 
@@ -27,7 +27,7 @@ set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
 set(CMAKE_OBJCOPY ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}objcopy CACHE INTERNAL "objcopy tool")
 set(CMAKE_SIZE_UTIL ${ARM_TOOLCHAIN_DIR}/${TOOLCHAIN_PREFIX}size CACHE INTERNAL "size tool")
 
-set(CMAKE_SYSROOT /usr/arm-linux-gnueabi)
+#set(CMAKE_SYSROOT /usr/arm-linux-gnueabi)
 set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabi/)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
