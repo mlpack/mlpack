bf238ed213d2d09dfeae40c506f57402d6e04293
Ryan Curtin <ryan@ratml.org>
2021-02-27 16:09:13 -0500

    Don't take a second argument for ToLower().

--- src/mlpack/core/util/to_lower.hpp
+++ src/mlpack/core/util/to_lower.hpp
@@ -19,12 +19,13 @@ namespace util {
  * Convert a string to lowercase letters.
  *
  * @param input The string to convert.
- * @param output The string to be converted.
  */
-inline void ToLower(const std::string& input, std::string& output)
+inline std::string ToLower(const std::string& input)
 {
+  std::string output;
   std::transform(input.begin(), input.end(), output.begin(),
       [](unsigned char c){ return std::tolower(c); });
+  return output;
 }
 
 } // namespace util
