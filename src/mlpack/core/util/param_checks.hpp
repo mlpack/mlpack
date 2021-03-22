85173d3e172e3c05375970a435e5eeb267af398a
visirion25 <workaakash3021@gmail.com>
2021-03-22 12:43:01 +0530

    trying after suggestions3

--- src/mlpack/core/util/param_checks.hpp
+++ src/mlpack/core/util/param_checks.hpp
@@ -206,6 +206,7 @@ void ReportIgnoredParam(
 void ReportIgnoredParam(const std::string& paramName,
                         const std::string& reason);
 
+
 /**
  * If the input has correct shapes(kind of santity checks). 
  *
@@ -219,14 +220,11 @@ void ReportIgnoredParam(const std::string& paramName,
  * @param x_cols the number of columns in training matrix X
  * @param y_cols the number of rows in training label matrix y
  * @param xt_rows the number of rows in test matrix
- * 
- * 
  */
-
 void CheckInputShape(const size_t x_rows,
-                        const size_t x_cols,
-                        const size_t y_cols, 
-                        const size_t xt_rows);
+                    const size_t x_cols,
+                    const size_t y_cols, 
+                    const size_t xt_rows);
 
 } // namespace util
 } // namespace mlpack
--- src/mlpack/tests/main_tests/adaboost_test.cpp
+++ src/mlpack/tests/main_tests/adaboost_test.cpp
@@ -68,17 +68,6 @@ TEST_CASE_METHOD(AdaBoostTestFixture, "AdaBoostOutputDimensionTest",
   SetInputParam("test", std::move(testData));
 
   mlpackMain();
-
-  // Check that number of predicted labels is equal to the input test points.
-  if(IO::GetParam<arma::Row<size_t>>("output").n_cols!=testSize)
-  {
-    size_t rrs, ccls;
-    rrs = IO::GetParam<arma::Row<size_t>>("output").n_rows;
-    ccls = IO::GetParam<arma::Row<size_t>>("output").n_cols;
-    Log::Fatal<<rrs<<" "<<ccls<<" "<<testSize;
-  }
-  REQUIRE(IO::GetParam<arma::Row<size_t>>("output").n_cols == testSize);
-  REQUIRE(IO::GetParam<arma::Row<size_t>>("output").n_rows == 1);
 }
 
 /**
