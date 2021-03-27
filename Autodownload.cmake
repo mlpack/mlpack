650eae0a687bd6b871cefbcc86bf5201f5b96337
Omar Shrit <omar@shrit.me>
2021-03-27 18:12:54 +0100

    Finish boost, but there are still some errors
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- Autodownload.cmake
+++ Autodownload.cmake
@@ -14,10 +14,15 @@ function(get_deps LINK DEPS_NAME PACKAGE)
         # Get the name of the directory.
         file (GLOB DIRECTORIES RELATIVE "${CMAKE_BINARY_DIR}/deps/"
             "${CMAKE_BINARY_DIR}/deps/${DEPS_NAME}*.*")
+        if(${DEPS_NAME} MATCHES "boost")
+          file (GLOB DIRECTORIES RELATIVE "${CMAKE_BINARY_DIR}/deps/"
+              "${CMAKE_BINARY_DIR}/deps/${DEPS_NAME}*_*")
+        endif()
         # list(FILTER) is not available on 3.5 or older, but try to keep
         # configuring without filtering the list anyway (it might work if only
         # the file ensmallen-latest.tar.gz is present.
         if (${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.6.0")
+          list(FILTER DIRECTORIES EXCLUDE REGEX ".*\.tar\.gz")
           list(FILTER DIRECTORIES EXCLUDE REGEX ".*\.tar\.xz")
         endif ()
         list(LENGTH DIRECTORIES DIRECTORIES_LEN)
@@ -33,41 +38,21 @@ function(get_deps LINK DEPS_NAME PACKAGE)
             set(ENSMALLEN_INCLUDE_DIR "${CMAKE_BINARY_DIR}/deps/${ENSMALLEN_DIR}/include")
             find_package(Ensmallen)
 
-            # set(MLPACK_INCLUDE_DIRS ${MLPACK_INCLUDE_DIRS}
-            #    "${CMAKE_BINARY_DIR}/deps/${ENSMALLEN_INCLUDE_DIR}/include" PARENT_SCOPE)
-            # message(STATUS
-            #    "Successfully downloaded ${DEPS_NAME}$ into ${CMAKE_BINARY_DIR}/deps/${ENSMALLEN_INCLUDE_DIR}/")
-
-            # # Now we have to also ensure these header files get installed.
-            # install(DIRECTORY "${CMAKE_BINARY_DIR}/deps/${ENSMALLEN_INCLUDE_DIR}/include/ensmallen_bits/" DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/ensmallen_bits")
-            # install(FILES "${CMAKE_BINARY_DIR}/deps/${ENSMALLEN_INCLUDE_DIR}/include/ensmallen.hpp" DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}")
-
           elseif (${DEPS_NAME} MATCHES "cereal")
             list(GET DIRECTORIES 0 CEREAL_DIR)
             set(CEREAL_INCLUDE_DIR "${CMAKE_BINARY_DIR}/deps/${CEREAL_DIR}/")
             find_package(cereal)
 
-            # set(MLPACK_INCLUDE_DIRS ${MLPACK_INCLUDE_DIRS}
-            #     "${CMAKE_BINARY_DIR}/deps/${CEREAL_INCLUDE_DIR}/include" PARENT_SCOPE)
-            # message(STATUS
-            #     "Successfully downloaded ${DEPS_NAME} into ${CMAKE_BINARY_DIR}/deps/${CEREAL_INCLUDE_DIR}/")
-
-            # # Now we have to also ensure these header files get installed.
-            # install(DIRECTORY "${CMAKE_BINARY_DIR}/deps/${CEREAL_INCLUDE_DIR}/include/cereal" DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/cereal")
-
           elseif(${DEPS_NAME} MATCHES "boost")
-            list(GET DIRECTORIES 0 Boost_INCLUDE_DIR)
-            set(MLPACK_INCLUDE_DIRS ${MLPACK_INCLUDE_DIRS}
-                "${CMAKE_BINARY_DIR}/deps/${Boost_INCLUDE_DIR}" PARENT_SCOPE)
+            list(GET DIRECTORIES 0 Boost_DIR)
+            set(Boost_INCLUDE_DIR "${CMAKE_BINARY_DIR}/deps/${Boost_DIR}/")
             find_package(Boost)
-            message(STATUS
-                "Successfully downloaded ${DEPS_NAME} into ${CMAKE_BINARY_DIR}/deps/${Boost_INCLUDE_DIR}/")
 
           endif()
 
         else ()
           message(FATAL_ERROR 
-                  "Problem unpacking ${DEPS_NAME}! Expected only one directory ${DEPS_NAME}-;. Try removing the directory ${CMAKE_BINARY_DIR}/deps and reconfiguring.")
+                  "Problem unpacking ${DEPS_NAME}! Expected only one directory ${DEPS_NAME};. Try removing the directory ${CMAKE_BINARY_DIR}/deps and reconfiguring.")
         endif ()
       endif ()
       if (${DEPS_NAME} MATCHES "stb")
