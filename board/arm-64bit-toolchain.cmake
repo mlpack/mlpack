49f8cca3469f0f3a22e041a8fc3640bcefc3c8ad
Omar Shrit <omar@shrit.me>
2021-04-10 18:52:14 +0200

    Adding pthread option if needed
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- board/arm-64bit-toolchain.cmake
+++ board/arm-64bit-toolchain.cmake
@@ -7,12 +7,21 @@ elseif(UNIX OR APPLE)
     set(UTIL_SEARCH_CMD which)
 endif()
 
-## You need to add the full path in TOOLCHAIN_PREFIX, if you do not use the
-## standard gcc toolchain 
-set(TOOLCHAIN_PREFIX aarch64-linux-gnu-)
-
+if(DEFINED TOOLCHAIN_PREFIX AND DEFINED CMAKE_SYSROOT)
+  message(STATUS "TOOLCHAIN_PREFIX is defined as: ${TOOLCHAIN_PREFIX}")
+  message(STATUS "CMAKE_SYSROOT is defined as: ${CMAKE_SYSROOT}")
+else ()
+  ## standard gcc toolchain
+  set(TOOLCHAIN_PREFIX aarch64-linux-gnu-)
+  ## There is no need to specify the CMAKE_SYSROOT if you are using the 
+  ## standard toolchain. If you download your own toolchain you have to specify
+  ## the path for sysroot as follows:
+  ## set(CMAKE_SYSROOT /PathToToolchain/aarch64-buildroot-linux-gnu/sysroot)
+  ## or it can be specified from command-line.
+  set(CMAKE_SYSROOT /usr/aarch64-linux-gnu/ CACHE STRING "Enter path for sysroot")
+endif()
 ## In some distribution, a dynamic link for aarch64-linux-gnu-gcc may not be
-## found or created, instead it might be labelled with the version at the end
+## found or created, instead it might be labeled with the version at the end
 ## For instance: aarch64-linux-gnu-gcc-5
 ## Therefore, if dynamic link exists, you do not have to specify the version
 set(VERSION_NUMBER "" CACHE STRING "Enter the version number of the compiler")
@@ -31,18 +40,9 @@ set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
 set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy${VERSION_NUMBER} CACHE INTERNAL "objcopy tool")
 set(CMAKE_SIZE_UTIL ${TOOLCHAIN_PREFIX}size${VERSION_NUMBER} CACHE INTERNAL "size tool")
 
-## There is no need to specify the CMAKE_SYSROOT if you are using the 
-## standard toolchain. If you download your own toolchain you have to specify
-## the path for sysroot as follows:
-## set(CMAKE_SYSROOT /PathToToolchain/aarch64-buildroot-linux-gnu/sysroot)
-## or it can be specified from command-line.
-set(CMAKE_SYSROOT /usr/aarch64-linux-gnu/ CACHE STRING "Enter path for sysroot")
-
 ## Here are the standard ROOT_PATH if you are using the standard toolchain
 ## if you are using a different toolchain you have to specify that too
-set(CMAKE_FIND_ROOT_PATH
-  /usr/aarch64-linux-gnu/ 
-)
+set(CMAKE_FIND_ROOT_PATH "${CMAKE_SYSROOT}")
 
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${CMAKE_SYSROOT}" CACHE INTERNAL "" FORCE)
 
