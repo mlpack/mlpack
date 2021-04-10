2d20ce9099847e89f084478894df981ce0805f5a
Rishabh Garg <56191449+RishabhGarg108@users.noreply.github.com>
2021-04-11 00:19:08 +0530

    Added tests for AllCategoricalSplit for regression

--- src/mlpack/tests/decision_tree_regressor_test.cpp
+++ src/mlpack/tests/decision_tree_regressor_test.cpp
@@ -27,20 +27,20 @@ using namespace mlpack::distribution;
 /**
  * Make sure the MSE gain is zero when the labels are perfect.
  */
-TEST_CASE("MSEGainPerfectTest", "[DecisionTreeRegressionTest]")
+TEST_CASE("MSEGainPerfectTest", "[DecisionTreeRegressorTest]")
 {
   arma::rowvec weights(10, arma::fill::ones);
   arma::rowvec labels;
   labels.ones(10);
 
-  REQUIRE(MSEGain::Evaluate<false>(labels, weights) ==
+  REQUIRE(MSEGain::Evaluate<false>(labels, 0, weights) ==
           Approx(0.0).margin(1e-5));
 }
 
 /**
  * Make sure that the MSE gain is equal to negative of variance.
  */
-TEST_CASE("MSEGainVarianceTest", "[DecisionTreeRegressionTest]")
+TEST_CASE("MSEGainVarianceTest", "[DecisionTreeRegressorTest]")
 {
   arma::rowvec weights(100, arma::fill::ones);
   arma::rowvec labels(100, arma::fill::randn);
@@ -49,7 +49,7 @@ TEST_CASE("MSEGainVarianceTest", "[DecisionTreeRegressionTest]")
   double theoreticalGain = - arma::var(labels) * 99.0 / 100.0;
 
   // Calculated gain.
-  const double calculatedGain = MSEGain::Evaluate<false>(labels, weights);
+  const double calculatedGain = MSEGain::Evaluate<false>(labels, 0, weights);
 
   REQUIRE(calculatedGain == Approx(theoreticalGain).margin(1e-9));
 }
@@ -57,27 +57,27 @@ TEST_CASE("MSEGainVarianceTest", "[DecisionTreeRegressionTest]")
 /**
  * The MSE gain of an empty vector is 0.
  */
-TEST_CASE("MSEGainEmptyTest", "[DecisionTreeRegressionTest]")
+TEST_CASE("MSEGainEmptyTest", "[DecisionTreeRegressorTest]")
 {
   arma::rowvec weights = arma::ones<arma::rowvec>(10);
   arma::rowvec labels;
-  REQUIRE(MSEGain::Evaluate<false>(labels, weights) ==
+  REQUIRE(MSEGain::Evaluate<false>(labels, 0, weights) ==
           Approx(0.0).margin(1e-5));
 
-  REQUIRE(MSEGain::Evaluate<true>(labels, weights) ==
+  REQUIRE(MSEGain::Evaluate<true>(labels, 0, weights) ==
           Approx(0.0).margin(1e-5));
 }
 
 /**
  * Make sure the MAD gain is zero when the labels are perfect.
  */
-TEST_CASE("MADGainPerfectTest", "[DecisionTreeRegressionTest]")
+TEST_CASE("MADGainPerfectTest", "[DecisionTreeRegressorTest]")
 {
   arma::rowvec weights(10, arma::fill::ones);
   arma::rowvec labels;
   labels.ones(10);
 
-  REQUIRE(MADGain::Evaluate<false>(labels, weights) ==
+  REQUIRE(MADGain::Evaluate<false>(labels, 0, weights) ==
           Approx(0.0).margin(1e-5));
 }
 
@@ -85,7 +85,7 @@ TEST_CASE("MADGainPerfectTest", "[DecisionTreeRegressionTest]")
  * Make sure that when mean of labels is zero, MAD_gain = mean of
  * absolute values of the distribution.
  */
-TEST_CASE("MADGainNormalTest", "[DecisionTreeRegressionTest")
+TEST_CASE("MADGainNormalTest", "[DecisionTreeRegressorTest")
 {
   arma::rowvec weights(10, arma::fill::ones);
   arma::rowvec labels = { 1, 2, 3, 4, 5, -1, -2, -3, -4, -5 }; // Mean = 0.
@@ -97,7 +97,7 @@ TEST_CASE("MADGainNormalTest", "[DecisionTreeRegressionTest")
   theoreticalGain /= (double) labels.n_elem;
 
   // Calculated gain.
-  const double calculatedGain = MADGain::Evaluate<false>(labels, weights);
+  const double calculatedGain = MADGain::Evaluate<false>(labels, 0, weights);
 
   REQUIRE(calculatedGain == Approx(theoreticalGain).margin(1e-5));
 }
@@ -105,13 +105,111 @@ TEST_CASE("MADGainNormalTest", "[DecisionTreeRegressionTest")
 /**
  * The MAD gain of an empty vector is 0.
  */
-TEST_CASE("MADGainEmptyTest", "[DecisionTreeRegressionTest]")
+TEST_CASE("MADGainEmptyTest", "[DecisionTreeRegressorTest]")
 {
   arma::rowvec weights = arma::ones<arma::rowvec>(10);
   arma::rowvec labels;
-  REQUIRE(MADGain::Evaluate<false>(labels, weights) ==
+  REQUIRE(MADGain::Evaluate<false>(labels, 0, weights) ==
           Approx(0.0).margin(1e-5));
 
-  REQUIRE(MADGain::Evaluate<true>(labels, weights) ==
+  REQUIRE(MADGain::Evaluate<true>(labels, 0, weights) ==
           Approx(0.0).margin(1e-5));
+}
+
+/**
+ * Check that AllCategoricalSplit will split when the split is obviously
+ * better.
+ */
+TEST_CASE("AllCategoricalSplitSimpleSplitTest1", "[DecisionTreeRegressorTest]")
+{
+  arma::vec predictor(100);
+  arma::rowvec labels(100);
+  arma::rowvec weights(labels.n_elem);
+  weights.ones();
+
+  for (size_t i = 0; i < 100; i+=2)
+  {
+    predictor[i] = 0;
+    labels[i] = 5.0;
+    predictor[i + 1] = 1;
+    labels[i + 1] = 100;
+  }
+
+  double splitInfo;
+  AllCategoricalSplit<MSEGain>::AuxiliarySplitInfo aux;
+
+  // Call the method to do the splitting.
+  const double bestGain = MSEGain::Evaluate<false>(labels, 0, weights);
+  const double gain = AllCategoricalSplit<MSEGain>::SplitIfBetter<false>(
+      bestGain, predictor, 2, labels, 0, 0, weights, 3, 1e-7, splitInfo, aux);
+  const double weightedGain =
+      AllCategoricalSplit<MSEGain>::SplitIfBetter<true>(bestGain, predictor, 2,
+      labels, 0, 0, weights, 3, 1e-7, splitInfo, aux);
+
+  // Make sure that a split was made.
+  REQUIRE(gain > bestGain);
+
+  REQUIRE(gain == weightedGain);
+
+  // Make sure that splitInfo now hold the number of children.
+  REQUIRE((size_t) splitInfo == 2);
+}
+
+/**
+ * Make sure that AllCategoricalSplit respects the minimum number of samples
+ * required to split.
+ */
+TEST_CASE("AllCategoricalSplitMinSamplesTest1", "[DecisionTreeRegressorTest]")
+{
+  arma::rowvec predictors = {0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3};
+  arma::rowvec labels = {0, 0, 0, 2, 2, 2, 1, 1, 1, 2, 2, 2};
+  arma::rowvec weights(labels.n_elem);
+  weights.ones();
+
+  double splitInfo;
+  AllCategoricalSplit<MSEGain>::AuxiliarySplitInfo aux;
+
+  // Call the method to do the splitting.
+  const double bestGain = MSEGain::Evaluate<false>(labels, 0, weights);
+  const double gain = AllCategoricalSplit<MSEGain>::SplitIfBetter<false>(
+      bestGain, predictors, 4, labels, 0, 0, weights, 4, 1e-7, splitInfo, aux);
+
+  // Make sure it's not split.
+  REQUIRE(gain == DBL_MAX);
+}
+
+/**
+ * Check that no split is made when it doesn't get us anything.
+ */
+TEST_CASE("AllCategoricalSplitNoGainTest1", "[DecisionTreeRegressorTest]")
+{
+  arma::rowvec predictors(300);
+  arma::rowvec labels(300);
+  arma::rowvec weights = arma::ones<arma::rowvec>(300);
+
+  for (size_t i = 0; i < 300; i += 3)
+  {
+    predictors[i] = int(i / 3) % 10;
+    labels[i] = -0.5;
+    predictors[i + 1] = int(i / 3) % 10;
+    labels[i + 1] = 0;
+    predictors[i + 2] = int(i / 3) % 10;
+    labels[i + 2] = 0.5;
+  }
+
+  double splitInfo;
+  AllCategoricalSplit<MSEGain>::AuxiliarySplitInfo aux;
+
+  // Call the method to do the splitting.
+  const double bestGain = MSEGain::Evaluate<false>(labels, 0, weights);
+  const double gain = AllCategoricalSplit<MSEGain>::SplitIfBetter<false>(
+      bestGain, predictors, 10, labels, 0, 0, weights, 10, 1e-7,
+      splitInfo, aux);
+  const double weightedGain =
+      AllCategoricalSplit<MSEGain>::SplitIfBetter<true>(bestGain, predictors,
+      10, labels, 0, 0, predictors, 10, 1e-7, splitInfo, aux);
+
+  // Make sure that there was no split.
+  REQUIRE(gain == DBL_MAX);
+  REQUIRE(gain == weightedGain);
 }
