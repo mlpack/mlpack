8220a774509b6a79a5d29b3633fa201a71b22f61
Omar Shrit <omar@shrit.me>
2021-04-18 23:46:14 +0200

    Resolve board_name variable issue
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- board/flags-config.cmake
+++ board/flags-config.cmake
@@ -3,18 +3,19 @@
 # Thus, all of the fllowing flags are chosen carefully to reduce binary 
 # footprints.
 
-function(flags BOARD_NAME)
-  # Set generic minimization flags for all platforms.
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Os -fdata-sections -ffunction-sections")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fomit-frame-pointer -fno-unwind-tables")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-asynchronous-unwind-tables -fvisibility=hidden")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fshort-enums -finline-small-functions")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -findirect-inlining -fno-common") 
-  #-flto -fuse-ld=gold # There is an issue with gold link when compiling on 
-  # Ubuntu 16. At that point gcc linker did not integrate the flto support 
-  # inside and it was a separate plugin that need to be added. Therefore, 
-  # this can be added when mlpack Azure CI moves toward Ubuntu 20.
+# Set generic minimization flags for all platforms.
+# These flags are the same for all cross-compilation cases.
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Os -fdata-sections -ffunction-sections")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fomit-frame-pointer -fno-unwind-tables")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-asynchronous-unwind-tables -fvisibility=hidden")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fshort-enums -finline-small-functions")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -findirect-inlining -fno-common") 
+#-flto -fuse-ld=gold # There is an issue with gold link when compiling on 
+# Ubuntu 16. At that point gcc linker did not integrate the flto support 
+# inside and it was a separate plugin that need to be added. Therefore, 
+# this can be added when mlpack Azure CI moves toward Ubuntu 20.
 
+function(flags BOARD_NAME)
   # Set specific platforms CMAKE CXX flags.
   if(BOARD_NAME STREQUAL "rpi0" OR BOARD_NAME STREQUAL "rpi1")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s" PARENT_SCOPE)
