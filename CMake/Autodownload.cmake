03b3a0be05492c2e88aba09fd8a857e831972531
Omar Shrit <omar@shrit.me>
2021-04-20 18:21:15 +0200

    Fix `if` style in CMake
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- CMake/Autodownload.cmake
+++ CMake/Autodownload.cmake
@@ -22,7 +22,7 @@ macro(get_deps LINK DEPS_NAME PACKAGE)
       file (GLOB DIRECTORIES RELATIVE "${CMAKE_BINARY_DIR}/deps/"
           "${CMAKE_BINARY_DIR}/deps/${DEPS_NAME}*.*")
       # Clean this line when boost is removed
-      if(${DEPS_NAME} MATCHES "boost")
+      if (${DEPS_NAME} MATCHES "boost")
         file (GLOB DIRECTORIES RELATIVE "${CMAKE_BINARY_DIR}/deps/"
             "${CMAKE_BINARY_DIR}/deps/${DEPS_NAME}*_*")
       elseif(${DEPS_NAME} MATCHES "stb")
