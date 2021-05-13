97edfc4f8a16e7daee007394e2f35c954b6bdc34
P_Pranav_Reddy <bestpranav.16@gmail.com>
2021-05-14 03:58:37 +0530

    refactored Logistic Regression to use CheckSameSize() function

--- src/mlpack/methods/logistic_regression/logistic_regression_function_impl.hpp
+++ src/mlpack/methods/logistic_regression/logistic_regression_function_impl.hpp
@@ -32,13 +32,7 @@ LogisticRegressionFunction<MatType>::LogisticRegressionFunction(
     lambda(lambda)
 {
   // Sanity check.
-  if (responses.n_elem != predictors.n_cols)
-  {
-    Log::Fatal << "LogisticRegressionFunction::LogisticRegressionFunction(): "
-        << "predictors matrix has " << predictors.n_cols << " points, but "
-        << "responses vector has " << responses.n_elem << " elements (should be"
-        << " " << predictors.n_cols << ")!" << std::endl;
-  }
+  util::CheckSameSizes(predictors, responses, "LogisticRegressionFunction::LogisticRegressionFunction()");
 }
 
 /**
