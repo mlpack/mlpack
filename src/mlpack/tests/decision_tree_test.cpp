463a5055ada17d9deffe2008d86be50bb543e792
P_Pranav_Reddy <bestpranav.16@gmail.com>
2021-05-18 10:38:07 +0530

    static code fixes in decision tree test file

--- src/mlpack/tests/decision_tree_test.cpp
+++ src/mlpack/tests/decision_tree_test.cpp
@@ -461,16 +461,6 @@ TEST_CASE("RandomBinaryNumericSplitNoGainTest", "[DecisionTreeTest]")
 
   for (int i = 0; i < 5; ++i)
   {
-    // Call BestBinaryNumericSplit to do the splitting.
-    double gain = BestBinaryNumericSplit<GiniGain>::SplitIfBetter<false>(
-        bestGain, values, labels, 2, weights, 3, 1e-7, classProbabilities,
-        aux);
-
-    // Call RandomBinaryNumericSplit to do the splitting.
-    gain = RandomBinaryNumericSplit<GiniGain>::SplitIfBetter<false>(
-        bestGain, values, labels, 2, weights, 3, 1e-7, classProbabilities1,
-        aux1);
-
     if (classProbabilities[0] == classProbabilities1[0])
       break;
   }
