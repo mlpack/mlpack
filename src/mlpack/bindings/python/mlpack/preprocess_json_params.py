719f23ee71764d4ab56d27f71cef6fd40066626b
NippunSharma <www.inbox.nippun@gmail.com>
2021-05-24 15:13:07 +0530

    removed unused imports

--- src/mlpack/bindings/python/mlpack/preprocess_json_params.py
+++ src/mlpack/bindings/python/mlpack/preprocess_json_params.py
@@ -13,7 +13,6 @@ terms of the 3-clause BSD license.  You should have received a copy of the
 3-clause BSD license along with mlpack.  If not, see
 http://www.opensource.org/licenses/BSD-3-Clause for more information.
 """
-from random import randint
 import numpy as np
 import json
 import pprint
