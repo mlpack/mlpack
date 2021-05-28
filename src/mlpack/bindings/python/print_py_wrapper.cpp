e69a16a51e27738d9f8bf7be3972de3d7ad1fee3
NippunSharma <www.inbox.nippun@gmail.com>
2021-05-28 16:17:11 +0530

    added skeleton

---
+++ src/mlpack/bindings/python/print_py_wrapper.cpp
@@ -0,0 +1,18 @@
+#include "print_py_wrapper.hpp"
+#include <mlpack/core/util/io.hpp>
+
+using namespace mlpack::util;
+using namespace std;
+
+namespace mlpack {
+namespace bindings {
+namespace python {
+
+void PrintWrapperPY()
+{
+	cout << "Here!!" << endl;
+}
+
+}
+}
+}
---
+++ src/mlpack/bindings/python/print_py_wrapper.hpp
@@ -0,0 +1,16 @@
+#ifndef MLPACK_BINDINGS_PYTHON_PRINT_PY_WRAPPER_HPP
+#define MLPACK_BINDINGS_PYTHON_PRINT_PY_WRAPPER_HPP
+
+#include <mlpack/core.hpp>
+
+namespace mlpack {
+namespace bindings {
+namespace python {
+
+void PrintWrapperPY();
+
+} // namespace mlpack
+} // namespace bindings
+} // namespace python
+
+#endif
