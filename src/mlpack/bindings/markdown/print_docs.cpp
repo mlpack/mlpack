218e13843f10929588ef08714711e0464aad6c57
Omar Shrit <omar@shrit.me>
2021-06-16 16:14:41 +0200

    Re-organize header, I am confused now
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/mlpack/bindings/markdown/print_docs.cpp
+++ src/mlpack/bindings/markdown/print_docs.cpp
@@ -9,14 +9,14 @@
  * 3-clause BSD license along with mlpack.  If not, see
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
-#include "binding_info.hpp"
-#include "print_docs.hpp"
-#include "print_doc_functions.hpp"
-
 #include <mlpack/core/util/io.hpp>
 #include <mlpack/core/util/binding_details.hpp>
 
 #include <boost/algorithm/string/replace.hpp>
+
+#include "binding_info.hpp"
+#include "print_docs.hpp"
+#include "print_doc_functions.hpp"
 
 // Make sure that this is defined.
 #ifndef DOXYGEN_PREFIX
