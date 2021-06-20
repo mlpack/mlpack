ff1041282a4a700674c27b9fa5444fa8cbe3c091
abh2k <41710346+abh2k@users.noreply.github.com>
2021-06-20 20:43:08 +0530

    Improved speed of lp forward pass

--- src/mlpack/methods/ann/layer/lp_pooling.hpp
+++ src/mlpack/methods/ann/layer/lp_pooling.hpp
@@ -161,12 +161,22 @@ class LpPooling
   template<typename eT>
   void Pooling(const arma::Mat<eT>& input, arma::Mat<eT>& output)
   {
+    arma::Mat<eT> inputPre = input;
+    input = arma::pow(input, normType);
+
+    for (size_t i = 1; i < input.n_cols; ++i)
+      inputPre.col(i) += inputPre.col(i - 1);
+
+    for (size_t i = 1; i < input.n_rows; ++i)
+      inputPre.row(i) += inputPre.row(i - 1);
+
     for (size_t j = 0, colidx = 0; j < output.n_cols;
          ++j, colidx += strideHeight)
     {
       for (size_t i = 0, rowidx = 0; i < output.n_rows;
            ++i, rowidx += strideWidth)
       {
+        double val = 0.0;
         size_t rowEnd = rowidx + kernelWidth - 1;
         size_t colEnd = colidx + kernelHeight - 1;
 
@@ -175,12 +185,16 @@ class LpPooling
         if (colEnd > input.n_cols - 1)
           colEnd = input.n_cols - 1;
 
-        arma::mat subInput = input(
-            arma::span(rowidx, rowEnd),
-            arma::span(colidx, colEnd));
+        if (rowidx >= 1)
+        {
+          if (colidx >= 1)
+            val += inputPre(rowidx - 1, colidx - 1);
+          val -= inputPre(rowidx - 1, colEnd);
+        }
+        if (colidx >= 1)
+          val -= inputPre(rowEnd, colidx - 1);
 
-        output(i, j) = pow(arma::accu(arma::pow(subInput,
-            normType)), 1.0 / normType);
+        output(i, j) = pow(val, 1.0 / normType);
       }
     }
   }
