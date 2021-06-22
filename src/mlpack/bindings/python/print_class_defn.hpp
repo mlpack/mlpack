9fc0513b26b14a5690b6d1fcb9022ada85b12d82
Marcus Edel <marcus.edel@fu-berlin.de>
2021-06-22 03:34:04 +0200

    Style fixes (line length, extra spaces, newline).

--- src/mlpack/bindings/python/print_class_defn.hpp
+++ src/mlpack/bindings/python/print_class_defn.hpp
@@ -63,11 +63,11 @@ void PrintClassDefn(
    * cdef class <ModelType>Type:
    *   cdef <ModelType>* modelptr
    *   cdef public dict scrubbed_params
-   * 
+   *
    *   def __cinit__(self):
    *     self.modelptr = new <ModelType>()
    *     self.scrubbed_params = dict()
-   * 
+   *
    *   def __dealloc__(self):
    *     del self.modelptr
    *
@@ -82,18 +82,18 @@ void PrintClassDefn(
    *
    *   def _get_cpp_params(self):
    *     return SerializeOutJSON(self.modelptr, "<ModelType>")
-   * 
+   *
    *   def _set_cpp_params(self, state):
    *     SerializeInJSON(self.modelptr, state, "<ModelType>")
-   * 
+   *
    *   def get_cpp_params(self, return_str=False):
    *     params = self._get_cpp_params()
    *     return process_params_out(self, params, return_str=return_str)
-   * 
+   *
    *   def set_cpp_params(self, params_dic):
    *     params_str = process_params_in(self, params_dic)
    *     self._set_cpp_params(params_str)
-   * 
+   *
    * @endcode
    */
   std::cout << "cdef class " << strippedType << "Type:" << std::endl;
@@ -129,11 +129,14 @@ void PrintClassDefn(
   std::cout << std::endl;
   std::cout << "  def get_cpp_params(self, return_str=False):" << std::endl;
   std::cout << "    params = self._get_cpp_params()" << std::endl;
-  std::cout << "    return process_params_out(self, params, return_str=return_str)" << std::endl;
+  std::cout << "    return process_params_out(self, params, "
+    << "return_str=return_str)" << std::endl;
   std::cout << std::endl;
   std::cout << "  def set_cpp_params(self, params_dic):" << std::endl;
-  std::cout << "    params_str = process_params_in(self, params_dic)" << std::endl;
-  std::cout << "    self._set_cpp_params(params_str.encode(\"utf-8\"))" << std::endl;
+  std::cout << "    params_str = process_params_in(self, params_dic)"
+    << std::endl;
+  std::cout << "    self._set_cpp_params(params_str.encode(\"utf-8\"))"
+    << std::endl;
   std::cout << std::endl;
 }
 
