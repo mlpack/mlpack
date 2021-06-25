33adf02ed880c76d3df2adea5f1c713e841de3c6
Ryan Curtin <ryan@ratml.org>
2021-06-25 10:14:16 -0400

    Make LinearRegressionMainTest compile successfully.

--- src/mlpack/bindings/tests/clean_memory.hpp
+++ src/mlpack/bindings/tests/clean_memory.hpp
@@ -13,6 +13,8 @@
 #ifndef MLPACK_BINDINGS_TESTS_CLEAN_MEMORY_HPP
 #define MLPACK_BINDINGS_TESTS_CLEAN_MEMORY_HPP
 
+#include <mlpack/core/util/params.hpp>
+
 namespace mlpack {
 namespace bindings {
 namespace tests {
