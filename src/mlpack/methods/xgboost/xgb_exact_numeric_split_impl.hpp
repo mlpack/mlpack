733b85eaf3dbadd40820d798bf2cd20b34f3fdcd
Rishabh Garg <56191449+RishabhGarg108@users.noreply.github.com>
2021-07-24 22:53:37 +0530

    Removed unrequired check

--- src/mlpack/methods/xgboost/xgb_exact_numeric_split_impl.hpp
+++ src/mlpack/methods/xgboost/xgb_exact_numeric_split_impl.hpp
@@ -76,17 +76,6 @@ double XGBExactNumericSplit<LossFunction>::SplitIfBetter(
     // Evaluate the gain for the split.
     const double gain = lossFunction.Evaluate();
 
-    // Corner case: is this the best possible split?
-    if (gain >= 0.0)
-    {
-      // We can take a shortcut: no split will be better than this, so just
-      // take this one. The actual split value will be halfway between the
-      // value at index - 1 and index.
-      splitInfo = (data[sortedIndices[index - 1]] +
-          data[sortedIndices[index]]) / 2.0;
-
-      return gain;
-    }
     if (gain > bestFoundGain)
     {
       // We still have a better split.
