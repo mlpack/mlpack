4720efc1bbf12ce0bb4b02d0d9aa18575cf0722f
Rishabh Garg <56191449+RishabhGarg108@users.noreply.github.com>
2021-07-28 00:55:57 +0530

    Add tests for XGBExactSplit

--- src/mlpack/tests/xgboost_test.cpp
+++ src/mlpack/tests/xgboost_test.cpp
@@ -11,6 +11,7 @@
  */
 #include <mlpack/core.hpp>
 #include <mlpack/methods/xgboost/loss_functions/sse_loss.hpp>
+#include <mlpack/methods/xgboost/xgb_exact_numeric_split.hpp>
 
 #include "catch.hpp"
 #include "serialization.hpp"
@@ -88,4 +89,56 @@ TEST_CASE("SSELossEmptyTest", "[XGBTest]")
 
   SSELoss Loss;
   REQUIRE(Loss.Evaluate<false>(input, weights) == Approx(0.0).margin(1e-5));
+}
+
+/**
+ * Check that the XGBExactNumericSplit will split on an obviously splittable
+ * dimension.
+ */
+TEST_CASE("XGBExactNumericSplitSimpleSplitTest", "[XGBTest]")
+{
+  arma::rowvec predictors = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  arma::mat input = { { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                      { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1 } };
+  arma::vec weights; // dummy weights not used.
+
+  SSELoss Loss;
+  double splitInfo;
+  XGBExactNumericSplit<SSELoss>::AuxiliarySplitInfo aux;
+
+  // Call the method to do the splitting.
+  const double bestGain = Loss.Evaluate<false>(input, weights);
+  const double gain = XGBExactNumericSplit<SSELoss>::SplitIfBetter<false>(
+      bestGain, predictors, input, weights, 0, 0, splitInfo, aux, Loss);
+
+  // Make sure that a split was made.
+  REQUIRE(gain > bestGain);
+
+  // Make sure that the split was between 5 and 6.
+  REQUIRE(splitInfo > 5);
+  REQUIRE(splitInfo < 6);
+}
+
+/**
+ * Check that the XGBExactNumericSplit won't split if both left and right
+ * possible childs violate the minChildWeight condition.
+ */
+TEST_CASE("XGBExactNumericSplitMinChildWeightTest", "[XGBTest]")
+{
+  arma::rowvec predictors = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  arma::mat input = { { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                      { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1 } };
+  arma::vec weights; // dummy weights not used.
+
+  SSELoss Loss(0, 0, 6); // minChildWeight = 6
+  double splitInfo;
+  XGBExactNumericSplit<SSELoss>::AuxiliarySplitInfo aux;
+
+  // Call the method to do the splitting.
+  const double bestGain = Loss.Evaluate<false>(input, weights);
+  const double gain = XGBExactNumericSplit<SSELoss>::SplitIfBetter<false>(
+      bestGain, predictors, input, weights, 0, 0, splitInfo, aux, Loss);
+
+  // Make sure that no split was made.
+  REQUIRE(gain == DBL_MAX);
 }
