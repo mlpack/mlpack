7873eb298025771b316e105516cf7fa0daf74dde
Rishabh Garg <56191449+RishabhGarg108@users.noreply.github.com>
2021-07-28 12:02:38 +0530

    Create files for implementation

---
+++ src/mlpack/methods/xgboost/xgboost.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file methods/xgboost/xgboost.hpp
+ * @author Rishabh Garg
+ *
+ * Definition of the XGBoost class.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_METHODS_XGBOOST_XGBOOST_HPP
+#define MLPACK_METHODS_XGBOOST_XGBOOST_HPP
+
+namespace mlpack {
+namespace ensemble {
+
+} // namespace ensemble
+} // namespace mlpack
+
+// Include implementation.
+#include "xgboost_impl.hpp"
+
+#endif
---
+++ src/mlpack/methods/xgboost/xgboost_impl.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file methods/xgboost/xgboost_impl.hpp
+ * @author Rishabh Garg
+ *
+ * Implementation of XGBoost.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_METHODS_XGBOOST_XGBOOST_IMPL_HPP
+#define MLPACK_METHODS_XGBOOST_XGBOOST_IMPL_HPP
+
+// In case it hasn't been included yet.
+#include "xgboost.hpp"
+
+namespace mlpack {
+namespace ensemble {
+
+} // namespace ensemble
+} // namespace mlpack
+
+#endif
