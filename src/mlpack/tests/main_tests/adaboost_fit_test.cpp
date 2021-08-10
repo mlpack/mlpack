27988ec7c42cd8232f98fc2b5ef5029089c2db32
Nippun Sharma <inbox.nippun@gmail.com>
2021-08-10 09:24:21 +0530

    added more tests in adaboost_fit_test

--- src/mlpack/tests/main_tests/adaboost_fit_test.cpp
+++ src/mlpack/tests/main_tests/adaboost_fit_test.cpp
@@ -86,3 +86,83 @@ TEST_CASE_METHOD(AdaBoostFitMainTestFixture, "AdaBoostFitWeakLearnerTest",
   REQUIRE_THROW_AS(RUN_BINDING(), std::runtime_error);
   Log::Fatal.ignoreInput = false;
 }
+
+// Make sure that the output model is "trained" and usable.
+TEST_CASE_METHOD(AdaBoostFitMainTestFixture, "AdaBoostFitModelReuseTest",
+                 "[AdaBoostFitMainTest][BindingTests]")
+{
+  arma::mat trainData;
+  if (!data::Load("vc2.csv", trainData))
+    FAIL("Unable to load train dataset vc2.csv!");
+
+  arma::Row<size_t> labels;
+  if (!data::Load("vc2_labels.txt", labels))
+    FAIL("Unable to load label dataset vc2_labels.txt!");
+
+  arma::mat testData;
+  if (!data::Load("vc2_test.csv", testData))
+    FAIL("Unable to load test dataset vc2.csv!");
+
+  SetInputParam("training", std::move(trainData));
+  SetInputParam("labels", std::move(labels));
+
+  RUN_BINDING();
+
+  AdaBoostModel* model = params.Get<AdaBoostModel*>("output_model");
+  REQUIRE(model->Dimensionality() == trainData.n_rows);
+
+  // Can we make predictions from this model.
+  arma::Row<size_t> predictions, probabilities;
+  model->Classify(testData, predictions, probabilities);
+
+  // Check if dims of predictions is same as train data.
+  REQUIRE(trainData.n_rows == probabilities.n_rows);
+}
+
+/**
+ * Check that the last dimension of the training set is
+ * used as labels when labels are not passed specifically 
+ * and results are same from both label and without label models.
+ */
+TEST_CASE_METHOD(AdaBoostFitMainTestFixture, "AdaBoostFitMainWithoutLabelTest",
+                 "[AdaBoostFitMainMainTest][BindingTests]")
+{
+  // Train adaboost without providing labels.
+  arma::mat trainData;
+  if (!data::Load("trainSet.csv", trainData))
+    FAIL("Unable to load train dataset trainSet.csv!");
+
+  // Give labels.
+  arma::Row<size_t> labels(trainData.n_cols);
+  for (size_t i = 0; i < trainData.n_cols; ++i)
+    labels[i] = trainData(trainData.n_rows - 1, i);
+
+  arma::mat testData;
+  if (!data::Load("testSet.csv", testData))
+    FAIL("Unable to load test dataset testSet.csv!");
+
+  // Delete the last row containing labels from test dataset.
+  testData.shed_row(testData.n_rows - 1);
+
+  SetInputParam("training", trainData);
+  SetInputParam("iterations",(int) 20);
+  SetInputParam("tolerance", 0.001);
+
+  RUN_BINDING();
+
+  // Get predictions from trained model.
+  arma::Row<size_t> predictions;
+  AdaBoostModel* model  = params.Get<AdaBoostModel*>("output_model");
+  model->Classify(testData, predictions);
+
+  // Train new model by providing labels seperately.
+  AdaBoostModel* model2 = new AdaBoostModel(model->Mappings(),
+                                            model->WeakLearnerType());
+  arma::Row<size_t> predictions2;
+  model2->Train(trainData, labels, model->Mappings().n_elem,
+      (int) 20, 0.001);
+  model2->Classify(testData, predictions2);
+
+  // Check if both predictions are same or not.
+  CheckMatrices(predictions, predictions2);
+}
