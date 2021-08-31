11beaeca436842bc92a9b65d59d3eb3a7d5101fd
unknown <yusufcricket200@gmail.com>
2021-08-31 18:15:24 +0530

    Added SQNL (#3044)

--- src/mlpack/methods/ann/layer_names.hpp
+++ src/mlpack/methods/ann/layer_names.hpp
@@ -417,6 +417,17 @@ class LayerNameVisitor : public boost::static_visitor<std::string>
   }
 
   /**
+   * Return the name of the given layer of type SQNL as a string.
+   *
+   * @param * Given layer of type SQNL.
+   * @return The string representation of the layer.
+   */
+  std::string LayerString(SQNL<>* /*layer*/) const
+  {
+    return "sqnl";
+  }
+
+  /**
    * Return the name of the layer of specified type as a string.
    *
    * @param * Given layer of any type.
