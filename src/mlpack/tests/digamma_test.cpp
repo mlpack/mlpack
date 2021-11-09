0d736d340a45b189e3607c154913b68bfd37bd54
heisenbuug <tgopimanohar@gmail.com>
2021-11-10 02:25:24 +0530

    adding a single test

---
+++ src/mlpack/tests/digamma_test.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file tests/digamma_test.cpp
+ * @author Gopi Tatiraju
+ * 
+ * Test the digamma distribution.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#include <mlpack/core.hpp>
+#include <mlpack/core/math/digamma.hpp>
+
+#include "test_catch_tools.hpp"
+#include "catch.hpp"
+
+using namespace mlpack;
+using namespace arma;
+
+/**
+ * Test the output of digamma for large input values.
+ */
+TEST_CASE("DigammaLarge", "[DigammaTest]")
+{
+  arma::mat data;
+
+  if (!data::Load("digamma_data.csv", data));
+    FAIL("Cannot load data digamma_data.csv");
+  
+  for (int i = 0; i < data.n_rows; i++)
+    REQUIRE(Digamma(data(i, 0) == Approx(data(i, 1).epsilon(1e-7)));
+}
+
+/**
+ * Test the output of digamma for negative input values.
+ */
+TEST_CASE("DigammaNegative", "[DigammaTest]")
+{
+  
+}
+
+/**
+ * Test the output of digamma for small input values.
+ */
+TEST_CASE("DigammaSmall", "[DigammaTest]")
+{
+  
+}
+
+/**
+ * Test the output of digamma for values near the positive roots.
+ */
+TEST_CASE("DigammaNearPositiveRoots", "[DigammaTest]")
+{
+  
+}
