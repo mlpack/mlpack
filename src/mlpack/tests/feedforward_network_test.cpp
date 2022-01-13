14728bbfc0ef58906c81c4637f3f13b214b98d41
dnabanita7 <dashnabanita@gmail.com>
2022-01-13 10:28:36 +0530

    fix labels in tests

--- src/mlpack/tests/feedforward_network_test.cpp
+++ src/mlpack/tests/feedforward_network_test.cpp
@@ -441,63 +441,9 @@ TEST_CASE("CheckCopyMovingMaxPoolingNetworkTest", "[FeedForwardNetworkTest]")
 }
 
 /**
- * Check whether copying and moving of Max Pooling network is working or not.
- */
-TEST_CASE("CheckCopyMovingMaxPoolingNetworkTest", "[FeedForwardNetworkTest]")
-{
-  // Load the dataset.
-  arma::mat trainData;
-  data::Load("thyroid_train.csv", trainData, true);
-
-  // Normalize labels to [0, 2].
-  arma::mat trainLabels = trainData.row(trainData.n_rows - 1) - 1;
-  trainData.shed_row(trainData.n_rows - 1);
-
-  /*
-   * Construct a feed forward network with trainData.n_rows input nodes,
-   * hiddenLayerSize hidden nodes and trainLabels.n_rows output nodes. The
-   * network structure looks like:
-   *
-   *  Input         Hidden        Output
-   *  Layer         Layer         Layer
-   * +-----+       +-----+       +-----+
-   * |     |       |     |       |     |
-   * |     +------>|     +------>|     |
-   * |     |     +>|     |     +>|     |
-   * +-----+     | +--+--+     | +-----+
-   *             |             |
-   *  Bias       |  Bias       |
-   *  Layer      |  Layer      |
-   * +-----+     | +-----+     |
-   * |     |     | |     |     |
-   * |     +-----+ |     +-----+
-   * |     |       |     |
-   * +-----+       +-----+
-   */
-
-  FFN<NegativeLogLikelihood<> > *model = new FFN<NegativeLogLikelihood<> >;
-  model->Add<Linear<> >(trainData.n_rows, 8);
-  model->Add<SigmoidLayer<> >();
-  model->Add<MaxPooling<> >();
-  model->Add<LogSoftMax<> >();
-
-  FFN<NegativeLogLikelihood<> > *model1 = new FFN<NegativeLogLikelihood<> >;
-  model1->Add<Linear<> >(trainData.n_rows, 8);
-  model1->Add<SigmoidLayer<> >();
-  model1->Add<MaxPooling<> >();
-  model1->Add<LogSoftMax<> >();
-
-  // Check whether copy constructor is working or not.
-  CheckCopyFunction<>(model, trainData, trainLabels, 1);
-
-  // Check whether move constructor is working or not.
-  CheckMoveFunction<>(model1, trainData, trainLabels, 1);
-}
-
-/**
  * Check whether copying and moving of Mean Pooling network is working or not.
  */
-TEST_CASE("CheckCopyMovingMaxPoolingNetworkTest", "[FeedForwardNetworkTest]")
+TEST_CASE("CheckCopyMovingMeanPoolingNetworkTest", "[FeedForwardNetworkTest]")
 {
   // Load the dataset.
   arma::mat trainData;
@@ -551,7 +497,7 @@ TEST_CASE("CheckCopyMovingMaxPoolingNetworkTest", "[FeedForwardNetworkTest]")
 /**
  * Check whether copying and moving of Adaptive Max Pooling network is working or not.
  */
-TEST_CASE("CheckCopyMovingMaxPoolingNetworkTest", "[FeedForwardNetworkTest]")
+TEST_CASE("CheckCopyMovingAdaptiveMaxPoolingNetworkTest", "[FeedForwardNetworkTest]")
 {
   // Load the dataset.
   arma::mat trainData;
@@ -605,7 +551,7 @@ TEST_CASE("CheckCopyMovingMaxPoolingNetworkTest", "[FeedForwardNetworkTest]")
 /**
  * Check whether copying and moving of Adaptive Mean Pooling network is working or not.
  */
-TEST_CASE("CheckCopyMovingMaxPoolingNetworkTest", "[FeedForwardNetworkTest]")
+TEST_CASE("CheckCopyMovingAdaptiveMeanPoolingNetworkTest", "[FeedForwardNetworkTest]")
 {
   // Load the dataset.
   arma::mat trainData;
