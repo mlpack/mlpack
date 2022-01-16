b2d61d83ec494f81b9672d5959907328531dc257
VedangAsgaonkar <81141571+VedangAsgaonkar@users.noreply.github.com>
2022-01-16 21:15:45 +0530

    Added size checks in Predict()

--- src/mlpack/methods/linear_regression/linear_regression.cpp
+++ src/mlpack/methods/linear_regression/linear_regression.cpp
@@ -100,7 +100,7 @@ void LinearRegression::Predict(const arma::mat& points,
   {
     // We want to be sure we have the correct number of dimensions in the
     // dataset.
-    Log::Assert(points.n_rows == parameters.n_rows - 1);
+    util::CheckSameDimensionality(points, parameters.n_rows-1, "LinearRegression::Predict()", "points");
     // Get the predictions, but this ignores the intercept value
     // (parameters[0]).
     predictions = arma::trans(parameters.subvec(1, parameters.n_elem - 1))
@@ -112,7 +112,7 @@ void LinearRegression::Predict(const arma::mat& points,
   {
     // We want to be sure we have the correct number of dimensions in
     // the dataset.
-    Log::Assert(points.n_rows == parameters.n_rows);
+    util::CheckSameDimensionality(points, parameters, "LinearRegression::Predict()", "points");
     predictions = arma::trans(parameters) * points;
   }
 }
