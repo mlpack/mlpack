031bb7ac3c5963a9fc0871720f26ff553144a7b5
shubham1206agra <tt1191044@iitd.ac.in>
2022-01-24 23:17:16 +0530

    fix styling issues

--- src/mlpack/methods/preprocess/preprocess_describe_main.cpp
+++ src/mlpack/methods/preprocess/preprocess_describe_main.cpp
@@ -93,11 +93,14 @@ PARAM_FLAG("row_major", "If specified, the program will calculate statistics "
 template<typename ... Args>
 std::string string_format(const std::string& format, Args ... args)
 {
-    size_t size = snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
-    if( size <= 0 ){ throw std::runtime_error( "Error during formatting." ); }
-    std::unique_ptr<char[]> buf( new char[ size ] ); 
-    snprintf( buf.get(), size, format.c_str(), args ... );
-    return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
+    size_t size = snprintf(nullptr, 0, format.c_str(), args ...) + 1;
+    if (size <= 0) 
+    {
+      Log::Fatal << "Error during formatting." << std::endl;
+    }
+    std::unique_ptr<char[]> buf(new char[ size ]); 
+    snprintf(buf.get(), size, format.c_str(), args ...);
+    return std::string(buf.get(), buf.get() + size - 1);
 }
 
 /**
