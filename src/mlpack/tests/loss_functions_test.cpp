46d491bc2f41f4ce640d0ca884af9e6b679ac0a9
Anwaar <shahanwaar.khalid8@gmail.com>
2022-02-04 17:17:46 +0530

    Margin Ranking Loss.

--- src/mlpack/tests/loss_functions_test.cpp
+++ src/mlpack/tests/loss_functions_test.cpp
@@ -74,7 +74,7 @@ TEST_CASE("HuberLossTest", "[LossFunctionsTest]")
 
   // Test the Backward function.
   module.Backward(input, target, output);
-  REQUIRE(arma::as_scalar(arma::accu(output)) == 
+  REQUIRE(arma::as_scalar(arma::accu(output)) ==
                 Approx(-0.8032).epsilon(1e-3));
   REQUIRE(output.n_rows == input.n_rows);
   REQUIRE(output.n_cols == input.n_cols);
@@ -584,7 +584,7 @@ TEST_CASE("SimpleMeanBiasErrorTest", "[LossFunctionsTest]")
 
   input.reshape(4, 3);
   target.reshape(4, 3);
-  
+
   // Test the forward function.
   // Loss should  be 0.1081.
   loss = module.Forward(input, target);
@@ -826,42 +826,51 @@ TEST_CASE("CosineEmbeddingLossTest", "[LossFunctionsTest]")
  */
 TEST_CASE("MarginRankingLossTest", "[LossFunctionsTest]")
 {
-  arma::mat input, input1, input2, target, output;
+  arma::mat input, input1, input2, target, output, expectedOutput;
+  double loss;
+  //  Test sum reduction
   MarginRankingLoss<> module;
-
-  // Test the Forward function on a user generator input and compare it against
-  // the manually calculated result.
-  input1 = arma::mat("1 2 5 7 -1 -3");
-  input2 = arma::mat("-1 3 -4 11 3 -3");
-  input = arma::join_cols(input1, input2);
-  target = arma::mat("1 -1 -1 1 -1 1");
-  double error = module.Forward(input, target);
-  // Computed using torch.nn.functional.margin_ranking_loss()
-  REQUIRE(error == Approx(2.66667).epsilon(1e-3));
-
-  // Test the Backward function.
-  module.Backward(input, target, output);
-
-  CheckMatrices(output, arma::mat("-0.000000 0.166667 -1.500000 0.666667 "
-      "0.000000 -0.000000"), 1e-3);
-  REQUIRE(output.n_rows == target.n_rows);
-  REQUIRE(output.n_cols == target.n_cols);
-
-  // Test the error function on another input.
   input1 = arma::mat("0.4287 -1.6208 -1.5006 -0.4473 1.5208 -4.5184 9.3574 "
       "-4.8090 4.3455 5.2070");
   input2 = arma::mat("-4.5288 -9.2766 -0.5882 -5.6643 -6.0175 8.8506 3.4759 "
       "-9.4886 2.2755 8.4951");
+  expectedOutput << 0.0000 << 0.0000 << 1.0000 << 0.0000 << 1.0000 << -1.0000 
+                 << 0.0000 << 0.0000 << 1.0000 << -1.0000 <<arma::endr << 0.0000
+                 << 0.0000 << -1.0000 << 0.0000 << -1.0000 << 1.0000 << 0.0000
+                 << 0.0000 << -1.0000 << 1.0000 << arma::endr;
   input = arma::join_cols(input1, input2);
   target = arma::mat("1 1 -1 1 -1 1 1 1 -1 1");
-  error = module.Forward(input, target);
-  REQUIRE(error == Approx(3.03530).epsilon(1e-3));
 
-  // Test the Backward function on the second input.
+  // Test the forward function
+  // loss should be 30.3530 
+  // value calculated using torch.nn.MarginRankingLoss(margin=1.0,reduction='sum')
+  loss = module.Forward(input, target);
+  REQUIRE(loss == Approx(30.3530).epsilon(1e-3));
+
+  // Test the Backward function.
   module.Backward(input, target, output);
+  REQUIRE(output.n_rows == input.n_rows);
+  REQUIRE(output.n_cols == input.n_cols);
+  CheckMatrices(output, expectedOutput, 0.1);
 
-  CheckMatrices(output, arma::mat("0.000000 0.000000 0.091240 0.000000 "
-      "-0.753830 1.336900 0.000000 0.000000 -0.207000 0.328810"), 1e-6);
+  // Test for mean reduction by modifying reduction parameter using accessor.
+  module.Reduction() = false;
+  
+  // Test the forward function
+  // loss should be 3.0353
+  // value calculated using torch.nn.MarginRankingLoss(margin=1.0,reduction='mean')
+  loss = module.Forward(input, target);
+  REQUIRE(loss == Approx(3.0353).epsilon(1e-5));
+
+  // Test the backward function
+  module.Backward(input, target, output);
+  expectedOutput << 0.0000 << 0.0000 << 0.1000 << 0.0000 << 0.1000 << -0.1000 
+                 << 0.0000 << 0.0000 << 0.1000 << -0.1000 <<arma::endr << 0.0000
+                 << 0.0000 << -0.1000 << 0.0000 << -0.1000 << 0.1000 << 0.0000
+                 << 0.0000 << -0.1000 << 0.1000 << arma::endr;
+  REQUIRE(output.n_rows == input.n_rows);
+  REQUIRE(output.n_cols == input.n_cols);
+  CheckMatrices(output, expectedOutput, 0.1);
 }
 
 /**
