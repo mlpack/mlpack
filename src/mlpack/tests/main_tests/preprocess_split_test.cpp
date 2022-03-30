330f9b76e8f164b7d255f4ef1f8a4c84d9d5678a
Nabanita Dash <dashnabanita@gmail.com>
2022-03-30 11:31:53 +0530

    Update preprocess_split_test.cpp

--- src/mlpack/tests/main_tests/preprocess_split_test.cpp
+++ src/mlpack/tests/main_tests/preprocess_split_test.cpp
@@ -375,7 +375,7 @@ TEST_CASE_METHOD(
 
   // Now check that the input dataset is not empty.
   Log::Fatal.ignoreInput = true;
-  // REQUIRE_THROWS_AS(RUN_BINDING() , std::unexpected_handler);
-  REQUIRE_THROWS_WITH(RUN_BINDING(), "Mat::max(): object has no elements");
+  REQUIRE_THROWS_AS(RUN_BINDING() , std::unexpected_handler);
+  // REQUIRE_THROWS_WITH(RUN_BINDING(), "Mat::max(): object has no elements");
   Log::Fatal.ignoreInput = false;
 }
