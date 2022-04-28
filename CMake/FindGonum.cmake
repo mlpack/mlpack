ad4569213a5d96625e0ced566451a8d64ea0c2bd
shubham1206agra <tt1191044@iitd.ac.in>
2022-04-28 19:32:14 +0530

    temp sol to version issue

--- CMake/FindGonum.cmake
+++ CMake/FindGonum.cmake
@@ -4,14 +4,19 @@
 if (GO_EXECUTABLE)
   execute_process(
      COMMAND ${GO_EXECUTABLE} list gonum.org/v1/gonum/mat
-     OUTPUT_VARIABLE GONUM_VERSION_STRING
+     OUTPUT_VARIABLE GONUM_RAW_STRING
      RESULT_VARIABLE RESULT
   )
   if (RESULT EQUAL 0)
-    string(REGEX MATCH "([0-9]+\\.[0-9]+\\.[0-9]+[\n]+)"
-        GONUM_VERSION_STRING "${GONUM_VERSION_STRING}")
     string(REGEX REPLACE "\n$" ""
-        GONUM_VERSION_STRING ${GONUM_VERSION_STRING})
+    GONUM_RAW_STRING ${GONUM_RAW_STRING})
+    if ("${GONUM_RAW_STRING}" STREQUAL "gonum.org/v1/gonum/mat")
+      set(GONUM_VERSION_STRING "0.0.1")
+    # string(REGEX MATCH "([0-9]+\\.[0-9]+\\.[0-9]+[\n]+)"
+    #     GONUM_VERSION_STRING "${GONUM_VERSION_STRING}")
+    # string(REGEX REPLACE "\n$" ""
+    #     GONUM_VERSION_STRING ${GONUM_VERSION_STRING})
+    endif()
   endif()
 endif()
 
