018eb427e96860de19c74df2d2a9241c05b031d0
shubham1206agra <tt1191044@iitd.ac.in>
2022-05-10 15:53:39 +0530

    converted fastMKS model to .hpp

--- src/mlpack/methods/fastmks/fastmks_model_impl.hpp
+++ src/mlpack/methods/fastmks/fastmks_model_impl.hpp
@@ -17,6 +17,238 @@
 namespace mlpack {
 namespace fastmks {
 
+inline FastMKSModel::FastMKSModel(const int kernelType) :
+    kernelType(kernelType),
+    linear(NULL),
+    polynomial(NULL),
+    cosine(NULL),
+    gaussian(NULL),
+    epan(NULL),
+    triangular(NULL),
+    hyptan(NULL)
+{
+  // Nothing to do.
+}
+
+inline FastMKSModel::FastMKSModel(const FastMKSModel& other) :
+    kernelType(other.kernelType),
+    linear(other.linear == NULL ? NULL :
+        new FastMKS<kernel::LinearKernel>(*other.linear)),
+    polynomial(other.polynomial == NULL ? NULL :
+        new FastMKS<kernel::PolynomialKernel>(*other.polynomial)),
+    cosine(other.cosine == NULL ? NULL :
+        new FastMKS<kernel::CosineDistance>(*other.cosine)),
+    gaussian(other.gaussian == NULL ? NULL :
+        new FastMKS<kernel::GaussianKernel>(*other.gaussian)),
+    epan(other.epan == NULL ? NULL :
+        new FastMKS<kernel::EpanechnikovKernel>(*other.epan)),
+    triangular(other.triangular == NULL ? NULL :
+        new FastMKS<kernel::TriangularKernel>(*other.triangular)),
+    hyptan(other.hyptan == NULL ? NULL :
+        new FastMKS<kernel::HyperbolicTangentKernel>(*other.hyptan))
+{
+  // Nothing to do.
+}
+
+inline FastMKSModel::FastMKSModel(FastMKSModel&& other) :
+    kernelType(other.kernelType),
+    linear(other.linear),
+    polynomial(other.polynomial),
+    cosine(other.cosine),
+    gaussian(other.gaussian),
+    epan(other.epan),
+    triangular(other.triangular),
+    hyptan(other.hyptan)
+{
+  // Clear other object.
+  other.kernelType = KernelTypes::LINEAR_KERNEL;
+  other.linear = NULL;
+  other.polynomial = NULL;
+  other.cosine = NULL;
+  other.gaussian = NULL;
+  other.epan = NULL;
+  other.triangular = NULL;
+  other.hyptan = NULL;
+}
+
+inline FastMKSModel& FastMKSModel::operator=(const FastMKSModel& other)
+{
+  if (this != &other)
+  {
+    // Clear memory.
+    delete linear;
+    delete polynomial;
+    delete cosine;
+    delete gaussian;
+    delete epan;
+    delete triangular;
+    delete hyptan;
+
+    // Set pointers to null.
+    linear = NULL;
+    polynomial = NULL;
+    cosine = NULL;
+    gaussian = NULL;
+    epan = NULL;
+    triangular = NULL;
+    hyptan = NULL;
+
+    kernelType = other.kernelType;
+    if (other.linear)
+      linear = new FastMKS<kernel::LinearKernel>(*other.linear);
+    if (other.polynomial)
+      polynomial = new FastMKS<kernel::PolynomialKernel>(*other.polynomial);
+    if (other.cosine)
+      cosine = new FastMKS<kernel::CosineDistance>(*other.cosine);
+    if (other.gaussian)
+      gaussian = new FastMKS<kernel::GaussianKernel>(*other.gaussian);
+    if (other.epan)
+      epan = new FastMKS<kernel::EpanechnikovKernel>(*other.epan);
+    if (other.triangular)
+      triangular = new FastMKS<kernel::TriangularKernel>(*other.triangular);
+    if (other.hyptan)
+      hyptan = new FastMKS<kernel::HyperbolicTangentKernel>(*other.hyptan);
+  }
+  return *this;
+}
+
+inline FastMKSModel& FastMKSModel::operator=(FastMKSModel&& other)
+{
+  if (this != &other)
+  {
+    kernelType = other.kernelType;
+    linear = other.linear;
+    polynomial = other.polynomial;
+    cosine = other.cosine;
+    gaussian = other.gaussian;
+    epan = other.epan;
+    triangular = other.triangular;
+    hyptan = other.hyptan;
+
+    // Clear other object.
+    other.kernelType = KernelTypes::LINEAR_KERNEL;
+    other.linear = nullptr;
+    other.polynomial = nullptr;
+    other.cosine = nullptr;
+    other.gaussian = nullptr;
+    other.epan = nullptr;
+    other.triangular = nullptr;
+    other.hyptan = nullptr;
+  }
+  return *this;
+}
+
+inline FastMKSModel::~FastMKSModel()
+{
+  // Clean memory.
+  if (linear)
+    delete linear;
+  if (polynomial)
+    delete polynomial;
+  if (cosine)
+    delete cosine;
+  if (gaussian)
+    delete gaussian;
+  if (epan)
+    delete epan;
+  if (triangular)
+    delete triangular;
+  if (hyptan)
+    delete hyptan;
+}
+
+inline bool FastMKSModel::Naive() const
+{
+  switch (kernelType)
+  {
+    case LINEAR_KERNEL:
+      return linear->Naive();
+    case POLYNOMIAL_KERNEL:
+      return polynomial->Naive();
+    case COSINE_DISTANCE:
+      return cosine->Naive();
+    case GAUSSIAN_KERNEL:
+      return gaussian->Naive();
+    case EPANECHNIKOV_KERNEL:
+      return epan->Naive();
+    case TRIANGULAR_KERNEL:
+      return triangular->Naive();
+    case HYPTAN_KERNEL:
+      return hyptan->Naive();
+  }
+
+  throw std::runtime_error("invalid model type");
+}
+
+inline bool& FastMKSModel::Naive()
+{
+  switch (kernelType)
+  {
+    case LINEAR_KERNEL:
+      return linear->Naive();
+    case POLYNOMIAL_KERNEL:
+      return polynomial->Naive();
+    case COSINE_DISTANCE:
+      return cosine->Naive();
+    case GAUSSIAN_KERNEL:
+      return gaussian->Naive();
+    case EPANECHNIKOV_KERNEL:
+      return epan->Naive();
+    case TRIANGULAR_KERNEL:
+      return triangular->Naive();
+    case HYPTAN_KERNEL:
+      return hyptan->Naive();
+  }
+
+  throw std::runtime_error("invalid model type");
+}
+
+inline bool FastMKSModel::SingleMode() const
+{
+  switch (kernelType)
+  {
+    case LINEAR_KERNEL:
+      return linear->SingleMode();
+    case POLYNOMIAL_KERNEL:
+      return polynomial->SingleMode();
+    case COSINE_DISTANCE:
+      return cosine->SingleMode();
+    case GAUSSIAN_KERNEL:
+      return gaussian->SingleMode();
+    case EPANECHNIKOV_KERNEL:
+      return epan->SingleMode();
+    case TRIANGULAR_KERNEL:
+      return triangular->SingleMode();
+    case HYPTAN_KERNEL:
+      return hyptan->SingleMode();
+  }
+
+  throw std::runtime_error("invalid model type");
+}
+
+inline bool& FastMKSModel::SingleMode()
+{
+  switch (kernelType)
+  {
+    case LINEAR_KERNEL:
+      return linear->SingleMode();
+    case POLYNOMIAL_KERNEL:
+      return polynomial->SingleMode();
+    case COSINE_DISTANCE:
+      return cosine->SingleMode();
+    case GAUSSIAN_KERNEL:
+      return gaussian->SingleMode();
+    case EPANECHNIKOV_KERNEL:
+      return epan->SingleMode();
+    case TRIANGULAR_KERNEL:
+      return triangular->SingleMode();
+    case HYPTAN_KERNEL:
+      return hyptan->SingleMode();
+  }
+
+  throw std::runtime_error("invalid model type");
+}
+
 //! This is called when the KernelType is the same as the model.
 template<typename KernelType>
 void BuildFastMKSModel(util::Timers& timers,
@@ -227,6 +459,78 @@ void FastMKSModel::Search(util::Timers& timers,
     f.Search(&queryTree, k, indices, kernels);
     timers.Stop("computing_products");
   }
+}
+
+inline void FastMKSModel::Search(
+    util::Timers& timers,
+    const arma::mat& querySet,
+    const size_t k,
+    arma::Mat<size_t>& indices,
+    arma::mat& kernels,
+    const double base)
+{
+  switch (kernelType)
+  {
+    case LINEAR_KERNEL:
+      Search(timers, *linear, querySet, k, indices, kernels, base);
+      break;
+    case POLYNOMIAL_KERNEL:
+      Search(timers, *polynomial, querySet, k, indices, kernels, base);
+      break;
+    case COSINE_DISTANCE:
+      Search(timers, *cosine, querySet, k, indices, kernels, base);
+      break;
+    case GAUSSIAN_KERNEL:
+      Search(timers, *gaussian, querySet, k, indices, kernels, base);
+      break;
+    case EPANECHNIKOV_KERNEL:
+      Search(timers, *epan, querySet, k, indices, kernels, base);
+      break;
+    case TRIANGULAR_KERNEL:
+      Search(timers, *triangular, querySet, k, indices, kernels, base);
+      break;
+    case HYPTAN_KERNEL:
+      Search(timers, *hyptan, querySet, k, indices, kernels, base);
+      break;
+    default:
+      throw std::runtime_error("invalid model type");
+  }
+}
+
+inline void FastMKSModel::Search(
+    util::Timers& timers,
+    const size_t k,
+    arma::Mat<size_t>& indices,
+    arma::mat& kernels)
+{
+  timers.Start("computing_products");
+  switch (kernelType)
+  {
+    case LINEAR_KERNEL:
+      linear->Search(k, indices, kernels);
+      break;
+    case POLYNOMIAL_KERNEL:
+      polynomial->Search(k, indices, kernels);
+      break;
+    case COSINE_DISTANCE:
+      cosine->Search(k, indices, kernels);
+      break;
+    case GAUSSIAN_KERNEL:
+      gaussian->Search(k, indices, kernels);
+      break;
+    case EPANECHNIKOV_KERNEL:
+      epan->Search(k, indices, kernels);
+      break;
+    case TRIANGULAR_KERNEL:
+      triangular->Search(k, indices, kernels);
+      break;
+    case HYPTAN_KERNEL:
+      hyptan->Search(k, indices, kernels);
+      break;
+    default:
+      throw std::invalid_argument("invalid model type");
+  }
+  timers.Stop("computing_products");
 }
 
 } // namespace fastmks
