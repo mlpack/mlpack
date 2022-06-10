481dd67a1ebd34e8521b20e690fd1c2c62a7a563
Shubham Agrawal <shubham.agra1206@gmail.com>
2022-06-10 18:35:47 +0530

    size issue logs

--- src/mlpack/methods/ann/layer/lambda_map_reduce_impl.hpp
+++ src/mlpack/methods/ann/layer/lambda_map_reduce_impl.hpp
@@ -301,7 +301,7 @@ void LambdaMapReduceType<ReductionRuleType, MatType>::ComputeOutputDimensions()
   {
     network[i]->InputDimensions() = this->inputDimensions;
     size_t layerOutputSize = network[i]->OutputSize();
-    Log::Info << layerOutputSize << std::endl;
+    Log::Warn << layerOutputSize << std::endl;
     totalOutputSize += layerOutputSize;
   }
 
