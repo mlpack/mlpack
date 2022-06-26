a09b3612fa89dd6cb69e90bbbcf5016a8df991ac
Shubham Agrawal <58412969+shubham1206agra@users.noreply.github.com>
2022-06-26 17:28:27 +0530

    Update adaptive_mean_pooling_impl.hpp
    Fixed Serialization issue.

--- src/mlpack/methods/ann/layer/adaptive_mean_pooling_impl.hpp
+++ src/mlpack/methods/ann/layer/adaptive_mean_pooling_impl.hpp
@@ -146,6 +146,8 @@ void AdaptiveMeanPoolingType<MatType>::serialize(
   ar(cereal::base_class<Layer<MatType>>(this));
 
   ar(CEREAL_NVP(poolingLayer));
+  ar(CEREAL_NVP(outputWidth));
+  ar(CEREAL_NVP(outputHeight));
 }
 
 } // namespace ann
