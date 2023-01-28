e748efcdaa6c03738b023949be555892781dc4cb
Aditya Raj <96882869+aadi-raj@users.noreply.github.com>
2023-01-28 11:30:47 +0530

    Update runtests.jl

--- src/mlpack/bindings/julia/tests/runtests.jl
+++ src/mlpack/bindings/julia/tests/runtests.jl
@@ -2,6 +2,12 @@
 # @author Ryan Curtin
 #
 # Tests for the Julia bindings.
+
+# mlpack is free software; you may redistribute it and/or modify it under the
+# terms of the 3-clause BSD license.  You should have received a copy of the
+# 3-clause BSD license along with mlpack.  If not, see
+# http://www.opensource.org/licenses/BSD-3-Clause for more information.
+
 using Pkg
 Pkg.activate(".")
 using Test
