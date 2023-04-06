ddba35f229f6482ef10e4106eda01bef3c51aa5d
IWNMWE <118750341+IWNMWE@users.noreply.github.com>
2023-04-06 17:15:36 +0000

    error fix

--- src/mlpack/methods/ann/layer/layer_types.hpp
+++ src/mlpack/methods/ann/layer/layer_types.hpp
@@ -33,8 +33,8 @@
 #include <mlpack/methods/ann/layer/elu.hpp>
 #include <mlpack/methods/ann/layer/grouped_convolution.hpp>
 #include <mlpack/methods/ann/layer/hard_tanh.hpp>
-
 #include <mlpack/methods/ann/layer/identity.hpp>
+#include <mlpack/methods/ann/layer/isrlu.hpp>
 #include <mlpack/methods/ann/layer/leaky_relu.hpp>
 #include <mlpack/methods/ann/layer/linear.hpp>
 #include <mlpack/methods/ann/layer/linear_no_bias.hpp>
