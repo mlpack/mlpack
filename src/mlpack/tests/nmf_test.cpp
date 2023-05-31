7af0de4011fb332850f082ec9ee67054e0888f67
Ryan Curtin <ryan@ratml.org>
2023-05-31 15:24:25 -0400

    It's fixed, so now remove the debugging output.

--- src/mlpack/tests/nmf_test.cpp
+++ src/mlpack/tests/nmf_test.cpp
@@ -223,19 +223,9 @@ TEST_CASE("SparseNMFALSTest", "[NMFTest]")
       // Reconstruct matrices.
       vp = w * h; // In general vp won't be sparse.
       dvp = dw * dh;
-      w.print("w");
-      h.print("h");
-      v.print("v");
-      vp.print("vp");
-      dw.print("dw");
-      dh.print("dh");
-      dv.print("dv");
-      dvp.print("dvp");
 
       denseResidue = arma::norm(v - vp, "fro");
       sparseResidue = arma::norm(dv - dvp, "fro");
-      std::cout << "sparse residue " << sparseResidue << std::endl;
-      std::cout << "dense residue " << denseResidue << std::endl;
     }
 
     // Make sure the results are about equal for the W and H matrices.
