e574a6ea65da2aa624479aa5db8a457eeef2728b
satyam <satyamtech42@gmail.com>
2023-07-06 13:36:42 +0300

    Fixed invertable activations / added jacobian tests for them

--- src/mlpack/methods/ann/layer/base_layer.hpp
+++ src/mlpack/methods/ann/layer/base_layer.hpp
@@ -243,6 +243,22 @@ typedef BaseLayer<SILUFunction, arma::mat> SILU;
 template<typename MatType = arma::mat>
 using SILUType = BaseLayer<SILUFunction, MatType>;
 
+/**
+ * Standard Hyper Sinh layer.
+ */
+typedef BaseLayer<HyperSinhFunction, arma::mat> HyperSinh;
+
+template<typename MatType = arma::mat>
+using HyperSinhType = BaseLayer<HyperSinhFunction, MatType>;
+
+/**
+ * Standard Bipolar Sigmoid layer.
+ */
+typedef BaseLayer<BipolarSigmoidFunction, arma::mat> BipolarSigmoid;
+
+template<typename MatType = arma::mat>
+using BipolarSigmoidType = BaseLayer<BipolarSigmoidFunction, MatType>;
+
 } // namespace mlpack
 
 #endif
