5d0325670266c5fac94323a1aaa5a1e7808045de
Ryan Curtin <ryan@ratml.org>
2023-08-16 14:58:09 -0400

    Add more configuration.

--- src/mlpack/config.hpp
+++ src/mlpack/config.hpp
@@ -13,22 +13,56 @@
 #ifndef MLPACK_CONFIG_HPP
 #define MLPACK_CONFIG_HPP
 
+//
+// mlpack has the capability of providing backtraces when errors are encountered
+// using libbfd and libdl.  This support is only available on Linux.  If
+// MLPACK_HAS_BFD_DL is enabled, then when compiling an mlpack program with
+// debugging symbols (e.g. -DDEBUG), you must link with -lbfd and -ldl.
+//
 #ifndef MLPACK_HAS_BFD_DL
-/**
- * 
- */
 // #define MLPACK_HAS_BFD_DL
 #endif
 
+//
+// mlpack provides image loading and saving support via STB, if available.  STB
+// is an optional dependency of mlpack.  When STB is found on a system,
+// MLPACK_HAS_STB will be defined and the files `stb_image.h` and
+// `stb_image_write.h` are expected to be found in the compiler include path.
+//
 #ifndef MLPACK_HAS_STB
 // #define MLPACK_HAS_STB
 #endif
 
+//
+// If the version of mlpack is built from a git repository and is not an
+// official release, then MLPACK_GIT_VERSION will be defined.  This causes
+// mlpack::util::GetVersion() to return the git revision instead of the version
+// number.
+//
 #ifndef MLPACK_GIT_VERSION
 // #define MLPACK_GIT_VERSION
 #endif
 
-// These macros can be defined to disable support.
+//
+// MLPACK_COUT_STREAM is used to change the default stream for printing
+// non-error messages.
+//
+#if !defined(MLPACK_COUT_STREAM)
+ #define MLPACK_COUT_STREAM std::cout
+#endif
+
+//
+// MLPACK_CERR_STREAM is used to change the stream for printing warnings and
+// errors.
+//
+#if !defined(MLPACK_CERR_STREAM)
+ #define MLPACK_CERR_STREAM std::cerr
+#endif
+
+//
+// These macros can be defined to disable support that is defined above.  (This
+// is useful if you cannot or do not want to modify config.hpp.)
+//
 #ifdef MLPACK_DISABLE_STB
   #ifdef MLPACK_HAS_STB
     #undef MLPACK_HAS_STB
