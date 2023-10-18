6f8a0d4c13cf37cb5406d56697bfd77348768c59
Ryan Curtin <ryan@ratml.org>
2023-10-18 19:57:03 -0400

    Apparently it wasn't needed to add this overload.

--- src/mlpack/methods/decision_tree/decision_tree.hpp
+++ src/mlpack/methods/decision_tree/decision_tree.hpp
@@ -206,37 +206,6 @@ class DecisionTree :
           typename std::remove_reference<WeightsType>::type>::value>* = 0);
 
   /**
-   * Construct the decision tree on the given data and labels with weights,
-   * assuming that the data is all of the numeric type. Setting minimumLeafSize
-   * and minimumGainSplit too small may cause the tree to overfit, but setting
-   * them too large may cause it to underfit.
-   *
-   * Use std::move if data, labels or weights are no longer needed to avoid
-   * copies.
-   *
-   * @param data Dataset to train on.
-   * @param labels Labels for each training point.
-   * @param numClasses Number of classes in the dataset.
-   * @param weights The Weight list of given labels.
-   * @param minimumLeafSize Minimum number of points in each leaf node.
-   * @param minimumGainSplit Minimum gain for the node to split.
-   * @param maximumDepth Maximum depth for the tree.
-   * @param dimensionSelector Instantiated dimension selection policy.
-   */
-  template<typename MatType, typename LabelsType, typename WeightsType>
-  DecisionTree(
-      MatType data,
-      LabelsType labels,
-      const size_t numClasses,
-      WeightsType weights,
-      const size_t minimumLeafSize = 10,
-      const double minimumGainSplit = 1e-7,
-      const size_t maximumDepth = 0,
-      DimensionSelectionType dimensionSelector = DimensionSelectionType(),
-      const std::enable_if_t<arma::is_arma_type<
-          typename std::remove_reference<WeightsType>::type>::value>* = 0);
-
-  /**
    * Take ownership of another decision tree and train on the given data and
    * labels with weights, assuming that the data is all of the numeric type.
    * Setting minimumLeafSize and minimumGainSplit too small may cause the tree
