aec1c16161e9e96a475aeeeeeabba2785c28e02e
Adam Kropp <30504149+akropp@users.noreply.github.com>
2023-11-01 17:30:20 -0400

    Update src/mlpack/methods/ann/layer/add.hpp
    Co-authored-by: Ryan Curtin <ryan@ratml.org>

--- src/mlpack/methods/ann/layer/add.hpp
+++ src/mlpack/methods/ann/layer/add.hpp
@@ -66,7 +66,7 @@ class AddType : public Layer<MatType>
    * @param g The calculated gradient.
    */
   void Backward(const MatType& /* input */,
-                const MatType& output,
+                const MatType& /* output */,
                 const MatType& gy,
                 MatType& g);
 
