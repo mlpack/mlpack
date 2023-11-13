4244a337a91a564a27b82bd0a49988ceae2d0c5c
Adam Kropp <30504149+akropp@users.noreply.github.com>
2023-11-13 15:27:46 -0500

    Update src/mlpack/methods/ann/activation_functions/spline_function.hpp
    Co-authored-by: Ryan Curtin <ryan@ratml.org>

--- src/mlpack/methods/ann/activation_functions/spline_function.hpp
+++ src/mlpack/methods/ann/activation_functions/spline_function.hpp
@@ -70,7 +70,9 @@ class SplineFunction
    * @param dy The resulting derivatives.
    */
   template<typename InputVecType, typename OutputVecType, typename DerivVecType>
-  static void Deriv(const InputVecType& x, const OutputVecType& /* y */, DerivVecType &dy)
+  static void Deriv(const InputVecType& x,
+                    const OutputVecType& /* y */,
+                    DerivVecType& dy)
   {
     dy = 2 * x % arma::log(1 + x) + arma::pow(x, 2) / (1 + x);
   }
