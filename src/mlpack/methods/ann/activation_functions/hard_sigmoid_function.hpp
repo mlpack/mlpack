18bac822679fc65af695442d1250633c05110daa
Adam Kropp <30504149+akropp@users.noreply.github.com>
2023-11-13 14:29:12 -0500

    Update src/mlpack/methods/ann/activation_functions/hard_sigmoid_function.hpp
    Co-authored-by: Ryan Curtin <ryan@ratml.org>

--- src/mlpack/methods/ann/activation_functions/hard_sigmoid_function.hpp
+++ src/mlpack/methods/ann/activation_functions/hard_sigmoid_function.hpp
@@ -84,7 +84,9 @@ class HardSigmoidFunction
    * @param dy The resulting derivatives.
    */
   template<typename InputVecType, typename OutputVecType, typename DerivVecType>
-  static void Deriv(const InputVecType& x, const OutputVecType& y, DerivVecType& dy)
+  static void Deriv(const InputVecType& x,
+                    const OutputVecType& y,
+                    DerivVecType& dy)
   {
     dy.set_size(size(y));
 
