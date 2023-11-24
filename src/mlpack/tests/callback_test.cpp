ce2836d7b84ca793235a2cb9d6e10d20a8c11400
Ryan Curtin <ryan@ratml.org>
2023-11-24 12:20:44 -0500

    Test new functions for SoftmaxRegression, and don't use deprecated functions.

--- src/mlpack/tests/callback_test.cpp
+++ src/mlpack/tests/callback_test.cpp
@@ -209,8 +209,9 @@ TEST_CASE("SRWithOptimizerCallback", "[CallbackTest]")
   ens::StandardSGD sgd(0.1, 1, 5);
   std::stringstream stream;
   // Train softmax regression object.
-  SoftmaxRegression<> sr(data, labels, numClasses, lambda);
-  sr.Train(data, labels, numClasses, sgd, ens::ProgressBar(70, stream));
+  SoftmaxRegression<> sr;
+  sr.Train(data, labels, numClasses, sgd, lambda, true,
+      ens::ProgressBar(70, stream));
 
   REQUIRE(stream.str().length() > 0);
 }
