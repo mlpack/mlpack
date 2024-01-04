550541df0f553dc40175d647cc005534cd4fbedc
Omar Shrit <omar@avontech.fr>
2024-01-04 19:10:23 +0100

    Fix the author name
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- src/mlpack/core/math/safe_linalg.hpp
+++ src/mlpack/core/math/safe_linalg.hpp
@@ -1,19 +1,18 @@
 /**
- * @file core/math/mat_redef.hpp
+ * @file core/math/safe_linalg.hpp
+ * @author Omar Shrit
+ * @author Ryan Curtin
  *
  * A shim around arma and coot functions to avoid confusion with standard 
  * library functions. This is necessary as we are using ADL to allow the
- * compiler to deduce to which library functions belong without the need
- * for namespace. This is mostly needed for MSVC compiiler, gcc seems to
+ * compiler to deduce which library the functions belong without the need
+ * for namespace. This is mostly needed for MSVC compiler, gcc seems to
  * pass without an issue.
  *
  * mlpack is free software; you may redistribute it and/or modify it under the
  * terms of the 3-clause BSD license.  You should have received a copy of the
  * 3-clause BSD license along with mlpack.  If not, see
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
- *
- * Author: Omar Shrit
- * Author: Ryan Curtin
  */
 #ifndef MLPACK_CORE_MAT_REDEF_HPP
 #define MLPACK_CORE_MAT_REDEF_HPP
