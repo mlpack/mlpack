f8a1936e8149480f862f909a478cd5b32c4224a2
Omar Shrit <omar@shrit.me>
2024-01-12 18:21:54 +0100

    Update src/mlpack/methods/lsh/lsh_search_impl.hpp
    Co-authored-by: Ryan Curtin <ryan@ratml.org>

--- src/mlpack/methods/lsh/lsh_search_impl.hpp
+++ src/mlpack/methods/lsh/lsh_search_impl.hpp
@@ -271,7 +271,7 @@ void LSHSearch<SortPolicy, MatType>::Train(MatType referenceSet,
     // point is obtained as:
     // key = { floor((<proj_i, point> + offset_i) / 'hashWidth') forall i }
     arma::mat offsetMat = repmat(offsets.unsafe_col(i), 1,
-                                       this->referenceSet.n_cols);
+                                 this->referenceSet.n_cols);
     arma::mat hashMat = projections.slice(i).t() * (this->referenceSet);
     hashMat += offsetMat;
     hashMat /= hashWidth;
