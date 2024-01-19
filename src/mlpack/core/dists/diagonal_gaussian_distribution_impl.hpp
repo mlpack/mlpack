885430384b7328ddb86bfee396fbf115a9f0f2e1
Omar Shrit <omar@avontech.fr>
2024-01-19 13:15:27 +0100

    Change arma::sum to sum using adl in core/
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- src/mlpack/core/dists/diagonal_gaussian_distribution_impl.hpp
+++ src/mlpack/core/dists/diagonal_gaussian_distribution_impl.hpp
@@ -84,11 +84,11 @@ inline void DiagonalGaussianDistribution::Train(const arma::mat& observations)
   }
 
   // Calculate and normalize the mean.
-  mean = arma::sum(observations, 1) / observations.n_cols;
+  mean = sum(observations, 1) / observations.n_cols;
 
   // Now calculate the covariance.
   const arma::mat diffs = observations.each_col() - mean;
-  covariance += arma::sum(diffs % diffs, 1);
+  covariance += sum(diffs % diffs, 1);
 
   // Finish estimating the covariance by normalizing, with the (1 / (n - 1))
   // to make the estimator unbiased.
