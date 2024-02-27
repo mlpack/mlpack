3433619e4272979f5d5b011ace6fe1368f658f42
Viswanath <157232225+ViswanathBalla22@users.noreply.github.com>
2024-02-27 19:26:01 +0000

    changes

--- src/mlpack/methods/decision_tree/mdl_penalty.hpp
+++ src/mlpack/methods/decision_tree/mdl_penalty.hpp
@@ -7,6 +7,8 @@
 namespace mlpack {
 namespace tree {
 
+template<typename FitnessFunction>
+class MDLPenalty {
  public:
   MDLPenalty(const FitnessFunction& fitnessFunction);
   double operator()(const arma::vec& childCounts,
