9c9d977c083a668038c9a56cce81156751eb4cd9
Omar Shrit <omar@shrit.me>
2024-02-28 12:33:30 +0100

    Update src/mlpack/tests/ann/loss_functions_test.cpp
    Co-authored-by: Ryan Curtin <ryan@ratml.org>

--- src/mlpack/tests/ann/loss_functions_test.cpp
+++ src/mlpack/tests/ann/loss_functions_test.cpp
@@ -1118,8 +1118,7 @@ TEST_CASE("MultiLabelSoftMarginLossTest", "[LossFunctionsTest]")
 
   // Test the Backward function.
   module1.Backward(input, target, output);
-  REQUIRE(arma::as_scalar(accu(output)) ==
-      Approx(0.505909).epsilon(1e-5));
+  REQUIRE(accu(output) == Approx(0.505909).epsilon(1e-5));
   REQUIRE(output.n_rows == input.n_rows);
   REQUIRE(output.n_cols == input.n_cols);
   CheckMatrices(output, expectedOutput, 0.1);
