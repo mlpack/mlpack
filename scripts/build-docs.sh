5169369f22fd3b527833e4a4f96735ea86da8f6d
Ryan Curtin <ryan@ratml.org>
2024-03-07 12:06:43 -0500

    Add a sidebar to the documentation, along with a script to automatically build it.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -18,6 +18,7 @@ fi
 
 template_html_header="${output_dir}/template.html.header";
 template_html_footer="${output_dir}/template.html.footer";
+template_html_sidebar="${output_dir}/template.html.sidebar";
 
 if ! command -v kramdown &>/dev/null
 then
@@ -117,7 +118,17 @@ run_kramdown()
       $input_file.tmp > "$output_file.tmp" || exit 1;
   cat "$template_html_header" | sed "s|LINKROOT|$link_root|" > "$output_file";
 
+  # Create the sidebar.  Extract anchors from the page, unless we are looking at
+  # index.md, since the permanent part of the sidebar links all over index.md
+  # anyway.
+  cat "$template_html_sidebar" | sed "s|LINKROOT|$link_root|" >> "$output_file";
+  if [[ $input_file != "doc/index.md" ]];
+  then
+    create_page_sidebar_section "$output_file.tmp" "$output_file" "$dir_name";
+  fi
+
   # Add clickable anchors to h2 and h3 headers.
+  echo "<div id=\"content\">" >> "$output_file";
   sed -E 's/<h([23]) id="([^"]*)">/<h\1 id="\2"><a href="#\2" class="pl">🔗<\/a> /' "$output_file.tmp" >> "$output_file";
 
   # Simple postprocessing to make tidy a little happier.
@@ -151,12 +162,126 @@ EOF
 # Create the template footer.
 create_template_footer()
 {
-  output_file=$1;
+  output_file="$1";
 
   cat > "$output_file" << EOF
+</div>
 </body>
 </html>
 EOF
+}
+
+# Extract anchors to build a sidebar.
+# This should take the input HTML (before anchor elements are added), and it
+# appends a sidebar list to the output HTML.
+create_page_sidebar_section()
+{
+  input_file="$1";
+  output_file="$2";
+  dir_name="$3"; # The directory containing the documentation.
+  input_file_base=`basename "$input_file" .html.tmp`;
+
+  # Extract h2/h3 anchors into a list.  For individual method documentation, we
+  # only extract h3 anchors because those use h2s as their headings.  And, for
+  # core.md, we want to extract both h2 and h3 anchors.
+  if [[ "$dir_name" == "user/methods" ]];
+  then
+    # The page title on individual methods is encoded as an h2.
+    page_title=`grep '<h2 id=' "$input_file" |\
+        head -1 |\
+        sed 's/^<h2 id="[^"]*">\(.*\)<\/h2>/\1/'`;
+
+    grep '<h3 id=' "$input_file" | sed 's/<h3 id="\([^"]*\)">\(.*\)<\/h3>/<li><a href="#\1">\2<\/a><\/li>/' > "$output_file.side.tmp";
+  elif [[ "$input_file_base" == "core" ]];
+  then
+    # The page title on the core class documentation page is encoded as an h1.
+    page_title=`grep '<h1 id=' "$input_file" |\
+        head -1 |\
+        sed 's/^<h1 id="[^"]*">\(.*\)<\/h1>/\1/'`;
+
+    # We want to collect h2s and h3s as individual documentation; each h2 should
+    # have a summary/details block.  This is a little tedious to create... we'll
+    # do this by creating a temporary tab-separated file with lines like
+    #
+    # h2  anchor_name   Anchor Title
+    # h3  anchor_name   Anchor Title
+    # ...
+    #
+    # and then we'll construct the actual sidebar using that list.
+    grep '<h[23] id=' "$input_file" |\
+        sed 's/^<\(h[23]\) id="\([^"]*\)">\(.*\)<\/h[23]>/\1\t\2\t\3/' > "$output_file.side.list.tmp";
+    cat "$output_file.side.list.tmp";
+    in_block=0;
+    while read line; do
+      # First, extract the pieces of each line.
+      line_type=`echo "$line"    | awk -F'\t' '{ print $1 }'`;
+      anchor_name=`echo "$line"  | awk -F'\t' '{ print $2 }'`;
+      anchor_title=`echo "$line" | awk -F'\t' '{ print $3 }'`;
+
+      # For an h2, we have to print a summary block.
+      # Note that this assumes that *all* h2s have h3 children.  If that's not
+      # true, some extra processing will be needed.
+      if [ "$line_type" = "h2" ];
+      then
+        if [ "$in_block" = "1" ];
+        then
+          # We have to close the previous block.
+          echo "</ul></details></li>" >> "$output_file.side.tmp";
+        fi
+
+        # Create the new details block.
+        echo "<li><details><summary>" >> "$output_file.side.tmp";
+        echo "<a href=\"#$anchor_name\">" >> "$output_file.side.tmp";
+        echo "$anchor_title" >> "$output_file.side.tmp";
+        echo "</a>" >> "$output_file.side.tmp";
+        echo "</summary>" >> "$output_file.side.tmp";
+        echo "<ul>" >> "$output_file.side.tmp";
+        in_block=1;
+      else
+        echo "  <li><a href=\"#$anchor_name\">" >> "$output_file.side.tmp";
+        echo "  $anchor_title" >> "$output_file.side.tmp";
+        echo "  </a></li>" >> "$output_file.side.tmp";
+      fi
+    done < "$output_file.side.list.tmp";
+
+    # Close the last h2 block, if we need to.
+    if [ "$in_block" = "1" ];
+    then
+      echo "</ul></details></li>" >> "$output_file.side.tmp";
+    fi
+
+    rm -f "$output_file.side.list.tmp";
+  else
+    # On other pages, the page title is encoded as an h1.
+    page_title=`grep '<h1 id=' "$input_file" |\
+        head -1 |\
+        sed 's/^<h1 id="[^"]*">\(.*\)<\/h1>/\1/'`;
+
+    grep '<h2 id=' "$input_file" | sed 's/<h2 id="\([^"]*\)">\(.*\)<\/h2>/<li><a href="#\1">\2<\/a><\/li>/' > "$output_file.side.tmp";
+  fi
+  lines=`cat "$output_file.side.tmp" | wc -l`;
+
+  echo "<ul>" >> "$output_file";
+
+  # Make the top of the sidebar.
+  if [ -n "$page_title" ];
+  then
+    echo "<li class=\"page_title\"><b>$page_title</b> <a href=\"#\">[top]</a>" >> "$output_file";
+  else
+    echo "<li><a href=\"#\">[top of page]</a>" >> "$output_file";
+  fi
+
+  if [[ "$lines" -gt 0 ]];
+  then
+    echo "<ul>" >> "$output_file";
+    cat "$output_file.side.tmp" >> "$output_file";
+    echo "</ul>" >> "$output_file";
+  fi
+  echo "</li>" >> "$output_file";
+  echo "</ul>" >> "$output_file";
+  echo "</div>" >> "$output_file";
+
+  rm -f "$output_file.side.tmp";
 }
 
 rm -rf "$output_dir";
@@ -170,6 +295,7 @@ cp doc/tutorials/res/* "$output_dir/tutorials/res/";
 # Create the template files we will use.
 create_template_header "$template_html_header";
 create_template_footer "$template_html_footer";
+cp doc/sidebar.html "$template_html_sidebar";
 
 # Process all the .md files.
 for f in README.md `find ./doc/ -iname '*.md'`;
