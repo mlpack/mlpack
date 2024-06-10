60376de1ba8053eb25ae4f5600279148aa6129b0
Omar Shrit <omar@avontech.fr>
2024-06-10 13:02:24 +0200

    Add const to static
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- src/mlpack/core/util/using.hpp
+++ src/mlpack/core/util/using.hpp
@@ -109,11 +109,11 @@ namespace mlpack {
     template<typename MatType>
     struct GetFillType
     {
-      static decltype(arma::fill::none) none = arma::fill::none;
-      static decltype(arma::fill::zeros) zeros = arma::fill::zeros;
-      static decltype(arma::fill::ones) ones = arma::fill::ones;
-      static decltype(arma::fill::randu) randu = arma::fill::randu;
-      static decltype(arma::fill::randn) randn = arma::fill::randn;
+      static const decltype(arma::fill::none) none = arma::fill::none;
+      static const decltype(arma::fill::zeros) zeros = arma::fill::zeros;
+      static const decltype(arma::fill::ones) ones = arma::fill::ones;
+      static const decltype(arma::fill::randu) randu = arma::fill::randu;
+      static const decltype(arma::fill::randn) randn = arma::fill::randn;
     };
 
   #ifdef MLPACK_HAS_COOT
@@ -122,11 +122,11 @@ namespace mlpack {
              typename = typename std::enable_if<is_coot_type<MatType>::value>::type*>
     struct GetFillType
     {
-      static decltype(coot::fill::none) none = coot::fill::none;
-      static decltype(coot::fill::zeros) zeros = coot::fill::zeros;
-      static decltype(coot::fill::ones) ones = coot::fill::ones;
-      static decltype(coot::fill::randu) randu = coot::fill::randu;
-      static decltype(coot::fill::randn) randn = coot::fill::randn;
+      static const decltype(coot::fill::none) none = coot::fill::none;
+      static const decltype(coot::fill::zeros) zeros = coot::fill::zeros;
+      static const decltype(coot::fill::ones) ones = coot::fill::ones;
+      static const decltype(coot::fill::randu) randu = coot::fill::randu;
+      static const decltype(coot::fill::randn) randn = coot::fill::randn;
     };
   #endif
 
