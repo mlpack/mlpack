94c2b2af29a9ab989f059ce74224cc1eeb9fb866
Abhimanyu Dayal <abhimanyudayal1@gmail.com>
2024-06-10 16:18:11 +0530

    Added a test case for weak learner training method. Added assertions to test cases.

--- src/mlpack/tests/grad_boosting_test.cpp
+++ src/mlpack/tests/grad_boosting_test.cpp
@@ -23,10 +23,10 @@ using namespace data;
 
 /**
  * This test case runs the GradBoosting model on the UCI Iris dataset. 
- * Tests if the model gives a valid output given only the data, without 
- * initiating a weak learner.
+ * Tests if the model gives a training accuracy > 60, without 
+ * initiating a weak learner. Used default empty constructor.
 */
-TEST_CASE("GBUninitiatedTest", "[GradBoostGeneralTest]") 
+TEST_CASE("GBTrainMethod1", "[GradBoostGeneralTest]") 
 {
   arma::mat db;
   if (!data::Load("iris.csv", db))
@@ -36,28 +36,38 @@ TEST_CASE("GBUninitiatedTest", "[GradBoostGeneralTest]")
   if (!data::Load("iris_labels.txt", labels))
     FAIL("Cannot load labels for iris iris_labels.txt");
 
-  const size_t numClasses = max(labels.row(0)) + 1;
-  const size_t numModels = 10;
+  const size_t numClasses = arma::max(labels.row(0)) + 1;
+  const size_t numModels = 5;
 
   GradBoosting gb;
 
   gb.Train(db, labels, numClasses, numModels);
 
-  arma::mat testDb(db.n_rows, db.n_cols, arma::fill::randu);
   arma::Row<size_t> predictions;
+  gb.Classify(db, predictions);
 
-  gb.Classify(testDb, predictions);
+  double accuracy = 0;
+  for (size_t i = 0; i < labels.n_elem; i++) 
+  {
+    if(labels(i) == predictions(i)) 
+    {
+      accuracy++;
+    }
+  }
 
-  predictions.print();
+  accuracy = accuracy / ((double) labels.n_elem);
+  accuracy *= 100.0;
+
+  REQUIRE(accuracy > 60);
 
 }
 
 /**
  * This test case runs the GradBoosting model on the UCI Iris dataset.
- * Tests if the model gives a valid output given the data and a pre-initiated 
- * weak learner.
+ * Tests if the model gives a training accuracy > 60 given the data and a 
+ * pre-initiated weak learner. Used default empty constructor.
 */
-TEST_CASE("GBInitiatedTest", "[GradBoostGeneralTest]") 
+TEST_CASE("GBTrainMethod2", "[GradBoostGeneralTest]") 
 {
   arma::mat db;
   if (!data::Load("iris.csv", db))
@@ -67,19 +77,86 @@ TEST_CASE("GBInitiatedTest", "[GradBoostGeneralTest]")
   if (!data::Load("iris_labels.txt", labels))
     FAIL("Cannot load labels for iris iris_labels.txt");
 
-  const size_t numClasses = max(labels.row(0)) + 1;
-  const size_t numModels = 10;
-  ID3DecisionStump tree;
+  const size_t numClasses = arma::max(labels.row(0)) + 1;
+  const size_t numModels = 5;
+  mlpack::DecisionTree<GiniGain,
+                        BestBinaryNumericSplit,
+                        AllCategoricalSplit,
+                        AllDimensionSelect,
+                        false> tree;
 
   GradBoosting gb;
 
   gb.Train(db, labels, numClasses, numModels, tree);
 
-  arma::mat testDb(db.n_rows, db.n_cols, arma::fill::randu);
   arma::Row<size_t> predictions;
+  gb.Classify(db, predictions);
 
-  gb.Classify(testDb, predictions);
+  double accuracy = 0;
+  for (size_t i = 0; i < labels.n_elem; i++) 
+  {
+    if(labels(i) == predictions(i)) 
+    {
+      accuracy++;
+    }
+  }
 
-  predictions.print();
+  accuracy = accuracy / ((double) labels.n_elem);
+  accuracy *= 100.0;
+
+  REQUIRE(accuracy > 60);
+
+}
+
+/**
+ * This test case runs the GradBoosting model on the UCI Iris dataset.
+ * Tests if the model gives a training accuracy > 60 given the data and a 
+ * weak learner arguments. Used default empty constructor.
+*/
+TEST_CASE("GBTrainMethod3", "[GradBoostGeneralTest]") 
+{
+  arma::mat db;
+  if (!data::Load("iris.csv", db))
+    FAIL("Cannot load test dataset iris.csv!");
+  
+  arma::Row<size_t> labels;
+  if (!data::Load("iris_labels.txt", labels))
+    FAIL("Cannot load labels for iris iris_labels.txt");
+
+  const size_t numClasses = arma::max(labels.row(0)) + 1;
+  const size_t numModels = 5;
+
+  const size_t minimumLeafSize=10;
+  const double minimumGainSplit=1e-7;
+  const size_t maximumDepth=2;
+  const AllDimensionSelect dimensionSelector;
+
+  GradBoosting gb;
+
+  gb.Train(db, 
+            labels, 
+            numClasses, 
+            numModels, 
+            minimumLeafSize,
+            minimumGainSplit,
+            maximumDepth,
+            dimensionSelector);
+
+  arma::Row<size_t> predictions;
+  gb.Classify(db, predictions);
+
+  double accuracy = 0;
+  for (size_t i = 0; i < labels.n_elem; i++) 
+  {
+    if(labels(i) == predictions(i)) 
+    {
+      accuracy++;
+    }
+  }
+
+  accuracy = accuracy / ((double) labels.n_elem);
+  accuracy *= 100.0;
+
+  REQUIRE(accuracy > 60);
 
 }
