76e7bbc37508a9af1ffe0c644e7591f4413cfc2e
Andrew Furey <andrewfurey2003@gmail.com>
2024-06-28 18:48:19 +0100

    updated tests

--- src/mlpack/tests/ann/layer/nearest_interpolation.cpp
+++ src/mlpack/tests/ann/layer/nearest_interpolation.cpp
@@ -22,43 +22,43 @@ using namespace mlpack;
 /**
  * Simple test for the NearestInterpolation layer
  */
-TEST_CASE("SimpleNearestInterpolationLayerTest", "[ANNLayerTest]")
+TEST_CASE("NearestInterpolationLayerTest", "[ANNLayerTest]")
 {
-  // Tested output against torch.nn.Upsample(mode="nearest").
   arma::mat input, output, unzoomedOutput, expectedOutput;
-  size_t inRowSize = 2;
+
   size_t inColSize = 2;
-  size_t outRowSize = 5;
-  size_t outColSize = 7;
-  size_t depth = 1;
-  input.zeros(inRowSize * inColSize * depth, 1);
+  size_t inRowSize = 2;
+  size_t channels = 1;
+
+  double scaleFactor = 2.0f;
+
+  input.zeros(channels, inRowSize * inColSize);
+  output.zeros(channels, inRowSize * scaleFactor * inColSize * scaleFactor);
+  unzoomedOutput = input;
   input[0] = 1.0;
-  input[1] = 3.0;
-  input[2] = 2.0;
+  input[1] = 2.0;
+  input[2] = 3.0;
   input[3] = 4.0;
-  NearestInterpolation layer(inRowSize, inColSize, outRowSize,
-      outColSize, depth);
 
-  expectedOutput << 1.0000 << 1.0000 << 1.0000 << 1.0000 << 2.0000
-                 << 2.0000 << 2.0000 << arma::endr
-                 << 1.0000 << 1.0000 << 1.0000 << 1.0000 << 2.0000
-                 << 2.0000 << 2.0000 << arma::endr
-                 << 1.0000 << 1.0000 << 1.0000 << 1.0000 << 2.0000
-                 << 2.0000 << 2.0000 << arma::endr
-                 << 3.0000 << 3.0000 << 3.0000 << 3.0000 << 4.0000
-                 << 4.0000 << 4.0000 << arma::endr
-                 << 3.0000 << 3.0000 << 3.0000 << 3.0000 << 4.0000
-                 << 4.0000 << 4.0000 << arma::endr;
-  expectedOutput.reshape(35, 1);
+  mlpack::NearestInterpolation layer({scaleFactor, scaleFactor});
 
+  layer.InputDimensions() = { channels, inRowSize, inColSize };
+  layer.ComputeOutputDimensions();
+  
+  expectedOutput  << 1.0000 << 1.0000 << 2.0000 << 2.0000
+		  << 1.0000 << 1.0000 << 2.0000 << 2.0000
+		  << 3.0000 << 3.0000 << 4.0000 << 4.0000
+		  << 3.0000 << 3.0000 << 4.0000 << 4.0000 << arma::endr;
+
+  expectedOutput.reshape(1, 16);
   layer.Forward(input, output);
   CheckMatrices(output - expectedOutput,
                 arma::zeros(output.n_rows), 1e-4);
 
   expectedOutput.clear();
-  expectedOutput << 12.0000 << 18.0000 << arma::endr
-                 << 24.0000 << 24.0000 << arma::endr;
-  expectedOutput.reshape(4, 1);
+  expectedOutput << 4.0000 << 8.0000
+                 << 12.0000 << 16.0000 << arma::endr;
+  expectedOutput.reshape(1, 4);
   layer.Backward(output, output, unzoomedOutput);
   CheckMatrices(unzoomedOutput - expectedOutput,
       arma::zeros(input.n_rows), 1e-4);
@@ -66,13 +66,16 @@ TEST_CASE("SimpleNearestInterpolationLayerTest", "[ANNLayerTest]")
   arma::mat input1, output1, unzoomedOutput1, expectedOutput1;
   inRowSize = 2;
   inColSize = 3;
-  outRowSize = 17;
-  outColSize = 23;
+
   input1 << 1 << 2 << 3 << arma::endr
          << 4 << 5 << 6 << arma::endr;
-  input1.reshape(6, 1);
-  NearestInterpolation layer1(inRowSize, inColSize, outRowSize,
-      outColSize, depth);
+  input1.reshape(1, 6);
+  output1.zeros(1, 17*23);
+  unzoomedOutput1.zeros(1, 6);
+  mlpack::NearestInterpolation layer1({17/2.0f, 23/3.0f});
+
+  layer1.InputDimensions() = { channels, 2, 3 };
+  layer1.ComputeOutputDimensions();
 
   layer1.Forward(input1, output1);
   layer1.Backward(output1, output1, unzoomedOutput1);
