0dfcaeebf4a323ae243924a4980a0d053a218635
Abhimanyu Dayal <abhimanyudayal1@gmail.com>
2024-07-03 16:58:11 +0530

    started work on xgbtree

---
+++ src/mlpack/methods/xgboost/xgbtree/node.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file methods/xgboost/xgbtree/node.hpp
+ * @author Abhimanyu Dayal
+ *
+ * A generic decision tree learner.  Its behavior can be controlled via template
+ * arguments.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_METHODS_XGBTREE_NODE_HPP
+#define MLPACK_METHODS_XGBTREE_NODE_HPP
+
