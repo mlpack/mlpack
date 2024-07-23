44947f083729107cd79dfa64bb0fb3ed3a19b933
Abhimanyu Dayal <abhimanyudayal1@gmail.com>
2024-07-23 12:55:54 +0530

    fixed prereqs

--- src/mlpack/methods/xgboost/xgbtree/dt_prereq.hpp
+++ src/mlpack/methods/xgboost/xgbtree/dt_prereq.hpp
@@ -1,12 +1,3 @@
-// to be edited later
-
-#include <mlpack/methods/decision_tree/all_categorical_split.hpp>
-#include <mlpack/methods/decision_tree/all_dimension_select.hpp>
-#include <mlpack/methods/decision_tree/best_binary_numeric_split.hpp>
-#include <mlpack/methods/decision_tree/gini_gain.hpp>
-#include <mlpack/methods/decision_tree/information_gain.hpp>
-#include <mlpack/methods/decision_tree/mad_gain.hpp>
-#include <mlpack/methods/decision_tree/mse_gain.hpp>
-#include <mlpack/methods/decision_tree/multiple_random_dimension_select.hpp>
-#include <mlpack/methods/decision_tree/random_binary_numeric_split.hpp>
-#include <mlpack/methods/decision_tree/random_dimension_select.hpp>
+#include <mlpack/methods/decision_tree/fitness_functions/fitness_functions.hpp>
+#include <mlpack/methods/decision_tree/select_functions/select_functions.hpp>
+#include <mlpack/methods/decision_tree/split_functions/split_functions.hpp>
