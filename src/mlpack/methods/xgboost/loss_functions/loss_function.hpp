4edb38c88726fefdeaaded2718418cbc9537c84b
Abhimanyu Dayal <abhimanyudayal1@gmail.com>
2024-07-28 18:37:39 +0530

    started work on customisable loss function

---
+++ src/mlpack/methods/xgboost/loss_functions/loss_function.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file methods/xgboost/loss_functions/loss_functions.hpp
+ * @author Abhimanyu Dayal
+ *
+ * Interface for customisable loss functions. Based on the user's choice, a 
+ * specific 
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+
+#include "log_loss.hpp"
+#include "sse_loss.hpp"
+
+#ifndef MLPACK_METHODS_XGBOOST_LOSS_FUNCTIONS_HPP
+#define MLPACK_METHODS_XGBOOST_LOSS_FUNCTIONS_HPP
+
+namespace mlpack {
+
+} // namespace mlpack
+
+#endif
+
