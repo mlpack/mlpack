485e6bf008dedcdb45ed45e996eddfa07b08687d
Abhimanyu Dayal <abhimanyudayal1@gmail.com>
2024-08-09 19:52:23 +0530

    added xgb test cases

--- src/mlpack/tests/xgboost_test.cpp
+++ src/mlpack/tests/xgboost_test.cpp
@@ -70,7 +70,7 @@ TEST_CASE("SSEGainTest", "[XGBTest]")
  * Tests if the model gives a training accuracy > 60, without 
  * initiating a weak learner. Used default empty constructor.
 */
-TEST_CASE("XGBIrisTrainMethod1", "[XGBTest]") 
+TEST_CASE("XGBIrisTrainMethod1", "[XGBoostGeneralTest]") 
 {
   arma::mat db;
   if (!data::Load("iris.csv", db))
@@ -83,46 +83,6 @@ TEST_CASE("XGBIrisTrainMethod1", "[XGBTest]")
   const size_t numClasses = arma::max(labels.row(0)) + 1;
   const size_t numModels = 5;
 
-  XGBoost xgb;
-
-  xgb.Train(db, labels, numClasses, numModels);
-
-  arma::Row<size_t> predictions;
-  xgb.Classify(db, predictions);
-
-  double accuracy = 0;
-  for (size_t i = 0; i < labels.n_elem; i++) 
-  {
-    if(labels(i) == predictions(i)) 
-    {
-      accuracy++;
-    }
-  }
-
-  accuracy = accuracy / ((double) labels.n_elem);
-  accuracy *= 100.0;
-
-  REQUIRE(accuracy > 60);
-
-}
-
-/**
- * This test case runs the XGBoost model on the Iris dataset.
- * Tests if the model gives a training accuracy > 60 given the data and a 
- * pre-initiated weak learner. Used default empty constructor.
-*/
-TEST_CASE("XGBIrisTrainMethod2", "[XGBTest]") 
-{
-  arma::mat db;
-  if (!data::Load("iris.csv", db))
-    FAIL("Cannot load test dataset iris.csv!");
-  
-  arma::Row<size_t> labels;
-  if (!data::Load("iris_labels.txt", labels))
-    FAIL("Cannot load labels for iris iris_labels.txt");
-
-  const size_t numClasses = arma::max(labels.row(0)) + 1;
-  const size_t numModels = 5;
   XGBoost xgb;
 
   xgb.Train(db, labels, numClasses, numModels);
@@ -151,7 +111,7 @@ TEST_CASE("XGBIrisTrainMethod2", "[XGBTest]")
  * Tests if the model gives a training accuracy > 60 given the data and a 
  * weak learner arguments. Used default empty constructor.
 */
-TEST_CASE("XGBIrisTrainMethod3", "[XGBTest]") 
+TEST_CASE("XGBIrisTrainMethod2", "[XGBoostGeneralTest]") 
 {
   arma::mat db;
   if (!data::Load("iris.csv", db))
@@ -177,8 +137,7 @@ TEST_CASE("XGBIrisTrainMethod3", "[XGBTest]")
             numModels, 
             minimumLeafSize,
             minimumGainSplit,
-            maximumDepth,
-            dimensionSelector);
+            maximumDepth);
 
   arma::Row<size_t> predictions;
   xgb.Classify(db, predictions);
@@ -196,5 +155,101 @@ TEST_CASE("XGBIrisTrainMethod3", "[XGBTest]")
   accuracy *= 100.0;
 
   REQUIRE(accuracy > 60);
+
+}
+
+/**
+ * This test case runs the XGBoost model on the Iris dataset.
+ * Tests if the model gives a testing accuracy > 60 given the data.
+ * Used default empty constructor.
+*/
+TEST_CASE("XGBIrisTestAccuracy", "[XGBoostGeneralTest]") 
+{
+  arma::mat db;
+  if (!data::Load("iris_train.csv", db))
+    FAIL("Cannot load test dataset iris_train.csv!");
+  
+  arma::Row<size_t> labels;
+  if (!data::Load("iris_train_labels.csv", labels))
+    FAIL("Cannot load labels for iris iris_train_labels.txt");
+
+  arma::mat testDb;
+  if (!data::Load("iris_test.csv", testDb))
+    FAIL("Cannot load test dataset iris_test.csv!");
+
+  arma::Row<size_t> testLabels;
+  if (!data::Load("iris_test_labels.csv", testLabels))
+    FAIL("Cannot load test dataset iris_test_labels.csv!");
+
+  const size_t numClasses = arma::max(labels.row(0)) + 1;
+  const size_t numModels = 5;
+
+  XGBoost xgb;
+
+  xgb.Train(db, labels, numClasses, numModels);
+
+  arma::Row<size_t> predictions;
+  xgb.Classify(testDb, predictions);
+
+  double accuracy = 0;
+  for (size_t i = 0; i < testLabels.n_elem; i++) 
+  {
+    if(testLabels(i) == predictions(i)) 
+    {
+      accuracy++;
+    }
+  }
+
+  accuracy = accuracy / ((double) testLabels.n_elem);
+  accuracy *= 100.0;
+
+  REQUIRE(accuracy > 60);
+
+}
+
+/**
+ * Testing the Weak Learner fetching function
+ */
+TEST_CASE("XGBWeakLearnerFunction", "[XGBoostUnitTest]")
+{
+  arma::mat db;
+  if (!data::Load("iris_train.csv", db))
+    FAIL("Cannot load test dataset iris_train.csv!");
+  
+  arma::Row<size_t> labels;
+  if (!data::Load("iris_train_labels.csv", labels))
+    FAIL("Cannot load labels for iris iris_train_labels.txt");
+
+  arma::mat testDb;
+  if (!data::Load("iris_test.csv", testDb))
+    FAIL("Cannot load test dataset iris_test.csv!");
+
+  arma::Row<size_t> testLabels;
+  if (!data::Load("iris_test_labels.csv", testLabels))
+    FAIL("Cannot load test dataset iris_test_labels.csv!");
+
+  const size_t numClasses = arma::max(labels.row(0)) + 1;
+  const size_t numModels = 5;
+
+  XGBoost xgb;
+  
+  xgb.Train(db, labels, numClasses, numModels);
+
+  arma::Row<size_t> predictions;
+  xgb.WeakLearner(0).Classify(testDb, predictions);
+
+  double accuracy = 0;
+  for (size_t i = 0; i < testLabels.n_elem; i++) 
+  {
+    if(testLabels(i) == predictions(i)) 
+    {
+      accuracy++;
+    }
+  }
+
+  accuracy = accuracy / ((double) testLabels.n_elem);
+  accuracy *= 100.0;
+
+  REQUIRE(accuracy > 0);
 
 }
