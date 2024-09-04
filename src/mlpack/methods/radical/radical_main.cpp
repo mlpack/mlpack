7855ea4955597a7687f3196b0516afc6eaf26702
Ryan Curtin <ryan@ratml.org>
2024-09-04 12:04:34 -0400

    Don't use deprecated function in binding.

--- src/mlpack/methods/radical/radical_main.cpp
+++ src/mlpack/methods/radical/radical_main.cpp
@@ -123,7 +123,7 @@ void BINDING_FUNCTION(util::Params& params, util::Timers& timers)
   Radical rad(noiseStdDev, nReplicates, nAngles, nSweeps);
   mat matY;
   mat matW;
-  rad.DoRadical(matX, matY, matW, timers);
+  rad.Apply(matX, matY, matW, timers);
 
   // Save results.
   if (params.Has("output_ic"))
