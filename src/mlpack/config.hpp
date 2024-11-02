79cb8ee594244e17e27f580ff43945e403a3142a
Omar Shrit <omar@avontech.fr>
2024-11-02 15:34:44 +0100

    Comment for now STB including macros
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- src/mlpack/config.hpp
+++ src/mlpack/config.hpp
@@ -23,28 +23,28 @@
 // #define MLPACK_HAS_BFD_DL
 #endif
 
-//
-// mlpack provides image loading and saving support via STB, if available.  STB
-// is an optional dependency of mlpack.  When STB is found on a system,
-// MLPACK_HAS_STB will be defined and the files `stb_image.h` and
-// `stb_image_write.h` are expected to be found in the compiler include path.
-//
-#ifndef MLPACK_HAS_STB
-// #define MLPACK_HAS_STB
-#endif
+////
+//// mlpack provides image loading and saving support via STB, if available.  STB
+//// is an optional dependency of mlpack.  When STB is found on a system,
+//// MLPACK_HAS_STB will be defined and the files `stb_image.h` and
+//// `stb_image_write.h` are expected to be found in the compiler include path.
+////
+//#ifndef MLPACK_HAS_STB
+//// #define MLPACK_HAS_STB
+//#endif
 
-//
-// If STB support is available but the STB headers do not live in an stb/
-// directory, then MLPACK_HAS_NO_STB_DIR should be defined.
-//
-// Note that this is ignored in newer compilers where __has_include is available
-// (C++17 and newer compilers support it).
-//
-#ifdef MLPACK_HAS_STB
-#ifndef MLPACK_HAS_NO_STB_DIR
-// #define MLPACK_HAS_NO_STB_DIR
-#endif
-#endif
+////
+//// If STB support is available but the STB headers do not live in an stb/
+//// directory, then MLPACK_HAS_NO_STB_DIR should be defined.
+////
+//// Note that this is ignored in newer compilers where __has_include is available
+//// (C++17 and newer compilers support it).
+////
+//#ifdef MLPACK_HAS_STB
+//#ifndef MLPACK_HAS_NO_STB_DIR
+//// #define MLPACK_HAS_NO_STB_DIR
+//#endif
+//#endif
 
 //
 // If the version of mlpack is built from a git repository and is not an
@@ -75,40 +75,40 @@
 //
 // Perform autodetection of STB if possible.
 //
-#ifndef MLPACK_HAS_STB
-  #if defined __has_include
-    #if __has_include("stb_image.h")
-      #define MLPACK_HAS_STB_IMAGE
-    #elif __has_include("stb/stb_image.h")
-      #define MLPACK_HAS_STB_IMAGE
-    #endif
+//#ifndef MLPACK_HAS_STB
+  //#if defined __has_include
+    //#if __has_include("stb_image.h")
+      //#define MLPACK_HAS_STB_IMAGE
+    //#elif __has_include("stb/stb_image.h")
+      //#define MLPACK_HAS_STB_IMAGE
+    //#endif
 
-    #if __has_include("stb_image_write.h")
-      #define MLPACK_HAS_STB_IMAGE_WRITE
-    #elif __has_include("stb/stb_image_write.h")
-      #define MLPACK_HAS_STB_IMAGE_WRITE
-    #endif
+    //#if __has_include("stb_image_write.h")
+      //#define MLPACK_HAS_STB_IMAGE_WRITE
+    //#elif __has_include("stb/stb_image_write.h")
+      //#define MLPACK_HAS_STB_IMAGE_WRITE
+    //#endif
 
-    #if defined(MLPACK_HAS_STB_IMAGE) && defined(MLPACK_HAS_STB_IMAGE_WRITE)
-      #define MLPACK_HAS_STB
-    #endif
+    //#if defined(MLPACK_HAS_STB_IMAGE) && defined(MLPACK_HAS_STB_IMAGE_WRITE)
+      //#define MLPACK_HAS_STB
+    //#endif
 
-    #undef MLPACK_HAS_STB_IMAGE
-    #undef MLPACK_HAS_STB_IMAGE_WRITE
-  #endif
-#endif
+    //#undef MLPACK_HAS_STB_IMAGE
+    //#undef MLPACK_HAS_STB_IMAGE_WRITE
+  //#endif
+//#endif
 
-//
-// These macros can be defined to disable support that is defined above.  (This
-// is useful if you cannot or do not want to modify config.hpp.)
-//
-#ifdef MLPACK_DISABLE_STB
-  #undef MLPACK_HAS_STB
-#endif
+////
+//// These macros can be defined to disable support that is defined above.  (This
+//// is useful if you cannot or do not want to modify config.hpp.)
+////
+//#ifdef MLPACK_DISABLE_STB
+  //#undef MLPACK_HAS_STB
+//#endif
 
-#ifdef MLPACK_DISABLE_NO_STB_DIR
-  #undef MLPACK_HAS_NO_STB_DIR
-#endif
+//#ifdef MLPACK_DISABLE_NO_STB_DIR
+  //#undef MLPACK_HAS_NO_STB_DIR
+//#endif
 
 #ifdef MLPACK_DISABLE_BFD_DL
   #undef MLPACK_HAS_BFD_DL
