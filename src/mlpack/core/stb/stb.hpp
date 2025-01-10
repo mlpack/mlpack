76a53ef200e9a869667a3cbd761546f244ccaf09
Omar Shrit <omar@avontech.fr>
2025-01-10 15:01:41 +0100

    Fix the bundle that if we are using system stb
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- src/mlpack/core/stb/stb.hpp
+++ src/mlpack/core/stb/stb.hpp
@@ -14,24 +14,78 @@
 #ifndef MLPACK_CORE_STB_STB_HPP
 #define MLPACK_CORE_STB_STB_HPP
 
-#if defined(MLPACK_STB) && !defined(MLPACK_USE_SYSTEM_STB)
+
+#if defined(MLPACK_USE_SYSTEM_STB)
 
 #include <mlpack/prereqs.hpp>
 
 #ifndef STB_IMAGE_STATIC
-#define STB_IMAGE_STATIC
+  #define STB_IMAGE_STATIC
 #endif
 
 #ifndef STB_IMAGE_RESIZE_IMPLEMENTATION
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
+  #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #endif
 
 #ifndef STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_IMPLEMENTATION
+  #define STB_IMAGE_IMPLEMENTATION
 #endif
 
 #ifndef STB_IMAGE_WRITE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
+  #define STB_IMAGE_WRITE_IMPLEMENTATION
+#endif
+
+#if defined __has_include
+  #if __has_include("stb_image_resize.h")
+    #include "stb_image_resize.h"
+  #elif __has_include("stb/stb_image_resize.h")
+    #include "stb/stb_image_resize.h"
+  #else
+    #define MLPACK_DISABLE_STB
+    #pragma message("Warning: STB disabled; stb_image_resize.h header not found")
+  #endif
+#endif
+
+#if defined __has_include
+  #if __has_include("stb_image.h")
+    #include "stb_image.h"
+  #elif __has_include("stb/stb_image.h")
+    #include "stb/stb_image.h"
+  #else
+    #define MLPACK_DISABLE_STB
+    #pragma message("Warning: STB disabled; stb_image.h header not found")
+  #endif
+#endif
+
+#if defined __has_include
+  #if __has_include("stb_image_write.h")
+    #include "stb_image_write.h"
+  #elif __has_include("stb/stb_image_write.h")
+    #include "stb/stb_image_write.h"
+  #else
+    #define MLPACK_DISABLE_STB
+    #pragma message("Warning: STB disabled; stb_image_write.h header not found")
+  #endif
+#endif
+
+#elif defined(MLPACK_STB)
+
+#include <mlpack/prereqs.hpp>
+
+#ifndef STB_IMAGE_STATIC
+  #define STB_IMAGE_STATIC
+#endif
+
+#ifndef STB_IMAGE_RESIZE_IMPLEMENTATION
+  #define STB_IMAGE_RESIZE_IMPLEMENTATION
+#endif
+
+#ifndef STB_IMAGE_IMPLEMENTATION
+  #define STB_IMAGE_IMPLEMENTATION
+#endif
+
+#ifndef STB_IMAGE_WRITE_IMPLEMENTATION
+  #define STB_IMAGE_WRITE_IMPLEMENTATION
 #endif
 
 // Now include STB headers
