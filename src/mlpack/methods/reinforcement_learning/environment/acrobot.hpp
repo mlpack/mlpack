2661fa5425a4cbf069c9aaee59c5fdda3c700cca
Andrea Novellini <andrea.novellini@datadoghq.com>
2025-01-12 19:57:29 +0100

    Fix broken links (#3876)

--- src/mlpack/methods/reinforcement_learning/environment/acrobot.hpp
+++ src/mlpack/methods/reinforcement_learning/environment/acrobot.hpp
@@ -3,7 +3,7 @@
  * @author Rohan Raj
  *
  * This file is an implementation of Acrobot task:
- * https://www.gymlibrary.ml/environments/classic_control/acrobot
+ * https://www.gymlibrary.dev/environments/classic_control/acrobot
  *
  * mlpack is free software; you may redistribute it and/or modify it under the
  * terms of the 3-clause BSD license.  You should have received a copy of the
--- src/mlpack/methods/reinforcement_learning/environment/mountain_car.hpp
+++ src/mlpack/methods/reinforcement_learning/environment/mountain_car.hpp
@@ -3,7 +3,7 @@
  * @author Shangtong Zhang
  *
  * This file is an implementation of Mountain Car task:
- * https://www.gymlibrary.ml/environments/classic_control/mountain_car
+ * https://www.gymlibrary.dev/environments/classic_control/mountain_car
  *
  * TODO: provide an option to use dynamics directly from OpenAI gym.
  *
