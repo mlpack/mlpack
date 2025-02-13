9ef2118989d60c743785d293d51ceb0b8c091ed5
Ryan Curtin <ryan@ratml.org>
2025-02-13 17:50:13 -0500

    Refactor style check job a bit.

---
+++ .jenkins/utils.groovy
@@ -0,0 +1,10 @@
+// A simple utility to set the build status on Github for a commit.
+void setBuildStatus(String context, String message, String state) {
+  step([
+      $class: "GitHubCommitStatusSetter",
+      reposSource: [$class: "ManuallyEnteredRepositorySource", url: "https://github.com/mlpack/mlpack"],
+      contextSource: [$class: "ManuallyEnteredCommitContextSource", context: context ],
+      errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
+      statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
+  ]);
+}
