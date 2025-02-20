8f8eaff3758c9ab727a44100cb56103973f53b2d
Ankit Singh <105532883+zakmii@users.noreply.github.com>
2025-02-20 16:27:04 +0530

    Add pdp.hpp and pdp_impl.hpp

---
+++ src/mlpack/methods/pdp/pdp.hpp
@@ -0,0 +1,27 @@
+/**
+ *	@file methods/pdp/pdp.hpp
+ *	@author: Ankit Singh
+ * 
+ *	Partial Dependene Plot (PDP) for mlpack models.
+ * 
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+
+#ifndef MLPACK_METHODS_PDP_PDP_HPP
+#define MLPACK_METHODS_PDP_PDP_HPP
+
+#include <mlpack/prereqs.hpp>
+
+namespace mlpack {
+	/**
+	* The Partial
+	*/
+
+} // namespace mlpack
+
+#include "pdp_impl.hpp"
+
+#endif
