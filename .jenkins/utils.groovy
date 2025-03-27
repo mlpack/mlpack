111777ebe702925cf9ea7c0b0e8bf5980bf54c33
Ryan Curtin <ryan@ratml.org>
2025-03-27 12:10:15 -0400

    Revert "Try and use the builtin checks only; maybe they will work."
    This reverts commit 461ea584206fcb12a4fc00cbcdb0d289a0bc7ef2.

--- .jenkins/utils.groovy
+++ .jenkins/utils.groovy
@@ -63,4 +63,45 @@ def setBuildStatus(Map paramsMap)
   ]);
 }
 
+def startCheck(String name, String status)
+{
+  // Set module-level variables that we will retain as we build.
+  this.name = name
+  this.status = status
+  this.time = currentBuild.duration
+
+  publishChecks(name: name,
+                status: 'IN_PROGRESS',
+                title: status,
+                text: status,
+                detailsURL: currentBuild.absoluteUrl + 'console')
+}
+
+def updateCheckStatus(String status)
+{
+  def stepTime = (currentBuild.duration - this.time) / 1000.0
+  this.status += '\n' + status + ' (' + stepTime.toString() + 's)'
+  this.time = currentBuild.duration
+
+  publishChecks(name: this.name,
+                status: 'IN_PROGRESS',
+                title: status,
+                text: this.status,
+                detailsURL: currentBuild.absoluteUrl + 'console')
+}
+
+def finishCheck(String status, boolean success)
+{
+  def stepTime = (currentBuild.duration - this.time) / 1000.0
+  this.status += '\n' + status + ' (' + stepTime.toString() + 's)'
+  this.time = currentBuild.duration
+
+  publishChecks(name: this.name,
+                status: 'COMPLETED',
+                conclusion: success ? 'SUCCESS' : 'FAILURE',
+                title: status,
+                text: this.status,
+                detailsURL: currentBuild.absoluteUrl + 'testReport')
+}
+
 return this
