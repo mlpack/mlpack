bb3f21c5a42eca44af7822401d2b36c255da804b
Omar Shrit <omar@shrit.me>
2025-06-19 12:02:03 +0200

    Update src/mlpack/core/data/data_options.hpp
    Co-authored-by: Ryan Curtin <ryan@ratml.org>

--- src/mlpack/core/data/data_options.hpp
+++ src/mlpack/core/data/data_options.hpp
@@ -293,9 +293,9 @@ class DataOptionsBase
       case FileType::HDF5Binary:  return "HDF5 data";
       case FileType::CoordASCII:
           return "ASCII formatted sparse coordinate data";
-      case FileType::XML:         return "XML Model";
-      case FileType::BIN:         return "BIN Model";
-      case FileType::JSON:        return "JSON Model";
+      case FileType::XML:         return "XML model";
+      case FileType::BIN:         return "binary model";
+      case FileType::JSON:        return "JSON model";
       case FileType::AutoDetect:  return "Detect automatically data type";
       case FileType::FileTypeUnknown: return "Unknown data type";
       default:                    return "";
