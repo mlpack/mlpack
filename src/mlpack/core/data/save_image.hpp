3bde3d523de37eb96c2604e8e6e2fd1d6a389aa4
Omar Shrit <omar@avontech.fr>
2025-08-26 19:13:12 +0200

    Fix indentation in the header comment
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- src/mlpack/core/data/save_image.hpp
+++ src/mlpack/core/data/save_image.hpp
@@ -1,14 +1,14 @@
-  /**
-   * @file core/data/save_image.hpp
-   * @author Ryan Curtin
-   *
-   * Implementation of save functionality.
-   *
-   * mlpack is free software; you may redistribute it and/or modify it under the
-   * terms of the 3-clause BSD license.  You should have received a copy of the
-   * 3-clause BSD license along with mlpack.  If not, see
-   * http://www.opensource.org/licenses/BSD-3-Clause for more information.
-   */
+/**
+ * @file core/data/save_image.hpp
+ * @author Ryan Curtin
+ *
+ * Implementation of save functionality.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
 #ifndef MLPACK_CORE_DATA_SAVE_IMAGE_HPP
 #define MLPACK_CORE_DATA_SAVE_IMAGE_HPP
 
