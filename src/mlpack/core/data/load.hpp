814115921e203b5c6152868584ce97ad4fcffdb7
Omar Shrit <omar@avontech.fr>
2025-11-21 14:18:09 +0100

    Add new vector API in here
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- src/mlpack/core/data/load.hpp
+++ src/mlpack/core/data/load.hpp
@@ -65,14 +65,60 @@ bool Load(const std::string& filename,
           const DataOptionsType& opts,
           const typename std::enable_if_t<
               IsDataOptions<DataOptionsType>::value>* = 0);
+/**
+ * This function loads a set of several dataset files into one matrix.
+ * This is usually the case if the dataset is collected on several occasions
+ * and not agglomerated into one file, or if the dataset has been partitioned
+ * into multiple files.
+ *
+ * Note, the load will fail if the number of dimension (data points) in all
+ * files is not equal, or if the dataset does not have the same filetype. For
+ * example, the load will fail one file is CSV and the other is binary.
+ *
+ * The user needs to specify all the filenames in one std::vector before using
+ * this function.
+ *
+ * @param filenames Names of files to load.
+ * @param matrix Matrix to load contents of files into.
+ * @param opts DataOptions to be passed to the function
+ * @return Boolean value indicating success or failure of load.
+ */
+template<typename eT, typename DataOptionsType>
+bool Load(const std::vector<std::string>& files,
+          arma::Mat<eT>& matrix,
+          const DataOptionsType& opts,
+          const typename std::enable_if_t<
+              IsDataOptions<DataOptionsType>::value>* = 0);
+/**
+ * This function loads a set of several dataset files into one matrix.
+ * This is usually the case if the dataset is collected on several occasions
+ * and not agglomerated into one file, or if the dataset has been partitioned
+ * into multiple files.
+ *
+ * Note, the load will fail if the number of dimension (data points) in all
+ * files is not equal, or if the dataset does not have the same filetype. For
+ * example, the load will fail one file is CSV and the other is binary.
+ *
+ * The user needs to specify all the filenames in one std::vector before using
+ * this function.
+ *
+ * @param filenames Names of files to load.
+ * @param matrix Matrix to load contents of files into.
+ * @param opts DataOptions to be passed to the function
+ * @return Boolean value indicating success or failure of load.
+ */
+template<typename eT, typename DataOptionsType>
+bool Load(const std::vector<std::string>& files,
+          arma::Mat<eT>& matrix,
+          DataOptionsType& opts,
+          const typename std::enable_if_t<
+              IsDataOptions<DataOptionsType>::value>* = 0);
 
 } // namespace data
 } // namespace mlpack
 
 // Include implementation of Load() for matrix.
 #include "load_impl.hpp"
-// Include implementation of model-loading Load() overload.
-#include "load_model_impl.hpp"
 // Include implementation of Load() for vectors.
 #include "load_vec_impl.hpp"
 
