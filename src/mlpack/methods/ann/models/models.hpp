5a1da074359b1b0f2244dac5d72d94900106e5ac
Andrew Furey <andrewfurey2003@gmail.com>
2025-11-29 07:08:10 +0000

    yolov3-tiny object detection model and yolov3 layer (#4023)

---
+++ src/mlpack/methods/ann/models/models.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file methods/ann/models/models.hpp
+ * @author Andrew Furey
+ *
+ * This includes all prebuilt models found in mlpack.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_METHODS_ANN_MODELS_MODELS_HPP
+#define MLPACK_METHODS_ANN_MODELS_MODELS_HPP
+
+#include "yolov3/yolov3_tiny.hpp"
+
+#endif
