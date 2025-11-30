904eff333d1bf8cad86a589100e6459a4d3ad9c1
Zachary Ng <zachn716@gmail.com>
2025-11-29 21:09:50 -0800

    match opt parameters

--- src/mlpack/tests/ann/recurrent_network_test.cpp
+++ src/mlpack/tests/ann/recurrent_network_test.cpp
@@ -1020,7 +1020,7 @@ TEMPLATE_TEST_CASE("RNNRaggedSequenceTest", "[RecurrentNetworkTest][long]",
   }
 
   // Build a network and train it.
-  RMSProp opt(0.003, 8, 0.99, 1e-08, 500 * numEpochs, 1e-5);
+  RMSProp opt(0.001, 8, 0.99, 1e-08, 500 * numEpochs, 1e-5);
 
   RNN<MeanSquaredError> net(rho);
   net.Add<TestType>(10);
@@ -1053,7 +1053,7 @@ TEMPLATE_TEST_CASE("RNNRaggedSequenceTest", "[RecurrentNetworkTest][long]",
   net2.Add<Linear>(1);
 
   // Train and predict, then compute the sum error.
-  RMSProp opt2(0.003, 8, 0.99, 1e-08, 500 * numEpochs / 2, 1e-5);
+  RMSProp opt2(0.001, 8, 0.99, 1e-08, 500 * numEpochs, 1e-5);
   net2.Train(origData, origResponses, opt2);
   net2.Predict(origData, prediction);
   const double refAverageError = mean(abs(vectorise(origResponses) -
