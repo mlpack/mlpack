2097a6917fb6a25d538bae6ff477f92a152daeba
Omar Shrit <omar@avontech.fr>
2026-02-05 18:39:56 +0100

    Add the comments found after doing some testing
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- src/mlpack/core/data/load_http.hpp
+++ src/mlpack/core/data/load_http.hpp
@@ -60,14 +60,57 @@ bool LoadHTTP(const std::string& url,
    //   << std::endl;
   std::stringstream data(res->body);
 
+  FilenameFromURL(filename, url);
   if (opts.EnableCache())
   {
-    FilenameFromURL(filename, url);
     success = WriteToFile(filename, opts, data.str(), stream);
   }
   else
   {
-    std::FILE* tmp = std::tmpfile();
+    // problems with tmpfile()
+    // It seems that using tmpfile() is a not good for the following reasons reasons.
+    // 1. First the file created with automatically generated name, also the
+    // file does not appear in the filesystem and it is fully held by the
+    // current process and destroyed when the process is destroyed.
+    // 2. tmpnam() exists so we can generate the temporary name, but we cannot
+    // use this temporary name with tmpfile()
+    // 3. tmpfile() return a file descriptor and there is no way to change the
+    // file descriptor to std::fstream. This will case a problem with the
+    // current functions.
+    //std::FILE* tmp = std::tmpfile();
+    //
+    // A solution is the following (Not possible, but it was explored):
+    //
+    // 1. Use the same filename that we extract from the downloaded file
+    // 2. Get the temprorary location of the system where the tempfiles are
+    // located using fs::temp_directory_path()
+    // 3. Create the temporary file using int fd = std::mkstemp(filename);
+    // 4. Write the data to the file using write, or fput (better write with fd)
+    //  It seems that it is not possible to use write because this is OS
+    //  relevant function
+    //  Also it seems that mkstemp() cannot be used with fputs because it
+    //  returns an int.
+    // 5. Once this is done close(fd) the file and only keep the file name for the
+    // Load function. Note, at this stage the file appears in the filesystem
+    // and it is not destroyed.
+    // 6. Open the file and load it using Load() interface.
+    // 7. Optional, we can delete this file using std::unlink(filename); Once
+    // we finished the manipulation process, but this is not obligatory.
+    //
+
+
+    // Another solution that is possible
+    // We follow the same above logic, until we arrive to temporary file
+    // creation.
+    //
+    // In this case, we open and create the file in the temporary directory, in
+    // the same way we do at mlpack using std::open. fstream, etc.
+    // We write to that file, using the write function that I have created.
+    // Then we return the std::fstream so we can have access to the file to
+    // load it if we are using CSV, or just return the filename so we can load
+    // it with STB in the case of an image.
+    //
+    // Finally we call ulink on the file if we are not interested in keeping it
 
   }
 
