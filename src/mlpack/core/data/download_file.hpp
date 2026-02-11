e763cfa4ad76b6646fc62c3cfa762052af419d5e
Omar Shrit <omar@avontech.fr>
2026-02-11 22:45:52 +0100

    Move header and implementation to two different files
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- src/mlpack/core/data/download_file.hpp
+++ src/mlpack/core/data/download_file.hpp
@@ -61,6 +61,7 @@ namespace mlpack {
 // If this works on Windows, I would recommend using it as it is, as
 // it is going to be much easier to be handle with our current infrastrucutre
 // without the need to use any C function. 
+// This function to be moved to handle_files.hpp and handle_files_impl.hpp
 //
 std::filesystem::path TempName()
 {
@@ -77,91 +78,16 @@ std::filesystem::path TempName()
   return std::filesystem::temp_directory_path() / nameStream.str();
 }
 
+inline void Decompress(const std::string& compressed, std::string decompressed);
+
 template<typename DataOptionsType>
 bool DownloadFile(const std::string& url,
                   std::string& filename,
                   std::fstream& stream,
-                  DataOptionsType& opts)
-{
-  bool success = false;
-  std::string originalFilename;
-  FilenameFromURL(originalFilename, url);
-
-#ifdef MLPACK_CACHE_REMOTE_DATASETS
-  if (std::filesystem::exists(originalFilename))
-  {
-    std::filesystem::file_time_type fileTime =
-        std::filesystem::last_write_time(originalFilename);
-
-    std::chrono::time_point<std::filesystem::file_time_type::clock,
-        FileTimeClock::duration> now = FileTimeClock::now();
-
-    FileTimeClock::duration difference = now - fileTime;
-
-    std::chrono::hours difference =
-        std::chrono::duration_cast<std::chrono::hours>(difference);
-    if (difference.count() > 5)
-    {
-      // only return, the file exist and we don't need to download it
-      return true;
-    }
-  }
-#endif
-
-  // If host is not extracted correctly, we will get a segmentation fault from
-  // httplib
-  std::string host = URLToHost(url);
-#ifdef CPPHTTPLIB_OPENSSL_SUPPORT
-  auto port = 443;
-  httplib::SSLClient cli(host, port);
-#else
-  auto port = 80;
-  httplib::Client cli(host, port);
-#endif
-  cli.set_connection_timeout(2);
-  httplib::Result res = cli.Get(url);
-
-  if (res->status != 200)
-  {
-    std::stringstream oss;
-    oss <<  "Unable to connect, status returned: '" << res->status;
-    return HandleError(oss, opts);
-  }
-
-  std::stringstream data(res->body);
-
-#ifdef MLPACK_CACHE_REMOTE_DATASETS
-  success = WriteToFile(originalFilename, opts, data.str(), stream);
-#else
-  // This does not work, please see above.
-  // filename = std::tmpnam(nullptr); 
-
-  filename = TempName();
-  // This is necessary to get the extension.
-  filename += originalFilename;
-  success = OpenFile(filename, opts, false, stream);
-  if (!success)
-  {
-    std::stringstream oss;
-    oss <<  "Unable to open a temporary file for downloading data.";
-    return HandleError(oss, opts);
-  }
-
-  stream.write(data.str().data(), data.str().size());
-  if (!stream.good())
-  {
-    std::stringstream oss;
-    oss << "Error writing to a '" << filename << "'.  "
-          << "Please check permissions or disk space.";
-    return HandleError(oss, opts);
-  }
-  stream.close();
-#endif
-  return success;
-}
+                  DataOptionsType& opts);
 
 } // namespace mlpack
 
-#endif
+#endif // MLPACK_ENABLE_HTTPLIB
 
 #endif
