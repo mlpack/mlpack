5454e538424f85797bd386ce124ebd589150a7a5
andrewfurey21 <andrewfurey2003@gmail.com>
2026-02-23 13:14:46 -0800

    update Predict overloads

--- src/mlpack/methods/ann/models/yolov3/yolov3.hpp
+++ src/mlpack/methods/ann/models/yolov3/yolov3.hpp
@@ -121,101 +121,29 @@ class YOLOv3
 
   /**
    * Ordinary feed forward pass of the network. Draws the bounding boxes
-   * onto the input image.
-   *
-   * @param image Image used for detection bounding boxes and drawing
-      those bounding boxes. Pixel values are expected to be 0-255.
-   * @param opt ImageOptions used for specifying the dimensions of the image.
-   * @param ignoreThresh used for ignoring boxes whose confidence is below
-      this threshold.
+   * onto the input `image`.
    */
   void Predict(MatType& image,
                const ImageOptions& opt,
                const double ignoreThresh = 0.7)
   {
-    // Preprocessing steps:
-    // normalize to 0-1, letterbox with grey = 0.5, group channels
-    ImageOptions preprocessedOpt = opt;
-    MatType preprocessed = image / 255.0;
-    const ElemType grey = 0.5;
-    LetterboxImages(preprocessed, preprocessedOpt, imgSize, imgSize, grey);
-    preprocessed = GroupChannels(preprocessed, preprocessedOpt);
+    MatType preprocessed, rawOutput;
 
-    MatType rawOutput;
+    PreprocessImage(image, opt, preprocessed);
     model.Predict(preprocessed, rawOutput);
+    DrawBoundingBoxes(rawOutput, image, opt, ignoreThresh);
+  }
 
-    // Helpers
-    const size_t numBoxes = model.OutputDimensions()[1];
-    const size_t size = opt.Width() * opt.Height() * opt.Channels();
-
-    const size_t numClasses = classNames.size();
-    const arma::Col<ElemType> red = {255.0f, 0, 0};
-
-    const size_t width = opt.Width();
-    const size_t height = opt.Height();
-
-    arma::ucolvec nmsIndices;
-
-    ElemType ratio;
-    ElemType xOffset = 0;
-    ElemType yOffset = 0;
-
-    if (width > height)
-    {
-      // landscape
-      ratio =  (ElemType)width / imgSize;
-      yOffset = (imgSize - (height * imgSize / (ElemType)width)) / 2;
-    }
-    else
-    {
-      // portrait
-      ratio =  (ElemType)height / imgSize;
-      xOffset = (imgSize - (width * imgSize / (ElemType)height)) / 2;
-    }
-
-    // Draw bounding boxes using NMS for each image in the batch.
-    for (size_t batch = 0; batch < rawOutput.n_cols; batch++)
-    {
-      MatType rawOutputAlias, imageAlias;
-
-      MakeAlias(rawOutputAlias, rawOutput, numAttributes, numBoxes,
-          rawOutput.n_rows * batch);
-      MakeAlias(imageAlias, image, size, 1, size * batch);
-
-      const MatType& bboxes = rawOutputAlias.submat(0, 0, 3, numBoxes - 1);
-      const MatType& objectness = rawOutputAlias.submat(4, 0, 4, numBoxes - 1);
-      const MatType& confs =
-        rawOutputAlias.submat(5, 0, numAttributes - 1, numBoxes - 1);
-
-
-      // NMS on objectness, not class confidences. Will produce false negatives.
-      NMS<>::Evaluate(bboxes, objectness, nmsIndices, 0.4);
-
-      MatType chosenBoxes = bboxes.cols(nmsIndices);
-      MatType classConfs = MatType(confs.cols(nmsIndices)).each_row() %
-        objectness.cols(nmsIndices);
-      arma::umat chosenConfs = arma::index_max(classConfs, 0);
-
-      for (size_t b = 0; b < nmsIndices.n_rows; b++)
-      {
-        const size_t chosenClass = chosenConfs.at(0, b);
-        if (classConfs.at(chosenClass, b) < ignoreThresh)
-          continue;
-
-        const std::string& label = classNames[chosenClass];
-        const ElemType x1 =
-          (chosenBoxes.at(0, b) - chosenBoxes.at(2, b) / 2 - xOffset) * ratio;
-        const ElemType y1 =
-          (chosenBoxes.at(1, b) - chosenBoxes.at(3, b) / 2 - yOffset) * ratio;
-        const ElemType x2 =
-          (chosenBoxes.at(0, b) + chosenBoxes.at(2, b) / 2 - xOffset) * ratio;
-        const ElemType y2 =
-          (chosenBoxes.at(1, b) + chosenBoxes.at(3, b) / 2 - yOffset) * ratio;
-
-        const arma::Col<ElemType> bbox = arma::Col<ElemType>({x1, y1, x2, y2});
-        BoundingBoxImage(imageAlias, opt, bbox, red, 1, label, 2);
-      }
-    }
+  /**
+   * Identical to the last overload, except writes to a different matrix.
+   */
+  void Predict(const MatType& input,
+               const ImageOptions& opt,
+               MatType& output,
+               const double ignoreThresh = 0.7)
+  {
+    output = input;
+    Predict(output, opt, ignoreThresh);
   }
 
   // Serialize the model.
@@ -261,6 +189,104 @@ class YOLOv3
   size_t DownsampleBlock(const size_t previousLayer,
                          const size_t maps,
                          const size_t shortcuts);
+
+  /**
+   * Preprocesses the `input` image and writes to `output`. The steps for
+   * `YOLOv3` are normalize pixel values to be 0-1, letterbox the image
+   * then group the channels for convolutions.
+   */
+  void PreprocessImage(const MatType& input,
+                       const ImageOptions& opt,
+                       MatType& output)
+  {
+    MatType preprocessed = input / 255.0;
+    ImageOptions preprocessedOpt = opt;
+    LetterboxImages(preprocessed, preprocessedOpt, imgSize, imgSize, 0.5);
+    output = GroupChannels(preprocessed, preprocessedOpt);
+  }
+
+  /**
+   * Read the raw output of the model and draw the bounding boxes onto the
+   * given `image`. If the bounding boxes confidence is greater than
+   * `ignoreThresh` it will be drawn onto the image.
+   */
+  void DrawBoundingBoxes(const MatType& rawOutput,
+                         MatType& image,
+                         const ImageOptions& opt,
+                         const double ignoreThresh)
+  {
+    // Helpers
+    const size_t numBoxes = model.OutputDimensions()[1];
+    const size_t size = opt.Width() * opt.Height() * opt.Channels();
+
+    const size_t numClasses = classNames.size();
+    const arma::Col<ElemType> red = {255.0f, 0, 0};
+
+    const size_t width = opt.Width();
+    const size_t height = opt.Height();
+
+    arma::ucolvec nmsIndices;
+
+    ElemType ratio;
+    ElemType xOffset = 0;
+    ElemType yOffset = 0;
+
+    if (width > height)
+    {
+      // landscape
+      ratio =  (ElemType)width / imgSize;
+      yOffset = (imgSize - (height * imgSize / (ElemType)width)) / 2;
+    }
+    else
+    {
+      // portrait
+      ratio =  (ElemType)height / imgSize;
+      xOffset = (imgSize - (width * imgSize / (ElemType)height)) / 2;
+    }
+
+    // Draw bounding boxes using NMS for each image in the batch.
+    for (size_t batch = 0; batch < rawOutput.n_cols; batch++)
+    {
+      MatType rawOutputAlias, imageAlias;
+
+      MakeAlias(rawOutputAlias, rawOutput, numAttributes, numBoxes,
+          rawOutput.n_rows * batch);
+      MakeAlias(imageAlias, image, size, 1, size * batch);
+
+      const MatType& bboxes = rawOutputAlias.submat(0, 0, 3, numBoxes - 1);
+      const MatType& objectness = rawOutputAlias.submat(4, 0, 4, numBoxes - 1);
+      const MatType& confs =
+        rawOutputAlias.submat(5, 0, numAttributes - 1, numBoxes - 1);
+
+      // NMS on objectness, not class confidences. Will produce false negatives.
+      NMS<>::Evaluate(bboxes, objectness, nmsIndices, 0.4);
+
+      MatType chosenBoxes = bboxes.cols(nmsIndices);
+      MatType classConfs = MatType(confs.cols(nmsIndices)).each_row() %
+        objectness.cols(nmsIndices);
+      arma::umat chosenConfs = arma::index_max(classConfs, 0);
+
+      for (size_t b = 0; b < nmsIndices.n_rows; b++)
+      {
+        const size_t chosenClass = chosenConfs.at(0, b);
+        if (classConfs.at(chosenClass, b) < ignoreThresh)
+          continue;
+
+        const std::string& label = classNames[chosenClass];
+        const ElemType x1 =
+          (chosenBoxes.at(0, b) - chosenBoxes.at(2, b) / 2 - xOffset) * ratio;
+        const ElemType y1 =
+          (chosenBoxes.at(1, b) - chosenBoxes.at(3, b) / 2 - yOffset) * ratio;
+        const ElemType x2 =
+          (chosenBoxes.at(0, b) + chosenBoxes.at(2, b) / 2 - xOffset) * ratio;
+        const ElemType y2 =
+          (chosenBoxes.at(1, b) + chosenBoxes.at(3, b) / 2 - yOffset) * ratio;
+
+        const arma::Col<ElemType> bbox = arma::Col<ElemType>({x1, y1, x2, y2});
+        BoundingBoxImage(imageAlias, opt, bbox, red, 1, label, 2);
+      }
+    }
+  }
 
   // DAGNetwork containing the graph of the YOLOv3 model
   ModelType model;
