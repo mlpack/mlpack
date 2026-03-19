21b02e91e4c86a62ade07dd098663be888873b35
Omar Shrit <omar@avontech.fr>
2026-03-19 13:04:14 +0100

    Let us see if this is going to fix the CI
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- CMake/crosscompile-arch-config.cmake
+++ CMake/crosscompile-arch-config.cmake
@@ -19,7 +19,7 @@ set(CMAKE_OPENBLAS_FLAGS "${CMAKE_CXX_FLAGS}") # OpenBLAS does not supoport flto
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -flto")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--hash-style=gnu -Wl,--build-id=none")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-z,norelro")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
+#set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
 ## Keep the following flag in comment, they might be relevant in the case of MCU's
 #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wl,-nmagic,-Bsymbolic -nostartfiles")
 
