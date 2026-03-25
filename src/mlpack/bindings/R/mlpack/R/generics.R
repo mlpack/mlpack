d117497af8c0646ac126a4d830fdc424eb6dcb37
Dirk Eddelbuettel <edd@debian.org>
2026-03-25 09:24:31 -0500

    Add documentation and example content to generics R code

--- src/mlpack/bindings/R/mlpack/R/generics.R
+++ src/mlpack/bindings/R/mlpack/R/generics.R
@@ -4,6 +4,15 @@ print.mlpack_model_binding <- function(x, ...) {
     cat("<mlpack object of class '", class(x)[1], "'>\n", sep="")
 }
 
+#' @rdname adaboost_train
+#' @examples
+#' data(iris)
+#' pp <- preprocess_split(input=iris[,1:4], input_label=as.matrix(as.integer(iris[,5])))
+#' trn <- adaboost_train(training = pp$training, labels = pp$training_labels)
+#' trn
+#' tst <- predict(trn, pp$test)
+#' table(tst)
+#' table(levels(iris[,5])[tst])
 #' @export
 predict.mlpack_adaboost_train <- function(object, newdata, type=c("predictions", "probabilities"), ...) {
     if (missing(newdata)) {
@@ -19,6 +28,16 @@ predict.mlpack_adaboost_train <- function(object, newdata, type=c("predictions",
     }
 }
 
+#' @rdname logistic_regression_train
+#' @examples
+#' data(iris)
+#' pp <- preprocess_split(input=iris[,1:4], input_label=as.matrix(as.integer(iris[,5])))
+#' trn <- logistic_regression_train(training = pp$training,
+#'                                  labels = as.matrix(as.integer(pp$training_labels == 2) + 1))
+#' trn
+#' tst <- predict(trn, pp$test)
+#' table(tst)
+#' table(levels(iris[,5])[tst])
 #' @export
 predict.mlpack_logistic_regression_train <- function(object, newdata, type=c("predictions", "probabilities"), ...) {
     if (missing(newdata)) {
@@ -34,6 +53,14 @@ predict.mlpack_logistic_regression_train <- function(object, newdata, type=c("pr
     }
 }
 
+#' @rdname linear_regression_train
+#' @examples
+#' data(mtcars)
+#' pp <- preprocess_split(input=mtcars[,-1], input_label=as.matrix(as.integer(mtcars[,1])))
+#' trn <- linear_regression_train(training = pp$training, training_responses = pp$training_labels)
+#' trn
+#' tst <- predict(trn, pp$test)
+#' tst
 #' @export
 predict.mlpack_linear_regression_train <- function(object, newdata, ...) {
     if (missing(newdata)) {
@@ -43,6 +70,14 @@ predict.mlpack_linear_regression_train <- function(object, newdata, ...) {
     res
 }
 
+#' @rdname lars_train
+#' @examples
+#' data(mtcars)
+#' pp <- preprocess_split(input=mtcars[,-1], input_label=as.matrix(as.integer(mtcars[,1])))
+#' trn <- lars_train(input = pp$training, responses = pp$training_labels)
+#' trn
+#' tst <- predict(trn, t(pp$test))
+#' tst
 #' @export
 predict.mlpack_lars_train <- function(object, newdata, ...) {
     if (missing(newdata)) {
