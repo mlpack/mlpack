29b9e4075922771b605e355436dd0241c3e24b91
Ryan Curtin <ryan@ratml.org>
2026-05-21 16:17:35 -0400

    Add licenses to files that are missing them.

--- src/mlpack/bindings/util/validate_methods.hpp
+++ src/mlpack/bindings/util/validate_methods.hpp
@@ -4,6 +4,11 @@
  *
  * Ensure that the parameters for each of the individual methods of a grouped
  * binding satisfy a handful of requirements.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
 #ifndef MLPACK_BINDINGS_UTIL_VALIDATE_METHODS_HPP
 #define MLPACK_BINDINGS_UTIL_VALIDATE_METHODS_HPP
