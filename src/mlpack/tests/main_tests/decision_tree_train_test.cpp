f10eec136b7fc3ee04820b8d14b3d275b4ba6b9f
Dirk Eddelbuettel <edd@debian.org>
2026-06-02 16:14:36 -0500

    Test ci issues 12: enable d_t_*_test

---
+++ src/mlpack/tests/main_tests/decision_tree_train_test.cpp
@@ -0,0 +1,75 @@
+/**
+ * @file tests/main_tests/decision_tree_train_test.cpp
+ * @author Manish Kumar
+ * @author Dirk Eddelbuettel
+ *
+ * Test RUN_BINDING() for decision_tree_train_main.cpp.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#define BINDING_TYPE BINDING_TYPE_TEST
+
+#include <mlpack/core.hpp>
+#include <mlpack/methods/decision_tree/decision_tree_train_main.cpp>
+#include <mlpack/core/util/mlpack_main.hpp>
+#include "main_test_fixture.hpp"
+
+#include "../test_catch_tools.hpp"
+#include "../catch.hpp"
+
+using namespace mlpack;
+
+BINDING_TEST_FIXTURE(DecisionTreeTrainTestFixture);
+
+/**
+ * Check that output dimension is correct.
+ */
+TEST_CASE_METHOD(DecisionTreeTrainTestFixture,
+                 "DecisionTreeTrainOutputDimensionTest",
+                 "[DecisionTreeTrainMainTest][BindingTests]")
+{
+  constexpr int N = 10;
+  constexpr int D = 2;
+  arma::mat trainX = arma::trans(arma::randu<arma::mat>(N, D));
+  arma::Row<size_t> trainY = arma::randu<arma::Row<size_t>>(N);
+  SetInputParam("training", std::move(trainX));
+  SetInputParam("labels", std::move(trainY));
+  RUN_BINDING();
+
+  arma::Row<size_t> preds;
+  arma::mat::fixed<2, 1> testX = { 0.123, 0.456 };
+  params.Get<DecisionTreeModel*>("output_model")->tree.Classify(testX, preds);
+  REQUIRE(preds.n_elem == 1);
+}
+
+/**
+ * Check that invalid parameter choices throw errors.
+ */
+TEST_CASE_METHOD(DecisionTreeTrainTestFixture,
+                 "DecisionTreeTrainParameters",
+                 "[DecisionTreeTrainMainTest][BindingTests]")
+{
+  constexpr int N = 10;
+  constexpr int D = 2;
+  arma::mat trainX = arma::trans(arma::randu<arma::mat>(N, D));
+  arma::Row<size_t> trainY = arma::randu<arma::Row<size_t>>(N);
+  SetInputParam("training", std::move(trainX));
+  SetInputParam("labels", std::move(trainY));
+
+  SetInputParam("minimum_leaf_size", (int) -1);         // Invalid.
+  REQUIRE_THROWS_AS(RUN_BINDING(), std::runtime_error);
+
+  SetInputParam("minimum_leaf_size", (int) 3);          // Now valid.
+  SetInputParam("minimum_gain_split", (double) -0.1);   // Invalid
+  REQUIRE_THROWS_AS(RUN_BINDING(), std::runtime_error);
+
+  SetInputParam("minimum_gain_split", (double) 1.1);    // Invalid
+  REQUIRE_THROWS_AS(RUN_BINDING(), std::runtime_error);
+
+  SetInputParam("minimum_gain_split", (double) 1e-7);   // Now valid.
+  SetInputParam("maximum_depth", (int) -1);             // Invalid.
+  REQUIRE_THROWS_AS(RUN_BINDING(), std::runtime_error);
+}
