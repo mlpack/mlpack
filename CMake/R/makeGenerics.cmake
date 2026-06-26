97954b191298302d9a07d0cd4b7071c647291828
Dirk Eddelbuettel <edd@debian.org>
2026-06-25 20:05:13 -0500

    Small documentation polishing

--- CMake/R/makeGenerics.cmake
+++ CMake/R/makeGenerics.cmake
@@ -3,10 +3,14 @@
 #
 # This file is called wit
 # - NAME: the binding name
-# - DIRECTORY: the methods directory name
 # - R_GENERICS_IN: the name of the base file
 # - R_GENERICS_OUT: the name of the created file
-#message(STATUS "Called Generics")
+# - METHODS: the stringified list of methods, eg "train;predict;probabilities"
+#            which is a driver of differentiation in the created code
+# - TYPES: a string with the types of supported results, usually
+#          'c("predict", "protbabilities")
+# - ELSE: the 'else' branch of code (as string) in case of probabilities
+# - ACTION: the 'action' generally of 'predict' or classify'
 
 # Now configure the file.
 configure_file("${R_GENERICS_IN}" "${R_GENERICS_OUT}")
