c462390b5a004ceee082a63d36861cfd872b8f5c
Dirk Eddelbuettel <edd@debian.org>
2026-06-26 10:32:14 -0500

    Simplify

--- CMake/R/makeGenerics.cmake
+++ CMake/R/makeGenerics.cmake
@@ -5,8 +5,8 @@
 # - NAME: the binding name
 # - R_GENERICS_IN: the name of the base file
 # - R_GENERICS_OUT: the name of the created file
-# - METHODS: the stringified list of methods, eg "train;predict;probabilities"
-#            which is a driver of differentiation in the created code
+# - VALID_METHODS: the (supplied) method list, eg "train;predict;probabilities"
+#           which is a driver of differentiation in the created code
 # - TYPES: a string with the types of supported results, usually
 #          'c("predict", "probabilities")
 # - ELSE: the 'else' branch of code (as string) in case of probabilities
