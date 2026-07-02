d84cd02472b078fbe42ae0838ab6df52703fa039
Dirk Eddelbuettel <edd@debian.org>
2026-07-02 12:37:36 -0500

    Also allow vectors to pass (Closes #4212)

--- src/mlpack/bindings/R/mlpack/R/matrix_utils.R
+++ src/mlpack/bindings/R/mlpack/R/matrix_utils.R
@@ -5,23 +5,23 @@
 # mlpack bindings(IO_SetParamMat/UMat).
 #
 # This file also defines the to_matirx_with_info() function, which can be used
-# to construct dataset information vector from the given dataset for use in 
+# to construct dataset information vector from the given dataset for use in
 # mlpack bindings(IO_SetParamMatWithInfo).
 #
-# 
+#
 # mlpack is free software; you may redistribute it and/or modify it under the
 # terms of the 3-clause BSD license.  You should have received a copy of the
 # 3-clause BSD license along with mlpack.  If not, see
 # http://www.opensource.org/licenses/BSD-3-Clause for more information.
 
 # Given some matrix-like x (which should be either a matrix or
-# data.frame), convert it into a matrix.
+# data.frame or vector), convert it into a matrix (or leave as is).
 to_matrix <- function(x) {
-  if (!is.matrix(x) && !is.data.frame(x)) {
+  if (!is.matrix(x) && !is.vector(x) && !is.data.frame(x)) {
     stop("Input must be either a `matrix` or `data.frame` not `",
         class(x)[1], "`.", call. = FALSE)
   }
-  if (is.matrix(x)) {
+  if (is.matrix(x) || is.vector(x)) {
     return(x)
   } else if (is.data.frame(x)) {
     y <- data.matrix(x) # requires R 4.0.0 for factor conversion.
