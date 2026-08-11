649e8b1902ed31b7be2ff13bfbd7407560964b53
Jagan Koipallil <106874502+NikoLaFerrari@users.noreply.github.com>
2026-08-11 09:42:58 +0300

    Linted focal_loss.hpp
    to satisfy style requirements

--- src/mlpack/methods/ann/loss_functions/focal_loss.hpp
+++ src/mlpack/methods/ann/loss_functions/focal_loss.hpp
@@ -9,7 +9,8 @@ namespace mlpack {
  * @code
  * @article{Lin2017
  *   title   = {Focal Loss for Dense Object Detection},
- *   author  = {Tsung-Yi Lin, Priya Goyal, Ross Girshick, Kaiming He, Piotr Dollár},
+ *   author  = {Tsung-Yi Lin, Priya Goyal, Ross Girshick, 
+ *              Kaiming He, Piotr Dollár},
  *   journal = {IEEE International Conference on Computer Vision (ICCV)},
  *   year    = {2017}
  * }
